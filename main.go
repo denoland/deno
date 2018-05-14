f7c5e19081920f59ce2006d3255a58fb611b6a17
Ryan Dahl <ry@tinyclouds.org>
2018-05-13 23:32:01 -0400

    Init

---
+++ main.go
@@ -0,0 +1,31 @@
+// To test: make && ./out/render test_input.js
+package main
+
+//go:generate go-bindata -pkg $GOPACKAGE -o assets.go dist/
+
+import (
+	"github.com/ry/v8worker2"
+)
+
+func recv(msg []byte) []byte {
+	println("recv cb", string(msg))
+	return nil
+}
+
+func main() {
+	indexFn := "dist/main.js"
+	data, err := Asset(indexFn)
+	if err != nil {
+		panic("asset not found")
+	}
+	code := string(data)
+
+	worker := v8worker2.New(recv)
+
+	// Load up index.js code.
+	err = worker.Load(indexFn, code)
+	if err != nil {
+		println("Problem executing Javascript.")
+		panic(err)
+	}
+}
---
+++ main.ts
@@ -0,0 +1,4 @@
+import * as ts from "typescript";
+
+
+V8Worker2.print("Hello World", ts.createProgram);
