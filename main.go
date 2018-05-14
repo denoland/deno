aba6a1dc871edbc6cbb286a350a4ba79ca645fb8
Ryan Dahl <ry@tinyclouds.org>
2018-05-14 02:50:55 -0400

    readFileSync is working

--- main.go
+++ main.go
@@ -3,12 +3,37 @@ package main
 import (
 	"github.com/golang/protobuf/proto"
 	"github.com/ry/v8worker2"
+	"io/ioutil"
 	"os"
 )
 
-func recv(msg []byte) []byte {
-	println("recv cb", string(msg))
-	return nil
+func ReadFileSync(filename string) []byte {
+	buf, err := ioutil.ReadFile(filename)
+	msg := &Msg{Kind: Msg_DATA_RESPONSE}
+	if err != nil {
+		msg.Error = err.Error()
+	} else {
+		msg.Data = buf
+	}
+	out, err := proto.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
+
+func recv(buf []byte) []byte {
+	msg := &Msg{}
+	err := proto.Unmarshal(buf, msg)
+	if err != nil {
+		panic(err)
+	}
+	switch msg.Kind {
+	case Msg_READ_FILE_SYNC:
+		return ReadFileSync(msg.Path)
+	default:
+		panic("Unexpected message")
+	}
 }
 
 func loadAsset(w *v8worker2.Worker, path string) {
--- main.ts
+++ main.ts
@@ -1,10 +1,29 @@
 //import * as ts from "typescript";
 import { main as pb } from "./msg.pb"
 import "./util";
+import { TextDecoder } from "text-encoding";
 
+function readFileSync(filename: string): string {
+  const msg = pb.Msg.fromObject({
+    kind: pb.Msg.MsgKind.READ_FILE_SYNC,
+    path: filename
+  });
+  const ui8 = pb.Msg.encode(msg).finish();
+  const ab = ui8.buffer.slice(ui8.byteOffset, ui8.byteOffset + ui8.byteLength);
+  const resBuf = V8Worker2.send(ab as ArrayBuffer);
+  const res = pb.Msg.decode(new Uint8Array(resBuf));
+  if (res.error != null && res.error.length > 0) {
+    throw Error(res.error);
+  }
+  const decoder = new TextDecoder("utf8");
+  return decoder.decode(res.data)
+}
 
 function load(argv: string[]): void {
   console.log("Load argv", argv);
+  const inputFn = argv[1];
+  const source = readFileSync(inputFn);
+  console.log("source", source)
 }
 
 V8Worker2.recv((ab: ArrayBuffer) => {
