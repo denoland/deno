ee9cfb5a60cbe68c7daa5e7edd292486e697bfa4
Ryan Dahl <ry@tinyclouds.org>
2018-05-18 11:59:05 -0400

    Add testdata/005_more_imports.ts

---
+++ testdata/subdir/mod1.ts
@@ -0,0 +1,13 @@
+import { returnsFoo, printHello2 } from "./subdir2/mod2.ts";
+
+export function returnsHi(): string {
+  return "Hi";
+}
+
+export function returnsFoo2(): string {
+  return returnsFoo();
+}
+
+export function printHello3(): void {
+  printHello2();
+}
