33f488354cf28e326d53e51edb50036005882933
Ryan Dahl <ry@tinyclouds.org>
2018-05-28 13:17:13 -0400

    Move testdata/read_file_sync.ts to tests.ts
    We don't need to start a new process for every test.

--- tests.ts
+++ tests.ts
@@ -1,5 +1,22 @@
-import { test, assert } from "./deno_testing/testing.ts";
+// This test is executed as part of integration_test.go
+// But it can also be run manually:
+//  ./deno tests.ts
+import { test, assert, assertEqual } from "./deno_testing/testing.ts";
+import { readFileSync } from "deno";
 
-test(async function test_matchTesters() {
-  assert(false, "assert failed on purpose.");
+test(async function tests_test() {
+  assert(true);
+});
+
+test(async function tests_readFileSync() {
+  let data = readFileSync("package.json");
+  if (!data.byteLength) {
+    throw Error(
+      `Expected positive value for data.byteLength ${data.byteLength}`
+    );
+  }
+  const decoder = new TextDecoder("utf-8");
+  const json = decoder.decode(data);
+  const pkg = JSON.parse(json);
+  assertEqual(pkg.name, "deno");
 });
