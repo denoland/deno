ce792dcafb10dc7cbcc01a4fe7f32d0d2f9165d8
方正 <215566435@qq.com>
2018-06-04 01:20:17 +0800

    chore: add ';' to adjust lint

--- deno.d.ts
+++ deno.d.ts
@@ -1,6 +1,6 @@
 // Copyright 2018 Ryan Dahl <ry@tinyclouds.org>
 // All rights reserved. MIT License.
-import { FileInfo } from './types'
+import { FileInfo } from './types';
 declare module "deno" {
   type MessageCallback = (msg: Uint8Array) => void;
   function sub(channel: string, cb: MessageCallback): void;
