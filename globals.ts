88929738cfbc5c6beb5710e53e2be34ceb8ebda6
zhoumingque <mail@zhoumq.cn>
2018-06-03 22:35:50 +0800

    Use the symbol classification management key

--- globals.ts
+++ globals.ts
@@ -13,17 +13,31 @@ export const globalEval = eval;
 // TODO The underscore is because it's conflicting with @types/node.
 export const _global = globalEval("this");
 
-_global["window"] = _global; // Create a window object.
+// Use the symbol classification management key
+const comm_window = Symbol.for("comm#window");
+const comm_console = Symbol.for("comm#console");
+
+const net_fetch = Symbol.for("net#fetch");
+
+const crypto_TextEncoder = Symbol.for("crypto#Textncoder");
+const crypto_TextDecoder = Symbol.for("crypto#TextDecoder");
+
+const time_setTimeout = Symbol.for("time#setTimeout");
+const time_setInterval = Symbol.for("time#setInterval");
+const time_clearTimeout = Symbol.for("time#clearTimeout");
+const time_clearInterval = Symbol.for("time#clearInterval");
+
+_global[comm_window] = _global; // Create a window object.
 import "./url";
 
-_global["setTimeout"] = timer.setTimeout;
-_global["setInterval"] = timer.setInterval;
-_global["clearTimeout"] = timer.clearTimer;
-_global["clearInterval"] = timer.clearTimer;
+_global[time_setTimeout] = timer.setTimeout;
+_global[time_setInterval] = timer.setInterval;
+_global[time_clearTimeout] = timer.clearTimer;
+_global[time_clearInterval] = timer.clearTimer;
 
 const print = V8Worker2.print;
 
-_global["console"] = {
+_global[comm_console] = {
   // tslint:disable-next-line:no-any
   log(...args: any[]): void {
     print(stringifyArgs(args));
@@ -49,8 +63,8 @@ function stringifyArgs(args: any[]): string {
 }
 
 import { fetch } from "./fetch";
-_global["fetch"] = fetch;
+_global[net_fetch] = fetch;
 
 import { TextEncoder, TextDecoder } from "text-encoding";
-_global["TextEncoder"] = TextEncoder;
-_global["TextDecoder"] = TextDecoder;
+_global[crypto_TextEncoder] = TextEncoder;
+_global[crypto_TextDecoder] = TextDecoder;
