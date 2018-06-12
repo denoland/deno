e73b2445990c69e5248d3567628293faba96b186
Ryan Dahl <ry@tinyclouds.org>
2018-06-12 03:59:22 +0200

    Format json too.

--- deno2/tools/format.sh
+++ deno2/tools/format.sh
@@ -4,4 +4,4 @@ clang-format -i -style Google *.cc *.h include/*.h
 gn format BUILD.gn
 gn format .gn
 yapf -i tools/*.py
-prettier --write js/*.ts js/*.js
+prettier --write js/*.ts js/*.js js/*.json
