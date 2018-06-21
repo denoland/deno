4ff6d3c03209bdb70f6c85eecee2a0a2d0349e54
Ryan Dahl <ry@tinyclouds.org>
2018-06-21 22:20:16 +0200

    Reorg

--- testdata/010_set_interval.ts
+++
@@ -1,7 +0,0 @@
-const id = setInterval(function() {
-  console.log("test")
-}, 200);
-
-setTimeout(function() {
-  clearInterval(id)
-}, 500)
--- testdata/async_error.ts
+++
@@ -1,9 +0,0 @@
-
-console.log("hello");
-const foo = async () => {
-  console.log("before error");
-  throw Error("error");
-}
-
-foo();
-console.log("world");
