6e1ab13cb841878dfb8e77b66f613b6bff74058c
Bert Belder <bertbelder@gmail.com>
2018-06-22 16:22:28 +0200

    Fix ctype signature in run.py

--- src/js/run_node.py
+++ src/js/run_node.py
@@ -10,15 +10,18 @@ import os
 
 def symlink(target, name, target_is_dir=False):
   if os.name == "nt":
-    from ctypes import windll, WinError
-    CreateSymbolicLinkW = windll.kernel32.CreateSymbolicLinkW
+    import ctypes
+    CreateSymbolicLinkW = ctypes.windll.kernel32.CreateSymbolicLinkW
+    CreateSymbolicLinkW.restype = ctypes.c_ubyte
+    CreateSymbolicLinkW.argtypes = (ctypes.c_wchar_p,
+                                    ctypes.c_wchar_p,
+                                    ctypes.c_uint32)
+
     flags = 0x02  # SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE
     if (target_is_dir):
       flags |= 0x01  # SYMBOLIC_LINK_FLAG_DIRECTORY
-    if not CreateSymbolicLinkW(name.encode('utf-16le'),
-                               target.encode('utf-16le'),
-                               flags):
-      raise WinError()
+    if not CreateSymbolicLinkW(name, target, flags):
+      raise ctypes.WinError()
   else:
     os.symlink(target, name)
 
