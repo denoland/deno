c4b71d942271307e0deabfce3e6a5e28f41d1004
Ryan Dahl <ry@tinyclouds.org>
2018-07-01 18:07:12 +0200

    Switch back to recv/send instead of pub/sub for low-level API.

--- js/deno.d.ts
+++ js/deno.d.ts
@@ -3,8 +3,8 @@
 type MessageCallback = (msg: ArrayBuffer) => void;
 
 interface Deno {
-  sub(channel: string, cb: MessageCallback): void;
-  pub(channel: string, msg: ArrayBuffer): null | ArrayBuffer;
+  recv(channel: string, cb: MessageCallback): void;
+  send(channel: string, msg: ArrayBuffer): null | ArrayBuffer;
   print(x: string): void;
 }
 
