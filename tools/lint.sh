235e5490357e977fe6c40a203d9ecadd744f881c
Parsa Ghadimi <me@qti3e.com>
2018-07-01 23:53:21 +0430

    Add tslint

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # TODO(ry) Rewrite this script in python for portability to Windows.
-# TODO(ry) Call tslint here too.
 set -e
 cd `dirname "$0"`/..
 ./third_party/cpplint/cpplint.py \
@@ -9,3 +8,4 @@ cd `dirname "$0"`/..
   src/*.cc \
   src/*.h \
   src/include/*.h
+node ./js/node_modules/.bin/tslint -p .
