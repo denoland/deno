65e0487ea97ef9709c2b1948fe35aaf5a7dbc04c
Yoshiya Hinosawa <stibium121@gmail.com>
2018-07-04 21:24:37 +0900

    refactor: update sync_third_party.py

--- tools/sync_third_party.py
+++ tools/sync_third_party.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
-# coding=utf-8
-#
-# build_third_party.py
-# --------------------
 # This script updates the third party dependencies of deno.
-#
 # - Get Depot Tools and make sure it's in your path.
 #   http://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html#_setting_up
 # - You need yarn installed as well.
 #   https://yarnpkg.com/lang/en/docs/install/
-#
 # Use gclient_config.py to modify the git deps.
 # Use js/package.json to modify the npm deps.
 
@@ -19,7 +13,7 @@ import argparse
 
 root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 third_party_path = os.path.join(root_path, "third_party")
-script_name = "build_third_party.py"
+script_name = "sync_third_party.py"
 
 parser = argparse.ArgumentParser(description="""
 This script updates the third party dependencies of deno.
@@ -28,13 +22,12 @@ parser.parse_args()
 
 def main():
     os.chdir(third_party_path)
-    print "cwd=" + third_party_path
     run(["gclient", "sync", "--no-history"])
     run(["yarn"])
-    print "✨  Done (" + script_name + ")"
+    print "Done (" + script_name + ")"
 
 def run(args):
-    print "+" + " ".join(args)
+    print " ".join(args)
     env = os.environ.copy()
     subprocess.check_call(args, env=env)
 
