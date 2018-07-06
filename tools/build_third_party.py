ce131b487d94ff47005af8bcd088891c91e6b1cf
Yoshiya Hinosawa <stibium121@gmail.com>
2018-07-06 09:25:38 +0900

    chore: clean up build_third_party.py

--- tools/build_third_party.py
+++ tools/build_third_party.py
@@ -9,27 +9,22 @@
 
 import os
 import subprocess
-import argparse
 
 root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 third_party_path = os.path.join(root_path, "third_party")
-script_name = "build_third_party.py"
 
-parser = argparse.ArgumentParser(description="""
-This script updates the third party dependencies of deno.
-""")
-parser.parse_args()
 
 def main():
     os.chdir(third_party_path)
     run(["gclient", "sync", "--no-history"])
     run(["yarn"])
-    print "Done (" + script_name + ")"
+
 
 def run(args):
     print " ".join(args)
     env = os.environ.copy()
     subprocess.check_call(args, env=env)
+
 
 if '__main__' == __name__:
     main()
