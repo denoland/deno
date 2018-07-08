05a4bb0cda84d5026b1e9bf54a1471dd4be3420a
Bert Belder <bertbelder@gmail.com>
2018-07-08 02:45:16 +0200

    Add 'command id' field to messages
    This allows for correlating response messages to the command message that
    caused them.

--- src/handlers.h
+++ src/handlers.h
@@ -3,7 +3,9 @@
 #ifndef HANDLERS_H_
 #define HANDLERS_H_
 extern "C" {
-void handle_code_fetch(const char* module_specifier,
+#include <stdint.h>
+
+void handle_code_fetch(uint32_t cmd_id, const char* module_specifier,
                        const char* containing_file);
 }
 #endif  // HANDLERS_H_
