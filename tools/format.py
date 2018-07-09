46db20172cc08e6427ceef9bb020f93c1e922952
Yoshiya Hinosawa <stibium121@gmail.com>
2018-07-09 14:09:56 +0900

    chore: use local prettier

--- tools/format.py
+++ tools/format.py
@@ -5,6 +5,9 @@ from glob import glob
 from util import run
 
 root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+third_party_path = os.path.join(root_path, "third_party")
+prettier = os.path.join(third_party_path, "node_modules", "prettier",
+                        "bin-prettier.js")
 
 os.chdir(root_path)
 # TODO(ry) Install clang-format in third_party.
@@ -14,12 +17,6 @@ for fn in ["BUILD.gn", ".gn"] + glob("build_extra/**/*.gn*"):
     run(["gn", "format", fn])
 # TODO(ry) Install yapf in third_party.
 run(["yapf", "-i"] + glob("tools/*.py"))
-# TODO(ry) Install prettier in third_party.
-run([
-    "prettier", "--write", "js/deno.d.ts", "js/main.ts", "js/mock_runtime.js",
-    "tsconfig.json"
-])
-# Do not format these.
-#  js/msg_generated.ts
-#  js/flatbuffers.js
+run(["node", prettier, "--write"] + glob("js/*.js") + glob("js/*.ts") +
+    ["tsconfig.json"] + ["tslint.json"])
 run(["rustfmt", "-f", "--write-mode", "overwrite"] + glob("src/*.rs"))
