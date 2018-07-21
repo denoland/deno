0cb4dd5b49efd731f7da0bf6613511a8b2c98803
Ryan Dahl <ry@tinyclouds.org>
2018-07-21 13:25:28 -0400

    Use 'filename' instead of 'fn'

--- tools/check_output_test.py
+++ tools/check_output_test.py
@@ -12,17 +12,20 @@ root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 tests_path = os.path.join(root_path, "tests")
 
 
-def check_output_test(deno_fn):
-    assert os.path.isfile(deno_fn)
-    outs = [fn for fn in os.listdir(tests_path) if fn.endswith(".out")]
+def check_output_test(deno_exe_filename):
+    assert os.path.isfile(deno_exe_filename)
+    outs = [
+        filename for filename in os.listdir(tests_path)
+        if filename.endswith(".out")
+    ]
     assert len(outs) > 1
-    tests = [(os.path.splitext(fn)[0], fn) for fn in outs]
-    for (script, out_fn) in tests:
+    tests = [(os.path.splitext(filename)[0], filename) for filename in outs]
+    for (script, out_filename) in tests:
         script_abs = os.path.join(tests_path, script)
-        out_abs = os.path.join(tests_path, out_fn)
+        out_abs = os.path.join(tests_path, out_filename)
         with open(out_abs, 'r') as f:
             expected_out = f.read()
-        cmd = [deno_fn, script_abs]
+        cmd = [deno_exe_filename, script_abs]
         print " ".join(cmd)
         actual_out = subprocess.check_output(cmd)
         if expected_out != actual_out:
