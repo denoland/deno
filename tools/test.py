2c64f27190bfb6089c026dce6ccec0779e49f45a
Ryan Dahl <ry@tinyclouds.org>
2018-07-21 18:44:04 -0400

    Improve error messages in test.py

--- tools/test.py
+++ tools/test.py
@@ -7,13 +7,34 @@ from check_output_test import check_output_test
 from util import executable_suffix, run
 
 
+def check_exists(filename):
+    if not os.path.exists(filename):
+        print "Required target doesn't exist:", filename
+        print "Build target :all"
+        sys.exit(1)
+
+
 def main(argv):
+    if len(argv) != 2:
+        print "Usage: tools/test.py [build dir]"
+        sys.exit(1)
     build_dir = argv[1]
-    os.path.isdir(build_dir)
-    run([os.path.join(build_dir, "test_cc")])
-    run([os.path.join(build_dir, "handlers_test")])
-    check_output_test(os.path.join(build_dir, "deno" + executable_suffix))
-    check_output_test(os.path.join(build_dir, "deno_ns" + executable_suffix))
+
+    test_cc = os.path.join(build_dir, "test_cc")
+    check_exists(test_cc)
+    run([test_cc])
+
+    handlers_test = os.path.join(build_dir, "handlers_test")
+    check_exists(handlers_test)
+    run([handlers_test])
+
+    deno_exe = os.path.join(build_dir, "deno" + executable_suffix)
+    check_exists(deno_exe)
+    check_output_test(deno_exe)
+
+    deno_ns_exe = os.path.join(build_dir, "deno_ns" + executable_suffix)
+    check_exists(deno_ns_exe)
+    check_output_test(deno_ns_exe)
 
 
 if __name__ == '__main__':
