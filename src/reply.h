d79fd48048f40380872b07ad3b3fc703ef0997a9
Ryan Dahl <ry@tinyclouds.org>
2018-07-23 14:13:12 -0400

    Remove handlers crate target.
    - Add build scripts for Rust flatbuffers.
    - Rewrites some reply.cc methods in Rust.
    - Changes some struct elements to table in msg.fbs (rust flatbuffers
      lacks support currently)
    - Renames handlers_test to test_rs.
    - This reorg is needed to make progress on the code cache handler.

--- src/reply.h
+++ src/reply.h
@@ -12,16 +12,6 @@
 #include "deno.h"
 
 extern "C" {
-
-void deno_reply_null(Deno* d, uint32_t cmd_id);
-void deno_reply_error(Deno* d, uint32_t cmd_id, const char* error_msg);
-
-void deno_reply_start(Deno* d, uint32_t cmd_id, int argc, char* argv[],
-                      char* cwd);
-void deno_reply_code_fetch(Deno* d, uint32_t cmd_id, const char* module_name,
-                           const char* filename, const char* source_code,
-                           const char* output_code);
-
 // Parse incoming messages with C++ Flatbuffers, call into rust handlers.
 void deno_handle_msg_from_js(Deno* d, deno_buf buf);
 }
