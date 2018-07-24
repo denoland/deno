d0939a00ed28fb9370544af3a0503ecf77969971
Ryan Dahl <ry@tinyclouds.org>
2018-07-24 13:42:23 -0400

    Add tools/build.py (#398)
    To allow better tab completion for ./tools/build.py
    mv build_third_party.py sync_third_party.py

--- tools/run_hooks.py
+++ tools/run_hooks.py
@@ -9,14 +9,14 @@ depot_tools_path = os.path.join(third_party_path, "depot_tools")
 os.chdir(root_path)
 
 
-def download(fn):
+def download(filename):
     run([
+        "python",
         os.path.join(depot_tools_path + '/download_from_google_storage.py'),
-        '--no_resume', '--platform=' + sys.platform, '--no_auth', '--bucket',
-        'chromium-gn', '-s',
-        os.path.join(root_path, fn)
-    ],
-        quiet=True)
+        '--platform=' + sys.platform, '--no_auth', '--bucket=chromium-gn',
+        '--sha1_file',
+        os.path.join(root_path, filename)
+    ])
 
 
 if sys.platform == 'win32':
@@ -25,5 +25,5 @@ elif sys.platform == 'darwin':
     download("third_party/v8/buildtools/mac/gn.sha1")
 elif sys.platform.startswith('linux'):
     download("third_party/v8/buildtools/linux64/gn.sha1")
-run(['python', 'third_party/v8/tools/clang/scripts/update.py', '--if-needed'],
-    quiet=True)
+
+run(['python', 'third_party/v8/tools/clang/scripts/update.py', '--if-needed'])
