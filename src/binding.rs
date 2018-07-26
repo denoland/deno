ddc2f284b5e210d7a341f02a32f10ce97f540100
Ryan Dahl <ry@tinyclouds.org>
2018-07-26 17:47:42 -0400

    Add from_c() to get a Deno object from ptr.
    This is a utility function for CodeCache and other handlers.

--- src/binding.rs
+++ src/binding.rs
@@ -27,6 +27,7 @@ extern "C" {
   pub fn deno_new(data: *const c_void, cb: DenoRecvCb) -> *const DenoC;
   pub fn deno_delete(d: *const DenoC);
   pub fn deno_last_exception(d: *const DenoC) -> *const c_char;
+  pub fn deno_get_data(d: *const DenoC) -> *const c_void;
   pub fn deno_set_response(d: *const DenoC, buf: deno_buf);
   pub fn deno_execute(
     d: *const DenoC,
