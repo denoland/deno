d0ecf4881f119610ccae0aca593aeb8dd087aa49
Li Hao <hao.x.li@intel.com>
2018-08-07 14:31:34 +0800

    Update tools/*.py for python3
    - Address #464
    - Fix missing sys import in setup.py
    
    Signed-off-by: Li Hao <hao.x.li@intel.com>

--- tools/build.py
+++ tools/build.py
@@ -8,9 +8,9 @@ from util import root_path, run, run_output, build_path
 
 third_party.fix_symlinks()
 
-print "DENO_BUILD_PATH:", build_path()
+print("DENO_BUILD_PATH: %s" % build_path())
 if not os.path.isdir(build_path()):
-    print "DENO_BUILD_PATH does not exist. Run tools/setup.py"
+    print("DENO_BUILD_PATH does not exist. Run tools/setup.py")
     sys.exit(1)
 os.chdir(build_path())
 
@@ -19,10 +19,10 @@ def maybe_add_default_target(args):
     lines = run_output(
         [third_party.ninja_path, "-t", "targets"],
         env=third_party.google_env(),
-        quiet=True).split("\n")
-    targets = [l.rsplit(":", 1)[0] for l in lines]
-    deno_targets = [target for target in targets if target.startswith(":")]
-    deno_targets += [target.lstrip(":") for target in deno_targets]
+        quiet=True).split(b"\n")
+    targets = [l.rsplit(b":", 1)[0] for l in lines]
+    deno_targets = [target for target in targets if target.startswith(b":")]
+    deno_targets += [target.lstrip(b":") for target in deno_targets]
 
     target_specified = False
     for a in args:
