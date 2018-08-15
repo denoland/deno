f7fd2389533f0ae387af30ea63755b67a6484e51
Ryan Dahl <ry@tinyclouds.org>
2018-08-15 19:08:03 -0400

    Enable http server for tests.

---
+++ tools/http_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Many tests expect there to be an http server on port 4545 servering the deno
+# root directory.
+import os
+from threading import Thread
+import SimpleHTTPServer
+import SocketServer
+from util import root_path
+from time import sleep
+
+PORT = 4545
+
+
+def serve_forever():
+    os.chdir(root_path)  # Hopefully the main thread doesn't also chdir.
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+    print "Deno test server http://localhost:%d/" % PORT
+    httpd.serve_forever()
+
+
+def spawn():
+    thread = Thread(target=serve_forever)
+    thread.daemon = True
+    thread.start()
+    sleep(1)  # TODO I'm too lazy to figure out how to do this properly.
+    return thread
+
+
+if __name__ == '__main__':
+    serve_forever()
