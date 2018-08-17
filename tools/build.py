be0aa4edafce3b261da9c7207464fb15d248b9e0
Ryan Dahl <ry@tinyclouds.org>
2018-08-17 18:23:14 -0400

    Rename :all target to :default

--- tools/build.py
+++ tools/build.py
@@ -14,27 +14,7 @@ if not os.path.isdir(build_path()):
     sys.exit(1)
 os.chdir(build_path())
 
-
-def maybe_add_default_target(args):
-    lines = run_output(
-        [third_party.ninja_path, "-t", "targets"],
-        env=third_party.google_env(),
-        quiet=True).split("\n")
-    targets = [l.rsplit(":", 1)[0] for l in lines]
-    deno_targets = [target for target in targets if target.startswith(":")]
-    deno_targets += [target.lstrip(":") for target in deno_targets]
-
-    target_specified = False
-    for a in args:
-        if a in deno_targets:
-            target_specified = True
-            break
-    if not target_specified:
-        args += [":all"]
-    return args
-
-
-ninja_args = maybe_add_default_target(sys.argv[1:])
+ninja_args = sys.argv[1:]
 
 run([third_party.ninja_path] + ninja_args,
     env=third_party.google_env(),
