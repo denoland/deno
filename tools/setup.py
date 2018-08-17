67d89ff692a88ae42145043eac1a5c58413ae110
Yoshiya Hinosawa <stibium121@gmail.com>
2018-08-17 12:44:55 +0900

    chore: download clang-format from google storage

--- tools/setup.py
+++ tools/setup.py
@@ -7,6 +7,7 @@ import distutils.spawn
 
 third_party.fix_symlinks()
 third_party.download_gn()
+third_party.download_clang_format()
 third_party.download_clang()
 third_party.maybe_download_sysroot()
 
