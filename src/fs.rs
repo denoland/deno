a762ea26f139bf7a41c746e2a963bbbb3f94bf3b
Ryan Dahl <ry@tinyclouds.org>
2018-08-22 13:19:32 -0400

    Implement writeFileSync
    In collaboration with Tommy Savaria <tommy.savaria@protonmail.ch>

--- src/fs.rs
+++ src/fs.rs
@@ -1,6 +1,7 @@
 use std;
 use std::fs::File;
 use std::io::Read;
+use std::io::Write;
 use std::path::Path;
 
 pub fn read_file_sync(path: &Path) -> std::io::Result<Vec<u8>> {
@@ -15,6 +16,11 @@ pub fn read_file_sync_string(path: &Path) -> std::io::Result<String> {
   let vec = read_file_sync(path)?;
   String::from_utf8(vec)
     .map_err(|err| std::io::Error::new(std::io::ErrorKind::InvalidData, err))
+}
+
+pub fn write_file_sync(path: &Path, content: &[u8]) -> std::io::Result<()> {
+  let mut f = File::create(path)?;
+  f.write_all(content)
 }
 
 pub fn mkdir(path: &Path) -> std::io::Result<()> {
