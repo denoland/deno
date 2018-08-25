7dee3bcab59506d3c9c21a2f2aa9355414a0afbc
Bert Belder <bertbelder@gmail.com>
2018-08-26 01:37:15 +0200

    libdeno: add file utilities Dirname() and ExePath()

--- libdeno/file_util.h
+++ libdeno/file_util.h
@@ -7,6 +7,8 @@
 namespace deno {
 bool ReadFileToString(const char* fn, std::string* contents);
 std::string Basename(std::string const& filename);
+std::string Dirname(std::string const& filename);
+bool ExePath(std::string* path);
 }  // namespace deno
 
 #endif  // FILE_UTIL_H_
--- libdeno/file_util_test.cc
+++ libdeno/file_util_test.cc
@@ -11,11 +11,36 @@ TEST(FileUtilTest, ReadFileToStringFileNotExist) {
 TEST(FileUtilTest, Basename) {
   EXPECT_EQ("foo.txt", deno::Basename("foo.txt"));
   EXPECT_EQ("foo.txt", deno::Basename("/foo.txt"));
+  EXPECT_EQ("", deno::Basename("/foo/"));
+  EXPECT_EQ("", deno::Basename("foo/"));
   EXPECT_EQ("", deno::Basename("/"));
   EXPECT_EQ("foo.txt", deno::Basename(".\\foo.txt"));
   EXPECT_EQ("foo.txt", deno::Basename("/home/ryan/foo.txt"));
   EXPECT_EQ("foo.txt", deno::Basename("C:\\home\\ryan\\foo.txt"));
 }
 
-// TODO(ry) success unit test. Needs a tempfile or fixture.
-// TEST(FileUtilTest, ReadFileToStringSuccess) { }
+TEST(FileUtilTest, Dirname) {
+  EXPECT_EQ("home/dank/", deno::Dirname("home/dank/memes.gif"));
+  EXPECT_EQ("/home/dank/", deno::Dirname("/home/dank/memes.gif"));
+  EXPECT_EQ("/home/dank/", deno::Dirname("/home/dank/"));
+  EXPECT_EQ("home/dank/", deno::Dirname("home/dank/memes.gif"));
+  EXPECT_EQ("/", deno::Dirname("/"));
+  EXPECT_EQ(".\\", deno::Dirname(".\\memes.gif"));
+  EXPECT_EQ("c:\\", deno::Dirname("c:\\stuff"));
+  EXPECT_EQ("./", deno::Dirname("nothing"));
+  EXPECT_EQ("./", deno::Dirname(""));
+}
+
+TEST(FileUtilTest, ExePath) {
+  std::string exe_path;
+  EXPECT_TRUE(deno::ExePath(&exe_path));
+  // Path is absolute.
+  EXPECT_TRUE(exe_path.find("/") == 0 || exe_path.find(":\\") == 1);
+  // FIlename is the name of the test binary.
+  std::string exe_filename = deno::Basename(exe_path);
+  EXPECT_EQ(exe_filename.find("test_cc"), 0);
+  // Path exists (also tests ReadFileToString).
+  std::string contents;
+  EXPECT_TRUE(deno::ReadFileToString(exe_path.c_str(), &contents));
+  EXPECT_NE(contents.find("Inception :)"), std::string::npos);
+}
