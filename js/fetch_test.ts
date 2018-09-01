bbc4e55bdae2d37fee92e778f4a13eaf1d4fb4e1
ShinDarth <borzifrancesco@gmail.com>
2018-09-02 00:49:47 +0200

    Minor code refactoring

--- js/fetch_test.ts
+++ js/fetch_test.ts
@@ -1,5 +1,5 @@
 // Copyright 2018 the Deno authors. All rights reserved. MIT license.
-import { test, testPerm, assert, assertEqual } from "./test_util.ts";
+import { testPerm, assertEqual } from "./test_util.ts";
 import * as deno from "deno";
 
 testPerm({ net: true }, async function fetchJsonSuccess() {
