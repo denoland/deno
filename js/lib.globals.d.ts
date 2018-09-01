6b1360b2b5a6ae8af598b42299b02dbc5c96e15e
Kitson Kelly <me@kitsonkelly.com>
2018-09-01 10:47:13 -0700

    Remove lib.globals.d.ts

--- js/lib.globals.d.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2018 the Deno authors. All rights reserved. MIT license.
-// This file contains the default TypeScript libraries for the deno runtime.
-/// <reference no-default-lib="true"/>
-/// <reference lib="esnext" />
-/// <reference path="globals.d.ts"/>
