98bc2b311bbe0cff5d4aa996535adec581f0ec01
jtenner <tenner.joshua@gmail.com>
2018-09-04 13:44:28 -0400

    Delete test.ts

--- test.ts
+++
@@ -1,5 +0,0 @@
-import * as deno from "deno";
-console.log({ a: 1, b: 2, c: "Testing nested string blah blah blahsfojhsdkjfhsdjkfhslkdjfblsdkjgbldsfkgblsdfkgblsdfjgbsdfjgbljsdfb" });
-
-//const result = deno.inspect({ a: 1, b: 2 });
-//console.log(result);
