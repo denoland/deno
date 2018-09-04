7258aac112c3d8bdb19fde47121e695226177b17
Ryan Dahl <ry@tinyclouds.org>
2018-09-04 16:32:08 -0400

    Upgrade Flatbuffers (now using master)

--- gclient_config.py
+++ gclient_config.py
@@ -34,11 +34,8 @@ solutions = [{
     'name':
     'cpplint'
 }, {
-    # Tracking a bleeding-edge branch. Upgrade frequently.
-    # https://github.com/google/flatbuffers/pull/4898
-    # https://github.com/rw/flatbuffers/tree/2018-08-28--rw-flatbuffers-rust
     'url':
-    'https://github.com/rw/flatbuffers.git@93f91622561907e2e51662be897631b5e0ed5634',
+    'https://github.com/google/flatbuffers.git@919c929d30f3e344537793794cdae8b204d40f32',
     'name':
     'flatbuffers'
 }]
