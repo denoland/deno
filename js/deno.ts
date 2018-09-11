1ffae651655746b95dd40207d91ba7c360b90c40
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-09-10 20:40:03 -0700

    Add remove(), removeAll().
    and removeSync(), removeAllSync().

--- js/deno.ts
+++ js/deno.ts
@@ -1,13 +1,9 @@
 // Copyright 2018 the Deno authors. All rights reserved. MIT license.
 // Public deno module.
 /// <amd-module name="deno"/>
-export {
-  env,
-  exit,
-  makeTempDirSync,
-  renameSync,
-} from "./os";
+export { env, exit, makeTempDirSync, renameSync } from "./os";
 export { mkdirSync, mkdir } from "./mkdir";
+export { removeSync, remove, removeAllSync, removeAll } from "./remove";
 export { readFileSync, readFile } from "./read_file";
 export { FileInfo, statSync, lstatSync, stat, lstat } from "./stat";
 export { writeFileSync, writeFile } from "./write_file";
