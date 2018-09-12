41c70b154f4dd9adf6df875c653bcf8701511ff0
qti3e <me@qti3e.com>
2018-09-12 23:46:42 +0430

    Add support for fetch() headers (#727)

--- js/deno.ts
+++ js/deno.ts
@@ -1,11 +1,7 @@
 // Copyright 2018 the Deno authors. All rights reserved. MIT license.
 // Public deno module.
 /// <amd-module name="deno"/>
-export {
-  env,
-  exit,
-  makeTempDirSync
-} from "./os";
+export { env, exit, makeTempDirSync } from "./os";
 export { mkdirSync, mkdir } from "./mkdir";
 export { removeSync, remove, removeAllSync, removeAll } from "./remove";
 export { readFileSync, readFile } from "./read_file";
