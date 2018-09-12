24e5e1ec984b79943ad8359ce5c3c8fa168d05a5
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-09-11 22:31:17 -0700

    Restore mode

--- js/mkdir_test.ts
+++ js/mkdir_test.ts
@@ -9,6 +9,19 @@ testPerm({ write: true }, function mkdirSyncSuccess() {
   assert(pathInfo.isDirectory());
 });
 
+// testPerm({ write: true }, function mkdirSyncMode() {
+//   const path = deno.makeTempDirSync() + "/dir/subdir";
+//   deno.mkdirSync(path, 0o755); // no perm for x
+//   let err;
+//   try {
+//     deno.statSync(path);
+//   } catch (e) {
+//     err = e;
+//   }
+//   assertEqual(err.kind, deno.ErrorKind.PermissionDenied);
+//   assertEqual(err.name, "PermissionDenied");
+// });
+
 testPerm({ write: false }, function mkdirSyncPerm() {
   let err;
   try {
