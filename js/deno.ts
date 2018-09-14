a0f0d54effd3857e4e573f4db9419aabb2293bee
ztplz <mysticzt@gmail.com>
2018-09-14 11:53:18 +0800

    fix build error

--- js/deno.ts
+++ js/deno.ts
@@ -1,12 +1,14 @@
 // Copyright 2018 the Deno authors. All rights reserved. MIT license.
 // Public deno module.
 /// <amd-module name="deno"/>
+import { FileInfo, statSync, lstatSync, stat, lstat } from './stat';
+
 export { env, exit, makeTempDirSync } from "./os";
 export { mkdirSync, mkdir } from "./mkdir";
 export { removeSync, remove, removeAllSync, removeAll } from "./remove";
 export { readFileSync, readFile } from "./read_file";
 export { renameSync, rename } from "./rename";
-export { FileInfo, statSync, lstatSync, stat, lstat } from "./stat";
+export { FileInfo, statSync, lstatSync, stat, lstat };
 export { writeFileSync, writeFile } from "./write_file";
 export { ErrorKind, DenoError } from "./errors";
 export { libdeno } from "./libdeno";
