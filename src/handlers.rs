194aaac449be817688cda81ee06deda9602970b5
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-09-16 21:47:34 -0700

    Revert rust format effect that introduces unexpected code change

--- src/handlers.rs
+++ src/handlers.rs
@@ -288,7 +288,8 @@ fn handle_env(d: *const DenoC, base: &msg::Base) -> Box<Op> {
           ..Default::default()
         },
       )
-    }).collect();
+    })
+    .collect();
   let tables = builder.create_vector(&vars);
   let msg = msg::EnvironRes::create(
     builder,
@@ -401,7 +402,8 @@ where
     .and_then(|_| {
       cb();
       Ok(())
-    }).select(cancel_rx)
+    })
+    .select(cancel_rx)
     .map(|_| ())
     .map_err(|_| ());
 
