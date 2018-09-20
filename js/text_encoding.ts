8a175e481fdebb99a40c0150e4a48d513b77b9a0
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-09-20 13:53:10 -0700

    Add atob/btoa based on base64-js

--- js/text_encoding.ts
+++ js/text_encoding.ts
@@ -1,4 +1,50 @@
 // Copyright 2018 the Deno authors. All rights reserved. MIT license.
+import * as base64 from "base64-js";
+import { InvalidCharacterError } from "./errors";
+
+export function atob(s: string): string {
+  const rem = s.length % 4;
+  // base64-js requires length exactly times of 4
+  if (rem > 0) {
+    s = s.padEnd(s.length + (4 - rem), "=");
+  }
+  let byteArray;
+  try {
+    byteArray = base64.toByteArray(s);
+  } catch (_) {
+    throw new InvalidCharacterError(
+      "The string to be decoded is not correctly encoded"
+    );
+  }
+  let result = "";
+  for (let i = 0; i < byteArray.length; i++) {
+    result += String.fromCharCode(byteArray[i]);
+  }
+  return result;
+}
+
+export function btoa(s: string): string {
+  const byteArray = [];
+  for (let i = 0; i < s.length; i++) {
+    const charCode = s[i].charCodeAt(0);
+    if (charCode > 0xff) {
+      throw new InvalidCharacterError(
+        "'btoa' failed: The string to be encoded contains " +
+          "characters outside of the Latin1 range."
+      );
+    }
+    byteArray.push(charCode);
+  }
+  let result;
+  try {
+    result = base64.fromByteArray(Uint8Array.from(byteArray));
+  } catch (_) {
+    throw new InvalidCharacterError(
+      "The string to be decoded is not correctly encoded"
+    );
+  }
+  return result;
+}
 
 // @types/text-encoding relies on lib.dom.d.ts for some interfaces. We do not
 // want to include lib.dom.d.ts (due to size) into deno's global type scope.
