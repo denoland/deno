f4f2da0a716ee78eeef1b51b34ab9ab9b4ce9a7d
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-09-24 10:57:03 -0700

    Add copyright header and comments

--- js/v8_source_maps_test.ts
+++ js/v8_source_maps_test.ts
@@ -1,5 +1,8 @@
+// Copyright 2018 the Deno authors. All rights reserved. MIT license.
 import { test, assert, assertEqual } from "./test_util.ts";
 
+// This test demonstrates a bug:
+// https://github.com/denoland/deno/issues/808
 test(function evalErrorFormatted() {
   let err;
   try {
