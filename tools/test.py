a7a5d94138a05bb1df79e1a02f6ded02d8abfd32
Ryan Dahl <ry@tinyclouds.org>
2018-09-24 17:51:36 -0400

    Re-order test.py

--- tools/test.py
+++ tools/test.py
@@ -31,9 +31,15 @@ def main(argv):
 
     http_server.spawn()
 
+    deno_exe = os.path.join(build_dir, "deno" + executable_suffix)
+    check_exists(deno_exe)
+    deno_ns_exe = os.path.join(build_dir, "deno_ns" + executable_suffix)
+    check_exists(deno_ns_exe)
+
     # Internal tools testing
     setup_test()
     util_test()
+    benchmark_test(deno_exe)
 
     test_cc = os.path.join(build_dir, "test_cc" + executable_suffix)
     check_exists(test_cc)
@@ -43,19 +49,10 @@ def main(argv):
     check_exists(test_rs)
     run([test_rs])
 
-    deno_exe = os.path.join(build_dir, "deno" + executable_suffix)
-    check_exists(deno_exe)
     unit_tests(deno_exe)
 
-    check_exists(deno_exe)
     check_output_test(deno_exe)
-
-    deno_ns_exe = os.path.join(build_dir, "deno_ns" + executable_suffix)
-    check_exists(deno_ns_exe)
     check_output_test(deno_ns_exe)
-
-    check_exists(deno_exe)
-    benchmark_test(deno_exe)
 
 
 if __name__ == '__main__':
