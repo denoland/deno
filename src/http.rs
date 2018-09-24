7c128df4a041f3b2c04725a0f5f3320db684d067
Ryan Dahl <ry@tinyclouds.org>
2018-09-24 19:51:37 -0400

    Use lazy_static for HttpsConnector
    And rename net.rs to http.rs
    
    Share HTTP connection.

--- src/http.rs
+++ src/http.rs
@@ -1,21 +1,37 @@
+// Copyright 2018 the Deno authors. All rights reserved. MIT license.
+
 use errors::DenoResult;
+
+use futures::Future;
+use futures::Stream;
 use hyper;
-use hyper::rt::{Future, Stream};
-use hyper::{Client, Uri};
+use hyper::client::HttpConnector;
+use hyper::Uri;
 use hyper_rustls;
-use tokio::runtime::current_thread::Runtime;
+
+type Connector = hyper_rustls::HttpsConnector<HttpConnector>;
+
+lazy_static! {
+  static ref CONNECTOR: Connector = {
+    let num_dns_threads = 4;
+    Connector::new(num_dns_threads)
+  };
+}
+
+pub fn get_client() -> Client<Connector, hyper::Body> {
+  // TODO use Hyper's connection pool.
+  let c = CONNECTOR.clone();
+  Client::builder().build(c)
+}
 
 // The CodeFetch message is used to load HTTP javascript resources and expects a
 // synchronous response, this utility method supports that.
 pub fn fetch_sync_string(module_name: &str) -> DenoResult<String> {
   let url = module_name.parse::<Uri>().unwrap();
-
-  let https = hyper_rustls::HttpsConnector::new(4);
-  let client: Client<_, hyper::Body> = Client::builder().build(https);
+  let client = get_client();
 
   // TODO Use Deno's RT
   let mut rt = Runtime::new().unwrap();
-
   let body = rt.block_on(
     client
       .get(url)
