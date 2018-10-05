6a649012bcd780f4b3adfa55647772fce0012fde
Chris Bystrek <chris.bystrek@gmail.com>
2018-10-05 07:29:55 -0400

    Changed tools/lint.py to lint the entire js and tests directories. (#900)
    * Changed tools/lint.py to lint the entire js and tests directorys and sub directories, currently it was pointing at tsconfig and would only lint files that were part of js/main.ts or node_modules/typescript/lib/lib.esnext.d.ts and their dependencies
    
    * Broke the typescript linting out into separate steps for the main typescript programing and tests.
    
    * Fixed linting issues in ts tests.

--- tests/cat.ts
+++ tests/cat.ts
@@ -2,8 +2,8 @@ import { stdout, open, copy, args } from "deno";
 
 async function main() {
   for (let i = 1; i < args.length; i++) {
-    let filename = args[i];
-    let file = await open(filename);
+    const filename = args[i];
+    const file = await open(filename);
     await copy(stdout, file);
   }
 }
