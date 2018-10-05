4532b0706773b4976689c0b67c7b07c14febe410
Ryan Dahl <ry@tinyclouds.org>
2018-10-05 17:41:49 -0400

    Store relative, not absolute, timeout in Isolate
    * Two fewer syscalls.
    * Simpler and matches the interface to epoll_wait().
    * Potentially fixes #919.

--- src/isolate.rs
+++ src/isolate.rs
@@ -14,12 +14,10 @@ use libc::c_void;
 use std;
 use std::ffi::CStr;
 use std::ffi::CString;
-use std::sync::atomic;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
-use std::time::Instant;
 use tokio;
 use tokio_util;
 
@@ -45,7 +43,7 @@ pub struct Isolate {
   dispatch: Dispatch,
   rx: mpsc::Receiver<(i32, Buf)>,
   ntasks: i32,
-  pub timeout_due: Option<Instant>,
+  pub timeout: Option<Duration>,
   pub state: Arc<IsolateState>,
 }
 
@@ -87,7 +85,7 @@ impl Isolate {
       dispatch,
       rx,
       ntasks: 0,
-      timeout_due: None,
+      timeout: None,
       state: Arc::new(IsolateState {
         dir: deno_dir::DenoDir::new(flags.reload, None).unwrap(),
         argv: argv_rest,
@@ -160,26 +158,14 @@ impl Isolate {
   pub fn event_loop(&mut self) {
     // Main thread event loop.
     while !self.is_idle() {
-      // Ideally, mpsc::Receiver would have a receive method that takes a optional
-      // timeout. But it doesn't so we need all this duplicate code.
-      match self.timeout_due {
-        Some(due) => {
-          // Subtracting two Instants causes a panic if the resulting duration
-          // would become negative. Avoid this.
-          let now = Instant::now();
-          let timeout = if due > now {
-            due - now
-          } else {
-            Duration::new(0, 0)
-          };
-          // TODO: use recv_deadline() instead of recv_timeout() when this
-          // feature becomes stable/available.
-          match self.rx.recv_timeout(timeout) {
-            Ok((req_id, buf)) => self.complete_op(req_id, buf),
-            Err(mpsc::RecvTimeoutError::Timeout) => self.timeout(),
-            Err(e) => panic!("mpsc::Receiver::recv_timeout() failed: {:?}", e),
-          }
-        }
+      // Ideally, mpsc::Receiver would have a receive method that takes a
+      // optional timeout. But it doesn't so we need all this duplicate code.
+      match self.timeout {
+        Some(t) => match self.rx.recv_timeout(t) {
+          Ok((req_id, buf)) => self.complete_op(req_id, buf),
+          Err(mpsc::RecvTimeoutError::Timeout) => self.timeout(),
+          Err(e) => panic!("mpsc::Receiver::recv_timeout() failed: {:?}", e),
+        },
         None => match self.rx.recv() {
           Ok((req_id, buf)) => self.complete_op(req_id, buf),
           Err(e) => panic!("mpsc::Receiver::recv() failed: {:?}", e),
@@ -194,20 +180,13 @@ impl Isolate {
   }
 
   fn ntasks_decrement(&mut self) {
-    // Do something that has no effect. This is done to work around a spooky
-    // bug that happens in release mode only (presumably a compiler bug), that
-    // causes nsize to unexpectedly contain zero.
-    // TODO: remove this workaround when no longer necessary.
-    #[allow(unused)]
-    static UNUSED: atomic::AtomicIsize = atomic::AtomicIsize::new(0);
-    UNUSED.fetch_add(self.ntasks as isize, atomic::Ordering::AcqRel);
     // Actually decrement the tasks counter here.
     self.ntasks = self.ntasks - 1;
     assert!(self.ntasks >= 0);
   }
 
   fn is_idle(&self) -> bool {
-    self.ntasks == 0 && self.timeout_due.is_none()
+    self.ntasks == 0 && self.timeout.is_none()
   }
 }
 
