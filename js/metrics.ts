5e891ddfbebbf7d0944ffae592a0cd8a13ddb9db
Bartek Iwańczuk <biwanczuk@gmail.com>
2018-10-05 19:24:31 +0200

    add 'metrics' handler to deno

---
+++ js/metrics.ts
@@ -0,0 +1,33 @@
+// Copyright 2018 the Deno authors. All rights reserved. MIT license.
+import * as msg from "gen/msg_generated";
+import { flatbuffers } from "flatbuffers";
+import { assert } from "./util";
+import * as dispatch from "./dispatch";
+
+interface DenoMetrics {
+    opsExecuted: number;
+    bytesRecv: number;
+    bytesSent: number;
+}
+
+export function metrics(): DenoMetrics {
+    return res(dispatch.sendSync(...req()));
+}
+
+function req(): [flatbuffers.Builder, msg.Any, flatbuffers.Offset] {
+    const builder = new flatbuffers.Builder();
+    msg.Metrics.startMetrics(builder);
+    const inner = msg.Metrics.endMetrics(builder);
+    return [builder, msg.Any.Metrics, inner];
+}
+
+function res(baseRes: null | msg.Base): DenoMetrics {
+    assert(baseRes !== null);
+    assert(msg.Any.MetricsRes === baseRes!.innerType());
+    const res = new msg.MetricsRes();
+    assert(baseRes!.inner(res) !== null);
+    const opsExecuted = res.opsExecuted();
+    const bytesSent = res.bytesSent();
+    const bytesRecv = res.bytesRecv();
+    return { opsExecuted, bytesRecv, bytesSent };
+}
