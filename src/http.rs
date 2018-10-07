9ad8bdc63e7f35470dcd04ca3e5b50285e2568c1
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-10-07 10:53:50 -0700

    Add redirect follow feature

--- src/http.rs
+++ src/http.rs
@@ -4,12 +4,10 @@ use errors;
 use errors::{DenoError, DenoResult};
 use tokio_util;
 
-use futures;
-use futures::future::Either;
+use futures::future::{loop_fn, Loop};
 use futures::{Future, Stream};
 use hyper;
-use hyper::client::Client;
-use hyper::client::HttpConnector;
+use hyper::client::{Client, HttpConnector};
 use hyper::Uri;
 use hyper_rustls;
 
@@ -33,24 +31,53 @@ pub fn get_client() -> Client<Connector, hyper::Body> {
 pub fn fetch_sync_string(module_name: &str) -> DenoResult<String> {
   let url = module_name.parse::<Uri>().unwrap();
   let client = get_client();
-  let fetch_future = client
-    .get(url)
-    .map_err(|err| DenoError::from(err))
-    .and_then(|response| {
-      if !response.status().is_success() {
-        return Either::A(futures::future::err(errors::new(
-          errors::ErrorKind::NotFound,
-          "module not found".to_string(),
-        )));
-      }
-      Either::B(
-        response
-          .into_body()
-          .concat2()
-          .map(|body| String::from_utf8(body.to_vec()).unwrap())
-          .map_err(|err| DenoError::from(err)),
-      )
-    });
+  type FetchStringTuple = (
+    Client<Connector, hyper::Body>,
+    Option<hyper::Uri>,
+    Option<hyper::Response<hyper::Body>>,
+  );
+  let fetch_future = loop_fn(
+    (client, Some(url), None),
+    |(client, maybe_url, _): FetchStringTuple| {
+      let url = maybe_url.expect("target url should not be None");
+      client
+        .get(url)
+        .map_err(|err| DenoError::from(err))
+        .and_then(|response| {
+          if response.status().is_redirection() {
+            let new_url_string = response
+              .headers()
+              .get("location")
+              .expect("url redirection should provide 'location' header")
+              .to_str()
+              .unwrap()
+              .to_string();
+            println!("Redirecting to {}...", &new_url_string);
+            let new_url = Some(
+              new_url_string
+                .parse::<Uri>()
+                .expect("provided redirect url should be a valid url"),
+            );
+            return Ok(Loop::Continue((client, new_url, None)));
+          }
+          if !response.status().is_success() {
+            return Err(errors::new(
+              errors::ErrorKind::NotFound,
+              "module not found".to_string(),
+            ));
+          }
+          Ok(Loop::Break((client, None, Some(response))))
+        })
+    },
+  ).and_then(|(_, _, maybe_response): FetchStringTuple| {
+    maybe_response
+      .expect("Should contain a valid response")
+      .into_body()
+      .concat2()
+      .map(|body| String::from_utf8(body.to_vec()).unwrap())
+      .map_err(|err| DenoError::from(err))
+  });
+
   tokio_util::block_on(fetch_future)
 }
 
