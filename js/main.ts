b78bd390203e65e7b6f017a0a592bb5ca50464fc
cedric05 <kesavararapu.siva@gmail.com>
2018-10-09 08:06:04 +0530

    repl loop inside deno, initial phase

--- js/main.ts
+++ js/main.ts
@@ -57,13 +57,14 @@ export default function denoMain() {
   }
   log("args", args);
   Object.freeze(args);
-
+  console.log('hahahaha')
   const inputFn = args[0];
   if (!inputFn) {
+    console.log("not exiting")
     console.log("No input script specified.");
-    os.exit(1);
+    // os.exit(1);
   }
-
+ else {
   const printDeps = startResMsg.depsFlag();
   if (printDeps) {
     for (const dep of compiler.getModuleDependencies(inputFn, `${cwd}/`)) {
@@ -74,4 +75,5 @@ export default function denoMain() {
 
   compiler.recompile = startResMsg.recompileFlag();
   compiler.run(inputFn, `${cwd}/`);
+}
 }
