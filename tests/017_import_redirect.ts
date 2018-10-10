888824c61787edb4a1a0d4141f6c08855d87d2b7
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-10-09 17:31:06 -0700

    Add redirect follow feature (#934)

---
+++ tests/017_import_redirect.ts
@@ -0,0 +1,5 @@
+// http -> https redirect would happen:
+// tslint:disable-next-line:max-line-length
+import { printHello } from "http://gist.githubusercontent.com/ry/f12b2aa3409e6b52645bc346a9e22929/raw/79318f239f51d764384a8bded8d7c6a833610dde/print_hello.ts";
+
+printHello();
