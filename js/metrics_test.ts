d510c766c68ba3b166a1a311e59bdf3263d37cb6
Bartek Iwańczuk <biwanczuk@gmail.com>
2018-10-10 22:31:23 +0200

    update test for metrics

--- js/metrics_test.ts
+++ js/metrics_test.ts
@@ -3,9 +3,6 @@ import { test, assert } from "./test_util.ts";
 import * as deno from "deno";
 
 test(function metrics() {
-  // TODO: metrics command does not send any data buffer to privileged side
-  // should we call other method that does to present it here?
-
   const metrics1 = deno.metrics();
 
   assert(metrics1.opsExecuted > 0);
@@ -13,10 +10,13 @@ test(function metrics() {
   assert(metrics1.dataBytesSent >= 0);
   assert(metrics1.bytesReceived > 0);
 
+  // write something to ensure data bytes are sent to privileged side
+  deno.stdout.write(new Uint8Array([41, 42, 43]));
+
   const metrics2 = deno.metrics();
 
   assert(metrics2.opsExecuted > metrics1.opsExecuted);
   assert(metrics2.controlBytesSent > metrics1.controlBytesSent);
-  assert(metrics2.dataBytesSent >= metrics1.dataBytesSent);
+  assert(metrics2.dataBytesSent > metrics1.dataBytesSent);
   assert(metrics2.bytesReceived > metrics1.bytesReceived);
 });
