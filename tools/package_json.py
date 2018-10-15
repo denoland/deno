befd0f7723eb2b745cbba6eab09c74c0e94b46e7
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-10-15 16:55:27 -0700

    Fix unicode issue; print -v on AppVeyor/Travis

--- tools/package_json.py
+++ tools/package_json.py
@@ -7,4 +7,4 @@ root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 def get_typescript_version():
     with open(os.path.join(root_path, 'package.json')) as f:
         package_json_data = json.load(f)
-        return package_json_data["devDependencies"]["typescript"]
+        return str(package_json_data["devDependencies"]["typescript"])
