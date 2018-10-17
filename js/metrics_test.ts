8d2cef8b15f90fafd194c54bbd96476a0cc11a44
Bartek Iwańczuk <biwanczuk@gmail.com>
2018-10-17 20:30:23 +0200

    add test case for metrics

--- js/metrics_test.ts
+++ js/metrics_test.ts
@@ -1,8 +1,8 @@
 // Copyright 2018 the Deno authors. All rights reserved. MIT license.
-import { test, assert } from "./test_util.ts";
+import { test, testPerm, assert } from "./test_util.ts";
 import * as deno from "deno";
 
-test(function metrics() {
+test(async function metrics() {
   const m1 = deno.metrics();
   assert(m1.opsDispatched > 0);
   assert(m1.opsCompleted > 0);
@@ -13,7 +13,7 @@ test(function metrics() {
   // Write to stdout to ensure a "data" message gets sent instead of just
   // control messages.
   const dataMsg = new Uint8Array([41, 42, 43]);
-  deno.stdout.write(dataMsg);
+  await deno.stdout.write(dataMsg);
 
   const m2 = deno.metrics();
   assert(m2.opsDispatched > m1.opsDispatched);
@@ -21,4 +21,24 @@ test(function metrics() {
   assert(m2.bytesSentControl > m1.bytesSentControl);
   assert(m2.bytesSentData >= m1.bytesSentData + dataMsg.byteLength);
   assert(m2.bytesReceived > m1.bytesReceived);
+});
+
+testPerm({ write: true }, function metricsUpdatedIfNoResponseSync() {
+  const filename = deno.makeTempDirSync() + "/test.txt";
+
+  const data = new Uint8Array([41, 42, 43]);
+  deno.writeFileSync(filename, data, 0o666);
+
+  const metrics = deno.metrics();
+  assert(metrics.opsDispatched === metrics.opsCompleted);
+});
+
+testPerm({ write: true }, async function metricsUpdatedIfNoResponseAsync() {
+  const filename = deno.makeTempDirSync() + "/test.txt";
+
+  const data = new Uint8Array([41, 42, 43]);
+  await deno.writeFile(filename, data, 0o666);
+
+  const metrics = deno.metrics();
+  assert(metrics.opsDispatched === metrics.opsCompleted);
 });
