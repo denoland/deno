e590dc2cbb3da90944dc6c0e6ba2064783eb79eb
Ryan Dahl <ry@tinyclouds.org>
2018-10-19 21:18:59 -0400

    Upgrade V8 to 7.2.49

--- gclient_config.py
+++ gclient_config.py
@@ -1,6 +1,6 @@
 # Copyright 2018 the Deno authors. All rights reserved. MIT license.
 solutions = [{
-    'url': 'https://chromium.googlesource.com/v8/v8.git@7.0.276.15',
+    'url': 'https://chromium.googlesource.com/v8/v8.git@7.2.49',
     'name': 'v8',
     'deps_file': 'DEPS',
     'custom_deps': {
