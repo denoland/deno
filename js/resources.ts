07a434e42cf0d8e6357f727b59ddc60e3b5acb14
Kitson Kelly <me@kitsonkelly.com>
2018-11-09 15:07:48 +1100

    Cleanups for runtime lib declarations.

--- js/resources.ts
+++ js/resources.ts
@@ -4,7 +4,12 @@ import * as flatbuffers from "./flatbuffers";
 import { assert } from "./util";
 import * as dispatch from "./dispatch";
 
-export function resources(): { [key: number]: string } {
+export type ResourceMap = { [rid: number]: string };
+
+/** Returns a map of open _file like_ resource ids along with their string
+ * representation.
+ */
+export function resources(): ResourceMap {
   const builder = flatbuffers.createBuilder();
   msg.Resources.startResources(builder);
   const inner = msg.Resource.endResource(builder);
@@ -14,7 +19,7 @@ export function resources(): { [key: number]: string } {
   const res = new msg.ResourcesRes();
   assert(baseRes!.inner(res) !== null);
 
-  const resources: { [key: number]: string } = {};
+  const resources = {} as ResourceMap;
 
   for (let i = 0; i < res.resourcesLength(); i++) {
     const item = res.resources(i)!;
