aea00b3289f0ffd7c7a5dda84c9d698382ccbffe
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-11-18 16:40:25 -0500

    [WIP] Add localStorage

--- js/globals.ts
+++ js/globals.ts
@@ -16,7 +16,7 @@ import * as fetchTypes from "./fetch";
 import * as headers from "./headers";
 import * as textEncoding from "./text_encoding";
 import * as timers from "./timers";
-import { sessionStorage } from "./storage";
+import { sessionStorage, getLocaleStorage } from "./storage";
 import * as urlSearchParams from "./url_search_params";
 
 // These imports are not exposed and therefore are fine to just import the
@@ -57,7 +57,16 @@ window.Blob = blob.DenoBlob;
 export type Blob = blob.DenoBlob;
 window.File = file.DenoFile;
 export type File = file.DenoFile;
-window.sessionStorage = sessionStorage as domTypes.Storage;
+window.localStorage = {} as domTypes.Storage;
+window.sessionStorage = {} as domTypes.Storage;
+Object.defineProperty(window, "localStorage", {
+  get: () => getLocaleStorage(),
+  set: () => {}
+});
+Object.defineProperty(window, "sessionStorage", {
+  get: () => sessionStorage,
+  set: () => {}
+});
 window.URLSearchParams = urlSearchParams.URLSearchParams;
 export type URLSearchParams = urlSearchParams.URLSearchParams;
 
--- js/storage.ts
+++ js/storage.ts
@@ -1,8 +1,10 @@
 // Copyright 2018 the Deno authors. All rights reserved. MIT license.
 import * as domTypes from "./dom_types";
+import { readFileSync, _compiler, File, open } from "./deno";
+import { TextDecoder, btoa } from "./text_encoding";
 
 class Storage implements domTypes.Storage {
-  private data: Map<string, string> = new Map();
+  protected data: Map<string, string> = new Map();
 
   get length() {
     return this.data.size;
@@ -30,4 +32,55 @@ class Storage implements domTypes.Storage {
   }
 }
 
+class LocalStorage extends Storage {
+  private file: Promise<File>;
+
+  constructor(fileName: string) {
+    super();
+
+    const localStorageFile = btoa(fileName);
+    this.unserialize(readFileSync(localStorageFile));
+    this.file = open(localStorageFile, "w");
+  }
+
+  private unserialize(data: Uint8Array) {
+    const decoder = new TextDecoder();
+    const decodedData = JSON.parse(decoder.decode(data)) as {
+      [key: string]: string;
+    };
+    Object.entries(decodedData).forEach(([key, value]) => {
+      super.setItem(key, value);
+    });
+  }
+
+  private serialize(): Uint8Array {
+    const data: { [key: string]: string } = {};
+    for (const [key, value] of this.data) {
+      data[key] = value;
+    }
+    const encoder = new TextEncoder();
+    return encoder.encode(JSON.stringify(data));
+  }
+
+  clear() {
+    super.clear();
+    this.file.then(file => file.write(Uint8Array.from([])));
+  }
+  removeItem(keyName: string) {
+    this.removeItem(keyName);
+    this.file.then(file => file.write(this.serialize()));
+  }
+  setItem(keyName: string, keyValue: string) {
+    super.setItem(keyName, keyValue);
+    this.file.then(file => file.write(this.serialize()));
+  }
+}
+
+let localStorage: LocalStorage | null = null;
+
+export const getLocaleStorage: () => Storage = () =>
+  localStorage ||
+  (localStorage = new LocalStorage(
+    _compiler.DenoCompiler.instance().getScriptFileNames()[0]
+  ));
 export const sessionStorage = new Storage();
