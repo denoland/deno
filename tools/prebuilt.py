cc126528f0a787da2e5c8910f0e8aa2df681a4dd
Ryan Dahl <ry@tinyclouds.org>
2018-11-29 23:25:59 -0800

    Add prebuilt/win/v8_debug.lib (#1249)
    The windows debug build was broken due to libc link errors.

--- tools/prebuilt.py
+++ tools/prebuilt.py
@@ -5,12 +5,16 @@ from third_party import tp, google_env
 
 def download_v8_prebuilt():
     if sys.platform == 'win32':
-        sha1_file = "prebuilt/win/v8.lib.sha1"
+        download_prebuilt("prebuilt/win/v8.lib.sha1")
+        # TODO Ideally we wouldn't have to download both builds of V8.
+        download_prebuilt("prebuilt/win/v8_debug.lib.sha1")
     elif sys.platform.startswith('linux'):
-        sha1_file = "prebuilt/linux64/libv8.a.sha1"
+        download_prebuilt("prebuilt/linux64/libv8.a.sha1")
     elif sys.platform == 'darwin':
-        sha1_file = "prebuilt/mac/libv8.a.sha1"
+        download_prebuilt("prebuilt/mac/libv8.a.sha1")
 
+
+def download_prebuilt(sha1_file):
     run([
         "python",
         tp('depot_tools/download_from_google_storage.py'),
