6ba0106f4879a59b12e64656505cffc79fdc3f8a
Bartek Iwańczuk <biwanczuk@gmail.com>
2018-12-01 21:45:15 +0100

    start porting WorkerNew

---
+++ libdeno/worker.cc
@@ -0,0 +1,254 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include <algorithm>
+#include <fstream>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "third_party/v8/src/globals.h"
+#include "third_party/v8/include/v8.h"
+#include "third_party/v8/src/d8.h"
+#include "third_party/v8/src/objects.h"
+#include "third_party/v8/src/utils.h"
+
+#include "worker.h"
+
+const int kMaxWorkers = 100;
+
+namespace deno {
+
+static v8::Local<v8::Value> Throw(v8::Isolate* isolate, const char* message) {
+  return isolate->ThrowException(
+      v8::String::NewFromUtf8(isolate, message, v8::NewStringType::kNormal)
+          .ToLocalChecked());
+}
+
+static v8::Local<v8::Value> GetValue(v8::Isolate* isolate, v8::Local<v8::Context> context,
+                             v8::Local<v8::Object> object, const char* property) {
+  v8::Local<v8::String> v8_str =
+      v8::String::NewFromUtf8(isolate, property, v8::NewStringType::kNormal)
+          .ToLocalChecked();
+  return object->Get(context, v8_str).ToLocalChecked();
+}
+
+class ExternalOwningOneByteStringResource
+    : public v8::String::ExternalOneByteStringResource {
+ public:
+  ExternalOwningOneByteStringResource() : length_(0) {}
+  ExternalOwningOneByteStringResource(std::unique_ptr<const char[]> data,
+                                      size_t length)
+      : data_(std::move(data)), length_(length) {}
+  const char* data() const override { return data_.get(); }
+  size_t length() const override { return length_; }
+
+ private:
+  std::unique_ptr<const char[]> data_;
+  size_t length_;
+};
+
+static FILE* FOpen(const char* path, const char* mode) {
+#if defined(_MSC_VER) && (defined(_WIN32) || defined(_WIN64))
+  FILE* result;
+  if (fopen_s(&result, path, mode) == 0) {
+    return result;
+  } else {
+    return nullptr;
+  }
+#else
+  FILE* file = fopen(path, mode);
+  if (file == nullptr) return nullptr;
+  struct stat file_stat;
+  if (fstat(fileno(file), &file_stat) != 0) return nullptr;
+  bool is_regular_file = ((file_stat.st_mode & S_IFREG) != 0);
+  if (is_regular_file) return file;
+  fclose(file);
+  return nullptr;
+#endif
+}
+
+
+static char* ReadChars(const char* name, int* size_out) {
+//  if (Shell::options.read_from_tcp_port >= 0) {
+//    return Shell::ReadCharsFromTcpPort(name, size_out);
+//  }
+
+  FILE* file = FOpen(name, "rb");
+  if (file == nullptr) return nullptr;
+
+  fseek(file, 0, SEEK_END);
+  size_t size = ftell(file);
+  rewind(file);
+
+  char* chars = new char[size + 1];
+  chars[size] = '\0';
+  for (size_t i = 0; i < size;) {
+    i += fread(&chars[i], 1, size - i, file);
+    if (ferror(file)) {
+      fclose(file);
+      delete[] chars;
+      return nullptr;
+    }
+  }
+  fclose(file);
+  *size_out = static_cast<int>(size);
+  return chars;
+}
+
+
+// Reads a file into a v8 string.
+v8::Local<v8::String> ReadFile(v8::Isolate* isolate, const char* name) {
+  int size = 0;
+  char* chars = ReadChars(name, &size);
+  if (chars == nullptr) return v8::Local<v8::String>();
+  v8::Local<v8::String> result;
+  if (i::FLAG_use_external_strings && i::String::IsAscii(chars, size)) {
+    v8::String::ExternalOneByteStringResource* resource =
+        new ExternalOwningOneByteStringResource(
+            std::unique_ptr<const char[]>(chars), size);
+    result = v8::String::NewExternalOneByte(isolate, resource).ToLocalChecked();
+  } else {
+    result = v8::String::NewFromUtf8(isolate, chars, v8::NewStringType::kNormal, size)
+                 .ToLocalChecked();
+    delete[] chars;
+  }
+  return result;
+}
+
+v8::base::LazyMutex workers_mutex_;
+bool allow_new_workers_ = true;
+std::vector<Worker*> workers_;
+
+void WorkerNew(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+  v8::HandleScope handle_scope(isolate);
+  if (args.Length() < 1 || !args[0]->IsString()) {
+    Throw(args.GetIsolate(), "1st argument must be string");
+    return;
+  }
+
+  // d8 honors `options={type: string}`, which means the first argument is
+  // not a filename but string of script to be run.
+  bool load_from_file = true;
+  if (args.Length() > 1 && args[1]->IsObject()) {
+    v8::Local<v8::Object> object = args[1].As<v8::Object>();
+    v8::Local<v8::Context> context = isolate->GetCurrentContext();
+    v8::Local<v8::Value> value = GetValue(args.GetIsolate(), context, object, "type");
+    if (value->IsString()) {
+      v8::Local<v8::String> worker_type = value->ToString(context).ToLocalChecked();
+      v8::String::Utf8Value str(isolate, worker_type);
+      if (strcmp("string", *str) == 0) {
+        load_from_file = false;
+      } else if (strcmp("classic", *str) == 0) {
+        load_from_file = true;
+      } else {
+        Throw(args.GetIsolate(), "Unsupported worker type");
+        return;
+      }
+    }
+  }
+
+  v8::Local<v8::Value> source;
+  if (load_from_file) {
+    v8::String::Utf8Value filename(args.GetIsolate(), args[0]);
+    source = ReadFile(args.GetIsolate(), *filename);
+    if (source.IsEmpty()) {
+      Throw(args.GetIsolate(), "Error loading worker script");
+      return;
+    }
+  } else {
+    source = args[0];
+  }
+
+  if (!args.IsConstructCall()) {
+    Throw(args.GetIsolate(), "Worker must be constructed with new");
+    return;
+  }
+
+  {
+    v8::base::LockGuard<v8::base::Mutex> lock_guard(workers_mutex_.Pointer());
+    if (workers_.size() >= kMaxWorkers) {
+      Throw(args.GetIsolate(), "Too many workers, I won't let you create more");
+      return;
+    }
+
+    // Initialize the embedder field to nullptr; if we return early without
+    // creating a new Worker (because the main thread is terminating) we can
+    // early-out from the instance calls.
+    args.Holder()->SetAlignedPointerInInternalField(0, nullptr);
+
+    if (!allow_new_workers_) return;
+
+    v8::Worker* worker = new v8::Worker;
+    args.Holder()->SetAlignedPointerInInternalField(0, worker);
+    workers_.push_back(worker);
+
+    v8::String::Utf8Value script(args.GetIsolate(), source);
+    if (!*script) {
+      Throw(args.GetIsolate(), "Can't get worker script");
+      return;
+    }
+    worker->StartExecuteInThread(*script);
+  }
+}
+
+
+// void WorkerPostMessage(const v8::FunctionCallbackInfo<v8::Value>& args) {
+//   v8::Isolate* isolate = args.GetIsolate();
+//   v8::HandleScope handle_scope(isolate);
+
+//   if (args.Length() < 1) {
+//     Throw(isolate, "Invalid argument");
+//     return;
+//   }
+
+//   Worker* worker = GetWorkerFromInternalField(isolate, args.Holder());
+//   if (!worker) {
+//     return;
+//   }
+
+//   v8::Local<Value> message = args[0];
+//   v8::Local<Value> transfer =
+//       args.Length() >= 2 ? args[1] : v8::Local<Value>::Cast(Undefined(isolate));
+//   std::unique_ptr<SerializationData> data =
+//       SerializeValue(isolate, message, transfer);
+//   if (data) {
+//     worker->PostMessage(std::move(data));
+//   }
+// }
+
+
+// void WorkerGetMessage(const v8::FunctionCallbackInfo<v8::Value>& args) {
+//   v8::Isolate* isolate = args.GetIsolate();
+//   v8::HandleScope handle_scope(isolate);
+//   Worker* worker = GetWorkerFromInternalField(isolate, args.Holder());
+//   if (!worker) {
+//     return;
+//   }
+
+//   std::unique_ptr<SerializationData> data = worker->GetMessage();
+//   if (data) {
+//     v8::Local<Value> value;
+//     if (DeserializeValue(isolate, std::move(data)).ToLocal(&value)) {
+//       args.GetReturnValue().Set(value);
+//     }
+//   }
+// }
+
+
+// void WorkerTerminate(const v8::FunctionCallbackInfo<v8::Value>& args) {
+//   Isolate* isolate = args.GetIsolate();
+//   HandleScope handle_scope(isolate);
+//   Worker* worker = GetWorkerFromInternalField(isolate, args.Holder());
+//   if (!worker) {
+//     return;
+//   }
+
+//   worker->Terminate();
+// }
+
+} // namespace "deno"
+
---
+++ libdeno/worker.h
@@ -0,0 +1,60 @@
+#include "third_party/v8/include/v8.h"
+
+namespace deno {
+class Worker {
+ public:
+  Worker();
+  ~Worker();
+
+  // Run the given script on this Worker. This function should only be called
+  // once, and should only be called by the thread that created the Worker.
+  void StartExecuteInThread(const char* script);
+//  // Post a message to the worker's incoming message queue. The worker will
+//  // take ownership of the SerializationData.
+//  // This function should only be called by the thread that created the Worker.
+//  void PostMessage(std::unique_ptr<SerializationData> data);
+//  // Synchronously retrieve messages from the worker's outgoing message queue.
+//  // If there is no message in the queue, block until a message is available.
+//  // If there are no messages in the queue and the worker is no longer running,
+//  // return nullptr.
+//  // This function should only be called by the thread that created the Worker.
+//  std::unique_ptr<SerializationData> GetMessage();
+//  // Terminate the worker's event loop. Messages from the worker that have been
+//  // queued can still be read via GetMessage().
+//  // This function can be called by any thread.
+//  void Terminate();
+//  // Terminate and join the thread.
+//  // This function can be called by any thread.
+//  void WaitForThread();
+
+ private:
+  class WorkerThread : public v8::base::Thread {
+   public:
+    explicit WorkerThread(Worker* worker)
+        : v8::base::Thread(v8::base::Thread::Options("WorkerThread")),
+          worker_(worker) {}
+
+    void Run() override { worker_->ExecuteInThread(); }
+
+   private:
+    Worker* worker_;
+  };
+
+  void ExecuteInThread();
+//  static void PostMessageOut(const v8::FunctionCallbackInfo<v8::Value>& args);
+//
+//  base::Semaphore in_semaphore_;
+//  base::Semaphore out_semaphore_;
+//  SerializationDataQueue in_queue_;
+//  SerializationDataQueue out_queue_;
+//  base::Thread* thread_;
+//  char* script_;
+//  base::Atomic32 running_;
+};
+
+  static void WorkerNew(const v8::FunctionCallbackInfo<v8::Value>& args);
+//   static void WorkerPostMessage(
+//       const v8::FunctionCallbackInfo<v8::Value>& args);
+//   static void WorkerGetMessage(const v8::FunctionCallbackInfo<v8::Value>& args);
+//   static void WorkerTerminate(const v8::FunctionCallbackInfo<v8::Value>& args);
+}
