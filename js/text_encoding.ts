290fae0073dabccbfcc7e82e3d91b30cda73e55e
Ryan Dahl <ry@tinyclouds.org>
2018-12-06 10:40:54 -0500

    paste license in full

--- js/text_encoding.ts
+++ js/text_encoding.ts
@@ -1,4 +1,27 @@
-// Copyright 2018 the Deno authors. All rights reserved. MIT license.
+// The following code is based off of text-encoding at:
+// https://github.com/inexorabletash/text-encoding
+//
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+//
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 import * as base64 from "base64-js";
 import * as domTypes from "./dom_types";
 import { DenoError, ErrorKind } from "./errors";
@@ -43,11 +66,6 @@ export function btoa(s: string): string {
   const result = base64.fromByteArray(Uint8Array.from(byteArray));
   return result;
 }
-
-// The following code is based off of text-encoding at:
-// https://github.com/inexorabletash/text-encoding.
-// Which the author asserts no copyright and licenses under the Unlicense which
-// can be found at: https://unlicense.org/.
 
 interface Decoder {
   handler(stream: Stream, byte: number): number | number[] | null;
