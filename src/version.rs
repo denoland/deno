10b010b65a118bfe1a15a7113b726d7750b5a1ac
Ryan Dahl <ry@tinyclouds.org>
2018-12-07 16:31:56 -0500

    v0.2.2
    - Don't crash when .mime file not exist in cache (#1291)
    - Process source maps in Rust instead of JS (#1280)
    - Use alternate TextEncoder/TextDecoder implementation (#1281)
    - Upgrade flatbuffers to 80d148
    - Fix memory leaks (#1265, #1275)

--- src/version.rs
+++ src/version.rs
@@ -3,7 +3,7 @@ use libdeno;
 use std::ffi::CStr;
 
 // TODO Extract this version string from Cargo.toml.
-pub const DENO: &str = "0.2.1";
+pub const DENO: &str = "0.2.2";
 
 pub fn v8() -> &'static str {
   let version = unsafe { libdeno::deno_v8_version() };
