bcc1b14b6cc04a206d49e9e07df90c3ca26d5e17
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-12-09 19:10:05 -0800

    Add more console types formatting support

--- js/console.ts
+++ js/console.ts
@@ -9,7 +9,19 @@ type ConsoleOptions = Partial<{
 }>;
 
 // Default depth of logging nested objects
-const DEFAULT_MAX_DEPTH = 2;
+const DEFAULT_MAX_DEPTH = 4;
+
+const TYPEDARRAY_TYPES = [
+  Int8Array,
+  Uint8Array,
+  Uint8ClampedArray,
+  Int16Array,
+  Uint16Array,
+  Int32Array,
+  Uint32Array,
+  Float32Array,
+  Float64Array
+];
 
 // tslint:disable-next-line:no-any
 function getClassInstanceName(instance: any): string {
@@ -35,6 +47,43 @@ function createFunctionString(value: Function, ctx: ConsoleContext): string {
   return `[${cstrName}]`;
 }
 
+interface IterablePrintConfig {
+  typeName: string;
+  displayName: string;
+  leftDelim: string;
+  rightDelim: string;
+  entryHandler: (
+    // tslint:disable-next-line:no-any
+    entry: any,
+    ctx: ConsoleContext,
+    level: number,
+    maxLevel: number
+  ) => string;
+}
+
+function createIterableString(
+  // tslint:disable-next-line:no-any
+  value: any,
+  ctx: ConsoleContext,
+  level: number,
+  maxLevel: number,
+  config: IterablePrintConfig
+): string {
+  if (level >= maxLevel) {
+    return `[${config.typeName}]`;
+  }
+  ctx.add(value);
+
+  const entries: string[] = [];
+  for (const el of value) {
+    entries.push(config.entryHandler(el, ctx, level + 1, maxLevel));
+  }
+  ctx.delete(value);
+  const iPrefix = `${config.displayName ? config.displayName + " " : ""}`;
+  const iContent = entries.length === 0 ? "" : ` ${entries.join(", ")} `;
+  return `${iPrefix}${config.leftDelim}${iContent}${config.rightDelim}`;
+}
+
 function createArrayString(
   // tslint:disable-next-line:no-any
   value: any[],
@@ -42,24 +91,126 @@ function createArrayString(
   level: number,
   maxLevel: number
 ): string {
-  const entries: string[] = [];
-  for (const el of value) {
-    entries.push(stringifyWithQuotes(ctx, el, level + 1, maxLevel));
-  }
-  ctx.delete(value);
-  if (entries.length === 0) {
-    return "[]";
-  }
-  return `[ ${entries.join(", ")} ]`;
+  const printConfig: IterablePrintConfig = {
+    typeName: "Array",
+    displayName: "",
+    leftDelim: "[",
+    rightDelim: "]",
+    entryHandler: (el, ctx, level, maxLevel) =>
+      stringifyWithQuotes(el, ctx, level + 1, maxLevel)
+  };
+  return createIterableString(value, ctx, level, maxLevel, printConfig);
 }
 
-function createObjectString(
+function createTypedArrayString(
+  typedArrayName: string,
   // tslint:disable-next-line:no-any
   value: any,
   ctx: ConsoleContext,
   level: number,
   maxLevel: number
 ): string {
+  const printConfig: IterablePrintConfig = {
+    typeName: typedArrayName,
+    displayName: typedArrayName,
+    leftDelim: "[",
+    rightDelim: "]",
+    entryHandler: (el, ctx, level, maxLevel) =>
+      stringifyWithQuotes(el, ctx, level + 1, maxLevel)
+  };
+  return createIterableString(value, ctx, level, maxLevel, printConfig);
+}
+
+function createSetString(
+  // tslint:disable-next-line:no-any
+  value: Set<any>,
+  ctx: ConsoleContext,
+  level: number,
+  maxLevel: number
+): string {
+  const printConfig: IterablePrintConfig = {
+    typeName: "Set",
+    displayName: "Set",
+    leftDelim: "{",
+    rightDelim: "}",
+    entryHandler: (el, ctx, level, maxLevel) =>
+      stringifyWithQuotes(el, ctx, level + 1, maxLevel)
+  };
+  return createIterableString(value, ctx, level, maxLevel, printConfig);
+}
+
+function createMapString(
+  // tslint:disable-next-line:no-any
+  value: Map<any, any>,
+  ctx: ConsoleContext,
+  level: number,
+  maxLevel: number
+): string {
+  const printConfig: IterablePrintConfig = {
+    typeName: "Map",
+    displayName: "Map",
+    leftDelim: "{",
+    rightDelim: "}",
+    entryHandler: (el, ctx, level, maxLevel) => {
+      const [key, val] = el;
+      return `${stringifyWithQuotes(
+        key,
+        ctx,
+        level + 1,
+        maxLevel
+      )} => ${stringifyWithQuotes(val, ctx, level + 1, maxLevel)}`;
+    }
+  };
+  return createIterableString(value, ctx, level, maxLevel, printConfig);
+}
+
+function createWeakSetString(): string {
+  return "WeakSet { [items unknown] }"; // as seen in Node
+}
+
+function createWeakMapString(): string {
+  return "WeakMap { [items unknown] }"; // as seen in Node
+}
+
+function createDateString(value: Date) {
+  // without quotes, ISO format
+  return value.toISOString();
+}
+
+function createRegExpString(value: RegExp) {
+  return value.toString();
+}
+
+// tslint:disable-next-line:ban-types
+function createStringWrapperString(value: String) {
+  return `[String: "${value.toString()}"]`;
+}
+
+// tslint:disable-next-line:ban-types
+function createBooleanWrapperString(value: Boolean) {
+  return `[Boolean: ${value.toString()}]`;
+}
+
+// tslint:disable-next-line:ban-types
+function createNumberWrapperString(value: Number) {
+  return `[Number: ${value.toString()}]`;
+}
+
+// TODO: Promise, requires v8 bindings to get info
+// TODO: Proxy
+
+function createRawObjectString(
+  // tslint:disable-next-line:no-any
+  value: any,
+  ctx: ConsoleContext,
+  level: number,
+  maxLevel: number
+): string {
+  if (level >= maxLevel) {
+    return "[Object]";
+  }
+  ctx.add(value);
+
   const entries: string[] = [];
   let baseString = "";
 
@@ -71,7 +222,7 @@ function createObjectString(
 
   for (const key of Object.keys(value)) {
     entries.push(
-      `${key}: ${stringifyWithQuotes(ctx, value[key], level + 1, maxLevel)}`
+      `${key}: ${stringifyWithQuotes(value[key], ctx, level + 1, maxLevel)}`
     );
   }
 
@@ -90,10 +241,59 @@ function createObjectString(
   return baseString;
 }
 
-function stringify(
-  ctx: ConsoleContext,
+function createObjectString(
   // tslint:disable-next-line:no-any
   value: any,
+  ...args: [ConsoleContext, number, number]
+): string {
+  if (value instanceof Error) {
+    return value.stack! || "";
+  } else if (Array.isArray(value)) {
+    return createArrayString(value, ...args);
+  } else if (value instanceof Number) {
+    // tslint:disable-next-line:ban-types
+    return createNumberWrapperString(value as Number);
+  } else if (value instanceof Boolean) {
+    // tslint:disable-next-line:ban-types
+    return createBooleanWrapperString(value as Boolean);
+  } else if (value instanceof String) {
+    // tslint:disable-next-line:ban-types
+    return createStringWrapperString(value as String);
+  } else if (value instanceof RegExp) {
+    return createRegExpString(value as RegExp);
+  } else if (value instanceof Date) {
+    return createDateString(value as Date);
+  } else if (value instanceof Set) {
+    // tslint:disable-next-line:no-any
+    return createSetString(value as Set<any>, ...args);
+  } else if (value instanceof Map) {
+    // tslint:disable-next-line:no-any
+    return createMapString(value as Map<any, any>, ...args);
+  } else if (value instanceof WeakSet) {
+    return createWeakSetString();
+  } else if (value instanceof WeakMap) {
+    return createWeakMapString();
+  } else {
+    // Check if it is a typed array
+    for (const cstr of TYPEDARRAY_TYPES) {
+      if (value instanceof cstr) {
+        return createTypedArrayString(
+          cstr.prototype.constructor.name,
+          value,
+          ...args
+        );
+      }
+    }
+
+    // Otherwise, default object formatting
+    return createRawObjectString(value, ...args);
+  }
+}
+
+function stringify(
+  // tslint:disable-next-line:no-any
+  value: any,
+  ctx: ConsoleContext,
   level: number,
   maxLevel: number
 ): string {
@@ -118,20 +318,7 @@ function stringify(
         return "[Circular]";
       }
 
-      if (level >= maxLevel) {
-        return `[object]`;
-      }
-
-      ctx.add(value);
-
-      if (value instanceof Error) {
-        return value.stack! || "";
-      } else if (Array.isArray(value)) {
-        // tslint:disable-next-line:no-any
-        return createArrayString(value as any[], ctx, level, maxLevel);
-      } else {
-        return createObjectString(value, ctx, level, maxLevel);
-      }
+      return createObjectString(value, ctx, level, maxLevel);
     default:
       return "[Not Implemented]";
   }
@@ -139,9 +326,9 @@ function stringify(
 
 // Print strings when they are inside of arrays or objects with quotes
 function stringifyWithQuotes(
-  ctx: ConsoleContext,
   // tslint:disable-next-line:no-any
   value: any,
+  ctx: ConsoleContext,
   level: number,
   maxLevel: number
 ): string {
@@ -149,7 +336,7 @@ function stringifyWithQuotes(
     case "string":
       return `"${value}"`;
     default:
-      return stringify(ctx, value, level, maxLevel);
+      return stringify(value, ctx, level, maxLevel);
   }
 }
 
@@ -167,9 +354,9 @@ export function stringifyArgs(
       out.push(
         // use default maximum depth for null or undefined argument
         stringify(
+          a,
           // tslint:disable-next-line:no-any
           new Set<any>(),
-          a,
           0,
           // tslint:disable-next-line:triple-equals
           options.depth != undefined ? options.depth : DEFAULT_MAX_DEPTH
