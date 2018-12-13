c21bddc994edddaa4d4fe74c42fba7365fa156f7
Bert Belder <bertbelder@gmail.com>
2018-12-13 22:19:17 +0100

    Make `cargo build` work

--- build.rs
+++ build.rs
@@ -16,14 +16,12 @@ fn main() {
   let out_dir = env::var_os("OUT_DIR").unwrap();
   let out_dir = env::current_dir().unwrap().join(out_dir);
 
-  // Normally we configure GN+Ninja to build into Cargo's OUT_DIR.
-  // However, when DENO_BUILD_PATH is set, perform the ninja build in that dir
-  // instead. This is used by CI to avoid building V8 etc twice.
+  // Normally we configure GN+Ninja to build into Cargo's OUT_DIR. However, when
+  // DENO_BUILD_PATH is set, perform the ninja build in that dir instead.
   let gn_out_dir = match env::var_os("DENO_BUILD_PATH") {
     None => {
       // out_dir looks like: "target/debug/build/deno-26d2b5325de0f0cf/out"
-      // The gn build is "target/debug"
-      // So we go up two directories. Blame cargo for these hacks.
+      // The gn build is "target/debug", so we go up three directories.
       let d = out_dir.parent().unwrap();
       let d = d.parent().unwrap();
       let d = d.parent().unwrap();
@@ -31,37 +29,38 @@ fn main() {
     }
     Some(deno_build_path) => PathBuf::from(deno_build_path),
   };
+  let gn_out_dir = normalize_path(&gn_out_dir);
 
-  // Give cargo some instructions. We do this first so the `rerun-if-*-changed`
-  // directives can take effect even if something the build itself fails.
-  println!("cargo:rustc-env=GN_OUT_DIR={}", normalize_path(&gn_out_dir));
-  println!(
-    "cargo:rustc-link-search=native={}/obj",
-    normalize_path(&gn_out_dir)
-  );
-  println!("cargo:rustc-link-lib=static=deno_deps");
-
+  // Tell Cargo when to re-run this file. We do this first, so these directives
+  // can take effect even if something goes wrong later in the build process.
   println!("cargo:rerun-if-env-changed=DENO_BUILD_PATH");
   // TODO: this is obviously not appropriate here.
   println!("cargo:rerun-if-env-changed=APPVEYOR_REPO_COMMIT");
 
-  // Detect if we're being invoked by the rust language server (RLS).
-  // Unfortunately we can't detect whether we're being run by `cargo check`.
-  let check_only = env::var_os("CARGO")
-    .map(PathBuf::from)
-    .as_ref()
-    .and_then(|p| p.file_stem())
-    .and_then(|f| f.to_str())
-    .map(|s| s.starts_with("rls"))
-    .unwrap_or(false);
+  // This helps Rust source files locate the snapshot, source map etc.
+  println!("cargo:rustc-env=GN_OUT_DIR={}", gn_out_dir);
 
-  // If we're being invoked by the RLS, build only the targets that are needed
-  // for `cargo check` to succeed.
-  let gn_target = if check_only {
-    "cargo_check_deps"
+  // Link with libdeno, which includes V8.
+  println!("cargo:rustc-link-search=native={}/obj/libdeno", gn_out_dir);
+  if cfg!(target_os = "windows") {
+    println!("cargo:rustc-link-lib=static=libdeno");
   } else {
-    "deno_deps"
-  };
+    println!("cargo:rustc-link-lib=static=deno");
+  }
+
+  // Link the system libraries that libdeno and V8 depend on.
+  if cfg!(any(target_os = "macos", target_os = "freebsd")) {
+    println!("cargo:rustc-link-lib=dylib=c++");
+  } else if cfg!(target_os = "windows") {
+    for lib in vec!["dbghelp", "shlwapi", "winmm", "ws2_32"] {
+      println!("cargo:rustc-link-lib={}", lib);
+    }
+  }
+
+  // TODO(piscisaureus): Bring back Rust Language Server support.
+  //   * Don't build any non-rust targets when the RLS is analyzing the crate.
+  //   * Use a feature flag to disable the include_bytes! and include_str! macro
+  //     invocations that embed the snapshot and source maps.
 
   let status = Command::new("python")
     .env("DENO_BUILD_PATH", &gn_out_dir)
@@ -75,7 +74,7 @@ fn main() {
     .env("DENO_BUILD_PATH", &gn_out_dir)
     .env("DENO_BUILD_MODE", &mode)
     .arg("./tools/build.py")
-    .arg(gn_target)
+    .arg("deno_deps")
     .arg("-v")
     .status()
     .expect("build.py failed");
