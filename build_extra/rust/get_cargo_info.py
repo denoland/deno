bab3ad9ec4bfcb18b89b8e8a9bd38cce9531dba5
Ryan Dahl <ry@tinyclouds.org>
2018-12-13 15:09:16 -0500

    deno should rebuild when cargo.toml changes

--- build_extra/rust/get_cargo_info.py
+++ build_extra/rust/get_cargo_info.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # Copyright 2018 the Deno authors. All rights reserved. MIT license.
 
-import os
+import sys
 import re
 
 # Read the package version from Cargo.toml and output as json
-current_path = os.path.dirname(os.path.realpath(__file__))
-for line in open(os.path.join(current_path, "../../Cargo.toml")):
+cargo_toml_path = sys.argv[1]
+
+for line in open(cargo_toml_path):
     match = re.search('version = "(.*)"', line)
     if match:
         print('{"version": "' + match.group(1) + '"}')
