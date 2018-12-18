7ba1f003a382cdd276887bb78813f09bf1fd1abb
djKooks <inylove82@gmail.com>
2018-12-18 16:49:31 +0900

    add format.ts file

--- js/format.ts
+++ js/format.ts
@@ -1,27 +1,53 @@
-import { args, exit, run } from "deno";
+// import * as msg from "gen/msg_generated";
+import { args, cwd, exit, run, chdir } from "deno";
+// import { symlink } from "./symlink";
 
-function qrun(args: string[]): run {
-    run({
-        args
-    })
+function qrun(args: string[]) {
+  run({
+    args
+  });
 }
+
+const rootPath = (): string => {
+  // remove '/js' in end of cwd to go on root path
+  return cwd().slice(0, -3);
+};
+
+const joinPath = (joinSet: string[]): string => {
+  return joinSet.join("/");
+};
 
 const clangFormat = () => {
-    console.log('clang_format')
-}
+  console.log("clang_format");
+};
 
 const gnFormat = () => {
-    console.log('gn Format')
-}
+  console.log("gn Format");
+};
 
 const yapf = () => {
-    console.log('yapf')
-}
+  console.log("yapf");
+};
 
 const prettier = () => {
-    console.log('prettier')
-}
+  console.log("prettier");
+};
 
 const rustfmt = () => {
-    console.log('rustfmt')
+  console.log("rustfmt");
+};
+
+function format() {
+  const prettier = joinPath([
+    rootPath(),
+    "third_party",
+    "node_modules",
+    "prettier",
+    "bin-prettier.js"
+  ]);
+  const toolsPath = joinPath([rootPath(), "tools"]);
+  const rustfmtConfig = joinPath([toolsPath, "rustfmt.toml"]);
+  console.log(rustfmtConfig);
 }
+
+format();
