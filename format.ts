2351df72db2be5906a7441e16c883926051284b1
Ryan Dahl <ry@tinyclouds.org>
2018-12-19 13:01:26 -0500

    Fix prettier version (denoland/deno_std#27)
    Original: https://github.com/denoland/deno_std/commit/772698f4d2b65d81118f6f70232a4eccb1becc83

--- format.ts
+++ format.ts
@@ -1,8 +1,31 @@
 #!/usr/bin/env deno --allow-run
+// Copyright 2018 the Deno authors. All rights reserved. MIT license.
 
-import { exit, run } from "deno";
+import { readAll, exit, run } from "deno";
+
+async function checkVersion() {
+  const prettierVersion = run({
+    args: ["bash", "-c", "prettier --version"],
+    stdout: "piped"
+  });
+  const b = await readAll(prettierVersion.stdout);
+  const s = await prettierVersion.status();
+  if (s.code != 0) {
+    console.log("error calling prettier --version error");
+    exit(s.code);
+  }
+  const version = new TextDecoder().decode(b).trim();
+  const requiredVersion = "1.15";
+  if (!version.startsWith(requiredVersion)) {
+    console.log(`Required prettier version: ${requiredVersion}`);
+    console.log(`Installed prettier version: ${version}`);
+    exit(1);
+  }
+}
 
 async function main() {
+  await checkVersion();
+
   const prettier = run({
     args: ["bash", "-c", "prettier --write *.ts **/*.ts"]
   });
