b0a530aed20b87909f667380e1c9c9b7e5a0e960
djKooks <inylove82@gmail.com>
2018-12-26 18:02:48 +0900

    Update 'walk' for recursive search

--- js/format.ts
+++ js/format.ts
@@ -5,14 +5,43 @@ const rootPath = (): string => {
   return cwd();
 };
 
+const walkExtSync = (path: string, exts: string[], pathList: string[], skipPaths?: string[]): string[] => {
+  const files = readDirSync(path)
+  files.forEach((file) => {
+    if (file.isDirectory()) {
+      if (skipPaths) {
+        let shouldSkip = false
+        for (let skipPath of skipPaths) {
+          if (file.path.endsWith(skipPath)) {
+            shouldSkip = true
+          }
+        }
+        if (!shouldSkip) {
+          walkExtSync(file.path, exts, pathList)  
+        }
+      } else {
+        walkExtSync(file.path, exts, pathList)
+      }
+    } else {
+      for (let ext of exts) {
+        if (file.name.endsWith(ext)) {
+          pathList.push(file.path)
+        }
+      }
+    }
+  })
+
+  // console.log(recursiveFileList)
+  return pathList
+}
 // this should return array of file path which has extension in 'exts'
-const findExts = (path: string, exts: string[]): string[] => {
+const findExtsSync = (path: string, exts: string[]): string[] => {
   const files = readDirSync(path)
   let fileWithTargetExts = []
   files.forEach((file) => {
     for (let ext of exts) {
       if (file.name.endsWith(ext)) {
-        fileWithTargetExts.push(path + '/' + file.name)
+        fileWithTargetExts.push(file.path)
       }
     }
   })
@@ -34,6 +63,8 @@ const joinPath = (joinSet: string[]): string => {
 
 const clangFormat = () => {
   console.log("clang Format");
+  const fileList = walkExtSync(rootPath() + '/libdeno', ['.cc', '.h'], [])
+  console.log(fileList)
   /*
   run({
     args: [
@@ -41,7 +72,7 @@ const clangFormat = () => {
       "-i",
       "-style",
       "Google",
-    ].concat(findExts(rootPath() + '/libdeno', ['.cc', '.h']))
+    ].concat(fileList)
   });
   */
 };
@@ -56,39 +87,65 @@ const gnFilePaths = [
 
 const gnFormat = () => {
   console.log("gn Format");
+  const fileList = walkExtSync(rootPath() + '/build_extra', ['.gn', '.gni'], [])
+                    .concat(walkExtSync(rootPath() + '/libdeno', ['.gn', '.gni'], []))
+  console.log(fileList)
+  /*
   let filesToFormat = [];
   gnFilePaths.forEach((path) => {
     filesToFormat = filesToFormat.concat(findExts(rootPath() + path, ['.gn', '.gni']))
   })
   
   for (let file of filesToFormat) {
-    console.log(file)
-    /*
     run({
       args: [
         gnFormatPath(),
         "format",
         file
       ]
-      // TODO: google_env
     });
-    */
   }
+  */
 };
 
 const yapf = () => {
   console.log("yapf");
+  let fileList = []
+  fileList = findExtsSync(rootPath(), ['.json', '.md']).concat(['.github'])
+  fileList.concat(walkExtSync(rootPath() + '/js', [".js", ".json", ".ts", ".md"], [], ["js/deps"]))
+  fileList.concat(walkExtSync(rootPath() + '/tests', [".js", ".json", ".ts", ".md"], []))
+  fileList.concat(walkExtSync(rootPath() + '/tools', [".js", ".json", ".ts", ".md"], [], ["tools/clang"]))
+  fileList.concat(walkExtSync(rootPath() + '/website', [".js", ".json", ".ts", ".md"], []))
+
+  console.log(fileList)
+  /*
+  qrun(["node", prettier, "--write", "--loglevel=error"] + ["rollup.config.js"] +
+     glob("*.json") + glob("*.md") +
+     find_exts([".github", "js", "tests", "tools", "website"],
+               [".js", ".json", ".ts", ".md"],
+               skip=["tools/clang", "js/deps"]))
+               });
+  */
+ 
+  run({
+    args: [
+      'python',
+      rootPath() + "/third_party/python_packages/bin/yapf",
+      "-style",
+      "Google",
+    ].concat(findExts(rootPath() + '/libdeno', ['.cc', '.h']))
+  });
 };
 
 const prettier = () => {
   console.log("prettier");
-};
-
-const rustfmt = () => {
-  console.log("rustfmt");
-};
-
-function format() {
+  /*
+  qrun(["node", prettier, "--write", "--loglevel=error"] + ["rollup.config.js"] +
+     glob("*.json") + glob("*.md") +
+     find_exts([".github", "js", "tests", "tools", "website"],
+               [".js", ".json", ".ts", ".md"],
+               skip=["tools/clang", "js/deps"]))
+  */
   const prettier = joinPath([
     rootPath(),
     "third_party",
@@ -96,10 +153,29 @@ function format() {
     "prettier",
     "bin-prettier.js"
   ]);
+  const targetFiles = ['rollup.config.js'].concat(findExts(rootPath(), ['.json', '.md']))
+  
+
+  run({
+    args: [
+      'node',
+      prettier,
+      '--write',
+      '--loglevel=error'
+    ]
+  });
+};
+
+const rustfmt = () => {
+  console.log("rustfmt");
+};
+
+function format() {
+  
   const toolsPath = joinPath([rootPath(), "tools"]);
   const rustfmtConfig = joinPath([toolsPath, "rustfmt.toml"]);
   // clangFormat();
-  gnFormat();
+  // gnFormat();
 }
 
 format();
