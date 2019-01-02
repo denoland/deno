4ce5e2fafdaa612508a108d6ee94ea35f1025124
Ryan Dahl <ry@tinyclouds.org>
2019-01-01 19:58:40 -0500

    Happy new year!

--- src/eager_unix.rs
+++ src/eager_unix.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 
 use resources::{EagerAccept, EagerRead, EagerWrite, Resource};
 use tokio_util;
--- src/ops.rs
+++ src/ops.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 use errors;
 use errors::{DenoError, DenoResult, ErrorKind};
 use fs as deno_fs;
