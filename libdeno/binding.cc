611d3826c00ac399721d05e1423a03d9d95ba016
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2019-01-04 17:16:38 -0800

    Use multimap with Persistent module handle to avoid IdentityHash collision

--- libdeno/binding.cc
+++ libdeno/binding.cc
@@ -357,21 +357,24 @@ void DenoIsolate::ClearModules() {
     it->second.Reset();
   }
   module_map_.clear();
-  module_filename_map_.clear();
+  for (auto it = module_info_map_.begin(); it != module_info_map_.end(); it++) {
+    it->second.second.Reset();
+  }
+  module_info_map_.clear();
 }
 
 void DenoIsolate::RegisterModule(const char* filename,
                                  v8::Local<v8::Module> module) {
   int id = module->GetIdentityHash();
 
-  // v8.h says that identity hash is not necessarily unique. It seems it's quite
-  // unique enough for the purposes of O(1000) modules, so we use it as a
-  // hashmap key here. The following check is to detect collisions.
-  CHECK_EQ(0, module_filename_map_.count(id));
+  // v8.h says that identity hash is not necessarily unique
 
-  module_filename_map_[id] = filename;
   module_map_.emplace(std::piecewise_construct, std::make_tuple(filename),
                       std::make_tuple(isolate_, module));
+  module_info_map_.emplace(
+      std::piecewise_construct, std::make_tuple(id),
+      std::make_tuple(std::piecewise_construct, std::make_tuple(filename),
+                      std::make_tuple(isolate_, module)));
 }
 
 v8::MaybeLocal<v8::Module> CompileModule(v8::Local<v8::Context> context,
@@ -409,7 +412,15 @@ v8::MaybeLocal<v8::Module> ResolveCallback(v8::Local<v8::Context> context,
   v8::Context::Scope context_scope(context);
 
   int ref_id = referrer->GetIdentityHash();
-  std::string referrer_filename = d->module_filename_map_[ref_id];
+  auto range = d->module_info_map_.equal_range(ref_id);
+  std::string referrer_filename = "";
+  for (auto it = range.first; it != range.second; ++it) {
+    if (it->second.second == referrer) {
+      referrer_filename = it->second.first;
+      break;
+    }
+  }
+  CHECK(referrer_filename.size() != 0);
 
   v8::String::Utf8Value specifier_(isolate, specifier);
   const char* specifier_c = ToCString(specifier_);
