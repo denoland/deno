c42686dc36ba888137fe734c59318466f41d7dfa
木杉 <zhmushan@qq.com>
2019-01-12 10:56:35 +0800

    style(net): format code (#104)

--- net/sha1_test.ts
+++ net/sha1_test.ts
@@ -1,8 +1,8 @@
-import {assertEqual, test} from "../testing/mod.ts";
-import {Sha1} from "./sha1.ts";
+import { assertEqual, test } from "../testing/mod.ts";
+import { Sha1 } from "./sha1.ts";
 
 test(function testSha1() {
   const sha1 = new Sha1();
   sha1.update("abcde");
-  assertEqual(sha1.toString(), "03de6c570bfe24bfc328ccd7ca46b76eadaf4334")
+  assertEqual(sha1.toString(), "03de6c570bfe24bfc328ccd7ca46b76eadaf4334");
 });
