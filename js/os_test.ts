373738dddfd6d111a3ec9eef9da8d2ec4b580944
JaePil Jung <jjp5023@gmail.com>
2019-01-13 23:39:23 +0900

    Remove unnecessary async keyword in test file (#1507)

--- js/os_test.ts
+++ js/os_test.ts
@@ -2,7 +2,7 @@
 import { test, testPerm, assert, assertEqual } from "./test_util.ts";
 import * as deno from "deno";
 
-testPerm({ env: true }, async function envSuccess() {
+testPerm({ env: true }, function envSuccess() {
   const env = deno.env();
   assert(env !== null);
   env.test_var = "Hello World";
@@ -10,7 +10,7 @@ testPerm({ env: true }, async function envSuccess() {
   assertEqual(env.test_var, newEnv.test_var);
 });
 
-test(async function envFailure() {
+test(function envFailure() {
   let caughtError = false;
   try {
     const env = deno.env();
