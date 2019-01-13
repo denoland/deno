e3a3439911ab7d66cec920cde26fd2a57bd87525
J2P <jjp5023@gmail.com>
2019-01-13 21:50:17 +0900

    Remove unnecessary async keyword in test file

--- js/process_test.ts
+++ js/process_test.ts
@@ -3,7 +3,7 @@ import { test, testPerm, assert, assertEqual } from "./test_util.ts";
 import { run, DenoError, ErrorKind } from "deno";
 import * as deno from "deno";
 
-test(async function runPermissions() {
+test(function runPermissions() {
   let caughtError = false;
   try {
     deno.run({ args: ["python", "-c", "print('hello world')"] });
@@ -52,7 +52,7 @@ testPerm({ run: true }, async function runCommandFailedWithSignal() {
   p.close();
 });
 
-testPerm({ run: true }, async function runNotFound() {
+testPerm({ run: true }, function runNotFound() {
   let error;
   try {
     run({ args: ["this file hopefully doesn't exist"] });
