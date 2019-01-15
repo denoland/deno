388bc47ea7569b97c1a729343970c3cdb376c668
James Garbutt <43081j@users.noreply.github.com>
2019-01-15 21:16:52 +0000

    colors: add test cases (#120)

--- colors/test.ts
+++ colors/test.ts
@@ -7,5 +7,19 @@ test(function singleColor() {
 });
 
 test(function doubleColor() {
-  assertEqual(color.red.bgBlue("Hello world"), "[44m[31mHello world[39m[49m");
+  assertEqual(color.red.bgBlue("Hello world"),
+    "[44m[31mHello world[39m[49m");
+});
+
+test(function newLinesContinueColors() {
+  assertEqual(color.red("Hello\nworld"),
+    "[31mHello[39m\n[31mworld[39m");
+  assertEqual(color.red("Hello\r\nworld"),
+    "[31mHello[39m\r\n[31mworld[39m");
+  assertEqual(color.red("Hello\n\nworld"),
+    "[31mHello[39m\n[31m[39m\n[31mworld[39m");
+});
+
+test(function replacesCloseCharacters() {
+  assertEqual(color.red("Hel[39mlo"), "[31mHel[31mlo[39m");
 });
