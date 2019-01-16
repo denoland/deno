9a3eb207dcd1d032a3c3f7e60f8ee9c5e793f022
Kitson Kelly <me@kitsonkelly.com>
2019-01-16 13:57:40 +1100

    Improve assert (#124)

--- testing/test.ts
+++ testing/test.ts
@@ -28,6 +28,7 @@ test(function testingAssertEqual() {
   const a = Object.create(null);
   a.b = "foo";
   assertEqual(a, a);
+  assert(assert.equal === assertEqual);
 });
 
 test(function testingAssertEqualActualUncoercable() {
@@ -53,5 +54,110 @@ test(function testingAssertEqualExpectedUncoercable() {
     console.log(e.message);
     assert(e.message === "actual: bar expected: [Cannot display]");
   }
+  assert(didThrow);
+});
+
+test(function testingAssertStrictEqual() {
+  const a = {};
+  const b = a;
+  assert.strictEqual(a, b);
+});
+
+test(function testingAssertNotStrictEqual() {
+  let didThrow = false;
+  const a = {};
+  const b = {};
+  try {
+    assert.strictEqual(a, b);
+  } catch (e) {
+    assert(e.message === "actual: [object Object] expected: [object Object]");
+    didThrow = true;
+  }
+  assert(didThrow);
+});
+
+test(function testingDoesThrow() {
+  let count = 0;
+  assert.throws(() => {
+    count++;
+    throw new Error();
+  });
+  assert(count === 1);
+});
+
+test(function testingDoesNotThrow() {
+  let count = 0;
+  let didThrow = false;
+  try {
+    assert.throws(() => {
+      count++;
+      console.log("Hello world");
+    });
+  } catch (e) {
+    assert(e.message === "Expected function to throw.");
+    didThrow = true;
+  }
+  assert(count === 1);
+  assert(didThrow);
+});
+
+test(function testingThrowsErrorType() {
+  let count = 0;
+  assert.throws(() => {
+    count++;
+    throw new TypeError();
+  }, TypeError);
+  assert(count === 1);
+});
+
+test(function testingThrowsNotErrorType() {
+  let count = 0;
+  let didThrow = false;
+  try {
+    assert.throws(() => {
+      count++;
+      throw new TypeError();
+    }, RangeError);
+  } catch (e) {
+    assert(e.message === `Expected error to be instance of "RangeError".`);
+    didThrow = true;
+  }
+  assert(count === 1);
+  assert(didThrow);
+});
+
+test(function testingThrowsMsgIncludes() {
+  let count = 0;
+  assert.throws(
+    () => {
+      count++;
+      throw new TypeError("Hello world!");
+    },
+    TypeError,
+    "world"
+  );
+  assert(count === 1);
+});
+
+test(function testingThrowsMsgNotIncludes() {
+  let count = 0;
+  let didThrow = false;
+  try {
+    assert.throws(
+      () => {
+        count++;
+        throw new TypeError("Hello world!");
+      },
+      TypeError,
+      "foobar"
+    );
+  } catch (e) {
+    assert(
+      e.message ===
+        `Expected error message to include "foobar", but got "Hello world!".`
+    );
+    didThrow = true;
+  }
+  assert(count === 1);
   assert(didThrow);
 });
