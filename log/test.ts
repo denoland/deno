c304ee503a2f2bef8cee2cb03d07785ad060b7bc
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-01-19 19:46:46 +0100

    Implement formatters for log (#127)

--- log/test.ts
+++ log/test.ts
@@ -3,16 +3,18 @@ import { assertEqual, test } from "../testing/mod.ts";
 import * as log from "./mod.ts";
 import { FileHandler } from "./handlers.ts";
 
-// TODO: establish something more sophisticated
-let testOutput = "";
+// constructor(levelName: string, options: HandlerOptions = {}) {
+//   this.level = getLevelByName(levelName);
+//   this.levelName = levelName;
+
+//   this.formatter = options.formatter || DEFAULT_FORMATTER;
+// }
 
 class TestHandler extends log.handlers.BaseHandler {
-  constructor(levelName: string) {
-    super(levelName);
-  }
+  testOutput = "";
 
   log(msg: string) {
-    testOutput += `${msg}\n`;
+    this.testOutput += `${msg}\n`;
   }
 }
 
@@ -25,4 +27,68 @@ test(function testDefaultlogMethods() {
 
   const logger = log.getLogger("");
   console.log(logger);
+});
+
+test(async function testDefaultFormatter() {
+  await log.setup({
+    handlers: {
+      test: new TestHandler("DEBUG"),
+    },
+
+    loggers: {
+      test: {
+        level: "DEBUG",
+        handlers: ["test"],
+      },
+    },
+  });
+
+  const logger = log.getLogger("test");
+  const handler = log.getHandler("test");
+  logger.debug("Hello, world!");
+  assertEqual(handler.testOutput, "DEBUG Hello, world!\n");
+});
+
+test(async function testFormatterAsString() {
+  await log.setup({
+    handlers: {
+      test: new TestHandler("DEBUG", {
+        formatter: "test {levelName} {msg}",
+      }),
+    },
+
+    loggers: {
+      test: {
+        level: "DEBUG",
+        handlers: ["test"],
+      },
+    },
+  });
+
+  const logger = log.getLogger("test");
+  const handler = log.getHandler("test");
+  logger.debug("Hello, world!");
+  assertEqual(handler.testOutput, "test DEBUG Hello, world!\n");
+});
+
+test(async function testFormatterAsFunction() {
+  await log.setup({
+    handlers: {
+      test: new TestHandler("DEBUG", {
+        formatter: logRecord => `fn formmatter ${logRecord.levelName} ${logRecord.msg}`,
+      }),
+    },
+
+    loggers: {
+      test: {
+        level: "DEBUG",
+        handlers: ["test"],
+      },
+    },
+  });
+
+  const logger = log.getLogger("test");
+  const handler = log.getHandler("test");
+  logger.error("Hello, world!");
+  assertEqual(handler.testOutput, "fn formmatter ERROR Hello, world!\n");
 });
