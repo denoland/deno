612b2b95f0c18d5e4e03dd6e493cc61f73cd94fd
Ryan Dahl <ry@tinyclouds.org>
2019-02-01 10:41:33 -0500

    Upgrade V8 to 7.4.98 (kKeep fix)
    https://github.com/denoland/deno/issues/814
    https://bugs.chromium.org/p/v8/issues/detail?id=8761

--- gclient_config.py
+++ gclient_config.py
@@ -1,6 +1,7 @@
 # Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 solutions = [{
-    'url': 'https://chromium.googlesource.com/v8/v8.git@7.2.502.16',
+    'url':
+    'https://chromium.googlesource.com/v8/v8.git@7.4.98',
     'name': 'v8',
     'deps_file': 'DEPS',
     'custom_deps': {
