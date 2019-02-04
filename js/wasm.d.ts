28d464baa13ebd9c0ade508925df8dfb8b399ba0
sh7dm <d3dx12.xx@gmail.com>
2019-02-04 21:07:36 +0300

    Format code

--- js/wasm.d.ts
+++ js/wasm.d.ts
@@ -16,15 +16,15 @@
  * for more information.
  */
 declare namespace WebAssembly {
-  type Imports =  Array<{
-      name: string;
-      kind: string;
+  type Imports = Array<{
+    name: string;
+    kind: string;
   }>;
 
   type Exports = Array<{
-      module: string;
-      name: string;
-      kind: string;
+    module: string;
+    name: string;
+    kind: string;
   }>;
 
   type BufferSource = ArrayBufferView | ArrayBuffer;
@@ -33,18 +33,18 @@ declare namespace WebAssembly {
    * WebAssembly.Module
    */
   class Module {
-      constructor(bufferSource: BufferSource);
-      static customSections(module: Module, sectionName: string): ArrayBuffer[];
-      static exports(module: Module): Imports;
-      static imports(module: Module): Exports;
+    constructor(bufferSource: BufferSource);
+    static customSections(module: Module, sectionName: string): ArrayBuffer[];
+    static exports(module: Module): Imports;
+    static imports(module: Module): Exports;
   }
 
   /**
    * WebAssembly.Instance
    */
   class Instance {
-      readonly exports: any;
-      constructor(module: Module, importObject?: any);
+    readonly exports: any;
+    constructor(module: Module, importObject?: any);
   }
 
   /**
@@ -52,69 +52,75 @@ declare namespace WebAssembly {
    * Note: A WebAssembly page has a constant size of 65,536 bytes, i.e., 64KiB.
    */
   interface MemoryDescriptor {
-      initial: number;
-      maximum?: number;
+    initial: number;
+    maximum?: number;
   }
 
   class Memory {
-      readonly buffer: ArrayBuffer;
-      constructor(memoryDescriptor: MemoryDescriptor);
-      grow(numPages: number): number;
+    readonly buffer: ArrayBuffer;
+    constructor(memoryDescriptor: MemoryDescriptor);
+    grow(numPages: number): number;
   }
 
   /**
    * WebAssembly.Table
    */
   interface TableDescriptor {
-      element: "anyfunc";
-      initial: number;
-      maximum?: number;
+    element: "anyfunc";
+    initial: number;
+    maximum?: number;
   }
 
   class Table {
-      readonly length: number;
-      constructor(tableDescriptor: TableDescriptor);
-      get(index: number): (args: any[]) => any;
-      grow(numElements: number): number;
-      set(index: number, value: (args: any[]) => any): void;
+    readonly length: number;
+    constructor(tableDescriptor: TableDescriptor);
+    get(index: number): (args: any[]) => any;
+    grow(numElements: number): number;
+    set(index: number, value: (args: any[]) => any): void;
   }
 
   /**
    * Errors
    */
   class CompileError extends Error {
-      readonly fileName: string;
-      readonly lineNumber: string;
-      readonly columnNumber: string;
-      constructor(message?: string, fileName?: string, lineNumber?: number);
-      toString(): string;
+    readonly fileName: string;
+    readonly lineNumber: string;
+    readonly columnNumber: string;
+    constructor(message?: string, fileName?: string, lineNumber?: number);
+    toString(): string;
   }
 
   class LinkError extends Error {
-      readonly fileName: string;
-      readonly lineNumber: string;
-      readonly columnNumber: string;
-      constructor(message?: string, fileName?: string, lineNumber?: number);
-      toString(): string;
+    readonly fileName: string;
+    readonly lineNumber: string;
+    readonly columnNumber: string;
+    constructor(message?: string, fileName?: string, lineNumber?: number);
+    toString(): string;
   }
 
   class RuntimeError extends Error {
-      readonly fileName: string;
-      readonly lineNumber: string;
-      readonly columnNumber: string;
-      constructor(message?: string, fileName?: string, lineNumber?: number);
-      toString(): string;
+    readonly fileName: string;
+    readonly lineNumber: string;
+    readonly columnNumber: string;
+    constructor(message?: string, fileName?: string, lineNumber?: number);
+    toString(): string;
   }
 
   function compile(bufferSource: BufferSource): Promise<Module>;
 
   interface ResultObject {
-      module: Module;
-      instance: Instance;
+    module: Module;
+    instance: Instance;
   }
 
-  function instantiate(bufferSource: BufferSource, importObject?: object): Promise<ResultObject>;
-  function instantiate(module: Module, importObject?: object): Promise<Instance>;
+  function instantiate(
+    bufferSource: BufferSource,
+    importObject?: object
+  ): Promise<ResultObject>;
+  function instantiate(
+    module: Module,
+    importObject?: object
+  ): Promise<Instance>;
 
   function validate(bufferSource: BufferSource): boolean;
 }
