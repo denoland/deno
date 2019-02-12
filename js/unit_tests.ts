034136a409395f365295951fdb20d9246c19bf95
Ryan Dahl <ry@tinyclouds.org>
2019-02-12 14:42:12 -0500

    Revert "Rewrite tools/format.py in deno (#1528)"
    tools/format.ts is making CI flaky and it's difficult to run right now.
    Reverting to tools/format.py
    
    This reverts commit f19622e7681b7753788137706e535f72c3ebb38e.

--- js/unit_tests.ts
+++ js/unit_tests.ts
@@ -45,8 +45,6 @@ import "./url_search_params_test.ts";
 import "./write_file_test.ts";
 import "./performance_test.ts";
 
-import "../tools/util_test.ts";
-
 import "../website/app_test.js";
 
 import "deps/https/deno.land/x/std/testing/main.ts";
