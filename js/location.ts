3a4c9719fe722be12eb7f7e8c023f8218c23942a
Ryan Dahl <ry@tinyclouds.org>
2019-02-12 21:14:02 -0500

    Add window.location

---
+++ js/location.ts
@@ -0,0 +1,52 @@
+// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+import { URL } from "./url";
+import { notImplemented } from "./util";
+import { Location } from "./dom_types";
+import { window } from "./globals";
+
+export function setLocation(url: string): void {
+  window.location = new LocationImpl(url);
+  Object.freeze(window.location);
+}
+
+export class LocationImpl implements Location {
+  constructor(url: string) {
+    const u = new URL(url);
+    this.url = u;
+    this.hash = u.hash;
+    this.host = u.host;
+    this.href = u.href;
+    this.hostname = u.hostname;
+    this.origin = u.protocol + "//" + u.host;
+    this.pathname = u.pathname;
+    this.protocol = u.protocol;
+    this.port = u.port;
+    this.search = u.search;
+  }
+
+  private url: URL;
+
+  toString(): string {
+    return this.url.toString();
+  }
+
+  readonly ancestorOrigins: string[] = [];
+  hash: string;
+  host: string;
+  hostname: string;
+  href: string;
+  readonly origin: string;
+  pathname: string;
+  port: string;
+  protocol: string;
+  search: string;
+  assign(url: string): void {
+    throw notImplemented();
+  }
+  reload(): void {
+    throw notImplemented();
+  }
+  replace(url: string): void {
+    throw notImplemented();
+  }
+}
--- tests/imports_meta2.js
+++
@@ -1,1 +0,0 @@
-console.log("imports_meta2", import.meta.url);
