9dc84640e1f97cb22a4c0a53bd0b8036a40eaa65
afinch7 <andyfinch7@gmail.com>
2019-02-27 17:07:24 -0500

    Created alternatives for deno_snapshot and compiler_snapshot that generate snapshots at runtime for cross compiled builds.

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,6 +1,31 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 use crate::libdeno::deno_buf;
 
+#[cfg(target_arch = "aarch64")]
+use crate::libdeno::{deno_generate_snapshot};
+#[cfg(target_arch = "aarch64")]
+use crate::flags::v8_set_flags;
+#[cfg(target_arch = "aarch64")]
+use std::ffi::CString;
+
+#[cfg(target_arch = "aarch64")]
+pub fn deno_snapshot() -> deno_buf {
+  let js_file = concat!(env!("GN_OUT_DIR"), "/gen/bundle/main.js");
+  #[cfg(not(feature = "check-only"))]
+  let js_source = include_bytes!(concat!(env!("GN_OUT_DIR"), "/gen/bundle/main.js"));
+  // The snapshot blob is not available when the Rust Language Server runs
+  // 'cargo check'.
+  #[cfg(feature = "check-only")]
+  let js_source = vec![];
+
+  let cjs_file = CString::new(js_file).unwrap();
+
+  v8_set_flags(vec![js_file.to_string(), std::str::from_utf8(js_source).unwrap().to_string()]);
+
+  unsafe { deno_generate_snapshot(cjs_file.as_ptr(), js_source.as_ptr()) }
+}
+
+#[cfg(not(target_arch = "aarch64"))]
 pub fn deno_snapshot() -> deno_buf {
   #[cfg(not(feature = "check-only"))]
   let data =
@@ -13,6 +38,24 @@ pub fn deno_snapshot() -> deno_buf {
   unsafe { deno_buf::from_raw_parts(data.as_ptr(), data.len()) }
 }
 
+#[cfg(target_arch = "aarch64")]
+pub fn compiler_snapshot() -> deno_buf {
+  let js_file = concat!(env!("GN_OUT_DIR"), "/gen/bundle/compiler.js");
+  #[cfg(not(feature = "check-only"))]
+  let js_source = include_bytes!(concat!(env!("GN_OUT_DIR"), "/gen/bundle/compiler.js"));
+  // The snapshot blob is not available when the Rust Language Server runs
+  // 'cargo check'.
+  #[cfg(feature = "check-only")]
+  let js_source = vec![];
+
+  let cjs_file = CString::new(js_file).unwrap();
+
+    v8_set_flags(vec![js_file.to_string(), std::str::from_utf8(js_source).unwrap().to_string()]);
+
+  unsafe { deno_generate_snapshot(cjs_file.as_ptr(), js_source.as_ptr()) }
+}
+
+#[cfg(not(target_arch = "aarch64"))]
 pub fn compiler_snapshot() -> deno_buf {
   #[cfg(not(feature = "check-only"))]
   let data =
