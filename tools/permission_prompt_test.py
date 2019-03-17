617496aea56b5d9af36b68251c75544d48fafb56
afinch7 <andyfinch7@gmail.com>
2019-03-17 14:19:54 -0400

    Cleaned some stuff up and added a timeout

--- tools/permission_prompt_test.py
+++ tools/permission_prompt_test.py
@@ -6,8 +6,9 @@ import pty
 import select
 import subprocess
 import sys
+import time
 
-from util import build_path, executable_suffix, green_ok
+from util import build_path, executable_suffix, green_ok, red_failed
 
 PERMISSIONS_PROMPT_TEST_TS = "tools/permission_prompt_test.ts"
 
@@ -15,7 +16,7 @@ PERMISSIONS_PROMPT_TEST_TS = "tools/permission_prompt_test.ts"
 # This function is copied from:
 # https://gist.github.com/hayd/4f46a68fc697ba8888a7b517a414583e
 # https://stackoverflow.com/q/52954248/1240268
-def tty_capture(cmd, bytes_input):
+def tty_capture(cmd, bytes_input, timeout=5):
     """Capture the output of cmd with bytes_input to stdin,
     with stdin, stdout and stderr as TTYs."""
     mo, so = pty.openpty()  # provide tty to enable line-buffering
@@ -23,21 +24,23 @@ def tty_capture(cmd, bytes_input):
     mi, si = pty.openpty()
     fdmap = {mo: 'stdout', me: 'stderr', mi: 'stdin'}
 
+    timeout_exact = time.time() + timeout
     p = subprocess.Popen(
         cmd, bufsize=1, stdin=si, stdout=so, stderr=se, close_fds=True)
     os.write(mi, bytes_input)
 
-    timeout = .04  # seconds
+    select_timeout = .04  #seconds
     res = {'stdout': b'', 'stderr': b''}
     while True:
-        ready, _, _ = select.select([mo, me], [], [], timeout)
+        ready, _, _ = select.select([mo, me], [], [], select_timeout)
         if ready:
             for fd in ready:
                 data = os.read(fd, 512)
                 if not data:
                     break
                 res[fdmap[fd]] += data
-        elif p.poll() is not None:  # select timed-out
+        elif p.poll() is not None or time.time(
+        ) > timeout_exact:  # select timed-out
             break  # p exited
     for fd in [si, so, se, mi, mo, me]:
         os.close(fd)  # can't do it sooner: it leads to errno.EIO error
@@ -45,10 +48,14 @@ def tty_capture(cmd, bytes_input):
     return p.returncode, res['stdout'], res['stderr']
 
 
-def wrap_test(test_method, test_name):
+def wrap_test(test_name, test_method, *argv):
     sys.stdout.write(test_name + " ... ")
-    test_method()
-    print green_ok()
+    try:
+        test_method(*argv)
+        print green_ok()
+    except AssertionError:
+        print red_failed()
+        raise
 
 
 class Prompt(object):
@@ -70,36 +77,21 @@ class Prompt(object):
     def test(self):
         for test_type in self.test_types:
             test_name_base = "test_" + test_type
-            sys.stdout.write(test_name_base + "_allow_flag" + " ... ")
-            self.test_allow_flag(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_yes_yes" + " ... ")
-            self.test_yes_yes(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_yes_no" + " ... ")
-            self.test_yes_no(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_no_no" + " ... ")
-            self.test_no_no(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_no_yes" + " ... ")
-            self.test_no_yes(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_allow" + " ... ")
-            self.test_allow(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_deny" + " ... ")
-            self.test_deny(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_unrecognized_option" + " ... ")
-            self.test_unrecognized_option(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_no_prompt" + " ... ")
-            self.test_no_prompt(test_type)
-            print green_ok()
-            sys.stdout.write(test_name_base + "_no_prompt_allow" + " ... ")
-            self.test_no_prompt_allow(test_type)
-            print green_ok()
+            wrap_test(test_name_base + "_allow_flag", self.test_allow_flag,
+                      test_type)
+            wrap_test(test_name_base + "_yes_yes", self.test_yes_yes,
+                      test_type)
+            wrap_test(test_name_base + "_yes_no", self.test_yes_no, test_type)
+            wrap_test(test_name_base + "_no_no", self.test_no_no, test_type)
+            wrap_test(test_name_base + "_no_yes", self.test_no_yes, test_type)
+            wrap_test(test_name_base + "_allow", self.test_allow, test_type)
+            wrap_test(test_name_base + "_deny", self.test_deny, test_type)
+            wrap_test(test_name_base + "_unrecognized_option",
+                      self.test_unrecognized_option, test_type)
+            wrap_test(test_name_base + "_no_prompt", self.test_no_prompt,
+                      test_type)
+            wrap_test(test_name_base + "_no_prompt_allow",
+                      self.test_no_prompt_allow, test_type)
 
     def test_allow_flag(self, test_type):
         code, stdout, stderr = self.run("needs" + test_type.capitalize(), b'',
