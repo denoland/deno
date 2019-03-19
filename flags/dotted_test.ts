e80f14489084d748bb52c4311519f5d0c7696f47
Axetroy <troy450409405@gmail.com>
2019-03-20 01:24:11 +0800

    move test file out of tests dir in flags module (#293)

--- flags/dotted_test.ts
+++ flags/dotted_test.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-import { test } from "../../testing/mod.ts";
-import { assertEquals } from "../../testing/asserts.ts";
-import { parse } from "../mod.ts";
+import { test } from "../testing/mod.ts";
+import { assertEquals } from "../testing/asserts.ts";
+import { parse } from "./mod.ts";
 
 test(function dottedAlias() {
   const argv = parse(["--a.b", "22"], {
--- flags/stop_early_test.ts
+++ flags/stop_early_test.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-import { test } from "../../testing/mod.ts";
-import { assertEquals } from "../../testing/asserts.ts";
-import { parse } from "../mod.ts";
+import { test } from "../testing/mod.ts";
+import { assertEquals } from "../testing/asserts.ts";
+import { parse } from "./mod.ts";
 
 // stops parsing on the first non-option when stopEarly is set
 test(function stopParsing() {
