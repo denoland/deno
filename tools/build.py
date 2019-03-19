8f3d0ca6649236434ee7a795f5a2cea114d44847
Ryan Dahl <ry@tinyclouds.org>
2019-03-18 21:24:12 -0400

    Deprecate DENO_BUILD_MODE=release
    Instead use: tools/build.py --release

--- tools/build.py
+++ tools/build.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 # Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 from __future__ import print_function
+import argparse
 import os
 import sys
 import third_party
 from util import build_path, enable_ansi_colors, run
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--release", help="Use target/release", action="store_true")
+
 
 def main(argv):
     enable_ansi_colors()
 
-    ninja_args = argv[1:]
+    args, rest_argv = parser.parse_known_args(argv)
+
+    if "DENO_BUILD_MODE" not in os.environ:
+        if args.release:
+            os.environ["DENO_BUILD_MODE"] = "release"
+
+    ninja_args = rest_argv[1:]
     if not "-C" in ninja_args:
         if not os.path.isdir(build_path()):
             print("Build directory '%s' does not exist." % build_path(),
