8286d4e0eb15220a85638e96bdb0ede2e4cb723d
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-03-25 05:23:24 +0100

    Update ts_library_builder (#1920)

--- tools/ts_library_builder/testdata/globals.ts
+++ tools/ts_library_builder/testdata/globals.ts
@@ -9,3 +9,7 @@ foobarbaz.process = moduleE.process;
 foobarbaz.reprocess = moduleD.reprocess;
 foobarbaz.Bar = moduleC.Bar;
 export type Bar = moduleC.Bar;
+export interface FizzBuzz {
+  foo: string;
+  bar: number;
+}
