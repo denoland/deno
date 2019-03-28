cac060f3882ba5df2e0e641350b33ef771c8ee44
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-03-28 17:29:27 +0100

    Fix parallel testing (#309)
    Fixes #308
    
    Co-authored by @chiefbiiko

--- testing/main.ts
+++ testing/main.ts
@@ -1,3 +1,11 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 import { runTests } from "./mod.ts";
-runTests();
+
+async function main() {
+  // Testing entire test suite serially
+  await runTests();
+  // Testing parallel execution on a subset that does not depend on exec order
+  await runTests({ parallel: true, only: /^testing/ });
+}
+
+main();
