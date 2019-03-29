e7f0c46a7f924066e995ced8c75ecb38f24b086d
Ryan Dahl <ry@tinyclouds.org>
2019-03-29 15:40:25 -0400

    cli needs a build.rs too

--- cli/build.rs
+++ cli/build.rs
@@ -3,7 +3,9 @@
 // Run "cargo build -vv" if you want to see gn output.
 
 use std::env;
-use std::path::{self, Path, PathBuf};
+use std::path;
+use std::path::Path;
+use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
@@ -18,28 +20,6 @@ fn main() {
     env::var("PROFILE").unwrap()
   };
 
-  // cd into workspace root.
-  assert!(env::set_current_dir("..").is_ok());
-
-  let cwd = env::current_dir().unwrap();
-  // If not using host default target the output folder will change
-  // target/release will become target/$TARGET/release
-  // Gn should also be using this output directory as well
-  // most things will work with gn using the default
-  // output directory but some tests depend on artifacts
-  // being in a specific directory relative to the main build output
-  let gn_out_path = cwd.join(format!("target/{}", gn_mode.clone()));
-  let gn_out_dir = normalize_path(&gn_out_path);
-
-  // Tell Cargo when to re-run this file. We do this first, so these directives
-  // can take effect even if something goes wrong later in the build process.
-  println!("cargo:rerun-if-env-changed=DENO_BUILD_PATH");
-  // TODO: this is obviously not appropriate here.
-  println!("cargo:rerun-if-env-changed=APPVEYOR_REPO_COMMIT");
-
-  // This helps Rust source files locate the snapshot, source map etc.
-  println!("cargo:rustc-env=GN_OUT_DIR={}", gn_out_dir);
-
   // Detect if we're being invoked by the rust language server (RLS).
   // Unfortunately we can't detect whether we're being run by `cargo check`.
   let check_only = env::var_os("CARGO")
@@ -50,42 +30,59 @@ fn main() {
     .map(|s| s.starts_with("rls"))
     .unwrap_or(false);
 
+  // If we are using the same target as the host's default
+  // "rustup target list" should show your default target
+  let is_default_target =
+    env::var("TARGET").unwrap() == env::var("HOST").unwrap();
+
+  // Equivalent to target arch != host arch
+  let is_different_target_arch =
+    env::var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() != env::var("HOST")
+      .unwrap()
+      .as_str()
+      .split("-")
+      .collect::<Vec<&str>>()[0];
+
+  // cd into workspace root.
+  assert!(env::set_current_dir("..").is_ok());
+
+  let cwd = env::current_dir().unwrap();
+  // If not using host default target the output folder will change
+  // target/release will become target/$TARGET/release
+  // Gn should also be using this output directory as well
+  // most things will work with gn using the default
+  // output directory but some tests depend on artifacts
+  // being in a specific directory relative to the main build output
+  let gn_out_path = cwd.join(format!(
+    "target/{}",
+    match is_default_target {
+      true => gn_mode.clone(),
+      false => format!("{}/{}", env::var("TARGET").unwrap(), gn_mode.clone()),
+    }
+  ));
+  let gn_out_dir = normalize_path(&gn_out_path);
+
+  // This helps Rust source files locate the snapshot, source map etc.
+  println!("cargo:rustc-env=GN_OUT_DIR={}", gn_out_dir);
+
+  let gn_target;
+
   if check_only {
+    // When RLS is running "cargo check" to analyze the source code, we're not
+    // trying to build a working executable, rather we're just compiling all
+    // rust code. Therefore, make ninja build only 'msg_generated.rs'.
+    gn_target = "msg_rs";
+
     // Enable the 'check_only' feature, which enables some workarounds in the
     // rust source code to compile successfully without a bundle and snapshot
     println!("cargo:rustc-cfg=feature=\"check-only\"");
-    return;
-  }
-
-  let gn_target = "core:deno_core_deps";
-  // Link with libdeno.a/.lib, which includes V8.
-  println!(
-    "cargo:rustc-link-search=native={}/obj/core/libdeno",
-    gn_out_dir
-  );
-  if cfg!(target_os = "windows") {
-    println!("cargo:rustc-link-lib=static=libdeno");
   } else {
-    println!("cargo:rustc-link-lib=static=deno");
-  }
-
-  // Link the system libraries that libdeno and V8 depend on.
-  if cfg!(any(target_os = "macos", target_os = "freebsd")) {
-    println!("cargo:rustc-link-lib=dylib=c++");
-  } else if cfg!(target_os = "windows") {
-    for lib in vec!["dbghelp", "shlwapi", "winmm", "ws2_32"] {
-      println!("cargo:rustc-link-lib={}", lib);
+    // "Full" (non-RLS) build.
+    if is_different_target_arch {
+      gn_target = "deno_deps_cross";
+    } else {
+      gn_target = "deno_deps";
     }
-  }
-
-  if !gn_out_path.join("build.ninja").exists() {
-    let status = Command::new("python")
-      .env("DENO_BUILD_PATH", &gn_out_dir)
-      .env("DENO_BUILD_MODE", &gn_mode)
-      .arg("./tools/setup.py")
-      .status()
-      .expect("setup.py failed");
-    assert!(status.success());
   }
 
   let status = Command::new("python")
