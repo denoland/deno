290cbcb9dafe8e1ff45f6de406f5d511144b145c
Axetroy <troy450409405@gmail.com>
2019-04-08 21:29:44 +0800

    feat: add entry point file for fs modules (#272)

---
+++ fs/mod.ts
@@ -0,0 +1,12 @@
+// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+export * from "./empty_dir.ts";
+export * from "./ensure_dir.ts";
+export * from "./ensure_file.ts";
+export * from "./exists.ts";
+export * from "./glob.ts";
+export * from "./globrex.ts";
+export * from "./move.ts";
+export * from "./read_json.ts";
+export * from "./write_json.ts";
+export * from "./walk.ts";
+export * from "./eol.ts";
