842cc2ec5ef3bfb09c6e7c9cec75a37a8368004c
Yoshiya Hinosawa <stibium121@gmail.com>
2019-04-13 03:54:13 +0900

    fix: re-expose DomFile (#2100)

--- js/unit_tests.ts
+++ js/unit_tests.ts
@@ -16,9 +16,7 @@ import "./dir_test.ts";
 import "./event_test.ts";
 import "./event_target_test.ts";
 import "./fetch_test.ts";
-// TODO The global "File" has been temporarily disabled. See the note in
-// js/globals.ts
-// import "./file_test.ts";
+import "./file_test.ts";
 import "./files_test.ts";
 import "./form_data_test.ts";
 import "./globals_test.ts";
