5e5c8553e70cfd092cb5ce8a1c77a29fbba770bf
Ryan Dahl <ry@tinyclouds.org>
2019-04-19 11:18:46 -0400

    core: test Modules::deps and handle error cases better (#2141)

--- cli/main.rs
+++ cli/main.rs
@@ -108,12 +108,18 @@ pub fn print_file_info(worker: &Worker, url: &str) {
     );
   }
 
-  let deps = worker.modules.deps(&out.module_name);
-  println!("{}{}", ansi::bold("deps:\n".to_string()), deps.name);
-  if let Some(ref depsdeps) = deps.deps {
-    for d in depsdeps {
-      println!("{}", d);
+  if let Some(deps) = worker.modules.deps(&out.module_name) {
+    println!("{}{}", ansi::bold("deps:\n".to_string()), deps.name);
+    if let Some(ref depsdeps) = deps.deps {
+      for d in depsdeps {
+        println!("{}", d);
+      }
     }
+  } else {
+    println!(
+      "{} cannot retrieve full dependency graph",
+      ansi::bold("deps:".to_string()),
+    );
   }
 }
 
