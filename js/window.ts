b136b794fe8257f62d80b61db8f049e42be8b72a
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2019-04-27 12:10:12 +0800

    Explicitly asserting globalThis to any

--- js/window.ts
+++ js/window.ts
@@ -1,6 +1,3 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 
-// See the links below for details:
-// - https://stackoverflow.com/a/14120023
-// - https://tc39.github.io/ecma262/#sec-performeval (spec)
-export const window = globalThis;
+export const window = globalThis as any;
