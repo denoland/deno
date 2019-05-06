3a9b42b6d6cbca0e1ede56c009691ff8f55bca34
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-05-06 22:29:11 +0200

    verify output of unit tests

--- js/unit_test_runner.ts
+++ js/unit_test_runner.ts
@@ -29,6 +29,40 @@ function fmtPerms(perms: Deno.Permissions): string {
   return fmt;
 }
 
+function parseUnitTestOutput(rawOutput: Uint8Array, print: boolean) {
+  const decoder = new TextDecoder();
+  const output = decoder.decode(rawOutput);
+
+  let expected = null,
+    actual = null,
+    result = null;
+
+  for (const line of output.split("\n")) {
+    if (!expected) {
+      // expect "running 30 tests"
+      const match = line.match(/running (\d+) tests/);
+      expected = Number.parseInt(match[1]);
+    } else if (line.indexOf("test result:") !== -1) {
+      result = line;
+    }
+
+    if (print) {
+      console.log(line);
+    }
+  }
+
+  // Check that the number of expected tests equals what was reported at the
+  // bottom.
+  if (result) {
+    // result should be a string like this:
+    // "test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; ..."
+    const match = result.match(/(\d+) passed/);
+    actual = Number.parseInt(match[1]);
+  }
+
+  return { actual, expected };
+}
+
 async function main(): Promise<void> {
   console.log(
     "Discovered permission combinations for tests:",
@@ -55,11 +89,20 @@ async function main(): Promise<void> {
 
     const p = Deno.run({
       args,
-      stdout: "inherit"
+      stdout: "piped"
     });
 
-    const { code } = await p.status();
-    testResults.push(code);
+    await p.status();
+    const { actual, expected } = parseUnitTestOutput(await p.output(), true);
+
+    if (!actual && !expected) {
+      console.error("Bad js/unit_test.ts output");
+      testResults.push(1);
+    } else if (expected !== actual) {
+      testResults.push(1);
+    } else {
+      testResults.push(0);
+    }
   }
 
   // if any run tests returned non-zero status then whole test
