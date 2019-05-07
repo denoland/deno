87142529163ea047066a2991f51cb2cf3001df74
Jun Kato <i@junkato.jp>
2019-05-08 01:02:31 +0900

    [bytes] fix bytesFindIndex and bytesFindLastIndex (#381)

--- bytes/bytes_test.ts
+++ bytes/bytes_test.ts
@@ -7,12 +7,17 @@ import {
 import { test } from "../testing/mod.ts";
 import { assertEquals } from "../testing/asserts.ts";
 
-test(function bytesBytesFindIndex(): void {
+test(function bytesBytesFindIndex1(): void {
   const i = bytesFindIndex(
     new Uint8Array([1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 3]),
     new Uint8Array([0, 1, 2])
   );
   assertEquals(i, 2);
+});
+
+test(function bytesBytesFindIndex2(): void {
+  const i = bytesFindIndex(new Uint8Array([0, 0, 1]), new Uint8Array([0, 1]));
+  assertEquals(i, 1);
 });
 
 test(function bytesBytesFindLastIndex1(): void {
@@ -21,6 +26,14 @@ test(function bytesBytesFindLastIndex1(): void {
     new Uint8Array([0, 1, 2])
   );
   assertEquals(i, 3);
+});
+
+test(function bytesBytesFindLastIndex2(): void {
+  const i = bytesFindLastIndex(
+    new Uint8Array([0, 1, 1]),
+    new Uint8Array([0, 1])
+  );
+  assertEquals(i, 0);
 });
 
 test(function bytesBytesBytesEqual(): void {
