82a3ff55f2128373f9c1319be4a31b53cf669d2d
Greg Altman <g.s.altman@gmail.com>
2019-05-10 17:11:49 -0400

    add interfaces and DepsOptions

--- js/deps.ts
+++ js/deps.ts
@@ -1,12 +1,27 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as msg from "gen/cli/msg_generated";
 import { sendSync } from "./dispatch";
 import { decodeMessage } from "./workers";
 import { assert } from "./util";
 import * as flatbuffers from "./flatbuffers";
 
-export function deps(): any {
+interface DepsArray {
+  0: string;
+  1: DepsArray[];
+}
+
+interface DepsObject {
+  [index: string]: DepsObject;
+}
+
+export interface DepsOptions {
+  returnObject?: boolean;
+}
+
+export function deps({ returnObject }: DepsOptions = {}):
+  | DepsArray
+  | DepsObject
+  | null {
   const builder = flatbuffers.createBuilder();
   const inner = msg.Deps.createDeps(builder);
   const baseRes = sendSync(builder, msg.Any.Deps, inner)!;
@@ -16,8 +31,21 @@ export function deps(): any {
   // TypeScript cannot track assertion above,
   const dataArray = res.dataArray();
   if (dataArray != null) {
-    return decodeMessage(dataArray);
+    const depsArray = decodeMessage(dataArray);
+    return returnObject ? depsArrayToDepsObject(depsArray) : depsArray;
   } else {
     return null;
   }
+}
+
+function depsArrayToDepsObject(depsArray: DepsArray): DepsObject {
+  return {
+    [depsArray[0]]: depsArray[1].reduce(
+      (depsObjects: object, subDepsArray: DepsArray) => ({
+        ...depsObjects,
+        ...depsArrayToDepsObject(subDepsArray)
+      }),
+      {}
+    )
+  };
 }
