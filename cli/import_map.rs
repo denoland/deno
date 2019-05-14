0a24861f094efa47f8835d558493e6b86d8845f7
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-05-14 21:00:00 +0200

    simple resolve in import maps

--- cli/import_map.rs
+++ cli/import_map.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use url::Url;
+use crate::worker::resolve_module_spec;
 
-#[allow(dead_code)]
 #[derive(PartialEq, Debug)]
 enum ParsedSpecifierType {
   Bare,
@@ -16,12 +16,15 @@ struct ParsedSpecifier {
   url: Option<Url>,
 }
 
+// TODO: make it more rusty
+// TODO: make sure we actually need this struct
 #[allow(dead_code)]
 impl ParsedSpecifier {
   pub fn new(
     specifier: &str,
     base: &str,
   ) -> Self {
+    // TODO: this method is very similar to `resolve_module_spec`
     // 1. Apply the URL parser to specifier. If the result is not failure, return
     //    the result.
     if let Ok(specifier_url) = Url::parse(specifier) {
@@ -67,7 +70,10 @@ impl ParsedSpecifier {
   }
 
   pub fn is_valid(&self) -> bool {
-    self.get_type() != ParsedSpecifierType::Invalid
+    match self.get_type() {
+      ParsedSpecifierType::Invalid => false,
+      _ => true,
+    }
   }
 
   pub fn get_import_map_key(&self) -> Option<String> {
@@ -109,16 +115,81 @@ impl From<String> for ParsedSpecifier {
   }
 }
 
+// TODO: this should return Result?
+/// This is only place where we allow for bare imports (eg. `import query from "jquery";`);
+fn get_import_map_key(specifier: &str, base_url: &str) -> Option<String> {
+  if specifier.is_empty() {
+    return None;
+  }
+
+  let parsed_specifier = ParsedSpecifier::new(specifier, base_url);
+  let map_key = parsed_specifier.get_import_map_key();
+  map_key
+}
+
+// TODO: this should return Result?
+/// Only resolvable URLs are allowed as values to import map
+fn get_import_map_value(specifier: &str, base_url: &str) -> Option<String> {
+  match resolve_module_spec(specifier, base_url) {
+    Ok(url) => Some(url),
+    Err(_) => None
+  }
+}
+
 #[allow(dead_code)]
-struct ImportMap {
-  modules: HashMap<String, Vec<Url>>,
+pub struct ImportMap {
+  base_url: String,
+  modules: HashMap<String, Vec<String>>,
 }
 
 #[allow(dead_code)]
 impl ImportMap {
-  pub fn new() -> Self {
+  // TODO(bartlomieju): add reading JSON string
+  // TODO: add proper error handling: https://github.com/WICG/import-maps/issues/100
+  pub fn new(base_url: &str) -> Self {
+    let mut modules = HashMap::new();
+
+    // TODO: to be removed
+    modules.insert(
+      get_import_map_key("bar", base_url.clone()).unwrap(),
+      vec![
+        get_import_map_value("./bar.ts", base_url.clone()).unwrap(),
+      ],
+    );
+    modules.insert(
+      get_import_map_key("./foo", base_url.clone()).unwrap(),
+      vec![
+          get_import_map_value("./test/foo.ts", base_url.clone()).unwrap(),
+        ],
+    );
+
     ImportMap {
-      modules: HashMap::new(),
+      base_url: base_url.to_string(),
+      modules,
+    }
+  }
+
+  pub fn resolve(&self, specifier: &str, referrer: &str) -> Option<String> {
+    println!("current map: {:?} {:?}", self.base_url, self.modules);
+    let key = get_import_map_key(specifier, referrer).unwrap();
+
+    if !self.modules.contains_key(&key) {
+      println!("Import map: no match for {:?}", key);
+      return None;
+    }
+
+    match self.modules.get(&key) {
+      Some(matches) => {
+        match matches.first() {
+          Some(url) => {
+            println!("Import map: found match for {:?}, maps to {:?}", key, url);
+            Some(url.to_string())
+          },
+          // we don't want to have entries in import map that are empty vectors
+          None => unreachable!(),
+        }
+      },
+      None => unreachable!(),
     }
   }
 }
@@ -129,7 +200,7 @@ mod tests {
 
   #[test]
   fn test_parsed_specifier() {
-    let spec1 = &ParsedSpecifier::new("./foo.ts", ".");
+    let spec1 = &ParsedSpecifier::new("./foo.ts", "file:///");
     assert!(spec1.is_valid());
     assert_eq!(
       spec1.get_type(),
@@ -137,10 +208,10 @@ mod tests {
     );
     assert_eq!(
       spec1.get_import_map_key(),
-      Some("./foo.ts".to_string()),
+      Some("file:///foo.ts".to_string()),
     );
 
-    let spec2 = &ParsedSpecifier::new("foo", ".");
+    let spec2 = &ParsedSpecifier::new("foo", "http://deno.land");
     assert!(spec2.is_valid());
     assert_eq!(
       spec2.get_type(),
@@ -155,7 +226,7 @@ mod tests {
       None,
     );
 
-    let invalid_spec = &ParsedSpecifier::new("", ".");
+    let invalid_spec = &ParsedSpecifier::new("", "http://deno.land");
     assert!(!invalid_spec.is_valid());
     assert_eq!(
       invalid_spec.get_type(),
@@ -169,10 +240,37 @@ mod tests {
       invalid_spec.get_url(),
       None,
     );
+
+    let invalid_base = &ParsedSpecifier::new("", ".");
+    assert!(!invalid_base.is_valid());
+    assert_eq!(
+      invalid_base.get_type(),
+      ParsedSpecifierType::Invalid,
+    );
+    assert_eq!(
+      invalid_base.get_import_map_key(),
+      None,
+    );
+    assert_eq!(
+      invalid_base.get_url(),
+      None,
+    );
   }
 
-//  #[test]
-//  fn test_remaps() {
-//    let mut import_map = ImportMap::new();
-//  }
+  #[test]
+  fn test_import_map() {
+    let import_map = ImportMap::new("file:///");
+
+    let url1 = import_map.resolve("bar", "file:///script.ts");
+    assert_eq!(url1, Some("file:///bar.ts".to_string()));
+
+    let url2 = import_map.resolve("./foo", "file:///script.ts");
+    assert_eq!(url2, Some("file:///test/foo.ts".to_string()));
+
+    let url2 = import_map.resolve("./foo", "file:///dev/script2.ts");
+    assert_eq!(url2, Some("file:///test/foo.ts".to_string()));
+
+    let url3 = import_map.resolve("./not_mapped_module.ts", "file:///script.ts");
+    assert_eq!(url3, None);
+  }
 }
