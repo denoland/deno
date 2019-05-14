6e7ccf0b49bc3d15f48184064d9c2e3eb76b33f7
Bert Belder <bertbelder@gmail.com>
2019-05-14 14:30:56 -0700

    tools: format .gn files in the 'cli' directory

--- tools/format.py
+++ tools/format.py
@@ -24,7 +24,7 @@ qrun([clang_format_path, "-i", "-style", "Google"] +
      find_exts(["core"], [".cc", ".h"]))
 
 print "gn format"
-for fn in ["BUILD.gn", ".gn"] + find_exts(["build_extra", "core"],
+for fn in ["BUILD.gn", ".gn"] + find_exts(["build_extra", "cli", "core"],
                                           [".gn", ".gni"]):
     qrun(["third_party/depot_tools/gn", "format", fn], env=google_env())
 
