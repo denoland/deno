fd759b39939874c60e99dd512023e70847cde318
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-05-21 21:39:50 +0200

    Use tagged version of prettier in CLI (#2387)

--- cli/flags.rs
+++ cli/flags.rs
@@ -398,7 +398,7 @@ pub fn parse_flags(matches: ArgMatches) -> DenoFlags {
 }
 
 /// Used for `deno fmt <files>...` subcommand
-const PRETTIER_URL: &str = "https://deno.land/std/prettier/main.ts";
+const PRETTIER_URL: &str = "https://deno.land/std@v0.5.0/prettier/main.ts";
 
 /// These are currently handled subcommands.
 /// There is no "Help" subcommand because it's handled by `clap::App` itself.
