83823fcdf55f408ea02cc6632d5ad54ffad02034
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-05-25 12:31:21 +0200

    port Python tests to unittest

--- tools/test.py
+++ tools/test.py
@@ -4,13 +4,13 @@
 # Usage: ./tools/test.py out/Debug
 import os
 import sys
+import unittest
+
 from integration_tests import integration_tests
 from deno_dir_test import deno_dir_test
-from setup_test import setup_test
 from util import build_path, enable_ansi_colors, executable_suffix, run, rmtree
 from util import run_output, tests_path, green_ok
 from unit_tests import unit_tests
-from util_test import util_test
 from benchmark_test import benchmark_test
 from repl_test import repl_tests
 from fetch_test import fetch_test
@@ -66,8 +66,10 @@ def main(argv):
     check_exists(deno_exe)
 
     # Python/build tools testing
-    setup_test()
-    util_test()
+    unit_test_loader = unittest.TestLoader()
+    suite = unit_test_loader.loadTestsFromNames(["util_test", "setup_test"])
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
     run([
         "node", "./node_modules/.bin/ts-node", "--project",
         "tools/ts_library_builder/tsconfig.json",
