cf9e0cf0373c8888da535f3cfb5ee8f224b92b4f
Kurt <mrkurt@gmail.com>
2019-06-06 14:43:58 -0500

    add node tcp proxy benchmark + fixup deno tcp proxy

--- tools/deno_tcp_proxy.ts
+++ tools/deno_tcp_proxy.ts
@@ -6,68 +6,15 @@ const listener = Deno.listen("tcp", addr);
 
 async function handle(conn: Deno.Conn): Promise<void> {
   const origin = await Deno.dial("tcp", originAddr);
-  const buffer = new Uint8Array(1024);
-  const originBuffer = new Uint8Array(1024);
-
-  let connPromise: Promise<{ eof: boolean }> | null;
-  let originPromise: Promise<{ eof: boolean }> | null;
-
-  let connEof = false;
-  let originEof = false;
-
   try {
-    while (true) {
-      const ops = [];
-      if (!connPromise && !connEof) {
-        connPromise = proxyRead(conn, origin, buffer);
-        connPromise.then(eof => {
-          if (eof) connEof = true;
-          connPromise = null;
-        });
-        ops.push(connPromise);
-      }
-
-      if (!originPromise && !originEof) {
-        originPromise = proxyRead(origin, conn, buffer);
-        originPromise.then(eof => {
-          if (eof) connEof = true;
-          originPromise = null;
-        });
-        ops.push(originPromise);
-      }
-
-      if (connPromise === null && originPromise === null) {
-        break;
-      }
-      const r = await Promise.race(ops);
-
-      if (connEof && originEof) {
-        break;
-      }
+    await Promise.all([Deno.copy(conn, origin), Deno.copy(origin, conn)]);
+  } catch (err) {
+    if (err.message !== "read error" && err.message !== "write error") {
+      throw err;
     }
   } finally {
     conn.close();
     origin.close();
-  }
-}
-
-async function proxyRead(
-  source: any,
-  dest: any,
-  buffer: Uint8Array
-): Promise<{ eof: boolean }> {
-  try {
-    const r = await source.read(buffer);
-    try {
-      await dest.write(buffer);
-    } catch (err) {
-      console.error("Error writing:", dest, err);
-      return { eof: true };
-    }
-    return r;
-  } catch (err) {
-    console.error("Error reading:", source);
-    return { eof: true };
   }
 }
 
