9bea576f3ea224ec72f371f6f0bc582171ca7890
Michał Sabiniarz <31597105+mhvsa@users.noreply.github.com>
2019-06-07 02:51:04 +0100

    Deno.core.evalContext & Deno.core.print fix (#2465)

--- core/libdeno/libdeno_test.cc
+++ core/libdeno/libdeno_test.cc
@@ -235,6 +235,20 @@ TEST(LibDenoTest, LibDenoEvalContextError) {
   deno_delete(d);
 }
 
+TEST(LibDenoTest, LibDenoEvalContextInvalidArgument) {
+  Deno* d = deno_new(deno_config{0, snapshot, empty, nullptr, nullptr});
+  deno_execute(d, nullptr, "a.js", "LibDenoEvalContextInvalidArgument();");
+  EXPECT_EQ(nullptr, deno_last_exception(d));
+  deno_delete(d);
+}
+
+TEST(LibDenoTest, LibDenoPrintInvalidArgument) {
+  Deno* d = deno_new(deno_config{0, snapshot, empty, nullptr, nullptr});
+  deno_execute(d, nullptr, "a.js", "LibDenoPrintInvalidArgument();");
+  EXPECT_EQ(nullptr, deno_last_exception(d));
+  deno_delete(d);
+}
+
 TEST(LibDenoTest, SharedAtomics) {
   int32_t s[] = {0, 1, 2};
   deno_buf shared = {reinterpret_cast<uint8_t*>(s), sizeof s};
