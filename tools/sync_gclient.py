40f28ab1b28d81eeb0c8a8088117a8dd15a6f886
Ryan Dahl <ry@tinyclouds.org>
2019-06-11 18:03:05 -0400

    Split up tools/sync_third_party
    This is just encoding my actual workflow; not suggesting that this
    workflow is ideal. Previously I would edit sync_third_party.py each time
    I ran it.

--- tools/sync_gclient.py
+++ tools/sync_gclient.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-# Run this script if you are changing Deno's dependencies.
+# Run this script if you are changing //gclient_config.py
 # To update the deno_third_party git repo after running this, try the following:
 # cd third_party
-# find . -type f | grep -v "\.git" | \
+# find v8 -type f | grep -v "\.git" | \
 #   xargs -I% git add -f --no-warn-embedded-repo "%"
 
 import third_party
 import util
 
 util.enable_ansi_colors()
-
-third_party.run_yarn()
-third_party.run_cargo()
-third_party.run_pip()
 third_party.run_gclient_sync()
