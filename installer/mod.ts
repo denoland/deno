33ffb89f5ca4dc0f671219349a084541da9db7c5
Axetroy <troy450409405@gmail.com>
2019-06-19 22:16:34 +0800

    installer: quote commands correctly (denoland/deno_std#510)
    Original: https://github.com/denoland/deno_std/commit/4317af11d18e67dbc79b0df9839180f8e4e55613

--- installer/mod.ts
+++ installer/mod.ts
@@ -153,6 +153,7 @@ async function generateExecutable(
   filePath: string,
   commands: string[]
 ): Promise<void> {
+  commands = commands.map((v): string => JSON.stringify(v));
   // On Windows if user is using Powershell .cmd extension is need to run the
   // installed module.
   // Generate batch script to satisfy that.
