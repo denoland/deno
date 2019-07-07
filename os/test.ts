88b48945799322c0bc2f34134eed538759de4174
Evgeniy Karagodin <ekaragodin@gmail.com>
2019-07-08 02:41:09 +0700

    Remove os.userHomeDir in favor of Deno.homeDir (#523)

--- os/test.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-import { test } from "../testing/mod.ts";
-import { assertNotEquals } from "../testing/asserts.ts";
-import { userHomeDir } from "./mod.ts";
-
-test(function testUserHomeDir(): void {
-  assertNotEquals(userHomeDir(), "");
-});
