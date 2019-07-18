563109b72a83d313d038516691e64c7bfcf073bf
Ryan Dahl <ry@tinyclouds.org>
2019-07-17 22:16:01 -0400

    Do binary downloads from cargo

--- tools/build_common.rs
+++ tools/build_common.rs
@@ -1,5 +1,18 @@
 use cargo_gn;
+use std::env;
 use std::path::PathBuf;
+use std::process::Command;
+
+fn binary_downloads() {
+  let cwd = env::current_dir().unwrap();
+  let root = cwd.join("..");
+  let status = Command::new("python")
+    .arg("tools/binary_downloads.py")
+    .current_dir(root)
+    .status()
+    .expect("tools/binary_downloads.py failed");
+  assert!(status.success());
+}
 
 // This is essentially a re-write of the original tools/setup.py
 // but in rust.
@@ -7,6 +20,11 @@ fn setup() -> PathBuf {
   let mut debug_args = "is_debug=true\n".to_string();
   let mut release_args = "is_official_build=true\nsymbol_level=0\n".to_string();
 
+  if env::var_os("DENO_NO_BINARY_DOWNLOAD").is_none() {
+    binary_downloads();
+  }
+
+  // TODO(ry) Support prebuilt/mac/sccache
   match cargo_gn::which("sccache") {
     Ok(sccache_path) => {
       debug_args += &format!("cc_wrapper={:?}\n", sccache_path);
@@ -15,10 +33,20 @@ fn setup() -> PathBuf {
       release_args += &format!("cc_wrapper={:?}\n", sccache_path);
       release_args += &format!("rustc_wrapper={:?}\n", sccache_path);
     }
-    Err(_) => {
-      unimplemented!();
-    }
+    Err(_) => {}
   }
+
+  match env::var("DENO_BUILD_ARGS") {
+    Ok(val) => {
+      for arg in val.split_whitespace() {
+        debug_args += arg;
+        debug_args += "\n";
+        release_args += arg;
+        release_args += "\n";
+      }
+    }
+    Err(_) => {}
+  };
 
   cargo_gn::maybe_gen("..", &debug_args, &release_args)
 }
