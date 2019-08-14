96cd05d24dc2b316c4e174e10c70a7eda24bf397
Ryan Dahl <ry@tinyclouds.org>
2019-08-14 19:45:06 -0400

    cleanup

--- core/libdeno/api.cc
+++ core/libdeno/api.cc
@@ -167,9 +167,6 @@ void deno_respond(Deno* d_, void* user_data, deno_op_id op_id, deno_buf buf) {
     if (buf.data_ptr != nullptr && buf.data_len > 0) {
       auto ab = deno::ImportBuf(d, buf);
       d->current_args_->GetReturnValue().Set(ab);
-      printf("deno_respond non-optimal sync %d\n", op_id);
-    } else {
-      // printf("deno_respond optimal sync %d\n", op_id);
     }
     d->current_args_ = nullptr;
     return;
