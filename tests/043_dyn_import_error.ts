e04cac7af4b219b213c327407a62675613073887
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-08-19 20:41:37 +0200

    remove integration test

--- tests/043_dyn_import_error.ts
+++
@@ -1,9 +0,0 @@
-async function main(): Promise<void> {
-  const root = `file://${Deno.cwd()}/`;
-  await import(root + "tests/001_hello.js");
-  // error occurred only for 2nd and following dynamic imports
-  // import non-js file - it will throw SyntaxError
-  await import(root + "README.md");
-}
-
-main();
