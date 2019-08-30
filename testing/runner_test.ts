5ef42ece7dfc314dbc5f4ecd30bdecdec4ee4336
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-08-30 19:43:32 +0200

    fix: better paths handling in test runner (#574)

---
+++ testing/runner_test.ts
@@ -0,0 +1,26 @@
+// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+import { test } from "./mod.ts";
+import { assertEquals } from "../testing/asserts.ts";
+import { getMatchingUrls } from "./runner.ts";
+
+const fileName = window.location.href;
+const TEST_ROOT_PATH = fileName.slice(7, fileName.indexOf("testing")) + "fmt";
+
+test(async function getMatchingUrlsRemote(): Promise<void> {
+  const matches = [
+    "https://deno.land/std/fmt/colors_test.ts",
+    "http://deno.land/std/fmt/printf_test.ts"
+  ];
+
+  const urls = await getMatchingUrls(matches, [], TEST_ROOT_PATH);
+  assertEquals(urls, matches);
+});
+
+test(async function getMatchingUrlsLocal(): Promise<void> {
+  const urls = await getMatchingUrls(
+    ["fmt/*_test.ts"],
+    ["colors*"],
+    TEST_ROOT_PATH
+  );
+  assertEquals(urls.length, 1);
+});
