eef8854ef956a0209975b2558b732fd8a21426c9
Ryan Dahl <ry@tinyclouds.org>
2019-08-30 22:12:12 -0400

    clean up

--- cli_snapshots/run.py
+++ cli_snapshots/run.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 # Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-# This file just executes its arguments, except that also adds GN_OUT_DIR and
-# CARGO_PKG_VERSION to the environ. This is for compatibility with cargo.
 import subprocess
 import sys
 import os
-import re
 
+d = os.path.dirname(os.path.realpath(__file__))
 exe = sys.argv[1]
-d = sys.argv[2]
-root_out_dir = sys.argv[3]
-
 env = os.environ.copy()
-env["CARGO_MANIFEST_DIR"] = os.path.abspath(d)
-env["OUT_DIR"] = root_out_dir
-
+env["CARGO_MANIFEST_DIR"] = d
+env["OUT_DIR"] = os.path.dirname(exe)
 os.chdir(d)
-sys.exit(subprocess.call([exe, "foo"], env=env))
+sys.exit(subprocess.call([exe], env=env))
