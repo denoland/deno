bc6b890f67b6f159936c3bec675fa5d2e093e1ac
Kitson Kelly <me@kitsonkelly.com>
2019-09-15 21:06:20 +1000

    Fix type directive parsing

--- tests/type_definitions.ts
+++ tests/type_definitions.ts
@@ -3,4 +3,8 @@ import { foo } from "./type_definitions/foo.js";
 // @deno-types="./type_definitions/fizz.d.ts"
 import "./type_definitions/fizz.js";
 
+import * as qat from "./type_definitions/qat.ts";
+
 console.log(foo);
+console.log(fizz);
+console.log(qat.qat);
