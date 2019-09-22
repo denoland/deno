9739ba55df5afef922f96b107f91dbb37128bf5a
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-09-22 20:04:46 +0300

    Implement readString (denoland/deno_std#607)
    Original: https://github.com/denoland/deno_std/commit/20b6408e105f5dd1dbcd0a252d1956b3de1024ca

--- io/bufio.ts
+++ io/bufio.ts
@@ -211,8 +211,11 @@ export class BufReader implements Reader {
    * delim.
    * For simple uses, a Scanner may be more convenient.
    */
-  async readString(_delim: string): Promise<string | Deno.EOF> {
-    throw new Error("Not implemented");
+  async readString(delim: string): Promise<string | Deno.EOF> {
+    if (delim.length !== 1)
+      throw new Error("Delimiter should be a single character");
+    const buffer = await this.readSlice(delim.charCodeAt(0));
+    return new TextDecoder().decode(buffer || undefined);
   }
 
   /** `readLine()` is a low-level line-reading primitive. Most callers should
