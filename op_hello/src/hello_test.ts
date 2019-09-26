ce4ae67cc5b739250b3fd190d84a8a2e84e91a2d
Ryan Dahl <ry@tinyclouds.org>
2019-09-26 00:34:06 -0400

    wip

--- op_hello/src/hello_test.ts
+++ op_hello/src/hello_test.ts
@@ -1,7 +1,7 @@
 // TODO  We need some way to import test modules.
 // Attempt one:
 //
-//   import { test, assert } from "../../js/test_util.ts";
+//   import { test } from "../../js/test_util.ts";
 //
 // Here it is referencing files across crate boundaries, which will break
 // 'cargo package' and means the crate is not useable outside the deno tree.
@@ -11,10 +11,9 @@
 // we invent a new URL for referencing files in other crates.
 // this is magic and not browser compatible.. Browser compatibility for
 // ops is not so important.
-import { test, assert } from "crate://deno_cli_snapshots@0.19.0/test_util.ts";
-
+import { test } from "crate://deno_std@0.19.0/testing/mod.ts";
 import { hello } from "./hello.ts";
 
-test(function testHello(): void {
+test("hello test", () => {
   hello();
 });
