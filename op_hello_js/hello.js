b04616a1d17ffd4f8f4ff4540f56e942382e3a26
Ryan Dahl <ry@tinyclouds.org>
2019-09-26 05:52:27 -0400

    adjust

--- op_hello_js/hello.js
+++ op_hello_js/hello.js
@@ -1,5 +1,6 @@
-const OP_HELLO = Deno.ops.add("hello");
+// TODO In the future maybe we can extract the op id in the top-level and use a
+// constant. But currently it's causing problems with snapshotting.
 
 export function hello() {
-  Deno.core.send(OP_HELLO);
+  Deno.core.send(Deno.ops["hello"]);
 }
