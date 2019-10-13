67007f1bff249ee11053d14d61d5c010739b308b
Kevin (Kun) Kassimo Qian <kevinkassimo@gmail.com>
2019-10-13 12:50:15 -0700

    Comment out unused dts byte include macro

--- cli/js.rs
+++ cli/js.rs
@@ -4,17 +4,17 @@ pub static CLI_SNAPSHOT: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.bin"));
 pub static CLI_SNAPSHOT_MAP: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.js.map"));
-#[allow(dead_code)]
-pub static CLI_SNAPSHOT_DTS: &[u8] =
-  include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.d.ts"));
+// #[allow(dead_code)]
+// pub static CLI_SNAPSHOT_DTS: &[u8] =
+//   include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.d.ts"));
 
 pub static COMPILER_SNAPSHOT: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.bin"));
 pub static COMPILER_SNAPSHOT_MAP: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.js.map"));
-#[allow(dead_code)]
-pub static COMPILER_SNAPSHOT_DTS: &[u8] =
-  include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.d.ts"));
+// #[allow(dead_code)]
+// pub static COMPILER_SNAPSHOT_DTS: &[u8] =
+//   include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.d.ts"));
 
 static DENO_RUNTIME: &str = include_str!("js/lib.deno_runtime.d.ts");
 
