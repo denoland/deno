429439d1983269ba40141a1b42f6ac809d1e8c86
James Wright <james@jamesswright.co.uk>
2019-11-04 15:21:43 +0000

    Consolidates asserts#equal branches for keyed collections (Map/Set) and supports deep equality of Map keys  (#3258)

--- std/testing/asserts_test.ts
+++ std/testing/asserts_test.ts
@@ -44,6 +44,7 @@ test(function testingEqual(): void {
   assert(equal(new Set([1]), new Set([1])));
   assert(!equal(new Set([1]), new Set([2])));
   assert(equal(new Set([1, 2, 3]), new Set([3, 2, 1])));
+  assert(equal(new Set([1, new Set([2, 3])]), new Set([new Set([3, 2]), 1])));
   assert(!equal(new Set([1, 2]), new Set([3, 2, 1])));
   assert(!equal(new Set([1, 2, 3]), new Set([4, 5, 6])));
   assert(equal(new Set("denosaurus"), new Set("denosaurussss")));
@@ -83,6 +84,16 @@ test(function testingEqual(): void {
       new Map([["foo", new Map([["bar", "qux"]])]])
     )
   );
+  assert(equal(new Map([[{ x: 1 }, true]]), new Map([[{ x: 1 }, true]])));
+  assert(!equal(new Map([[{ x: 1 }, true]]), new Map([[{ x: 1 }, false]])));
+  assert(!equal(new Map([[{ x: 1 }, true]]), new Map([[{ x: 2 }, true]])));
+  assert(equal([1, 2, 3], [1, 2, 3]));
+  assert(equal([1, [2, 3]], [1, [2, 3]]));
+  assert(!equal([1, 2, 3, 4], [1, 2, 3]));
+  assert(!equal([1, 2, 3, 4], [1, 2, 3]));
+  assert(!equal([1, 2, 3, 4], [1, 4, 2, 3]));
+  assert(equal(new Uint8Array([1, 2, 3, 4]), new Uint8Array([1, 2, 3, 4])));
+  assert(!equal(new Uint8Array([1, 2, 3, 4]), new Uint8Array([2, 1, 4, 3])));
 });
 
 test(function testingNotEquals(): void {
