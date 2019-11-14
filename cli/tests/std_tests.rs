7b1917792409d6969103ab8ddad91ff2c58f7dad
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-11-14 20:15:56 +0100

    try

--- cli/tests/std_tests.rs
+++ cli/tests/std_tests.rs
@@ -11,31 +11,24 @@ fn std_tests() {
   // TODO: this is hacky and should conditionally skipped
   // some more idiomatic way
   // Run only with release build
-  match env::var("DENO_BUILD_MODE") {
-    Ok(mode) => {
-      if mode != "release" {
-        return;
-      }
-    }
-    Err(_) => {
-      return;
+  if let Ok(mode) = env::var("DENO_BUILD_MODE") {
+    if mode == "release" {
+      let dir = TempDir::new().expect("tempdir fail");
+      let mut deno_cmd = Command::new(deno_exe_path());
+      deno_cmd.env("DENO_DIR", dir.path());
+
+      let mut cwd = root_path();
+      cwd.push("std");
+      let mut deno = deno_cmd
+        .current_dir(cwd) // TODO: std tests expect to run from "std" dir
+        .arg("-A")
+        .arg("./testing/runner.ts")
+        .arg("--exclude=testing/testdata")
+        .spawn()
+        .expect("failed to spawn script");
+      let status = deno.wait().expect("failed to wait for the child process");
+      assert_eq!(Some(0), status.code());
+      assert!(status.success());
     }
   }
-
-  let dir = TempDir::new().expect("tempdir fail");
-  let mut deno_cmd = Command::new(deno_exe_path());
-  deno_cmd.env("DENO_DIR", dir.path());
-
-  let mut cwd = root_path();
-  cwd.push("std");
-  let mut deno = deno_cmd
-    .current_dir(cwd) // TODO: std tests expect to run from "std" dir
-    .arg("-A")
-    .arg("./testing/runner.ts")
-    .arg("--exclude=testing/testdata")
-    .spawn()
-    .expect("failed to spawn script");
-  let status = deno.wait().expect("failed to wait for the child process");
-  assert_eq!(Some(0), status.code());
-  assert!(status.success());
 }
