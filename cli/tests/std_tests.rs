69346f7c28d5780de9b90a1459ce9681644d1519
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-11-14 19:18:33 +0100

    run std tests only in release build

--- cli/tests/std_tests.rs
+++ cli/tests/std_tests.rs
@@ -3,10 +3,25 @@ extern crate lazy_static;
 extern crate tempfile;
 use deno_cli::test_util::*;
 use std::process::Command;
+use std::env;
 use tempfile::TempDir;
 
 #[test]
 fn std_tests() {
+  // TODO: this is hacky and should conditionally skipped
+  // some more idiomatic way
+  // Run only with release build
+  match env::var("DENO_BUILD_MODE") {
+    Ok(mode) => {
+      if mode != "release" {
+        return;
+      }
+    }
+    None => {
+      return;
+    }
+  }
+
   let dir = TempDir::new().expect("tempdir fail");
   let mut deno_cmd = Command::new(deno_exe_path());
   deno_cmd.env("DENO_DIR", dir.path());
