e4ce5164104f0ce04d3317ae7391f9b73c6d6e58
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-11-14 04:16:57 +0100

    refactor: per-worker resource table (#3306)
    - removes global `RESOURCE_TABLE` - resource tables are now created per `Worker`
      in `State`
    - renames `CliResource` to `StreamResource` and moves all logic related
      to it to `cli/ops/io.rs`
    - removes `cli/resources.rs`
    - adds `state` argument to `op_read` and `op_write` and consequently adds
      `stateful_minimal_op` to `State`
    - IMPORTANT NOTE: workers don't have access to process stdio - this is
      caused by fact that dropping worker would close stdout for process
      (because it's constructed from raw handle, which closes underlying file
      descriptor on drop)

--- cli/ops/repl.rs
+++ cli/ops/repl.rs
@@ -4,9 +4,8 @@ use crate::deno_error::bad_resource;
 use crate::ops::json_op;
 use crate::repl;
 use crate::repl::Repl;
-use crate::resources;
-use crate::resources::Resource;
 use crate::state::ThreadSafeState;
+use deno::Resource;
 use deno::*;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -44,7 +43,7 @@ fn op_repl_start(
     repl::history_path(&state.global_state.dir, &args.history_file);
   let repl = repl::Repl::new(history_path);
   let resource = ReplResource(Arc::new(Mutex::new(repl)));
-  let mut table = resources::lock_resource_table();
+  let mut table = state.lock_resource_table();
   let rid = table.add("repl", Box::new(resource));
   Ok(JsonOp::Sync(json!(rid)))
 }
@@ -56,7 +55,7 @@ struct ReplReadlineArgs {
 }
 
 fn op_repl_readline(
-  _state: &ThreadSafeState,
+  state: &ThreadSafeState,
   args: Value,
   _zero_copy: Option<PinnedBuf>,
 ) -> Result<JsonOp, ErrBox> {
@@ -64,9 +63,10 @@ fn op_repl_readline(
   let rid = args.rid as u32;
   let prompt = args.prompt;
   debug!("op_repl_readline {} {}", rid, prompt);
+  let state = state.clone();
 
   blocking_json(false, move || {
-    let table = resources::lock_resource_table();
+    let table = state.lock_resource_table();
     let resource = table.get::<ReplResource>(rid).ok_or_else(bad_resource)?;
     let repl = resource.0.clone();
     let line = repl.lock().unwrap().readline(&prompt)?;
