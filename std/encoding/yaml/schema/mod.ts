23fda4ec0e633f42baf72a7dcc10adf465050750
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2019-11-17 19:03:04 +0100

    :page_facing_up: Updating Licence header

--- std/encoding/yaml/schema/mod.ts
+++ std/encoding/yaml/schema/mod.ts
@@ -1,5 +1,6 @@
 // Ported from js-yaml v3.13.1:
 // https://github.com/nodeca/js-yaml/commit/665aadda42349dcae869f12040d9b10ef18d12da
+// Copyright 2011-2015 by Vitaly Puzrin. All rights reserved. MIT license.
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 
 export { core as CORE_SCHEMA } from "./core.ts";
--- std/encoding/yaml/type/binary.ts
+++ std/encoding/yaml/type/binary.ts
@@ -1,4 +1,5 @@
 // Ported from js-yaml v3.13.1:
+// Copyright 2011-2015 by Vitaly Puzrin. All rights reserved. MIT license.
 // https://github.com/nodeca/js-yaml/commit/665aadda42349dcae869f12040d9b10ef18d12da
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
 
