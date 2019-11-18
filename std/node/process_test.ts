205c80b6448fc97e4bb41f6f741ea64a6f3667c7
Rafał Pocztarski <r.pocztarski@gmail.com>
2019-11-18 18:02:41 +0100

    Add tests for process implementation

---
+++ std/node/process_test.ts
@@ -0,0 +1,87 @@
+import { test } from "../testing/mod.ts";
+import { assert, assertThrows, assertEquals } from "../testing/asserts.ts";
+import * as process from "./process.ts";
+
+// NOTE: Deno.cwd() and Deno.chdir() currently require --allow-env
+// (Also Deno.env() requires --allow-env but it's more obvious)
+
+test({
+  name: "process.cwd and process.chdir success",
+  fn() {
+    // this should be run like other tests from directory up
+    assert(process.cwd().match(/\Wstd$/));
+    process.chdir("node");
+    assert(process.cwd().match(/\Wnode$/));
+    process.chdir("..");
+    assert(process.cwd().match(/\Wstd$/));
+    },
+});
+
+test({
+  name: "process.chdir failure",
+  fn() {
+    assertThrows(() => {
+      process.chdir("non-existent-directory-name");
+    }, Deno.DenoError, "No such file");
+  },
+});
+
+test({
+  name: "process.version",
+  fn() {
+    assertEquals(typeof process, "object")
+    assertEquals(typeof process.version, "string");
+    assertEquals(typeof process.versions, "object");
+    assertEquals(typeof process.versions.node, "string");
+  }
+});
+
+test({
+  name: "process.platform",
+  fn() {
+    assertEquals(typeof process.platform, "string");
+  },
+});
+
+test({
+  name: "process.arch",
+  fn() {
+    assertEquals(typeof process.arch, "string");
+    // TODO(rsp): make sure that the arch strings should be the same in Node and Deno:
+    assertEquals(process.arch, Deno.build.arch);
+  },
+});
+
+test({
+  name: "process.pid",
+  fn() {
+    assertEquals(typeof process.pid, "number");
+    assertEquals(process.pid, Deno.pid);
+  },
+});
+
+test({
+  name: "process.on",
+  fn() {
+    assertEquals(typeof process.on, "function");
+    process.on("uncaughtException", (err: any) => {
+    });
+  },
+});
+
+test({
+  name: "process.argv",
+  fn() {
+    assert(Array.isArray(process.argv));
+    assert(process.argv[0].match(/[^/\\]*deno[^/\\]*$/));
+    assert(process.argv.slice(-1)[0].match(/process_test[.]ts$/));
+  },
+});
+
+test({
+  name: "process.env",
+  fn() {
+    assertEquals(typeof process.env.PATH, 'string'); 
+  },
+});
+
