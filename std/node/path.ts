6708fcc38688b80d4e052f755f02efb09a2071d1
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2019-11-19 13:44:59 -0800

    std/node: add some Node.js polyfill to require() (#3382)

---
+++ std/node/path.ts
@@ -0,0 +1,1 @@
+export * from "../path/mod.ts";
---
+++ std/node/tests/cjs/cjs_builtin.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+
+module.exports = {
+  readFileSync: fs.readFileSync,
+  isNull: util.isNull,
+  extname: path.extname
+};
