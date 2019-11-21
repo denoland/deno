c3126d256a4cf78eafdc494fa1ba314cd7923f1e
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-11-21 13:37:18 +0100

    debug

--- cli/file_fetcher.rs
+++ cli/file_fetcher.rs
@@ -997,51 +997,51 @@ mod tests {
     drop(http_server_guard);
   }
 
-  #[test]
-  fn test_get_source_code_multiple_downloads_of_same_file() {
-    let http_server_guard = crate::test_util::http_server();
-    let (_temp_dir, fetcher) = test_setup();
-    // http_util::fetch_sync_string requires tokio
-    tokio_util::init(|| {
-      let specifier = ModuleSpecifier::resolve_url(
-        "http://localhost:4545/tests/subdir/mismatch_ext.ts",
-      )
-      .unwrap();
-      let headers_file_name = fetcher.deps_cache.location.join(
-        fetcher.deps_cache.get_cache_filename_with_extension(
-          specifier.as_url(),
-          "headers.json",
-        ),
-      );
-
-      // first download
-      let result = block_on(fetcher.fetch_source_file_async(&specifier));
-      assert!(result.is_ok());
-
-      let result = fs::File::open(&headers_file_name);
-      assert!(result.is_ok());
-      let headers_file = result.unwrap();
-      // save modified timestamp for headers file
-      let headers_file_metadata = headers_file.metadata().unwrap();
-      let headers_file_modified = headers_file_metadata.modified().unwrap();
-
-      // download file again, it should use already fetched file even though `use_disk_cache` is set to
-      // false, this can be verified using source header file creation timestamp (should be
-      // the same as after first download)
-      let result = block_on(fetcher.fetch_source_file_async(&specifier));
-      assert!(result.is_ok());
-
-      let result = fs::File::open(&headers_file_name);
-      assert!(result.is_ok());
-      let headers_file_2 = result.unwrap();
-      // save modified timestamp for headers file
-      let headers_file_metadata_2 = headers_file_2.metadata().unwrap();
-      let headers_file_modified_2 = headers_file_metadata_2.modified().unwrap();
-
-      assert_eq!(headers_file_modified, headers_file_modified_2);
-    });
-    drop(http_server_guard);
-  }
+  //  #[test]
+  //  fn test_get_source_code_multiple_downloads_of_same_file() {
+  //    let http_server_guard = crate::test_util::http_server();
+  //    let (_temp_dir, fetcher) = test_setup();
+  //    // http_util::fetch_sync_string requires tokio
+  //    tokio_util::init(|| {
+  //      let specifier = ModuleSpecifier::resolve_url(
+  //        "http://localhost:4545/tests/subdir/mismatch_ext.ts",
+  //      )
+  //      .unwrap();
+  //      let headers_file_name = fetcher.deps_cache.location.join(
+  //        fetcher.deps_cache.get_cache_filename_with_extension(
+  //          specifier.as_url(),
+  //          "headers.json",
+  //        ),
+  //      );
+  //
+  //      // first download
+  //      let result = block_on(fetcher.fetch_source_file_async(&specifier));
+  //      assert!(result.is_ok());
+  //
+  //      let result = fs::File::open(&headers_file_name);
+  //      assert!(result.is_ok());
+  //      let headers_file = result.unwrap();
+  //      // save modified timestamp for headers file
+  //      let headers_file_metadata = headers_file.metadata().unwrap();
+  //      let headers_file_modified = headers_file_metadata.modified().unwrap();
+  //
+  //      // download file again, it should use already fetched file even though `use_disk_cache` is set to
+  //      // false, this can be verified using source header file creation timestamp (should be
+  //      // the same as after first download)
+  //      let result = block_on(fetcher.fetch_source_file_async(&specifier));
+  //      assert!(result.is_ok());
+  //
+  //      let result = fs::File::open(&headers_file_name);
+  //      assert!(result.is_ok());
+  //      let headers_file_2 = result.unwrap();
+  //      // save modified timestamp for headers file
+  //      let headers_file_metadata_2 = headers_file_2.metadata().unwrap();
+  //      let headers_file_modified_2 = headers_file_metadata_2.modified().unwrap();
+  //
+  //      assert_eq!(headers_file_modified, headers_file_modified_2);
+  //    });
+  //    drop(http_server_guard);
+  //  }
 
   #[test]
   fn test_get_source_code_3() {
