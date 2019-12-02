db32a287e834c93c33416dc3ce7a12c370e4cb14
木杉 <zhmushan@qq.com>
2019-12-03 07:25:17 +0800

    merge template.ts into file_server.ts

--- std/http/file_server.ts
+++ std/http/file_server.ts
@@ -16,7 +16,7 @@ import {
 } from "./server.ts";
 import { dirViewerTemplate } from "./template.ts";
 
-export interface EntryInfo {
+interface EntryInfo {
   mode: string;
   size: string;
   url: string;
@@ -79,7 +79,6 @@ function fileLenToString(len: number): string {
   return `${(len / base).toFixed(2)}${suffix[suffixIndex]}`;
 }
 
-// TODO: load files using template
 async function serveFile(
   req: ServerRequest,
   filePath: string
@@ -176,6 +175,111 @@ function setCORS(res: Response): void {
     "access-control-allow-headers",
     "Origin, X-Requested-With, Content-Type, Accept, Range"
   );
+}
+
+function dirViewerTemplate(dirname: string, entries: EntryInfo[]): string {
+  return html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Deno File Server</title>
+        <style>
+          :root {
+            --background-color: #fafafa;
+            --color: rgba(0, 0, 0, 0.87);
+          }
+          @media (prefers-color-scheme: dark) {
+            :root {
+              --background-color: #303030;
+              --color: #fff;
+            }
+          }
+          @media (min-width: 960px) {
+            main {
+              max-width: 960px;
+            }
+            body {
+              padding-left: 32px;
+              padding-right: 32px;
+            }
+          }
+          @media (min-width: 600px) {
+            main {
+              padding-left: 24px;
+              padding-right: 24px;
+            }
+          }
+          body {
+            background: var(--background-color);
+            color: var(--color);
+            font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+            font-weight: 400;
+            line-height: 1.43;
+            font-size: 0.875rem;
+          }
+          a {
+            color: #2196f3;
+            text-decoration: none;
+          }
+          a:hover {
+            text-decoration: underline;
+          }
+          table th {
+            text-align: left;
+          }
+          table td {
+            padding: 12px 24px 0 0;
+          }
+        </style>
+      </head>
+      <body>
+        <main>
+          <h1>Index of ${dirname}</h1>
+          <table>
+            <tr>
+              <th>Mode</th>
+              <th>Size</th>
+              <th>Name</th>
+            </tr>
+            ${entries.map(
+              entry => html`
+                <tr>
+                  <td class="mode">
+                    ${entry.mode}
+                  </td>
+                  <td>
+                    ${entry.size}
+                  </td>
+                  <td>
+                    <a href="${entry.url}">${entry.name}</a>
+                  </td>
+                </tr>
+              `
+            )}
+          </table>
+        </main>
+      </body>
+    </html>
+  `;
+}
+
+function html(strings: TemplateStringsArray, ...values: unknown[]): string {
+  const l = strings.length - 1;
+  let html = "";
+
+  for (let i = 0; i < l; i++) {
+    let v = values[i];
+    if (v instanceof Array) {
+      v = v.join("");
+    }
+    const s = strings[i] + v;
+    html += s;
+  }
+  html += strings[l];
+  return html;
 }
 
 listenAndServe(
