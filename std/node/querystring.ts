fcd402b1f7ea04c10fb1e8e5f1c4ae326bbc6cc5
木杉 <zhmushan@qq.com>
2019-12-14 12:34:49 +0800

    add todo

--- std/node/querystring.ts
+++ std/node/querystring.ts
@@ -19,6 +19,8 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// TODO: replace the implementation with URL.searchParams
+
 interface StringifyOptions {
   encodeURIComponent?: (str: string) => string;
 }
