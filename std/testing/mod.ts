19623cc72038f5cc765776b979be043dd1c09f6e
Yoshiya Hinosawa <stibium121@gmail.com>
2019-12-17 23:09:11 +0900

    feat: improve empty test case error msg

--- std/testing/mod.ts
+++ std/testing/mod.ts
@@ -121,14 +121,26 @@ export function test(
       throw new Error("Missing test function");
     }
     name = t;
-  } else {
-    fn = typeof t === "function" ? t : t.fn;
+    if (!name) {
+      throw new Error("The name of test case can't be empty");
+    }
+  } else if (typeof t === "function") {
+    fn = t;
     name = t.name;
+    if (!name) {
+      throw new Error("Test function can't be anonymous");
+    }
+  } else {
+    fn = t.fn;
+    if (!fn) {
+      throw new Error("Missing test function");
+    }
+    name = t.name;
+    if (!name) {
+      throw new Error("The name of test case can't be empty");
+    }
   }
 
-  if (!name) {
-    throw new Error("Test function may not be anonymous");
-  }
   if (filter(name)) {
     candidates.push({ fn, name });
   } else {
