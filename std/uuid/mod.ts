49721cb04a802538eb69426d1384f0058ecae884
axetroy <axetroy.dev@gmail.com>
2019-12-23 17:01:32 +0800

    update

--- std/uuid/mod.ts
+++ std/uuid/mod.ts
@@ -1,5 +1,6 @@
 // Based on https://github.com/kelektiv/node-uuid
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+import * as v4 from "./v4.ts";
 
 export const NIL_UUID = "00000000-0000-0000-0000-000000000000";
 
@@ -21,7 +22,6 @@ export const v1 = NOT_IMPLEMENTED;
 // TODO Implement
 export const v3 = NOT_IMPLEMENTED;
 
-import * as v4 from "./v4.ts";
 export { v4 };
 
 // TODO Implement
