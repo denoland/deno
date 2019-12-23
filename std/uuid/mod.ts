a77964eff4eb0bc74ad1bd7cfbfdc1566ce5dce9
axetroy <axetroy.dev@gmail.com>
2019-12-23 15:26:59 +0800

    fix

--- std/uuid/mod.ts
+++ std/uuid/mod.ts
@@ -7,8 +7,10 @@ export function isNil(val: string): boolean {
   return val === NIL_UUID;
 }
 
-const NOT_IMPLEMENTED = (): void => {
-  throw new Error("Not implemented");
+const NOT_IMPLEMENTED = {
+  generate(): never {
+    throw new Error("Not implemented");
+  }
 };
 
 // TODO Implement
@@ -16,7 +18,7 @@ export const v1 = NOT_IMPLEMENTED;
 // TODO Implement
 export const v3 = NOT_IMPLEMENTED;
 
-import _v4 from "./v4.ts";
+import * as _v4 from "./v4.ts";
 export const v4 = _v4;
 
 // TODO Implement
