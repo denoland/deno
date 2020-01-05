52a0f3a5ef7e83a3abe18263caef9a82ec003944
Bert Belder <bertbelder@gmail.com>
2020-01-05 17:41:54 +0100

    Upgrade rust crates and modernize hyper_hello (#3606)

--- tools/hyper_hello/hyper_hello.rs
+++ tools/hyper_hello/hyper_hello.rs
@@ -2,16 +2,18 @@
 // Adapted from https://github.com/hyperium/hyper/blob/master/examples/hello.rs
 
 #![deny(warnings)]
-extern crate hyper;
 
-use hyper::rt::{self, Future};
-use hyper::service::service_fn_ok;
-use hyper::{Body, Response, Server};
+use std::convert::Infallible;
 use std::env;
+use std::error::Error;
 
-static PHRASE: &[u8] = b"Hello World!";
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Response, Server};
 
-fn main() {
+type Just<T> = Result<T, Infallible>;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
   let mut port: u16 = 4544;
   if let Some(custom_port) = env::args().nth(1) {
     port = custom_port.parse::<u16>().unwrap();
@@ -19,21 +21,20 @@ fn main() {
 
   let addr = ([127, 0, 0, 1], port).into();
 
-  // new_service is run for each connection, creating a 'service'
-  // to handle requests for that specific connection.
-  let new_service = || {
+  // For every connection, we must make a `Service` to handle all
+  // incoming HTTP requests on said connection.
+  let new_service = make_service_fn(|_| {
     // This is the `Service` that will handle the connection.
-    // `service_fn_ok` is a helper to convert a function that
+    // `service_fn` is a helper to convert a function that
     // returns a Response into a `Service`.
-    service_fn_ok(|_| Response::new(Body::from(PHRASE)))
-  };
+    async {
+      Just::Ok(service_fn(|_req| {
+        async { Just::Ok(Response::new(Body::from(&b"Hello World!"[..]))) }
+      }))
+    }
+  });
 
-  let server = Server::bind(&addr)
-    .tcp_nodelay(true)
-    .serve(new_service)
-    .map_err(|e| eprintln!("server error: {}", e));
-
+  let server = Server::bind(&addr).tcp_nodelay(true).serve(new_service);
   println!("Listening on http://{}", addr);
-
-  rt::run(server);
+  Ok(server.await?)
 }
