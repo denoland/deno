b9fedd08a21c46393408c40ecca4863b61501584
Ryan Dahl <ry@tinyclouds.org>
2020-01-09 12:47:11 -0500

    fix

--- test_plugin/tests/test.js
+++ test_plugin/tests/test.js
@@ -11,7 +11,7 @@ if (Deno.build.os === "mac") {
   filenameSuffix = ".dylib";
 }
 
-const filename = `../target/${Deno.args[1]}/${filenamePrefix}${filenameBase}${filenameSuffix}`;
+const filename = `../target/${Deno.args[0]}/${filenamePrefix}${filenameBase}${filenameSuffix}`;
 
 const plugin = Deno.openPlugin(filename);
 
