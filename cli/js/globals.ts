819e2770bf36f55dabda9c1cc56cf117fb00f53d
Kitson Kelly <me@kitsonkelly.com>
2020-01-20 21:47:49 +1100

    Cleanup defining globalThis properties

--- cli/js/globals.ts
+++ cli/js/globals.ts
@@ -1,12 +1,10 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+
 // This is a "special" module, in that it define the global runtime scope of
 // Deno, and therefore it defines a lot of the runtime environment that code
-// is evaluated in.  We use this file to automatically build the runtime type
-// library.
+// is evaluated in.
 
-// Modules which will make up part of the global public API surface should be
-// imported as namespaces, so when the runtime type library is generated they
-// can be expressed as a namespace in the type library.
+// By convention we import those items that are globally exposed as namespaces
 import * as blob from "./blob.ts";
 import * as consoleTypes from "./console.ts";
 import * as csprng from "./get_random_values.ts";
@@ -30,31 +28,10 @@ import * as request from "./request.ts";
 // These imports are not exposed and therefore are fine to just import the
 // symbols required.
 import { core } from "./core.ts";
-
 import { internalObject } from "./internals.ts";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const window = globalThis as any;
-
-interface MessageCallback {
-  (msg: Uint8Array): void;
-}
-
-interface EvalErrorInfo {
-  // Is the object thrown a native Error?
-  isNativeError: boolean;
-  // Was the error happened during compilation?
-  isCompileError: boolean;
-  // The actual thrown entity
-  // (might be an Error or anything else thrown by the user)
-  // If isNativeError is true, this is an Error
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  thrown: any;
-}
-
-// During the build process, augmentations to the variable `window` in this
-// file are tracked and created as part of default library that is built into
-// Deno, we only need to declare the enough to compile Deno.
+// This global augmentation is just enough types to be able to build Deno,
+// the runtime types are fully defined in `lib.deno_runtime.d.ts`.
 declare global {
   interface CallSite {
     getThis(): unknown;
@@ -83,6 +60,13 @@ declare global {
     [consoleTypes.customInspect]?(): string;
   }
 
+  interface EvalErrorInfo {
+    isNativeError: boolean;
+    isCompileError: boolean;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    thrown: any;
+  }
+
   interface DenoCore {
     print(s: string, isErr?: boolean): void;
     dispatch(
@@ -90,7 +74,7 @@ declare global {
       control: Uint8Array,
       zeroCopy?: ArrayBufferView | null
     ): Uint8Array | null;
-    setAsyncHandler(opId: number, cb: MessageCallback): void;
+    setAsyncHandler(opId: number, cb: (msg: Uint8Array) => void): void;
     sharedQueue: {
       head(): number;
       numRecords(): number;
@@ -112,29 +96,27 @@ declare global {
 
     shared: SharedArrayBuffer;
 
-    /** Evaluate provided code in the current context.
-     * It differs from eval(...) in that it does not create a new context.
-     * Returns an array: [output, errInfo].
-     * If an error occurs, `output` becomes null and `errInfo` is non-null.
-     */
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     evalContext(code: string): [any, EvalErrorInfo | null];
 
     errorToJSON: (e: Error) => string;
   }
 
+  // Only `var` variables show up in the `globalThis` type when doing a global
+  // scope augmentation.
   /* eslint-disable no-var */
+  var addEventListener: (
+    type: string,
+    callback: (event: domTypes.Event) => void | null,
+    options?: boolean | domTypes.AddEventListenerOptions | undefined
+  ) => void;
+  var compilerMain: (() => void) | undefined;
+  var console: consoleTypes.Console;
   var Deno: {
     core: DenoCore;
   };
-  var location: domTypes.Location;
-  var console: consoleTypes.Console;
   var denoMain: (() => void) | undefined;
-  var workerMain: (() => Promise<void> | void) | undefined;
-  var compilerMain: (() => void) | undefined;
-  var wasmCompilerMain: (() => void) | undefined;
-  var queueMicrotask: (callback: () => void) => void;
-  var onmessage: ((e: { data: any }) => Promise<void> | void) | undefined;
+  var location: domTypes.Location;
   var onerror:
     | ((
         msg: string,
@@ -144,85 +126,111 @@ declare global {
         e: domTypes.Event
       ) => boolean | void)
     | undefined;
+  var onload: ((e: domTypes.Event) => void) | undefined;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  var onmessage: ((e: { data: any }) => Promise<void> | void) | undefined;
+  var onunload: ((e: domTypes.Event) => void) | undefined;
+  var queueMicrotask: (callback: () => void) => void;
+  var wasmCompilerMain: (() => void) | undefined;
+  var workerMain: (() => Promise<void> | void) | undefined;
   /* eslint-enable */
 }
-
-// A self reference to the global object.
-window.window = window;
 
 // Add internal object to Deno object.
 // This is not exposed as part of the Deno types.
 // @ts-ignore
 Deno[Deno.symbols.internal] = internalObject;
-// This is the Deno namespace, it is handled differently from other window
-// properties when building the runtime type library, as the whole module
-// is flattened into a single namespace.
-window.Deno = Deno;
 
-// Globally available functions and object instances.
-window.atob = textEncoding.atob;
-window.btoa = textEncoding.btoa;
-window.fetch = fetchTypes.fetch;
-window.clearTimeout = timers.clearTimeout;
-window.clearInterval = timers.clearInterval;
-window.console = new consoleTypes.Console(core.print);
-window.setTimeout = timers.setTimeout;
-window.setInterval = timers.setInterval;
-window.location = undefined;
-window.onload = undefined;
-window.onunload = undefined;
-window.crypto = csprng;
-window.Blob = blob.DenoBlob;
-window.File = domFile.DomFileImpl;
-window.CustomEvent = customEvent.CustomEvent;
-window.Event = event.Event;
-window.EventTarget = eventTarget.EventTarget;
-window.URL = url.URL;
-window.URLSearchParams = urlSearchParams.URLSearchParams;
-window.Headers = headers.Headers;
-window.FormData = formData.FormData;
-window.TextEncoder = textEncoding.TextEncoder;
-window.TextDecoder = textEncoding.TextDecoder;
-window.Request = request.Request;
-window.Response = fetchTypes.Response;
-window.performance = new performanceUtil.Performance();
-window.onmessage = workers.onmessage;
-window.onerror = workers.onerror;
-window.workerMain = workers.workerMain;
-window.workerClose = workers.workerClose;
-window.postMessage = workers.postMessage;
-window.Worker = workers.WorkerImpl;
+function writable(value: unknown): PropertyDescriptor {
+  return {
+    value,
+    writable: true,
+    enumerable: true,
+    configurable: true
+  };
+}
 
-window[domTypes.eventTargetHost] = null;
-window[domTypes.eventTargetListeners] = {};
-window[domTypes.eventTargetMode] = "";
-window[domTypes.eventTargetNodeType] = 0;
-window[eventTarget.eventTargetAssignedSlot] = false;
-window[eventTarget.eventTargetHasActivationBehavior] = false;
+function nonEnumerable(value: unknown): PropertyDescriptor {
+  return {
+    value,
+    writable: true,
+    configurable: true
+  };
+}
 
-window.addEventListener = eventTarget.EventTarget.prototype.addEventListener;
-window.dispatchEvent = eventTarget.EventTarget.prototype.dispatchEvent;
-window.removeEventListener =
-  eventTarget.EventTarget.prototype.removeEventListener;
+function readOnly(value: unknown): PropertyDescriptor {
+  return {
+    value,
+    enumerable: true
+  };
+}
+
+const globalProperties = {
+  window: readOnly(globalThis),
+  Deno: readOnly(Deno),
+  atob: writable(textEncoding.atob),
+  btoa: writable(textEncoding.btoa),
+  fetch: writable(fetchTypes.fetch),
+  clearTimeout: writable(timers.clearTimeout),
+  clearInterval: writable(timers.clearInterval),
+  console: writable(new consoleTypes.Console(core.print)),
+  setTimeout: writable(timers.setTimeout),
+  setInterval: writable(timers.setInterval),
+  onload: writable(undefined),
+  onunload: writable(undefined),
+  crypto: readOnly(csprng),
+  Blob: nonEnumerable(blob.DenoBlob),
+  File: nonEnumerable(domFile.DomFileImpl),
+  CustomEvent: nonEnumerable(customEvent.CustomEvent),
+  Event: nonEnumerable(event.Event),
+  EventTarget: nonEnumerable(eventTarget.EventTarget),
+  URL: nonEnumerable(url.URL),
+  URLSearchParams: nonEnumerable(urlSearchParams.URLSearchParams),
+  Headers: nonEnumerable(headers.Headers),
+  FormData: nonEnumerable(formData.FormData),
+  TextEncoder: nonEnumerable(textEncoding.TextEncoder),
+  TextDecoder: nonEnumerable(textEncoding.TextDecoder),
+  Request: nonEnumerable(request.Request),
+  Response: nonEnumerable(fetchTypes.Response),
+  performance: writable(new performanceUtil.Performance()),
+
+  onmessage: writable(workers.onmessage),
+  onerror: writable(workers.onerror),
+
+  workerMain: nonEnumerable(workers.workerMain),
+  workerClose: nonEnumerable(workers.workerClose),
+  postMessage: writable(workers.postMessage),
+  Worker: nonEnumerable(workers.WorkerImpl),
+
+  [domTypes.eventTargetHost]: nonEnumerable(null),
+  [domTypes.eventTargetListeners]: nonEnumerable({}),
+  [domTypes.eventTargetMode]: nonEnumerable(""),
+  [domTypes.eventTargetNodeType]: nonEnumerable(0),
+  [eventTarget.eventTargetAssignedSlot]: nonEnumerable(false),
+  [eventTarget.eventTargetHasActivationBehavior]: nonEnumerable(false),
+
+  addEventListener: readOnly(
+    eventTarget.EventTarget.prototype.addEventListener
+  ),
+  dispatchEvent: readOnly(eventTarget.EventTarget.prototype.dispatchEvent),
+  removeEventListener: readOnly(
+    eventTarget.EventTarget.prototype.removeEventListener
+  )
+};
+
+Object.defineProperties(globalThis, globalProperties);
 
 // Registers the handler for window.onload function.
-window.addEventListener("load", (e: domTypes.Event): void => {
-  const onload = window.onload;
+globalThis.addEventListener("load", (e: domTypes.Event): void => {
+  const { onload } = globalThis;
   if (typeof onload === "function") {
     onload(e);
   }
 });
 // Registers the handler for window.onunload function.
-window.addEventListener("unload", (e: domTypes.Event): void => {
-  const onunload = window.onunload;
+globalThis.addEventListener("unload", (e: domTypes.Event): void => {
+  const { onunload } = globalThis;
   if (typeof onunload === "function") {
     onunload(e);
   }
 });
-
-// below are interfaces that are available in TypeScript but
-// have different signatures
-// export interface ImportMeta {
-//   url: string;
-//   main: boolean;
-// }
