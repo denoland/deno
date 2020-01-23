f14c4197c6c5265754a05ef99935c670a0ff6575
scandar <thescandar@gmail.com>
2020-01-23 17:45:14 +0200

    handles missing parameter in std/examples/curl, shows a usage message instead of exception

--- std/examples/curl.ts
+++ std/examples/curl.ts
@@ -1,4 +1,12 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-const url_ = Deno.args[0];
-const res = await fetch(url_);
-await Deno.copy(Deno.stdout, res.body);
+const curl = async (url_: string) => {
+  const res = await fetch(url_);
+  await Deno.copy(Deno.stdout, res.body);
+}
+
+const usage = () => {
+  console.log('Usage:');
+  console.log('   curl --allow-net [url]');
+}
+
+Deno.args.length ? curl(Deno.args[0]) : usage();
