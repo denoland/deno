aa252d3a67a59f12c289a98ea7490c8008d38336
Tilman Roeder <dyed.green.info@gmail.com>
2020-02-04 16:27:50 +0000

    Do not encode files loaded from network as utf8 (#3856)

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -834,6 +834,12 @@ itest!(_053_import_compression {
   http_server: true,
 });
 
+itest!(import_wasm_via_network {
+  args: "run --reload http://127.0.0.1:4545/cli/tests/055_import_wasm_via_network.ts",
+  output: "055_import_wasm_via_network.ts.out",
+  http_server: true,
+});
+
 mod util {
   use deno::colors::strip_ansi_codes;
   pub use deno::test_util::*;
