a649e8c3f2a9d3aa605e424bdea5e5e293179f6e
Chris Knight <cknight1234@gmail.com>
2020-02-04 21:43:42 +0000

    test: apply test patch courtesy of bartlomieju

--- cli/js/dispatch_minimal_test.ts
+++ cli/js/dispatch_minimal_test.ts
@@ -16,15 +16,19 @@ const readErrorStackPattern = new RegExp(
 
 test(async function sendAsyncStackTrace(): Promise<void> {
   const buf = new Uint8Array(10);
-  await Deno.read(10, buf)
-    .then(unreachable)
-    .catch((error): void => {
-      assertMatch(error.stack, readErrorStackPattern);
-    });
+  try {
+    await Deno.read(10, buf);
+    unreachable();
+  } catch (error) {
+    assertMatch(error.stack, readErrorStackPattern);
+  }
 });
+
 test(async function malformedMinimalControlBuffer(): Promise<void> {
   // @ts-ignore
-  const res = Deno.core.send(1, new Uint8Array([1, 2, 3, 4, 5]));
+  const readOpId = Deno.core.ops()["read"];
+  // @ts-ignore
+  const res = Deno.core.send(readOpId, new Uint8Array([1, 2, 3, 4, 5]));
   const header = res.slice(0, 12);
   const buf32 = new Int32Array(
     header.buffer,
@@ -33,7 +37,7 @@ test(async function malformedMinimalControlBuffer(): Promise<void> {
   );
   const arg = buf32[1];
   const result = buf32[2];
-  const message = new TextDecoder().decode(res.slice(12));
+  const message = new TextDecoder().decode(res.slice(12)).trim();
   assert(arg < 0);
   assertEquals(result, Deno.ErrorKind.InvalidInput);
   assertEquals(message, "Unparsable control buffer");
