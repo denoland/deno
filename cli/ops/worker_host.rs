5ad409d806f0afdb096a996e7aa9e1e4651a5f4c
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-02-06 14:32:17 +0100

    fmt

--- cli/ops/worker_host.rs
+++ cli/ops/worker_host.rs
@@ -60,12 +60,12 @@ fn op_create_worker(
   let args_name = args.name;
   let parent_state = state.clone();
 
-
   let (load_sender, load_receiver) =
     std::sync::mpsc::sync_channel::<JsonResult>(1);
 
   let referrer = parent_state.main_module.to_string();
-  let module_specifier = ModuleSpecifier::resolve_import(&specifier, &referrer)?;
+  let module_specifier =
+    ModuleSpecifier::resolve_import(&specifier, &referrer)?;
 
   std::thread::spawn(move || {
     let result = ThreadSafeState::new_for_worker(
