99eeb9a05750c80d4902eeee5ad3f1b0eff8c1ca
Ryan Dahl <ry@tinyclouds.org>
2020-02-07 03:30:05 -0500

    Fix deno types | head (#3910)

--- cli/lib.rs
+++ cli/lib.rs
@@ -124,12 +124,15 @@ fn create_main_worker(
 }
 
 fn types_command() {
-  println!(
+  let types = format!(
     "{}\n{}\n{}",
     crate::js::DENO_NS_LIB,
     crate::js::SHARED_GLOBALS_LIB,
     crate::js::WINDOW_LIB
   );
+  use std::io::Write;
+  let _r = std::io::stdout().write_all(types.as_bytes());
+  // TODO(ry) Only ignore SIGPIPE. Currently ignoring all errors.
 }
 
 fn print_cache_info(state: &GlobalState) {
