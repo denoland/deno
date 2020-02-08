9a5fc4929b8ee8ba0f16064225ee7ad586ba8586
flyyuan <740004544@qq.com>
2020-02-08 17:58:19 +0800

    Add readlink, readlinkSync for std/node/fs.ts

--- std/node/fs.ts
+++ std/node/fs.ts
@@ -3,7 +3,12 @@ import {
   intoCallbackAPIWithIntercept,
   MaybeEmpty
 } from "./_utils.ts";
-const { readFile: denoReadFile, readFileSync: denoReadFileSync } = Deno;
+const {
+  readFile: denoReadFile,
+  readFileSync: denoReadFileSync,
+  readlink: denoReadlink,
+  readlinkSync: denoReadlinkSync
+} = Deno;
 
 type ReadFileCallback = (
   err: MaybeEmpty<Error>,
@@ -13,6 +18,15 @@ type ReadFileCallback = (
 interface ReadFileOptions {
   encoding?: string | null;
   flag?: string;
+}
+
+type ReadlinkCallback = (
+  err: MaybeEmpty<Error>,
+  linkString: MaybeEmpty<string | Uint8Array>
+) => void;
+
+interface ReadlinkOptions {
+  encoding?: string | null;
 }
 
 function getEncoding(
@@ -27,6 +41,8 @@ function getEncoding(
         optOrCallback.encoding === "utf-8"
       ) {
         return "utf8";
+      } else if (optOrCallback.encoding === "buffer") {
+        return "buffer";
       } else {
         notImplemented();
       }
@@ -41,6 +57,16 @@ function maybeDecode(
 ): string | Uint8Array {
   if (encoding === "utf8") {
     return new TextDecoder().decode(data);
+  }
+  return data;
+}
+
+function maybeEncode(
+  data: string,
+  encoding: string | null
+): string | Uint8Array {
+  if (encoding === "buffer") {
+    return new TextEncoder().encode(data);
   }
   return data;
 }
@@ -72,4 +98,33 @@ export function readFileSync(
   opt?: ReadFileOptions
 ): string | Uint8Array {
   return maybeDecode(denoReadFileSync(path), getEncoding(opt));
+}
+
+export function readlink(
+  path: string,
+  optOrCallback: ReadlinkCallback | ReadlinkOptions,
+  callback?: ReadlinkCallback
+): void {
+  let cb: ReadlinkCallback | undefined;
+  if (typeof optOrCallback === "function") {
+    cb = optOrCallback;
+  } else {
+    cb = callback;
+  }
+
+  const encoding = getEncoding(optOrCallback);
+
+  intoCallbackAPIWithIntercept<string, Uint8Array | string>(
+    denoReadlink,
+    (data: string): string | Uint8Array => maybeEncode(data, encoding),
+    cb,
+    path
+  );
+}
+
+export function readlinkSync(
+  path: string,
+  opt?: ReadlinkOptions
+): string | Uint8Array {
+  return maybeEncode(denoReadlinkSync(path), getEncoding(opt));
 }
--- std/node/fs_test.ts
+++ std/node/fs_test.ts
@@ -1,9 +1,15 @@
-import { readFile, readFileSync } from "./fs.ts";
+import { readFile, readFileSync, readlink, readlinkSync } from "./fs.ts";
 import { test } from "../testing/mod.ts";
 import * as path from "../path/mod.ts";
 import { assertEquals, assert } from "../testing/asserts.ts";
+const { run } = Deno;
 
 const testData = path.resolve(path.join("node", "testdata", "hello.txt"));
+const testLinkPath = "./testdata/hello.txt";
+const testLink = "hello.txt";
+run({
+  args: ["ln", "-s", testLinkPath, testLink]
+});
 
 // Need to convert to promises, otherwise test() won't report error correctly.
 test(async function readFileSuccess() {
@@ -44,4 +50,47 @@ test(function readFileEncodeUtf8Success() {
   const data = readFileSync(testData, { encoding: "utf8" });
   assertEquals(typeof data, "string");
   assertEquals(data as string, "hello world");
+});
+
+test(async function readlinkSuccess() {
+  const data = await new Promise((res, rej) => {
+    readlink(testLink, (err, data) => {
+      if (err) {
+        rej(err);
+      }
+      res(data);
+    });
+  });
+
+  assertEquals(typeof data, "string");
+  assertEquals(data as string, testLinkPath);
+});
+
+test(async function readlinkEncodeBufferSuccess() {
+  const data = await new Promise((res, rej) => {
+    readlink(testLink, { encoding: "buffer" }, (err, data) => {
+      if (err) {
+        rej(err);
+      }
+      res(data);
+    });
+  });
+
+  assert(data instanceof Uint8Array);
+  assertEquals(new TextDecoder().decode(data as Uint8Array), testLinkPath);
+});
+
+test(function readlinkSyncSuccess() {
+  const data = readlinkSync(testLink);
+  assertEquals(typeof data, "string");
+  assertEquals(data as string, testLinkPath);
+});
+
+test(function readlinkEncodeBufferSuccess() {
+  const data = readlinkSync(testLink, { encoding: "buffer" });
+  assert(data instanceof Uint8Array);
+  assertEquals(new TextDecoder().decode(data as Uint8Array), testLinkPath);
+  run({
+    args: ["rm", "-rf", testLink]
+  });
 });
