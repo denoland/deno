c78fa119220aac57da44800903709839dc4b6d4c
flyyuan <740004544@qq.com>
2020-02-09 12:48:28 +0800

    Using symlinkSync for creating the links

--- std/node/fs_test.ts
+++ std/node/fs_test.ts
@@ -2,12 +2,15 @@ import { readFile, readFileSync, readlink, readlinkSync } from "./fs.ts";
 import { test } from "../testing/mod.ts";
 import * as path from "../path/mod.ts";
 import { assertEquals, assert } from "../testing/asserts.ts";
-const { run } = Deno;
 
 const testData = path.resolve(path.join("node", "testdata", "hello.txt"));
-const testLinkPath = "./testdata/hello.txt";
-const testLink = "hello.txt";
+const testDir = Deno.makeTempDirSync();
+const oldname = testDir + "/oldname";
+const newname = testDir + "/newname";
 
+Deno.symlinkSync(oldname, newname);
+
+// Deno.removeSync(newname);
 // Need to convert to promises, otherwise test() won't report error correctly.
 test(async function readFileSuccess() {
   const data = await new Promise((res, rej) => {
@@ -49,51 +52,42 @@ test(function readFileEncodeUtf8Success() {
   assertEquals(data as string, "hello world");
 });
 
-if (Deno.build.os !== "win") {
-  run({
-    args: ["ln", "-s", testLinkPath, testLink]
-  });
-
-  test(async function readlinkSuccess() {
-    const data = await new Promise((res, rej) => {
-      readlink(testLink, (err, data) => {
-        if (err) {
-          rej(err);
-        }
-        res(data);
-      });
-    });
-
-    assertEquals(typeof data, "string");
-    assertEquals(data as string, testLinkPath);
-  });
-
-  test(async function readlinkEncodeBufferSuccess() {
-    const data = await new Promise((res, rej) => {
-      readlink(testLink, { encoding: "buffer" }, (err, data) => {
-        if (err) {
-          rej(err);
-        }
-        res(data);
-      });
-    });
-
-    assert(data instanceof Uint8Array);
-    assertEquals(new TextDecoder().decode(data as Uint8Array), testLinkPath);
-  });
-
-  test(function readlinkSyncSuccess() {
-    const data = readlinkSync(testLink);
-    assertEquals(typeof data, "string");
-    assertEquals(data as string, testLinkPath);
-  });
-
-  test(function readlinkEncodeBufferSuccess() {
-    const data = readlinkSync(testLink, { encoding: "buffer" });
-    assert(data instanceof Uint8Array);
-    assertEquals(new TextDecoder().decode(data as Uint8Array), testLinkPath);
-    run({
-      args: ["rm", "-rf", testLink]
+test(async function readlinkSuccess() {
+  const data = await new Promise((res, rej) => {
+    readlink(newname, (err, data) => {
+      if (err) {
+        rej(err);
+      }
+      res(data);
     });
   });
-}
+
+  assertEquals(typeof data, "string");
+  assertEquals(data as string, oldname);
+});
+
+test(async function readlinkEncodeBufferSuccess() {
+  const data = await new Promise((res, rej) => {
+    readlink(newname, { encoding: "buffer" }, (err, data) => {
+      if (err) {
+        rej(err);
+      }
+      res(data);
+    });
+  });
+
+  assert(data instanceof Uint8Array);
+  assertEquals(new TextDecoder().decode(data as Uint8Array), oldname);
+});
+
+test(function readlinkSyncSuccess() {
+  const data = readlinkSync(newname);
+  assertEquals(typeof data, "string");
+  assertEquals(data as string, oldname);
+});
+
+test(function readlinkEncodeBufferSuccess() {
+  const data = readlinkSync(newname, { encoding: "buffer" });
+  assert(data instanceof Uint8Array);
+  assertEquals(new TextDecoder().decode(data as Uint8Array), oldname);
+});
