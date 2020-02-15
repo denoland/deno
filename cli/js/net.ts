eb1dc1483bcbdf8950c23219b2a3c2fe7bf26509
hazæ41 <hazae41@gmail.com>
2020-02-15 22:59:23 +0100

    fix recvfrom return type

--- cli/js/net.ts
+++ cli/js/net.ts
@@ -148,9 +148,8 @@ export class ListenerImpl implements Listener {
   }
 }
 
-export async function recvfrom(rid: number, p: Uint8Array): [number, Addr] {
+export async function recvfrom(rid: number, p: Uint8Array): Promise<[number, Addr]> {
   const { size, remoteAddr } = await sendAsync(dispatch.OP_RECEIVE, { rid }, p);
-
   return [size, remoteAddr];
 }
 
