d8de27a754aaff7098a4bc767937d5aa6359aca0
Kitson Kelly <me@kitsonkelly.com>
2020-02-20 12:12:36 +1100

    Fix emit when bundle contains single module.
    Fixes #4031
    
    When a bundle contains a single module, we were incorrectly determining
    the module name, resulting in a non-functional bundle.  This PR corrects
    that determination.

--- cli/js/compiler_bundler.ts
+++ cli/js/compiler_bundler.ts
@@ -38,7 +38,10 @@ export function buildBundle(
   // specifiers which are used to define the modules, and doesn't expose them
   // publicly, so we have to try to replicate
   const sources = sourceFiles.map(sf => sf.fileName);
-  const sharedPath = commonPath(sources);
+  const sharedPath =
+    sources.length > 1
+      ? commonPath(sources)
+      : rootName.substring(0, rootName.lastIndexOf("/") + 1);
   rootName = normalizeUrl(rootName)
     .replace(sharedPath, "")
     .replace(/\.\w+$/i, "");
