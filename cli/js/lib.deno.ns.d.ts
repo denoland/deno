bd640bc7e6a946dec4477afc64d8083e372660f6
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-02-21 13:21:51 -0500

    feat: Deno.fsEvents() (#3452)

--- cli/js/lib.deno.ns.d.ts
+++ cli/js/lib.deno.ns.d.ts
@@ -1620,6 +1620,21 @@ declare namespace Deno {
    */
   export function resources(): ResourceMap;
 
+  /** UNSTABLE: new API. Needs docs. */
+  export interface FsEvent {
+    kind: "any" | "access" | "create" | "modify" | "remove";
+    paths: string[];
+  }
+
+  /** UNSTABLE: new API. Needs docs.
+   *
+   * recursive option is true by default.
+   */
+  export function fsEvents(
+    paths: string | string[],
+    options?: { recursive: boolean }
+  ): AsyncIterableIterator<FsEvent>;
+
   /** How to handle subprocess stdio.
    *
    * "inherit" The default if unspecified. The child inherits from the
