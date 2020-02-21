58868c5f3fb9d3e8d0e87c491b0759a92c55eef4
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-02-21 00:15:21 -0500

    fmt

--- cli/tests/subdir/fetching_worker.js
+++ cli/tests/subdir/fetching_worker.js
@@ -1,4 +1,6 @@
-const r = await fetch("http://localhost:4545/cli/tests/subdir/fetching_worker.js");
+const r = await fetch(
+  "http://localhost:4545/cli/tests/subdir/fetching_worker.js"
+);
 await r.text();
 postMessage("Done!");
 close();
