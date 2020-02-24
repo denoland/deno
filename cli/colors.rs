763731ee0762172f8f4e5fd29a58a2bdc30b4b41
jsouto18 <joao.jpgs@hotmail.com>
2020-02-24 21:40:06 +0000

    Fixed termial not reseting after color print

--- cli/colors.rs
+++ cli/colors.rs
@@ -33,6 +33,7 @@ fn style(s: &str, colorspec: ColorSpec) -> impl fmt::Display {
     ansi_writer.set_color(&colorspec).unwrap();
   }
   ansi_writer.write_all(s.as_bytes()).unwrap();
+  ansi_writer.reset().unwrap();
   String::from_utf8_lossy(&v).into_owned()
 }
 
