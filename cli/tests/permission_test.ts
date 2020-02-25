e53064c4f22efeb8a4eda2712e15c77d2699a686
João Souto <joao.jpgs@hotmail.com>
2020-02-25 20:36:35 +0000

    Port permission_prompt_tests to Rust (#4129)

---
+++ cli/tests/permission_test.ts
@@ -0,0 +1,34 @@
+// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+const { args, listen, env, exit, makeTempDirSync, readFileSync, run } = Deno;
+
+const name = args[0];
+const test: { [key: string]: Function } = {
+  async readRequired(): Promise<void> {
+    readFileSync("README.md");
+  },
+  writeRequired(): void {
+    makeTempDirSync();
+  },
+  envRequired(): void {
+    env().home;
+  },
+  netRequired(): void {
+    listen({ transport: "tcp", port: 4541 });
+  },
+  runRequired(): void {
+    run({
+      args: [
+        "python",
+        "-c",
+        "import sys; sys.stdout.write('hello'); sys.stdout.flush()"
+      ]
+    });
+  }
+};
+
+if (!test[name]) {
+  console.log("Unknown test:", name);
+  exit(1);
+}
+
+test[name]();
