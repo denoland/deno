79f0dd51054e4b65adfaed30b66ab9989f6fdfd7
Dubiousjim <dubiousjim@gmail.com>
2020-02-27 16:55:57 -0500

    Better perm tests for stat/writeFile
    We verify that the `perm` applied by writeFile are only used when
    creating a new file; also that they are subject to the process's umask
    (here just assumed to be the default 0o022 on Unix, or 0 on Windows).
    
    Perhaps this test should be moved from stat_test.ts to
    write_file_test.ts?

--- cli/js/stat_test.ts
+++ cli/js/stat_test.ts
@@ -196,13 +196,14 @@ if (isWindows) {
       const tempDir = Deno.makeTempDirSync();
       const filename = tempDir + "/test.txt";
       const filename2 = tempDir + "/test2.txt";
-      Deno.writeFileSync(filename, data, { perm: 0o666 });
+      Deno.writeFileSync(filename, data, { perm: 0o626 });
       // Create a link
       Deno.linkSync(filename, filename2);
       const s = Deno.statSync(filename);
       assert(s.dev !== null);
       assert(s.ino !== null);
-      assertEquals(s.perm! & 0o666, 0o666);
+      // assertEquals(s.perm!, 0o626 & ~Deno.umask());
+      assertEquals(s.perm! & 0o777, 0o604); // assume umask 0o022
       assertEquals(s.nlink, 2);
       assert(s.uid !== null);
       assert(s.gid !== null);
