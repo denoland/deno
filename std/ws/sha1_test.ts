b06b3639ab17c2f1a5e2c8cc41b1a7ae1a3a4222
Yusuke Sakurai <kerokerokerop@gmail.com>
2020-02-29 01:17:00 +0900

    [std/ws]  provide default close code for ws.close() (#4172)

--- std/ws/sha1_test.ts
+++ std/ws/sha1_test.ts
@@ -3,20 +3,20 @@ const { test } = Deno;
 import { assertEquals } from "../testing/asserts.ts";
 import { Sha1 } from "./sha1.ts";
 
-test(function testSha1(): void {
+test("[ws/sha] test1", () => {
   const sha1 = new Sha1();
   sha1.update("abcde");
   assertEquals(sha1.toString(), "03de6c570bfe24bfc328ccd7ca46b76eadaf4334");
 });
 
-test(function testSha1WithArray(): void {
+test("[ws/sha] testWithArray", () => {
   const data = Uint8Array.of(0x61, 0x62, 0x63, 0x64, 0x65);
   const sha1 = new Sha1();
   sha1.update(data);
   assertEquals(sha1.toString(), "03de6c570bfe24bfc328ccd7ca46b76eadaf4334");
 });
 
-test(function testSha1WithBuffer(): void {
+test("[ws/sha] testSha1WithBuffer", () => {
   const data = Uint8Array.of(0x61, 0x62, 0x63, 0x64, 0x65);
   const sha1 = new Sha1();
   sha1.update(data.buffer);
