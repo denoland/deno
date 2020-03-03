a0cc1e4c0057ac536a3d72266aa1000bed859b0c
Chris Knight <cknight1234@gmail.com>
2020-03-03 13:56:10 +0000

    feat(std/node): add directory classes (#4087)

---
+++ std/node/_fs_dir.ts
@@ -0,0 +1,114 @@
+import Dirent from "./_fs_dirent.ts";
+
+export default class Dir {
+  private dirPath: string | Uint8Array;
+  private files: Dirent[] = [];
+  private filesReadComplete = false;
+
+  constructor(path: string | Uint8Array) {
+    this.dirPath = path;
+  }
+
+  get path(): string {
+    if (this.dirPath instanceof Uint8Array) {
+      return new TextDecoder().decode(this.dirPath);
+    }
+    return this.dirPath;
+  }
+
+  /**
+   * NOTE: Deno doesn't provide an interface to the filesystem like readdir
+   * where each call to readdir returns the next file.  This function simulates this
+   * behaviour by fetching all the entries on the first call, putting them on a stack
+   * and then popping them off the stack one at a time.
+   *
+   * TODO: Rework this implementation once https://github.com/denoland/deno/issues/4218
+   * is resolved.
+   */
+  read(callback?: Function): Promise<Dirent | null> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (this.initializationOfDirectoryFilesIsRequired()) {
+          const denoFiles: Deno.FileInfo[] = await Deno.readDir(this.path);
+          this.files = denoFiles.map(file => new Dirent(file));
+        }
+        const nextFile = this.files.pop();
+        if (nextFile) {
+          resolve(nextFile);
+          this.filesReadComplete = this.files.length === 0;
+        } else {
+          this.filesReadComplete = true;
+          resolve(null);
+        }
+        if (callback) {
+          callback(null, !nextFile ? null : nextFile);
+        }
+      } catch (err) {
+        if (callback) {
+          callback(err, null);
+        }
+        reject(err);
+      }
+    });
+  }
+
+  readSync(): Dirent | null {
+    if (this.initializationOfDirectoryFilesIsRequired()) {
+      this.files.push(
+        ...Deno.readDirSync(this.path).map(file => new Dirent(file))
+      );
+    }
+    const dirent: Dirent | undefined = this.files.pop();
+    this.filesReadComplete = this.files.length === 0;
+
+    return !dirent ? null : dirent;
+  }
+
+  private initializationOfDirectoryFilesIsRequired(): boolean {
+    return this.files.length === 0 && !this.filesReadComplete;
+  }
+
+  /**
+   * Unlike Node, Deno does not require managing resource ids for reading
+   * directories, and therefore does not need to close directories when
+   * finished reading.
+   */
+  close(callback?: Function): Promise<void> {
+    return new Promise((resolve, reject) => {
+      try {
+        if (callback) {
+          callback(null);
+        }
+        resolve();
+      } catch (err) {
+        if (callback) {
+          callback(err);
+        }
+        reject(err);
+      }
+    });
+  }
+
+  /**
+   * Unlike Node, Deno does not require managing resource ids for reading
+   * directories, and therefore does not need to close directories when
+   * finished reading
+   */
+  closeSync(): void {
+    //No op
+  }
+
+  async *[Symbol.asyncIterator](): AsyncIterableIterator<Dirent> {
+    try {
+      while (true) {
+        const dirent: Dirent | null = await this.read();
+        if (dirent === null) {
+          break;
+        }
+        yield dirent;
+      }
+    } finally {
+      await this.close();
+    }
+  }
+}
---
+++ std/node/_fs_dirent_test.ts
@@ -0,0 +1,123 @@
+const { test } = Deno;
+import { assert, assertEquals, assertThrows } from "../testing/asserts.ts";
+import Dirent from "./_fs_dirent.ts";
+
+class FileInfoMock implements Deno.FileInfo {
+  len = -1;
+  modified = -1;
+  accessed = -1;
+  created = -1;
+  name = "";
+  dev = -1;
+  ino = -1;
+  mode = -1;
+  nlink = -1;
+  uid = -1;
+  gid = -1;
+  rdev = -1;
+  blksize = -1;
+  blocks: number | null = null;
+
+  isFileMock = false;
+  isDirectoryMock = false;
+  isSymlinkMock = false;
+
+  isFile(): boolean {
+    return this.isFileMock;
+  }
+  isDirectory(): boolean {
+    return this.isDirectoryMock;
+  }
+  isSymlink(): boolean {
+    return this.isSymlinkMock;
+  }
+}
+
+test({
+  name: "Block devices are correctly identified",
+  fn() {
+    const fileInfo: FileInfoMock = new FileInfoMock();
+    fileInfo.blocks = 5;
+    assert(new Dirent(fileInfo).isBlockDevice());
+    assert(!new Dirent(fileInfo).isCharacterDevice());
+  }
+});
+
+test({
+  name: "Character devices are correctly identified",
+  fn() {
+    const fileInfo: FileInfoMock = new FileInfoMock();
+    fileInfo.blocks = null;
+    assert(new Dirent(fileInfo).isCharacterDevice());
+    assert(!new Dirent(fileInfo).isBlockDevice());
+  }
+});
+
+test({
+  name: "Directories are correctly identified",
+  fn() {
+    const fileInfo: FileInfoMock = new FileInfoMock();
+    fileInfo.isDirectoryMock = true;
+    fileInfo.isFileMock = false;
+    fileInfo.isSymlinkMock = false;
+    assert(new Dirent(fileInfo).isDirectory());
+    assert(!new Dirent(fileInfo).isFile());
+    assert(!new Dirent(fileInfo).isSymbolicLink());
+  }
+});
+
+test({
+  name: "Files are correctly identified",
+  fn() {
+    const fileInfo: FileInfoMock = new FileInfoMock();
+    fileInfo.isDirectoryMock = false;
+    fileInfo.isFileMock = true;
+    fileInfo.isSymlinkMock = false;
+    assert(!new Dirent(fileInfo).isDirectory());
+    assert(new Dirent(fileInfo).isFile());
+    assert(!new Dirent(fileInfo).isSymbolicLink());
+  }
+});
+
+test({
+  name: "Symlinks are correctly identified",
+  fn() {
+    const fileInfo: FileInfoMock = new FileInfoMock();
+    fileInfo.isDirectoryMock = false;
+    fileInfo.isFileMock = false;
+    fileInfo.isSymlinkMock = true;
+    assert(!new Dirent(fileInfo).isDirectory());
+    assert(!new Dirent(fileInfo).isFile());
+    assert(new Dirent(fileInfo).isSymbolicLink());
+  }
+});
+
+test({
+  name: "File name is correct",
+  fn() {
+    const fileInfo: FileInfoMock = new FileInfoMock();
+    fileInfo.name = "my_file";
+    assertEquals(new Dirent(fileInfo).name, "my_file");
+  }
+});
+
+test({
+  name: "Socket and FIFO pipes aren't yet available",
+  fn() {
+    const fileInfo: FileInfoMock = new FileInfoMock();
+    assertThrows(
+      () => {
+        new Dirent(fileInfo).isFIFO();
+      },
+      Error,
+      "does not yet support"
+    );
+    assertThrows(
+      () => {
+        new Dirent(fileInfo).isSocket();
+      },
+      Error,
+      "does not yet support"
+    );
+  }
+});
