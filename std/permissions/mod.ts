d86962c9a71654db60e39d80fb7f861faef5ce7e
Kitson Kelly <me@kitsonkelly.com>
2020-03-06 11:26:22 +1100

    Improve API based on feedback.

--- std/permissions/mod.ts
+++ std/permissions/mod.ts
@@ -2,10 +2,102 @@
 
 const { PermissionDenied } = Deno.errors;
 
-/** Attempts to grant a given permission, resolving with `true` if it is
+/** Attempts to grant a set of permissions, resolving with the descriptors of
+ * the permissions that are granted.
+ *
+ *      const perms = await grant({ name: "net" }, { name: "read" });
+ *      if (perms.length === 2) {
+ *        // do something cool that connects to the net and reads files
+ *      } else {
+ *        // notify user of missing permissions
+ *      }
+ *
+ * If one of the permissions requires a prompt, the function will attempt to
+ * prompt for it.  The function resolves with all of the granted permissions. */
+export async function grant(
+  ...descriptors: Deno.PermissionDescriptor[]
+): Promise<Deno.PermissionDescriptor[]>;
+/** Attempts to grant a set of permissions, resolving with the descriptors of
+ * the permissions that are granted.
+ *
+ *      const perms = await grant([{ name: "net" }, { name: "read" }]);
+ *      if (perms.length === 2) {
+ *        // do something cool that connects to the net and reads files
+ *      } else {
+ *        // notify user of missing permissions
+ *      }
+ *
+ * If one of the permissions requires a prompt, the function will attempt to
+ * prompt for it.  The function resolves with all of the granted permissions. */
+export async function grant(
+  descriptors: Deno.PermissionDescriptor[]
+): Promise<Deno.PermissionDescriptor[]>;
+export async function grant(
+  descriptor: Deno.PermissionDescriptor[] | Deno.PermissionDescriptor,
+  ...descriptors: Deno.PermissionDescriptor[]
+): Promise<Deno.PermissionDescriptor[]> {
+  const result: Deno.PermissionDescriptor[] = [];
+  descriptors = Array.isArray(descriptor)
+    ? descriptor
+    : [descriptor, ...descriptors];
+  for (const descriptor of descriptors) {
+    let state = (await Deno.permissions.query(descriptor)).state;
+    if (state === "prompt") {
+      state = (await Deno.permissions.request(descriptor)).state;
+    }
+    if (state === "granted") {
+      result.push(descriptor);
+    }
+  }
+  return result;
+}
+
+/** Attempts to grant a set of permissions or rejects.
+ *
+ *      await grantOrThrow({ name: "env" }, { name: "net" });
+ *
+ * If the permission can be prompted for, the function will attempt to prompt.
+ * If any of the permissions are denied, the function will reject for the first
+ * permission that is denied.  If all permissions are granted, the function
+ * will resolve. */
+export async function grantOrThrow(
+  ...descriptors: Deno.PermissionDescriptor[]
+): Promise<void>;
+/** Attempts to grant a set of permissions or rejects.
+ *
+ *      await grantOrThrow([{ name: "env" }, { name: "net" }]);
+ *
+ * If the permission can be prompted for, the function will attempt to prompt.
+ * If any of the permissions are denied, the function will reject for the first
+ * permission that is denied.  If all permissions are granted, the function
+ * will resolve. */
+export async function grantOrThrow(
+  descriptors: Deno.PermissionDescriptor[]
+): Promise<void>;
+export async function grantOrThrow(
+  descriptor: Deno.PermissionDescriptor[] | Deno.PermissionDescriptor,
+  ...descriptors: Deno.PermissionDescriptor[]
+): Promise<void> {
+  descriptors = Array.isArray(descriptor)
+    ? descriptor
+    : [descriptor, ...descriptors];
+  for (const descriptor of descriptors) {
+    let state = (await Deno.permissions.query(descriptor)).state;
+    if (state === "prompt") {
+      state = (await Deno.permissions.request(descriptor)).state;
+    }
+    if (state !== "granted") {
+      throw new PermissionDenied(
+        `Permission for "${descriptor.name}" has been denied.`
+      );
+    }
+  }
+}
+
+/** Attempts to grant the requested permissions, resolving with `true` if it is
  * granted or `false` if it isn't.
  *
- *      if (grant({ name: "env" })) {
+ *      if (await isGranted({ name: "env" })) {
  *        // ...
  *      } else {
  *        console.error("Cannot access the environment.");
@@ -14,7 +106,7 @@ const { PermissionDenied } = Deno.errors;
  * If the permission is denied, the function will resolve with `false`.  If the
  * permission is not granted, but can be prompted for, the function will prompt
  * for it. If the permission is granted, the function resolves with `true`. */
-export async function grant(
+export async function isGranted(
   descriptor: Deno.PermissionDescriptor
 ): Promise<boolean> {
   let state = (await Deno.permissions.query(descriptor)).state;
@@ -22,26 +114,4 @@ export async function grant(
     state = (await Deno.permissions.request(descriptor)).state;
   }
   return state === "granted" ? true : false;
-}
-
-/** Attempts to grant a given permission or throws.
- *
- *      await grantOrThrow({ name: "env" });
- *
- * If the permission is denied, this function will throw with a
- * `Deno.errors.PermissionDenied`.  If the permission is not granted, but can be
- * prompted for, this function will prompt for it.  If the permission is
- * granted, the function resolves.*/
-export async function grantOrThrow(
-  descriptor: Deno.PermissionDescriptor
-): Promise<void> {
-  let state = (await Deno.permissions.query(descriptor)).state;
-  if (state === "prompt") {
-    state = (await Deno.permissions.request(descriptor)).state;
-  }
-  if (state !== "granted") {
-    throw new PermissionDenied(
-      `Permission for "${descriptor.name}" has been denied.`
-    );
-  }
 }
--- std/permissions/test.ts
+++ std/permissions/test.ts
@@ -1,20 +1,47 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
-import { grant, grantOrThrow } from "./mod.ts";
-import { assert } from "../testing/asserts.ts";
+import { grant, grantOrThrow, isGranted } from "./mod.ts";
+import { assert, assertEquals } from "../testing/asserts.ts";
 
 const { test } = Deno;
 
 test({
   name: "grant basic",
   async fn() {
-    assert(await grant({ name: "net" }));
+    assertEquals(await grant({ name: "net" }, { name: "env" }), [
+      { name: "net" },
+      { name: "env" }
+    ]);
+  }
+});
+
+test({
+  name: "grant array",
+  async fn() {
+    assertEquals(await grant([{ name: "net" }, { name: "env" }]), [
+      { name: "net" },
+      { name: "env" }
+    ]);
   }
 });
 
 test({
   name: "grantOrThrow basic",
   async fn() {
-    await grantOrThrow({ name: "net" });
+    await grantOrThrow({ name: "net" }, { name: "env" });
+  }
+});
+
+test({
+  name: "grantOrThrow array",
+  async fn() {
+    await grantOrThrow([{ name: "net" }, { name: "env" }]);
+  }
+});
+
+test({
+  name: "isGranted basic",
+  async fn() {
+    assert(await isGranted({ name: "net" }));
   }
 });
