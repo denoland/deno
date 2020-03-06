4a9817c3c4f229453a04a8fef99eb38b7b23ca5d
Ryan Dahl <ry@tinyclouds.org>
2020-03-06 11:40:18 -0500

    Fix flaky test test_permissions_net_listen_allow_localhost (#4271)

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -1889,8 +1889,9 @@ fn test_permissions_net_listen_allow_localhost_4555_fail() {
 
 #[test]
 fn test_permissions_net_listen_allow_localhost() {
+  // Port 4600 is chosen to not colide with those used by tools/http_server.py
   let (_, err, code) = util::run_and_collect_output(
-			"run --allow-net=localhost complex_permissions_test.ts netListen localhost:4545 localhost:4546 localhost:4547",
+			"run --allow-net=localhost complex_permissions_test.ts netListen localhost:4600",
 			None,
 			None,
 			false,
