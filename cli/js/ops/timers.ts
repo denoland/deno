5f708d97ee687a650ab564a1d45433099bedb523
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-03-09 12:53:50 +0100

    cli/js/ops/timers.ts

---
+++ cli/js/ops/timers.ts
@@ -0,0 +1,10 @@
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+import { sendSync, sendAsync } from "./dispatch_json.ts";
+
+export function stopGlobalTimer(): void {
+  sendSync("op_global_timer_stop");
+}
+
+export async function startGlobalTimer(timeout: number): Promise<void> {
+  await sendAsync("op_global_timer", { timeout });
+}
