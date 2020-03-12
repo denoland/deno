3b91b9e7467269bd7a30925442fc78f9015824c8
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-03-12 23:48:49 +0100

    small cleanup

--- cli/js/tests/unit_test_runner.ts
+++ cli/js/tests/unit_test_runner.ts
@@ -3,15 +3,17 @@
 import { readLines } from "../../../std/io/bufio.ts";
 import "./unit_tests.ts";
 import {
+  assert,
   permissionCombinations,
   Permissions,
   registerUnitTests
 } from "./test_util.ts";
-import { assert } from "../../../std/testing/asserts.ts";
 
-interface TestResult {
-  perms: string;
-  output?: string;
+interface PermissionSetTestResult {
+  perms: Permissions;
+  passed: boolean;
+  stats: Deno.TestStats;
+  permsStr: string;
   result: number;
 }
 
@@ -88,6 +90,7 @@ async function workerRunnerMain(
   // Create reporter, then drop permissions to requested set
   // const fileReporter = new FileReporter(filename);
   const conn = await Deno.connect(addr);
+  console.log("Test worker online", addr, permissions);
   const socketReporter = new SocketReporter(conn);
   await dropWorkerPermissions(permissions);
 
@@ -102,20 +105,6 @@ async function workerRunnerMain(
   });
   console.log("worker finished running tests", results.stats);
   socketReporter.close();
-}
-
-function permsToStrings(perms: Permissions): string[] {
-  return Object.keys(perms)
-    .map(key => {
-      if (!perms[key as keyof Permissions]) return "";
-
-      const cliFlag = key.replace(
-        /\.?([A-Z])/g,
-        (x, y): string => `-${y.toLowerCase()}`
-      );
-      return `${cliFlag}`;
-    })
-    .filter((e): boolean => e.length > 0);
 }
 
 function permsToCliFlags(perms: Permissions): string[] {
@@ -143,9 +132,13 @@ function fmtPerms(perms: Permissions): string {
 }
 
 function spawnWorkerRunner(addr: string, perms: Permissions): Deno.Process {
-  const permStrs = permsToStrings(perms);
-
   // run subsequent tests using same deno executable
+  const permStr = Object.keys(perms)
+    .filter((permName): boolean => {
+      return perms[permName as Deno.PermissionName] === true;
+    })
+    .join(",");
+
   const args = [
     Deno.execPath(),
     "run",
@@ -154,7 +147,7 @@ function spawnWorkerRunner(addr: string, perms: Permissions): Deno.Process {
     "--",
     "--worker",
     `--addr=${addr}`,
-    `--perms=${permStrs.join(",")}`
+    `--perms=${permStr}`
   ];
 
   const p = Deno.run({
@@ -162,6 +155,85 @@ function spawnWorkerRunner(addr: string, perms: Permissions): Deno.Process {
   });
 
   return p;
+}
+
+async function runTestsForPermissionSet(
+  reporter: Deno.ConsoleReporter,
+  perms: Permissions
+): Promise<PermissionSetTestResult> {
+  const permsFmt = fmtPerms(perms);
+  console.log(`Running tests for: ${permsFmt}`);
+  const addr = { hostname: "127.0.0.1", port: 4510 };
+  const addrStr = `${addr.hostname}:${addr.port}`;
+  const workerListener = Deno.listen(addr);
+
+  const workerProcess = spawnWorkerRunner(addrStr, perms);
+
+  // Wait for worker subprocess to go online
+  const conn = await workerListener.accept();
+
+  let err;
+  let hasThrown = false;
+  let expectedTests;
+  let permTestResult;
+
+  try {
+    for await (const line of readLines(conn)) {
+      const msg = JSON.parse(line);
+
+      if (msg.kind === "start") {
+        expectedTests = msg.tests;
+        await reporter.start(msg);
+        continue;
+      }
+
+      if (msg.kind === "test") {
+        await reporter.test(msg);
+        continue;
+      }
+
+      permTestResult = msg.stats;
+      await reporter.end(msg);
+      break;
+    }
+  } catch (e) {
+    hasThrown = true;
+    err = e;
+  } finally {
+    workerListener.close();
+  }
+
+  if (hasThrown) {
+    throw err;
+  }
+
+  if (typeof expectedTests === "undefined") {
+    throw new Error("Worker runner didn't report start");
+  }
+
+  if (typeof permTestResult === "undefined") {
+    throw new Error("Worker runner didn't report end");
+  }
+
+  const workerStatus = await workerProcess.status();
+  if (!workerStatus.success) {
+    throw new Error(
+      `Worker runner exited with status code: ${workerStatus.code}`
+    );
+  }
+
+  workerProcess.close();
+
+  const actual = permTestResult.passed;
+  console.log("expected", expectedTests, "actual", actual);
+  const result = expectedTests === actual ? 0 : 1;
+  return {
+    perms,
+    passed: expectedTests === actual,
+    permsStr: permsFmt,
+    result,
+    stats: permTestResult
+  };
 }
 
 async function masterRunnerMain(): Promise<void> {
@@ -174,97 +246,32 @@ async function masterRunnerMain(): Promise<void> {
     console.log("\t" + fmtPerms(perms));
   }
 
-  const testResults = new Set<TestResult>();
+  const testResults = new Set<PermissionSetTestResult>();
   const consoleReporter = new Deno.ConsoleReporter();
 
   for (const perms of permissionCombinations.values()) {
-    const permsFmt = fmtPerms(perms);
-    console.log(`Running tests for: ${permsFmt}`);
-
-    const addr = { hostname: "127.0.0.1", port: 4510 };
-    const addrStr = `${addr.hostname}:${addr.port}`;
-    const workerListener = Deno.listen(addr);
-
-    const workerProcess = spawnWorkerRunner(addrStr, perms);
-
-    // Wait for worker subprocess to go online
-    const conn = await workerListener.accept();
-    const linesIterator = readLines(conn);
-
-    let err;
-    let hasThrown = false;
-    let expectedTests;
-    let permTestResult;
-
-    try {
-      const { value } = await linesIterator.next();
-      const msg = JSON.parse(value);
-      assert(msg.kind === "start");
-      expectedTests = msg.tests;
-      consoleReporter.start(msg);
-
-      for await (const line of readLines(conn)) {
-        const msg = JSON.parse(line);
-
-        if (msg.kind === "test") {
-          await consoleReporter.test(msg);
-          continue;
-        }
-
-        permTestResult = msg.stats;
-        await consoleReporter.end(msg);
-        break;
-      }
-    } catch (e) {
-      hasThrown = true;
-      err = e;
-    } finally {
-      workerListener.close();
-    }
-
-    if (hasThrown) {
-      throw err;
-    }
-
-    if (typeof expectedTests === "undefined") {
-      throw new Error("Worker runner didn't report start");
-    }
-
-    if (typeof permTestResult === "undefined") {
-      throw new Error("Worker runner didn't report end");
-    }
-
-    const workerStatus = await workerProcess.status();
-    if (!workerStatus.success) {
-      throw new Error(
-        `Worker runner exited with status code: ${workerStatus.code}`
-      );
-    }
-
-    workerProcess.close();
-
-    const actual = permTestResult.passed;
-    console.log("expected", expectedTests, "actual", actual);
-    const result = expectedTests === actual ? 0 : 1;
-
-    testResults.add({
-      perms: permsFmt,
-      output: "",
-      result
-    });
+    const result = await runTestsForPermissionSet(consoleReporter, perms);
+    testResults.add(result);
   }
 
   // if any run tests returned non-zero status then whole test
   // run should fail
-  let testsFailed = false;
+  let testsPassed = true;
 
   for (const testResult of testResults) {
-    console.log(`Summary for ${testResult.perms} ${testResult.result}`);
-    console.log(testResult.output + "\n");
-    testsFailed = testsFailed || Boolean(testResult.result);
+    const { permsStr, stats } = testResult;
+    // TODO: use common functionality from ConsoleReporter
+    console.log(`Summary for ${permsStr}`);
+    console.log(
+      `test result: ${stats.failed ? "FAIL" : "OK"} ` +
+        `${stats.passed} passed; ${stats.failed} failed; ` +
+        `${stats.ignored} ignored; ${stats.measured} measured; ` +
+        `${stats.filtered} filtered out\n`
+    );
+    testsPassed = testsPassed && testResult.passed;
   }
 
-  if (testsFailed) {
+  if (!testsPassed) {
     console.error("Unit tests failed");
     Deno.exit(1);
   }
@@ -273,21 +280,30 @@ async function masterRunnerMain(): Promise<void> {
 }
 
 async function main(): Promise<void> {
-  const args = Deno.args.slice(1);
-  console.log("worker main", args);
-
   // TODO: use `flags` parser
-  if (args[0] === "--worker") {
-    // Worker runner
-    const addr = args[1].split("=")[1];
-    const perms = args[2].split("=")[1].split(",") as Deno.PermissionName[];
-    console.log("test worker", addr, perms);
-    const [hostname, port] = addr.split(":");
-    await workerRunnerMain({ hostname, port: Number(port) }, perms);
-  } else {
-    // Master runner
-    await masterRunnerMain();
+  const args = Deno.args;
+
+  const isWorker = args.includes("--worker");
+
+  if (!isWorker) {
+    return await masterRunnerMain();
   }
+
+  const addrArg = args.find(e => e.includes("--addr"));
+  assert(typeof addrArg === "string");
+  const addrStr = addrArg.split("=")[1];
+  const [hostname, port] = addrStr.split(":");
+  const addr = { hostname, port: Number(port) };
+
+  let perms: Deno.PermissionName[] = [];
+  const permsArg = args.find(e => e.includes("--perms"));
+  assert(typeof permsArg === "string");
+  const permsStr = permsArg.split("=")[1];
+  if (permsStr.length > 0) {
+    perms = permsStr.split(",") as Deno.PermissionName[];
+  }
+
+  await workerRunnerMain(addr, perms);
 }
 
 main();
