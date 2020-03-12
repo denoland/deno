957c38d739039950a4f8f6390300e1508739078b
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-03-12 15:37:18 +0100

    use TestApi

--- cli/js/testing.ts
+++ cli/js/testing.ts
@@ -3,6 +3,10 @@ import { red, green, bgRed, gray, italic } from "./colors.ts";
 import { exit } from "./ops/os.ts";
 import { Console } from "./web/console.ts";
 
+const RED_FAILED = red("FAILED");
+const GREEN_OK = green("OK");
+const RED_BG_FAIL = bgRed(" FAIL ");
+
 function formatDuration(time = 0): string {
   const timeStr = `(${time}ms)`;
   return gray(italic(timeStr));
@@ -85,12 +89,122 @@ export interface RunTestsOptions {
   disableLog?: boolean;
 }
 
-function filterTests(
-  tests: TestDefinition[],
+type FilterFn = (testDef: TestDefinition) => boolean;
+
+interface TestResult {
+  passed: boolean;
+  name: string;
+  fn: TestFunction;
+  skipped: boolean;
+  hasRun: boolean;
+  duration: number;
+  error?: Error;
+}
+
+enum MsgKind {
+  Start = "start",
+  Test = "test",
+  End = "end"
+}
+
+interface StartMsg {
+  kind: MsgKind.Start;
+  tests: number;
+  stats: TestStats;
+}
+
+interface TestMsg {
+  kind: MsgKind.Test;
+  result: TestResult;
+}
+
+interface EndMsg {
+  kind: MsgKind.End;
+  stats: TestStats;
+}
+
+type RunTestsMessage = StartMsg | TestMsg | EndMsg;
+
+// interface TestsResult {
+//   results: TestResult[];
+//   stats: TestStats;
+// }
+
+function testDefinitionToResult(def: TestDefinition): TestResult {
+  return {
+    fn: def.fn,
+    name: def.name,
+    passed: false,
+    skipped: false,
+    hasRun: false,
+    duration: 0
+  };
+}
+
+// TODO: implements AsyncGenerator<RunTestsMessage>
+// TODO: implements PromiseLike<TestsResult>
+class TestApi {
+  readonly stats: TestStats;
+  readonly testsToRun: TestDefinition[];
+  readonly results: TestResult[];
+
+  constructor(
+    public tests: TestDefinition[],
+    public filterFn: FilterFn,
+    public failFast: boolean
+  ) {
+    this.stats = {
+      filtered: 0,
+      ignored: 0,
+      measured: 0,
+      passed: 0,
+      failed: 0
+    };
+    this.testsToRun = tests.filter(filterFn);
+    this.stats.filtered = tests.length - this.testsToRun.length;
+    this.results = this.testsToRun.map(testDefinitionToResult);
+  }
+
+  async *[Symbol.asyncIterator](): AsyncIterator<RunTestsMessage> {
+    yield {
+      kind: MsgKind.Start,
+      stats: this.stats,
+      tests: this.testsToRun.length
+    };
+
+    for (const testResult of this.results) {
+      let shouldBreak = false;
+      try {
+        const start = +new Date();
+        await testResult.fn();
+        testResult.duration = +new Date() - start;
+        testResult.passed = true;
+        this.stats.passed++;
+      } catch (err) {
+        testResult.passed = false;
+        testResult.error = err;
+        this.stats.failed++;
+        if (this.failFast) {
+          shouldBreak = true;
+        }
+      } finally {
+        testResult.hasRun = true;
+        yield { kind: MsgKind.Test, result: testResult };
+        if (shouldBreak) {
+          break;
+        }
+      }
+    }
+
+    yield { kind: MsgKind.End, stats: this.stats };
+  }
+}
+
+function createFilterFn(
   only: undefined | string | RegExp,
   skip: undefined | string | RegExp
-): TestDefinition[] {
-  return tests.filter((def: TestDefinition): boolean => {
+): (def: TestDefinition) => boolean {
+  return (def: TestDefinition): boolean => {
     let passes = true;
 
     if (only) {
@@ -110,7 +224,7 @@ function filterTests(
     }
 
     return passes;
-  });
+  };
 }
 
 export async function runTests({
@@ -120,26 +234,8 @@ export async function runTests({
   skip = undefined,
   disableLog = false
 }: RunTestsOptions = {}): Promise<void> {
-  const testsToRun = filterTests(TEST_REGISTRY, only, skip);
-
-  const stats: TestStats = {
-    measured: 0,
-    ignored: 0,
-    filtered: 0,
-    passed: 0,
-    failed: 0
-  };
-
-  const testCases = testsToRun.map(
-    ({ name, fn }): TestCase => {
-      return {
-        name,
-        fn,
-        timeElapsed: 0,
-        error: undefined
-      };
-    }
-  );
+  const filterFn = createFilterFn(only, skip);
+  const testApi = new TestApi(TEST_REGISTRY, filterFn, failFast);
 
   // @ts-ignore
   const originalConsole = globalThis.console;
@@ -154,50 +250,46 @@ export async function runTests({
     globalThis.console = disabledConsole;
   }
 
-  const RED_FAILED = red("FAILED");
-  const GREEN_OK = green("OK");
-  const RED_BG_FAIL = bgRed(" FAIL ");
-
-  originalConsole.log(`running ${testsToRun.length} tests`);
   const suiteStart = +new Date();
+  let stats: TestStats;
 
-  for (const testCase of testCases) {
-    try {
-      const start = +new Date();
-      await testCase.fn();
-      testCase.timeElapsed = +new Date() - start;
-      originalConsole.log(
-        `${GREEN_OK}     ${testCase.name} ${formatDuration(
-          testCase.timeElapsed
-        )}`
-      );
-      stats.passed++;
-    } catch (err) {
-      testCase.error = err;
-      originalConsole.log(`${RED_FAILED} ${testCase.name}`);
-      originalConsole.log(err.stack);
-      stats.failed++;
-      if (failFast) {
-        break;
-      }
+  for await (const testMsg of testApi) {
+    if (testMsg.kind === MsgKind.Start) {
+      originalConsole.log(`running ${testMsg.tests} tests`);
+      continue;
     }
-  }
 
-  const suiteDuration = +new Date() - suiteStart;
+    if (testMsg.kind === MsgKind.Test) {
+      const { result } = testMsg;
+
+      if (result.passed) {
+        originalConsole.log(
+          `${GREEN_OK}     ${result.name} ${formatDuration(result.duration)}`
+        );
+      } else {
+        originalConsole.log(`${RED_FAILED} ${result.name}`);
+        originalConsole.log(result.error!.stack);
+      }
+
+      continue;
+    }
+
+    stats = testMsg.stats;
+    const suiteDuration = +new Date() - suiteStart;
+    // Attempting to match the output of Rust's test runner.
+    originalConsole.log(
+      `\ntest result: ${stats.failed ? RED_BG_FAIL : GREEN_OK} ` +
+        `${stats.passed} passed; ${stats.failed} failed; ` +
+        `${stats.ignored} ignored; ${stats.measured} measured; ` +
+        `${stats.filtered} filtered out ` +
+        `${formatDuration(suiteDuration)}\n`
+    );
+  }
 
   if (disableLog) {
     // @ts-ignore
     globalThis.console = originalConsole;
   }
-
-  // Attempting to match the output of Rust's test runner.
-  originalConsole.log(
-    `\ntest result: ${stats.failed ? RED_BG_FAIL : GREEN_OK} ` +
-      `${stats.passed} passed; ${stats.failed} failed; ` +
-      `${stats.ignored} ignored; ${stats.measured} measured; ` +
-      `${stats.filtered} filtered out ` +
-      `${formatDuration(suiteDuration)}\n`
-  );
 
   // TODO(bartlomieju): is `defer` really needed? Shouldn't unhandled
   // promise rejection be handled per test case?
@@ -205,9 +297,9 @@ export async function runTests({
   // promise rejections being swallowed.
   await defer(0);
 
-  if (stats.failed > 0) {
-    originalConsole.error(`There were ${stats.failed} test failures.`);
-    testCases
+  if (stats!.failed > 0) {
+    originalConsole.error(`There were ${stats!.failed} test failures.`);
+    testApi.results
       .filter(testCase => !!testCase.error)
       .forEach(testCase => {
         originalConsole.error(`${RED_BG_FAIL} ${red(testCase.name)}`);
