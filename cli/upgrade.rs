e135aa1224074d0944792592d0b60f4710f3cb75
Bartossh <lenart.consulting@gmail.com>
2020-03-12 11:49:52 +0100

    add docs and tests

--- cli/upgrade.rs
+++ cli/upgrade.rs
@@ -1,28 +1,29 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-#[cfg(unix)]
+
+//! This module provides feature to upgrade deno executable
+//!
+//! At the moment it is only consumed using CLI but in
+//! the future it can be easily extended to provide
+//! the same functions as ops available in JS runtime.
+
 extern crate flate2;
-#[cfg(windows)]
-extern crate unzip;
 use crate::fs::write_file;
 use crate::futures::FutureExt;
 use crate::{
   http_util::{fetch_once, FetchOnceResult},
   version, ErrBox,
 };
-#[cfg(unix)]
 use flate2::write::GzDecoder;
 use regex::Regex;
 use reqwest::{redirect::Policy, Client};
 use std::env::current_exe;
+use std::fs::{remove_file, rename};
 use std::future::Future;
 use std::io::prelude::*;
 use std::path::Path;
 use std::pin::Pin;
 use std::string::String;
-#[cfg(windows)]
-use unzip::Unzipper;
 use url::Url;
-use std::fs::{rename, remove_file};
 
 lazy_static! {
   static ref LATEST_VERSION_URL: String =
@@ -51,10 +52,11 @@ impl ErrorMsg {
   }
 }
 
-// TODO: docs
+/// Asynchronously updates deno executable to greatest version
+/// if newest version is available.
 pub async fn exec_upgrade() -> Result<(), ErrBox> {
   let client = Client::builder().redirect(Policy::none()).build()?;
-  println!("Checking for latest version.");
+  println!("Checking for latest version");
   let body = client
     .get(Url::parse(&LATEST_VERSION_URL)?)
     .send()
@@ -62,8 +64,7 @@ pub async fn exec_upgrade() -> Result<(), ErrBox> {
     .text()
     .await?;
   let checked_version = find_version(&body)?;
-  if is_latest_version_greater(&version::DENO.to_string(), &checked_version)
-  {
+  if is_latest_version_greater(&version::DENO.to_string(), &checked_version) {
     println!(
       "New version has been found.\nDeno is upgrading to version {}",
       &checked_version
@@ -72,8 +73,8 @@ pub async fn exec_upgrade() -> Result<(), ErrBox> {
       download_package(&compose_url_to_exec(&checked_version)?, client).await?;
     let path = current_exe()?;
     unpack(archive, &path)?;
-    replace(&path)?;
-    println!("Upgrade successful")
+    replace_exec(&path)?;
+    println!("Upgrade done successfully")
   } else {
     println!("Local deno version {} is the greatest one", &version::DENO);
   }
@@ -88,7 +89,9 @@ fn download_package(
   let fut = async move {
     match fetch_once(client.clone(), &url, None).await? {
       FetchOnceResult::Code(source, _) => Ok(source),
-      FetchOnceResult::NotModified => Ok(Vec::new()), // TODO: this should be handled better
+      FetchOnceResult::NotModified => Err(
+        ErrorMsg(format!("Cannot fetch data from url: {}", &url)).to_err_box(),
+      ),
       FetchOnceResult::Redirect(_url, _) => {
         download_package(&_url, client).await
       }
@@ -128,25 +131,60 @@ fn is_latest_version_greater(old_v: &String, new_v: &String) -> bool {
   old_v_num < new_v_num
 }
 
-#[cfg(unix)]
 fn unpack(archive: Vec<u8>, path: &Path) -> Result<(), ErrBox> {
   let mut exec = Vec::new();
   let mut decoder = GzDecoder::new(exec);
   decoder.write_all(&archive[..])?;
   decoder.try_finish()?;
   exec = decoder.finish()?;
-  write_file::<Vec<u8>>(&path.with_file_name(DENO_EXEC_TEMP_NAME.as_str()), exec, 0o777)?;
+  write_file::<Vec<u8>>(
+    &path.with_file_name(DENO_EXEC_TEMP_NAME.as_str()),
+    exec,
+    0o777,
+  )?;
   Ok(())
 }
 
-#[cfg(windows)]
-fn unpack(_exec: Vec<u8>, _path: &Path) -> Result<(), ErrBox> {
-  // TODO: use Unzipper
-  Ok(())
-}
-
-fn replace(path: &Path) -> Result<(), ErrBox> {
+fn replace_exec(path: &Path) -> Result<(), ErrBox> {
   remove_file(path)?;
   rename(path.with_file_name(DENO_EXEC_TEMP_NAME.as_str()), path)?;
   Ok(())
+}
+
+#[cfg(test)]
+mod test {
+  #[test]
+  fn test_is_latest_version_greater() {
+    let mut version = "0.0.0".to_string();
+    let versions = [
+      "0.0.1".to_string(),
+      "0.1.0".to_string(),
+      "1.0.0".to_string(),
+      "11.22.33".to_string(),
+      "22.0.44".to_string(),
+      "30.0.0".to_string(),
+    ];
+    for v in versions.iter() {
+      assert_eq!(super::is_latest_version_greater(&version, v), true);
+      version = v.clone();
+    }
+    assert_eq!(super::is_latest_version_greater(&version, &version), false);
+  }
+
+  #[test]
+  fn test_find_version() {
+    let url = "<html><body>You are being <a href=\"https://github.com/denoland/deno/releases/tag/v0.36.0\">redirected</a>.</body></html>".to_string();
+    assert_eq!(super::find_version(&url).unwrap(), "0.36.0".to_string());
+  }
+
+  #[test]
+  fn test_compose_url_to_exec() {
+    let url = super::compose_url_to_exec(&"0.0.1".to_string()).unwrap();
+    #[cfg(windows)]
+    assert_eq!(url.to_string(), "https://github.com/denoland/deno/releases/download/v0.0.1/deno_win_x64.zip".to_string());
+    #[cfg(macos)]
+    assert_eq!(url.to_string(), "https://github.com/denoland/deno/releases/download/v0.0.1/deno_osx_x64.gz".to_string());
+    #[cfg(unix)]
+    assert_eq!(url.to_string(), "https://github.com/denoland/deno/releases/download/v0.0.1/deno_linux_x64.gz".to_string());
+  }
 }
