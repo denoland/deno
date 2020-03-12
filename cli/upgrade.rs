6b1b00e418deb95c6d154433e5be4d7d58050d26
Bartossh <lenart.consulting@gmail.com>
2020-03-12 10:20:42 +0100

    upgrade for unix

--- cli/upgrade.rs
+++ cli/upgrade.rs
@@ -1,11 +1,28 @@
-use crate::{http_util::{fetch_once, FetchOnceResult}, version, ErrBox};
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+#[cfg(unix)]
+extern crate flate2;
+#[cfg(windows)]
+extern crate unzip;
+use crate::fs::write_file;
 use crate::futures::FutureExt;
+use crate::{
+  http_util::{fetch_once, FetchOnceResult},
+  version, ErrBox,
+};
+#[cfg(unix)]
+use flate2::write::GzDecoder;
 use regex::Regex;
 use reqwest::{redirect::Policy, Client};
-use std::future::{Future};
 use std::env::current_exe;
+use std::future::Future;
+use std::io::prelude::*;
+use std::path::Path;
 use std::pin::Pin;
+use std::string::String;
+#[cfg(windows)]
+use unzip::Unzipper;
 use url::Url;
+use std::fs::{rename, remove_file};
 
 lazy_static! {
   static ref LATEST_VERSION_URL: String =
@@ -13,12 +30,29 @@ lazy_static! {
   static ref EXEC_DOWNLOAD_URL: String =
     "https://github.com/denoland/deno/releases/download/v".to_string();
   static ref REGEX_STRING: String = r#"v([^\?]+)?""#.to_string();
+  static ref DENO_EXEC_TEMP_NAME: String = "deno_temp".to_string();
 }
 
+#[cfg(windows)]
+const EXEC_FILE_NAME: &str = "deno_win_x64.zip";
+#[cfg(macos)]
+const EXEC_FILE_NAME: &str = "deno_osx_x64.gz";
+#[cfg(unix)]
+const EXEC_FILE_NAME: &str = "deno_linux_x64.gz";
+
+struct ErrorMsg(String);
+
+impl ErrorMsg {
+  fn to_err_box(&self) -> ErrBox {
+    ErrBox::from(std::io::Error::new(
+      std::io::ErrorKind::Other,
+      self.0.clone(),
+    ))
+  }
+}
 
 // TODO: docs
 pub async fn exec_upgrade() -> Result<(), ErrBox> {
-  println!("{:?}", current_exe()); // TODO: use current_exe to set path to copy over exec
   let client = Client::builder().redirect(Policy::none()).build()?;
   println!("Checking for latest version.");
   let body = client
@@ -28,17 +62,25 @@ pub async fn exec_upgrade() -> Result<(), ErrBox> {
     .text()
     .await?;
   let checked_version = find_version(&body)?;
-  if is_latest_version_greater(&version::DENO.to_string(), &checked_version) {
-    println!("Deno found greater version.\nDeno is upgrading to version {}", &checked_version);
-    let exec = download_exec(&compose_url_to_exec(&checked_version)?, client).await?;
-    println!("{:?}", &exec.len());
+  if is_latest_version_greater(&version::DENO.to_string(), &checked_version)
+  {
+    println!(
+      "New version has been found.\nDeno is upgrading to version {}",
+      &checked_version
+    );
+    let archive =
+      download_package(&compose_url_to_exec(&checked_version)?, client).await?;
+    let path = current_exe()?;
+    unpack(archive, &path)?;
+    replace(&path)?;
+    println!("Upgrade successful")
   } else {
     println!("Local deno version {} is the greatest one", &version::DENO);
   }
   Ok(())
 }
 
-fn download_exec(
+fn download_package(
   url: &Url,
   client: Client,
 ) -> Pin<Box<dyn Future<Output = Result<Vec<u8>, ErrBox>>>> {
@@ -46,8 +88,10 @@ fn download_exec(
   let fut = async move {
     match fetch_once(client.clone(), &url, None).await? {
       FetchOnceResult::Code(source, _) => Ok(source),
-      FetchOnceResult::NotModified => Ok(Vec::new()),
-      FetchOnceResult::Redirect(_url, _) => download_exec(&_url, client).await,
+      FetchOnceResult::NotModified => Ok(Vec::new()), // TODO: this should be handled better
+      FetchOnceResult::Redirect(_url, _) => {
+        download_package(&_url, client).await
+      }
     }
   };
   fut.boxed_local()
@@ -56,15 +100,8 @@ fn download_exec(
 fn compose_url_to_exec(version: &String) -> Result<Url, ErrBox> {
   let mut url_str = EXEC_DOWNLOAD_URL.clone();
   url_str.push_str(&format!("{}/", version));
-  if cfg!(target_os = "windows") {
-    url_str.push_str("deno_win_x64.zip");
-  } else if cfg!(target_os = "macos") {
-    url_str.push_str("deno_osx_x64.gz");
-  } else {
-    url_str.push_str("deno_linux_x64.gz");
-  }
-  let url = Url::parse(&url_str[..])?;
-  Ok(url)
+  url_str.push_str(&EXEC_FILE_NAME);
+  Ok(Url::parse(&url_str[..])?)
 }
 
 fn find_version(text: &String) -> Result<String, ErrBox> {
@@ -73,11 +110,7 @@ fn find_version(text: &String) -> Result<String, ErrBox> {
     let mat = _mat.as_str();
     return Ok(mat[1..mat.len() - 1].to_string());
   }
-  let e = std::io::Error::new(
-    std::io::ErrorKind::Other,
-    "Cannot read latest tag version".to_string(),
-  );
-  Err(ErrBox::from(e))
+  Err(ErrorMsg("Cannot read latest tag version".to_string()).to_err_box())
 }
 
 fn is_latest_version_greater(old_v: &String, new_v: &String) -> bool {
@@ -93,4 +126,27 @@ fn is_latest_version_greater(old_v: &String, new_v: &String) -> bool {
       power -= 2;
     });
   old_v_num < new_v_num
+}
+
+#[cfg(unix)]
+fn unpack(archive: Vec<u8>, path: &Path) -> Result<(), ErrBox> {
+  let mut exec = Vec::new();
+  let mut decoder = GzDecoder::new(exec);
+  decoder.write_all(&archive[..])?;
+  decoder.try_finish()?;
+  exec = decoder.finish()?;
+  write_file::<Vec<u8>>(&path.with_file_name(DENO_EXEC_TEMP_NAME.as_str()), exec, 0o777)?;
+  Ok(())
+}
+
+#[cfg(windows)]
+fn unpack(_exec: Vec<u8>, _path: &Path) -> Result<(), ErrBox> {
+  // TODO: use Unzipper
+  Ok(())
+}
+
+fn replace(path: &Path) -> Result<(), ErrBox> {
+  remove_file(path)?;
+  rename(path.with_file_name(DENO_EXEC_TEMP_NAME.as_str()), path)?;
+  Ok(())
 }
