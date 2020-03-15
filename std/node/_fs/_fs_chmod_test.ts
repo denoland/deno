be277b09efe483e8a30492911fb0ca3d5f5f06a3
Chris Knight <cknight1234@gmail.com>
2020-03-15 13:23:15 +0000

    refactor: move existing fs implementation to internal _fs directory

--- std/node/_fs/_fs_chmod_test.ts
+++ std/node/_fs/_fs_chmod_test.ts
@@ -3,46 +3,46 @@ const { test } = Deno;
 import { fail, assert } from "../../testing/asserts.ts";
 import { chmod, chmodSync } from "./_fs_chmod.ts";
 
-if (Deno.build.os !== "win") {
-  test({
-    name: "ASYNC: Permissions are changed (non-Windows)",
-    async fn() {
-      const tempFile: string = await Deno.makeTempFile();
-      const originalFileMode: number | null = (await Deno.lstat(tempFile)).mode;
-      await new Promise((resolve, reject) => {
-        chmod(tempFile, 0o777, err => {
-          if (err) reject(err);
-          else resolve();
-        });
+test({
+  name: "ASYNC: Permissions are changed (non-Windows)",
+  skip: Deno.build.os === "win",
+  async fn() {
+    const tempFile: string = await Deno.makeTempFile();
+    const originalFileMode: number | null = (await Deno.lstat(tempFile)).mode;
+    await new Promise((resolve, reject) => {
+      chmod(tempFile, 0o777, err => {
+        if (err) reject(err);
+        else resolve();
+      });
+    })
+      .then(() => {
+        const newFileMode: number | null = Deno.lstatSync(tempFile).mode;
+        assert(newFileMode && originalFileMode);
+        assert(newFileMode === 33279 && newFileMode > originalFileMode);
       })
-        .then(() => {
-          const newFileMode: number | null = Deno.lstatSync(tempFile).mode;
-          assert(newFileMode && originalFileMode);
-          assert(newFileMode === 33279 && newFileMode > originalFileMode);
-        })
-        .catch(() => {
-          fail();
-        })
-        .finally(() => {
-          Deno.removeSync(tempFile);
-        });
-    }
-  });
+      .catch(() => {
+        fail();
+      })
+      .finally(() => {
+        Deno.removeSync(tempFile);
+      });
+  }
+});
 
-  test({
-    name: "SYNC: Permissions are changed (non-Windows)",
-    fn() {
-      const tempFile: string = Deno.makeTempFileSync();
-      const originalFileMode: number | null = Deno.lstatSync(tempFile).mode;
-      chmodSync(tempFile, "777");
+test({
+  name: "SYNC: Permissions are changed (non-Windows)",
+  skip: Deno.build.os === "win",
+  fn() {
+    const tempFile: string = Deno.makeTempFileSync();
+    const originalFileMode: number | null = Deno.lstatSync(tempFile).mode;
+    chmodSync(tempFile, "777");
 
-      const newFileMode: number | null = Deno.lstatSync(tempFile).mode;
-      assert(newFileMode && originalFileMode);
-      assert(newFileMode === 33279 && newFileMode > originalFileMode);
-      Deno.removeSync(tempFile);
-    }
-  });
-}
+    const newFileMode: number | null = Deno.lstatSync(tempFile).mode;
+    assert(newFileMode && originalFileMode);
+    assert(newFileMode === 33279 && newFileMode > originalFileMode);
+    Deno.removeSync(tempFile);
+  }
+});
 
 test({
   name: "ASYNC: Error passed in callback function when bad mode passed in",
