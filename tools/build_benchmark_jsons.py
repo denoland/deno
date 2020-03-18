5c7d4922ff239a70ab7534806c9241e5b4e2bff2
Bert Belder <bertbelder@gmail.com>
2020-03-17 19:38:27 -0700

    Fix benchmark upload step in Github Actions (#4415)

--- tools/build_benchmark_jsons.py
+++ tools/build_benchmark_jsons.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 # Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 import os
+import sys
 from util import build_path
 from benchmark import read_json, write_json
 
-current_data_file = os.path.join(build_path(), "bench.json")
-all_data_file = "gh-pages/data.json"  # Includes all benchmark data.
-recent_data_file = "gh-pages/recent.json"  # Includes recent 20 benchmark data.
 
-assert os.path.exists(current_data_file)
-assert os.path.exists(all_data_file)
+def main(argv):
+    if len(argv) == 2:
+        build_dir = sys.argv[1]
+    elif len(argv) == 1:
+        build_dir = build_path()
+    else:
+        print "Usage: tools/build_benchmark_jsons.py [build_dir]"
+        sys.exit(1)
 
-new_data = read_json(current_data_file)
-all_data = read_json(all_data_file)
-all_data.append(new_data)
+    current_data_file = os.path.join(build_dir, "bench.json")
+    all_data_file = "gh-pages/data.json"  # Includes all benchmark data.
+    recent_data_file = "gh-pages/recent.json"  # Has 20 most recent results.
 
-write_json(all_data_file, all_data)
-write_json(recent_data_file, all_data[-20:])
+    assert os.path.exists(current_data_file)
+    assert os.path.exists(all_data_file)
+
+    new_data = read_json(current_data_file)
+    all_data = read_json(all_data_file)
+    all_data.append(new_data)
+
+    write_json(all_data_file, all_data)
+    write_json(recent_data_file, all_data[-20:])
+
+
+if __name__ == '__main__':
+    main(sys.argv)
