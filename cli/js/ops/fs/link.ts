ca315621b7d2b30f2a66a4b691c338092618d30c
Dubiousjim <dubiousjim@gmail.com>
2020-03-17 20:07:40 -0400

    symlink,link: oldname->oldpath, newname->newpath

--- cli/js/ops/fs/link.ts
+++ cli/js/ops/fs/link.ts
@@ -1,10 +1,10 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 import { sendSync, sendAsync } from "../dispatch_json.ts";
 
-export function linkSync(oldname: string, newname: string): void {
-  sendSync("op_link", { oldname, newname });
+export function linkSync(oldpath: string, newpath: string): void {
+  sendSync("op_link", { oldpath, newpath });
 }
 
-export async function link(oldname: string, newname: string): Promise<void> {
-  await sendAsync("op_link", { oldname, newname });
+export async function link(oldpath: string, newpath: string): Promise<void> {
+  await sendAsync("op_link", { oldpath, newpath });
 }
