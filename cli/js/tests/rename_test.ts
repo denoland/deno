1b30e43401996c77095be3358328464d4ac89fb5
Dubiousjim <dubiousjim@gmail.com>
2020-03-24 07:49:19 -0400

    rename tests: CreateNew (files and dirs)

--- cli/js/tests/rename_test.ts
+++ cli/js/tests/rename_test.ts
@@ -14,6 +14,11 @@ function assertMissing(path: string): void {
   assertEquals(info, undefined);
 }
 
+function assertFile(path: string): void {
+  const info = Deno.lstatSync(path);
+  assert(info.isFile());
+}
+
 function assertDirectory(path: string, mode?: number): void {
   const info = Deno.lstatSync(path);
   assert(info.isDirectory());
@@ -77,5 +82,161 @@ unitTest(
     await Deno.rename(oldpath, newpath);
     assertDirectory(newpath);
     assertMissing(oldpath);
+  }
+);
+
+function readFileString(filename: string): string {
+  const dataRead = Deno.readFileSync(filename);
+  const dec = new TextDecoder("utf-8");
+  return dec.decode(dataRead);
+}
+
+function writeFileString(filename: string, s: string): void {
+  const enc = new TextEncoder();
+  const data = enc.encode(s);
+  Deno.writeFileSync(filename, data, { mode: 0o666 });
+}
+
+unitTest(
+  { perms: { read: true, write: true } },
+  function renameSyncCreateNewFile(): void {
+    const testDir = Deno.makeTempDirSync();
+    const oldfile = testDir + "/oldfile";
+    const goodpath = testDir + "/goodpath";
+    const badpath = testDir + "/badpath";
+    writeFileString(oldfile, "Hello");
+    writeFileString(badpath, "");
+    Deno.renameSync(oldfile, goodpath, { createNew: true });
+    assertMissing(oldfile);
+    assertFile(goodpath);
+    assertEquals("Hello", readFileString(goodpath));
+
+    let caughtErr = false;
+    try {
+      Deno.renameSync(goodpath, badpath, { createNew: true });
+    } catch (e) {
+      caughtErr = true;
+      assert(e instanceof Deno.errors.AlreadyExists);
+    }
+    assert(caughtErr);
+
+    Deno.renameSync(goodpath, badpath, { createNew: false });
+    assertMissing(goodpath);
+    assertFile(badpath);
+    assertEquals("Hello", readFileString(badpath));
+  }
+);
+
+unitTest(
+  { perms: { read: true, write: true } },
+  async function renameCreateNewFile(): Promise<void> {
+    const testDir = Deno.makeTempDirSync();
+    const oldfile = testDir + "/oldfile";
+    const goodpath = testDir + "/goodpath";
+    const badpath = testDir + "/badpath";
+    writeFileString(oldfile, "Hello");
+    writeFileString(badpath, "");
+    await Deno.rename(oldfile, goodpath, { createNew: true });
+    assertMissing(oldfile);
+    assertFile(goodpath);
+    assertEquals("Hello", readFileString(goodpath));
+
+    let caughtErr = false;
+    try {
+      await Deno.rename(goodpath, badpath, { createNew: true });
+    } catch (e) {
+      caughtErr = true;
+      assert(e instanceof Deno.errors.AlreadyExists);
+    }
+    assert(caughtErr);
+
+    await Deno.rename(goodpath, badpath, { createNew: false });
+    assertMissing(goodpath);
+    assertFile(badpath);
+    assertEquals("Hello", readFileString(badpath));
+  }
+);
+
+unitTest(
+  { perms: { read: true, write: true } },
+  function renameSyncCreateNewDir(): void {
+    const testDir = Deno.makeTempDirSync();
+    const olddir = testDir + "/olddir";
+    const goodpath = testDir + "/goodpath";
+    const badpath = testDir + "/badpath";
+    Deno.mkdirSync(olddir, { mode: 0o701 });
+    Deno.mkdirSync(badpath);
+
+    Deno.renameSync(olddir, goodpath, { createNew: true });
+    assertMissing(olddir);
+    assertDirectory(goodpath, 0o701);
+
+    let caughtErr = false;
+    try {
+      Deno.renameSync(goodpath, badpath, { createNew: true });
+    } catch (e) {
+      caughtErr = true;
+      assert(e instanceof Deno.errors.AlreadyExists);
+    }
+    assert(caughtErr);
+
+    if (Deno.build.os === "win") {
+      // Windows won't overwrite a directory
+      caughtErr = false;
+      try {
+        Deno.renameSync(goodpath, badpath, { createNew: false });
+      } catch (e) {
+        caughtErr = true;
+        assert(e instanceof Deno.errors.PermissionDenied);
+      }
+      assert(caughtErr);
+      Deno.removeSync(badpath);
+    }
+
+    Deno.renameSync(goodpath, badpath, { createNew: false });
+    assertMissing(goodpath);
+    assertDirectory(badpath, 0o701);
+  }
+);
+
+unitTest(
+  { perms: { read: true, write: true } },
+  async function renameCreateNewDir(): Promise<void> {
+    const testDir = Deno.makeTempDirSync();
+    const olddir = testDir + "/olddir";
+    const goodpath = testDir + "/goodpath";
+    const badpath = testDir + "/badpath";
+    Deno.mkdirSync(olddir, { mode: 0o701 });
+    Deno.mkdirSync(badpath);
+
+    await Deno.rename(olddir, goodpath, { createNew: true });
+    assertMissing(olddir);
+    assertDirectory(goodpath, 0o701);
+
+    let caughtErr = false;
+    try {
+      await Deno.rename(goodpath, badpath, { createNew: true });
+    } catch (e) {
+      caughtErr = true;
+      assert(e instanceof Deno.errors.AlreadyExists);
+    }
+    assert(caughtErr);
+
+    if (Deno.build.os === "win") {
+      // Windows won't overwrite a directory
+      caughtErr = false;
+      try {
+        await Deno.rename(goodpath, badpath, { createNew: false });
+      } catch (e) {
+        caughtErr = true;
+        assert(e instanceof Deno.errors.PermissionDenied);
+      }
+      assert(caughtErr);
+      Deno.removeSync(badpath);
+    }
+
+    await Deno.rename(goodpath, badpath, { createNew: false });
+    assertMissing(goodpath);
+    assertDirectory(badpath, 0o701);
   }
 );
