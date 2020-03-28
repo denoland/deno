95605de904ad898c6a214068e200d75a3f1e82b3
Michał Sabiniarz <michal.sabiniarz@gmail.com>
2020-03-28 17:18:23 +0100

    console: stringify iterable improvements

--- cli/js/web/console.ts
+++ cli/js/web/console.ts
@@ -18,6 +18,9 @@ const DEFAULT_MAX_DEPTH = 4;
 
 const LINE_BREAKING_LENGTH = 80;
 
+const MAX_ITERABLE_LENGTH = 100;
+const MIN_GROUP_LENGTH = 6;
+
 const STR_ABBREVIATE_SIZE = 100;
 
 // Char codes
@@ -82,6 +85,7 @@ interface IterablePrintConfig<T> {
     level: number,
     maxLevel: number
   ) => string;
+  group: boolean;
 }
 
 function createIterableString<T>(
@@ -104,9 +108,149 @@ function createIterableString<T>(
     }
   } catch (e) {}
   ctx.delete(value);
+
+  if (entries.length > MAX_ITERABLE_LENGTH) {
+    const nmore = entries.length - MAX_ITERABLE_LENGTH;
+    entries.length = MAX_ITERABLE_LENGTH;
+    entries.push(`... ${nmore} more items`);
+  }
+
   const iPrefix = `${config.displayName ? config.displayName + " " : ""}`;
-  const iContent = entries.length === 0 ? "" : ` ${entries.join(", ")} `;
+
+  let iContent: string;
+  if (config.group && entries.length > MIN_GROUP_LENGTH) {
+    const groups = groupEntries(entries, level, value);
+    const initIndentation = `\n${"  ".repeat(level + 1)}`;
+    const entryIndetation = `,\n${"  ".repeat(level + 1)}`;
+    const closingIndentation = `\n${"  ".repeat(level)}`;
+
+    iContent = `${initIndentation}${groups.join(
+      entryIndetation
+    )}${closingIndentation}`;
+  } else {
+    iContent = entries.length === 0 ? "" : ` ${entries.join(", ")} `;
+    if (iContent.length > LINE_BREAKING_LENGTH) {
+      const initIndentation = `\n${" ".repeat(level + 1)}`;
+      const entryIndetation = `,\n${" ".repeat(level + 1)}`;
+      const closingIndentation = `\n`;
+
+      iContent = `${initIndentation}${entries.join(
+        entryIndetation
+      )}${closingIndentation}`;
+    }
+  }
+
   return `${iPrefix}${config.delims[0]}${iContent}${config.delims[1]}`;
+}
+
+// Ported from Node.js
+// Copyright Node.js contributors. All rights reserved.
+function groupEntries<T>(
+  entries: string[],
+  level: number,
+  value: Iterable<T>
+): string[] {
+  let totalLength = 0;
+  let maxLength = 0;
+  let entriesLength = entries.length;
+  if (MAX_ITERABLE_LENGTH < entriesLength) {
+    // This makes sure the "... n more items" part is not taken into account.
+    entriesLength--;
+  }
+  const separatorSpace = 2; // Add 1 for the space and 1 for the separator.
+  const dataLen = new Array(entriesLength);
+  // Calculate the total length of all output entries and the individual max
+  // entries length of all output entries. In future colors should be taken
+  // here into the account
+  for (let i = 0; i < entriesLength; i++) {
+    const len = entries[i].length;
+    dataLen[i] = len;
+    totalLength += len + separatorSpace;
+    if (maxLength < len) maxLength = len;
+  }
+  // Add two to `maxLength` as we add a single whitespace character plus a comma
+  // in-between two entries.
+  const actualMax = maxLength + separatorSpace;
+  // Check if at least three entries fit next to each other and prevent grouping
+  // of arrays that contains entries of very different length (i.e., if a single
+  // entry is longer than 1/5 of all other entries combined). Otherwise the
+  // space in-between small entries would be enormous.
+  if (
+    actualMax * 3 + (level + 1) < LINE_BREAKING_LENGTH &&
+    (totalLength / actualMax > 5 || maxLength <= 6)
+  ) {
+    const approxCharHeights = 2.5;
+    const averageBias = Math.sqrt(actualMax - totalLength / entries.length);
+    const biasedMax = Math.max(actualMax - 3 - averageBias, 1);
+    // Dynamically check how many columns seem possible.
+    const columns = Math.min(
+      // Ideally a square should be drawn. We expect a character to be about 2.5
+      // times as high as wide. This is the area formula to calculate a square
+      // which contains n rectangles of size `actualMax * approxCharHeights`.
+      // Divide that by `actualMax` to receive the correct number of columns.
+      // The added bias increases the columns for short entries.
+      Math.round(
+        Math.sqrt(approxCharHeights * biasedMax * entriesLength) / biasedMax
+      ),
+      // Do not exceed the breakLength.
+      Math.floor((LINE_BREAKING_LENGTH - (level + 1)) / actualMax),
+      // Limit the columns to a maximum of fifteen.
+      15
+    );
+    // Return with the original output if no grouping should happen.
+    if (columns <= 1) {
+      return entries;
+    }
+    const tmp = [];
+    const maxLineLength = [];
+    for (let i = 0; i < columns; i++) {
+      let lineMaxLength = 0;
+      for (let j = i; j < entries.length; j += columns) {
+        if (dataLen[j] > lineMaxLength) lineMaxLength = dataLen[j];
+      }
+      lineMaxLength += separatorSpace;
+      maxLineLength[i] = lineMaxLength;
+    }
+    let order = "padStart";
+    if (value !== undefined) {
+      for (let i = 0; i < entries.length; i++) {
+        //@ts-ignore
+        if (typeof value[i] !== "number" && typeof value[i] !== "bigint") {
+          order = "padEnd";
+          break;
+        }
+      }
+    }
+    // Each iteration creates a single line of grouped entries.
+    for (let i = 0; i < entriesLength; i += columns) {
+      // The last lines may contain less entries than columns.
+      const max = Math.min(i + columns, entriesLength);
+      let str = "";
+      let j = i;
+      for (; j < max - 1; j++) {
+        // In future, colors should be taken here into the account
+        const padding = maxLineLength[j - i];
+        //@ts-ignore
+        str += `${entries[j]}, `[order](padding, " ");
+      }
+      if (order === "padStart") {
+        const padding =
+          maxLineLength[j - i] +
+          entries[j].length -
+          dataLen[j] -
+          separatorSpace;
+        str += entries[j].padStart(padding, " ");
+      } else {
+        str += entries[j];
+      }
+      tmp.push(str);
+    }
+    if (MAX_ITERABLE_LENGTH < entries.length) {
+      tmp.push(entries[entriesLength]);
+    }
+    entries = tmp;
+  }
+  return entries;
 }
 
 function stringify(
@@ -175,6 +319,7 @@ function createArrayString(
     delims: ["[", "]"],
     entryHandler: (el, ctx, level, maxLevel): string =>
       stringifyWithQuotes(el, ctx, level + 1, maxLevel),
+    group: true,
   };
   return createIterableString(value, ctx, level, maxLevel, printConfig);
 }
@@ -186,12 +331,14 @@ function createTypedArrayString(
   level: number,
   maxLevel: number
 ): string {
+  const valueLength = value.length;
   const printConfig: IterablePrintConfig<unknown> = {
     typeName: typedArrayName,
-    displayName: typedArrayName,
+    displayName: `${typedArrayName}(${valueLength})`,
     delims: ["[", "]"],
     entryHandler: (el, ctx, level, maxLevel): string =>
       stringifyWithQuotes(el, ctx, level + 1, maxLevel),
+    group: true,
   };
   return createIterableString(value, ctx, level, maxLevel, printConfig);
 }
@@ -208,6 +355,7 @@ function createSetString(
     delims: ["{", "}"],
     entryHandler: (el, ctx, level, maxLevel): string =>
       stringifyWithQuotes(el, ctx, level + 1, maxLevel),
+    group: false,
   };
   return createIterableString(value, ctx, level, maxLevel, printConfig);
 }
@@ -231,6 +379,7 @@ function createMapString(
         maxLevel
       )} => ${stringifyWithQuotes(val, ctx, level + 1, maxLevel)}`;
     },
+    group: false,
   };
   return createIterableString(value, ctx, level, maxLevel, printConfig);
 }
