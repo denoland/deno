97c162e2192c420b3ef4410358e1eeee0c081f9c
Ryan Dahl <ry@tinyclouds.org>
2020-04-02 15:00:27 -0400

    Revert "feat(std/http/server): Respond with 400 on request parse failure (#4551)"
    readRequest should not write a response.
    
    This reverts commit 017a611131a35ccf5dbfce6a2a665fa569e32ec1.

--- std/http/io_test.ts
+++ std/http/io_test.ts
@@ -5,7 +5,6 @@ import {
   assert,
   assertNotEOF,
   assertNotEquals,
-  assertMatch,
 } from "../testing/asserts.ts";
 import {
   bodyReader,
@@ -350,28 +349,13 @@ malformedHeader
 `;
   const reader = new BufReader(new StringReader(input));
   let err;
-  let responseString: string;
   try {
-    // Capture whatever `readRequest()` attempts to write to the connection on
-    // error. We expect it to be a 400 response.
-    await readRequest(
-      mockConn({
-        write(p: Uint8Array): Promise<number> {
-          responseString = decode(p);
-          return Promise.resolve(p.length);
-        },
-      }),
-      reader
-    );
+    await readRequest(mockConn(), reader);
   } catch (e) {
     err = e;
   }
   assert(err instanceof Error);
   assertEquals(err.message, "malformed MIME header line: malformedHeader");
-  assertMatch(
-    responseString!,
-    /^HTTP\/1\.1 400 Bad Request\r\ncontent-length: \d+\r\n\r\n.*\r\n\r\n$/ms
-  );
 });
 
 // Ported from Go
