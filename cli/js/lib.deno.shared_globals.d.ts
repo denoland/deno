2476f438f78d6eb7cb2a7883d515a929f5b7ddcf
Ryan Dahl <ry@tinyclouds.org>
2020-04-08 10:57:54 -0400

    fmt

--- cli/js/lib.deno.shared_globals.d.ts
+++ cli/js/lib.deno.shared_globals.d.ts
@@ -921,17 +921,17 @@ interface BlobPropertyBag {
 
 /** A file-like object of immutable, raw data. Blobs represent data that isn't necessarily in a JavaScript-native format. The File interface is based on Blob, inheriting blob functionality and expanding it to support files on the user's system. */
 interface Blob {
-    readonly size: number;
-    readonly type: string;
-    arrayBuffer(): Promise<ArrayBuffer>;
-    slice(start?: number, end?: number, contentType?: string): Blob;
-    stream(): ReadableStream;
-    text(): Promise<string>;
+  readonly size: number;
+  readonly type: string;
+  arrayBuffer(): Promise<ArrayBuffer>;
+  slice(start?: number, end?: number, contentType?: string): Blob;
+  stream(): ReadableStream;
+  text(): Promise<string>;
 }
 
 declare const Blob: {
-    prototype: Blob;
-    new(blobParts?: BlobPart[], options?: BlobPropertyBag): Blob;
+  prototype: Blob;
+  new (blobParts?: BlobPart[], options?: BlobPropertyBag): Blob;
 };
 
 declare namespace __console {
