c5d1c10aa62d1ffd7a6855449a3a244e151c7ae2
Ali Hasani <a.hassssani@gmail.com>
2020-04-08 16:56:56 +0430

    add isPrimitive & isBuffer to node/util

--- std/node/util.ts
+++ std/node/util.ts
@@ -46,6 +46,16 @@ export function isRegExp(value: unknown): boolean {
   return value instanceof RegExp;
 }
 
+export function isBuffer(value: unknown): boolean {
+  return value instanceof Deno.Buffer;
+}
+
+export function isPrimitive(value: unknown): boolean {
+  return (
+    value === null || (typeof value !== "object" && typeof value !== "function")
+  );
+}
+
 export function validateIntegerRange(
   value: number,
   name: string,
