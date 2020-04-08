e75f0cde27f346d6ef404ca20d8f4057ff3ea251
Ali Hasani <a.hassssani@gmail.com>
2020-04-08 20:27:55 +0430

    fix exists tests with temporary data

--- std/node/_fs/_fs_exists_test.ts
+++ std/node/_fs/_fs_exists_test.ts
@@ -2,26 +2,25 @@
 
 import { assertEquals } from "../../testing/asserts.ts";
 import { exists, existsSync } from "./_fs_exists.ts";
-import * as path from "../../path/mod.ts";
 
 const { test } = Deno;
 
-const testDir = path.resolve("_fs", "testdata");
-const testAvailableData = path.join(testDir, "hello.txt");
-const testNotAvailableData = path.join(testDir, "notAvailable.txt");
-
 test(async function existsFile() {
-  const availableFile = await new Promise((resolve) => {
-    exists(testAvailableData, (exists: boolean) => resolve(exists));
+  const availableFile = await new Promise(async (resolve) => {
+    const tmpFilePath = await Deno.makeTempFile();
+    exists(tmpFilePath, (exists: boolean) => resolve(exists));
+    Deno.remove(tmpFilePath);
   });
   const notAvailableFile = await new Promise((resolve) => {
-    exists(testNotAvailableData, (exists: boolean) => resolve(exists));
+    exists("./notAvailable.txt", (exists: boolean) => resolve(exists));
   });
   assertEquals(availableFile, true);
   assertEquals(notAvailableFile, false);
 });
 
 test(function existsSyncFile() {
-  assertEquals(existsSync(testAvailableData), true);
-  assertEquals(existsSync(testNotAvailableData), false);
+  const tmpFilePath = Deno.makeTempFileSync();
+  assertEquals(existsSync(tmpFilePath), true);
+  Deno.removeSync(tmpFilePath);
+  assertEquals(existsSync("./notAvailable.txt"), false);
 });
