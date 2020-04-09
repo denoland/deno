14cfd0e4b1aff2e3cd9b4423ed6b180c8489e37c
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-04-09 20:56:30 +0200

    lint

--- cli/tests/subdir/racy_worker.js
+++ cli/tests/subdir/racy_worker.js
@@ -1,7 +1,7 @@
 // See issue for details
 // https://github.com/denoland/deno/issues/4080
 //
-// After first call to `postMessage() this worker schedules 
+// After first call to `postMessage() this worker schedules
 // [close(), postMessage()] ops on the same turn of microtask queue
 // (because message is rather big).
 // Only single `postMessage()` call should make it
