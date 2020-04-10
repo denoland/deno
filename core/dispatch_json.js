ba1b841172e4c7713df81fa8f5315012b4db7844
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-04-10 19:08:51 +0200

    add basic dispatch_json.js

---
+++ core/dispatch_json.js
@@ -0,0 +1,96 @@
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+
+function assert(cond) {
+  if (!cond) {
+    throw Error(msg);
+  }
+}
+
+export function createResolvable() {
+  let resolve;
+  let reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  promise.resolve = resolve;
+  promise.reject = reject;
+  return promise;
+}
+
+export function createDispatchJson(core, errorFactory) {
+  // Using an object without a prototype because `Map` was causing GC problems.
+  const promiseTable = Object.create(null);
+  let _nextPromiseId = 1;
+  const OPS_CACHE = core.ops();
+
+  function nextPromiseId() {
+    return _nextPromiseId++;
+  }
+
+  function decode(ui8) {
+    const s = core.decode(ui8);
+    return JSON.parse(s);
+  }
+
+  function encode(args) {
+    const s = JSON.stringify(args);
+    return core.encode(s);
+  }
+
+  function asyncMsgFromRust(resUi8) {
+    const res = decode(resUi8);
+    assert(res.promiseId != null);
+  
+    const promise = promiseTable[res.promiseId];
+    assert(promise != null);
+    delete promiseTable[res.promiseId];
+    promise.resolve(res);
+  };
+
+  function sendSync(opName, args, zeroCopy) {
+    const opId = OPS_CACHE[opName];
+    const argsUi8 = encode(args);
+    const resUi8 = core.dispatch(opId, argsUi8, zeroCopy);
+    assert(resUi8 != null);
+
+    const res = decode(resUi8);
+    assert(res.promiseId == null);
+
+    if (res.err) {
+      return errorFactory(res.err);
+    }
+    assert(res.ok);
+    return res.ok;
+  };
+
+  function sendAsync(opName, args, zeroCopy) {
+    const opId = OPS_CACHE[opName];
+    const promiseId = nextPromiseId();
+    args = Object.assign(args, { promiseId });
+    const promise = createResolvable();
+  
+    const argsUi8 = encode(args);
+    const buf = core.dispatch(opId, argsUi8, zeroCopy);
+    if (buf) {
+      // Sync result.
+      const res = decode(buf);
+      promise.resolve(res);
+    } else {
+      // Async result.
+      promiseTable[promiseId] = promise;
+    }
+    const res = await promise;
+    if (res.err) {
+      return errorFactory(res.err);
+    }
+    assert(res.ok);
+    return res.ok;
+  };
+  
+  return {
+    sendSync,
+    sendAsync,
+    asyncMsgFromRust,
+  }
+}
