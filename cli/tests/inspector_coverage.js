258ad9b3ba07b3c9d205d9fd65f3b48729f44ad4
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-04-12 13:33:21 +0200

    simple test

---
+++ cli/tests/inspector_coverage.js
@@ -0,0 +1,16 @@
+function a() {
+  console.log("hello a");
+}
+
+function b() {
+  console.log("hello b");
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function c() {
+  console.log("hello c");
+}
+
+a();
+b();
+b();
