b8e6a8db0ab63c44c58f3fb800725445039467d7
Ryan Dahl <ry@tinyclouds.org>
2020-04-14 15:12:56 -0400

    delete TS code

--- cli/js/web/performance.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-import { now as opNow } from "../ops/timers.ts";
-
-export class Performance {
-  now(): number {
-    const res = opNow();
-    return res.seconds * 1e3 + res.subsecNanos / 1e6;
-  }
-}
