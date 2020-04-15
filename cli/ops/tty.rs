bf5b9ea67438cef863de042966678a3e41417c88
Ryan Dahl <ry@tinyclouds.org>
2020-04-15 17:54:23 -0400

    fix windows again

--- cli/ops/tty.rs
+++ cli/ops/tty.rs
@@ -75,7 +75,7 @@ pub fn op_set_raw(
     let handle = match &resource_holder.unwrap().resource {
       StreamResource::Stdin(_, _) => std::io::stdin().as_raw_handle(),
       StreamResource::FsFile(None) => todo!(),
-      StreamResource::FsFile(Some(f, _)) => {
+      StreamResource::FsFile(Some((f, _))) => {
         let tokio_file = futures::executor::block_on(f.try_clone())?;
         let std_file = futures::executor::block_on(tokio_file.into_std());
         std_file.as_raw_handle()
