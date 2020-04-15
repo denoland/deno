043704c768525f3a1b811be21f69f7af7b23a57a
Ali Hasani <a.hassssani@gmail.com>
2020-04-15 14:16:03 +0430

    fix import extention

--- std/uuid/v4.ts
+++ std/uuid/v4.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
-import { bytesToUuid } from "./_common";
+import { bytesToUuid } from "./_common.ts";
 
 const UUID_RE = new RegExp(
   "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
