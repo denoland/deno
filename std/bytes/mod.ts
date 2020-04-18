186d672639dc87a3e92552cfb2bfb65f07b7be2c
Ali Hasani <a.hassssani@gmail.com>
2020-04-18 14:59:17 +0430

    hasSuffix & contains & jsDocs & refactor

--- std/bytes/mod.ts
+++ std/bytes/mod.ts
@@ -1,65 +1,54 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 import { copyBytes } from "../io/util.ts";
 
-/** Find first index of binary pattern from a. If not found, then return -1 **/
+/**
+ * Find first index of binary pattern from a. If not found, then return -1
+ * @param a soruce array
+ * @param b pattern to find in source array
+ */
 export function findIndex(a: Uint8Array, pat: Uint8Array): number {
-  const s = pat[0];
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== s) continue;
-    const pin = i;
-    let matched = 1;
-    let j = i;
-    while (matched < pat.length) {
-      j++;
-      if (a[j] !== pat[j - pin]) {
-        break;
-      }
-      matched++;
-    }
-    if (matched === pat.length) {
-      return pin;
-    }
-  }
-  return -1;
+  const indexOf = String(a).indexOf(String(pat));
+  return indexOf == -1 ? indexOf : indexOf / 2;
 }
 
-/** Find last index of binary pattern from a. If not found, then return -1 **/
+/**
+ * Find last index of binary pattern from a. If not found, then return -1.
+ * @param a soruce array
+ * @param b pattern to find in source array
+ */
 export function findLastIndex(a: Uint8Array, pat: Uint8Array): number {
-  const e = pat[pat.length - 1];
-  for (let i = a.length - 1; i >= 0; i--) {
-    if (a[i] !== e) continue;
-    const pin = i;
-    let matched = 1;
-    let j = i;
-    while (matched < pat.length) {
-      j--;
-      if (a[j] !== pat[pat.length - 1 - (pin - j)]) {
-        break;
-      }
-      matched++;
-    }
-    if (matched === pat.length) {
-      return pin - pat.length + 1;
-    }
-  }
-  return -1;
+  const indexOf = String(a).lastIndexOf(String(pat));
+  return indexOf == -1 ? indexOf : indexOf / 2;
 }
 
-/** Check whether binary arrays are equal to each other **/
+/**
+ * Check whether binary arrays are equal to each other.
+ * @param a First Array To Check
+ * @param b Second Array To Check
+ */
 export function equal(a: Uint8Array, match: Uint8Array): boolean {
-  if (a.length !== match.length) return false;
-  for (let i = 0; i < match.length; i++) {
-    if (a[i] !== match[i]) return false;
-  }
-  return true;
+  return String(a) === String(match);
 }
 
-/** Check whether binary array has binary prefix  **/
+/**
+ * Check whether binary array starts with prefix.
+ * @param a First Array To Concatenate
+ * @param b Second Array To Concatenate
+ */
 export function hasPrefix(a: Uint8Array, prefix: Uint8Array): boolean {
   for (let i = 0, max = prefix.length; i < max; i++) {
     if (a[i] !== prefix[i]) return false;
   }
   return true;
+}
+
+/**
+ * check whether binary array ends with suffix.
+ * @param a First Array To Concatenate
+ * @param b Second Array To Concatenate
+ */
+export function hasSuffix(a: Uint8Array, suffix: Uint8Array): boolean {
+  return a.length >= suffix.length && equal(a.slice(suffix.length - 1), suffix);
 }
 
 /**
@@ -95,10 +84,23 @@ export function repeat(b: Uint8Array, count: number): Uint8Array {
   return nb;
 }
 
-/** Concatenate two binary arrays and return new one  */
+/**
+ * Concatenate two binary arrays and return new one.
+ * @param a First array to concatenate
+ * @param b Second array to concatenate
+ */
 export function concat(a: Uint8Array, b: Uint8Array): Uint8Array {
   const output = new Uint8Array(a.length + b.length);
   output.set(a, 0);
   output.set(b, a.length);
   return output;
+}
+
+/**
+ * Check srouce array contains pattern array.
+ * @param s srouce array
+ * @param pat patter array
+ */
+export function contains(s: Uint8Array, pat: Uint8Array): boolean {
+  return findIndex(s, pat) != -1;
 }
--- std/bytes/test.ts
+++ std/bytes/test.ts
@@ -5,8 +5,10 @@ import {
   findLastIndex,
   equal,
   hasPrefix,
+  hasSuffix,
   repeat,
   concat,
+  contains,
 } from "./mod.ts";
 import { assertEquals, assertThrows, assert } from "../testing/asserts.ts";
 import { encode, decode } from "../encoding/utf8.ts";
@@ -44,6 +46,11 @@ Deno.test("[bytes] equal", () => {
 
 Deno.test("[bytes] hasPrefix", () => {
   const v = hasPrefix(new Uint8Array([0, 1, 2]), new Uint8Array([0, 1]));
+  assertEquals(v, true);
+});
+
+Deno.test("[bytes] hasSuffix", () => {
+  const v = hasSuffix(new Uint8Array([0, 1, 2]), new Uint8Array([1, 2]));
   assertEquals(v, true);
 });
 
@@ -96,4 +103,12 @@ Deno.test("[bytes] concat empty arrays", () => {
   assertEquals(joined.byteLength, 0);
   assert(u1 !== joined);
   assert(u2 !== joined);
+});
+
+Deno.test("[bytes] contain", () => {
+  const source = encode("deno.land");
+  const pattern = encode("deno");
+  assert(contains(source, pattern));
+
+  assert(contains(new Uint8Array([0, 1, 2, 3]), new Uint8Array([2, 3])));
 });
