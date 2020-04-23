440d45c8ed3f16738212e8b7abf794f4b7e52c47
likui <2218301630@qq.com>
2020-04-24 01:23:36 +0800

    update type

--- std/uuid/tests/v4/validate.ts
+++ std/uuid/tests/v4/validate.ts
@@ -9,7 +9,7 @@ Deno.test({
     const t = "84fb7824-b951-490e-8afd-0c13228a8282";
     const n = "84fb7824-b951-490g-8afd-0c13228a8282";
 
-    assert(validate(u), `generated ${u} should be valid`);
+    assert(validate(u as string), `generated ${u} should be valid`);
     assert(validate(t), `${t} should be valid`);
     assert(!validate(n), `${n} should not be valid`);
   },
