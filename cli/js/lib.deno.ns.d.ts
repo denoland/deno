757d8617b45573f3ea54b43d20a95738b908e5be
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-04-24 22:58:38 +0200

    BREAKING CHANGE: rename TLS APIs to camel case
    This commit renames all APIs containing "TLS" to use camel case
    (connectTLS -> connectTls, etc.)

--- cli/js/lib.deno.ns.d.ts
+++ cli/js/lib.deno.ns.d.ts
@@ -2018,7 +2018,7 @@ declare namespace Deno {
     options: UnixListenOptions & { transport: "unixpacket" }
   ): DatagramConn;
 
-  export interface ListenTLSOptions extends ListenOptions {
+  export interface ListenTlsOptions extends ListenOptions {
     /** Server certificate file. */
     certFile: string;
     /** Server public key file. */
@@ -2030,10 +2030,10 @@ declare namespace Deno {
   /** Listen announces on the local transport address over TLS (transport layer
    * security).
    *
-   *      const lstnr = Deno.listenTLS({ port: 443, certFile: "./server.crt", keyFile: "./server.key" });
+   *      const lstnr = Deno.listenTls({ port: 443, certFile: "./server.crt", keyFile: "./server.key" });
    *
    * Requires `allow-net` permission. */
-  export function listenTLS(options: ListenTLSOptions): Listener;
+  export function listenTls(options: ListenTlsOptions): Listener;
 
   export interface ConnectOptions {
     /** The port to connect to. */
@@ -2064,7 +2064,7 @@ declare namespace Deno {
     options: ConnectOptions | UnixConnectOptions
   ): Promise<Conn>;
 
-  export interface ConnectTLSOptions {
+  export interface ConnectTlsOptions {
     /** The port to connect to. */
     port: number;
     /** A literal IP address or host name that can be resolved to an IP address.
@@ -2079,16 +2079,16 @@ declare namespace Deno {
    * cert file is optional and if not included Mozilla's root certificates will
    * be used (see also https://github.com/ctz/webpki-roots for specifics)
    *
-   *     const conn1 = await Deno.connectTLS({ port: 80 });
-   *     const conn2 = await Deno.connectTLS({ certFile: "./certs/my_custom_root_CA.pem", hostname: "192.0.2.1", port: 80 });
-   *     const conn3 = await Deno.connectTLS({ hostname: "[2001:db8::1]", port: 80 });
-   *     const conn4 = await Deno.connectTLS({ certFile: "./certs/my_custom_root_CA.pem", hostname: "golang.org", port: 80});
+   *     const conn1 = await Deno.connectTls({ port: 80 });
+   *     const conn2 = await Deno.connectTls({ certFile: "./certs/my_custom_root_CA.pem", hostname: "192.0.2.1", port: 80 });
+   *     const conn3 = await Deno.connectTls({ hostname: "[2001:db8::1]", port: 80 });
+   *     const conn4 = await Deno.connectTls({ certFile: "./certs/my_custom_root_CA.pem", hostname: "golang.org", port: 80});
    *
    * Requires `allow-net` permission.
    */
-  export function connectTLS(options: ConnectTLSOptions): Promise<Conn>;
+  export function connectTls(options: ConnectTlsOptions): Promise<Conn>;
 
-  export interface StartTLSOptions {
+  export interface StartTlsOptions {
     /** A literal IP address or host name that can be resolved to an IP address.
      * If not specified, defaults to `127.0.0.1`. */
     hostname?: string;
@@ -2106,13 +2106,13 @@ declare namespace Deno {
    * prepared for TLS handshake.
    *
    *     const conn = await Deno.connect({ port: 80, hostname: "127.0.0.1" });
-   *     const tlsConn = await Deno.startTLS(conn, { certFile: "./certs/my_custom_root_CA.pem", hostname: "127.0.0.1", port: 80 });
+   *     const tlsConn = await Deno.startTls(conn, { certFile: "./certs/my_custom_root_CA.pem", hostname: "127.0.0.1", port: 80 });
    *
    * Requires `allow-net` permission.
    */
-  export function startTLS(
+  export function startTls(
     conn: Conn,
-    options?: StartTLSOptions
+    options?: StartTlsOptions
   ): Promise<Conn>;
 
   /** **UNSTABLE**: not sure if broken or not */
