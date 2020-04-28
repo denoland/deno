87a6811a02018531e759a8f9851ff69f2e956bc8
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-04-28 12:59:15 +0200

    fix

--- cli/js/lib.deno.ns.d.ts
+++ cli/js/lib.deno.ns.d.ts
@@ -751,11 +751,11 @@ declare namespace Deno {
   }
 
   /** A handle for `stdin`. */
-  export const stdin: Reader & SyncReader & Closer & { rid: number };
+  export const stdin: Reader & ReaderSync & Closer & { rid: number };
   /** A handle for `stdout`. */
-  export const stdout: Writer & SyncWriter & Closer & { rid: number };
+  export const stdout: Writer & WriterSync & Closer & { rid: number };
   /** A handle for `stderr`. */
-  export const stderr: Writer & SyncWriter & Closer & { rid: number };
+  export const stderr: Writer & WriterSync & Closer & { rid: number };
 
   export interface OpenOptions {
     /** Sets the option for read access. This option, when `true`, means that the
