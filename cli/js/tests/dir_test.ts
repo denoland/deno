4efcfa1228b190892a046ef38ca4a08f92ee522b
Ali Hasani <a.hassssani@gmail.com>
2020-04-28 05:23:12 +0430

    format

--- cli/js/tests/dir_test.ts
+++ cli/js/tests/dir_test.ts
@@ -5,18 +5,21 @@ unitTest(function dirCwdNotNull(): void {
   assert(Deno.cwd() != null);
 });
 
-unitTest({ perms: { read: true, write: true } }, function dirCwdChdirSuccess(): void {
-  const initialdir = Deno.cwd();
-  const path = Deno.makeTempDirSync();
-  Deno.chdir(path);
-  const current = Deno.cwd();
-  if (Deno.build.os === "mac") {
-    assertEquals(current, "/private" + path);
-  } else {
-    assertEquals(current, path);
+unitTest(
+  { perms: { read: true, write: true } },
+  function dirCwdChdirSuccess(): void {
+    const initialdir = Deno.cwd();
+    const path = Deno.makeTempDirSync();
+    Deno.chdir(path);
+    const current = Deno.cwd();
+    if (Deno.build.os === "mac") {
+      assertEquals(current, "/private" + path);
+    } else {
+      assertEquals(current, path);
+    }
+    Deno.chdir(initialdir);
   }
-  Deno.chdir(initialdir);
-});
+);
 
 unitTest({ perms: { read: true, write: true } }, function dirCwdError(): void {
   // excluding windows since it throws resource busy, while removeSync
@@ -39,16 +42,19 @@ unitTest({ perms: { read: true, write: true } }, function dirCwdError(): void {
   }
 });
 
-unitTest({ perms: { read: true, write: true } }, function dirChdirError(): void {
-  const path = Deno.makeTempDirSync() + "test";
-  try {
-    Deno.chdir(path);
-    throw Error("directory not available, should throw error");
-  } catch (err) {
-    if (err instanceof Deno.errors.NotFound) {
-      assert(err.name === "NotFound");
-    } else {
-      throw Error("raised different exception");
+unitTest(
+  { perms: { read: true, write: true } },
+  function dirChdirError(): void {
+    const path = Deno.makeTempDirSync() + "test";
+    try {
+      Deno.chdir(path);
+      throw Error("directory not available, should throw error");
+    } catch (err) {
+      if (err instanceof Deno.errors.NotFound) {
+        assert(err.name === "NotFound");
+      } else {
+        throw Error("raised different exception");
+      }
     }
   }
-});
+);
