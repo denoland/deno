8926b6b000067e048a183e265f3814b9f1cca635
Ryan Dahl <ry@tinyclouds.org>
2020-04-27 21:28:40 -0400

    fix typescript

--- cli/js/build.ts
+++ cli/js/build.ts
@@ -1,25 +1,19 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
-//@ts-ignore
-export const build: {
-  target: string;
-  arch: string;
-  os: string;
-  vendor: string;
-  env?: string;
-} = {};
+export const build = {
+  target: "unknown",
+  arch: "unknown",
+  os: "unknown",
+  vendor: "unknown",
+  env: undefined as (string | undefined)
+};
 
 export function setBuildInfo(target: string): void {
   const [arch, vendor, os, env] = target.split("-", 4);
-  //@ts-ignore
   build.target = target;
-  //@ts-ignore
   build.arch = arch;
-  //@ts-ignore
   build.vendor = vendor;
-  //@ts-ignore
   build.os = os;
-  //@ts-ignore
   build.env = env;
   Object.freeze(build);
 }
