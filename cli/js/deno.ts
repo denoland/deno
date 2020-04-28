199aeac274ef7d28b91a8063eecec784444855a6
Luca Casonato <lucacasonato@yahoo.com>
2020-04-28 15:20:12 +0200

    Fixed formatting

--- cli/js/deno.ts
+++ cli/js/deno.ts
@@ -59,13 +59,7 @@ export {
 } from "./ops/fs/make_temp.ts";
 export { metrics, Metrics } from "./ops/runtime.ts";
 export { mkdirSync, mkdir, MkdirOptions } from "./ops/fs/mkdir.ts";
-export {
-  connect,
-  listen,
-  DatagramConn,
-  Listener,
-  Conn,
-} from "./net.ts";
+export { connect, listen, DatagramConn, Listener, Conn } from "./net.ts";
 export { dir, env, exit, execPath, hostname } from "./ops/os.ts";
 export {
   permissions,
