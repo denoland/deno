99f325440beb224656a8e8c35d1d596930c0a8d9
Cinnamon <32660718+cinnamon-bun@users.noreply.github.com>
2020-04-29 11:34:43 -0700

    cat.ts example is broken: Deno.copy args are reversed
    The example `cat.ts` hangs forever.  The arguments to `Deno.copy` are in the wrong order.
    
    https://deno.land/typedoc/index.html#copy
    
    The correct order is `destination, source`, which is surprising.
    
    This is the first example in the [manual](https://deno.land/std/manual.md#an-implementation-of-the-unix-cat-program).  The code in the manual works, but the code at `https://deno.land/std/examples/cat.ts` is different and fails.

--- std/examples/cat.ts
+++ std/examples/cat.ts
@@ -2,6 +2,6 @@
 const filenames = Deno.args;
 for (const filename of filenames) {
   const file = await Deno.open(filename);
-  await Deno.copy(file, Deno.stdout);
+  await Deno.copy(Deno.stdout, file);
   file.close();
 }
