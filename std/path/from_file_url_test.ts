cf18b05da56cf293ea38e000e83e36f5226ac320
Nayeem Rahman <nayeemrmn99@gmail.com>
2020-04-29 19:49:02 +0100

    fixup! Format

--- std/path/from_file_url_test.ts
+++ std/path/from_file_url_test.ts
@@ -2,27 +2,36 @@
 import * as path from "./mod.ts";
 import { assertEquals } from "../testing/asserts.ts";
 
-Deno.test("[path] fromUrl", function () {
-  assertEquals(path.posix.fromUrl(new URL("file:///home/foo")), "/home/foo");
-  assertEquals(path.posix.fromUrl("file:///home/foo"), "/home/foo");
-  assertEquals(path.posix.fromUrl("https://example.com/foo"), "/foo");
-  assertEquals(path.posix.fromUrl("file:///"), "/");
+Deno.test("[path] fromFileUrl", function () {
+  assertEquals(
+    path.posix.fromFileUrl(new URL("file:///home/foo")),
+    "/home/foo"
+  );
+  assertEquals(path.posix.fromFileUrl("file:///home/foo"), "/home/foo");
+  assertEquals(path.posix.fromFileUrl("https://example.com/foo"), "/foo");
+  assertEquals(path.posix.fromFileUrl("file:///"), "/");
 });
 
-Deno.test("[path] fromUrl (win32)", function () {
-  assertEquals(path.win32.fromUrl(new URL("file:///home/foo")), "\\home\\foo");
-  assertEquals(path.win32.fromUrl("file:///home/foo"), "\\home\\foo");
-  assertEquals(path.win32.fromUrl("https://example.com/foo"), "\\foo");
-  assertEquals(path.win32.fromUrl("file:///"), "\\");
+Deno.test("[path] fromFileUrl (win32)", function () {
+  assertEquals(
+    path.win32.fromFileUrl(new URL("file:///home/foo")),
+    "\\home\\foo"
+  );
+  assertEquals(path.win32.fromFileUrl("file:///home/foo"), "\\home\\foo");
+  assertEquals(path.win32.fromFileUrl("https://example.com/foo"), "\\foo");
+  assertEquals(path.win32.fromFileUrl("file:///"), "\\");
   // FIXME(nayeemrmn): Support UNC paths. Needs support in the underlying URL
   // built-in like Chrome has.
-  // assertEquals(path.win32.fromUrl("file:////"), "\\");
-  // assertEquals(path.win32.fromUrl("file:////server"), "\\");
-  // assertEquals(path.win32.fromUrl("file:////server/file"), "\\file");
-  assertEquals(path.win32.fromUrl("file:///c"), "\\c");
-  assertEquals(path.win32.fromUrl("file:///c:"), "\\c:");
-  assertEquals(path.win32.fromUrl("file:///c:/"), "c:\\");
-  assertEquals(path.win32.fromUrl("file:///C:/"), "C:\\");
-  assertEquals(path.win32.fromUrl("file:///C:/Users/"), "C:\\Users\\");
-  assertEquals(path.win32.fromUrl("file:///C:cwd/another"), "\\C:cwd\\another");
+  // assertEquals(path.win32.fromFileUrl("file:////"), "\\");
+  // assertEquals(path.win32.fromFileUrl("file:////server"), "\\");
+  // assertEquals(path.win32.fromFileUrl("file:////server/file"), "\\file");
+  assertEquals(path.win32.fromFileUrl("file:///c"), "\\c");
+  assertEquals(path.win32.fromFileUrl("file:///c:"), "\\c:");
+  assertEquals(path.win32.fromFileUrl("file:///c:/"), "c:\\");
+  assertEquals(path.win32.fromFileUrl("file:///C:/"), "C:\\");
+  assertEquals(path.win32.fromFileUrl("file:///C:/Users/"), "C:\\Users\\");
+  assertEquals(
+    path.win32.fromFileUrl("file:///C:cwd/another"),
+    "\\C:cwd\\another"
+  );
 });
