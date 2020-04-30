512472d45973a9be2d02d018a6abd76700988892
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-04-30 19:54:39 +0200

    analyze dynamic imports

--- cli/swc_util.rs
+++ cli/swc_util.rs
@@ -167,6 +167,7 @@ impl AstParser {
 
 struct DependencyVisitor {
   dependencies: Vec<String>,
+  analyze_dynamic_imports: bool,
 }
 
 impl Visit for DependencyVisitor {
@@ -198,22 +199,75 @@ impl Visit for DependencyVisitor {
     let src_str = export_all.src.value.to_string();
     self.dependencies.push(src_str);
   }
+
+  fn visit_call_expr(
+    &mut self,
+    call_expr: &swc_ecma_ast::CallExpr,
+    _parent: &dyn Node,
+  ) {
+    if !self.analyze_dynamic_imports {
+      return;
+    }
+
+    use swc_ecma_ast::Expr::*;
+    use swc_ecma_ast::ExprOrSuper::*;
+
+    let boxed_expr = match call_expr.callee.clone() {
+      Super(_) => return,
+      Expr(boxed) => boxed,
+    };
+
+    match &*boxed_expr {
+      Ident(ident) => {
+        if &ident.sym.to_string() != "import" {
+          return;
+        }
+      }
+      _ => return,
+    };
+
+    if let Some(arg) = call_expr.args.get(0) {
+      match &*arg.expr {
+        Lit(lit) => {
+          if let swc_ecma_ast::Lit::Str(str_) = lit {
+            let src_str = str_.value.to_string();
+            self.dependencies.push(src_str);
+          }
+        }
+        _ => return,
+      }
+    }
+  }
 }
 
 /// Given file name and source code return vector
 /// of unresolved import specifiers.
 ///
 /// Returned vector may contain duplicate entries.
+///
+/// Second argument allows to configure if dynamic
+/// imports should be analyzed.
+///
+/// NOTE: Only statically analyzable dynamic imports
+/// are considered; ie. the ones that have plain string specifier:
+///
+///    await import("./fizz.ts")
+///
+/// These imports will be ignored:
+///
+///    await import(`./${dir}/fizz.ts`)
+///    await import("./" + "fizz.ts")
 #[allow(unused)]
 pub fn analyze_dependencies(
-  file_name: &str,
   source_code: &str,
+  analyze_dynamic_imports: bool,
 ) -> Result<Vec<String>, SwcDiagnosticBuffer> {
   let parser = AstParser::new();
-  parser.parse_module(file_name, source_code, |parse_result| {
+  parser.parse_module("root.ts", source_code, |parse_result| {
     let module = parse_result?;
     let mut collector = DependencyVisitor {
       dependencies: vec![],
+      analyze_dynamic_imports,
     };
     collector.visit_module(&module, &module);
     Ok(collector.dependencies)
@@ -229,13 +283,37 @@ export * from "./bar.ts";
 "#;
 
   let dependencies =
-    analyze_dependencies("main.ts", source).expect("Failed to parse");
+    analyze_dependencies(source, false).expect("Failed to parse");
   assert_eq!(
     dependencies,
     vec![
       "./foo.ts".to_string(),
       "./foo.ts".to_string(),
       "./bar.ts".to_string(),
+    ]
+  );
+}
+
+#[test]
+fn test_analyze_dependencies_dyn_imports() {
+  let source = r#"
+import { foo } from "./foo.ts";
+export { bar } from "./foo.ts";
+export * from "./bar.ts";
+
+const a = await import("./fizz.ts");
+const a = await import("./" + "buzz.ts");
+"#;
+
+  let dependencies =
+    analyze_dependencies(source, true).expect("Failed to parse");
+  assert_eq!(
+    dependencies,
+    vec![
+      "./foo.ts".to_string(),
+      "./foo.ts".to_string(),
+      "./bar.ts".to_string(),
+      "./fizz.ts".to_string(),
     ]
   );
 }
