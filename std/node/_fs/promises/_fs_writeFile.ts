dc15b620db5b43b26c1f6c976d06453af9ddaa1c
Marcos Casagrande <marcoscvp90@gmail.com>
2020-05-03 01:56:30 +0200

    fs.promises writeFile

---
+++ std/node/_fs/promises/_fs_writeFile.ts
@@ -0,0 +1,17 @@
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+import { WriteFileOptions } from "../_fs_common.ts";
+
+import { writeFile as writeFileCallback } from "../_fs_writeFile.ts";
+
+export async function writeFile(
+	pathOrRid: string | number,
+	data: string | Uint8Array,
+	options?: string | WriteFileOptions
+) {
+	return new Promise((resolve, reject) => {
+		writeFileCallback(pathOrRid, data, options, (err?: Error) => {
+			if (err) return reject(err);
+			resolve();
+		});
+	});
+}
---
+++ std/node/_fs/promises/mod.ts
@@ -0,0 +1,1 @@
+export { writeFile } from "./_fs_writeFile.ts";
