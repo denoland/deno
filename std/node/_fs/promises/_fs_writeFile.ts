999f74d03b2713e5a58547668864768d4f7ad739
Marcos Casagrande <marcoscvp90@gmail.com>
2020-05-03 10:59:39 +0200

    format

--- std/node/_fs/promises/_fs_writeFile.ts
+++ std/node/_fs/promises/_fs_writeFile.ts
@@ -4,14 +4,14 @@ import { WriteFileOptions } from "../_fs_common.ts";
 import { writeFile as writeFileCallback } from "../_fs_writeFile.ts";
 
 export function writeFile(
-	pathOrRid: string | number,
-	data: string | Uint8Array,
-	options?: string | WriteFileOptions
+  pathOrRid: string | number,
+  data: string | Uint8Array,
+  options?: string | WriteFileOptions
 ): Promise<void> {
-	return new Promise((resolve, reject) => {
-		writeFileCallback(pathOrRid, data, options, (err?: Error | null) => {
-			if (err) return reject(err);
-			resolve();
-		});
-	});
+  return new Promise((resolve, reject) => {
+    writeFileCallback(pathOrRid, data, options, (err?: Error | null) => {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
 }
