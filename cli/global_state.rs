161a36150242dd87ad75db3fea2da1fe0265ae23
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-05-07 18:33:25 +0200

    remove cli/compilers/; add cli/tsc.rs

--- cli/global_state.rs
+++ cli/global_state.rs
@@ -1,7 +1,4 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-use crate::compilers::CompiledModule;
-use crate::compilers::TargetLib;
-use crate::compilers::TsCompiler;
 use crate::deno_dir;
 use crate::file_fetcher::SourceFileFetcher;
 use crate::flags;
@@ -147,12 +144,10 @@ impl GlobalState {
           })
         }
       }
-      _ => {
-        Ok(CompiledModule {
-          code: String::from_utf8(out.source_code)?,
-          name: out.url.to_string(),
-        })
-      },
+      _ => Ok(CompiledModule {
+        code: String::from_utf8(out.source_code)?,
+        name: out.url.to_string(),
+      }),
     }?;
     drop(compile_lock);
 
