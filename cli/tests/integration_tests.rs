684929bee5df0ee991e12ba2b3e6183cdc619899
Ryan Dahl <ry@tinyclouds.org>
2020-05-11 11:16:56 -0400

    Merge std_tests.rs into integration_tests.rs.rs

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -13,6 +13,27 @@ use std::process::Command;
 use tempfile::TempDir;
 
 #[test]
+fn std_tests() {
+  let dir = TempDir::new().expect("tempdir fail");
+  let mut deno_cmd = Command::new(util::deno_exe_path());
+  deno_cmd.env("DENO_DIR", dir.path());
+
+  let mut cwd = util::root_path();
+  cwd.push("std");
+  let mut deno = deno_cmd
+    .current_dir(cwd) // note: std tests expect to run from "std" dir
+    .arg("test")
+    .arg("--unstable")
+    .arg("--seed=86") // Some tests rely on specific random numbers.
+    .arg("-A")
+    // .arg("-Ldebug")
+    .spawn()
+    .expect("failed to spawn script");
+  let status = deno.wait().expect("failed to wait for the child process");
+  assert!(status.success());
+}
+
+#[test]
 fn x_deno_warning() {
   let g = util::http_server();
   let output = util::deno_cmd()
