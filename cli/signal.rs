7782a576709b452b538e2854d3f1fecabb5f9106
Ali Hasani <a.hassssani@gmail.com>
2020-05-14 16:05:45 +0430

    fix cfg

--- cli/signal.rs
+++ cli/signal.rs
@@ -1,6 +1,11 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 use crate::op_error::OpError;
 
+#[cfg(not(unix))]
+use winapi::um::handleapi::CloseHandle;
+use winapi::um::processthreadsapi::{OpenProcess, TerminateProcess};
+use winapi::um::winnt::PROCESS_TERMINATE;
+
 #[cfg(unix)]
 pub fn kill(pid: i32, signo: i32) -> Result<(), OpError> {
   use nix::sys::signal::{kill as unix_kill, Signal};
@@ -11,9 +16,6 @@ pub fn kill(pid: i32, signo: i32) -> Result<(), OpError> {
 }
 
 #[cfg(not(unix))]
-use winapi::um::handleapi::CloseHandle;
-use winapi::um::processthreadsapi::{OpenProcess, TerminateProcess};
-use winapi::um::winnt::PROCESS_TERMINATE;
 pub fn kill(pid: i32, signal: i32) -> Result<(), OpError> {
   unsafe {
     let handle = OpenProcess(PROCESS_TERMINATE, 0, pid as u32);
