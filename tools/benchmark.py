eb39a599ea6bc7bef9156fc1901022f50f4623e0
Colin Harrington <colin@laelfrog.io>
2020-05-14 17:52:28 -0500

    #4187 Remove hyperfine `--ignore-failure` flag

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -179,7 +179,7 @@ def run_exec_time(deno_exe, build_dir):
     hyperfine_exe = third_party.get_prebuilt_tool_path("hyperfine")
     benchmark_file = os.path.join(build_dir, "hyperfine_results.json")
     run([
-        hyperfine_exe, "--ignore-failure", "--export-json", benchmark_file,
+        hyperfine_exe, "--export-json", benchmark_file,
         "--warmup", "3"
     ] + [
         deno_exe + " " + " ".join(args) for [_, args] in exec_time_benchmarks
