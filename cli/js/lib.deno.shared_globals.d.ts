f5d2bcda139e4219769af10af4dfeec0f40380e0
Ryan Dahl <ry@tinyclouds.org>
2020-05-17 16:20:57 -0400

    Revert "Fix definition of URL constructor (#5521)"
    This reverts commit 63bc468365bceda929a39b5eb93b605e2dc2bd9c.

--- cli/js/lib.deno.shared_globals.d.ts
+++ cli/js/lib.deno.shared_globals.d.ts
@@ -1172,7 +1172,7 @@ interface URL {
 
 declare const URL: {
   prototype: URL;
-  new (url: string, base?: string | URL): URL;
+  new (url: string | URL, base?: string | URL): URL;
   createObjectURL(object: any): string;
   revokeObjectURL(url: string): void;
 };
