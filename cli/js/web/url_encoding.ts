4155844a1bea35b3cf253efda717ee677151ea8a
Nayeem Rahman <nayeemrmn99@gmail.com>
2020-05-17 18:55:46 +0100

    fixup! fix(cli/js/web/url): Fix URL encoding

--- cli/js/web/url_encoding.ts
+++ cli/js/web/url_encoding.ts
@@ -1,28 +1,28 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
-function charIsC0ControlSet(c: string): boolean {
+function charInC0ControlSet(c: string): boolean {
   return c >= "\u0000" && c <= "\u001F";
 }
 
-function charIsSearchSet(c: string): boolean {
+function charInSearchSet(c: string): boolean {
   // prettier-ignore
-  return charIsC0ControlSet(c) || ["\u0020", "\u0022", "\u0023", "\u0027", "\u003C", "\u003E"].includes(c) || c > "\u007E";
+  return charInC0ControlSet(c) || ["\u0020", "\u0022", "\u0023", "\u0027", "\u003C", "\u003E"].includes(c) || c > "\u007E";
 }
 
-function charIsFragmentSet(c: string): boolean {
+function charInFragmentSet(c: string): boolean {
   // prettier-ignore
-  return charIsC0ControlSet(c) || ["\u0020", "\u0022", "\u003C", "\u003E", "\u0060"].includes(c);
+  return charInC0ControlSet(c) || ["\u0020", "\u0022", "\u003C", "\u003E", "\u0060"].includes(c);
 }
 
-function charIsPathSet(c: string): boolean {
+function charInPathSet(c: string): boolean {
   // prettier-ignore
-  return charIsFragmentSet(c) || ["\u0023", "\u003F", "\u007B", "\u007D"].includes(c);
+  return charInFragmentSet(c) || ["\u0023", "\u003F", "\u007B", "\u007D"].includes(c);
 }
 
-function charIsUserinfoSet(c: string): boolean {
+function charInUserinfoSet(c: string): boolean {
   // "\u0027" ("'") seemingly isn't in the spec, but matches Chrome and Firefox.
   // prettier-ignore
-  return charIsPathSet(c) || ["\u0027", "\u002F", "\u003A", "\u003B", "\u003D", "\u0040", "\u005B", "\u005C", "\u005D", "\u005E", "\u007C"].includes(c);
+  return charInPathSet(c) || ["\u0027", "\u002F", "\u003A", "\u003B", "\u003D", "\u0040", "\u005B", "\u005C", "\u005D", "\u005E", "\u007C"].includes(c);
 }
 
 function encodeChar(c: string): string {
@@ -30,7 +30,7 @@ function encodeChar(c: string): string {
 }
 
 export function encodeUserinfo(s: string): string {
-  return [...s].map((c) => (charIsUserinfoSet(c) ? encodeChar(c) : c)).join("");
+  return [...s].map((c) => (charInUserinfoSet(c) ? encodeChar(c) : c)).join("");
 }
 
 export function encodeHostname(s: string): string {
@@ -42,13 +42,13 @@ export function encodeHostname(s: string): string {
 }
 
 export function encodePathname(s: string): string {
-  return [...s].map((c) => (charIsPathSet(c) ? encodeChar(c) : c)).join("");
+  return [...s].map((c) => (charInPathSet(c) ? encodeChar(c) : c)).join("");
 }
 
 export function encodeSearch(s: string): string {
-  return [...s].map((c) => (charIsSearchSet(c) ? encodeChar(c) : c)).join("");
+  return [...s].map((c) => (charInSearchSet(c) ? encodeChar(c) : c)).join("");
 }
 
 export function encodeHash(s: string): string {
-  return [...s].map((c) => (charIsFragmentSet(c) ? encodeChar(c) : c)).join("");
+  return [...s].map((c) => (charInFragmentSet(c) ? encodeChar(c) : c)).join("");
 }
