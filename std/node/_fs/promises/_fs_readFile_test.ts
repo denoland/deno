cb70383d4f12fe37e448d335dc99aa105bd3e99d
Marcos Casagrande <marcoscvp90@gmail.com>
2020-05-20 01:16:13 +0200

    feat(std/node) - Add fs.promises readFile

---
+++ std/node/_fs/promises/_fs_readFile_test.ts
@@ -0,0 +1,39 @@
+const { test } = Deno;
+import { readFile } from "./_fs_readFile.ts";
+import * as path from "../../../path/mod.ts";
+import { assertEquals, assert } from "../../../testing/asserts.ts";
+
+const dirname = new URL(".", import.meta.url).pathname;
+
+const testData = path.resolve(
+  path.join(dirname, "..", "testdata", "hello.txt")
+);
+
+test("readFileSuccess", async function () {
+  const data = await readFile(testData);
+
+  assert(data instanceof Uint8Array);
+  assertEquals(new TextDecoder().decode(data as Uint8Array), "hello world");
+});
+
+test("readFileEncodeUtf8Success", async function () {
+  const data = await readFile(testData, { encoding: "utf8" });
+
+  assertEquals(typeof data, "string");
+  assertEquals(data as string, "hello world");
+});
+
+test("readFileEncodingAsString", async function () {
+  const data = await readFile(testData, "utf8");
+
+  assertEquals(typeof data, "string");
+  assertEquals(data as string, "hello world");
+});
+
+test("readFileError", async function () {
+  try {
+    await readFile("invalid-file", "utf8");
+  } catch (e) {
+    assert(e instanceof Deno.errors.NotFound);
+  }
+});
