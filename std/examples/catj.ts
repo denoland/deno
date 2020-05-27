54574e4f100b8911fa46cffe6b4705110025bbb9
Stephan Seidt <stephan.seidt@gmail.com>
2020-05-27 21:24:57 +0200

    Fix shebangs (#5898)

--- std/examples/catj.ts
+++ std/examples/catj.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno --allow-read
+#!/usr/bin/env -S deno run --allow-read
 // Ported from: https://github.com/soheilpro/catj
 // Copyright (c) 2014 Soheil Rashidi
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
