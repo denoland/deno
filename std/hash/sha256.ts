cb72d065647e2491c8637cb84acbba41af1fcfa6
Kitson Kelly <me@kitsonkelly.com>
2020-05-27 22:21:18 +1000

    Migrate from dprint-ignore to deno-fmt-ignore (#5659)

--- std/hash/sha256.ts
+++ std/hash/sha256.ts
@@ -15,7 +15,7 @@ const HEX_CHARS = "0123456789abcdef".split("");
 const EXTRA = [-2147483648, 8388608, 32768, 128] as const;
 const SHIFT = [24, 16, 8, 0] as const;
 // prettier-ignore
-// dprint-ignore
+// deno-fmt-ignore
 const K = [
   0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
   0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
