2668637e9bad75bef016e7f8a5f481b3c6221891
Kitson Kelly <me@kitsonkelly.com>
2020-05-29 20:24:06 +1000

    fix: REPL evaluates in strict mode (#5565)
    Since everything that Deno loads is treated as an ES Module, 
    it means that all code is treated as "use strict" except for 
    when using the REPL. This PR changes that so code in the 
    REPL is also always evaluated with "use strict". There are 
    also a couple other places where we load code as scripts 
    which should also use "use strict" just in case.

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -693,6 +693,24 @@ fn repl_test_eof() {
   assert!(err.is_empty());
 }
 
+#[test]
+fn repl_test_strict() {
+  let (_, err) = util::run_and_collect_output(
+    true,
+    "repl",
+    Some(vec![
+      "let a = {};",
+      "Object.preventExtensions(a);",
+      "a.c = 1;",
+    ]),
+    None,
+    false,
+  );
+  assert!(err.contains(
+    "Uncaught TypeError: Cannot add property c, object is not extensible"
+  ));
+}
+
 const REPL_MSG: &str = "exit using ctrl+d or close()\n";
 
 #[test]
