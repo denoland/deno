f07104185e49c8aa69b32d2f59a890d27a35c9bc
Ali Mihandoost <alimihandoost@gmail.com>
2020-05-29 05:34:29 +0430

    doc(std/async): improve documents style

--- std/async/delay.ts
+++ std/async/delay.ts
@@ -1,5 +1,14 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-/* Resolves after the given number of milliseconds. */
+
+/**
+ * Resolves after the given number of milliseconds.
+ *
+ * @param {number} ms - Number of delay in milliseconds.
+ * @return {Promise<void>}
+ *
+ * @example
+ *  await delay(1000);
+ */
 export function delay(ms: number): Promise<void> {
   return new Promise((res): number =>
     setTimeout((): void => {
