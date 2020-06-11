ced44d2e86fbab56df7b52510f078233e6e4413b
Ryan Dahl <ry@tinyclouds.org>
2020-06-11 11:22:39 -0400

    clean up cli/tests/unit/path_from_url_test.ts

--- cli/tests/unit/path_from_url_test.ts
+++ cli/tests/unit/path_from_url_test.ts
@@ -1,22 +1,22 @@
 import { assertThrows, assertEquals, unitTest } from "./test_util.ts";
 
-const {
-  pathFromURL,
-  // @ts-expect-error TypeScript (as of 3.7) does not support indexing namespaces by symbol
-} = Deno[Deno.internal];
+// @ts-expect-error TypeScript (as of 3.7) does not support indexing namespaces by symbol
+const { pathFromURL } = Deno[Deno.internal];
 
 unitTest(
-  { ignore: Deno.build.os === "windows", perms: {} },
+  { ignore: Deno.build.os === "windows" },
   function pathFromURLPosix(): void {
     assertEquals(pathFromURL("file:///test/directory"), "/test/directory");
     assertThrows(() => pathFromURL("file://host/test/directory"));
+    assertThrows(() => pathFromURL("https://deno.land/welcome.ts"));
   }
 );
 
 unitTest(
-  { ignore: Deno.build.os !== "windows", perms: {} },
+  { ignore: Deno.build.os !== "windows" },
   function pathFromURLWin32(): void {
     assertEquals(pathFromURL("file:///c:/windows/test"), "c:\\windows\\test");
     assertThrows(() => pathFromURL("file:///thing/test"));
+    assertThrows(() => pathFromURL("https://deno.land/welcome.ts"));
   }
 );
