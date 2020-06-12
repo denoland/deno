6fce6cb9c799e038bed936b5f5ef1eee3b55d25c
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-06-12 16:55:31 +0200

    v1.1.0

--- std/version.ts
+++ std/version.ts
@@ -4,4 +4,4 @@
  * the cli's API is stable. In the future when std becomes stable, likely we
  * will match versions with cli as we have in the past.
  */
-export const VERSION = "0.56.0";
+export const VERSION = "0.57.0";
