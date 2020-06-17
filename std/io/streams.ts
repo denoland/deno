b67ea00cf9994612c25643b09777f6d31c128ebf
Marcos Casagrande <marcoscvp90@gmail.com>
2020-06-18 00:11:04 +0200

    feat(std/io): Add toReader & toWriter

---
+++ std/io/streams.ts
@@ -0,0 +1,34 @@
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+
+export function toWriter(
+  streamWriter: WritableStreamDefaultWriter<Uint8Array>
+): Deno.Writer {
+  return {
+    async write(p: Uint8Array): Promise<number> {
+      await streamWriter.ready;
+      await streamWriter.write(p);
+      return p.length;
+    },
+  };
+}
+
+export function toReader(
+  streamReader: ReadableStreamDefaultReader<Uint8Array>
+): Deno.Reader {
+  const buffer = new Deno.Buffer();
+
+  return {
+    async read(p: Uint8Array): Promise<number | null> {
+      if (buffer.empty()) {
+        const res = await streamReader.read();
+        if (res.done) {
+          return null; // EOF
+        }
+
+        await Deno.writeAll(buffer, res.value);
+      }
+
+      return buffer.read(p);
+    },
+  };
+}
