47a5b49df78c376ae26c40e40e632449e169d69d
Kitson Kelly <me@kitsonkelly.com>
2020-06-18 18:30:54 +1000

    Only change to Object.create(null)

--- deno_typescript/system_loader.js
+++ deno_typescript/system_loader.js
@@ -9,11 +9,11 @@
 let System, __instantiateAsync, __instantiate;
 
 (() => {
-  const r = new Map();
+  const r = Object.create(null);
 
   System = {
     register(id, d, f) {
-      r.set(id, { d, f, exp: {} });
+      r[id] = { d, f, exp: {} };
     },
   };
 
@@ -33,7 +33,7 @@ let System, __instantiateAsync, __instantiate;
       if (s < sa.length) oa.push(...sa.slice(s));
       id = oa.reverse().join("/");
     }
-    return r.has(id) ? gExpA(id) : import(mid);
+    return id in r ? gExpA(id) : import(mid);
   }
 
   function gC(id, main) {
@@ -58,7 +58,9 @@ let System, __instantiateAsync, __instantiate;
   }
 
   function rF(main) {
-    for (const [id, m] of r.entries()) {
+    let m;
+    for (const id in r) {
+      m = r[id];
       const { f, exp } = m;
       const { execute: e, setters: s } = f(gE(exp), gC(id, id === main));
       delete m.f;
@@ -68,8 +70,8 @@ let System, __instantiateAsync, __instantiate;
   }
 
   async function gExpA(id) {
-    if (!r.has(id)) return;
-    const m = r.get(id);
+    if (!(id in r)) return;
+    const m = r[id];
     if (m.s) {
       const { d, e, s } = m;
       delete m.s;
@@ -82,8 +84,8 @@ let System, __instantiateAsync, __instantiate;
   }
 
   function gExp(id) {
-    if (!r.has(id)) return;
-    const m = r.get(id);
+    if (!(id in r)) return;
+    const m = r[id];
     if (m.s) {
       const { d, e, s } = m;
       delete m.s;
