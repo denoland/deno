dca0bb59e6f9c2105e93f0cb7fddfe3e1b9dcfd5
Casper Beyer <caspervonb@pm.me>
2020-06-28 12:43:04 +0000

    feat(std/wasi) implement fd_filestat_set_size

--- std/wasi/snapshot_preview1.ts
+++ std/wasi/snapshot_preview1.ts
@@ -607,7 +607,18 @@ export default class Module {
       },
 
       fd_filestat_set_size: (fd: number, size: bigint): number => {
-        return ERRNO_NOSYS;
+        const entry = this.fds[fd];
+        if (!entry) {
+          return ERRNO_BADF;
+        }
+
+        try {
+          Deno.ftruncateSync(entry.handle.rid, Number(size));
+        } catch (err) {
+          return errno(err);
+        }
+
+        return ERRNO_SUCCESS;
       },
 
       fd_filestat_set_times: (
