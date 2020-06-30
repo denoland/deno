7ebb769b5607e95fb2fcc4763039da8c543cdee0
Wong Jia Hau <hou32hou@gmail.com>
2020-07-01 00:39:49 +0800

    Fix stringify_test

--- std/testing/stringify_test.ts
+++ std/testing/stringify_test.ts
@@ -4,41 +4,41 @@ import { stringify } from "./stringify.ts";
 Deno.test("array(1)", () => {
   assertEquals(
     stringify([1, 2, 3]),
-    `  [
-    1,
-    2,
-    3,
-  ]`
+    `[
+  1,
+  2,
+  3,
+]`
   );
 });
 
 Deno.test("object(1)", () => {
   assertEquals(
     stringify({ x: { y: { z: 3 } } }),
-    `  {
-    "x": {
-      "y": {
-        "z": 3,
-      },
+    `{
+  "x": {
+    "y": {
+      "z": 3,
     },
-  }`
+  },
+}`
   );
 });
 
 Deno.test("array of objects", () => {
   assertEquals(
     stringify([{ x: 1 }, { y: 2 }, { z: 3 }]),
-    `  [
-    {
-      "x": 1,
-    },
-    {
-      "y": 2,
-    },
-    {
-      "z": 3,
-    },
-  ]`
+    `[
+  {
+    "x": 1,
+  },
+  {
+    "y": 2,
+  },
+  {
+    "z": 3,
+  },
+]`
   );
 });
 
@@ -55,16 +55,16 @@ Deno.test("primitive values", () => {
       (x: number): number => x + 1,
       Number.call,
     ]),
-    `  [
-    1,
-    "hi",
-    true,
-    false,
-    undefined,
-    null,
-    Symbol(x),
-    (x) => x + 1,
-    function call() { [native code] },
-  ]`
+    `[
+  1,
+  "hi",
+  true,
+  false,
+  undefined,
+  null,
+  Symbol(x),
+  (x) => x + 1,
+  function call() { [native code] },
+]`
   );
 });
