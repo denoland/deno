c8c76ea3faf7eb87829260cd1f8df6f7212b2eb7
Wong Jia Hau <hou32hou@gmail.com>
2020-07-01 12:29:06 +0800

    Fix failing diff_message_test.ts

--- std/testing/diff_message_test.ts
+++ std/testing/diff_message_test.ts
@@ -1,31 +1,32 @@
 import { assertEquals } from "./asserts.ts";
 import { diffMessageBody } from "./diff_message.ts";
 import { stripColor } from "../fmt/colors.ts";
+import diff from "./diff.ts";
 
 Deno.test("object with short keys", () => {
   const actual = diffMessageBody(
     { x: { y: { z: 2 } } },
     { x: { y: { z: 3 } } }
   );
-  const expected = `  {
-    x: {
-      y: {
--       z: 2,
-+       z: 3,
+  const expected = `    {
+      x: {
+        y: {
+-         z: 2,
++         z: 3,
+        },
       },
-    },
-  }`;
+    }`;
   assertEquals(stripColor(actual), expected);
 });
 
 Deno.test("similar values but with different key order", () => {
   const actual = diffMessageBody({ a: 1, b: 2, c: 3 }, { c: 2, b: 2, a: 1 });
-  const expected = `  {
-    a: 1,
-    b: 2,
--   c: 3,
-+   c: 2,
-  }`;
+  const expected = `    {
+      a: 1,
+      b: 2,
+-     c: 3,
++     c: 2,
+    }`;
   assertEquals(stripColor(actual), expected);
 });
 
@@ -34,14 +35,14 @@ Deno.test("array of objects (1)", () => {
     [2, { a: 1, b: 2, c: 3 }],
     [{ c: 3, b: 2, a: 1 }]
   );
-  const expected = `  [
--   2,
-    {
-      a: 1,
-      b: 2,
-      c: 3,
-    },
-  ]`;
+  const expected = `    [
+-     2,
+      {
+        a: 1,
+        b: 2,
+        c: 3,
+      },
+    ]`;
   assertEquals(stripColor(actual), expected);
 });
 
@@ -50,29 +51,29 @@ Deno.test("array of objects (2)", () => {
     [{ a: 1, b: 2, c: 3 }],
     [{ c: 2, b: 2, a: 1 }, 2]
   );
-  const expected = `  [
-    {
-      a: 1,
-      b: 2,
-+     c: 2,
--     c: 3,
-    },
-+   2,
-  ]`;
+  const expected = `    [
+      {
+        a: 1,
+        b: 2,
++       c: 2,
+-       c: 3,
+      },
++     2,
+    ]`;
   assertEquals(stripColor(actual), expected);
 });
 
 Deno.test("nested array objects", () => {
   const actual = diffMessageBody([{ x: [{ y: 2 }] }], [{ x: [{ y: 3 }] }]);
-  const expected = `  [
-    {
-      x: [
-        {
--         y: 2,
-+         y: 3,
-        },
-      ],
-    },
-  ]`;
+  const expected = `    [
+      {
+        x: [
+          {
+-           y: 2,
++           y: 3,
+          },
+        ],
+      },
+    ]`;
   assertEquals(stripColor(actual), expected);
 });
