2fba7d194277c0b8e656a55206796a417b893a9c
Ryan Dahl <ry@tinyclouds.org>
2020-07-15 13:25:40 -0400

    remove more dead code

--- cli/js.rs
+++ cli/js.rs
@@ -4,17 +4,11 @@ pub static CLI_SNAPSHOT: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.bin"));
 pub static CLI_SNAPSHOT_MAP: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.js.map"));
-#[allow(dead_code)]
-pub static CLI_SNAPSHOT_DTS: &[u8] =
-  include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.d.ts"));
 
 pub static COMPILER_SNAPSHOT: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.bin"));
 pub static COMPILER_SNAPSHOT_MAP: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.js.map"));
-#[allow(dead_code)]
-pub static COMPILER_SNAPSHOT_DTS: &[u8] =
-  include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.d.ts"));
 
 pub static DENO_NS_LIB: &str = include_str!("js/lib.deno.ns.d.ts");
 pub static SHARED_GLOBALS_LIB: &str =
