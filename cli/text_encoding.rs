637b061db57f30d56112f2f6508d6784944e97ab
Maayan Hanin <maayan.asa.hanin@gmail.com>
2020-07-16 16:25:47 +0300

    Adding unit tests for charset detection

--- cli/text_encoding.rs
+++ cli/text_encoding.rs
@@ -27,3 +27,36 @@ pub fn to_utf8(
     None => Err(std::io::ErrorKind::InvalidData.into()),
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  fn test_detection(test_data: &Vec<u8>, expected_charset: &str) {
+    let detected_charset = detect_charset(&test_data);
+    assert_eq!(
+      expected_charset.to_lowercase(),
+      detected_charset.to_lowercase()
+    );
+  }
+
+  #[test]
+  fn test_detection_utf8_no_bom() {
+    let test_data = "Hello UTF-8 it is \u{23F0} for Deno!"
+      .to_owned()
+      .into_bytes();
+    test_detection(&test_data, "utf-8");
+  }
+
+  #[test]
+  fn test_detection_utf16_little_endian() {
+    let test_data = b"\xFF\xFEHello UTF-16LE".to_owned().to_vec();
+    test_detection(&test_data, "utf-16le");
+  }
+
+  #[test]
+  fn test_detection_utf16_big_endian() {
+    let test_data = b"\xFE\xFFHello UTF-16BE".to_owned().to_vec();
+    test_detection(&test_data, "utf-16be");
+  }
+}
