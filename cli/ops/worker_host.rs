deb3c8763e7855f3617ece20e90fb89fa4080338
Wong Jia Hau <hou32hou@gmail.com>
2020-07-16 20:52:32 +0800

    Check for unstable when using import map with Worker

--- cli/ops/worker_host.rs
+++ cli/ops/worker_host.rs
@@ -214,7 +214,10 @@ fn op_create_worker(
   let args_name = args.name;
   let use_deno_namespace = args.use_deno_namespace;
   if use_deno_namespace {
-    state.check_unstable("Worker.deno");
+    state.check_unstable("Worker.deno.namespace");
+  }
+  if args.import_map.is_some() {
+    state.check_unstable("Worker.deno.importMap");
   }
   let import_map = args.import_map;
   let parent_state = state.clone();
