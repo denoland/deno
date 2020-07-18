61ea29abf352c0daa18be8f72059bc7db5913c36
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-07-18 18:39:53 +0200

    stub out source maps

--- cli/js.rs
+++ cli/js.rs
@@ -2,13 +2,11 @@ pub const TS_VERSION: &str = env!("TS_VERSION");
 
 pub static CLI_SNAPSHOT: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.bin"));
-pub static CLI_SNAPSHOT_MAP: &[u8] =
-  include_bytes!(concat!(env!("OUT_DIR"), "/CLI_SNAPSHOT.js.map"));
+pub static CLI_SNAPSHOT_MAP: &[u8] = &[];
 
 pub static COMPILER_SNAPSHOT: &[u8] =
   include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.bin"));
-pub static COMPILER_SNAPSHOT_MAP: &[u8] =
-  include_bytes!(concat!(env!("OUT_DIR"), "/COMPILER_SNAPSHOT.js.map"));
+pub static COMPILER_SNAPSHOT_MAP: &[u8] = &[];
 
 pub static DENO_NS_LIB: &str = include_str!("js/lib.deno.ns.d.ts");
 pub static SHARED_GLOBALS_LIB: &str =
