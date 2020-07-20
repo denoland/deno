bef873209458ff2ac3670214a097b1b1facd0b93
Marcin Puc <marcin.e.puc@gmail.com>
2020-07-20 18:25:38 +0200

    Fix tests

--- std/fs/write_json_test.ts
+++ std/fs/write_json_test.ts
@@ -4,10 +4,12 @@ import {
   assertThrowsAsync,
   assertThrows,
 } from "../testing/asserts.ts";
+import { EOL, format } from "./eol.ts";
 import * as path from "../path/mod.ts";
 import { writeJson, writeJsonSync } from "./write_json.ts";
 
 const testdataDir = path.resolve("fs", "testdata");
+const platformEol = Deno.build.os === "windows" ? EOL.CRLF : EOL.LF;
 
 Deno.test("writeJsonIfNotExists", async function (): Promise<void> {
   const notExistsJsonFile = path.join(testdataDir, "file_not_exists.json");
@@ -25,7 +27,8 @@ Deno.test("writeJsonIfNotExists", async function (): Promise<void> {
 
   await Deno.remove(notExistsJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{"a":"1"}\n`);
+  const expected = format(`{"a":"1"}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonIfExists", async function (): Promise<void> {
@@ -46,7 +49,8 @@ Deno.test("writeJsonIfExists", async function (): Promise<void> {
 
   await Deno.remove(existsJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{"a":"1"}\n`);
+  const expected = format(`{"a":"1"}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonIfExistsAnInvalidJson", async function (): Promise<void> {
@@ -71,7 +75,8 @@ Deno.test("writeJsonIfExistsAnInvalidJson", async function (): Promise<void> {
 
   await Deno.remove(existsInvalidJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{"a":"1"}\n`);
+  const expected = format(`{"a":"1"}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonWithSpaces", async function (): Promise<void> {
@@ -93,7 +98,8 @@ Deno.test("writeJsonWithSpaces", async function (): Promise<void> {
 
   await Deno.remove(existsJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{\n  "a": "1"\n}\n`);
+  const expected = format(`{\n  "a": "1"\n}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonWithReplacer", async function (): Promise<void> {
@@ -121,7 +127,8 @@ Deno.test("writeJsonWithReplacer", async function (): Promise<void> {
 
   await Deno.remove(existsJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{"a":"1"}\n`);
+  const expected = format(`{"a":"1"}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonSyncIfNotExists", function (): void {
@@ -140,7 +147,8 @@ Deno.test("writeJsonSyncIfNotExists", function (): void {
 
   Deno.removeSync(notExistsJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{"a":"1"}\n`);
+  const expected = format(`{"a":"1"}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonSyncIfExists", function (): void {
@@ -161,7 +169,8 @@ Deno.test("writeJsonSyncIfExists", function (): void {
 
   Deno.removeSync(existsJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{"a":"1"}\n`);
+  const expected = format(`{"a":"1"}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonSyncIfExistsAnInvalidJson", function (): void {
@@ -186,7 +195,8 @@ Deno.test("writeJsonSyncIfExistsAnInvalidJson", function (): void {
 
   Deno.removeSync(existsInvalidJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{"a":"1"}\n`);
+  const expected = format(`{"a":"1"}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonWithSpaces", function (): void {
@@ -208,7 +218,8 @@ Deno.test("writeJsonWithSpaces", function (): void {
 
   Deno.removeSync(existsJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{\n  "a": "1"\n}\n`);
+  const expected = format(`{\n  "a": "1"\n}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
 
 Deno.test("writeJsonWithReplacer", function (): void {
@@ -239,5 +250,6 @@ Deno.test("writeJsonWithReplacer", function (): void {
 
   Deno.removeSync(existsJsonFile);
 
-  assertEquals(new TextDecoder().decode(content), `{"a":"1"}\n`);
+  const expected = format(`{"a":"1"}\n`, platformEol);
+  assertEquals(new TextDecoder().decode(content), expected);
 });
