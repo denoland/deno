baf022cb3464ec6bbdad63fa59062ee5b646b5de
divy-work <dj.srivastava23@gmail.com>
2020-07-26 20:15:57 +0530

    feat: add ast parser op

--- cli/ops/runtime_compiler.rs
+++ cli/ops/runtime_compiler.rs
@@ -2,17 +2,21 @@
 use super::dispatch_json::{Deserialize, JsonOp, Value};
 use crate::futures::FutureExt;
 use crate::op_error::OpError;
+use crate::permissions::Permissions;
 use crate::state::State;
+use crate::swc_util::AstParser;
 use crate::tsc::runtime_bundle;
 use crate::tsc::runtime_compile;
 use crate::tsc::runtime_transpile;
 use deno_core::CoreIsolate;
+use deno_core::ModuleSpecifier;
 use deno_core::ZeroCopyBuf;
 use std::collections::HashMap;
 
 pub fn init(i: &mut CoreIsolate, s: &State) {
   i.register_op("op_compile", s.stateful_json_op(op_compile));
   i.register_op("op_transpile", s.stateful_json_op(op_transpile));
+  i.register_op("op_parse", s.stateful_json_op(op_parse));
 }
 
 #[derive(Deserialize, Debug)]
@@ -80,6 +84,43 @@ fn op_transpile(
   let fut = async move {
     runtime_transpile(global_state, permissions, &args.sources, &args.options)
       .await
+  }
+  .boxed_local();
+  Ok(JsonOp::Async(fut))
+}
+
+#[derive(Deserialize, Debug)]
+struct ParseArgs {
+  source_file: String,
+}
+
+fn op_parse(
+  state: &State,
+  args: Value,
+  _zero_copy: &mut [ZeroCopyBuf],
+) -> Result<JsonOp, OpError> {
+  state.check_unstable("Deno.ast");
+  let args: ParseArgs = serde_json::from_value(args)?;
+  let s = state.borrow();
+  let global_state = s.global_state.clone();
+  let module_specifier =
+    ModuleSpecifier::resolve_url_or_path(&args.source_file)?;
+  let fut = async move {
+    let out = global_state
+      .file_fetcher
+      .fetch_source_file(&module_specifier, None, Permissions::allow_all())
+      .await?;
+    let src = std::str::from_utf8(&out.source_code).unwrap();
+    let parser = AstParser::new();
+    parser.parse_module::<_, Result<Value, OpError>>(
+      &module_specifier.to_string(),
+      out.media_type,
+      &src,
+      |parse_result| {
+        let module = parse_result.unwrap();
+        Ok(serde_json::to_value(module)?)
+      },
+    )
   }
   .boxed_local();
   Ok(JsonOp::Async(fut))
