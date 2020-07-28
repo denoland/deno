527cacc252a6c7d9be0d3e864db5a6013d8c0e6b
zfx <502545703@qq.com>
2020-07-28 22:54:36 +0800

    Update tar_test.ts

--- std/archive/tar_test.ts
+++ std/archive/tar_test.ts
@@ -398,12 +398,33 @@ Deno.test("untarLinuxGeneratedTar", async function (): Promise<void> {
 
 Deno.test("directoryEntryType", async function (): Promise<void> {
   const tar = new Tar();
+  
   tar.append("folder/", {
     reader: new Deno.Buffer(),
     contentSize: 0,
+    type: "directory",
   });
-  const untar = new Untar(tar.getReader());
+  
+  const filePath = path.resolve("archive", "folder_type_test");
+  const outputFile = path.resolve("archive", "folder_type_test.tar");
+  await Deno.mkdir(filePath);
+  tar.append("archive/folder_type_test/", {
+    filePath,
+    contentSize: 0,
+    reader: new Deno.Buffer(),
+  });
+  
+  const file = await Deno.open(outputFile, { create: true, write: true });
+  await Deno.copy(tar.getReader(), file);
+  await file.close();
+  
+  const reader = await Deno.open(outputFile, { read: true });
+  const untar = new Untar(reader);
   for await (const entry of untar) {
     assertEquals(entry.type, "directory");
   }
+  
+  await reader.close();
+  await Deno.remove(filePath);
+  await Deno.remove(outputFile);
 });
