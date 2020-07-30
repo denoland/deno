fd13c55779491805072c144f76a618c572c3a0ce
Nayeem Rahman <nayeemrmn99@gmail.com>
2020-07-30 18:49:32 +0100

    fix(std/path): Remove drive letter support in posix fromFileUrl()

--- std/path/from_file_url_test.ts
+++ std/path/from_file_url_test.ts
@@ -8,12 +8,9 @@ Deno.test("[path] fromFileUrl", function () {
   assertEquals(posix.fromFileUrl("file:///home/foo%20bar"), "/home/foo bar");
   assertEquals(posix.fromFileUrl("https://example.com/foo"), "/foo");
   assertEquals(posix.fromFileUrl("file:///"), "/");
-  // Drive letters are supported platform-independently to align with the WHATWG
-  // URL specification.
-  assertEquals(posix.fromFileUrl("file:///c:"), "c:/");
-  assertEquals(posix.fromFileUrl("file:///c:/"), "c:/");
-  assertEquals(posix.fromFileUrl("file:///C:/"), "C:/");
-  assertEquals(posix.fromFileUrl("file:///C:/Users/"), "C:/Users/");
+  assertEquals(posix.fromFileUrl("file:///C:"), "/C:");
+  assertEquals(posix.fromFileUrl("file:///C:/"), "/C:/");
+  assertEquals(posix.fromFileUrl("file:///C:/Users/"), "/C:/Users/");
   assertEquals(posix.fromFileUrl("file:///C:foo/bar"), "/C:foo/bar");
 });
 
@@ -23,8 +20,7 @@ Deno.test("[path] fromFileUrl (win32)", function () {
   assertEquals(win32.fromFileUrl("file:///home/foo%20bar"), "\\home\\foo bar");
   assertEquals(win32.fromFileUrl("https://example.com/foo"), "\\foo");
   assertEquals(win32.fromFileUrl("file:///"), "\\");
-  assertEquals(win32.fromFileUrl("file:///c:"), "c:\\");
-  assertEquals(win32.fromFileUrl("file:///c:/"), "c:\\");
+  assertEquals(win32.fromFileUrl("file:///C:"), "C:\\");
   assertEquals(win32.fromFileUrl("file:///C:/"), "C:\\");
   assertEquals(win32.fromFileUrl("file:///C:/Users/"), "C:\\Users\\");
   assertEquals(win32.fromFileUrl("file:///C:foo/bar"), "\\C:foo\\bar");
