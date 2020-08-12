5256f1cadf3f386b2df654cfc405aa8b1991e4aa
crowlkats <13135287+crowlKats@users.noreply.github.com>
2020-08-12 14:32:54 +0200

    Change to async

--- cli/ops/websocket.rs
+++ cli/ops/websocket.rs
@@ -4,14 +4,12 @@ use crate::op_error::OpError;
 use crate::state::State;
 use deno_core::ZeroCopyBuf;
 use deno_core::{CoreIsolate, CoreIsolateState};
-//use futures::future::FutureExt;
+use futures::future::FutureExt;
 use std::borrow::Cow;
-//use tokio::net::TcpStream;
+use tokio::net::TcpStream;
 use tokio_tungstenite::tungstenite::protocol::frame::coding::CloseCode;
 use tokio_tungstenite::tungstenite::protocol::CloseFrame;
-//use tokio_tungstenite::{connect_async, WebSocketStream};
-use tokio_tungstenite::tungstenite::client::AutoStream;
-use tokio_tungstenite::tungstenite::{connect, WebSocket};
+use tokio_tungstenite::{connect_async, WebSocketStream};
 
 pub fn init(i: &mut CoreIsolate, s: &State) {
   i.register_op("op_ws_create", s.stateful_json_op2(op_ws_create));
@@ -25,16 +23,20 @@ struct CreateArgs {
 }
 
 pub fn op_ws_create(
-  isolate_state: &mut CoreIsolateState,
+  isolate_state: & mut CoreIsolateState,
   _state: &State,
   args: Value,
   _zero_copy: &mut [ZeroCopyBuf],
 ) -> Result<JsonOp, OpError> {
   let args: CreateArgs = serde_json::from_value(args)?;
-  let mut resource_table = isolate_state.resource_table.borrow_mut();
-  let (stream, _) = connect(args.url).unwrap();
-  let rid = resource_table.add("webSocketStream", Box::new(stream));
-  Ok(JsonOp::Sync(json!(rid)))
+  let resource_table = isolate_state.resource_table.clone();
+  let future = async move {
+    let mut resource_table = resource_table.borrow_mut();
+    let (stream, _) = connect_async(args.url).await.unwrap();
+    let rid = resource_table.add("webSocketStream", Box::new(stream));
+    Ok(json!(rid))
+  };
+  Ok(JsonOp::Async(future.boxed_local()))
 }
 
 #[derive(Deserialize)]
@@ -52,24 +54,24 @@ pub fn op_ws_close(
   _zero_copy: &mut [ZeroCopyBuf],
 ) -> Result<JsonOp, OpError> {
   let args: CloseArgs = serde_json::from_value(args)?;
-  let mut resource_table = isolate_state.resource_table.borrow_mut();
-  let stream = resource_table
-    .get_mut::<WebSocket<AutoStream>>(args.rid)
-    .ok_or_else(OpError::bad_resource_id)?;
+  let resource_table = isolate_state.resource_table.clone();
+  let future = async move {
+    let mut resource_table = resource_table.borrow_mut();
+    let stream = resource_table
+      .get_mut::<WebSocketStream<TcpStream>>(args.rid)
+      .ok_or_else(OpError::bad_resource_id)?;
+    stream
+      .close(Some(CloseFrame {
+        code: CloseCode::from(args.code.unwrap_or(1005)),
+        reason: match args.reason {
+          Some(reason) => Cow::from(reason),
+          None => Default::default(),
+        },
+      })).await.unwrap();
 
-  stream
-    .close(Some(CloseFrame {
-      code: CloseCode::from(args.code.unwrap_or(1005)),
-      reason: match args.reason {
-        Some(reason) => Cow::from(reason),
-        None => Default::default(),
-      },
-    }))
-    .unwrap();
+    Ok(json!({}))
+  };
 
-  resource_table
-    .close(args.rid)
-    .ok_or_else(OpError::bad_resource_id)?;
 
-  Ok(JsonOp::Sync(json!({})))
+  Ok(JsonOp::Async(future.boxed_local()))
 }
--- cli/rt/27_websocket.js
+++ cli/rt/27_websocket.js
@@ -1,20 +1,41 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
 ((window) => {
-  const { sendSync } = window.__bootstrap.dispatchJson;
+  const { sendAsync } = window.__bootstrap.dispatchJson;
 
   class WebSocket extends EventTarget {
     constructor(url) {
       super();
 
-      this.rid = sendSync("op_ws_create", { url });
+      const wsURL = new URL(url);
+
+      if ((wsURL.protocol !== "ws:" && wsURL.protocol !== "wss:") || wsURL.hash !== "") {
+        throw new SyntaxError();
+      }
+
+      this.url = wsURL.href;
+
+      sendAsync("op_ws_create", { url: wsURL.href }).then(rid => {
+        this.rid = rid;
+      });
     }
 
     close(code, reason) {
-      sendSync("op_ws_close", {
+      if (code && (code !== 1000 && !(3000 <= code > 5000))) {
+        throw new TypeError();
+      }
+
+      let encoder = new TextEncoder();
+      if (reason && encoder.encode(reason).byteLength > 123) {
+        throw new SyntaxError();
+      }
+
+      sendAsync("op_ws_close", {
         rid: this.rid,
         code,
         reason,
+      }).then(() => {
+        console.log("closed");
       });
     }
   }
