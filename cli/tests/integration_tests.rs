69e546363b39b15d77b2780ccf0c1babae1ca1be
Nayeem Rahman <nayeemrmn99@gmail.com>
2020-08-18 02:05:50 +0100

    fixup! Fix request tests

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -1609,7 +1609,6 @@ fn itest_pty(args: &str, output: &str, input: &[u8]) {
     let mut output_actual = String::new();
     master.write_all(input).unwrap();
     master.read_to_string(&mut output_actual).unwrap();
-    println!("{}", &output_actual);
     fork.wait().unwrap();
 
     let output_expected =
