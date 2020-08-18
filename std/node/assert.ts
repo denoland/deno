6cad8f3947ed56fcca60678759a5c4ca9a3a1687
Benjamin Lupton <b@lupton.cc>
2020-08-18 10:45:06 +1000

    feat(std/node/assert.ts): add basic support
    - https://nodejs.org/dist/latest-v14.x/docs/api/assert.html
    - https://github.com/denoland/deno/blob/master/std/testing/asserts.ts

---
+++ std/node/assert.ts
@@ -0,0 +1,9 @@
+import { assert, assertEquals, assertNotEquals, assertStrictEquals, assertMatch, fail, assertThrows } from "../testing/asserts.ts";
+
+export assert;
+export const equal = assertEquals;
+export const notEqual = assertNotEquals;
+export const strictEqual = assertStrictEquals;
+export const match = assertMatch;
+export fail;
+export const throws = assertThrows;
