8ba66b96b5901f7bc9f1b6a22046bd096b0a4586
Benjamin Lupton <b@lupton.cc>
2020-08-18 10:47:23 +1000

    Update assert.ts

--- std/node/assert.ts
+++ std/node/assert.ts
@@ -1,9 +1,9 @@
-import { assert, assertEquals, assertNotEquals, assertStrictEquals, assertMatch, fail, assertThrows } from "../testing/asserts.ts";
+import { assertEquals, assertNotEquals, assertStrictEquals, assertMatch, assertThrows } from "../testing/asserts.ts";
 
-export assert;
+export { assert, fail } from "../testing/asserts.ts";
+
 export const equal = assertEquals;
 export const notEqual = assertNotEquals;
 export const strictEqual = assertStrictEquals;
 export const match = assertMatch;
-export fail;
 export const throws = assertThrows;
