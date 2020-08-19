39226a768b23d950753b7f310e6e0fb5400dcba1
Luca Casonato <lucacasonato@yahoo.com>
2020-08-19 13:27:09 +0200

    fmt

--- op_crates/web/lib.deno_web.d.ts
+++ op_crates/web/lib.deno_web.d.ts
@@ -188,51 +188,50 @@ declare class TextEncoder {
   readonly [Symbol.toStringTag]: string;
 }
 
-
 /** A controller object that allows you to abort one or more DOM requests as and
  * when desired. */
 declare class AbortController {
-   /** Returns the AbortSignal object associated with this object. */
-   readonly signal: AbortSignal;
-   /** Invoking this method will set this object's AbortSignal's aborted flag and
+  /** Returns the AbortSignal object associated with this object. */
+  readonly signal: AbortSignal;
+  /** Invoking this method will set this object's AbortSignal's aborted flag and
     * signal to any observers that the associated activity is to be aborted. */
-   abort(): void;
- }
- 
- interface AbortSignalEventMap {
-   abort: Event;
- }
- 
- /** A signal object that allows you to communicate with a DOM request (such as a
+  abort(): void;
+}
+
+interface AbortSignalEventMap {
+  abort: Event;
+}
+
+/** A signal object that allows you to communicate with a DOM request (such as a
   * Fetch) and abort it if required via an AbortController object. */
- interface AbortSignal extends EventTarget {
-   /** Returns true if this AbortSignal's AbortController has signaled to abort,
+interface AbortSignal extends EventTarget {
+  /** Returns true if this AbortSignal's AbortController has signaled to abort,
     * and false otherwise. */
-   readonly aborted: boolean;
-   onabort: ((this: AbortSignal, ev: Event) => any) | null;
-   addEventListener<K extends keyof AbortSignalEventMap>(
-     type: K,
-     listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any,
-     options?: boolean | AddEventListenerOptions,
-   ): void;
-   addEventListener(
-     type: string,
-     listener: EventListenerOrEventListenerObject,
-     options?: boolean | AddEventListenerOptions,
-   ): void;
-   removeEventListener<K extends keyof AbortSignalEventMap>(
-     type: K,
-     listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any,
-     options?: boolean | EventListenerOptions,
-   ): void;
-   removeEventListener(
-     type: string,
-     listener: EventListenerOrEventListenerObject,
-     options?: boolean | EventListenerOptions,
-   ): void;
- }
- 
- declare const AbortSignal: {
-   prototype: AbortSignal;
-   new (): AbortSignal;
- };
+  readonly aborted: boolean;
+  onabort: ((this: AbortSignal, ev: Event) => any) | null;
+  addEventListener<K extends keyof AbortSignalEventMap>(
+    type: K,
+    listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  removeEventListener<K extends keyof AbortSignalEventMap>(
+    type: K,
+    listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any,
+    options?: boolean | EventListenerOptions,
+  ): void;
+  removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventListenerOptions,
+  ): void;
+}
+
+declare const AbortSignal: {
+  prototype: AbortSignal;
+  new (): AbortSignal;
+};
