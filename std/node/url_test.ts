3ccec1b35e58d362519c3d2c3356f84f02df9149
Benjamin Lupton <b@lupton.cc>
2020-08-21 01:38:05 +1000

    Create url_test.ts

---
+++ std/node/url_test.ts
@@ -0,0 +1,9 @@
+import { assert, assertEquals } from "../testing/asserts.ts";
+import * as url from "./url.ts";
+
+Deno.test({
+  name: "[url] URL",
+  fn() {
+    assertEquals(url.URL, URL);
+  },
+});
