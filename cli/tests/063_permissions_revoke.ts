1aaf2546c7a6254b2b83719f465b4fd3aab70d0d
Chris Knight <cknight1234@gmail.com>
2020-08-24 22:43:46 +0100

    chore: format

--- cli/tests/063_permissions_revoke.ts
+++ cli/tests/063_permissions_revoke.ts
@@ -1,9 +1,15 @@
 const status1 = await Deno.permissions.revoke({ name: "read", path: "foo" });
 const status2 = await Deno.permissions.query({ name: "read", path: "bar" });
 const status3 = await Deno.permissions.revoke({ name: "read", path: "bar" });
-const status4 = await Deno.permissions.revokeSync({ name: "write", path: "foo" });
-const status5 = await Deno.permissions.querySync({ name: "write", path: "bar" });
-const status6 = await Deno.permissions.revokeSync({ name: "write", path: "bar" });
+const status4 = await Deno.permissions.revokeSync(
+  { name: "write", path: "foo" },
+);
+const status5 = await Deno.permissions.querySync(
+  { name: "write", path: "bar" },
+);
+const status6 = await Deno.permissions.revokeSync(
+  { name: "write", path: "bar" },
+);
 console.log(status1);
 console.log(status2);
 console.log(status3);
--- cli/tests/064_permissions_revoke_global.ts
+++ cli/tests/064_permissions_revoke_global.ts
@@ -2,8 +2,12 @@ const status1 = await Deno.permissions.revoke({ name: "read" });
 const status2 = await Deno.permissions.query({ name: "read", path: "foo" });
 const status3 = await Deno.permissions.query({ name: "read", path: "bar" });
 const status4 = await Deno.permissions.revokeSync({ name: "write" });
-const status5 = await Deno.permissions.querySync({ name: "write", path: "foo" });
-const status6 = await Deno.permissions.querySync({ name: "write", path: "bar" });
+const status5 = await Deno.permissions.querySync(
+  { name: "write", path: "foo" },
+);
+const status6 = await Deno.permissions.querySync(
+  { name: "write", path: "bar" },
+);
 console.log(status1);
 console.log(status2);
 console.log(status3);
