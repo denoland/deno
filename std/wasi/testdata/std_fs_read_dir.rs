f7174267e3e521636cd4abebbecd107d7079a313
Casper Beyer <caspervonb@pm.me>
2020-08-25 20:36:50 +0800

    test(std/wasi): add wasi-testsuite as a submodule (#7042)

--- std/wasi/testdata/std_fs_read_dir.rs
+++
@@ -1,9 +0,0 @@
-// { "preopens": { "/fixture": "fixture" } }
-
-fn main() {
-  let entries = std::fs::read_dir("/fixture").unwrap();
-  assert_eq!(entries.count(), 4);
-
-  let entries = std::fs::read_dir("/fixture/directory").unwrap();
-  assert_eq!(entries.count(), 2);
-}
