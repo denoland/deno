295c7c85119148c315b2e759e9ff27e3b7d45cf7
Yusuke Tanaka <yusuktan@maguro.dev>
2020-09-06 23:43:42 +0900

    fix type definition

--- cli/dts/lib.deno.ns.d.ts
+++ cli/dts/lib.deno.ns.d.ts
@@ -162,7 +162,7 @@ declare namespace Deno {
    * });
    * ```
    * */
-  export function test(name: string, fn: () => void | Promise<void>): void;
+  export function test(name: string, fn: () => void | Promise<void>, timeout?: number): void;
 
   /** Exit the Deno process with optional exit code. If no exit code is supplied
    * then Deno will exit with return code of 0.
