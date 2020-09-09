7da57e45ff8d5d4ec2e9d1dd7144b02488fbd37b
Casper Beyer <caspervonb@pm.me>
2020-09-09 02:32:06 +0000

    Make op_datasync_async async via tokio::fs::File::sync_data

--- cli/ops/fs.rs
+++ cli/ops/fs.rs
@@ -297,12 +297,21 @@ async fn op_fdatasync_async(
   state.check_unstable("Deno.fdatasync");
   let args: FdatasyncArgs = serde_json::from_value(args)?;
   let rid = args.rid as u32;
-  std_file_resource(&state, rid, |r| match r {
-    Ok(std_file) => std_file.sync_data().map_err(ErrBox::from),
-    Err(_) => Err(ErrBox::type_error(
-      "cannot sync this type of resource".to_string(),
+
+  let mut resource_table = state.resource_table.borrow_mut();
+  let resource_holder = resource_table
+    .get_mut::<StreamResourceHolder>(rid)
+    .ok_or_else(ErrBox::bad_resource_id)?;
+
+  match &mut resource_holder.resource {
+    StreamResource::FsFile(Some((file, _))) => {
+      file.sync_data().await.map_err(ErrBox::from)
+    }
+    _ => Err(ErrBox::type_error(
+      "cannot sync on this type of resource".to_string(),
     )),
-  })?;
+  }?;
+
   Ok(json!({}))
 }
 
