72e22c49fb8b3b0edc1729fcb3878ad7c75f5432
tokiedokie <thetokiedokie@gmail.com>
2020-09-14 20:18:00 +0900

    test(cli/tests/https_import): use ts file in localhost instead of remote (#7431)

--- cli/tests/https_import.ts
+++ cli/tests/https_import.ts
@@ -1,5 +1,3 @@
-// TODO Use https://localhost:4555/ but we need more infrastructure to
-// support verifying self-signed certificates.
-import { printHello } from "https://gist.githubusercontent.com/ry/f12b2aa3409e6b52645bc346a9e22929/raw/79318f239f51d764384a8bded8d7c6a833610dde/print_hello.ts";
+import { printHello } from "https://localhost:5545/cli/tests/subdir/print_hello.ts";
 
 printHello();
