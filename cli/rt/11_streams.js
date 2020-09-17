4ca2dd39f8747308ca67ae034d5d0b276a95e3c8
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-09-17 13:22:44 +0200

    don't depend on Deno.inspect in streams and performance

--- cli/rt/11_streams.js
+++ cli/rt/11_streams.js
@@ -11,7 +11,8 @@
 
   const { cloneValue, setFunctionName } = window.__bootstrap.webUtil;
   const { assert, AssertionError } = window.__bootstrap.util;
-  const { customInspect, inspect } = window.__bootstrap.console;
+
+  const customInspect = Symbol.for("Deno.customInspect");
 
   const sym = {
     abortAlgorithm: Symbol("abortAlgorithm"),
@@ -636,9 +637,7 @@
     }
 
     [customInspect]() {
-      return `${this.constructor.name} {\n  readable: ${
-        inspect(this.readable)
-      }\n  writable: ${inspect(this.writable)}\n}`;
+      return this.constructor.name;
     }
   }
 
