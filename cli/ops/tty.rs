d84eea1396bc7f2ff0e6dd0f4c90ae727b387097
MikaelUrankar <mikael.urankar@gmail.com>
2020-09-19 10:03:26 +0200

    Fix build on FreeBSD
    libc::TIOCGWINSZ is c_uint on FreeBSD but libc::ioctl expects a c_ulong arg
    
    error[E0308]: mismatched types
       --> cli/ops/tty.rs:314:30
        |
    314 |           if libc::ioctl(fd, libc::TIOCGWINSZ, &mut size as *mut _) != 0 {
        |                              ^^^^^^^^^^^^^^^^
        |                              |
        |                              expected `u64`, found `u32`
        |                              help: you can convert an `u32` to `u64`: `libc::TIOCGWINSZ.into()`

--- cli/ops/tty.rs
+++ cli/ops/tty.rs
@@ -311,7 +311,7 @@ fn op_console_size(
         let fd = std_file.as_raw_fd();
         unsafe {
           let mut size: libc::winsize = std::mem::zeroed();
-          if libc::ioctl(fd, libc::TIOCGWINSZ, &mut size as *mut _) != 0 {
+          if libc::ioctl(fd, libc::TIOCGWINSZ.into(), &mut size as *mut _) != 0 {
             return Err(last_os_error());
           }
 
