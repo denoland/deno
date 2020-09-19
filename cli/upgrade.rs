5d94324a955a04ed14f859d19cbe51f9137da1e2
MikaelUrankar <mikael.urankar@gmail.com>
2020-09-19 10:11:22 +0200

    Add FreeBSD to the list

--- cli/upgrade.rs
+++ cli/upgrade.rs
@@ -36,6 +36,8 @@ const ARCHIVE_NAME: &str = "deno-x86_64-pc-windows-msvc.zip";
 const ARCHIVE_NAME: &str = "deno-x86_64-apple-darwin.zip";
 #[cfg(target_os = "linux")]
 const ARCHIVE_NAME: &str = "deno-x86_64-unknown-linux-gnu.zip";
+#[cfg(target_os = "freebsd")]
+const ARCHIVE_NAME: &str = "deno-x86_64-unknown-freebsd.zip";
 
 async fn get_latest_version(client: &Client) -> Result<Version, AnyError> {
   println!("Checking for latest version");
