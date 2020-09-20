fa8228b33fee79e4c9423fa529d06315281e8360
tokiedokie <thetokiedokie@gmail.com>
2020-09-20 15:19:28 +0900

    lint ignore

--- std/wasi/snapshot_preview1.ts
+++ std/wasi/snapshot_preview1.ts
@@ -1,5 +1,5 @@
-// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 /* eslint-disable */
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
 import { resolve } from "../path/mod.ts";
 
