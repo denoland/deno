93615bbbd7b9cc93c3dad5460a95eb9585f04c28
alianmad <aliahmedreda34@gmail.com>
2020-09-25 23:47:51 +0200

    handling "bigint" options in fs.stat and fs.lstat

--- std/node/_fs/_fs_lstat.ts
+++ std/node/_fs/_fs_lstat.ts
@@ -1,34 +1,56 @@
 import {
   statCallback,
+  statCallbackBigInt,
   statOptions,
-  convertFileInfoToStats,
+  CFISBIS,
   Stats,
+  BigIntStats,
 } from "./_fs_stat.ts";
 
-export function lstat(
-  path: string | URL,
-  options: statOptions,
-  callback: statCallback
-): void;
 export function lstat(path: string | URL, callback: statCallback): void;
 export function lstat(
   path: string | URL,
-  optionsOrCallback: statCallback | statOptions,
-  maybeCallback?: statCallback
+  options: { bigint: false },
+  callback: statCallback
+): void;
+export function lstat(
+  path: string | URL,
+  options: { bigint: true },
+  callback: statCallbackBigInt
+): void;
+export function lstat(
+  path: string | URL,
+  optionsOrCallback: statCallback | statCallbackBigInt | statOptions,
+  maybeCallback?: statCallback | statCallbackBigInt
 ) {
   const callback =
     typeof optionsOrCallback === "function"
       ? optionsOrCallback
       : maybeCallback || (() => {});
-  // const options =
-  //  typeof optionsOrCallback === "object" ? optionsOrCallback : null;
+  const options =
+    typeof optionsOrCallback === "object"
+      ? optionsOrCallback
+      : { bigint: false };
   Deno.lstat(path)
-    .then((stat) => callback(undefined, convertFileInfoToStats(stat)))
+    // @ts-ignore
+    .then((stat) => callback(undefined, CFISBIS(stat, options.bigint)))
     // @ts-ignore
     .catch((err) => callback(err, null));
 }
 
-export function lstatSync(path: string | URL, options?: statOptions): Stats {
+export function lstatSync(path: string | URL): Stats;
+export function lstatSync(
+  path: string | URL,
+  options: { bigint: false }
+): Stats;
+export function lstatSync(
+  path: string | URL,
+  options: { bigint: true }
+): BigIntStats;
+export function lstatSync(
+  path: string | URL,
+  options?: statOptions
+): Stats | BigIntStats {
   const origin = Deno.lstatSync(path);
-  return convertFileInfoToStats(origin);
+  return CFISBIS(origin, options?.bigint || false);
 }
