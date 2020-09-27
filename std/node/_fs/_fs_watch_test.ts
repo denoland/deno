04e3805f5b080a8a492b4b61716e02c2f8426824
alianmad <aliahmedreda34@gmail.com>
2020-09-27 03:37:40 +0200

    fix watch testing

--- std/node/_fs/_fs_watch_test.ts
+++ std/node/_fs/_fs_watch_test.ts
@@ -1,16 +1,28 @@
 import { watch } from "./_fs_watch.ts";
 import { assertEquals, fail } from "../../testing/asserts.ts";
 
+function wait(time: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
 Deno.test({
   name: "watching a file",
-  fn() {
+  async fn() {
     const file = Deno.makeTempFileSync();
     const result: [string, string][] = [];
     const watcher = watch(file, (eventType, filename) =>
       result.push([eventType, filename])
     );
+    await wait(100);
     Deno.writeTextFileSync(file, "something");
+    await wait(100);
     watcher.close();
-    assertEquals(result, [["modify", file]]);
+    assertEquals(result, [
+      ["modify", file],
+      ["modify", file],
+      ["access", file],
+    ]);
   },
 });
