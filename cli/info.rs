99ec6ae80b2c7c874420189d1b76cd8d081bbd1d
Valentin Anger <syrupthinker@gryphno.de>
2020-09-27 21:50:18 +0200

    feat(unstable): add module specifier to deno info --json output (#7725)

--- cli/info.rs
+++ cli/info.rs
@@ -16,6 +16,7 @@ use std::sync::Arc;
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ModuleDepInfo {
+  module: String,
   local: String,
   file_type: String,
   compiled: Option<String>,
@@ -70,6 +71,7 @@ impl ModuleDepInfo {
     let files = FileInfoDepFlatGraph::new(&module_graph);
 
     let info = Self {
+      module: module_specifier.to_string(),
       local: local_filename,
       file_type,
       compiled: compiled_filename,
