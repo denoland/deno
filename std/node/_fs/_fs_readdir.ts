474a201a2de92184f8134d0322947997a060cd1c
alianmad <aliahmedreda34@gmail.com>
2020-10-01 17:31:32 +0200

    using options in readdir and readdirSync

--- std/node/_fs/_fs_readdir.ts
+++ std/node/_fs/_fs_readdir.ts
@@ -1,4 +1,7 @@
 import { asyncIterableToCallback } from "./_fs_watch.ts";
+import { lstatSync } from "./_fs_lstat.ts";
+import { join } from "../../path/mod.ts";
+import { fromFileUrl } from "../path.ts";
 
 type readDirOptions = {
   encoding?: string;
@@ -7,44 +10,146 @@ type readDirOptions = {
 
 type readDirCallback = (err: Error | undefined, files: string[]) => void;
 
+type readDirCallbackDirent = (err: Error | undefined, files: Dirent[]) => void;
+
+type readDirBoth = (
+  err: Error | undefined,
+  files: string[] | Dirent[] | (string | Dirent)[]
+) => void;
+
 export function readdir(
   path: string | URL,
-  options: readDirOptions,
-  callback: readDirCallback,
+  options: { withFileTypes?: false; encoding?: string },
+  callback: readDirCallback
+): void;
+export function readdir(
+  path: string | URL,
+  options: { withFileTypes: true; encoding?: string },
+  callback: readDirCallbackDirent
 ): void;
 export function readdir(path: string | URL, callback: readDirCallback): void;
 export function readdir(
   path: string | URL,
-  optionsOrCallback: readDirOptions | readDirCallback,
-  maybeCallback?: readDirCallback,
+  optionsOrCallback: readDirOptions | readDirCallback | readDirCallbackDirent,
+  maybeCallback?: readDirCallback | readDirCallbackDirent
 ) {
-  const callback = typeof optionsOrCallback === "function"
+  const callback = (typeof optionsOrCallback === "function"
     ? optionsOrCallback
-    : maybeCallback;
-  // const options = typeof optionsOrCallback === "object" ? optionsOrCallback : null;
-  const result: string[] = [];
+    : maybeCallback) as readDirBoth | undefined;
+  const options =
+    typeof optionsOrCallback === "object" ? optionsOrCallback : null;
+  const result: (string | Dirent)[] = [];
+  path = path instanceof URL ? fromFileUrl(path) : path;
 
   if (!callback) throw new Error("No callback function supplied");
 
+  if (options?.encoding) {
+    try {
+      new TextDecoder(options.encoding);
+    } catch (error) {
+      throw new Error(
+        `TypeError [ERR_INVALID_OPT_VALUE_ENCODING]: The value "${options.encoding}" is invalid for option "encoding"`
+      );
+    }
+  }
+
   try {
     asyncIterableToCallback(Deno.readDir(path), (val, done) => {
+      if (typeof path !== "string") return;
       if (done) {
         callback(undefined, result);
         return;
       }
-      result.push(val.name);
+      if (options?.withFileTypes)
+        result.push(toDirent(path, val, options.encoding));
+      else result.push(decode(val.name));
     });
   } catch (error) {
     callback(error, result);
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function readdirSync(path: string | URL, options?: readDirOptions) {
+type Dirent = {
+  name: string;
+  isBlockDevice: () => boolean;
+  isCharacterDevice: () => boolean;
+  isDirectory: () => boolean;
+  isFIFO: () => boolean;
+  isFile: () => boolean;
+  isSocket: () => boolean;
+  isSymbolicLink: () => boolean;
+};
+
+function toDirent(
+  path: string,
+  file: Deno.DirEntry,
+  encoding?: string
+): Dirent {
+  const lstat = lstatSync(join(path, file.name));
+  return {
+    name: decode(file.name, encoding),
+    isBlockDevice() {
+      return lstat.isBlockDevice();
+    },
+    isFile() {
+      return lstat.isFile();
+    },
+    isFIFO() {
+      return lstat.isFIFO();
+    },
+    isDirectory() {
+      return lstat.isDirectory();
+    },
+    isCharacterDevice() {
+      return lstat.isCharacterDevice();
+    },
+    isSocket() {
+      return lstat.isSocket();
+    },
+    isSymbolicLink() {
+      return lstat.isSymbolicLink();
+    },
+  };
+}
+
+function decode(str: string, encoding?: string): string {
+  if (!encoding) return str;
+  else {
+    const decoder = new TextDecoder(encoding);
+    const encoder = new TextEncoder();
+    return decoder.decode(encoder.encode(str));
+  }
+}
+
+export function readdirSync(
+  path: string | URL,
+  options: { withFileTypes: true; encoding?: string }
+): Dirent[];
+export function readdirSync(
+  path: string | URL,
+  options?: { withFileTypes?: false; encoding?: string }
+): string[];
+export function readdirSync(
+  path: string | URL,
+  options?: readDirOptions
+): (string | Dirent)[] {
   const result = [];
+  path = path instanceof URL ? fromFileUrl(path) : path;
+
+  if (options?.encoding) {
+    try {
+      new TextDecoder(options.encoding);
+    } catch (error) {
+      throw new Error(
+        `TypeError [ERR_INVALID_OPT_VALUE_ENCODING]: The value "${options.encoding}" is invalid for option "encoding"`
+      );
+    }
+  }
 
   for (const file of Deno.readDirSync(path)) {
-    result.push(file.name);
+    if (options?.withFileTypes) {
+      result.push(toDirent(path, file, options.encoding));
+    } else result.push(decode(file.name));
   }
   return result;
 }
