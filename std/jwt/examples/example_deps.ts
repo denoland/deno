7fdf1a3eb8f6e8b831fdec518f0d5b0d4bf2dead
Tim Reichen <tim.reichen@gmail.com>
2020-10-02 23:36:58 +0200

    relative std module specifiers

--- std/jwt/examples/example_deps.ts
+++ std/jwt/examples/example_deps.ts
@@ -1,2 +1,2 @@
-export { serve } from "https://deno.land/std@0.69.0/http/server.ts";
-export { encode, decode } from "https://deno.land/std@0.69.0/encoding/utf8.ts";
+export { serve } from "../../http/server.ts";
+export { encode, decode } from "../../encoding/utf8.ts";
