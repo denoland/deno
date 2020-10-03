2839eefc370010b72655aeeb559266a3bf9e87f2
bartossh <lenart.consulting@gmail.com>
2020-10-03 21:42:40 +0200

    refactor debounce to be awaited, remove select to fix race condition

--- cli/file_watcher.rs
+++ cli/file_watcher.rs
@@ -1,8 +1,9 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
 use crate::colors;
+use core::task::{Context, Poll};
 use deno_core::error::AnyError;
-use deno_core::futures::stream::StreamExt;
+use deno_core::futures::stream::{Stream, StreamExt};
 use deno_core::futures::Future;
 use notify::event::Event as NotifyEvent;
 use notify::event::EventKind;
@@ -14,35 +15,60 @@ use notify::Watcher;
 use std::path::PathBuf;
 use std::pin::Pin;
 use std::sync::Mutex;
-use std::time::Instant;
-use tokio::select;
+use std::time::{Duration, Instant};
 use tokio::sync::{mpsc, mpsc::Receiver};
 
 /// Time without update required to pass to assume after which fluctuations are over
-const DEBOUNCE_TIME_MS: usize = 200;
+const DEBOUNCE_TIME_MS: Duration = Duration::from_millis(200);
 
 // TODO(bartlomieju): rename
 type WatchFuture = Pin<Box<dyn Future<Output = Result<(), AnyError>>>>;
 
+// TODO(bartossh): make generic and move to unique mod
 struct Debounce {
-  last: Instant,
-  duration_ms: usize,
+  rx: Receiver<Result<NotifyEvent, AnyError>>,
+  debounce_time: Duration,
+  last_event: NotifyEvent,
 }
 
 impl Debounce {
-  /// Creates debounce instance
-  /// that takes time in ms after which fluctuations are recognized to be over
-  fn new(duration_ms: usize) -> Self {
-    let last = Instant::now();
-    Self { last, duration_ms }
+  fn new(
+    rx: Receiver<Result<NotifyEvent, AnyError>>,
+    debounce_time: Duration,
+  ) -> Self {
+    Self {
+      rx,
+      debounce_time,
+      last_event: Default::default(),
+    }
   }
+}
 
-  /// Check if fluctuations has passed
-  fn fluct_is_over(&mut self) -> bool {
-    let waiting_fluct_is_over =
-      self.duration_ms <= self.last.elapsed().as_millis() as usize;
-    self.last = Instant::now();
-    waiting_fluct_is_over
+impl Stream for Debounce {
+  type Item = NotifyEvent;
+
+  fn poll_next(
+    self: Pin<&mut Self>,
+    _cx: &mut Context,
+  ) -> Poll<Option<Self::Item>> {
+    let mut _self = self.get_mut();
+    let mut timeout = Instant::now();
+    let mut recv = false;
+    loop {
+      if let Ok(result) = _self.rx.try_recv() {
+        if let Ok(event) = result {
+          if event == _self.last_event {
+            timeout = Instant::now();
+          }
+          _self.last_event = event;
+          recv = true;
+        }
+      }
+      if recv && timeout.elapsed() >= _self.debounce_time {
+        break;
+      }
+    }
+    Poll::Ready(Some(_self.last_event.clone()))
   }
 }
 
@@ -61,49 +87,32 @@ pub async fn watch_func<F>(
 where
   F: Fn() -> WatchFuture,
 {
-  let mut debounce = Debounce::new(DEBOUNCE_TIME_MS);
   let (_watcher, receiver) = new_watcher(paths)?;
-  let receiver = Mutex::new(receiver);
+  let debounce = Mutex::new(Debounce::new(receiver, DEBOUNCE_TIME_MS));
   loop {
     let func = error_handler(closure());
-    let mut is_file_changed = false;
-    select! {
-      _ = wait_for_file_change(&receiver, &mut debounce) => {
-          is_file_changed = true;
-          info!(
-            "{} File change detected! Restarting!",
-            colors::intense_blue("Watcher")
-          );
-        },
-      _ = func => { },
-    };
-    if !is_file_changed {
-      info!(
-        "{} Process terminated! Restarting on file change...",
-        colors::intense_blue("Watcher")
-      );
-      wait_for_file_change(&receiver, &mut debounce).await?;
-      info!(
-        "{} File change detected! Restarting!",
-        colors::intense_blue("Watcher")
-      );
-    }
+    func.await;
+    info!(
+      "{} Process terminated! Restarting on file change...",
+      colors::intense_blue("Watcher")
+    );
+    wait_for_file_change(&debounce).await?;
+    info!(
+      "{} File change detected! Restarting!",
+      colors::intense_blue("Watcher")
+    );
   }
 }
 
 async fn wait_for_file_change(
-  receiver: &Mutex<Receiver<Result<NotifyEvent, AnyError>>>,
-  debounce: &mut Debounce,
+  debounce: &Mutex<Debounce>,
 ) -> Result<(), AnyError> {
-  while let Some(result) = receiver.lock().unwrap().next().await {
-    let event = result?;
-    if debounce.fluct_is_over() {
-      match event.kind {
-        EventKind::Create(_) => break,
-        EventKind::Modify(_) => break,
-        EventKind::Remove(_) => break,
-        _ => continue,
-      }
+  while let Some(event) = debounce.lock().unwrap().next().await {
+    match event.kind {
+      EventKind::Create(_) => break,
+      EventKind::Modify(_) => break,
+      EventKind::Remove(_) => break,
+      _ => continue,
     }
   }
   Ok(())
@@ -134,18 +143,4 @@ fn new_watcher(
   }
 
   Ok((watcher, receiver))
-}
-
-#[test]
-fn debounce_test() {
-  let mut debounce = Debounce::new(50);
-  assert!(!debounce.fluct_is_over());
-  std::thread::sleep(std::time::Duration::from_millis(0));
-  assert!(!debounce.fluct_is_over());
-  std::thread::sleep(std::time::Duration::from_millis(10));
-  assert!(!debounce.fluct_is_over());
-  std::thread::sleep(std::time::Duration::from_millis(49));
-  assert!(!debounce.fluct_is_over());
-  std::thread::sleep(std::time::Duration::from_millis(50));
-  assert!(debounce.fluct_is_over());
 }
