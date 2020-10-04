3cb2e0d9b378a7c5c872d47df2d5c83e986b2df3
Kitson Kelly <me@kitsonkelly.com>
2020-10-05 10:01:59 +1100

    address feedback, dry up code

--- core/core.js
+++ core/core.js
@@ -213,6 +213,20 @@ SharedQueue Binary Layout
   let nextPromiseId = 1;
   const promiseTable = {};
 
+  function processResponse(res) {
+    if ("ok" in res) {
+      return res.ok;
+    } else {
+      const ErrorClass = getErrorClass(res.err.className);
+      if (!ErrorClass) {
+        throw new Error(
+          `Unregistered error class: "${res.err.className}"\n  ${res.err.message}\n  Classes of errors returned from ops should be registered via Deno.core.registerErrorClass().`,
+        );
+      }
+      throw new ErrorClass(res.err.message);
+    }
+  }
+
   async function jsonOpAsync(opName, args = {}, ...zeroCopy) {
     setAsyncHandler(opsCache[opName], jsonOpAsyncHandler);
 
@@ -227,35 +241,13 @@ SharedQueue Binary Layout
     promise.resolve = resolve;
     promise.reject = reject;
     promiseTable[args.promiseId] = promise;
-    const res = await promise;
-    if ("ok" in res) {
-      return res.ok;
-    } else {
-      const ErrorClass = getErrorClass(res.err.className);
-      if (!ErrorClass) {
-        throw new Error(
-          `Unregistered Deno core error class: ${res.err.className}\n  ${res.err.message}`,
-        );
-      }
-      throw new ErrorClass(res.err.message);
-    }
+    return processResponse(await promise);
   }
 
   function jsonOpSync(opName, args = {}, ...zeroCopy) {
     const argsBuf = encodeJson(args);
     const res = dispatch(opName, argsBuf, ...zeroCopy);
-    const r = decodeJson(res);
-    if ("ok" in r) {
-      return r.ok;
-    } else {
-      const ErrorClass = getErrorClass(res.err.className);
-      if (!ErrorClass) {
-        throw new Error(
-          `Unregistered Deno core error class: ${res.err.className}\n  ${res.err.message}`,
-        );
-      }
-      throw new ErrorClass(res.err.message);
-    }
+    return processResponse(decodeJson(res));
   }
 
   function jsonOpAsyncHandler(buf) {
