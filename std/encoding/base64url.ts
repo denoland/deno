301ef83e9d52c5ae208a5d191a569dcab05a2ac6
timonson <timo.rossa@tutanota.com>
2020-10-04 14:32:26 +0200

    Redo unnecessary changes

--- std/encoding/base64url.ts
+++ std/encoding/base64url.ts
@@ -5,28 +5,22 @@ import * as base64 from "./base64.ts";
 /*
  * Some variants allow or require omitting the padding '=' signs:
  * https://en.wikipedia.org/wiki/Base64#URL_applications
- * @param string
+ * @param base64url
  */
-export function addPaddingToBase64url(b64url: string): string {
-  if (b64url.length % 4 === 2) return b64url + "==";
-  if (b64url.length % 4 === 3) return b64url + "=";
-  if (b64url.length % 4 === 1) {
-    throw new TypeError("Illegal b64url string!");
+export function addPaddingToBase64url(base64url: string): string {
+  if (base64url.length % 4 === 2) return base64url + "==";
+  if (base64url.length % 4 === 3) return base64url + "=";
+  if (base64url.length % 4 === 1) {
+    throw new TypeError("Illegal base64url string!");
   }
-  return b64url;
+  return base64url;
 }
 
-/**
- * @param string
- */
-export function convertBase64urlToBase64(b64url: string): string {
+function convertBase64urlToBase64(b64url: string): string {
   return addPaddingToBase64url(b64url).replace(/\-/g, "+").replace(/_/g, "/");
 }
 
-/**
- * @param string
- */
-export function convertBase64ToBase64url(b64: string): string {
+function convertBase64ToBase64url(b64: string): string {
   return b64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
 }
 
@@ -39,7 +33,7 @@ export function encode(uint8: Uint8Array): string {
 }
 
 /**
- * Decodes a given base64url representation
+ * Converts given base64url encoded data back to original
  * @param b64url
  */
 export function decode(b64url: string): Uint8Array {
