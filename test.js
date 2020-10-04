a42dead828d4c0d84e156ec52381a6e1374d94b6
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-10-04 22:16:09 +0200

    fmt

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-
 const sleep = (n) => new Promise((r) => setTimeout(r, n));
 
 window.addEventListener("load", () => {
-    console.log("module loaded");
+  console.log("module loaded");
 });
 
 console.log("before sleep");
@@ -10,10 +9,7 @@ await sleep(5000);
 
 console.log("after sleep");
 
-
-
 export default 1;
-
 
 // ▶ deno run -A test1.js
 // before sleep
