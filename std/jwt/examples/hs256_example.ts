93690b26759a2ae935dd501e038e220efbb42a15
timonson <timo.rossa@tutanota.com>
2020-10-07 10:26:06 +0200

    Update example

---
+++ std/jwt/examples/hs256_example.ts
@@ -0,0 +1,27 @@
+import { serve, decode } from "./example_deps.ts";
+import { verify, create, setExpiration } from "../mod.ts";
+import type { Header, Payload } from "../mod.ts";
+
+const key = "your-secret";
+const payload: Payload = {
+  iss: "joe",
+  exp: setExpiration(60),
+};
+const header: Header = {
+  alg: "HS256",
+  typ: "JWT",
+};
+
+console.log("server is listening at 0.0.0.0:8000");
+for await (const req of serve("0.0.0.0:8000")) {
+  if (req.method === "GET") {
+    req.respond({ body: (await create({ header, payload, key })) + "\n" });
+  } else {
+    const jwt = decode(await Deno.readAll(req.body));
+    let j = await verify({ jwt, key, algorithm: "HS256" });
+    j = () => {};
+    req.respond({
+      body: JSON.stringify(await verify({ jwt, key, algorithm: "HS256" })),
+    });
+  }
+}
