f61678dd84f25b4a4f54a00cc55b31dd66985c64
Nayeem Rahman <nayeemrmn99@gmail.com>
2020-10-09 06:12:44 +0100

    fix(op_crates/fetch): Stringify and parse Request URLs (#7838)
    Fixes #7837

--- cli/tests/unit/request_test.ts
+++ cli/tests/unit/request_test.ts
@@ -1,8 +1,8 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-import { assert, assertEquals, unitTest } from "./test_util.ts";
+import { assert, assertEquals, assertThrows, unitTest } from "./test_util.ts";
 
 unitTest(function fromInit(): void {
-  const req = new Request("https://example.com", {
+  const req = new Request("http://foo/", {
     body: "ahoyhoy",
     method: "POST",
     headers: {
@@ -12,12 +12,12 @@ unitTest(function fromInit(): void {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   assertEquals("ahoyhoy", (req as any)._bodySource);
-  assertEquals(req.url, "https://example.com");
+  assertEquals(req.url, "http://foo/");
   assertEquals(req.headers.get("test-header"), "value");
 });
 
 unitTest(function fromRequest(): void {
-  const r = new Request("https://example.com");
+  const r = new Request("http://foo/");
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (r as any)._bodySource = "ahoyhoy";
   r.headers.set("test-header", "value");
@@ -30,10 +30,26 @@ unitTest(function fromRequest(): void {
   assertEquals(req.headers.get("test-header"), r.headers.get("test-header"));
 });
 
+unitTest(function requestNonString(): void {
+  const nonString = {
+    toString() {
+      return "http://foo/";
+    },
+  };
+  // deno-lint-ignore ban-ts-comment
+  // @ts-expect-error
+  assertEquals(new Request(nonString).url, "http://foo/");
+});
+
+unitTest(function requestRelativeUrl(): void {
+  // TODO(nayeemrmn): Base from `--location` when implemented and set.
+  assertThrows(() => new Request("relative-url"), TypeError, "Invalid URL.");
+});
+
 unitTest(async function cloneRequestBodyStream(): Promise<void> {
   // hack to get a stream
-  const stream = new Request("", { body: "a test body" }).body;
-  const r1 = new Request("https://example.com", {
+  const stream = new Request("http://foo/", { body: "a test body" }).body;
+  const r1 = new Request("http://foo/", {
     body: stream,
   });
 
