5bd9711f132ce65f90486e1055d3f4af7f57677f
Divy Srivastava <dj.srivastava23@gmail.com>
2020-10-10 17:15:35 +0000

    WIP std/mail

---
+++ std/mail/_wasm/lib.rs
@@ -0,0 +1,15 @@
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+
+use mailparse::dateparse;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn parse_date(data: &[u8]) {
+    let date = str::from_utf8(data).unwrap();
+    dateparse()
+}
+
+#[wasm_bindgen]
+pub fn digest_hash(hash: &mut DenoHash) -> Box<[u8]> {
+  hash.inner.finalize_reset()
+}
