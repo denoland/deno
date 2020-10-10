283c73b09e18da709922ebecc548e7d9f63ae30c
Divy Srivastava <dj.srivastava23@gmail.com>
2020-10-10 17:22:05 +0000

    x

--- std/mail/_wasm/lib.rs
+++ std/mail/_wasm/lib.rs
@@ -4,8 +4,8 @@ use mailparse::dateparse;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn parse_date(data: &[u8]) -> {
-    let date = str::from_utf8(data).unwrap();
-    dateparse(date)
+pub fn parse_date(data: &[u8]) -> i64 {
+    let date = std::str::from_utf8(data).unwrap();
+    return dateparse(date).unwrap()
 }
 
