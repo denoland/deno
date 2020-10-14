cf204b3f3583c1b2f39a1577be9b5e64030a24d1
Nayeem Rahman <nayeemrmn99@gmail.com>
2020-10-14 17:12:52 +0100

    feat(std/path): Add toFileUrl()

--- std/path/win32.ts
+++ std/path/win32.ts
@@ -917,11 +917,8 @@ export function fromFileUrl(url: string | URL): string {
     throw new TypeError("Must be a file URL.");
   }
   let path = decodeURIComponent(
-    url.pathname
-      .replace(/^\/*([A-Za-z]:)(\/|$)/, "$1/")
-      .replace(/\//g, "\\")
-      .replace(/%(?![0-9A-Fa-f]{2})/g, "%25"),
-  );
+    url.pathname.replace(/\//g, "\\").replace(/%(?![0-9A-Fa-f]{2})/g, "%25"),
+  ).replace(/^\\*([A-Za-z]:)(\\|$)/, "$1\\");
   if (url.hostname != "") {
     // Note: The `URL` implementation guarantees that the drive letter and
     // hostname are mutually exclusive. Otherwise it would not have been valid
@@ -929,4 +926,28 @@ export function fromFileUrl(url: string | URL): string {
     path = `\\\\${url.hostname}${path}`;
   }
   return path;
+}
+
+/** Converts a path string to a file URL.
+ *
+ *      toFileUrl("\\home\\foo"); // new URL("file:///home/foo")
+ *      toFileUrl("C:\\Users\\foo"); // new URL("file:///C:/Users/foo")
+ *      toFileUrl("\\\\localhost\\home\\foo"); // new URL("file://localhost/home/foo")
+ */
+export function toFileUrl(path: string): URL {
+  if (!isAbsolute(path)) {
+    throw new TypeError("Must be an absolute path.");
+  }
+  const [, hostname, pathname] = path.match(
+    /^(?:[/\\]{2}([^/\\]+)(?=[/\\][^/\\]))?(.*)/,
+  )!;
+  const url = new URL("file:///");
+  url.pathname = pathname.replace(/%/g, "%25");
+  if (hostname != null) {
+    url.hostname = hostname;
+    if (!url.hostname) {
+      throw new TypeError("Invalid hostname.");
+    }
+  }
+  return url;
 }
