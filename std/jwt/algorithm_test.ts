12765ed13597e49f67454ad8ddabe30800e1f757
timonson <timo.rossa@tutanota.com>
2020-10-16 00:40:42 +0200

    Add algorithm test for type string

--- std/jwt/algorithm_test.ts
+++ std/jwt/algorithm_test.ts
@@ -3,6 +3,7 @@ import { assertEquals } from "../testing/asserts.ts";
 import { verify as verifyAlgorithm } from "./algorithm.ts";
 
 Deno.test("[jwt] verify algorithm", function () {
+  assertEquals(verifyAlgorithm("HS512", "HS512"), true);
   assertEquals(verifyAlgorithm(["HS512"], "HS512"), true);
   assertEquals(verifyAlgorithm(["HS256", "HS512"], "HS512"), true);
   assertEquals(verifyAlgorithm(["HS512"], "HS256"), false);
