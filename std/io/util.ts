f0d10978c421e1641d471b062f6fa94e8c2cfdec
Leonard Ginters <leonard@linters.de>
2020-10-19 16:02:14 +0200

    Remove path import

--- std/io/util.ts
+++ std/io/util.ts
@@ -1,6 +1,4 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-import * as path from "../path/mod.ts";
-
 export function charCode(s: string): number {
   return s.charCodeAt(0);
 }
