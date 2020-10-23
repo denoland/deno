9248350d508e68b092462b44739b853946a354c2
Casper Beyer <caspervonb@pm.me>
2020-10-23 06:09:52 +0000

    Even more logging

--- std/wasi/snapshot_preview1.ts
+++ std/wasi/snapshot_preview1.ts
@@ -1211,7 +1211,7 @@ export default class Context {
 
         console.log("RESOLVED PATH: %s", resolvedPath);
         if (relative(entry.path, resolvedPath).startsWith("..")) {
-          console.log("RESOLVED NOTCAPABLE");
+          console.log("RESOLVED NOTCAPABLE: %s", relative(entry.path, resolvedPath));
           return ERRNO_NOTCAPABLE;
         }
 
@@ -1224,7 +1224,7 @@ export default class Context {
 
             console.log("RESOLVED REAL PATH: %s", path);
             if (relative(entry.path, path).startsWith("..")) {
-              console.log("RESOLVED REAL NOTCAPABLE");
+              console.log("RESOLVED REAL NOTCAPABLE: %s", relative(entry.path, path));
               return ERRNO_NOTCAPABLE;
             }
           } catch (_err) {
