f27556955c78f30635fe2567b4a8ef434a1ea641
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-10-30 14:15:56 +0100

    fix

--- cli/lint.rs
+++ cli/lint.rs
@@ -17,6 +17,7 @@ use deno_core::serde_json;
 use deno_lint::diagnostic::LintDiagnostic;
 use deno_lint::linter::Linter;
 use deno_lint::linter::LinterBuilder;
+use deno_lint::linter::FileType;
 use deno_lint::rules;
 use deno_lint::rules::LintRule;
 use serde::Serialize;
@@ -139,7 +140,7 @@ fn lint_file(
   let lint_rules = rules::get_recommended_rules();
   let mut linter = create_linter(syntax, lint_rules);
 
-  let file_diagnostics = linter.lint(file_name, source_code.clone())?;
+  let file_diagnostics = linter.lint(file_name, source_code.clone(), FileType::Module)?;
 
   Ok((file_diagnostics, source_code))
 }
@@ -165,7 +166,7 @@ fn lint_stdin(json: bool) -> Result<(), AnyError> {
   let mut has_error = false;
   let pseudo_file_name = "_stdin.ts";
   match linter
-    .lint(pseudo_file_name.to_string(), source.clone())
+    .lint(pseudo_file_name.to_string(), source.clone(), FileType::Module)
     .map_err(|e| e.into())
   {
     Ok(diagnostics) => {
