b3f0a6675d954aabef021790dc827b8d24bbdda4
Yoshiya Hinosawa <stibium121@gmail.com>
2020-10-31 20:36:23 +0900

    fix: fix when not in git repository

--- cli/build.rs
+++ cli/build.rs
@@ -111,10 +111,17 @@ fn git_commit_hash() -> String {
     .arg("HEAD")
     .output()
   {
-    std::str::from_utf8(&output.stdout[..7])
-      .unwrap()
-      .to_string()
+    if output.status.success() {
+      std::str::from_utf8(&output.stdout[..7])
+        .unwrap()
+        .to_string()
+    } else {
+      // When not in git repository
+      // (e.g. when the user install by `cargo install deno`)
+      "UNKNOWN".to_string()
+    }
   } else {
+    // When there is no git command for some reason
     "UNKNOWN".to_string()
   }
 }
