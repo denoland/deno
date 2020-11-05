5aff1aa9fb21ff1fb04538efca9b7db6d0f5b324
Kitson Kelly <me@kitsonkelly.com>
2020-11-05 14:42:02 +1100

    fix bench test

--- cli/tests/workers_round_robin_bench.ts
+++ cli/tests/workers_round_robin_bench.ts
@@ -6,14 +6,14 @@ const workerCount = 4;
 const cmdsPerWorker = 400;
 
 export interface ResolvableMethods<T> {
-  resolve: (value?: T | PromiseLike<T>) => void;
+  resolve: (value: T | PromiseLike<T>) => void;
   // deno-lint-ignore no-explicit-any
   reject: (reason?: any) => void;
 }
 
 export type Resolvable<T> = Promise<T> & ResolvableMethods<T>;
 
-export function createResolvable<T>(): Resolvable<T> {
+export function createResolvable<T = void>(): Resolvable<T> {
   let methods: ResolvableMethods<T>;
   const promise = new Promise<T>((resolve, reject): void => {
     methods = { resolve, reject };
