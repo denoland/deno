539aa40d1feca74972e9fca06d83868a6553667c
Yoshiya Hinosawa <stibium121@gmail.com>
2020-11-09 00:49:39 +0900

    refactor: use checkLicense function instead of license_checker cli

--- tools/check_license.js
+++ tools/check_license.js
@@ -1,26 +1,32 @@
-#!/usr/bin/env -S deno run --unstable --allow-read --allow-run
+#!/usr/bin/env -S deno run --unstable --allow-read
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
 
+import { checkLicense } from "https://deno.land/x/license_checker@v3.1.0/lib.ts";
 import { ROOT_PATH } from "./util.js";
 
 await Deno.chdir(ROOT_PATH);
 console.log("checking license headers");
 
-const p = Deno.run(
-  {
-    cmd: [
-      "deno",
-      "run",
-      "--unstable",
-      "--allow-read",
-      "https://deno.land/x/license_checker@v3.0.4/main.ts",
-      "-q",
+const success = await checkLicense([{
+  config: [
+    [
+      "**/*.{ts,js,toml,rs}",
+      "Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.",
     ],
-  },
-);
-const { success } = await p.status();
+  ],
+  ignore: [
+    "testdata",
+    "cli/dts",
+    "cli/tests",
+    "std/node/tests",
+    "std/hash/_wasm/wasm.js",
+    "cli/tsc/00_typescript.js",
+    "target",
+  ],
+}], { quiet: true });
+
 if (!success) {
-  throw new Error("license check failed");
+  console.log("license check failed");
+  Deno.exit(1);
 }
 console.log("ok");
-p.close();
