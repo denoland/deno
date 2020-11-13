c4841badcf94c1c173aa2acc84768cb4a1644883
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-11-13 12:11:06 -0500

    Add tests

---
+++ std/node/global_test.ts
@@ -0,0 +1,45 @@
+import "./global.ts";
+import {
+  assert,
+  assertStrictEquals,
+} from "../testing/asserts.ts";
+import {Buffer as BufferModule} from "./buffer.ts";
+import processModule from "./process.ts";
+
+// Definitions for this are quite delicate
+// This ensures modifications to the global namespace don't break on TypeScript
+
+// TODO
+// Deno lint marks globals defined by this module as undefined
+// probably gonna change in the future
+
+Deno.test("global is correctly defined", () => {
+  // deno-lint-ignore no-undef
+  assertStrictEquals(global, globalThis);
+  // deno-lint-ignore no-undef
+  assertStrictEquals(global.Buffer, BufferModule);
+  // deno-lint-ignore no-undef
+  assertStrictEquals(global.process, process);
+});
+
+Deno.test("Buffer is correctly defined", () => {
+  // deno-lint-ignore no-undef
+  assertStrictEquals(Buffer, BufferModule);
+  // deno-lint-ignore no-undef
+  assert(Buffer.from);
+  assertStrictEquals(globalThis.Buffer, BufferModule);
+  assert(globalThis.Buffer.from);
+  assertStrictEquals(window.Buffer, BufferModule);
+  assert(window.Buffer.from);
+});
+
+Deno.test("process is correctly defined", () => {
+  // deno-lint-ignore no-undef
+  assertStrictEquals(process, processModule);
+  // deno-lint-ignore no-undef
+  assert(process.arch);
+  assertStrictEquals(globalThis.process, processModule);
+  assert(globalThis.process.arch);
+  assertStrictEquals(window.process, processModule);
+  assert(window.process.arch);
+});
