eb610252652a3eeb543328efd8cc8902e0a92f45
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-11-13 17:36:09 -0500

    Remove redundant test

--- std/node/buffer_test.ts
+++ std/node/buffer_test.ts
@@ -1,14 +1,6 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-import { assert, assertEquals, assertThrows } from "../testing/asserts.ts";
+import { assertEquals, assertThrows } from "../testing/asserts.ts";
 import { Buffer } from "./buffer.ts";
-
-Deno.test({
-  name: "Buffer global scope",
-  fn() {
-    assert(window.Buffer === Buffer);
-    assert(globalThis.Buffer === Buffer);
-  },
-});
 
 Deno.test({
   name: "alloc fails on negative numbers",
