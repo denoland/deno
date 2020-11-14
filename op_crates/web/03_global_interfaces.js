fb2a13e12c8d333d071174558192398bd61c68da
Benjamin Gruenbaum <benjamingr@gmail.com>
2020-11-14 10:43:46 +0200

    fix(op_crates/web) various constructor lengths

--- op_crates/web/03_global_interfaces.js
+++ op_crates/web/03_global_interfaces.js
@@ -17,7 +17,7 @@
   }
 
   class WorkerGlobalScope extends EventTarget {
-    constructor(key) {
+    constructor(key = null) {
       if (key != illegalConstructorKey) {
         throw new TypeError("Illegal constructor.");
       }
@@ -30,7 +30,7 @@
   }
 
   class DedicatedWorkerGlobalScope extends WorkerGlobalScope {
-    constructor(key) {
+    constructor(key = null) {
       if (key != illegalConstructorKey) {
         throw new TypeError("Illegal constructor.");
       }
