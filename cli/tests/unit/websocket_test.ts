ec5ffd0c5e51be14c8ec9e99bee0e6018fafd213
Ryan Dahl <ry@tinyclouds.org>
2020-11-20 07:22:14 -0500

    lint

--- cli/tests/unit/websocket_test.ts
+++ cli/tests/unit/websocket_test.ts
@@ -1,8 +1,8 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
-import { assertThrowsAsync, unitTest } from "./test_util.ts";
+import { assertThrows, unitTest } from "./test_util.ts";
 
 unitTest(function websocketPermissionDenied(): void {
-  await assertThrowsAsync(async () => {
+  assertThrows(() => {
     const socket = new WebSocket("wss://gateway.discord.gg");
     socket.onmessage = (ev) => console.log(ev.data);
   }, Deno.errors.PermissionDenied);
