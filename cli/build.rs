a22dbe280e78798d3ba43541b1effe9dfee97275
crowlkats <13135287+crowlKats@users.noreply.github.com>
2020-11-24 14:03:27 +0100

    change to unwrap_or_default

--- cli/build.rs
+++ cli/build.rs
@@ -259,7 +259,7 @@ fn main() {
   println!("cargo:rustc-env=PROFILE={}", env::var("PROFILE").unwrap());
   println!(
     "cargo:rustc-env=DENO_CANARY={}",
-    env::var("DENO_CANARY").unwrap_or_else("")
+    env::var("DENO_CANARY").unwrap_or_default()
   );
 
   let c = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
