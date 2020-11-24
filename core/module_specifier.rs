4ad130d3c36eb49ea53f09daf87fb6f55de21000
William Perron <hey@wperron.io>
2020-11-24 15:19:04 -0500

    fix(cli): module info map item order
    changed the HashSet used to build the output graph for a BTreeSet to keep the items sorted
    
    Fixes #8458

--- core/module_specifier.rs
+++ core/module_specifier.rs
@@ -16,6 +16,7 @@ pub enum ModuleResolutionError {
   InvalidPath(PathBuf),
   ImportPrefixMissing(String, Option<String>),
 }
+use std::cmp::Ordering;
 use ModuleResolutionError::*;
 
 impl Error for ModuleResolutionError {
@@ -204,6 +205,18 @@ impl From<Url> for ModuleSpecifier {
 impl PartialEq<String> for ModuleSpecifier {
   fn eq(&self, other: &String) -> bool {
     &self.to_string() == other
+  }
+}
+
+impl Ord for ModuleSpecifier {
+  fn cmp(&self, other: &Self) -> Ordering {
+    self.0.cmp(&other.0)
+  }
+}
+
+impl PartialOrd for ModuleSpecifier {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
   }
 }
 
