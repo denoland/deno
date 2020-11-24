09778322da173237052ded5e2d07e70ee9d51f88
crowlkats <13135287+crowlKats@users.noreply.github.com>
2020-11-24 13:16:55 +0100

    Add env to build.rs

--- cli/build.rs
+++ cli/build.rs
@@ -257,6 +257,7 @@ fn main() {
 
   println!("cargo:rustc-env=TARGET={}", env::var("TARGET").unwrap());
   println!("cargo:rustc-env=PROFILE={}", env::var("PROFILE").unwrap());
+  println!("cargo:rustc-env=DENO_CANARY={}", env::var("DENO_CANARY").unwrap_or_else(""));
 
   let c = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
   let o = PathBuf::from(env::var_os("OUT_DIR").unwrap());
