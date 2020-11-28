3e8b1cb82511ac69066c22a416465853f3ec1a03
yasser <spookyframework@gmail.com>
2020-11-28 22:11:03 +0100

    refactor(std/http): Display cookie name in validateCookieValue

--- std/http/cookie.ts
+++ std/http/cookie.ts
@@ -40,7 +40,7 @@ function toString(cookie: Cookie): string {
   }
   const out: string[] = [];
   validateCookieName(cookie.name);
-  validateCookieValue(cookie.value);
+  validateCookieValue(cookie.name, cookie.value);
   out.push(`${cookie.name}=${cookie.value}`);
 
   // Fallback for invalid Set-Cookie
@@ -120,8 +120,8 @@ function validatePath(path: string | null): void {
  * @see https://tools.ietf.org/html/rfc6265#section-4.1
  * @param value Cookie value.
  */
-function validateCookieValue(value: string | null): void {
-  if (value == null) return;
+function validateCookieValue(name:string, value: string | null): void {
+  if (value == null || name == null) return;
   for (let i = 0; i < value.length; i++) {
     const c = value.charAt(i);
     if (
@@ -129,11 +129,11 @@ function validateCookieValue(value: string | null): void {
       c == String.fromCharCode(0x2c) || c == String.fromCharCode(0x3b) ||
       c == String.fromCharCode(0x5c) || c == String.fromCharCode(0x7f)
     ) {
-      throw new Error("RFC2616 cookie value cannot have '" + c + "'");
+      throw new Error("RFC2616 cookie '"+ name + "' cannot have '" + c + "' as value");
     }
     if (c > String.fromCharCode(0x80)) {
       throw new Error(
-        "RFC2616 cookie value can only have US-ASCII chars" +
+        "RFC2616 cookie '"+ name + "' can only have US-ASCII chars as value" +
           c.charCodeAt(0).toString(16),
       );
     }
