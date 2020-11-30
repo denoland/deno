fc50f47a2ff01a9f29cc60458ea14b20c4ee1dc2
mayank agarwal <mayankagarwal44442@gmail.com>
2020-11-30 11:37:16 +0530

    Revert "fix(std): Removed unnecessary calls to Deno.cwd()"
    This reverts commit 8b400b79be364bbb96e02fe2bc79a21c6c39d7ab.

--- std/wasi/snapshot_preview1_test.ts
+++ std/wasi/snapshot_preview1_test.ts
@@ -64,7 +64,7 @@ for (const pathname of tests) {
     ignore: ignore.includes(pathname),
     fn: async function () {
       const prelude = await Deno.readTextFile(
-        path.resolve(rootdir, pathname.replace(/\.wasm$/, ".json")),
+        path.resolve(Deno.cwd(), rootdir, pathname.replace(/\.wasm$/, ".json")),
       );
       const options = JSON.parse(prelude);
 
@@ -91,11 +91,12 @@ for (const pathname of tests) {
             "--unstable",
             "--allow-all",
             path.resolve(
+              Deno.cwd(),
               rootdir,
               "snapshot_preview1_test_runner.ts",
             ),
             prelude,
-            path.resolve(rootdir, pathname),
+            path.resolve(Deno.cwd(), rootdir, pathname),
           ],
           stdin: "piped",
           stdout: "piped",
