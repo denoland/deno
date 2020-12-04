1aef3fba4f52a5dd814f2a757d585c7854cd6791
Louis-Philippe Perron <32278+lp@users.noreply.github.com>
2020-12-04 17:41:00 -0500

    [std/node] adds support for nodejs encoding options and 6 characters suffix

--- std/node/_fs/_fs_mkdtemp.ts
+++ std/node/_fs/_fs_mkdtemp.ts
@@ -1,4 +1,5 @@
 // Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+import { existsSync } from "./_fs_exists.ts";
 
 export type mkdtempCallback = (
   err: Error | undefined,
@@ -12,7 +13,6 @@ export function mkdtemp(
   options: { encoding: string } | string,
   callback: mkdtempCallback,
 ): void;
-// TODO - 'encoding' handling needs implementation in Deno.makeTempDir
 export function mkdtemp(
   prefix: string,
   optionsOrCallback: { encoding: string } | string | mkdtempCallback,
@@ -20,19 +20,63 @@ export function mkdtemp(
 ): void {
   const callback: mkdtempCallback | undefined =
     optionsOrCallback instanceof Function ? optionsOrCallback : maybeCallback;
-
   if (!callback) throw new Error("No callback function supplied");
+  
+  const encoding: string | undefined = parseEncoding(optionsOrCallback);
+  const path = tempDirPath(prefix);
 
-  Deno.makeTempDir({ dir: prefix })
-    .then((directory) => callback(undefined, directory))
+  Deno.mkdir(path, { recursive: false, mode: 0o700 })
+    .then(() => callback(undefined, decode(path, encoding)))
     .catch((error) => callback(error));
 }
 
 // https://nodejs.org/dist/latest-v15.x/docs/api/fs.html#fs_fs_mkdtempsync_prefix_options
-// TODO - 'encoding' handling needs implementation in Deno.makeTempDirSync
 export function mkdtempSync(
   prefix: string,
   options?: { encoding: string } | string,
 ): string {
-  return Deno.makeTempDirSync({ dir: prefix });
+  const encoding: string | undefined = parseEncoding(options);
+  const path = tempDirPath(prefix);
+
+  Deno.mkdirSync(path, { recursive: false, mode: 0o700 });
+  return decode(path, encoding);
+}
+
+function parseEncoding(
+  optionsOrCallback?: { encoding: string } | string | mkdtempCallback,
+): string | undefined {
+  let encoding: string | undefined;
+  if (optionsOrCallback instanceof Function) encoding = undefined;
+  else if (optionsOrCallback instanceof Object) encoding = optionsOrCallback?.encoding;
+  else encoding = optionsOrCallback
+
+  if (encoding) {
+    try {
+      new TextDecoder(encoding);
+    } catch (error) {
+      throw new Error(
+        `TypeError [ERR_INVALID_OPT_VALUE_ENCODING]: The value "${encoding}" is invalid for option "encoding"`,
+      );
+    }
+  }
+
+  return encoding;
+}
+
+function decode(str: string, encoding?: string): string {
+  if (!encoding) return str;
+  else {
+    const decoder = new TextDecoder(encoding);
+    const encoder = new TextEncoder();
+    return decoder.decode(encoder.encode(str));
+  }
+}
+
+function tempDirPath(prefix: string): string {
+  let path: string;
+  do {
+    path = prefix + Math.random().toString(36).substring(2, 8);
+  } while (existsSync(path))
+
+  return path;
 }
--- std/node/_fs/_fs_mkdtemp_test.ts
+++ std/node/_fs/_fs_mkdtemp_test.ts
@@ -5,34 +5,16 @@ import { existsSync } from "./_fs_exists.ts";
 import { env } from "../process.ts";
 import { isWindows } from "../../_util/os.ts";
 
-const prefix = isWindows ? env.TEMP : env.TMPDIR;
+const prefix = isWindows ? env.TEMP+'\\' : env.TMPDIR+'/';
 const doesNotExists = "/does/not/exists/";
-const options = { encoding: "utf8" };
+const options = { encoding: "ascii" };
+const badOptions = { encoding: "bogus" };
 
 Deno.test({
   name: "[node/fs] mkdtemp",
   fn: async () => {
     const result = await new Promise((resolve) => {
       mkdtemp(prefix, (err, directory) => {
-        if (err) {
-          resolve(false);
-        } else if (directory) {
-          resolve(existsSync(directory));
-          Deno.removeSync(directory);
-        } else {
-          resolve(false);
-        }
-      });
-    });
-    assert(result);
-  },
-});
-
-Deno.test({
-  name: "[node/fs] mkdtemp (with options)",
-  fn: async () => {
-    const result = await new Promise((resolve) => {
-      mkdtemp(prefix, options, (err, directory) => {
         if (err) {
           resolve(false);
         } else if (directory) {
@@ -64,19 +46,45 @@ Deno.test({
 });
 
 Deno.test({
-  name: "[node/fs] mkdtempSync",
-  fn: () => {
-    const directory = mkdtempSync(prefix);
-    const dirExists = existsSync(directory);
-    Deno.removeSync(directory);
-    assert(dirExists);
+  name: "[node/fs] mkdtemp (with options)",
+  fn: async () => {
+    const result = await new Promise((resolve) => {
+      mkdtemp(prefix, options, (err, directory) => {
+        if (err) {
+          resolve(false);
+        } else if (directory) {
+          resolve(existsSync(directory));
+          Deno.removeSync(directory);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+    assert(result);
   },
 });
 
 Deno.test({
-  name: "[node/fs] mkdtempSync (with options)",
+  name: "[node/fs] mkdtemp (with bad options)",
+  fn: async () => {
+    const result = await new Promise((resolve) => {
+      try {
+        mkdtemp(prefix, badOptions, (err, directory) => {
+          // should have thrown already...
+          resolve(false);
+        });
+      } catch (error) {
+        resolve(true);
+      }
+    });
+    assert(result);
+  },
+});
+
+Deno.test({
+  name: "[node/fs] mkdtempSync",
   fn: () => {
-    const directory = mkdtempSync(prefix, options);
+    const directory = mkdtempSync(prefix);
     const dirExists = existsSync(directory);
     Deno.removeSync(directory);
     assert(dirExists);
@@ -98,3 +106,29 @@ Deno.test({
     }
   },
 });
+
+Deno.test({
+  name: "[node/fs] mkdtempSync (with options)",
+  fn: () => {
+    const directory = mkdtempSync(prefix, options);
+    const dirExists = existsSync(directory);
+    Deno.removeSync(directory);
+    assert(dirExists);
+  },
+});
+
+Deno.test({
+  name: "[node/fs] mkdtempSync (with bad options)",
+  fn: () => {
+    try {
+      const directory = mkdtempSync(prefix, badOptions);
+      // should have thrown already...
+
+      Deno.removeSync(directory);
+      assert(false);
+    } catch (error) {
+      assert(true);
+    }
+  },
+});
+
