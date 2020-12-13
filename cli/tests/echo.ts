435dbb4557e5b7e46defe4847bd976e90d3cfcfe
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-12-13 01:58:51 +0100

    fix

--- cli/tests/echo.ts
+++ cli/tests/echo.ts
@@ -1,6 +1,6 @@
 function echo(args: string[]): void {
   const msg = args.join(", ");
-  Deno.stdout.write(new TextEncoder().encode(msg));
+  Deno.stdout.writeSync(new TextEncoder().encode(msg));
 }
 
 echo(Deno.args);
