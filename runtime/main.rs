da1d8f8b3f54145544529759d78788a700823dc3
iamyaoxi <yosi.pramajaya@gmail.com>
2020-12-31 15:33:19 +0700

    Fix standalone args

--- runtime/main.rs
+++ runtime/main.rs
@@ -2,7 +2,8 @@ fn main() {
   #[cfg(windows)]
   colors::enable_ansi(); // For Windows 10
 
-  let args: Vec<String> = std::env::args().collect();
+  let args_raw: Vec<String> = std::env::args().collect();
+  let args: Vec<String> = args_raw[1..].to_vec();
   if let Err(err) = deno_runtime::standalone::try_run_standalone_binary(args) {
     eprintln!(
       "{}: {}",
