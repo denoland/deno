c444a4102598c9d9d46e129502952aa4e89db829
iamyaoxi <yosi.pramajaya@gmail.com>
2021-01-01 00:42:49 +0700

    rustfmt cli/main.rs

--- cli/main.rs
+++ cli/main.rs
@@ -1220,9 +1220,7 @@ pub fn main() {
   colors::enable_ansi(); // For Windows 10
 
   let args: Vec<String> = env::args().collect();
-  if let Err(err) =
-    crate::rt::try_run_standalone_binary(args.clone())
-  {
+  if let Err(err) = crate::rt::try_run_standalone_binary(args.clone()) {
     eprintln!("{}: {}", colors::red_bold("error"), err.to_string());
     std::process::exit(1);
   }
