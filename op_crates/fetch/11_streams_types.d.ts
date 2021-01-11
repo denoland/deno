2ffd555207490934799475691b9c53325ba3e724
Kitson Kelly <me@kitsonkelly.com>
2021-01-11 11:09:56 +1100

    reafactor(op_crate/fetch): align streams to spec

---
+++ op_crates/fetch/11_streams_types.d.ts
@@ -0,0 +1,45 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
+// ** Internal Interfaces **
+
+interface PendingAbortRequest {
+  deferred: Deferred<void>;
+  // deno-lint-ignore no-explicit-any
+  reason: any;
+  wasAlreadyErroring: boolean;
+}
+
+// deno-lint-ignore no-explicit-any
+interface ReadRequest<R = any> {
+  chunkSteps: (chunk: R) => void;
+  closeSteps: () => void;
+  // deno-lint-ignore no-explicit-any
+  errorSteps: (error: any) => void;
+}
+
+interface ReadableByteStreamQueueEntry {
+  buffer: ArrayBufferLike;
+  byteOffset: number;
+  byteLength: number;
+}
+
+interface ReadableStreamGetReaderOptions {
+  mode?: "byob";
+}
+
+interface ReadableStreamIteratorOptions {
+  preventCancel?: boolean;
+}
+
+interface ValueWithSize<T> {
+  value: T;
+  size: number;
+}
+
+interface VoidFunction {
+  (): void;
+}
+
+// ** Ambient Definitions and Interfaces not provided by fetch **
+
+declare function queueMicrotask(callback: VoidFunction): void;
--- op_crates/fetch/lib.deno_fetch.d.ts
+++ op_crates/fetch/lib.deno_fetch.d.ts
@@ -37,11 +37,21 @@ interface ReadableStreamDefaultReader<R = any> {
   releaseLock(): void;
 }
 
+declare var ReadableStreamDefaultReader: {
+  prototype: ReadableStreamDefaultReader;
+  new <R>(stream: ReadableStream<R>): ReadableStreamDefaultReader<R>;
+};
+
 interface ReadableStreamReader<R = any> {
   cancel(): Promise<void>;
   read(): Promise<ReadableStreamReadResult<R>>;
   releaseLock(): void;
 }
+
+declare var ReadableStreamReader: {
+  prototype: ReadableStreamReader;
+  new (): ReadableStreamReader;
+};
 
 interface ReadableByteStreamControllerCallback {
   (controller: ReadableByteStreamController): void | PromiseLike<void>;
@@ -53,6 +63,14 @@ interface UnderlyingByteSource {
   pull?: ReadableByteStreamControllerCallback;
   start?: ReadableByteStreamControllerCallback;
   type: "bytes";
+}
+
+interface UnderlyingSink<W = any> {
+  abort?: WritableStreamErrorCallback;
+  close?: WritableStreamDefaultControllerCloseCallback;
+  start?: WritableStreamDefaultControllerStartCallback;
+  type?: undefined;
+  write?: WritableStreamDefaultControllerWriteCallback<W>;
 }
 
 interface UnderlyingSource<R = any> {
@@ -77,13 +95,23 @@ interface ReadableStreamDefaultController<R = any> {
   error(error?: any): void;
 }
 
+declare var ReadableStreamDefaultController: {
+  prototype: ReadableStreamDefaultController;
+  new (): ReadableStreamDefaultController;
+};
+
 interface ReadableByteStreamController {
-  readonly byobRequest: undefined;
+  readonly byobRequest: ReadableStreamBYOBRequest | undefined;
   readonly desiredSize: number | null;
   close(): void;
   enqueue(chunk: ArrayBufferView): void;
   error(error?: any): void;
 }
+
+declare var ReadableByteStreamController: {
+  prototype: ReadableByteStreamController;
+  new (): ReadableByteStreamController;
+};
 
 interface PipeOptions {
   preventAbort?: boolean;
@@ -122,14 +150,10 @@ declare class ByteLengthQueuingStrategy
 interface ReadableStream<R = any> {
   readonly locked: boolean;
   cancel(reason?: any): Promise<void>;
-  getIterator(options?: { preventCancel?: boolean }): AsyncIterableIterator<R>;
-  // getReader(options: { mode: "byob" }): ReadableStreamBYOBReader;
+  getReader(options: { mode: "byob" }): ReadableStreamBYOBReader;
   getReader(): ReadableStreamDefaultReader<R>;
   pipeThrough<T>(
-    {
-      writable,
-      readable,
-    }: {
+    { writable, readable }: {
       writable: WritableStream<R>;
       readable: ReadableStream<T>;
     },
@@ -154,6 +178,31 @@ declare var ReadableStream: {
   ): ReadableStream<R>;
 };
 
+interface ReadableStreamBYOBReader {
+  readonly closed: Promise<void>;
+  cancel(reason?: any): Promise<void>;
+  read<T extends ArrayBufferView>(
+    view: T,
+  ): Promise<ReadableStreamReadResult<T>>;
+  releaseLock(): void;
+}
+
+declare var ReadableStreamBYOBReader: {
+  prototype: ReadableStreamBYOBReader;
+  new (): ReadableStreamBYOBReader;
+};
+
+interface ReadableStreamBYOBRequest {
+  readonly view: ArrayBufferView;
+  respond(bytesWritten: number): void;
+  respondWithNewView(view: ArrayBufferView): void;
+}
+
+declare var ReadableStreamBYOBRequest: {
+  prototype: ReadableStreamBYOBRequest;
+  new (): ReadableStreamBYOBRequest;
+};
+
 interface WritableStreamDefaultControllerCloseCallback {
   (): void | PromiseLike<void>;
 }
@@ -174,27 +223,22 @@ interface WritableStreamErrorCallback {
   (reason: any): void | PromiseLike<void>;
 }
 
-interface UnderlyingSink<W = any> {
-  abort?: WritableStreamErrorCallback;
-  close?: WritableStreamDefaultControllerCloseCallback;
-  start?: WritableStreamDefaultControllerStartCallback;
-  type?: undefined;
-  write?: WritableStreamDefaultControllerWriteCallback<W>;
-}
-
 /** This Streams API interface provides a standard abstraction for writing
  * streaming data to a destination, known as a sink. This object comes with
  * built-in backpressure and queuing. */
-declare class WritableStream<W = any> {
-  constructor(
-    underlyingSink?: UnderlyingSink<W>,
-    strategy?: QueuingStrategy<W>,
-  );
+interface WritableStream<W = any> {
   readonly locked: boolean;
   abort(reason?: any): Promise<void>;
-  close(): Promise<void>;
   getWriter(): WritableStreamDefaultWriter<W>;
 }
+
+declare var WritableStream: {
+  prototype: WritableStream;
+  new <W = any>(
+    underlyingSink?: UnderlyingSink<W>,
+    strategy?: QueuingStrategy<W>,
+  ): WritableStream<W>;
+};
 
 /** This Streams API interface represents a controller allowing control of a
  * WritableStream's state. When constructing a WritableStream, the underlying
@@ -203,6 +247,11 @@ declare class WritableStream<W = any> {
 interface WritableStreamDefaultController {
   error(error?: any): void;
 }
+
+declare var WritableStreamDefaultController: {
+  prototype: WritableStreamDefaultController;
+  new (): WritableStreamDefaultController;
+};
 
 /** This Streams API interface is the object returned by
  * WritableStream.getWriter() and once created locks the < writer to the
@@ -218,15 +267,24 @@ interface WritableStreamDefaultWriter<W = any> {
   write(chunk: W): Promise<void>;
 }
 
-declare class TransformStream<I = any, O = any> {
-  constructor(
-    transformer?: Transformer<I, O>,
-    writableStrategy?: QueuingStrategy<I>,
-    readableStrategy?: QueuingStrategy<O>,
-  );
+declare var WritableStreamDefaultWriter: {
+  prototype: WritableStreamDefaultWriter;
+  new (): WritableStreamDefaultWriter;
+};
+
+interface TransformStream<I = any, O = any> {
   readonly readable: ReadableStream<O>;
   readonly writable: WritableStream<I>;
 }
+
+declare var TransformStream: {
+  prototype: TransformStream;
+  new <I = any, O = any>(
+    transformer?: Transformer<I, O>,
+    writableStrategy?: QueuingStrategy<I>,
+    readableStrategy?: QueuingStrategy<O>,
+  ): TransformStream<I, O>;
+};
 
 interface TransformStreamDefaultController<O = any> {
   readonly desiredSize: number | null;
