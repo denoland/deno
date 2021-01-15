03bf2fc01f0d877c0b39b5de64670af200724b22
Liam Murphy <43807659+Liamolucko@users.noreply.github.com>
2021-01-15 13:18:49 +1100

    core(runtime): fix copyright in date (#9116)

--- runtime/ops/crypto.rs
+++ runtime/ops/crypto.rs
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 use deno_crypto::op_crypto_get_random_values;
 use deno_crypto::rand::rngs::StdRng;
 use deno_crypto::rand::SeedableRng;
