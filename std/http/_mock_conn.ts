1e0013cae7531f11dfde3ce22b198d05e4f40eed
James Bradlee <james@ihacks.dev>
2021-01-16 22:38:23 +0100

    Added  to dummy connections

--- std/http/_mock_conn.ts
+++ std/http/_mock_conn.ts
@@ -14,6 +14,7 @@ export function mockConn(base: Partial<Deno.Conn> = {}): Deno.Conn {
       port: 0,
     },
     rid: -1,
+    sni: null,
     closeWrite: (): void => {},
     read: (): Promise<number | null> => {
       return Promise.resolve(0);
