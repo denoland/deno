c490f8c1ad18489ae6b56febab0695461cc1def9
Carter Snook <cartersnook04@gmail.com>
2021-01-29 14:05:25 -0600

    cleanup(std/uuid): modernize uuid v4

--- std/uuid/v4.ts
+++ std/uuid/v4.ts
@@ -1,21 +1,19 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 import { bytesToUuid } from "./_common.ts";
 
-const UUID_RE = new RegExp(
-  "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
-  "i",
-);
+const UUID_RE =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 /**
- * Validates the UUID v4
- * @param id UUID value
+ * Validates the UUID v4.
+ * @param id UUID value.
  */
-export function validate(id: string): boolean {
+export function validate(id: string) {
   return UUID_RE.test(id);
 }
 
 /** Generates a RFC4122 v4 UUID (pseudo-randomly-based) */
-export function generate(): string {
+export function generate() {
   const rnds = crypto.getRandomValues(new Uint8Array(16));
 
   rnds[6] = (rnds[6] & 0x0f) | 0x40; // Version 4
