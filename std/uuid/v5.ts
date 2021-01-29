7597892a15ea0c49f273d694ed6d205f2b287202
Carter Snook <cartersnook04@gmail.com>
2021-01-29 14:10:00 -0600

    cleanup(std/uuid): modernize uuid v5

--- std/uuid/v5.ts
+++ std/uuid/v5.ts
@@ -12,23 +12,24 @@ const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 /**
- * Validates the UUID v5
- * @param id UUID value
+ * Validates the UUID v5.
+ * @param id UUID value.
  */
-export function validate(id: string): boolean {
+export function validate(id: string) {
   return UUID_RE.test(id);
 }
 
-interface V5Options {
+/** The options used for generating a v5 uuid. */
+export interface V5Options {
   value: string | number[];
   namespace: string | number[];
 }
 
 /**
- * Generates a RFC4122 v5 UUID (SHA-1 namespace-based)
- * @param options Can use a namespace and value to create SHA-1 hash
- * @param buf Can allow the UUID to be written in byte-form starting at the offset
- * @param offset Index to start writing on the UUID bytes in buffer
+ * Generates a RFC4122 v5 UUID (SHA-1 namespace-based).
+ * @param options Can use a namespace and value to create SHA-1 hash.
+ * @param buf Can allow the UUID to be written in byte-form starting at the offset.
+ * @param offset Index to start writing on the UUID bytes in buffer.
  */
 export function generate(
   options: V5Options,
@@ -38,12 +39,12 @@ export function generate(
   const i = (buf && offset) || 0;
 
   let { value, namespace } = options;
-  if (typeof value == "string") {
-    value = stringToBytes(value as string);
+  if (typeof value === "string") {
+    value = stringToBytes(value);
   }
 
-  if (typeof namespace == "string") {
-    namespace = uuidToBytes(namespace as string);
+  if (typeof namespace === "string") {
+    namespace = uuidToBytes(namespace);
   }
 
   assert(
@@ -51,17 +52,17 @@ export function generate(
     "namespace must be uuid string or an Array of 16 byte values",
   );
 
-  const content = (namespace as number[]).concat(value as number[]);
+  const content = namespace.concat(value);
   const bytes = new Sha1().update(createBuffer(content)).digest();
 
   bytes[6] = (bytes[6] & 0x0f) | 0x50;
   bytes[8] = (bytes[8] & 0x3f) | 0x80;
 
-  if (buf) {
+  if (buf !== undefined) {
     for (let idx = 0; idx < 16; ++idx) {
       buf[i + idx] = bytes[idx];
     }
   }
 
-  return buf || bytesToUuid(bytes);
+  return buf ?? bytesToUuid(bytes);
 }
