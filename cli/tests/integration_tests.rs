c83e261b42019f5a1ef1b67f9c764a3ed61b68a2
Casper Beyer <caspervonb@pm.me>
2021-02-05 19:09:52 +0800

    fix(repl): prevent symbol completion panic (#9400)

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -1751,6 +1751,31 @@ mod integration {
       }
     }
 
+    #[cfg(unix)]
+    #[test]
+    fn pty_complete_symbol() {
+      use std::io::Read;
+      use util::pty::fork::*;
+      let deno_exe = util::deno_exe_path();
+      let fork = Fork::from_ptmx().unwrap();
+      if let Ok(mut master) = fork.is_parent() {
+        master.write_all(b"Deno.internal\t\n").unwrap();
+        master.write_all(b"close();\n").unwrap();
+
+        let mut output = String::new();
+        master.read_to_string(&mut output).unwrap();
+
+        assert!(output.contains("Symbol(Deno.internal)"));
+
+        fork.wait().unwrap();
+      } else {
+        std::env::set_var("NO_COLOR", "1");
+        let err = exec::Command::new(deno_exe).arg("repl").exec();
+        println!("err {}", err);
+        unreachable!()
+      }
+    }
+
     #[test]
     fn console_log() {
       let (out, err) = util::run_and_collect_output(
