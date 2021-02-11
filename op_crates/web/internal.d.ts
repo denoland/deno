1051584d123c95f8a0b2fc6d272a5dc7e49b8258
Luca Casonato <lucacasonato@yahoo.com>
2021-02-11 16:53:54 +0100

    lint

--- op_crates/web/internal.d.ts
+++ op_crates/web/internal.d.ts
@@ -1,4 +1,5 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+// deno-lint-ignore-file no-explicit-any ban-types
 
 /// <reference no-default-lib="true" />
 /// <reference lib="esnext" />
