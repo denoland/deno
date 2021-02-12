cb324c84a4399df58de2d4fe81ad41939435bb63
Ryan Dahl <ry@tinyclouds.org>
2021-02-12 05:40:31 -0500

    Revert "disable borken tests"
    This reverts commit b51c0d93f6073aa5aface4eecc06411953d9c794.

--- cli/lsp/language_server.rs
+++ cli/lsp/language_server.rs
@@ -2398,7 +2398,6 @@ mod tests {
   }
 
   #[tokio::test]
-  #[ignore] // Disabled during rusty_v8 0.18.0 upgrade
   async fn test_code_lens_request() {
     let mut harness = LspTestHarness::new(vec![
       ("initialize_request.json", LspResponse::RequestAny),
@@ -2501,7 +2500,6 @@ mod tests {
   }
 
   #[tokio::test]
-  #[ignore] // Disabled during rusty_v8 0.18.0 upgrade
   async fn test_code_lens_impl_request() {
     let mut harness = LspTestHarness::new(vec![
       ("initialize_request.json", LspResponse::RequestAny),
@@ -2627,7 +2625,6 @@ mod tests {
   }
 
   #[tokio::test]
-  #[ignore] // Disabled during rusty_v8 0.18.0 upgrade
   async fn test_code_lens_non_doc_nav_tree() {
     let mut harness = LspTestHarness::new(vec![
       ("initialize_request.json", LspResponse::RequestAny),
