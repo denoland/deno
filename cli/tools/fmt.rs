cd7d7055ec16705a0a111bb4542c62ebf0169e6b
Yusuke Tanaka <yusuktan@maguro.dev>
2021-02-18 23:20:48 +0900

    fix(lint, fmt): check if target files are found

--- cli/tools/fmt.rs
+++ cli/tools/fmt.rs
@@ -37,7 +37,13 @@ pub async fn format(
 ) -> Result<(), AnyError> {
   let target_file_resolver = || {
     // collect the files that are to be formatted
-    collect_files(&args, &ignore, is_supported_ext_md)
+    collect_files(&args, &ignore, is_supported_ext_md).and_then(|files| {
+      if files.is_empty() {
+        Err(generic_error("No target files found."))
+      } else {
+        Ok(files)
+      }
+    })
   };
   let operation = |paths: Vec<PathBuf>| {
     let config = get_typescript_config();
