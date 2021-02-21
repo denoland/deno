dd764d31ed1403fad94fa32024ada335b87fddd4
Casper Beyer <caspervonb@pm.me>
2021-02-21 15:40:58 +0000

    Fixup test

--- cli/tests/test_finally_cleartimeout.ts
+++ cli/tests/test_finally_cleartimeout.ts
@@ -1,8 +1,11 @@
-Deno.test("test", function () {
+Deno.test("error", function () {
   const timer = setTimeout(() => null, 10000);
   try {
     throw new Error("fail");
   } finally {
     clearTimeout(timer);
   }
+});
+
+Deno.test("success", function () {
 });
