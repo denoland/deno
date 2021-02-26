edaca1737c6fa3b19a489f628181102c56834a75
Divy Srivastava <dj.srivastava23@gmail.com>
2021-02-26 12:31:54 +0000

    fix(deno_crypto.d.ts): remove unused deno lint directive.

--- op_crates/crypto/lib.deno_crypto.d.ts
+++ op_crates/crypto/lib.deno_crypto.d.ts
@@ -1,7 +1,5 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
-// deno-lint-ignore-file no-explicit-any
-
 /// <reference no-default-lib="true" />
 /// <reference lib="esnext" />
 
