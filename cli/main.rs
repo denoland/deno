1344d263b94893cfd39799bdcae038952598d8b0
Akiacode <catry.me@gmail.com>
2021-03-12 01:09:26 +0900

    lint

--- cli/main.rs
+++ cli/main.rs
@@ -506,8 +506,6 @@ async fn eval_command(
   // Save our fake file into file fetcher cache
   // to allow module access by TS compiler.
   program_state.file_fetcher.insert_cached(file);
-
-
   debug!("main_module {}", &main_module);
   worker.execute_module(&main_module).await?;
   worker.execute("window.dispatchEvent(new Event('load'))")?;
