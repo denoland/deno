efc7b810cc5f1ec4a83eaf5e18e31c86f00831a9
Rémi Doreau <remi.d45@gmail.com>
2021-03-18 14:50:59 +0100

    test(coverage): coverage check e2e tests

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -4153,6 +4153,52 @@ console.log("finish");
 
       assert!(output.status.success());
     }
+
+    #[test]
+    fn coverage_threshold() {
+      let tempdir = TempDir::new().expect("tempdir fail");
+      let status = util::deno_cmd()
+        .current_dir(util::root_path())
+        .arg("test")
+        .arg("--quiet")
+        .arg("--unstable")
+        .arg(format!("--coverage={}", tempdir.path().to_str().unwrap()))
+        .arg("cli/tests/coverage/branch_test.ts")
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::inherit())
+        .status()
+        .expect("failed to spawn test runner");
+
+      assert!(status.success());
+
+      let output = util::deno_cmd()
+        .current_dir(util::root_path())
+        .arg("coverage")
+        .arg("--check")
+        .arg("--lines=70")
+        .arg("--branches=80")
+        .arg("--unstable")
+        .arg("--lcov")
+        .arg(format!("{}/", tempdir.path().to_str().unwrap()))
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::inherit())
+        .output()
+        .expect("failed to spawn coverage reporter");
+
+      let actual =
+        util::strip_ansi_codes(std::str::from_utf8(&output.stderr).unwrap())
+          .to_string();
+
+      let expected = "The coverage threshold is not met";
+
+      if !util::wildcard_match(&expected, &actual) {
+        println!("OUTPUT\n{}\nOUTPUT", actual);
+        println!("EXPECTED\n{}\nEXPECTED", expected);
+        panic!("pattern match failed");
+      }
+
+      assert!(output.status.success());
+    }
   }
 
   mod permissions {
