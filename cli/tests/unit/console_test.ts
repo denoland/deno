154467103a7429760f009b743945c37e4d0d2a74
Luca Casonato <lucacasonato@yahoo.com>
2021-03-18 14:10:45 +0100

    chore(console): distinguish between log levels
    Change `Console.#printFunc` to pass a log level as the second argument
    (0 = debug, 3 = error), instead of a boolean for `isErr`. This does not
    change the Deno runtime behaviour at all.

--- cli/tests/unit/console_test.ts
+++ cli/tests/unit/console_test.ts
@@ -1191,9 +1191,9 @@ function mockConsole(f: ConsoleExamineFunc): void {
   const err = new StringBuffer();
   const both = new StringBuffer();
   const csl = new Console(
-    (x: string, isErr: boolean, printsNewLine: boolean): void => {
+    (x: string, level: number, printsNewLine: boolean): void => {
       const content = x + (printsNewLine ? "\n" : "");
-      const buf = isErr ? err : out;
+      const buf = level > 1 ? err : out;
       buf.add(content);
       both.add(content);
     },
