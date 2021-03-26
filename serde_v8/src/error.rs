0186a606f0e3d3f7eb08c83e66834aca5811ba07
Aaron O'Mullan <aaron.omullan@gmail.com>
2021-03-27 00:29:47 +0100

    serde_v8: add Deno copyright headers to all rust files

--- serde_v8/src/error.rs
+++ serde_v8/src/error.rs
@@ -1,3 +1,4 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 use std::fmt::{self, Display};
 
 use serde::{de, ser};
--- serde_v8/src/payload.rs
+++ serde_v8/src/payload.rs
@@ -1,3 +1,4 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 use rusty_v8 as v8;
 
 // TODO: maybe add a Payload type that holds scope & v8::Value
