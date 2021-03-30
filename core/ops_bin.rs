f9498f3bf6cedcf11d1c5e8c9b17559586fde84f
Aaron O'Mullan <aaron.omullan@gmail.com>
2021-03-30 23:49:52 +0200

    core/ops_bin: re-enable non-empty buffer assert

--- core/ops_bin.rs
+++ core/ops_bin.rs
@@ -1,6 +1,6 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
-// use crate::error::type_error;
+use crate::error::type_error;
 use crate::error::AnyError;
 use crate::futures::future::FutureExt;
 use crate::serialize_op_result;
@@ -78,12 +78,12 @@ where
     }
     .into();
     // Bin op buffer arg assert
-    // if bufs.is_empty() {
-    //   return Op::Sync(serialize_bin_result::<u32>(
-    //     Err(type_error("bin-ops require a non-null buffer arg")),
-    //     state,
-    //   ));
-    // }
+    if bufs.is_empty() {
+      return Op::Sync(serialize_bin_result::<u32>(
+        Err(type_error("bin-ops require a non-null buffer arg")),
+        state,
+      ));
+    }
 
     let result = op_fn(&mut state.borrow_mut(), min_arg, &mut bufs);
     Op::Sync(serialize_bin_result(result, state))
@@ -155,12 +155,12 @@ where
       }
       .into();
       // Bin op buffer arg assert
-      // if bufs.is_empty() {
-      //   return Op::Sync(serialize_bin_result::<u32>(
-      //     Err(type_error("bin-ops require a non-null buffer arg")),
-      //     state,
-      //   ));
-      // }
+      if bufs.is_empty() {
+        return Op::Sync(serialize_bin_result::<u32>(
+          Err(type_error("bin-ops require a non-null buffer arg")),
+          state,
+        ));
+      }
 
       let fut = op_fn(state.clone(), min_arg, bufs)
         .map(move |result| serialize_bin_result(result, state));
