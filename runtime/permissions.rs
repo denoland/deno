8257f51d7e705851401b6c298942b0002c6f026a
upendra1997 <Upendra.upadhyay.97@gmail.com>
2021-04-03 02:19:51 +0530

    fix(permissions): don't panic when no input is given (#9894)
    Fixes #9633 
    
    Co-authored-by: Kitson Kelly <me@kitsonkelly.com>

--- runtime/permissions.rs
+++ runtime/permissions.rs
@@ -695,7 +695,10 @@ fn permission_prompt(message: &str) -> bool {
     if result.is_err() {
       return false;
     };
-    let ch = input.chars().next().unwrap();
+    let ch = match input.chars().next() {
+      None => return false,
+      Some(v) => v,
+    };
     match ch.to_ascii_lowercase() {
       'g' => return true,
       'd' => return false,
