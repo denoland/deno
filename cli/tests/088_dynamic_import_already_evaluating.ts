5f5229fce66d58c22bc34d7254fa2161021c326e
Nayeem Rahman <nayeemrmn99@gmail.com>
2021-04-03 20:44:24 +0100

    fixup! fix(core/runtime): Properly await already evaluating dynamic imports

--- cli/tests/088_dynamic_import_already_evaluating.ts
+++ cli/tests/088_dynamic_import_already_evaluating.ts
@@ -1,2 +1,2 @@
 import("./088_dynamic_import_target.ts").then(() => console.log(3));
-import("./088_dynamic_import_target.ts").then(() => console.log(4));
+import("./088_dynamic_import_target.ts").then(() => console.log(3));
