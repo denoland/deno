9d53dab4df64361f00dda2534eecbd04797d56a2
Casper Beyer <caspervonb@pm.me>
2021-04-12 20:32:58 +0800

    feat(runtime): add truncate and truncateSync methods to Deno.File (#10130)

--- cli/tests/unit/file_test.ts
+++ cli/tests/unit/file_test.ts
@@ -104,6 +104,50 @@ unitTest(function fileUsingEmptyStringFileName(): void {
   testSecondArgument("", "");
 });
 
+unitTest(
+  { perms: { read: true, write: true } },
+  function fileTruncateSyncSuccess(): void {
+    const filename = Deno.makeTempDirSync() + "/test_fileTruncateSync.txt";
+    const file = Deno.openSync(filename, {
+      create: true,
+      read: true,
+      write: true,
+    });
+
+    file.truncateSync(20);
+    assertEquals(Deno.readFileSync(filename).byteLength, 20);
+    file.truncateSync(5);
+    assertEquals(Deno.readFileSync(filename).byteLength, 5);
+    file.truncateSync(-5);
+    assertEquals(Deno.readFileSync(filename).byteLength, 0);
+
+    file.close();
+    Deno.removeSync(filename);
+  },
+);
+
+unitTest(
+  { perms: { read: true, write: true } },
+  async function fileTruncateSuccess(): Promise<void> {
+    const filename = Deno.makeTempDirSync() + "/test_fileTruncate.txt";
+    const file = await Deno.open(filename, {
+      create: true,
+      read: true,
+      write: true,
+    });
+
+    await file.truncate(20);
+    assertEquals((await Deno.readFile(filename)).byteLength, 20);
+    await file.truncate(5);
+    assertEquals((await Deno.readFile(filename)).byteLength, 5);
+    await file.truncate(-5);
+    assertEquals((await Deno.readFile(filename)).byteLength, 0);
+
+    file.close();
+    await Deno.remove(filename);
+  },
+);
+
 unitTest({ perms: { read: true } }, function fileStatSyncSuccess(): void {
   const file = Deno.openSync("README.md");
   const fileInfo = file.statSync();
