de1005ec028337e058fb32afcd49ea7698a13083
Casper Beyer <caspervonb@pm.me>
2021-04-15 05:47:47 +0000

    Move the meat into cli/tools/test_runner.rs

--- cli/tools/test_runner.rs
+++ cli/tools/test_runner.rs
@@ -1,10 +1,29 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
+use crate::colors;
+use crate::module_graph;
+use crate::tokio_util;
+use deno_core::ModuleSpecifier;
+use std::sync::mpsc::channel;
+use crate::flags::Flags;
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
+use deno_runtime::permissions::Permissions;
+use crate::program_state::ProgramState;
+use deno_core::serde_json::json;
+use crate::create_main_worker;
 use crate::fs_util;
 use crate::tools::installer::is_remote_url;
+use crate::tools::coverage::CoverageCollector;
 use deno_core::error::AnyError;
 use deno_core::url::Url;
 use std::path::Path;
+use crate::test_dispatcher::TestMessage;
+use crate::test_dispatcher::TestResult;
+use deno_core::futures::future;
+use deno_core::futures::stream;
+use deno_core::futures::StreamExt;
+use std::path::PathBuf;
 
 fn is_supported(p: &Path) -> bool {
   use std::path::Component;
@@ -60,6 +79,273 @@ pub fn collect_test_module_specifiers(
   }
 
   Ok(prepared)
+}
+
+pub async fn run_test(
+  program_state: Arc<ProgramState>,
+  main_module: ModuleSpecifier,
+  permissions: Permissions,
+  channel: Sender<TestMessage>,
+  quiet: bool,
+  filter: Option<String>,
+) -> Result<(), AnyError> {
+  let mut worker =
+    create_main_worker(&program_state, main_module.clone(), permissions, true);
+
+  {
+    let js_runtime = &mut worker.js_runtime;
+    js_runtime
+      .op_state()
+      .borrow_mut()
+      .put::<Sender<TestMessage>>(channel.clone());
+  }
+
+  let mut maybe_coverage_collector =
+    if let Some(ref coverage_dir) = program_state.coverage_dir {
+      let session = worker.create_inspector_session();
+      let coverage_dir = PathBuf::from(coverage_dir);
+      let mut coverage_collector = CoverageCollector::new(coverage_dir, session);
+      coverage_collector.start_collecting().await?;
+
+      Some(coverage_collector)
+    } else {
+      None
+    };
+
+  let options = json!({
+    "filter": filter,
+    "quiet": quiet,
+  });
+
+  let execute_result = worker.execute_module(&main_module).await;
+  execute_result?;
+  worker.execute("window.dispatchEvent(new Event('load'))")?;
+  worker.execute(&format!("Deno[Deno.internal].runTests({})", options))?;
+
+  worker.run_event_loop().await?;
+  worker.execute("window.dispatchEvent(new Event('unload'))")?;
+  worker.run_event_loop().await?;
+
+  if let Some(coverage_collector) = maybe_coverage_collector.as_mut() {
+    coverage_collector.stop_collecting().await?;
+  }
+
+  Ok(())
+}
+
+
+#[allow(clippy::too_many_arguments)]
+pub async fn run_tests(
+  flags: Flags,
+  include: Option<Vec<String>>,
+  no_run: bool,
+  fail_fast: bool,
+  quiet: bool,
+  allow_none: bool,
+  filter: Option<String>,
+  concurrent_jobs: usize,
+  ) -> Result<(), AnyError> {
+  let program_state = ProgramState::build(flags.clone()).await?;
+  let permissions = Permissions::from_options(&flags.clone().into());
+  let cwd = std::env::current_dir().expect("No current directory");
+  let include = include.unwrap_or_else(|| vec![".".to_string()]);
+  let test_modules = collect_test_module_specifiers(include, &cwd)?;
+
+  if test_modules.is_empty() {
+    println!("No matching test modules found");
+    if !allow_none {
+      std::process::exit(1);
+    }
+    return Ok(());
+  }
+
+  let lib = if flags.unstable {
+    module_graph::TypeLib::UnstableDenoWindow
+  } else {
+    module_graph::TypeLib::DenoWindow
+  };
+
+  program_state
+    .prepare_module_graph(
+      test_modules.clone(),
+      lib.clone(),
+      permissions.clone(),
+      program_state.maybe_import_map.clone(),
+    )
+    .await?;
+
+  if no_run {
+    return Ok(());
+  }
+
+  let (sender, receiver) = channel::<TestMessage>();
+
+  let join_handles = test_modules.iter().map(move |module_specifier| {
+    let program_state = program_state.clone();
+    let module_specifier = module_specifier.clone();
+    let permissions = permissions.clone();
+    let sender = sender.clone();
+    let filter = filter.clone();
+
+    tokio::task::spawn_blocking(move || {
+      let join_handle = std::thread::spawn(move || {
+        let future = run_test(
+          program_state.clone(),
+          module_specifier.clone(),
+          permissions.clone(),
+          sender.clone(),
+          quiet,
+          filter,
+        );
+
+        tokio_util::run_basic(future)
+      });
+
+      join_handle.join().unwrap()
+    })
+  });
+
+  let join_futures = stream::iter(join_handles)
+    .buffer_unordered(concurrent_jobs)
+    .collect::<Vec<Result<Result<(), AnyError>, tokio::task::JoinError>>>();
+
+  let handler = {
+    tokio::task::spawn_blocking(move || {
+      let time = std::time::Instant::now();
+      let mut failed = 0;
+      let mut filtered_out = 0;
+      let mut ignored = 0;
+      let mut passed = 0;
+      let measured = 0;
+
+      let mut used_only = false;
+      let mut has_error = false;
+      let mut failures: Vec<(String, String)> = Vec::new();
+
+      for message in receiver.iter() {
+        match message {
+          TestMessage::Plan {
+            pending,
+            filtered,
+            only,
+          } => {
+            println!("running {} tests", pending);
+
+            if only {
+              used_only = true;
+            }
+
+            filtered_out += filtered;
+          }
+
+          TestMessage::Result {
+            name,
+            duration,
+            result,
+          } => match result {
+            TestResult::Ok => {
+              println!(
+                "test {} ... {} {}",
+                name,
+                colors::green("ok"),
+                colors::gray(format!("({}ms)", duration))
+              );
+
+              passed += 1;
+            }
+            TestResult::Ignored => {
+              println!(
+                "test {} ... {} {}",
+                name,
+                colors::yellow("ignored"),
+                colors::gray(format!("({}ms)", duration))
+              );
+
+              ignored += 1;
+            }
+            TestResult::Failed(error) => {
+              println!(
+                "test {} ... {} {}",
+                name,
+                colors::red("FAILED"),
+                colors::gray(format!("({}ms)", duration))
+              );
+
+              failed += 1;
+              failures.push((name, error));
+              has_error = true;
+            }
+          },
+          _ => {}
+        }
+
+        if has_error && fail_fast {
+          break;
+        }
+      }
+
+      if !failures.is_empty() {
+        println!("\nfailures:\n");
+        for (name, error) in &failures {
+          println!("{}", name);
+          println!("{}", error);
+          println!();
+        }
+
+        println!("failures:\n");
+        for (name, _) in &failures {
+          println!("\t{}", name);
+        }
+      }
+
+      let status = if failures.is_empty() {
+        colors::green("ok").to_string()
+      } else {
+        colors::red("FAILED").to_string()
+      };
+
+      println!(
+        "\ntest result: {}. {} passed; {} failed; {} ignored; {} measured; {} filtered out {}\n",
+        status,
+        passed,
+        failed,
+        ignored,
+        measured,
+        filtered_out,
+        colors::gray(format!("({}ms)", time.elapsed().as_millis())),
+      );
+
+      if used_only {
+        println!(
+          "{} because the \"only\" option was used\n",
+          colors::red("FAILED")
+        );
+
+        has_error = true;
+      }
+
+      has_error
+    })
+  };
+
+  let (result, join_results) = future::join(handler, join_futures).await;
+
+  let mut join_errors = join_results.into_iter().filter_map(|join_result| {
+    join_result
+      .ok()
+      .map(|handle_result| handle_result.err())
+      .flatten()
+  });
+
+  if result.unwrap_or(false) {
+    std::process::exit(1);
+  }
+
+  if let Some(e) = join_errors.next() {
+    Err(e)
+  } else {
+    Ok(())
+  }
 }
 
 #[cfg(test)]
