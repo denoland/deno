5d8682d03f29046d60b64e4719f04d139745ee82
Satya Rohith <me@satyarohith.com>
2021-04-15 23:35:28 +0530

    refactor(deno_http_native): pass string directly to Response

--- cli/bench/deno_http_native.js
+++ cli/bench/deno_http_native.js
@@ -5,13 +5,11 @@ const [hostname, port] = addr.split(":");
 const listener = Deno.listen({ hostname, port: Number(port) });
 console.log("Server listening on", addr);
 
-const body = Deno.core.encode("Hello World");
-
 for await (const conn of listener) {
   (async () => {
     const requests = Deno.serveHttp(conn);
     for await (const { respondWith } of requests) {
-      respondWith(new Response(body));
+      respondWith(new Response("Hello World"));
     }
   })();
 }
