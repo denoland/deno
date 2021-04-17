b344c7cb1d5534f9daea7b094be9d24a3eadfabc
Aaron O'Mullan <aaron.omullan@gmail.com>
2021-04-18 00:45:43 +0200

    bench_util: add js_runtime benching utils

---
+++ bench_util/src/profiling.rs
@@ -0,0 +1,81 @@
+use bencher::{DynBenchFn, StaticBenchFn, TestDescAndFn, TestOpts};
+
+pub fn is_profiling() -> bool {
+  std::env::var("PROFILING").is_ok()
+}
+
+#[macro_export]
+// Tweaked and copied from https://github.com/bluss/bencher/blob/master/macros.rs
+macro_rules! bench_or_profile {
+  ($($group_name:path),+) => {
+    fn main() {
+      use bencher::TestOpts;
+      use bencher::run_tests_console;
+      let mut test_opts = TestOpts::default();
+      // check to see if we should filter:
+      if let Some(arg) = ::std::env::args().skip(1).find(|arg| *arg != "--bench") {
+          test_opts.filter = Some(arg);
+      }
+      let mut benches = Vec::new();
+      $(
+          benches.extend($group_name());
+      )+
+
+      if $crate::is_profiling() {
+        // Run profling
+        $crate::run_profiles(&test_opts, benches);
+      } else {
+        // Run benches
+        run_tests_console(&test_opts, benches).unwrap();
+      }
+    }
+  };
+  ($($group_name:path,)+) => {
+      bench_or_profile!($($group_name),+);
+  };
+}
+
+pub fn run_profiles(opts: &TestOpts, tests: Vec<TestDescAndFn>) {
+  let tests = filter_tests(opts, tests);
+  // let decs = tests.iter().map(|t| t.desc.clone()).collect();
+
+  println!();
+  for b in tests {
+    println!("Profiling {}", b.desc.name);
+    run_profile(b);
+  }
+  println!();
+}
+
+fn run_profile(test: TestDescAndFn) {
+  match test.testfn {
+    DynBenchFn(bencher) => {
+      bencher::bench::run_once(|harness| bencher.run(harness));
+    }
+    StaticBenchFn(benchfn) => {
+      bencher::bench::run_once(|harness| benchfn(harness));
+    }
+  };
+}
+
+// Copied from https://github.com/bluss/bencher/blob/master/lib.rs
+fn filter_tests(
+  opts: &TestOpts,
+  tests: Vec<TestDescAndFn>,
+) -> Vec<TestDescAndFn> {
+  let mut filtered = tests;
+
+  // Remove tests that don't match the test filter
+  filtered = match opts.filter {
+    None => filtered,
+    Some(ref filter) => filtered
+      .into_iter()
+      .filter(|test| test.desc.name.contains(&filter[..]))
+      .collect(),
+  };
+
+  // Sort the tests alphabetically
+  filtered.sort_by(|t1, t2| t1.desc.name.cmp(&t2.desc.name));
+
+  filtered
+}
--- core/benches/op_baseline.rs
+++ core/benches/op_baseline.rs
@@ -1,39 +1,25 @@
-use bench_util::bencher::{benchmark_group, Bencher};
-use bench_util::{bench_or_profile, is_profiling};
-
 use deno_core::error::AnyError;
 use deno_core::op_async;
 use deno_core::op_sync;
 use deno_core::serialize_op_result;
-use deno_core::v8;
 use deno_core::JsRuntime;
 use deno_core::Op;
 use deno_core::OpState;
 use deno_core::ZeroCopyBuf;
 
+use bench_util::bench_or_profile;
+use bench_util::bencher::{benchmark_group, Bencher};
+use bench_util::{bench_js_async, bench_js_sync};
+
 use std::cell::RefCell;
 use std::rc::Rc;
 
-fn create_js_runtime() -> JsRuntime {
-  let mut runtime = JsRuntime::new(Default::default());
-  runtime.register_op("pi_json", op_sync(|_, _: (), _| Ok(314159)));
-  runtime.register_op("pi_async", op_async(op_pi_async));
-  runtime.register_op("nop", |state, _, _| {
+fn setup(rt: &mut JsRuntime) {
+  rt.register_op("pi_json", op_sync(|_, _: (), _| Ok(314159)));
+  rt.register_op("pi_async", op_async(op_pi_async));
+  rt.register_op("nop", |state, _, _| {
     Op::Sync(serialize_op_result(Ok(9), state))
   });
-
-  // Init ops
-  runtime
-    .execute(
-      "init",
-      r#"
-      Deno.core.ops();
-      Deno.core.registerErrorClass('Error', Error);
-    "#,
-    )
-    .unwrap();
-
-  runtime
 }
 
 // this is a function since async closures aren't stable
@@ -45,69 +31,20 @@ async fn op_pi_async(
   Ok(314159)
 }
 
-fn loop_code(iters: u64, src: &str) -> String {
-  format!(r#"for(let i=0; i < {}; i++) {{ {} }}"#, iters, src,)
-}
-
-pub fn bench_js_sync(b: &mut Bencher, src: &str) {
-  let mut runtime = create_js_runtime();
-  let context = runtime.global_context();
-  let scope = &mut v8::HandleScope::with_context(runtime.v8_isolate(), context);
-
-  // Increase JS iterations if profiling for nicer flamegraphs
-  let inner_iters = 1000 * if is_profiling() { 10000 } else { 1 };
-  // Looped code
-  let looped_src = loop_code(inner_iters, src);
-
-  let code = v8::String::new(scope, looped_src.as_ref()).unwrap();
-  let script = v8::Script::compile(scope, code, None).unwrap();
-
-  // Run once if profiling, otherwise regular bench loop
-  if is_profiling() {
-    script.run(scope).unwrap();
-  } else {
-    b.iter(|| {
-      script.run(scope).unwrap();
-    });
-  }
-}
-
-pub fn bench_js_async(b: &mut Bencher, src: &str) {
-  let mut runtime = create_js_runtime();
-  let tokio_runtime = tokio::runtime::Builder::new_current_thread()
-    .enable_all()
-    .build()
-    .unwrap();
-
-  // Looped code
-  let looped = loop_code(1000, src);
-  let src = looped.as_ref();
-
-  if is_profiling() {
-    for _ in 0..10000 {
-      runtime.execute("inner_loop", src).unwrap();
-      let future = runtime.run_event_loop();
-      tokio_runtime.block_on(future).unwrap();
-    }
-  } else {
-    b.iter(|| {
-      runtime.execute("inner_loop", src).unwrap();
-      let future = runtime.run_event_loop();
-      tokio_runtime.block_on(future).unwrap();
-    });
-  }
-}
-
 fn bench_op_pi_json(b: &mut Bencher) {
-  bench_js_sync(b, r#"Deno.core.opSync("pi_json");"#);
+  bench_js_sync(b, r#"Deno.core.opSync("pi_json");"#, setup);
 }
 
 fn bench_op_nop(b: &mut Bencher) {
-  bench_js_sync(b, r#"Deno.core.dispatchByName("nop", null, null, null);"#);
+  bench_js_sync(
+    b,
+    r#"Deno.core.dispatchByName("nop", null, null, null);"#,
+    setup,
+  );
 }
 
 fn bench_op_async(b: &mut Bencher) {
-  bench_js_async(b, r#"Deno.core.opAsync("pi_async");"#);
+  bench_js_async(b, r#"Deno.core.opAsync("pi_async");"#, setup);
 }
 
 benchmark_group!(benches, bench_op_pi_json, bench_op_nop, bench_op_async);
