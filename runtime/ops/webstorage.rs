82d38c4055e9d501da00da444f6b76f34dee28e0
crowlkats <13135287+crowlKats@users.noreply.github.com>
2021-04-19 04:12:54 +0200

    fmt

--- runtime/ops/webstorage.rs
+++ runtime/ops/webstorage.rs
@@ -24,9 +24,5 @@ pub fn init(rt: &mut deno_core::JsRuntime, deno_dir: Option<PathBuf>) {
   super::reg_sync(rt, "op_webstorage_get", op_webstorage_get);
   super::reg_sync(rt, "op_webstorage_remove", op_webstorage_remove);
   super::reg_sync(rt, "op_webstorage_clear", op_webstorage_clear);
-  super::reg_sync(
-    rt,
-    "op_webstorage_iterate_keys",
-    op_webstorage_iterate_keys,
-  );
+  super::reg_sync(rt, "op_webstorage_iterate_keys", op_webstorage_iterate_keys);
 }
