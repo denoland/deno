01355bf02e609b9982e5898bec18a1734bf96b86
Casper Beyer <caspervonb@pm.me>
2021-04-26 11:52:17 +0000

    Revert "Close all webgpu resources?"
    This reverts commit d059c46b949c16e048e21b5390dfe55d51110e9d.

--- cli/tests/unit/webgpu_test.ts
+++ cli/tests/unit/webgpu_test.ts
@@ -217,12 +217,6 @@ unitTest({
 
   // TODO(lucacasonato): webgpu spec should add a explicit destroy method for
   // adapters.
-  let resources = Object.keys(Deno.resources());
-  Deno.close(Number(resources[resources.length - 1]));
-
-  resources = Object.keys(Deno.resources());
-  Deno.close(Number(resources[resources.length - 1]));
-
-  resources = Object.keys(Deno.resources());
+  const resources = Object.keys(Deno.resources());
   Deno.close(Number(resources[resources.length - 1]));
 });
