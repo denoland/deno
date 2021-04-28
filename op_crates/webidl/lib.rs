e390a72fdefd65810136b41753fef52c17c21dfc
Aaron O'Mullan <aaron.omullan@gmail.com>
2021-04-28 22:35:52 +0200

    refactor: use include_js_files for timers & webidl
    They don't necessarily need it, since they each include a single file, but for consistency's sake might be best to align with others (also makes adding extra files easier)

--- op_crates/webidl/lib.rs
+++ op_crates/webidl/lib.rs
@@ -1,13 +1,14 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
+use deno_core::include_js_files;
 use deno_core::Extension;
 
 /// Load and execute the javascript code.
 pub fn init() -> Extension {
   Extension::builder()
-    .js(vec![(
-      "deno:op_crates/webidl/00_webidl.js",
-      include_str!("00_webidl.js"),
-    )])
+    .js(include_js_files!(
+      prefix "deno:op_crates/webidl",
+      "00_webidl.js",
+    ))
     .build()
 }
