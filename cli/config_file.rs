b45a4e451ebcf46c9f8959c5c3cf71c7c7770e0b
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-05-03 14:07:48 +0200

    add json struct

--- cli/config_file.rs
+++ cli/config_file.rs
@@ -2,12 +2,21 @@
 
 use crate::fs_util::canonicalize_path;
 use deno_core::error::AnyError;
+use deno_core::serde::Deserialize;
+use deno_core::serde_json;
 use deno_core::serde_json::Value;
 use std::path::PathBuf;
 
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ConfigFileJson {
+  pub compiler_options: Option<Value>,
+  pub import_map: Option<Value>,
+}
+
 pub struct ConfigFile {
   pub path: PathBuf,
-  pub value: Value,
+  pub json: ConfigFileJson,
 }
 
 impl ConfigFile {
@@ -24,11 +33,12 @@ impl ConfigFile {
       )
     })?;
     let config_text = std::fs::read_to_string(config_path.clone())?;
-    let json_value = jsonc_parser::parse_to_serde_value(&config_text)?.unwrap();
+    let jsonc = jsonc_parser::parse_to_serde_value(&config_text)?.unwrap();
+    let json: ConfigFileJson = serde_json::from_value(jsonc)?;
 
     Ok(Self {
       path: config_path,
-      value: json_value,
+      json,
     })
   }
 }
@@ -41,5 +51,7 @@ mod tests {
   fn read_config_file() {
     let config_file = ConfigFile::read("tests/module_graph/tsconfig.json")
       .expect("Failed to load config file");
+    assert!(config_file.json.compiler_options.is_some());
+    assert!(config_file.json.import_map.is_none());
   }
 }
