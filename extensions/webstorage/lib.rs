3e6992557074eb4adf4d9bde0ee75a28422372d9
crowlkats <13135287+crowlKats@users.noreply.github.com>
2021-05-05 19:19:24 +0200

    sled

--- extensions/webstorage/lib.rs
+++ extensions/webstorage/lib.rs
@@ -8,9 +8,6 @@ use deno_core::Extension;
 use deno_core::OpState;
 use deno_core::Resource;
 use deno_core::ZeroCopyBuf;
-use rusqlite::params;
-use rusqlite::Connection;
-use rusqlite::OptionalExtension;
 use serde::Deserialize;
 use std::borrow::Cow;
 use std::fmt;
@@ -51,7 +48,7 @@ pub fn get_declaration() -> PathBuf {
   PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("lib.deno_webstorage.d.ts")
 }
 
-struct WebStorageConnectionResource(Connection);
+struct WebStorageConnectionResource(sled::Db);
 
 impl Resource for WebStorageConnectionResource {
   fn name(&self) -> Cow<str> {
@@ -64,26 +61,19 @@ pub fn op_webstorage_open(
   persistent: bool,
   _zero_copy: Option<ZeroCopyBuf>,
 ) -> Result<u32, AnyError> {
-  let connection = if persistent {
+  let db = if persistent {
     let path = state.try_borrow::<LocationDataDir>().ok_or_else(|| {
       DomExceptionNotSupportedError::new(
         "LocalStorage is not supported in this context.",
       )
     })?;
     std::fs::create_dir_all(&path.0)?;
-    Connection::open(path.0.join("local_storage"))?
+    sled::open(path.0.join("local_storage"))?
   } else {
-    Connection::open_in_memory()?
+    sled::open(std::env::temp_dir())?
   };
 
-  connection.execute(
-    "CREATE TABLE IF NOT EXISTS data (key VARCHAR UNIQUE, value VARCHAR)",
-    params![],
-  )?;
-
-  let rid = state
-    .resource_table
-    .add(WebStorageConnectionResource(connection));
+  let rid = state.resource_table.add(WebStorageConnectionResource(db));
   Ok(rid)
 }
 
@@ -97,11 +87,7 @@ pub fn op_webstorage_length(
     .get::<WebStorageConnectionResource>(rid)
     .ok_or_else(bad_resource_id)?;
 
-  let mut stmt = resource.0.prepare("SELECT COUNT(*) FROM data")?;
-
-  let length: u32 = stmt.query_row(params![], |row| row.get(0))?;
-
-  Ok(length)
+  Ok(resource.0.len() as u32)
 }
 
 #[derive(Deserialize)]
@@ -121,13 +107,13 @@ pub fn op_webstorage_key(
     .get::<WebStorageConnectionResource>(args.rid)
     .ok_or_else(bad_resource_id)?;
 
-  let mut stmt = resource
+  let key = resource
     .0
-    .prepare("SELECT key FROM data LIMIT 1 OFFSET ?")?;
-
-  let key: Option<String> = stmt
-    .query_row(params![args.index], |row| row.get(0))
-    .optional()?;
+    .iter()
+    .nth(args.index as usize)
+    .transpose()?
+    .map(|(key, _)| String::from_utf8(key.to_vec()))
+    .transpose()?;
 
   Ok(key)
 }
@@ -150,22 +136,14 @@ pub fn op_webstorage_set(
     .get::<WebStorageConnectionResource>(args.rid)
     .ok_or_else(bad_resource_id)?;
 
-  let mut stmt = resource
-    .0
-    .prepare("SELECT SUM(pgsize) FROM dbstat WHERE name = 'data'")?;
-  let size: u32 = stmt.query_row(params![], |row| row.get(0))?;
-
-  if size >= 5000000 {
+  if resource.0.size_on_disk()? >= 5000000 {
     return Err(
       DomExceptionQuotaExceededError::new("Exceeded maximum storage size")
         .into(),
     );
   }
 
-  resource.0.execute(
-    "INSERT OR REPLACE INTO data (key, value) VALUES (?, ?)",
-    params![args.key_name, args.key_value],
-  )?;
+  resource.0.insert(args.key_name, args.key_value.as_bytes())?;
 
   Ok(())
 }
@@ -187,11 +165,11 @@ pub fn op_webstorage_get(
     .get::<WebStorageConnectionResource>(args.rid)
     .ok_or_else(bad_resource_id)?;
 
-  let mut stmt = resource.0.prepare("SELECT value FROM data WHERE key = ?")?;
-
-  let val = stmt
-    .query_row(params![args.key_name], |row| row.get(0))
-    .optional()?;
+  let val = resource
+    .0
+    .get(args.key_name)?
+    .map(|value| String::from_utf8(value.to_vec()))
+    .transpose()?;
 
   Ok(val)
 }
@@ -213,9 +191,7 @@ pub fn op_webstorage_remove(
     .get::<WebStorageConnectionResource>(args.rid)
     .ok_or_else(bad_resource_id)?;
 
-  resource
-    .0
-    .execute("DELETE FROM data WHERE key = ?", params![args.key_name])?;
+  resource.0.remove(args.key_name)?;
 
   Ok(())
 }
@@ -230,11 +206,7 @@ pub fn op_webstorage_clear(
     .get::<WebStorageConnectionResource>(rid)
     .ok_or_else(bad_resource_id)?;
 
-  resource.0.execute("DROP TABLE data", params![])?;
-  resource.0.execute(
-    "CREATE TABLE data (key VARCHAR UNIQUE, value VARCHAR)",
-    params![],
-  )?;
+  resource.0.clear()?;
 
   Ok(())
 }
@@ -249,11 +221,13 @@ pub fn op_webstorage_iterate_keys(
     .get::<WebStorageConnectionResource>(rid)
     .ok_or_else(bad_resource_id)?;
 
-  let mut stmt = resource.0.prepare("SELECT key FROM data")?;
-
-  let keys = stmt
-    .query_map(params![], |row| row.get::<_, String>(0))?
-    .map(|r| r.unwrap())
+  let keys = resource
+    .0
+    .iter()
+    .map(|entry| {
+      let (key, _) = entry.unwrap();
+      String::from_utf8(key.to_vec()).unwrap()
+    })
     .collect();
 
   Ok(keys)
