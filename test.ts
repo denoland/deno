35d375acb51fa1d4062429519dd42d25368c124b
crowlkats <13135287+crowlKats@users.noreply.github.com>
2021-05-05 14:15:43 +0200

    clean up

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { assertEquals } from "./test_util/std/testing/asserts.ts";
-
-const ws = new WebSocketStream("ws://echo.websocket.org");
-const { readable, writable } = await ws.connection;
-await writable.getWriter().write("foo");
-const res = await readable.getReader().read();
-assertEquals(res.value, "foo");
-ws.close();
-await ws.closed;
