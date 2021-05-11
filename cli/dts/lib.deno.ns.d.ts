2b8bac3c85d90de44c00677b015f69fd445c1489
Casper Beyer <caspervonb@pm.me>
2021-05-11 10:18:29 +0800

    docs(cli/dts): fix Deno.watchFs example (#10570)

--- cli/dts/lib.deno.ns.d.ts
+++ cli/dts/lib.deno.ns.d.ts
@@ -1970,7 +1970,13 @@ declare namespace Deno {
    *
    * ```ts
    * const watcher = Deno.watchFs("/");
-   * setTimeout(() => { watcher.return(); }, 5000);
+   *
+   * setTimeout(() => {
+   *   if (watcher.return) {
+   *     watcher.return();
+   *   }
+   * }, 5000);
+   *
    * for await (const event of watcher) {
    *    console.log(">>>> event", event);
    * }
