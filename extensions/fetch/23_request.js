0b290aef1276e63c939e30a0e45e297dc3c993a1
Yoshiya Hinosawa <stibium121@gmail.com>
2021-05-27 16:40:50 +0900

    fix(ext/fetch): fix error message of request constructor

--- extensions/fetch/23_request.js
+++ extensions/fetch/23_request.js
@@ -273,7 +273,7 @@
         ((init.body !== undefined && init.body !== null) ||
           inputBody !== null)
       ) {
-        throw new TypeError("HEAD and GET requests may not have a body.");
+        throw new TypeError("HEAD or GET request cannot have a body.");
       }
 
       // 34.
