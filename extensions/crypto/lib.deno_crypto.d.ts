9a479eb9109ac861c48e4b04e0152c43414d7d79
Casper Beyer <caspervonb@pm.me>
2021-05-30 19:37:48 +0000

    Format

--- extensions/crypto/lib.deno_crypto.d.ts
+++ extensions/crypto/lib.deno_crypto.d.ts
@@ -26,17 +26,31 @@ declare interface Crypto {
 }
 
 interface Algorithm {
-    name: string;
+  name: string;
 }
 
 /** This Web Crypto API interface provides a number of low-level cryptographic functions. It is accessed via the Crypto.subtle properties available in a window context (via Window.crypto). */
 interface SubtleCrypto {
-    digest(algorithm: AlgorithmIdentifier, data: Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array | DataView | ArrayBuffer): Promise<ArrayBuffer>;
+  digest(
+    algorithm: AlgorithmIdentifier,
+    data:
+      | Int8Array
+      | Int16Array
+      | Int32Array
+      | Uint8Array
+      | Uint16Array
+      | Uint32Array
+      | Uint8ClampedArray
+      | Float32Array
+      | Float64Array
+      | DataView
+      | ArrayBuffer,
+  ): Promise<ArrayBuffer>;
 }
 
 declare var SubtleCrypto: {
-    prototype: SubtleCrypto;
-    new(): SubtleCrypto;
+  prototype: SubtleCrypto;
+  new (): SubtleCrypto;
 };
 
 type AlgorithmIdentifier = string | Algorithm;
