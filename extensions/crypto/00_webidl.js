49b2b10cfa2f71b631f1ca639b8dd498694015fc
Casper Beyer <caspervonb@pm.me>
2021-06-02 22:26:31 +0000

    Add webidl.js

---
+++ extensions/crypto/00_webidl.js
@@ -0,0 +1,25 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+"use strict";
+
+((window) => {
+  const webidl = window.__bootstrap.webidl;
+  webidl.converters["AlgorithmIdentifier"] = (V, opts) => {
+    if (typeof V == "string") {
+      return webidl.converters["DOMString"](V);
+    }
+
+    return webidl.converters["Algorithm"](V);
+  };
+
+  const algorithmDictionary = [
+    {
+      key: "name",
+      converter: webidl.converters["DOMString"],
+    },
+  ];
+
+  webidl.converters["Algorithm"] = webidl.createDictionaryConverter(
+    "Algorithm",
+    algorithmDictionary,
+  );
+})(this);
