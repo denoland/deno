a7d30ac93aa7180a43f20aaf15418923f7b4c31d
Casper Beyer <caspervonb@pm.me>
2021-06-06 07:09:00 +0000

    Fixup webidl

--- extensions/crypto/01_crypto.js
+++ extensions/crypto/01_crypto.js
@@ -5,58 +5,6 @@
   const core = window.Deno.core;
   const webidl = window.__bootstrap.webidl;
 
-  class Crypto {
-    constructor() {
-      webidl.illegalConstructor();
-    }
-
-    getRandomValues(arrayBufferView) {
-      webidl.assertBranded(this, Crypto);
-      const prefix = "Failed to execute 'getRandomValues' on 'Crypto'";
-      webidl.requiredArguments(arguments.length, 1, { prefix });
-      arrayBufferView = webidl.converters.ArrayBufferView(arrayBufferView, {
-        prefix,
-        context: "Argument 1",
-      });
-      if (
-        !(
-          arrayBufferView instanceof Int8Array ||
-          arrayBufferView instanceof Uint8Array ||
-          arrayBufferView instanceof Int16Array ||
-          arrayBufferView instanceof Uint16Array ||
-          arrayBufferView instanceof Int32Array ||
-          arrayBufferView instanceof Uint32Array ||
-          arrayBufferView instanceof Uint8ClampedArray
-        )
-      ) {
-        throw new DOMException(
-          "The provided ArrayBufferView is not an integer array type",
-          "TypeMismatchError",
-        );
-      }
-      const ui8 = new Uint8Array(
-        arrayBufferView.buffer,
-        arrayBufferView.byteOffset,
-        arrayBufferView.byteLength,
-      );
-      core.opSync("op_crypto_get_random_values", ui8);
-      return arrayBufferView;
-    }
-
-    randomUUID() {
-      webidl.assertBranded(this, Crypto);
-      return core.opSync("op_crypto_random_uuid");
-    }
-
-    get [Symbol.toStringTag]() {
-      return "Crypto";
-    }
-
-    [Symbol.for("Deno.customInspect")](inspect) {
-      return `${this.constructor.name} ${inspect({})}`;
-    }
-  }
-
   const supportedAlgorithms = {
     "digest": {
       "SHA-1": {},
@@ -151,15 +99,74 @@
     }
   }
 
-  // TODO(caspervonb) don't merge, fixup the subtle attribute
+
   const subtle = webidl.createBranded(SubtleCrypto);
-  const crypto = Object.assign(webidl.createBranded(Crypto), {
-    subtle,
+
+  class Crypto {
+    constructor() {
+      webidl.illegalConstructor();
+    }
+
+    getRandomValues(arrayBufferView) {
+      webidl.assertBranded(this, Crypto);
+      const prefix = "Failed to execute 'getRandomValues' on 'Crypto'";
+      webidl.requiredArguments(arguments.length, 1, { prefix });
+      arrayBufferView = webidl.converters.ArrayBufferView(arrayBufferView, {
+        prefix,
+        context: "Argument 1",
+      });
+      if (
+        !(
+          arrayBufferView instanceof Int8Array ||
+          arrayBufferView instanceof Uint8Array ||
+          arrayBufferView instanceof Int16Array ||
+          arrayBufferView instanceof Uint16Array ||
+          arrayBufferView instanceof Int32Array ||
+          arrayBufferView instanceof Uint32Array ||
+          arrayBufferView instanceof Uint8ClampedArray
+        )
+      ) {
+        throw new DOMException(
+          "The provided ArrayBufferView is not an integer array type",
+          "TypeMismatchError",
+        );
+      }
+      const ui8 = new Uint8Array(
+        arrayBufferView.buffer,
+        arrayBufferView.byteOffset,
+        arrayBufferView.byteLength,
+      );
+      core.opSync("op_crypto_get_random_values", ui8);
+      return arrayBufferView;
+    }
+
+    randomUUID() {
+      webidl.assertBranded(this, Crypto);
+      return core.opSync("op_crypto_random_uuid");
+    }
+
+    get subtle() {
+      webidl.assertBranded(this, Crypto);
+      return subtle;
+    }
+
+    get [Symbol.toStringTag]() {
+      return "Crypto";
+    }
+
+    [Symbol.for("Deno.customInspect")](inspect) {
+      return `${this.constructor.name} ${inspect({})}`;
+    }
+  }
+
+  Object.defineProperty(Crypto.prototype, "subtle", {
+    configurable: true,
+    enumerable: true,
   });
 
   window.__bootstrap.crypto = {
     SubtleCrypto,
-    crypto,
+    crypto: webidl.createBranded(Crypto),
     Crypto,
   };
 })(this);
