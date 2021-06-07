f93dfd3fc746f5a03f871052e3c926e7050e3990
Divy Srivastava <dj.srivastava23@gmail.com>
2021-06-07 14:53:25 +0530

    Update standalone_compiler_ops.ts

--- cli/tests/standalone_compiler_ops.ts
+++ cli/tests/standalone_compiler_ops.ts
@@ -2,7 +2,7 @@ const { files } = await Deno.emit("/mod.ts", {
   bundle: "classic",
   sources: {
     "/mod.ts": `import { hello } from "/hello.ts"; console.log(hello);`,
-    "/hello.ts": `export const hello: string = "Hello, Compiler API"`,
+    "/hello.ts": `export const hello: string = "Hello, Compiler API!"`,
   },
   compilerOptions: {
     sourceMap: false,
