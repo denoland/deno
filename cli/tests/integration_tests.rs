9d706d71b55ca254d1bbd87754533a60142d2a6a
David Sherret <dsherret@users.noreply.github.com>
2021-06-10 12:35:38 -0400

    chore: Fix broken test on Windows (#10900)

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -3115,7 +3115,7 @@ console.log("finish");
   });
 
   itest!(_089_run_allow_list {
-    args: "run --allow-run=cat 089_run_allow_list.ts",
+    args: "run --allow-run=curl 089_run_allow_list.ts",
     output: "089_run_allow_list.ts.out",
   });
 
