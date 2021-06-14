1b8d3f1080404136a3e1e79d7d27631bef8a42f5
Luca Casonato <hello@lcas.dev>
2021-06-14 13:39:55 +0200

    fmt

--- core/runtime.rs
+++ core/runtime.rs
@@ -1776,7 +1776,8 @@ pub mod tests {
 
   #[test]
   fn test_heap_limit_cb_multiple() {
-    let create_params = v8::Isolate::create_params().heap_limits(0, 3 * 1024 * 1024);
+    let create_params =
+      v8::Isolate::create_params().heap_limits(0, 3 * 1024 * 1024);
     let mut runtime = JsRuntime::new(RuntimeOptions {
       create_params: Some(create_params),
       ..Default::default()
