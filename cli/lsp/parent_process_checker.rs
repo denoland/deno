419fe2e6b4b77fbc97dee67eaa32a420accb8cfc
David Sherret <dsherret@users.noreply.github.com>
2021-06-17 21:48:16 -0400

    chore: fix new linting errors flagged by Rust 1.53 (#11029)

--- cli/lsp/parent_process_checker.rs
+++ cli/lsp/parent_process_checker.rs
@@ -62,9 +62,9 @@ mod test {
     let mut child = Command::new(deno_exe_path()).arg("lsp").spawn().unwrap();
 
     let pid = child.id();
-    assert_eq!(is_process_active(pid), true);
+    assert!(is_process_active(pid));
     child.kill().unwrap();
     child.wait().unwrap();
-    assert_eq!(is_process_active(pid), false);
+    assert!(!is_process_active(pid));
   }
 }
