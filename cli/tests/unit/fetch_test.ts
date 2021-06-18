35f6f9a17dee981a073c06c5651a43e5c24e00ae
zlumyo <zlumyo@gmail.com>
2021-06-18 14:09:10 +0300

    Fix unit-test

--- cli/tests/unit/fetch_test.ts
+++ cli/tests/unit/fetch_test.ts
@@ -1150,17 +1150,44 @@ unitTest({}, function fetchWritableRespProps(): void {
   assertEquals(new_.headers.get("x-deno"), "foo");
 });
 
+function returnHostHeaderServer(addr: string): void {
+  const [hostname, port] = addr.split(":");
+  const listener = Deno.listen({
+    hostname,
+    port: Number(port),
+  }) as Deno.Listener;
+
+  listener.accept().then(async (conn: Deno.Conn) => {
+    const httpConn = Deno.serveHttp(conn);
+
+    await httpConn.nextRequest()
+      .then(async (requestEvent: Deno.RequestEvent | null) => {
+        const hostHeader = requestEvent?.request.headers.get("Host");
+        const headersToReturn = hostHeader ? { "Host": hostHeader } : undefined;
+
+        await requestEvent?.respondWith(
+          new Response("", {
+            status: 200,
+            headers: headersToReturn,
+          }),
+        );
+      });
+
+    listener.close();
+  });
+}
+
 unitTest(
   { perms: { net: true } },
   async function fetchFilterOutCustomHostHeader(): Promise<
     void
   > {
-    const response = await fetch("http://localhost:4545/echo_server", {
+    const addr = "127.0.0.1:4502";
+    returnHostHeaderServer(addr);
+    const response = await fetch(`http://${addr}/`, {
       headers: { "Host": "example.com" },
     });
-    assertEquals(
-      response.headers.get("host"),
-      "localhost:4545",
-    );
+
+    assertEquals(response.headers.get("Host"), addr);
   },
 );
