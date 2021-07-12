00484d24ba93418bbdde8e42483d56e3714efaa0
Divy Srivastava <dj.srivastava23@gmail.com>
2021-07-12 18:15:36 +0530

    feat(extensions/crypto): implement verify() for RSA (#11312)

--- extensions/crypto/lib.deno_crypto.d.ts
+++ extensions/crypto/lib.deno_crypto.d.ts
@@ -111,6 +111,34 @@ interface SubtleCrypto {
       | DataView
       | ArrayBuffer,
   ): Promise<ArrayBuffer>;
+  verify(
+    algorithm: AlgorithmIdentifier | RsaPssParams,
+    key: CryptoKey,
+    signature:
+      | Int8Array
+      | Int16Array
+      | Int32Array
+      | Uint8Array
+      | Uint16Array
+      | Uint32Array
+      | Uint8ClampedArray
+      | Float32Array
+      | Float64Array
+      | DataView
+      | ArrayBuffer,
+    data:
+      | Int8Array
+      | Int16Array
+      | Int32Array
+      | Uint8Array
+      | Uint16Array
+      | Uint32Array
+      | Uint8ClampedArray
+      | Float32Array
+      | Float64Array
+      | DataView
+      | ArrayBuffer,
+  ): Promise<boolean>;
   digest(
     algorithm: AlgorithmIdentifier,
     data:
