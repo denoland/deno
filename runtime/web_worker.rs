ad01ff0c46721589c9b2237fe1a751b911a511bb
Divy Srivastava <dj.srivastava23@gmail.com>
2021-07-19 18:19:45 +0530

    fix type signature

--- runtime/web_worker.rs
+++ runtime/web_worker.rs
@@ -271,7 +271,7 @@ pub struct WebWorkerOptions {
   pub blob_store: BlobStore,
   pub broadcast_channel: InMemoryBroadcastChannel,
   pub shared_array_buffer_store: Option<SharedArrayBufferStore>,
-  pub cpu_count: u32,
+  pub cpu_count: usize,
 }
 
 impl WebWorker {
