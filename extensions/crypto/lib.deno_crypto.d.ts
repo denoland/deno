bee7a7df300bcef9b6a42b35635f59502e63552c
Divy Srivastava <dj.srivastava23@gmail.com>
2021-07-24 22:53:33 +0530

    use BufferSource for typings

--- extensions/crypto/lib.deno_crypto.d.ts
+++ extensions/crypto/lib.deno_crypto.d.ts
@@ -102,18 +102,7 @@ interface SubtleCrypto {
   ): Promise<CryptoKeyPair | CryptoKey>;
   importKey(
     format: "raw",
-    keyData:
-      | Int8Array
-      | Int16Array
-      | Int32Array
-      | Uint8Array
-      | Uint16Array
-      | Uint32Array
-      | Uint8ClampedArray
-      | Float32Array
-      | Float64Array
-      | DataView
-      | ArrayBuffer,
+    keyData: BufferSource,
     algorithm: AlgorithmIdentifier | HmacImportParams,
     extractable: boolean,
     keyUsages: KeyUsage[],
