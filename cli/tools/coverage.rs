b2fcd3d01458072265e5ce9bfe9e8ed34fe4c00b
Casper Beyer <caspervonb@pm.me>
2021-07-26 20:05:44 +0800

    fix(cli): side-load test modules (#11515)
    This fixes a regression introduced in 1.9 where test modules became main
    modules by side loading them in a generated module.

--- cli/tools/coverage.rs
+++ cli/tools/coverage.rs
@@ -653,7 +653,7 @@ fn filter_coverages(
     .filter(|e| {
       let is_internal = e.url.starts_with("deno:")
         || e.url.ends_with("__anonymous__")
-        || e.url.ends_with("$deno$test.ts");
+        || e.url.ends_with("$deno$test.js");
 
       let is_included = include.iter().any(|p| p.is_match(&e.url));
       let is_excluded = exclude.iter().any(|p| p.is_match(&e.url));
