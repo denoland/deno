bdcc65ab3b4cdd73c25cb5b8baed48c233856a93
Luca Casonato <hello@lcas.dev>
2021-08-07 23:28:38 +0200

    remove debug code

--- cli/tsc.rs
+++ cli/tsc.rs
@@ -948,7 +948,6 @@ mod tests {
     let actual = test_exec(&specifier)
       .await
       .expect("exec should not have errored");
-    eprintln!("diagnostics: {:?}", actual.diagnostics);
     assert!(actual.diagnostics.is_empty());
     assert!(actual.emitted_files.is_empty());
     assert!(actual.maybe_tsbuildinfo.is_some());
