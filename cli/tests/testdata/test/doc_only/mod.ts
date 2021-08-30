b72bfb345c5713ff3bb277e9db37fb51e1f618a0
Casper Beyer <caspervonb@pm.me>
2021-08-31 00:08:14 +0800

    Format

--- cli/tests/testdata/test/doc_only/mod.ts
+++ cli/tests/testdata/test/doc_only/mod.ts
@@ -3,6 +3,8 @@
  * import "./mod.ts";
  * ```
  */
-Deno.test("unreachable", function() {
-  throw new Error("modules that don't end with _test are scanned for documentation tests only should not be executed");
+Deno.test("unreachable", function () {
+  throw new Error(
+    "modules that don't end with _test are scanned for documentation tests only should not be executed",
+  );
 });
