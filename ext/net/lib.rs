8883715d4a181809ffbf21e96cfcc8ebf31ae9c5
Satya Rohith <me@satyarohith.com>
2021-09-20 10:37:14 +0000

    fix

--- ext/net/lib.rs
+++ ext/net/lib.rs
@@ -94,13 +94,7 @@ pub struct DefaultTlsOptions {
 /// using type alias for a `Option<Vec<String>>` could work, but there's a high chance
 /// that there might be another type alias pointing to a `Option<Vec<String>>`, which
 /// would override previously used alias.
-pub struct UnsafelyIgnoreCertificateErrors(Option<Vec<String>>);
-
-impl Default for UnsafelyIgnoreCertificateErrors {
-  fn default() -> Self {
-    Self(None)
-  }
-}
+pub struct UnsafelyIgnoreCertificateErrors(pub Option<Vec<String>>);
 
 pub fn init<P: NetPermissions + 'static>(
   root_cert_store: Option<RootCertStore>,
