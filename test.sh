df132bad1789dfcb967c00a47ffbac6bd6a02ad9
Casper Beyer <caspervonb@pm.me>
2021-09-21 19:53:38 +0800

    Remove temporary test fixtures

--- test.sh
+++
@@ -1,8 +0,0 @@
-# DO NOT MERGE IF THIS IS HERE
-# Helper script to make iterating on this less painful with some new testdata.
-# These new fixtures will replace the old ones.
-rm -rf cov
-mkdir cov
-
-target/debug/deno test --coverage=cov $1
-target/debug/deno coverage cov
--- testdata/end_with_new_line.ts
+++
@@ -1,6 +0,0 @@
-export default function fn() {
-  return true;
-}
-
-fn();
-
