5c7a0751e24b2f66f1d0cc81cba3a90bc5f44443
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-10-02 19:05:55 +0200

    don't store url in static

--- cli/compat.rs
+++ cli/compat.rs
@@ -2,9 +2,6 @@
 
 use std::collections::HashMap;
 
-// TODO(bartlomieju): this is unversioned, and should be fixed to use latest stable?
-static DENO_STD_URL: &str = "https://deno.land/std/node/";
-
 static SUPPORTED_MODULES: &[&str] = &[
   "assert",
   "assert/strict",
@@ -41,7 +38,9 @@ pub fn get_mapped_node_builtins() -> HashMap<String, String> {
   let mut mappings = HashMap::new();
 
   for module in SUPPORTED_MODULES {
-    let module_url = format!("{}{}.ts", DENO_STD_URL, module);
+    // TODO(bartlomieju): this is unversioned, and should be fixed to use latest stable?
+    let module_url =
+      format!("https://deno.land/std/node/{}.ts", DENO_STD_URL, module);
     mappings.insert(module.to_string(), module_url);
   }
 
