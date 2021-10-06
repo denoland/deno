d6fb695bf7d5764199a9c3078899f5cb695faa8e
Divy Srivastava <dj.srivastava23@gmail.com>
2021-10-06 13:21:57 +0000

    fix

--- ext/crypto/lib.rs
+++ ext/crypto/lib.rs
@@ -2,6 +2,7 @@
 
 use deno_core::error::custom_error;
 use deno_core::error::not_supported;
+use deno_core::error::null_opbuf;
 use deno_core::error::type_error;
 use deno_core::error::AnyError;
 use deno_core::include_js_files;
@@ -803,11 +804,12 @@ pub struct DeriveKeyArg {
 pub async fn op_crypto_derive_bits(
   _state: Rc<RefCell<OpState>>,
   args: DeriveKeyArg,
-  zero_copy: ZeroCopyBuf,
+  zero_copy: Option<ZeroCopyBuf>,
 ) -> Result<ZeroCopyBuf, AnyError> {
   let algorithm = args.algorithm;
   match algorithm {
     Algorithm::Pbkdf2 => {
+      let zero_copy = zero_copy.ok_or_else(null_opbuf)?;
       let salt = &*zero_copy;
       // The caller must validate these cases.
       assert!(args.length > 0);
@@ -856,6 +858,7 @@ pub async fn op_crypto_derive_bits(
       }
     }
     Algorithm::Hkdf => {
+      let zero_copy = zero_copy.ok_or_else(null_opbuf)?;
       let salt = &*zero_copy;
       let algorithm = match args.hash.ok_or_else(not_supported)? {
         CryptoHash::Sha1 => hkdf::HKDF_SHA1_FOR_LEGACY_USE_ONLY,
