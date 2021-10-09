780e60843e21c65310bd632c8cb5ffc2346fed54
MierenManz <christiaanmens@gmail.com>
2021-10-09 17:19:34 +0200

    dedupe code

--- ext/fetch/27_eventsource.js
+++ ext/fetch/27_eventsource.js
@@ -7,18 +7,13 @@
   const { URL } = window.__bootstrap.url;
   const { EventTarget } = window.__bootstrap.eventTarget;
   const {
-    FunctionPrototypeCall,
-    MapPrototypeGet,
-    MapPrototypeSet,
     ObjectDefineProperties,
-    ObjectDefineProperty,
     Symbol,
     StringPrototypeStartsWith,
     Promise,
     isNaN,
     isFinite,
     Number,
-    Map,
     StringPrototypeTrim,
     StringPrototypeSlice,
     StringPrototypeReplaceAll,
@@ -33,45 +28,7 @@
   const { getLocationHref } = window.__bootstrap.location;
   const { TextDecoderStream } = window.__bootstrap.encoding;
   const { fetch } = window.__bootstrap.fetch;
-
-  const handlerSymbol = Symbol("eventHandlers");
-  function makeWrappedHandler(handler) {
-    function wrappedHandler(...args) {
-      if (typeof wrappedHandler.handler !== "function") {
-        return;
-      }
-      return FunctionPrototypeCall(wrappedHandler.handler, this, ...args);
-    }
-    wrappedHandler.handler = handler;
-    return wrappedHandler;
-  }
-
-  function defineEventHandler(emitter, name) {
-    // HTML specification section 8.1.5.1
-    ObjectDefineProperty(emitter, `on${name}`, {
-      get() {
-        if (!this[handlerSymbol]) {
-          return null;
-        }
-        return MapPrototypeGet(this[handlerSymbol], name)?.handler;
-      },
-      set(value) {
-        if (!this[handlerSymbol]) {
-          this[handlerSymbol] = new Map();
-        }
-        let handlerWrapper = MapPrototypeGet(this[handlerSymbol], name);
-        if (handlerWrapper) {
-          handlerWrapper.handler = value;
-        } else {
-          handlerWrapper = makeWrappedHandler(value);
-          this.addEventListener(name, handlerWrapper);
-        }
-        MapPrototypeSet(this[handlerSymbol], name, handlerWrapper);
-      },
-      configurable: true,
-      enumerable: true,
-    });
-  }
+  const { defineEventHandler } = window.__bootstrap.event;
 
   webidl.converters["EventSourceInit"] = webidl.createDictionaryConverter(
     "EventSourceInit",
