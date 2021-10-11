9200bb2a744c56711bb46e20eebffb6233cbd789
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-10-12 00:53:54 +0200

    parse_package_name

--- cli/node_module_loader.rs
+++ cli/node_module_loader.rs
@@ -1,76 +1,66 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
-use crate::module_graph::TypeLib;
-use crate::proc_state::ProcState;
+use deno_core::error::generic_error;
 use deno_core::error::AnyError;
-use deno_core::futures::future::FutureExt;
-use deno_core::futures::Future;
-use deno_core::ModuleLoadId;
-use deno_core::ModuleLoader;
+use deno_core::url::Url;
 use deno_core::ModuleSpecifier;
-use deno_core::OpState;
-use deno_runtime::permissions::Permissions;
-use import_map::ImportMap;
-use std::cell::RefCell;
-use std::pin::Pin;
-use std::rc::Rc;
-use std::str;
 
 /// This function is an implementation of `defaultResolve` in
 /// `lib/internal/modules/esm/resolve.js` from Node.
-fn node_resolve(
-  &self,
-  specifier: &str,
-  referrer: &str,
-  is_main: bool,
-) -> Result<ModuleSpecifier, AnyError> {
-  // TODO(bartlomieju): shipped "policy" part
+// fn node_resolve(
+//   specifier: &str,
+//   referrer: &str,
+//   is_main: bool,
+// ) -> Result<ModuleSpecifier, AnyError> {
+//   // TODO(bartlomieju): shipped "policy" part
 
-  if let Ok(url) = Url::parse(specifier) {
-    if url.scheme() == "data:" {
-      return Ok(url);
-    }
+//   if let Ok(url) = Url::parse(specifier) {
+//     if url.scheme() == "data:" {
+//       return Ok(url);
+//     }
 
-    let protocol = url.protocol();
+//     let protocol = url.scheme();
 
-    if protocol == "node:" {
-      return Ok(url);
-    }
+//     if protocol == "node" {
+//       return Ok(url);
+//     }
 
-    if protocol != "file:" && protocol != "data:" {
-      return Err(generic_error(format!("Only file and data URLs are supported by the default ESM loader. Received protocol '{}'", protocol)));
-    }
+//     if protocol != "file" && protocol != "data" {
+//       return Err(generic_error(format!("Only file and data URLs are supported by the default ESM loader. Received protocol '{}'", protocol)));
+//     }
 
-    // In Deno there's no way to expose internal Node modules anyway,
-    // so calls to NativeModule.canBeRequiredByUsers would only work for built-in modules.
+//     // In Deno there's no way to expose internal Node modules anyway,
+//     // so calls to NativeModule.canBeRequiredByUsers would only work for built-in modules.
 
-    if referrer.starts_with("data:") {
-      return Url::parse(specifier, referrer).map_err(AnyError::from);
-    }
+//     if referrer.starts_with("data:") {
+//       let referrer_url = Url::parse(referrer)?;
+//       return referrer_url.join(specifier).map_err(AnyError::from);
+//     }
 
-    let referrer = if is_main {
-      // path_to_file_url()
-      referrer
-    } else {
-      referrer
-    };
+//     let referrer = if is_main {
+//       // path_to_file_url()
+//       referrer
+//     } else {
+//       referrer
+//     };
 
-    let url = module_resolve(specifier, referrer)?;
+//     let url = module_resolve(specifier, referrer)?;
 
-    // TODO: check codes
+//     // TODO: check codes
 
-    Ok(url)
-  }
+//     Ok(url)
+//   }
 
-  Ok(module_specifier)
-}
+//   // Ok(module_specifier)
+//   todo!()
+// }
 
 fn should_be_treated_as_relative_or_absolute_path(specifier: &str) -> bool {
   if specifier == "" {
     return false;
   }
 
-  if specifier[0] == "/" {
+  if specifier.chars().nth(0) == Some('/') {
     return true;
   }
 
@@ -78,12 +68,15 @@ fn should_be_treated_as_relative_or_absolute_path(specifier: &str) -> bool {
 }
 
 fn is_relative_specifier(specifier: &str) -> bool {
-  if specifier[0] == "." {
-    if specifier.len() == 1 || specifier[1] == "/" {
+  let specifier_len = specifier.len();
+  let mut specifier_chars = specifier.chars();
+
+  if specifier_chars.nth(0) == Some('.') {
+    if specifier_len == 1 || specifier_chars.nth(1) == Some('/') {
       return true;
     }
-    if specifier[1] == "." {
-      if specifier.len() == 2 || specifier[2] == "/" {
+    if specifier_chars.nth(1) == Some('.') {
+      if specifier_len == 2 || specifier_chars.nth(2) == Some('/') {
         return true;
       }
     }
@@ -91,37 +84,91 @@ fn is_relative_specifier(specifier: &str) -> bool {
   false
 }
 
-fn module_resolve(specifier: &str, base: &str) {
-  let resolved = if should_be_treated_as_relative_or_absolute_path(specifier) {
-    Url::parse(specifier, base)
-    // TODO(bartlomieju): check len, can panic
-  } else if specifier[0] == "#" {
-    package_imports_resolve(specifier, base)
+// fn module_resolve(
+//   specifier: &str,
+//   base: &str,
+// ) -> Result<ModuleSpecifier, AnyError> {
+//   let resolved = if should_be_treated_as_relative_or_absolute_path(specifier) {
+//     Url::parse(specifier, base)
+//     // TODO(bartlomieju): check len, can panic
+//   } else if specifier[0] == "#" {
+//     package_imports_resolve(specifier, base)
+//   } else {
+//     if let Ok(resolved) = Url::parse(specifier) {
+//       resolved
+//     } else {
+//       package_resolve(specifier, base)
+//     }
+//   };
+//   finalize_resolution(resolved, base)
+// }
+
+// fn finalize_resolution(resolved: &str, base: &str) {
+//   todo!()
+// }
+
+// fn package_imports_resolve(specifier: &str, base: &str) {
+//   todo!()
+// }
+
+// fn package_resolve(
+//   specifier: &str,
+//   base: &str,
+// ) -> Result<ModuleSpecifier, AnyError> {
+//   let (package_name, package_subpath, is_scoped) =
+//     parse_package_name(specifier, base);
+
+//   todo!()
+// }
+
+fn parse_package_name(
+  specifier: &str,
+  base: &str,
+) -> Result<(String, String, bool), AnyError> {
+  let mut separator_index = specifier.find('/');
+  let mut valid_package_name = false;
+  let mut is_scoped = false;
+  if specifier.is_empty() {
+    valid_package_name = false;
   } else {
-    if let Ok(resolved) = Url::parse(specifier) {
-      resolved
-    } else {
-      package_resolve(specifier, base)
+    if specifier.chars().nth(0) == Some('@') {
+      is_scoped = true;
+      if let Some(index) = separator_index {
+        separator_index = specifier[index + 1..].find('/');
+      } else {
+        valid_package_name = false;
+      }
     }
-  };
-  finalize_resolution(resolved, base)
-}
-
-fn finalize_resolution(resolved: &str, base: &str) {
-  todo!()
-}
-
-fn package_imports_resolve(specifier: &str, base: &str) {
-  todo!()
-}
-
-fn package_resolve(specifier: &str, base: &str) -> Result<ModuleSpecifier, AnyError> {
-  let (package_name, package_subpath, is_scoped) = parse_package_name(specifier, base);
-
-  todo!()
-}
-
-fn parse_package_name(specifier: &str, base: &str) -> (&str, &str, &str) {
-    todo!()
   }
-  
+
+  let package_name = if let Some(index) = separator_index {
+    specifier[0..index].to_string()
+  } else {
+    specifier.to_string()
+  };
+
+  // Package name cannot have leading . and cannot have percent-encoding or separators.
+  for ch in package_name.chars() {
+    if ch == '%' || ch == '\\' {
+      valid_package_name = false;
+      break;
+    }
+  }
+
+  if !valid_package_name {
+    // TODO(bartlomieju): apply fileURLToPath
+    return Err(generic_error(format!(
+      "{} is not a valid package name {}",
+      specifier, base
+    )));
+  }
+
+  let package_subpath = if let Some(index) = separator_index {
+    format!(".{}", specifier.chars().skip(index).collect::<String>())
+      .to_string()
+  } else {
+    ".".to_string()
+  };
+
+  Ok((package_name, package_subpath, is_scoped))
+}
