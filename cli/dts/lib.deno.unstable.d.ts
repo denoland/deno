cf9c4f0031a46b58989a984af0528a2005547e2d
Bert Belder <bertbelder@gmail.com>
2021-10-26 22:27:47 +0200

    feat(ext/net): add TlsConn.handshake() (#12467)
    A `handshake()` method was added that returns when the TLS handshake is
    complete. The `TlsListener` and `TlsConn` interfaces were added to
    accomodate this new method.
    
    Closes: #11759.

--- cli/dts/lib.deno.unstable.d.ts
+++ cli/dts/lib.deno.unstable.d.ts
@@ -1091,7 +1091,7 @@ declare namespace Deno {
    *
    * Requires `allow-net` permission.
    */
-  export function connectTls(options: ConnectTlsOptions): Promise<Conn>;
+  export function connectTls(options: ConnectTlsOptions): Promise<TlsConn>;
 
   export interface StartTlsOptions {
     /** A literal IP address or host name that can be resolved to an IP address.
@@ -1130,7 +1130,7 @@ declare namespace Deno {
   export function startTls(
     conn: Conn,
     options?: StartTlsOptions,
-  ): Promise<Conn>;
+  ): Promise<TlsConn>;
 
   export interface ListenTlsOptions {
     /** **UNSTABLE**: new API, yet to be vetted.
