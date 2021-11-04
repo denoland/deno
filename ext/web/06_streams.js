1d9a0bbe43f61b8e15517c878d68936faa464d71
crowlkats <crowlkats@toaxl.com>
2021-11-04 20:51:55 +0100

    remove getIterator

--- ext/web/06_streams.js
+++ ext/web/06_streams.js
@@ -4212,15 +4212,6 @@
     }
 
     /**
-     * @deprecated TODO(@kitsonk): Remove in Deno 1.8
-     * @param {ReadableStreamIteratorOptions=} options
-     * @returns {AsyncIterableIterator<R>}
-     */
-    getIterator(options = {}) {
-      return this[SymbolAsyncIterator](options);
-    }
-
-    /**
      * @param {ReadableStreamGetReaderOptions=} options
      * @returns {ReadableStreamDefaultReader<R> | ReadableStreamBYOBReader}
      */
