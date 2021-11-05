cc3992b3540e4478a2dd32cacc33d26496d2bf20
crowlkats <crowlkats@toaxl.com>
2021-11-05 03:03:41 +0100

    add WritableStreamDefaultController.signal

--- ext/web/06_streams.js
+++ ext/web/06_streams.js
@@ -9,7 +9,7 @@
 
 ((window) => {
   const webidl = window.__bootstrap.webidl;
-  // TODO(lucacasonato): get AbortSignal from __bootstrap.
+  const { add, signalAbort, newSignal } = window.__bootstrap.abortSignal;
   const {
     ArrayBuffer,
     ArrayBufferIsView,
@@ -286,6 +286,7 @@
   const _readRequests = Symbol("[[readRequests]]");
   const _readIntoRequests = Symbol("[[readIntoRequests]]");
   const _readyPromise = Symbol("[[readyPromise]]");
+  const _signal = Symbol("[[signal]]");
   const _started = Symbol("[[started]]");
   const _state = Symbol("[[state]]");
   const _storedError = Symbol("[[storedError]]");
@@ -1902,8 +1903,7 @@
         abortAlgorithm();
         return promise.promise;
       }
-      // TODO(lucacasonato): use the internal API to listen for abort.
-      signal.addEventListener("abort", abortAlgorithm);
+      signal[add](abortAlgorithm);
     }
 
     function pipeLoop() {
@@ -2107,8 +2107,7 @@
       readableStreamReaderGenericRelease(reader);
 
       if (signal !== undefined) {
-        // TODO(lucacasonato): use the internal API to remove the listener.
-        signal.removeEventListener("abort", abortAlgorithm);
+        signal[add](abortAlgorithm);
       }
       if (isError) {
         promise.reject(error);
@@ -2985,6 +2984,7 @@
     controller[_stream] = stream;
     stream[_controller] = controller;
     resetQueue(controller);
+    controller[_signal] = newSignal();
     controller[_started] = false;
     controller[_strategySizeAlgorithm] = sizeAlgorithm;
     controller[_strategyHWM] = highWaterMark;
@@ -3339,6 +3339,10 @@
    */
   function writableStreamAbort(stream, reason) {
     const state = stream[_state];
+    if (state === "closed" || state === "errored") {
+      return resolvePromiseWith(undefined);
+    }
+    stream[_controller][_signal][signalAbort]();
     if (state === "closed" || state === "errored") {
       return resolvePromiseWith(undefined);
     }
@@ -5430,6 +5434,13 @@
     [_stream];
     /** @type {(chunk: W, controller: this) => Promise<void>} */
     [_writeAlgorithm];
+    /** @type {AbortSignal} */
+    [_signal];
+
+    get signal() {
+      webidl.assertBranded(this, WritableStreamDefaultController);
+      return this[_signal];
+    }
 
     constructor() {
       webidl.illegalConstructor();
