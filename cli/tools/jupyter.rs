0f6e0d5b2aabdd1c9038b88e03ccc119d8d474fa
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-12-09 01:47:49 +0100

    add install command

--- cli/tools/jupyter.rs
+++ cli/tools/jupyter.rs
@@ -3,9 +3,54 @@
 use crate::flags::Flags;
 use crate::flags::JupyterFlags;
 use deno_core::error::AnyError;
+use deno_core::serde_json;
+use deno_core::serde_json::json;
+use tempfile::TempDir;
 
 pub fn install() -> Result<(), AnyError> {
-  unimplemented!();
+  let temp_dir = TempDir::new().unwrap();
+  let kernel_json_path = temp_dir.path().join("kernel.json");
+
+  // TODO(bartlomieju): add remaining fields as per
+  // https://jupyter-client.readthedocs.io/en/stable/kernels.html#kernel-specs
+  let json_data = json!({
+      "argv": ["deno", "jupyter", "--conn", "{connection_file}"],
+      "display_name": "Deno",
+      "language": "typescript",
+  });
+
+  let f = std::fs::File::create(kernel_json_path)?;
+  serde_json::to_writer_pretty(f, &json_data)?;
+
+  let child_result = std::process::Command::new("jupyter")
+    .args([
+      "kernelspec",
+      "install",
+      "--name",
+      "deno",
+      &temp_dir.path().to_string_lossy(),
+    ])
+    .spawn();
+
+  // TODO(bartlomieju): copy icons the the kernelspec directory
+
+  if let Ok(mut child) = child_result {
+    let wait_result = child.wait();
+    match wait_result {
+      Ok(status) => {
+        if !status.success() {
+          eprintln!("Failed to install kernelspec, try again.");
+        }
+      }
+      Err(err) => {
+        eprintln!("Failed to install kernelspec: {}", err);
+      }
+    }
+  }
+
+  let _ = std::fs::remove_dir(temp_dir);
+  println!("Deno kernelspec installed successfully.");
+  Ok(())
 }
 
 pub fn kernel(
