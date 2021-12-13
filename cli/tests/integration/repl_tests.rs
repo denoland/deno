561288e0986ed32247970ffc4e4d6a662a818163
David Sherret <dsherret@gmail.com>
2021-12-13 16:30:34 -0500

    Add test using relative paths

--- cli/tests/integration/repl_tests.rs
+++ cli/tests/integration/repl_tests.rs
@@ -123,6 +123,22 @@ fn pty_complete_primitives() {
 }
 
 #[test]
+fn pty_complete_imports() {
+  util::with_pty(&["repl"], |mut console| {
+    // single quotes
+    console.write_line("import './001_hel\t'");
+    // double quotes
+    console.write_line("import { output } from \"./045_out\t\"");
+    console.write_line("output('testing output');");
+    console.write_line("close();");
+
+    let output = console.read_all_output();
+    assert!(output.contains("Hello World"));
+    assert!(output.contains("\ntesting output"));
+  });
+}
+
+#[test]
 fn pty_ignore_symbols() {
   util::with_pty(&["repl"], |mut console| {
     console.write_line("Array.Symbol\t");
