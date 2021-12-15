b026f460783fb6a5d5ba7e22e24411e5d1be72ab
Jesper van den Ende <jespertheend@users.noreply.github.com>
2021-12-15 01:27:33 +0100

    Change —help text
    Co-authored-by: Bartek Iwańczuk <biwanczuk@gmail.com>

--- cli/flags.rs
+++ cli/flags.rs
@@ -1651,8 +1651,7 @@ fn watch_arg<'a, 'b>() -> Arg<'a, 'b> {
     .help("UNSTABLE: Watch for file changes and restart process automatically")
     .long_help(
       "UNSTABLE: Watch for file changes and restart process automatically.
-Watches used script files by default, or listed files when specified.
-Passing values only has an effect on the 'deno run' command.",
+Local files from entry point module graph are watched by default. Additional paths might be watched by passing them as arguments to this flag.
     )
 }
 
