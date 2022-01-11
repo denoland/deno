219afc487731bcb5fc7d6a08b95ccfd5ad7f0be1
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-01-11 21:17:25 +0100

    fix(coverage): merge coverage ranges (#13334)
    Covered ranges were not merged and thus it appeared that some lines
    might be uncovered. To fix this I used "v8-coverage" that takes care
    of merging the ranges properly. With this change, coverage collected
    from a file by multiple entrypoints is now correctly calculated.
    
    I ended up forking https://github.com/demurgos/v8-coverage and adding
    "cli/tools/coverage/merge.rs" and "cli/tools/coverage/range_tree.rs".

--- cli/tests/integration/coverage_tests.rs
+++ cli/tests/integration/coverage_tests.rs
@@ -97,3 +97,83 @@ fn run_coverage_text(test_name: &str, extension: &str) {
 
   assert!(output.status.success());
 }
+
+#[test]
+fn multifile_coverage() {
+  let deno_dir = TempDir::new().expect("tempdir fail");
+  let tempdir = TempDir::new().expect("tempdir fail");
+  let tempdir = tempdir.path().join("cov");
+
+  let status = util::deno_cmd_with_deno_dir(deno_dir.path())
+    .current_dir(util::testdata_path())
+    .arg("test")
+    .arg("--quiet")
+    .arg("--unstable")
+    .arg(format!("--coverage={}", tempdir.to_str().unwrap()))
+    .arg("coverage/multifile/")
+    .stdout(std::process::Stdio::piped())
+    .stderr(std::process::Stdio::inherit())
+    .status()
+    .expect("failed to spawn test runner");
+
+  assert!(status.success());
+
+  let output = util::deno_cmd_with_deno_dir(deno_dir.path())
+    .current_dir(util::testdata_path())
+    .arg("coverage")
+    .arg("--unstable")
+    .arg(format!("{}/", tempdir.to_str().unwrap()))
+    .stdout(std::process::Stdio::piped())
+    .stderr(std::process::Stdio::piped())
+    .output()
+    .expect("failed to spawn coverage reporter");
+
+  // Verify there's no "Check" being printed
+  assert!(output.stderr.is_empty());
+
+  let actual =
+    util::strip_ansi_codes(std::str::from_utf8(&output.stdout).unwrap())
+      .to_string();
+
+  let expected = fs::read_to_string(
+    util::testdata_path().join("coverage/multifile/expected.out"),
+  )
+  .unwrap();
+
+  if !util::wildcard_match(&expected, &actual) {
+    println!("OUTPUT\n{}\nOUTPUT", actual);
+    println!("EXPECTED\n{}\nEXPECTED", expected);
+    panic!("pattern match failed");
+  }
+
+  assert!(output.status.success());
+
+  let output = util::deno_cmd_with_deno_dir(deno_dir.path())
+    .current_dir(util::testdata_path())
+    .arg("coverage")
+    .arg("--quiet")
+    .arg("--unstable")
+    .arg("--lcov")
+    .arg(format!("{}/", tempdir.to_str().unwrap()))
+    .stdout(std::process::Stdio::piped())
+    .stderr(std::process::Stdio::inherit())
+    .output()
+    .expect("failed to spawn coverage reporter");
+
+  let actual =
+    util::strip_ansi_codes(std::str::from_utf8(&output.stdout).unwrap())
+      .to_string();
+
+  let expected = fs::read_to_string(
+    util::testdata_path().join("coverage/multifile/expected.lcov"),
+  )
+  .unwrap();
+
+  if !util::wildcard_match(&expected, &actual) {
+    println!("OUTPUT\n{}\nOUTPUT", actual);
+    println!("EXPECTED\n{}\nEXPECTED", expected);
+    panic!("pattern match failed");
+  }
+
+  assert!(output.status.success());
+}
