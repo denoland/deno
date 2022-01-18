e7dbdd09e3c4a3e8e8feffed598839613465202b
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-01-18 01:36:24 +0100

    wgpu sync to 58771175290bca7b9db546b3451d32fab2dc5d7b

--- tools/wgpu_sync.js
+++ tools/wgpu_sync.js
@@ -4,7 +4,7 @@
 import { join, ROOT_PATH } from "./util.js";
 
 // const COMMIT = "c00e471274b6c21acda89b4b13d41742c0285d71"; // Release 12
-const COMMIT = "0183e7d1e85ac95f2461426a910b2f86f5373119"; // tip
+const COMMIT = "58771175290bca7b9db546b3451d32fab2dc5d7b"; // tip
 const REPO = "gfx-rs/wgpu";
 // const V_WGPU = "0.12.0";
 const V_DENO_CORE = "0.114.0";
