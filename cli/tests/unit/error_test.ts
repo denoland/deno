ee51c3ddd90f6f4e852a65895519768c8a815116
Yoshiya Hinosawa <stibium121@gmail.com>
2022-01-20 12:29:37 +0900

    fix(cli/dts): add NotSupported error type (#13432)

---
+++ cli/tests/unit/error_test.ts
@@ -0,0 +1,24 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+import { assert } from "./test_util.ts";
+
+Deno.test("Errors work", () => {
+  assert(new Deno.errors.NotFound("msg") instanceof Error);
+  assert(new Deno.errors.PermissionDenied("msg") instanceof Error);
+  assert(new Deno.errors.ConnectionRefused("msg") instanceof Error);
+  assert(new Deno.errors.ConnectionReset("msg") instanceof Error);
+  assert(new Deno.errors.ConnectionAborted("msg") instanceof Error);
+  assert(new Deno.errors.NotConnected("msg") instanceof Error);
+  assert(new Deno.errors.AddrInUse("msg") instanceof Error);
+  assert(new Deno.errors.AddrNotAvailable("msg") instanceof Error);
+  assert(new Deno.errors.BrokenPipe("msg") instanceof Error);
+  assert(new Deno.errors.AlreadyExists("msg") instanceof Error);
+  assert(new Deno.errors.InvalidData("msg") instanceof Error);
+  assert(new Deno.errors.TimedOut("msg") instanceof Error);
+  assert(new Deno.errors.Interrupted("msg") instanceof Error);
+  assert(new Deno.errors.WriteZero("msg") instanceof Error);
+  assert(new Deno.errors.UnexpectedEof("msg") instanceof Error);
+  assert(new Deno.errors.BadResource("msg") instanceof Error);
+  assert(new Deno.errors.Http("msg") instanceof Error);
+  assert(new Deno.errors.Busy("msg") instanceof Error);
+  assert(new Deno.errors.NotSupported("msg") instanceof Error);
+});
