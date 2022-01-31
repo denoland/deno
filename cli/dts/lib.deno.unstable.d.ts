fb60115672d45dead4b15589f83132f29a2a54c3
Yoshiya Hinosawa <stibium121@gmail.com>
2022-01-31 14:44:19 +0900

    feat(unstable): add Deno.getUid (#13496)

--- cli/dts/lib.deno.unstable.d.ts
+++ cli/dts/lib.deno.unstable.d.ts
@@ -133,6 +133,18 @@ declare namespace Deno {
    */
   export function networkInterfaces(): NetworkInterfaceInfo[];
 
+  /** **Unstable** new API. yet to be vetted.
+   *
+   * Returns the user id of the process on POSIX platforms. Returns null on windows.
+   *
+   * ```ts
+   * console.log(Deno.getUid());
+   * ```
+   *
+   * Requires `allow-env` permission.
+   */
+  export function getUid(): number | null;
+
   /** All possible types for interfacing with foreign functions */
   export type NativeType =
     | "void"
