9da2ca0e190a1d9624a447f84eb2fcaf644eac00
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-02-07 04:37:38 +0100

    temp

---
+++ cli/compat/cjs.rs
@@ -0,0 +1,112 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+
+use std::path::PathBuf;
+
+struct Module {
+    id: String,
+    // path: PathBuf,
+    // exports:
+    // module_parent_cache:
+    filename: Option<PathBuf>,
+    loaded: bool,
+    // children: Vec<Module?>
+}
+
+impl Module {
+    fn new(id: &str, /*parent: Option<>*/) -> Self {
+        Self {
+            id: id.to_string(),
+            filename: None,
+            loaded: false,
+        }
+    }
+
+    fn _resolve_lookup_paths(request: &str, parent: Option<&Module>) -> Vec<String> {
+        // if NativeModule.can_be_required_by_users(request) {
+        //     return vec![];
+        // }
+
+        // Check for node modules paths.
+        let request_chars = request.chars();
+        if let Some(char_) = request_chars.nth(0) {
+            // if char_ != '.'
+        }
+
+        // In REPL, parent.filename is None
+        let mut no_parent = false;
+        if parent.is_none() {
+            no_parent = true;
+        } else if let Some(parent) = parent {
+            if parent.id.is_empty() || parent.filename.is_none() {
+                no_parent = true;
+            }
+        }
+        if no_parent {
+            // Make require('./path/to/foo') work - normally the path is taken
+            // from realpath(__filename) but in REPL the is no filename
+            let main_paths = vec![".".to_string()];
+            return main_paths;
+        }
+
+        let parent = parent.unwrap();
+        let filename = parent.filename.unwrap();
+
+        let parent_dir = vec![
+            parent.filename.unwrap().parent().to_string_lossy()
+        ];
+        return parent_dir;
+    }
+
+    fn _resolve_filename(request: &str, parent: Option<&Module>, is_main: bool, options: Option<()>) -> String {
+        if request.starts_with("node:") /*|| NativeModule.can_be_required_by_users(request)*/ {
+            return request.to_string();
+        }
+
+        let mut paths = vec![];
+
+        if let Some(opts) = options {
+            todo!()
+        } else {
+            paths = Module::_resolve_lookup_paths(request, parent);
+        }
+
+        // if let Some(parent) = parent {
+        //     if let Some(prent_filename) = parent.filename {
+        //         if request[0] == '#' {
+        //             let pkg = read_package_scope(parent_filename).unwrap_or_default();
+        //             if pkg.data.imports.is_some() {
+
+        //             }
+        //         }
+        //     }
+        // }
+
+        // Try module self resolution first
+        // let parent_path = try_self_parent_path(parent);
+        // let self_resolved = try_self(parent_path, request);
+        // if let Some(self_resolved) = self_resolved {
+        //     let cache_key = format!("{}\x00{}", request, if paths.len() == 1 {
+        //         paths[0]
+        //     } else {
+        //         paths.join("\x00")
+        //     });
+        //     Module::_path_cache[cache_key] = self_resolved;
+        //     return self_resolved;
+        // }
+
+        // // Look up the filename first, since that's the cache key.
+        // let filename = Module::_find_path(request, paths, is_main, false);
+        // if let Some(filename) = filename {
+        //     return filename;
+        // }
+
+        // let mut require_stack = vec![];
+
+        // loop {
+        //     let cursor = parent;
+        //     //
+        // }
+
+        todo!();
+    }
+}
