d2f2bdb20fe2f9a5e206dfdbb1260e5f34461228
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-02-09 14:33:07 +0000

    fmt

--- cli/napi/napi_is_date.rs
+++ cli/napi/napi_is_date.rs
@@ -1,7 +1,11 @@
 use deno_core::napi::*;
 
 #[napi_sym::napi_sym]
-fn napi_is_date(_env: napi_env, value: napi_value, result: *mut bool) -> Result {
+fn napi_is_date(
+  _env: napi_env,
+  value: napi_value,
+  result: *mut bool,
+) -> Result {
   let value: v8::Local<v8::Value> = transmute(value);
   *result = value.is_date();
   Ok(())
