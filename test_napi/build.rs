0ba5f8037a7f25c659152914809453413296a37f
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-02-09 15:05:29 +0000

    fix(test_napi): use napi-build

--- test_napi/build.rs
+++ test_napi/build.rs
@@ -1,6 +1,5 @@
+extern crate napi_build;
+
 fn main() {
-  // macOS
-  println!("cargo:rustc-cdylib-link-arg=-Wl");
-  println!("cargo:rustc-cdylib-link-arg=-undefined");
-  println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
+  napi_build::setup();
 }
