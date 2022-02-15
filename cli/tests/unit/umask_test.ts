e04ab1529517c595bfc6bbcdc4851ad8b38bcf06
Yoshiya Hinosawa <stibium121@gmail.com>
2022-02-15 13:21:37 +0900

    test: clean up after assertion

--- cli/tests/unit/umask_test.ts
+++ cli/tests/unit/umask_test.ts
@@ -8,10 +8,14 @@ Deno.test(
   async function umaskSuccess() {
     const prevMask = Deno.umask(0o023);
     const tmpDir = await Deno.makeTempDir();
-    const targetDir = tmpDir + "/foo";
-    await Deno.mkdir(targetDir, { mode: 0o777 });
-    const stat = await Deno.lstat(targetDir);
-    assertEquals(stat.mode! & 0o777, 0o754);
-    Deno.umask(prevMask);
+    try {
+      const targetDir = tmpDir + "/foo";
+      await Deno.mkdir(targetDir, { mode: 0o777 });
+      const stat = await Deno.lstat(targetDir);
+      assertEquals(stat.mode! & 0o777, 0o754);
+    } finally {
+      Deno.remove(tmpDir, { recursive: true });
+      Deno.umask(prevMask);
+    }
   },
 );
