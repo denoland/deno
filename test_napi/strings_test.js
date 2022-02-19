ac83b1ecbc8c2dfb8ea0fc99ce06477af53027cc
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-02-19 07:04:04 +0100

    temp

--- test_napi/strings_test.js
+++ test_napi/strings_test.js
@@ -1,6 +1,13 @@
-import { assertEquals, loadTestLibrary } from "./common.js";
+import { assertEquals, loadTestLibrary, assertRejects } from "./common.js";
 
 const strings = loadTestLibrary();
+
+const simpleWasm = new Uint8Array([
+  0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01, 0x60,
+  0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07, 0x07, 0x01,
+  0x03, 0x61, 0x64, 0x64, 0x00, 0x00, 0x0a, 0x09, 0x01, 0x07, 0x00, 0x20,
+  0x00, 0x20, 0x01, 0x6a, 0x0b
+]);
 
 Deno.test("napi string utf8", function () {
   assertEquals(strings.test_utf8(""), "");
@@ -11,3 +18,16 @@ Deno.test("napi string", function () {
   assertEquals(strings.test_utf16(""), "");
   assertEquals(strings.test_utf16("🦕"), "🦕");
 });
+
+Deno.test(
+  async function wasmInstantiateStreamingNoContentType() {
+    await assertRejects(
+      async () => {
+        const response = Promise.resolve(new Response(simpleWasm));
+        await WebAssembly.instantiateStreaming(response);
+      },
+      TypeError,
+      "Invalid WebAssembly content type.",
+    );
+  },
+);
