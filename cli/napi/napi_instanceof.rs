f9e20f928bca18dd3abd573f512f49c29ffc66c3
Divy Srivastava <dj.srivastava23@gmail.com>
2022-02-19 18:35:31 +0530

    implement napi_instanceof

--- cli/napi/napi_instanceof.rs
+++ cli/napi/napi_instanceof.rs
@@ -2,15 +2,21 @@ use deno_core::napi::*;
 
 #[napi_sym::napi_sym]
 fn napi_instanceof(
-  _env: napi_env,
-  _value: napi_value,
-  _constructor: napi_value,
-  _result: *mut bool,
+  env: &mut Env,
+  value: v8::Local<v8::Value>,
+  constructor: v8::Local<v8::Value>,
+  result: *mut bool,
 ) -> Result {
-  // let mut env = &mut (env as *mut Env);
-  // let value: v8::Local<v8::Value> = transmute(value);
-  // let constructor: v8::Local<v8::Value> = transmute(constructor);
-  // TODO: https://github.com/denoland/rusty_v8/pull/879
-  // *result = value.instance_of(constructor);
-  Ok(())
+  let ctor = constructor.to_object(env.scope).unwrap();
+  if !ctor.is_function() {
+    return Err(Error::FunctionExpected);
+  }
+  let maybe = value.instance_of(env.scope, ctor);
+  match maybe {
+    Some(res) => {
+      *result = res;
+      Ok(())
+    }
+    None => Err(Error::GenericFailure),
+  }
 }
