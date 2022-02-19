be95b1024defb12507f3c90febe6451b1d486c21
Divy Srivastava <dj.srivastava23@gmail.com>
2022-02-19 17:28:21 +0530

    Add napi_has_own_property

---
+++ cli/napi/napi_has_own_property.rs
@@ -0,0 +1,30 @@
+use deno_core::napi::*;
+
+#[napi_sym::napi_sym]
+fn napi_has_own_property(
+  env: napi_env,
+  object: napi_value,
+  key: napi_value,
+  result: *mut bool,
+) -> Result {
+  let mut env = &mut *(env as *mut Env);
+  let value: v8::Local<v8::Value> = transmute::<napi_value, _>(object);
+  let object = value.to_object(env.scope).unwrap();
+
+  let key: v8::Local<v8::Value> = transmute::<napi_value, _>(key);
+  if !key.is_name() {
+    return Err(Error::NameExpected);  
+  }
+
+  let maybe = object
+    .has_own_property(env.scope, key)
+    .unwrap_or(false);
+  
+  match maybe {
+    Some(res) => {
+      *result = res;
+      Ok(())
+    }
+    None => Err(Error::GenericFailure),
+  }
+}
