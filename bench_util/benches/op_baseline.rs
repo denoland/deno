511f329bba8bf8f53aa309c294382f2b02720bd6
Divy Srivastava <dj.srivastava23@gmail.com>
2022-03-06 17:20:46 +0530

    bring back baseline benchmarks

--- bench_util/benches/op_baseline.rs
+++ bench_util/benches/op_baseline.rs
@@ -7,38 +7,60 @@ use deno_core::serialize_op_result;
 use deno_core::Extension;
 use deno_core::Op;
 use deno_core::OpState;
+use deno_core::op;
+use deno_core::op_async;
 
 use std::cell::RefCell;
 use std::rc::Rc;
 
 fn setup() -> Vec<Extension> {
-  vec![Extension::builder().build()]
+  vec![Extension::builder()
+    .ops(|ctx| {
+      ctx.register("pi_json", op_pi_json);
+      ctx.register("pi_async", op_pi_async);
+      ctx.register(
+        "nop",
+        op_nop,
+      );
+    })
+    .build()]
 }
 
-// // this is a function since async closures aren't stable
-// async fn op_pi_async(
-//   _: Rc<RefCell<OpState>>,
-//   _: (),
-//   _: (),
-// ) -> Result<i64, AnyError> {
-//   Ok(314159)
-// }
+#[op]
+fn op_nop(_: &mut OpState, _: (), _: ()) -> Result<u8, AnyError> {
+  Ok(9)
+}
 
-// fn bench_op_pi_json(b: &mut Bencher) {
-//   bench_js_sync(b, r#"Deno.core.opSync("pi_json", null);"#, setup);
-// }
+#[op]
+fn op_pi_json(_: &mut OpState, _: (), _: ()) -> Result<i64, AnyError> {
+  Ok(314159)
+}
 
-// fn bench_op_nop(b: &mut Bencher) {
-//   bench_js_sync(b, r#"Deno.core.opSync("nop", null, null, null);"#, setup);
-// }
+// this is a function since async closures aren't stable
+#[op_async]
+async fn op_pi_async(
+  _: Rc<RefCell<OpState>>,
+  _: (),
+  _: (),
+) -> Result<i64, AnyError> {
+  Ok(314159)
+}
 
-// fn bench_op_async(b: &mut Bencher) {
-//   bench_js_async(b, r#"Deno.core.opAsync("pi_async", null);"#, setup);
-// }
+fn bench_op_pi_json(b: &mut Bencher) {
+  bench_js_sync(b, r#"Deno.core.pi_json(null);"#, setup);
+}
 
-// fn bench_is_proxy(b: &mut Bencher) {
-//   bench_js_sync(b, r#"Deno.core.isProxy(42);"#, setup);
-// }
+fn bench_op_nop(b: &mut Bencher) {
+  bench_js_sync(b, r#"Deno.core.nop(null, null, null);"#, setup);
+}
+
+fn bench_op_async(b: &mut Bencher) {
+  bench_js_async(b, r#"Deno.core.opAsync("pi_async", null);"#, setup);
+}
+
+fn bench_is_proxy(b: &mut Bencher) {
+  bench_js_sync(b, r#"Deno.core.isProxy(42);"#, setup);
+}
 
 fn bench_op_void_sync(b: &mut Bencher) {
   bench_js_sync(b, r#"Deno.core.op_void_sync(null, null);"#, setup);
@@ -48,5 +70,13 @@ fn bench_op_void_async(b: &mut Bencher) {
   bench_js_sync(b, r#"Deno.core.op_void_async(1, null, null);"#, setup);
 }
 
-benchmark_group!(benches, bench_op_void_sync, bench_op_void_async,);
+benchmark_group!(
+  benches, 
+  bench_op_void_sync, 
+  bench_op_void_async,
+  bench_op_pi_json,
+  bench_op_nop,
+  bench_op_async,
+  bench_is_proxy
+);
 bench_or_profile!(benches);
