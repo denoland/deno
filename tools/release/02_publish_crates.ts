a3d6be025c14174ce6e270a3004b9892d5aae071
David Sherret <dsherret@users.noreply.github.com>
2022-03-08 18:10:03 -0500

    chore: reduce release PRs from 2 to 1 (#13878)

--- tools/release/02_publish_crates.ts
+++ tools/release/02_publish_crates.ts
@@ -4,6 +4,7 @@ import { DenoWorkspace } from "./deno_workspace.ts";
 import { getCratesPublishOrder } from "./deps.ts";
 
 const workspace = await DenoWorkspace.load();
+const cliCrate = workspace.getCliCrate();
 
 const dependencyCrates = getCratesPublishOrder(workspace.getDependencyCrates());
 
@@ -12,6 +13,8 @@ try {
     await crate.publish();
     console.log(`Published ${i + 1} of ${dependencyCrates.length} crates.`);
   }
+
+  await cliCrate.publish();
 } finally {
   // system beep to notify error or completion
   console.log("\x07");
