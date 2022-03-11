84e464006f845fc87ee212faf9d351f6af79396a
Divy Srivastava <dj.srivastava23@gmail.com>
2022-03-11 18:57:11 +0530

    x

--- core/serialize_deserialize_test.js
+++ core/serialize_deserialize_test.js
@@ -12,6 +12,7 @@ function assertArrayEquals(a1, a2) {
 
   for (const index in a1) {
     if (a1[index] !== a2[index]) {
+      console.log(a1);
       throw Error(`assert: (index ${index}) ${a1[index]} !== ${a2[index]}`);
     }
   }
@@ -19,7 +20,7 @@ function assertArrayEquals(a1, a2) {
 
 function main() {
   const emptyString = "";
-  const emptyStringSerialized = [255, 14, 34, 0];
+  const emptyStringSerialized = [255, 15, 34, 0];
   assertArrayEquals(Deno.core.serialize(emptyString), emptyStringSerialized);
   assert(
     Deno.core.deserialize(new Uint8Array(emptyStringSerialized)) ===
@@ -29,7 +30,7 @@ function main() {
   const primitiveValueArray = ["test", "a", null, undefined];
   // deno-fmt-ignore
   const primitiveValueArraySerialized = [
-    255, 14, 65, 4, 34, 4, 116, 101, 115, 116,
+    255, 15, 65, 4, 34, 4, 116, 101, 115, 116,
     34, 1, 97, 48, 95, 36, 0, 4,
   ];
   assertArrayEquals(
@@ -48,7 +49,7 @@ function main() {
   circularObject.test = circularObject;
   // deno-fmt-ignore
   const circularObjectSerialized = [
-    255, 14, 111, 34, 4, 116, 101, 115,
+    255, 15, 111, 34, 4, 116, 101, 115,
     116, 94, 0, 34, 5, 116, 101, 115,
     116, 50, 34, 2, 100, 100, 34, 5,
     116, 101, 115, 116, 51, 34, 2, 97,
