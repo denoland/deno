c63e2434426fbdd0303416f63fe1fd156ba65069
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-03-15 16:40:28 +0100

    lint

--- cli/tests/unit/http_test.ts
+++ cli/tests/unit/http_test.ts
@@ -1741,7 +1741,7 @@ Deno.test({
 
 Deno.test("upgradeHttp", async () => {
   async function client() {
-    let tcpConn = await Deno.connect({ port: 4501 });
+    const tcpConn = await Deno.connect({ port: 4501 });
     await tcpConn.write(
       new TextEncoder().encode(
         "CONNECT server.example.com:80 HTTP/1.1\r\n\r\nbla bla bla\nbla bla\nbla\n",
@@ -1760,10 +1760,10 @@ Deno.test("upgradeHttp", async () => {
   const signal = abortController.signal;
 
   const server = serve((req) => {
-    let p = Deno.upgradeHttp(req);
+    const p = Deno.upgradeHttp(req);
 
     (async () => {
-      let [conn, firstPacket] = await p;
+      const [conn, firstPacket] = await p;
       const buf = new Uint8Array(1024);
       const firstPacketText = new TextDecoder().decode(firstPacket);
       assertEquals(firstPacketText, "bla bla bla\nbla bla\nbla\n");
