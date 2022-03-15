f25fa0e895a74d934e18810ab9737cf6fcdacf8d
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-03-15 19:38:17 +0100

    fix assertion

--- ext/fetch/23_response.js
+++ ext/fetch/23_response.js
@@ -265,7 +265,7 @@
         context: "Argument 2",
       });
 
-      if ((init.status < 100 || init.status > 599) && init.status != 101) {
+      if ((init.status < 200 || init.status > 599) && init.status != 101) {
         throw new RangeError(
           `The status provided (${init.status}) is not equal to 101 and outside the range [200, 599].`,
         );
