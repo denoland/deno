f9399d992c16b7b623fda73afccdcdaa3b0b73d9
Kitson Kelly <me@kitsonkelly.com>
2022-03-23 15:12:39 +1100

    remove diagnostic logging

--- cli/lsp/testing.rs
+++ cli/lsp/testing.rs
@@ -954,7 +954,6 @@ impl LspTestReporter {
   }
 
   fn progress(&self, message: TestRunProgressMessage) {
-    log::info!("progress {}", json!(message));
     self
       .channel
       .send(TestingNotification::Progress(
