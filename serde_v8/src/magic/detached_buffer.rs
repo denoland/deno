2678a562da8a5665e1766c835e57b19f4f9ad4b4
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-03-24 00:41:20 +0100

    cleanup

--- serde_v8/src/magic/detached_buffer.rs
+++ serde_v8/src/magic/detached_buffer.rs
@@ -1,5 +1,6 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
+use core::ops::Range;
 use std::ops::Deref;
 use std::ops::DerefMut;
 
@@ -43,24 +44,11 @@ impl ToV8 for DetachedBuffer {
     &self,
     scope: &mut v8::HandleScope<'a>,
   ) -> Result<v8::Local<'a, v8::Value>, crate::Error> {
-    // TODO: restore range ?
     let buffer = v8::ArrayBuffer::with_backing_store(scope, &self.0.store);
-    let (off, len) =
-      (self.0.range.start, self.0.range.end - self.0.range.start);
+    let Range { start, end } = self.0.range.clone();
+    let (off, len) = (start, end - start);
     let v = v8::Uint8Array::new(scope, buffer, off, len).unwrap();
     Ok(v.into())
-
-    // Re-box
-    // let boxed = Box::from(self.0.as_ref());
-    // let store = v8::ArrayBuffer::new_backing_store_from_boxed_slice(boxed);
-    // let store = store.make_shared();
-    // let ab = v8::ArrayBuffer::with_backing_store(scope, &store);
-    // let (off, len) = (self.0.range.start, self.0.range.end-self.0.range.start);
-    // Ok(
-    //   v8::Uint8Array::new(scope, ab, off, len)
-    //     .expect("Failed to create Uint8Array")
-    //     .into(),
-    // )
   }
 }
 
