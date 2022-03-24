a114287b60c324ea9f4573380f3bba057e5750be
David Sherret <dsherret@gmail.com>
2022-03-24 18:01:12 -0400

    Should have just done `-A` at this rate.

--- tools/release/02_create_pr.ts
+++ tools/release/02_create_pr.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-run=cargo,git
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-net --allow-run=cargo,git
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 import { DenoWorkspace } from "./deno_workspace.ts";
 import { createOctoKit, getGitHubRepository } from "./deps.ts";
