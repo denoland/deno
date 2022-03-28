51ac189cd50f74418d9e26743b4c02383649ea89
Robert <52195359+SeparateRecords@users.noreply.github.com>
2022-03-28 12:57:56 +1100

    chore: update clap and completions (#14136)

--- cli/main.rs
+++ cli/main.rs
@@ -1469,8 +1469,8 @@ pub fn main() {
     let flags = match flags::flags_from_vec(args) {
       Ok(flags) => flags,
       Err(err @ clap::Error { .. })
-        if err.kind == clap::ErrorKind::DisplayHelp
-          || err.kind == clap::ErrorKind::DisplayVersion =>
+        if err.kind() == clap::ErrorKind::DisplayHelp
+          || err.kind() == clap::ErrorKind::DisplayVersion =>
       {
         err.print().unwrap();
         std::process::exit(0);
