d68d4f1621579a8ad7bed58da69db7f67550a849
David Sherret <dsherret@gmail.com>
2022-04-01 17:17:59 -0400

    Try again.

--- tools/release/05_output_release_notes.ts
+++ tools/release/05_output_release_notes.ts
@@ -5,4 +5,10 @@ import { DenoWorkspace } from "./deno_workspace.ts";
 const workspace = await DenoWorkspace.load();
 
 // outputs the release notes for use when creating the GH draft
-console.log(workspace.getReleasesMdFile().getLatestReleaseText().fullText);
+console.log(
+  workspace.getReleasesMdFile().getLatestReleaseText().fullText
+    // escape percent signs and newlines in order to allow multiple
+    // lines to be set in the GH action step output
+    .replace(/%/, "%25")
+    .replace(/\r?\n/, "%0A"),
+);
