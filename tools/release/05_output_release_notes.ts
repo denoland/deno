1038444a2aa2703f5578bbba6db2bb7da080a8a7
David Sherret <dsherret@gmail.com>
2022-04-01 17:26:49 -0400

    Sigh.

--- tools/release/05_output_release_notes.ts
+++ tools/release/05_output_release_notes.ts
@@ -9,6 +9,6 @@ console.log(
   workspace.getReleasesMdFile().getLatestReleaseText().fullText
     // escape percent signs and newlines in order to allow multiple
     // lines to be set in the GH action step output
-    .replace(/%/, "%25")
-    .replace(/\r?\n/, "%0A"),
+    .replace(/%/g, "%25")
+    .replace(/\r?\n/g, "%0A"),
 );
