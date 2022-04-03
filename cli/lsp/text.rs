a6e4b4297d840e9cfe4bdf373f24da8202f58e2d
Jason <m.jason.liu@outlook.com>
2022-04-03 12:17:30 +0800

    refactor(lsp): migrate from lspower back to tower-lsp (#14163)

--- cli/lsp/text.rs
+++ cli/lsp/text.rs
@@ -6,14 +6,14 @@ use deno_core::serde_json::json;
 use deno_core::serde_json::Value;
 use dissimilar::diff;
 use dissimilar::Chunk;
-use lspower::jsonrpc;
-use lspower::lsp;
-use lspower::lsp::TextEdit;
 use std::collections::HashMap;
 use std::ops::Bound;
 use std::ops::RangeBounds;
 use text_size::TextRange;
 use text_size::TextSize;
+use tower_lsp::jsonrpc;
+use tower_lsp::lsp_types as lsp;
+use tower_lsp::lsp_types::TextEdit;
 
 fn partition_point<T, P>(slice: &[T], mut predicate: P) -> usize
 where
