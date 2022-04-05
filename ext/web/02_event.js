d367f07d91042a5cd9154afcf8eca02b1bb9c419
Nayeem Rahman <nayeemrmn99@gmail.com>
2022-04-05 18:26:16 +0100

    Review

--- ext/web/02_event.js
+++ ext/web/02_event.js
@@ -791,6 +791,9 @@
     try {
       innerInvokeEventListeners(eventImpl, getListeners(tuple.item));
     } catch (error) {
+      // We can't store `__bootstrap.reportError` ahead of time because there
+      // is a dependency cycle between this and `**_report_error.js` and one
+      // will not be available for the other at bootstrap time.
       __bootstrap.reportError.reportException(error);
     }
   }
