1850b70e7f0806515401b331b0d258bfc426b35d
Divy Srivastava <dj.srivastava23@gmail.com>
2022-04-08 18:04:22 +0530

    cleanup

--- fetch.js
+++
@@ -1,9 +0,0 @@
-// Test GC.
-for (let i = 0; i < 1000; i++) await fetch("http://localhost:8000/README.md");
-gc();
-
-// Test that fetch ops work.
-const response = await fetch("http://localhost:8000/README.md");
-const text = await response.text();
-console.log(text.length);
-gc();
