720500a94303ec2c8fdc00e785c804ad10ae5446
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-04-18 01:40:08 +0200

    revert unrelated change

--- runtime/colors.rs
+++ runtime/colors.rs
@@ -76,15 +76,6 @@ pub fn white_on_red<S: AsRef<str>>(s: S) -> impl fmt::Display {
   style(s, style_spec)
 }
 
-pub fn white_bold_on_red<S: AsRef<str>>(s: S) -> impl fmt::Display {
-  let mut style_spec = ColorSpec::new();
-  style_spec
-    .set_bold(true)
-    .set_bg(Some(Red))
-    .set_fg(Some(White));
-  style(s, style_spec)
-}
-
 pub fn black_on_green<S: AsRef<str>>(s: S) -> impl fmt::Display {
   let mut style_spec = ColorSpec::new();
   style_spec.set_bg(Some(Green)).set_fg(Some(Black));
