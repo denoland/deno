0bb5c3eecc6fe28a5fb17259aedfd36e062ac261
David Sherret <dsherret@gmail.com>
2022-04-26 09:49:26 -0400

    Ooops :)

--- cli/tests/testdata/test/captured_subprocess_output.ts
+++ cli/tests/testdata/test/captured_subprocess_output.ts
@@ -1,20 +1,20 @@
 Deno.test("output", async () => {
   const p = Deno.run({
-    cmd: [Deno.exePath(), "eval", "console.log(1); console.error(2);"],
+    cmd: [Deno.execPath(), "eval", "console.log(1); console.error(2);"],
   });
   await p.status();
   await p.close();
-  Deno.spawnSync(Deno.exePath(), {
+  Deno.spawnSync(Deno.execPath(), {
     args: ["eval", "console.log(3); console.error(4);"],
     stdout: "inherit",
     stderr: "inherit",
   });
-  await Deno.spawn(Deno.exePath(), {
+  await Deno.spawn(Deno.execPath(), {
     args: ["eval", "console.log(5); console.error(6);"],
     stdout: "inherit",
     stderr: "inherit",
   });
-  const c = await Deno.spawnChild(Deno.exePath(), {
+  const c = await Deno.spawnChild(Deno.execPath(), {
     args: ["eval", "console.log(7); console.error(8);"],
     stdout: "inherit",
     stderr: "inherit",
