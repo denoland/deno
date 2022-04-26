f490c623443a1c8f10c1830ff6b020dc3c1cb072
David Sherret <dsherret@gmail.com>
2022-04-25 20:26:06 -0400

    Fix CI maybe.

--- cli/tests/testdata/test/captured_subprocess_output.ts
+++ cli/tests/testdata/test/captured_subprocess_output.ts
@@ -1,20 +1,20 @@
 Deno.test("output", async () => {
   const p = Deno.run({
-    cmd: ["deno", "eval", "console.log(1); console.error(2);"],
+    cmd: [Deno.exePath(), "eval", "console.log(1); console.error(2);"],
   });
   await p.status();
   await p.close();
-  Deno.spawnSync("deno", {
+  Deno.spawnSync(Deno.exePath(), {
     args: ["eval", "console.log(3); console.error(4);"],
     stdout: "inherit",
     stderr: "inherit",
   });
-  await Deno.spawn("deno", {
+  await Deno.spawn(Deno.exePath(), {
     args: ["eval", "console.log(5); console.error(6);"],
     stdout: "inherit",
     stderr: "inherit",
   });
-  const c = await Deno.spawnChild("deno", {
+  const c = await Deno.spawnChild(Deno.exePath(), {
     args: ["eval", "console.log(7); console.error(8);"],
     stdout: "inherit",
     stderr: "inherit",
