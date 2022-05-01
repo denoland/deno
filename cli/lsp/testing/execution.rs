3d9aecfa7aea655bf249ba30159d4aeebb584a4f
David Sherret <dsherret@users.noreply.github.com>
2022-05-01 14:44:55 -0400

    fix(test): actually capture stdout and stderr in workers (#14435)

--- cli/lsp/testing/execution.rs
+++ cli/lsp/testing/execution.rs
@@ -16,6 +16,7 @@ use crate::lsp::logging::lsp_log;
 use crate::ops;
 use crate::proc_state;
 use crate::tools::test;
+use crate::tools::test::TestEventSender;
 
 use deno_core::anyhow::anyhow;
 use deno_core::error::AnyError;
@@ -180,21 +181,20 @@ async fn test_specifier(
   permissions: Permissions,
   specifier: ModuleSpecifier,
   mode: test::TestMode,
-  channel: mpsc::UnboundedSender<test::TestEvent>,
+  sender: TestEventSender,
   token: CancellationToken,
   options: Option<Value>,
 ) -> Result<(), AnyError> {
   if !token.is_cancelled() {
-    let (stdout, stderr) = test::create_stdout_stderr_pipes(channel.clone());
     let mut worker = create_main_worker(
       &ps,
       specifier.clone(),
       permissions,
-      vec![ops::testing::init(channel.clone())],
+      vec![ops::testing::init(sender.clone())],
       Stdio {
         stdin: StdioPipe::Inherit,
-        stdout: StdioPipe::File(stdout),
-        stderr: StdioPipe::File(stderr),
+        stdout: StdioPipe::File(sender.stdout()),
+        stderr: StdioPipe::File(sender.stderr()),
       },
     );
 
@@ -318,6 +318,7 @@ impl TestRun {
     .await?;
 
     let (sender, mut receiver) = mpsc::unbounded_channel::<test::TestEvent>();
+    let sender = TestEventSender::new(sender);
 
     let (concurrent_jobs, fail_fast) =
       if let flags::DenoSubcommand::Test(test_flags) = &ps.flags.subcommand {
@@ -754,19 +755,14 @@ impl test::TestReporter for LspTestReporter {
     self.progress(lsp_custom::TestRunProgressMessage::Started { test });
   }
 
-  fn report_output(&mut self, output: &test::TestOutput) {
+  fn report_output(&mut self, output: &[u8]) {
     let test = self.current_origin.as_ref().and_then(|origin| {
       self
         .stack
         .get(origin)
         .and_then(|v| v.last().map(|td| td.into()))
     });
-    let value = match output {
-      test::TestOutput::String(value) => value.replace('\n', "\r\n"),
-      test::TestOutput::Bytes(bytes) => {
-        String::from_utf8_lossy(bytes).replace('\n', "\r\n")
-      }
-    };
+    let value = String::from_utf8_lossy(output).replace('\n', "\r\n");
 
     self.progress(lsp_custom::TestRunProgressMessage::Output {
       value,
--- cli/tests/integration/test_tests.rs
+++ cli/tests/integration/test_tests.rs
@@ -302,11 +302,37 @@ itest!(no_prompt_with_denied_perms {
   output: "test/no_prompt_with_denied_perms.out",
 });
 
-itest!(captured_output {
-  args: "test --allow-run --allow-read --unstable test/captured_output.ts",
-  exit_code: 0,
-  output: "test/captured_output.out",
-});
+#[test]
+fn captured_output() {
+  let output = util::deno_cmd()
+    .current_dir(util::testdata_path())
+    .arg("test")
+    .arg("--allow-run")
+    .arg("--allow-read")
+    .arg("--unstable")
+    .arg("test/captured_output.ts")
+    .env("NO_COLOR", "1")
+    .stdout(std::process::Stdio::piped())
+    .spawn()
+    .unwrap()
+    .wait_with_output()
+    .unwrap();
+
+  let output_start = "------- output -------";
+  let output_end = "----- output end -----";
+  assert!(output.status.success());
+  let output_text = String::from_utf8(output.stdout).unwrap();
+  let start = output_text.find(output_start).unwrap() + output_start.len();
+  let end = output_text.find(output_end).unwrap();
+  let output_text = output_text[start..end].trim();
+  let mut lines = output_text.lines().collect::<Vec<_>>();
+  // the output is racy on either stdout or stderr being flushed
+  // from the runtime into the rust code, so sort it... the main
+  // thing here to ensure is that we're capturing the output in
+  // this block on stdout
+  lines.sort_unstable();
+  assert_eq!(lines.join(" "), "0 1 2 3 4 5 6 7 8 9");
+}
 
 #[test]
 fn recursive_permissions_pledge() {
