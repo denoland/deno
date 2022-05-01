de33017a8bc3ddc79664df0fae3ded5607b38180
David Sherret <dsherret@users.noreply.github.com>
2022-05-01 14:44:55 -0400

    fix(test): actually capture stdout and stderr in workers (#14435)

--- cli/tests/testdata/test/captured_output.ts
+++ cli/tests/testdata/test/captured_output.ts
@@ -1,21 +1,21 @@
 Deno.test("output", async () => {
   const p = Deno.run({
-    cmd: [Deno.execPath(), "eval", "console.log(1); console.error(2);"],
+    cmd: [Deno.execPath(), "eval", "console.log(0); console.error(1);"],
   });
   await p.status();
   await p.close();
   Deno.spawnSync(Deno.execPath(), {
-    args: ["eval", "console.log(3); console.error(4);"],
+    args: ["eval", "console.log(2); console.error(3);"],
     stdout: "inherit",
     stderr: "inherit",
   });
   await Deno.spawn(Deno.execPath(), {
-    args: ["eval", "console.log(5); console.error(6);"],
+    args: ["eval", "console.log(4); console.error(5);"],
     stdout: "inherit",
     stderr: "inherit",
   });
   const c = await Deno.spawnChild(Deno.execPath(), {
-    args: ["eval", "console.log(7); console.error(8);"],
+    args: ["eval", "console.log(6); console.error(7);"],
     stdout: "inherit",
     stderr: "inherit",
   });
