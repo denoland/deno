69aae8e8d4cfbd6f2839250de5f79eba0ff31315
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-05-03 15:48:17 +0200

    Remove unstable Deno.applySourceMap API

--- runtime/js/40_error_stack.js
+++ runtime/js/40_error_stack.js
@@ -4,21 +4,12 @@
 ((window) => {
   const core = window.Deno.core;
 
-  function opFormatDiagnostics(diagnostics) {
-    return core.opSync("op_format_diagnostic", diagnostics);
-  }
-
   function opFormatFileName(location) {
     return core.opSync("op_format_file_name", location);
-  }
-
-  function opApplySourceMap(location) {
-    return core.applySourceMap(location);
   }
 
   window.__bootstrap.errorStack = {
     opFormatDiagnostics,
     opFormatFileName,
-    opApplySourceMap,
   };
 })(this);
