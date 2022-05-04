1104d14e0304e75cf4cd94a0cd16812d69558a12
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-05-04 13:10:55 +0200

    fix(coverage): exclude .snap files (#14480)

--- cli/tests/integration/coverage_tests.rs
+++ cli/tests/integration/coverage_tests.rs
@@ -19,6 +19,11 @@ fn final_blankline() {
   run_coverage_text("final_blankline", "js");
 }
 
+#[test]
+fn no_snaps() {
+  no_snaps_included("no_snaps_included", "ts");
+}
+
 fn run_coverage_text(test_name: &str, extension: &str) {
   let deno_dir = TempDir::new();
   let tempdir = TempDir::new();
@@ -166,6 +171,60 @@ fn multifile_coverage() {
 
   let expected = fs::read_to_string(
     util::testdata_path().join("coverage/multifile/expected.lcov"),
+  )
+  .unwrap();
+
+  if !util::wildcard_match(&expected, &actual) {
+    println!("OUTPUT\n{}\nOUTPUT", actual);
+    println!("EXPECTED\n{}\nEXPECTED", expected);
+    panic!("pattern match failed");
+  }
+
+  assert!(output.status.success());
+}
+
+fn no_snaps_included(test_name: &str, extension: &str) {
+  let deno_dir = TempDir::new();
+  let tempdir = TempDir::new();
+  let tempdir = tempdir.path().join("cov");
+
+  let status = util::deno_cmd_with_deno_dir(&deno_dir)
+    .current_dir(util::testdata_path())
+    .arg("test")
+    .arg("--quiet")
+    .arg("--unstable")
+    .arg("--allow-read")
+    .arg(format!("--coverage={}", tempdir.to_str().unwrap()))
+    .arg(format!(
+      "coverage/no_snaps_included/{}_test.{}",
+      test_name, extension
+    ))
+    .stdout(std::process::Stdio::piped())
+    .stderr(std::process::Stdio::piped())
+    .status()
+    .unwrap();
+
+  assert!(status.success());
+
+  let output = util::deno_cmd_with_deno_dir(&deno_dir)
+    .current_dir(util::testdata_path())
+    .arg("coverage")
+    .arg("--unstable")
+    .arg(format!("{}/", tempdir.to_str().unwrap()))
+    .stdout(std::process::Stdio::piped())
+    .stderr(std::process::Stdio::piped())
+    .output()
+    .unwrap();
+
+  // Verify there's no "Check" being printed
+  assert!(output.stderr.is_empty());
+
+  let actual =
+    util::strip_ansi_codes(std::str::from_utf8(&output.stdout).unwrap())
+      .to_string();
+
+  let expected = fs::read_to_string(
+    util::testdata_path().join("coverage/no_snaps_included/expected.out"),
   )
   .unwrap();
 
