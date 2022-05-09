d4ad2b809c62b59ee4c3767bf1ff096b30886bdb
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-05-09 13:25:04 +0200

    feat(test): repeat test name if there's user output (#14495)
    This commit changes test report output to repeat test name
    before printing result, but only if there's user output, denoted
    by markers.

--- cli/tools/test.rs
+++ cli/tools/test.rs
@@ -312,6 +312,10 @@ impl PrettyTestReporter {
       print!("{}", "  ".repeat(description.level));
     }
 
+    if wrote_user_output {
+      print!("{} ... ", description.name);
+    }
+
     println!(
       "{} {}",
       status,
@@ -412,6 +416,10 @@ impl TestReporter for PrettyTestReporter {
     let wrote_user_output = self.write_output_end();
     if !wrote_user_output && self.last_wait_output_level == 0 {
       print!(" ");
+    }
+
+    if wrote_user_output {
+      print!("{} ... ", description.name);
     }
 
     let status = match result {
