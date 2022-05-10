d07e3ff8bfbb4f320ac4273e026ab4c8499037e4
Divy Srivastava <dj.srivastava23@gmail.com>
2022-05-10 11:05:10 +0530

    hmm?

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 for (let i = 0; i < 10; i++) {
   const response = await fetch("http://localhost:8000/README.md");
-
+  response.text();
   console.log(i);
 }
--- serde_v8/magic/resource.rs
+++ serde_v8/magic/resource.rs
@@ -19,7 +19,7 @@ use std::rc::Rc;
 /// The underlying Rc<T> will always have a strong count >= 1 until either
 /// the JavaScript object is garbage collected OR `into_inner` is called.
 pub struct Resource<T: ?Sized> {
-  inner: Option<Rc<T>>,
+  inner: Option<*const T>,
   cancel_finalization: Rc<Cell<bool>>,
   _marker: PhantomData<T>,
 }
@@ -53,8 +53,7 @@ impl<T: ?Sized> Resource<T> {
   const CANCEL_FINALIZATION_FIELD_INDEX: usize = 1;
 
   pub fn borrow(mut self) -> Rc<T> {
-    let rc = self.inner.take().unwrap();
-    let ptr = Rc::into_raw(rc);
+    let ptr = self.inner.take().unwrap();
     // SAFETY: Rc is immediately constructed from it's raw pointer.
     let rc = unsafe { Rc::from_raw(ptr) };
     if Rc::strong_count(&rc) == 1 {
@@ -73,7 +72,7 @@ impl<T> Resource<T> {
   pub fn new(value: T) -> Self {
     let rc = Rc::new(value);
     Resource {
-      inner: Some(rc),
+      inner: Some(Rc::into_raw(rc)),
       cancel_finalization: Rc::new(Cell::new(false)),
       _marker: PhantomData,
     }
@@ -82,8 +81,7 @@ impl<T> Resource<T> {
   // Returns the underlying owned value held by the Resource.
   /// This will return None if the Resource is already in use.
   pub fn into_inner(mut self) -> Option<T> {
-    let rc = self.inner.take()?;
-    let ptr = Rc::into_raw(rc);
+    let ptr = self.inner.take()?;
     // SAFETY: Rc is immediately constructed from it's raw pointer.
     let rc = unsafe { Rc::from_raw(ptr) };
 
@@ -140,9 +138,7 @@ impl<T> ToV8 for Resource<T> {
         .build(scope)
         .unwrap();
 
-    let rc = self.inner.clone().unwrap();
-    let ptr = Rc::into_raw(rc) as *mut c_void;
-
+    let ptr = self.inner.unwrap() as *mut c_void;
     let field = v8::External::new(scope, ptr);
     let wrap = tpl.new_instance(scope).unwrap();
     assert!(
@@ -181,11 +177,12 @@ impl<T> ToV8 for Resource<T> {
           let _weak = v8::Weak::from_raw(isolate, Some(raw_weak));
         }
         if cancel_finalization.get() {
+          println!("  Cancel finalization");
           // Rust tells us to prevent dropping the Rc.
           // It decrements the strong count and is the sole owner of the resource data.
           return;
         }
-        println!("Dropping Rc<T>");
+        println!("  Drop resource");
         // SAFETY: We own the Rc<T>, no other Resource can hold the pointer
         // to it. Here, we say bye-bye to the object.
         unsafe {
@@ -219,7 +216,8 @@ impl<T> FromV8 for Resource<T> {
       v8::Local::<v8::External>::try_from(cancel_external).unwrap();
 
     // SAFETY: The internal field of this Object is a valid External pointer to the Rc<T>.
-    let inner = unsafe { Rc::from_raw(ptr.value() as *const _) };
+    let inner = ptr.value() as *const _;
+    unsafe { Rc::increment_strong_count(inner) };
     let cancel_finalization =
       unsafe { Rc::from_raw(cancel_ptr.value() as *const _) };
     Ok(Resource {
