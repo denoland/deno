b0ff8f94071ccfb2576a45d904507dba249a06ff
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-05-13 14:55:28 +0200

    serde_v8 safety comments, CallbackScope safety comments

--- core/modules.rs
+++ core/modules.rs
@@ -122,6 +122,7 @@ fn json_module_evaluation_steps<'a>(
   context: v8::Local<'a, v8::Context>,
   module: v8::Local<v8::Module>,
 ) -> Option<v8::Local<'a, v8::Value>> {
+  // SAFETY: `CallbackScope` can be safely constructed from `Local<Context>`
   let scope = &mut unsafe { v8::CallbackScope::new(context) };
   let tc_scope = &mut v8::TryCatch::new(scope);
   let module_map = tc_scope
