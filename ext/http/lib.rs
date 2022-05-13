67e5a850cc9b37b76638cfbac9dee08286a3469a
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-05-13 12:53:13 +0200

    feat(serde_v8): bytes::Bytes support (#14412)

--- ext/http/lib.rs
+++ ext/http/lib.rs
@@ -589,10 +589,7 @@ fn http_response(
     Some(data) => {
       // If a buffer was passed, but isn't compressible, we use it to
       // construct a response body.
-      Ok((
-        HttpResponseWriter::Closed,
-        Bytes::copy_from_slice(&data).into(),
-      ))
+      Ok((HttpResponseWriter::Closed, Bytes::from(data).into()))
     }
     None if compressing => {
       // Create a one way pipe that implements tokio's async io traits. To do
@@ -766,7 +763,7 @@ async fn op_http_write(
       }
     }
     HttpResponseWriter::BodyUncompressed(body) => {
-      let bytes = Bytes::copy_from_slice(&buf[..]);
+      let bytes = Bytes::from(buf);
       match body.send_data(bytes).await {
         Ok(_) => Ok(()),
         Err(err) => {
