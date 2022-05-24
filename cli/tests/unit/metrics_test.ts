0578e9d23f8681ea0683dff95ec6b76355d7cdd1
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-05-24 21:58:33 +0200

    fix test with .sort()

--- cli/tests/unit/metrics_test.ts
+++ cli/tests/unit/metrics_test.ts
@@ -82,7 +82,7 @@ Deno.test(function metricsForOpCrates() {
 Deno.test(function opNamesMatch() {
   assertEquals(
     // @ts-ignore: Deno.core allowed
-    Deno.core.opNames(),
+    Deno.core.opNames().sort(),
     // @ts-ignore: Deno.core allowed
     Object.keys(Deno.core.ops).sort(),
   );
