3724c2e5b337c17495eda560ffc0aaaedd006883
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-06-08 03:00:22 +0200

    wip

--- cli/ops/mod.rs
+++ cli/ops/mod.rs
@@ -1,7 +1,12 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
 use crate::proc_state::ProcState;
+use deno_core::op;
 use deno_core::Extension;
+use deno_core::OpState;
+use std::sync::atomic::AtomicI32;
+use std::sync::atomic::Ordering::Relaxed;
+use std::sync::Arc;
 
 pub mod bench;
 pub mod testing;
@@ -11,10 +16,28 @@ pub fn cli_exts(ps: ProcState) -> Vec<Extension> {
 }
 
 fn init_proc_state(ps: ProcState) -> Extension {
-  Extension::builder()
-    .state(move |state| {
-      state.put(ps.clone());
-      Ok(())
-    })
-    .build()
+  let is_file_watcher = ps.flags.watch.is_some();
+
+  let mut ext = Extension::builder();
+  
+  ext.state(move |state| {
+    state.put(ps.clone());
+    Ok(())
+  });
+
+  if is_file_watcher {
+    ext.middleware(|op| match op.name {
+      "op_exit" => op_exit::decl(),
+      _ => op,
+    });
+  }
+
+  ext.build()
+}
+
+#[op]
+fn op_exit(state: &mut OpState) {
+  let code = state.borrow::<Arc<AtomicI32>>().load(Relaxed);
+  eprintln!("I should have exited with code {}", code);
+  std::process::exit(code);
 }
