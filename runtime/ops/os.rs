c39edd9255f8f99aa0b3ec2594b1562fa714e3f8
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-06-09 20:59:41 +0200

    refactor

--- runtime/ops/os.rs
+++ runtime/ops/os.rs
@@ -4,32 +4,36 @@ use super::utils::into_string;
 use crate::permissions::Permissions;
 use crate::worker::ExitCode;
 use deno_core::error::{generic_error, type_error, AnyError};
-use deno_core::op;
 use deno_core::url::Url;
 use deno_core::Extension;
 use deno_core::OpState;
+use deno_core::{op, ExtensionBuilder};
 use serde::Serialize;
 use std::collections::HashMap;
 use std::env;
 
+fn init_ops(builder: &mut ExtensionBuilder) -> &mut ExtensionBuilder {
+  builder.ops(vec![
+    op_env::decl(),
+    op_exec_path::decl(),
+    op_exit::decl(),
+    op_delete_env::decl(),
+    op_get_env::decl(),
+    op_getgid::decl(),
+    op_getuid::decl(),
+    op_hostname::decl(),
+    op_loadavg::decl(),
+    op_network_interfaces::decl(),
+    op_os_release::decl(),
+    op_set_env::decl(),
+    op_set_exit_code::decl(),
+    op_system_memory_info::decl(),
+  ])
+}
+
 pub fn init(exit_code: ExitCode) -> Extension {
-  Extension::builder()
-    .ops(vec![
-      op_env::decl(),
-      op_exec_path::decl(),
-      op_exit::decl(),
-      op_delete_env::decl(),
-      op_get_env::decl(),
-      op_getgid::decl(),
-      op_getuid::decl(),
-      op_hostname::decl(),
-      op_loadavg::decl(),
-      op_network_interfaces::decl(),
-      op_os_release::decl(),
-      op_set_env::decl(),
-      op_set_exit_code::decl(),
-      op_system_memory_info::decl(),
-    ])
+  let mut builder = Extension::builder();
+  init_ops(&mut builder)
     .state(move |state| {
       state.put::<ExitCode>(exit_code.clone());
       Ok(())
@@ -38,23 +42,8 @@ pub fn init(exit_code: ExitCode) -> Extension {
 }
 
 pub fn init_for_worker() -> Extension {
-  Extension::builder()
-    .ops(vec![
-      op_env::decl(),
-      op_exec_path::decl(),
-      op_exit::decl(),
-      op_delete_env::decl(),
-      op_get_env::decl(),
-      op_getgid::decl(),
-      op_getuid::decl(),
-      op_hostname::decl(),
-      op_loadavg::decl(),
-      op_network_interfaces::decl(),
-      op_os_release::decl(),
-      op_set_env::decl(),
-      op_set_exit_code::decl(),
-      op_system_memory_info::decl(),
-    ])
+  let mut builder = Extension::builder();
+  init_ops(&mut builder)
     .middleware(|op| match op.name {
       "op_exit" => op_exit_for_worker::decl(),
       "op_set_exit_code" => op_set_exit_code_for_worker::decl(),
