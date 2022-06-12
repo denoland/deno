3d53899b1e36aaa84dbb621a2b3c27e222644ab6
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2022-06-12 10:53:11 +0300

    Fix more FFI ops call signatures

--- cli/tests/testdata/unstable_ffi_2.js
+++ cli/tests/testdata/unstable_ffi_2.js
@@ -1,4 +1,4 @@
-Deno.core.opSync("op_ffi_call_ptr", [0, 0], {
+Deno.core.opSync("op_ffi_call_ptr", 0n, {
   name: null,
   parameters: [],
   result: "void",
--- cli/tests/testdata/unstable_ffi_3.js
+++ cli/tests/testdata/unstable_ffi_3.js
@@ -1,4 +1,4 @@
-Deno.core.opAsync("op_ffi_call_ptr_nonblocking", [0, 0], {
+Deno.core.opAsync("op_ffi_call_ptr_nonblocking", 0n, {
   name: null,
   parameters: [],
   result: "void",
