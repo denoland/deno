7b2c176b0b1092ced30540254a6e5d4e3860223c
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2022-06-12 10:36:22 +0300

    fmt

--- cli/tests/testdata/unstable_ffi_2.js
+++ cli/tests/testdata/unstable_ffi_2.js
@@ -1,9 +1,5 @@
-Deno.core.opSync("op_ffi_call_ptr",
-  [0, 0],
-  {
-    name: null,
-    parameters: [],
-    result: "void",
-  },
-  [],
-);
+Deno.core.opSync("op_ffi_call_ptr", [0, 0], {
+  name: null,
+  parameters: [],
+  result: "void",
+}, []);
--- cli/tests/testdata/unstable_ffi_3.js
+++ cli/tests/testdata/unstable_ffi_3.js
@@ -1,9 +1,5 @@
-Deno.core.opAsync("op_ffi_call_ptr_nonblocking",
-  [0, 0],
-  {
-    name: null,
-    parameters: [],
-    result: "void",
-  },
-  [],
-);
+Deno.core.opAsync("op_ffi_call_ptr_nonblocking", [0, 0], {
+  name: null,
+  parameters: [],
+  result: "void",
+}, []);
