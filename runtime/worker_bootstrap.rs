7d5438418650a026115fddf95df71b37ae55067b
Christian Duerr <chris.durr@phylum.io>
2022-06-13 18:40:23 +0200

    Add default implementation for WorkerOptions
    This adds an implementation of `Default` for both `WorkerOptions` and
    `BootstrapOptions`. Since both of these structs are rather big, this
    should make it easier for people unfamiliar with the internals to focus
    on the options relevant to them.
    
    As a user of `deno_runtime` I feel like these should serve as good
    defaults, getting people them started without having to tweak the
    runtime. Additionally even if some changes are made, the usage of
    `..Default::default()` will significantly help with code clarity and
    verbosity.

--- runtime/worker_bootstrap.rs
+++ runtime/worker_bootstrap.rs
@@ -1,7 +1,11 @@
-use crate::ops::runtime::ppid;
+use std::thread;
+
 use deno_core::serde_json;
 use deno_core::serde_json::json;
 use deno_core::ModuleSpecifier;
+
+use crate::colors;
+use crate::ops::runtime::ppid;
 
 /// Common bootstrap options for MainWorker & WebWorker
 #[derive(Clone)]
@@ -21,6 +25,31 @@ pub struct BootstrapOptions {
   pub ts_version: String,
   pub unstable: bool,
   pub user_agent: String,
+}
+
+impl Default for BootstrapOptions {
+  fn default() -> Self {
+    let cpu_count = thread::available_parallelism()
+        .map(|p| p.get())
+        .unwrap_or(1);
+
+    let runtime_version = env!("CARGO_PKG_VERSION").into();
+    let user_agent = format!("Deno/{}", runtime_version);
+
+    Self {
+        runtime_version,
+        user_agent,
+        cpu_count,
+        no_color: !colors::use_color(),
+        is_tty: colors::is_tty(),
+        enable_testing_features: Default::default(),
+        debug_flag: Default::default(),
+        ts_version: Default::default(),
+        location: Default::default(),
+        unstable: Default::default(),
+        args: Default::default(),
+    }
+  }
 }
 
 impl BootstrapOptions {
