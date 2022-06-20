b02b631e3fc6b77c1564c87bc52596e1f3e9c844
David Sherret <dsherret@gmail.com>
2022-06-20 15:53:32 -0400

    refactor: add emit cache trait

--- cli/tools/bench.rs
+++ cli/tools/bench.rs
@@ -1,7 +1,6 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
 use crate::cache;
-use crate::cache::CacherLoader;
 use crate::colors;
 use crate::compat;
 use crate::create_main_worker;
@@ -632,7 +631,7 @@ pub async fn run_benchmarks_with_watch(
           .collect(),
         false,
         maybe_imports,
-        cache.as_mut_loader(),
+        &mut cache,
         maybe_resolver,
         maybe_locker,
         None,
