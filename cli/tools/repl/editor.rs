fbd943f534d8c5362d376ceaa0950acaf1418055
David Sherret <dsherret@gmail.com>
2022-06-21 13:12:06 -0400

    Format.

--- cli/tools/repl/editor.rs
+++ cli/tools/repl/editor.rs
@@ -411,7 +411,10 @@ impl ConditionalEventHandler for TabEventHandler {
     _: bool,
     ctx: &EventContext,
   ) -> Option<Cmd> {
-    debug_assert_eq!(*evt, Event::from(KeyEvent(KeyCode::Tab, Modifiers::NONE)));
+    debug_assert_eq!(
+      *evt,
+      Event::from(KeyEvent(KeyCode::Tab, Modifiers::NONE))
+    );
     if ctx.line().is_empty()
       || ctx.line()[..ctx.pos()]
         .chars()
