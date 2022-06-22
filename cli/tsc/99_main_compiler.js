10b40bf86b2603b819bdcc53abdf5177e1466daf
Nayeem Rahman <nayeemrmn99@gmail.com>
2022-06-22 16:57:25 +0100

    fix(check): Ignore TS2306

--- cli/tsc/99_main_compiler.js
+++ cli/tsc/99_main_compiler.js
@@ -188,6 +188,11 @@ delete Object.prototype.__proto__;
   /** Diagnostics that are intentionally ignored when compiling TypeScript in
    * Deno, as they provide misleading or incorrect information. */
   const IGNORED_DIAGNOSTICS = [
+    // TS2306: File '.../index.d.ts' is not a module.
+    // In a lot of cases, CDNs like esm.sh wrongly provide cjs-style `.d.ts`
+    // files in `x-typescript-types`. While our behaviour is correct without
+    // ignoring this diagnostic, that may be too disruptive for now.
+    2306,
     // TS2688: Cannot find type definition file for '...'.
     // We ignore because type defintion files can end with '.ts'.
     2688,
