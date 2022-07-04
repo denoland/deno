f5def5161d51f8a5afc78dea4ca0ba314751d837
Divy Srivastava <dj.srivastava23@gmail.com>
2022-07-04 12:48:52 +0530

    order

--- cli/bench/deno_common.js
+++ cli/bench/deno_common.js
@@ -1,13 +1,26 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
-const input = "long-string".repeat(99999);
-Deno.bench("b64_rt_long", { n: 100 }, () => {
-  atob(btoa(input));
+
+// v8 builtin that's close to the upper bound non-NOPs
+Deno.bench("date_now", { n: 5e5 }, () => {
+  Date.now();
 });
 
-Deno.bench("b64_rt_short", { n: 1e6 }, () => {
-  atob(btoa("123"));
+// Void ops measure op-overhead
+Deno.bench("op_void_sync", { n: 1e7 }, () => Deno.core.opSync("op_void_sync"));
+
+Deno.bench(
+  "op_void_async",
+  { n: 1e6 },
+  () => Deno.core.opAsync("op_void_async"),
+);
+
+// A very lightweight op, that should be highly optimizable
+Deno.bench("perf_now", { n: 5e5 }, () => {
+  performance.now();
 });
 
+// A common "language feature", that should be fast
+// also a decent representation of a non-trivial JSON-op
 {
   let i = 0;
   Deno.bench("url_parse", { n: 5e4 }, () => {
@@ -20,13 +33,22 @@ Deno.bench("blob_text_large", { n: 100 }, () => {
   new Blob([input]).text();
 });
 
-Deno.bench("date_now", { n: 5e5 }, () => {
-  Date.now();
+const input = "long-string".repeat(99999);
+Deno.bench("b64_rt_long", { n: 100 }, () => {
+  atob(btoa(input));
 });
 
-Deno.bench("perf_now", { n: 5e5 }, () => {
-  performance.now();
+Deno.bench("b64_rt_short", { n: 1e6 }, () => {
+  atob(btoa("123"));
 });
+
+{
+  const buf = new Uint8Array(100);
+  const file = Deno.openSync("/dev/zero");
+  Deno.bench("read_zero", { n: 5e5 }, () => {
+    Deno.readSync(file.rid, buf);
+  });
+}
 
 {
   // Not too large since we want to measure op-overhead not sys IO
@@ -37,14 +59,6 @@ Deno.bench("perf_now", { n: 5e5 }, () => {
   });
 }
 
-{
-  const buf = new Uint8Array(100);
-  const file = Deno.openSync("/dev/zero");
-  Deno.bench("read_zero", { n: 5e5 }, () => {
-    Deno.readSync(file.rid, buf);
-  });
-}
-
 Deno.bench(
   "read_128k",
   { n: 5e4 },
@@ -52,16 +66,3 @@ Deno.bench(
 );
 
 Deno.bench("request_new", { n: 5e5 }, () => new Request("https://deno.land"));
-
-Deno.bench("op_void_sync", { n: 1e7 }, () => Deno.core.opSync("op_void_sync"));
-
-Deno.bench(
-  "op_void_async",
-  { n: 1e6 },
-  async () => {
-    const range = new Array(1e3).fill(null);
-    for (let i = 0; i < 1e6 / 1e3; i++) {
-      await Promise.all(range.map(() => Deno.core.opAsync("op_void_async")));
-    }
-  },
-);
