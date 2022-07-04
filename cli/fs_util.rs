4b893233f6ccddc71afc522f62e34ac91b5983cd
Roj <ez@roj.im>
2022-07-04 17:21:26 +0300

    fix: use char

--- cli/fs_util.rs
+++ cli/fs_util.rs
@@ -719,7 +719,7 @@ mod tests {
           .join("child")
           .to_str()
           .unwrap()
-          .replace("/", "\\")
+          .replace('/', "\\")
       )],
       &[],
       predicate,
