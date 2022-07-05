009f5c0b3af2057b45f308e1d63a77602186982f
David Sherret <dsherret@gmail.com>
2022-07-05 19:47:14 -0400

    Add incrementing count to output.

--- cli/tests/testdata/run/stdout_write_all.ts
+++ cli/tests/testdata/run/stdout_write_all.ts
@@ -6,7 +6,7 @@ const pending = [];
 // and everything happens in order
 for (let i = 0; i < 100; i++) {
   pending.push(Deno.stdout.write(encoder.encode("Hello, ")));
-  pending.push(Deno.stdout.write(encoder.encode("world!")));
+  pending.push(Deno.stdout.write(encoder.encode(`world! ${i}`)));
   pending.push(Deno.stdout.write(encoder.encode("\n")));
 }
 
