c57d34d0d69e1d0d578fb0f57e1cfdba12f317bf
David Sherret <dsherret@users.noreply.github.com>
2022-07-05 13:00:42 -0400

    chore: unpin pin project to fix cargo publish (#15085)

--- cli/standalone.rs
+++ cli/standalone.rs
@@ -311,14 +311,7 @@ pub async fn run(
   );
   worker.execute_main_module(main_module).await?;
   worker.dispatch_load_event(&located_script_name!())?;
-
-  loop {
-    worker.run_event_loop(false).await?;
-    if !worker.dispatch_beforeunload_event(&located_script_name!())? {
-      break;
-    }
-  }
-
+  worker.run_event_loop(true).await?;
   worker.dispatch_unload_event(&located_script_name!())?;
   std::process::exit(0);
 }
