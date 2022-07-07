d510007e374f2aa706ae94b800d0aecdddfa8c85
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-07-08 01:10:25 +0200

    Fmt

--- 1m.js
+++ 1m.js
@@ -4,20 +4,20 @@ const conn = new Connection("inserts.db");
 {
   // Pragmas
   const smth = conn.prepare(
-  `PRAGMA journal_mode = OFF;
+    `PRAGMA journal_mode = OFF;
 PRAGMA synchronous = 0;
 PRAGMA cache_size = 1000000;
 PRAGMA locking_mode = EXCLUSIVE;
-PRAGMA temp_store = MEMORY;`
+PRAGMA temp_store = MEMORY;`,
   );
   smth.query();
 
   const table = conn.prepare(
-  `CREATE TABLE IF NOT EXISTS user (
+    `CREATE TABLE IF NOT EXISTS user (
   id INTEGER not null primary key,
   age INTEGER not null,
   active INTEGER not null
-);`
+);`,
   );
   table.run();
 }
