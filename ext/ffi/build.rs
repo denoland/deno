17d8c8c8013df98cf0d5ce9ef04c24578e910f27
Divy Srivastava <dj.srivastava23@gmail.com>
2022-07-11 10:43:06 +0530

    win: use prebuilt libtcc

--- ext/ffi/build.rs
+++ ext/ffi/build.rs
@@ -6,33 +6,19 @@ use std::process::exit;
 use std::process::Command;
 
 fn build_tcc() {
-  let tcc_src = env::current_dir().unwrap().join("tinycc");
-  let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+  let root = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR")))
+    .parent()
+    .unwrap()
+    .to_path_buf();
   #[cfg(target_os = "windows")]
   {
-    let mut build_tcc_bat = Command::new("cmd");
-    let win32_dir = tcc_src.join("win32");
-    build_tcc_bat.current_dir(&win32_dir).args(&[
-      "/C",
-      "build-tcc.bat",
-      "-c",
-      "cl",
-    ]);
-    let status = build_tcc_bat.status().unwrap();
-    if !status.success() {
-      eprintln!("Fail to run build-tcc.bat: {:?}", status);
-      exit(1);
-    }
-    println!("cargo:rustc-link-search=native={}", win32_dir.display());
-    // check if libtcc.lib exists
-    let libtcc_lib = win32_dir.join("libtcc.lib");
-    if !libtcc_lib.exists() {
-      eprintln!("libtcc.lib not found");
-      exit(1);
-    }
+    let tcc_path = root.join("third_party").join("prebuilt").join("win");
+    println!("cargo:rustc-link-search=native={}", tcc_path.display());
   }
   #[cfg(not(target_os = "windows"))]
   {
+    let tcc_src = root.join("ext").join("ffi").join("tinycc");
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let mut configure = Command::new(tcc_src.join("configure"));
     configure.current_dir(&out_dir);
     configure.args(&["--enable-static", "--extra-cflags=-fPIC -O3 -g -static"]);
@@ -54,9 +40,9 @@ fn build_tcc() {
       eprintln!("Fail to make: {:?}", status);
       exit(1);
     }
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
+    println!("cargo:rerun-if-changed={}", tcc_src.display());
   }
-  println!("cargo:rustc-link-search=native={}", out_dir.display());
-  println!("cargo:rerun-if-changed={}", tcc_src.display());
 }
 
 fn main() {
