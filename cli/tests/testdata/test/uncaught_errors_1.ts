c18d864e9a87a6508e03387cf674fcebf32c1f02
Nayeem Rahman <nayeemrmn99@gmail.com>
2022-07-18 19:21:56 +0100

    Try rename tests

--- cli/tests/testdata/test/uncaught_errors_1.ts
+++ cli/tests/testdata/test/uncaught_errors_1.ts
@@ -1,9 +1,9 @@
-Deno.test("foo 1", () => {
+Deno.test("foo 1 (renamed)", () => {
   throw new Error("foo 1 message");
 });
 
-Deno.test("foo 2", () => {});
+Deno.test("foo 2 (renamed)", () => {});
 
-Deno.test("foo 3", () => {
+Deno.test("foo 3 (renamed)", () => {
   Promise.reject(new Error("foo 3 message"));
 });
