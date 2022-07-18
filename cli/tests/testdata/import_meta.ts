999cbfb52bc42c58467e3592d5fead9c4ee8a2c1
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-07-18 20:05:26 +0200

    feat: import.meta.resolve() (#15074)
    This commit adds new "import.meta.resolve()" API which
    allows to resolve specifiers relative to the module the API
    is called in. This API supports resolving using import maps.

--- cli/tests/testdata/import_meta.ts
+++ cli/tests/testdata/import_meta.ts
@@ -1,3 +1,34 @@
+import { assertThrows } from "../../../test_util/std/testing/asserts.ts";
+
 console.log("import_meta", import.meta.url, import.meta.main);
 
 import "./import_meta2.ts";
+
+console.log("Resolving ./foo.js", import.meta.resolve("./foo.js"));
+console.log("Resolving bare from import map", import.meta.resolve("bare"));
+console.log(
+  "Resolving https://example.com/rewrite from import map",
+  import.meta.resolve("https://example.com/rewrite"),
+);
+console.log(
+  "Resolving without a value from import map",
+  import.meta.resolve(),
+);
+console.log(
+  "Resolving 1 from import map",
+  import.meta.resolve(1),
+);
+console.log(
+  "Resolving null from import map",
+  import.meta.resolve(null),
+);
+console.log(
+  "Resolving object from import map",
+  import.meta.resolve({}),
+);
+assertThrows(() => {
+  import.meta.resolve("too", "many", "arguments");
+}, TypeError);
+assertThrows(() => {
+  import.meta.resolve("://malformed/url?asdf");
+}, TypeError);
