fad8d7d32a08095c82af39f77e3280c52a7ab2ae
Divy Srivastava <dj.srivastava23@gmail.com>
2022-07-27 19:32:21 +0530

    perf(ext/ffi): support Uint8Array in fast calls (#15319)

--- ext/ffi/prelude.h
+++ ext/ffi/prelude.h
@@ -17,3 +17,9 @@ typedef unsigned long int uint64_t;
 /* Types for `void *' pointers.  */
 typedef long int intptr_t;
 typedef unsigned long int uintptr_t;
+
+// https://source.chromium.org/chromium/chromium/src/+/main:v8/include/v8-fast-api-calls.h;l=336
+struct FastApiTypedArray {
+  uintptr_t length_;
+  void* data;
+};
