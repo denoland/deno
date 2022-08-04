6e60a924d323186042d1a67ef5a14a2c86032bc0
Divy Srivastava <dj.srivastava23@gmail.com>
2022-08-04 08:20:55 +0530

    add node react-dom benchmark

--- cli/bench/http/deno_http_flash.js
+++ cli/bench/http/deno_http_flash.js
@@ -1,16 +1,5 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
-// const requests = new Deno.flash.HttpConn();
-// for await (const { respondWith } of requests) {
-//  respondWith(new Response("Hello World")).catch(console.error);
-// }
-
 const { serve } = Deno.flash;
 serve(async (req) => {
-  try {
-    console.log("JSON response", await req.json());
-  } catch (e) {
-    console.log("Failed to parse JSON", e);
-  }
-
   return new Response("Hello World");
 });
