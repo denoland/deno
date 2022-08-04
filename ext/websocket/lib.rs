0aa17e5b5eda8480f998ec1b654c28d355c88ffe
crowlkats <crowlkats@toaxl.com>
2022-08-04 15:25:13 +0200

    fix: increase websocket message size

--- ext/websocket/lib.rs
+++ ext/websocket/lib.rs
@@ -41,11 +41,13 @@ use tokio_rustls::rustls::RootCertStore;
 use tokio_rustls::rustls::ServerName;
 use tokio_rustls::TlsConnector;
 use tokio_tungstenite::client_async;
+use tokio_tungstenite::client_async_with_config;
 use tokio_tungstenite::tungstenite::handshake::client::Response;
 use tokio_tungstenite::tungstenite::protocol::frame::coding::CloseCode;
 use tokio_tungstenite::tungstenite::protocol::CloseFrame;
 use tokio_tungstenite::tungstenite::protocol::Message;
 use tokio_tungstenite::tungstenite::protocol::Role;
+use tokio_tungstenite::tungstenite::protocol::WebSocketConfig;
 use tokio_tungstenite::MaybeTlsStream;
 use tokio_tungstenite::WebSocketStream;
 
@@ -84,8 +86,15 @@ pub async fn ws_create_server_stream(
   state: &Rc<RefCell<OpState>>,
   transport: hyper::upgrade::Upgraded,
 ) -> Result<ResourceId, AnyError> {
-  let ws_stream =
-    WebSocketStream::from_raw_socket(transport, Role::Server, None).await;
+  let ws_stream = WebSocketStream::from_raw_socket(
+    transport,
+    Role::Server,
+    Some(WebSocketConfig {
+      max_message_size: Some(128 << 20),
+      ..Default::default()
+    }),
+  )
+  .await;
   let (ws_tx, ws_rx) = ws_stream.split();
 
   let ws_resource = WsStreamResource {
@@ -322,7 +331,14 @@ where
     _ => unreachable!(),
   };
 
-  let client = client_async(request, socket);
+  let client = client_async_with_config(
+    request,
+    socket,
+    Some(WebSocketConfig {
+      max_message_size: Some(128 << 20),
+      ..Default::default()
+    }),
+  );
   let (stream, response): (WsStream, Response) =
     if let Some(cancel_resource) = cancel_resource {
       client.or_cancel(cancel_resource.0.to_owned()).await?
