cddee4e2440437cfc39d70423ef2a896319eb847
Divy Srivastava <dj.srivastava23@gmail.com>
2022-08-05 11:09:30 +0530

    update raw ops bench

--- cli/bench/http/deno_http_flash_ops.js
+++ cli/bench/http/deno_http_flash_ops.js
@@ -2,15 +2,16 @@
 
 // deno-lint-ignore-file
 
-const { core } = Deno;
-const listener = core.opAsync("op_flash_serve");
+const { core: { opAsync, ops: { op_flash_next, op_flash_respond, op_flash_serve } } } = Deno;
+const serverId = op_flash_serve({ hostname: "127.0.0.1", port: 9000 });
+const serverPromise = opAsync("op_flash_drive_server", serverId);
+
 // FIXME(bartlomieju): should be a field on "listener"
-const serverId = 0;
 while (true) {
-  let token = core.ops.op_flash_next(serverId);
-  if (token === 0) token = await core.opAsync("op_flash_next_async", serverId);
+  let token = op_flash_next();
+  if (token === 0) token = await opAsync("op_flash_next_async", serverId);
   for (let i = 0; i < token; i++) {
-    core.ops.op_flash_respond(
+    op_flash_respond(
       serverId,
       i,
       "HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\nHello World",
