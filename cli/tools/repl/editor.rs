fb03ea5e9bfd90aac5aa78635a96d84e94b6e0ab
sigmaSd <bedisnbiba@gmail.com>
2022-08-09 22:07:34 +0000

    feat(repl): Add color to functions

--- cli/tools/repl/editor.rs
+++ cli/tools/repl/editor.rs
@@ -26,6 +26,7 @@ use rustyline::Modifiers;
 use rustyline::{ConditionalEventHandler, Event, EventContext, RepeatCount};
 use rustyline_derive::{Helper, Hinter};
 use std::borrow::Cow;
+use std::collections::VecDeque;
 use std::path::PathBuf;
 use std::sync::Arc;
 
@@ -304,7 +305,9 @@ impl Highlighter for EditorHelper {
   fn highlight<'l>(&self, line: &'l str, _: usize) -> Cow<'l, str> {
     let mut out_line = String::from(line);
 
-    for item in deno_ast::lex(line, deno_ast::MediaType::TypeScript) {
+    let mut lexed_items: VecDeque<_> =
+      deno_ast::lex(line, deno_ast::MediaType::TypeScript).into();
+    while let Some(item) = lexed_items.pop_front() {
       // Adding color adds more bytes to the string,
       // so an offset is needed to stop spans falling out of sync.
       let offset = out_line.len() - line.len();
@@ -334,7 +337,14 @@ impl Highlighter for EditorHelper {
                 } else if ident == *"async" || ident == *"of" {
                   colors::cyan(&line[range]).to_string()
                 } else {
-                  line[range].to_string()
+                  let next = lexed_items.get(0).map(|item|&item.inner);
+                  if matches!(next, Some(deno_ast::TokenOrComment::Token(next_token)) if matches!(next_token, Token::LParen)) {
+                    // We're looking for something that looks like a function
+                    // We use a simple heuristic: 'ident' followed by 'LParen'
+                    colors::intense_blue(&line[range]).to_string()
+                  } else {
+                    line[range].to_string()
+                  }
                 }
               }
             },
