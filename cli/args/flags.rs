0884f0fa5567062561e54a00df595dfa2be116fb
David Sherret <dsherret@gmail.com>
2022-08-10 09:45:42 -0400

    Revert "Keep raw_args in sync with matches.index_of result."
    This reverts commit 6b8c492b09bebc2d1a04bf844d108374907b3867.

--- cli/args/flags.rs
+++ cli/args/flags.rs
@@ -582,7 +582,15 @@ pub fn flags_from_vec(args: Vec<String>) -> clap::Result<Flags> {
     Some(("lsp", m)) => lsp_parse(&mut flags, m),
     Some(("repl", m)) => repl_parse(&mut flags, m),
     Some(("run", m)) => run_parse(&mut flags, m),
-    Some(("task", m)) => task_parse(&mut flags, m, &args),
+    Some(("task", m)) => task_parse(
+      &mut flags,
+      m,
+      // temporary workaround until https://github.com/clap-rs/clap/issues/1538 is fixed
+      &args[args
+        .iter()
+        .position(|el| el == matches.subcommand_name().unwrap())
+        .unwrap()..],
+    ),
     Some(("test", m)) => test_parse(&mut flags, m),
     Some(("types", m)) => types_parse(&mut flags, m),
     Some(("uninstall", m)) => uninstall_parse(&mut flags, m),
@@ -2637,6 +2645,9 @@ fn run_parse(flags: &mut Flags, matches: &clap::ArgMatches) {
   flags.subcommand = DenoSubcommand::Run(RunFlags { script });
 }
 
+/// raw_args is the subcommand's command line, eg. for the deno invocation
+/// `deno task echo`
+/// raw_args has value ["task", "echo"]
 fn task_parse(
   flags: &mut Flags,
   matches: &clap::ArgMatches,
@@ -2658,9 +2669,6 @@ fn task_parse(
   }
 
   if let Some(mut index) = matches.index_of("task_name_and_args") {
-    let task_word_index = raw_args.iter().position(|el| el == "task").unwrap();
-    index = std::cmp::max(task_word_index + 1, index); // ensure skip `task`
-
     // temporary workaround until https://github.com/clap-rs/clap/issues/1538 is fixed
     while index < raw_args.len() {
       match raw_args[index].as_str() {
