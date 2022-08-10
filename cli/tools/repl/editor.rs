e8588ecfbb85892f14ec51d5a70578f9e5e626ea
sigmaSd <bedisnbiba@gmail.com>
2022-08-10 07:17:34 +0000

    use iterator and peekable

--- cli/tools/repl/editor.rs
+++ cli/tools/repl/editor.rs
@@ -26,7 +26,6 @@ use rustyline::Modifiers;
 use rustyline::{ConditionalEventHandler, Event, EventContext, RepeatCount};
 use rustyline_derive::{Helper, Hinter};
 use std::borrow::Cow;
-use std::collections::VecDeque;
 use std::path::PathBuf;
 use std::sync::Arc;
 
@@ -305,9 +304,10 @@ impl Highlighter for EditorHelper {
   fn highlight<'l>(&self, line: &'l str, _: usize) -> Cow<'l, str> {
     let mut out_line = String::from(line);
 
-    let mut lexed_items: VecDeque<_> =
-      deno_ast::lex(line, deno_ast::MediaType::TypeScript).into();
-    while let Some(item) = lexed_items.pop_front() {
+    let mut lexed_items = deno_ast::lex(line, deno_ast::MediaType::TypeScript)
+      .into_iter()
+      .peekable();
+    while let Some(item) = lexed_items.next() {
       // Adding color adds more bytes to the string,
       // so an offset is needed to stop spans falling out of sync.
       let offset = out_line.len() - line.len();
@@ -337,7 +337,7 @@ impl Highlighter for EditorHelper {
                 } else if ident == *"async" || ident == *"of" {
                   colors::cyan(&line[range]).to_string()
                 } else {
-                  let next = lexed_items.get(0).map(|item|&item.inner);
+                  let next = lexed_items.peek().map(|item|&item.inner);
                   if matches!(next, Some(deno_ast::TokenOrComment::Token(next_token)) if matches!(next_token, Token::LParen)) {
                     // We're looking for something that looks like a function
                     // We use a simple heuristic: 'ident' followed by 'LParen'
