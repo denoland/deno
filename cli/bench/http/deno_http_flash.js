6c1dd3744c415810dbc692c35dc5323868f3639f
Divy Srivastava <dj.srivastava23@gmail.com>
2022-08-16 10:01:56 +0530

    lint

--- cli/bench/http/deno_http_flash.js
+++ cli/bench/http/deno_http_flash.js
@@ -1,12 +1,10 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
-// deno-lint-ignore-file no-unused-vars require-await
-
 const addr = Deno.args[0] || "127.0.0.1:4500";
 const [hostname, port] = addr.split(":");
 const { serve } = Deno;
 
-async function handler() {
+function handler() {
   return new Response("Hello World");
 }
 
