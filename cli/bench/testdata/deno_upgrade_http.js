3e8f29352966be54eb272916cf41afd383de20f2
Luca Casonato <hello@lcas.dev>
2022-08-23 18:10:25 +0200

    fix benches

--- cli/bench/testdata/deno_upgrade_http.js
+++ cli/bench/testdata/deno_upgrade_http.js
@@ -1,14 +1,14 @@
 const { serve, upgradeHttp } = Deno;
 const u8 = Deno.core.encode("HTTP/1.1 101 Switching Protocols\r\n\r\n");
 
-async function fetch(req) {
+async function handler(req) {
   const [conn, _firstPacket] = upgradeHttp(req);
   await conn.write(u8);
   await conn.close();
 }
 
 serve({
-  fetch,
+  handler,
   hostname: "127.0.0.1",
   port: 9000,
 });
