452df99222911c772657c39491469bd97935f23a
David Sherret <dsherret@users.noreply.github.com>
2022-08-23 22:01:21 -0400

    feat(npm): support packages with multiple command names (#15565)

--- cli/tests/integration/npm_tests.rs
+++ cli/tests/integration/npm_tests.rs
@@ -184,6 +184,27 @@ fn deno_run_cjs_module() {
   assert!(deno_dir.path().join("test_dir").exists());
 }
 
+itest!(deno_run_cowsay {
+  args: "run --unstable -A --quiet npm:cowsay@1.5.0 Hello",
+  output: "npm/deno_run_cowsay.out",
+  envs: env_vars_no_sync_download(),
+  http_server: true,
+});
+
+itest!(deno_run_cowsay_explicit {
+  args: "run --unstable -A --quiet npm:cowsay@1.5.0/cowsay Hello",
+  output: "npm/deno_run_cowsay.out",
+  envs: env_vars_no_sync_download(),
+  http_server: true,
+});
+
+itest!(deno_run_cowthink {
+  args: "run --unstable -A --quiet npm:cowsay@1.5.0/cowthink Hello",
+  output: "npm/deno_run_cowthink.out",
+  envs: env_vars_no_sync_download(),
+  http_server: true,
+});
+
 itest!(deno_run_non_existent {
   args: "run --unstable npm:mkdirp@0.5.125",
   output: "npm/deno_run_non_existent.out",
