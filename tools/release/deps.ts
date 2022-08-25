5717fcc58d2d12afaeee43c322e32dc9f2ff6ab5
David Sherret <dsherret@gmail.com>
2022-08-25 09:28:02 -0400

    Update

--- tools/release/deps.ts
+++ tools/release/deps.ts
@@ -1,4 +1,4 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
-export * from "../../../deno_automation/mod.ts";
-export * from "../../../deno_automation/github_actions.ts";
+export * from "https://raw.githubusercontent.com/denoland/automation/0.12.2/mod.ts";
+export * from "https://raw.githubusercontent.com/denoland/automation/0.12.2/github_actions.ts";
