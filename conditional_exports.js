eea600b2e8dfeb473c867d91d0fce436e97e2450
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-08-28 12:07:50 +0200

    fix(npm): conditional exports with wildcards

---
+++ conditional_exports.js
@@ -0,0 +1,4 @@
+import { SendOperationOptions } from "npm:@azure/notification-hubs@1.0.0-beta.2/models/options";
+import { createClientContext } from "npm:@azure/notification-hubs@1.0.0-beta.2/client";
+import { createAppleNotification } from "npm:@azure/notification-hubs@1.0.0-beta.2/models/notification";
+import { sendDirectNotification } from "npm:@azure/notification-hubs@1.0.0-beta.2/client/sendDirectNotification";
