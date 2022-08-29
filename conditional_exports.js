6cfda2f1265861ce9d0e278f4a095159b321c35e
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-08-29 14:58:28 +0200

    remove unneeded file

--- conditional_exports.js
+++
@@ -1,6 +0,0 @@
-// deno-lint-ignore-file
-
-import { SendOperationOptions } from "npm:@azure/notification-hubs@1.0.0-beta.2/models/options";
-import { createClientContext } from "npm:@azure/notification-hubs@1.0.0-beta.2/client";
-import { createAppleNotification } from "npm:@azure/notification-hubs@1.0.0-beta.2/models/notification";
-import { sendDirectNotification } from "npm:@azure/notification-hubs@1.0.0-beta.2/client/sendDirectNotification";
