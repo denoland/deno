b2e5e885de6ad26c6e95f5e1c595a943d440de68
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-09-05 22:42:18 +0200

    fmt

--- ext/node/resolution.rs
+++ ext/node/resolution.rs
@@ -709,10 +709,7 @@ pub fn legacy_main_resolve(
 ) -> Result<PathBuf, AnyError> {
   let maybe_main =
     if referrer_kind == NodeModuleKind::Esm && package_json.typ == "module" {
-      package_json
-        .module
-        .as_ref()
-        .or(package_json.main.as_ref())
+      package_json.module.as_ref().or(package_json.main.as_ref())
     } else {
       package_json.main.as_ref()
     };
