b650a0ae2223c93554def0746111514b18128ce1
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-09-08 18:38:17 +0200

    store is_tty

--- cli/progress_bar.rs
+++ cli/progress_bar.rs
@@ -6,7 +6,22 @@ use std::sync::Arc;
 use std::time::Duration;
 
 #[derive(Clone, Debug, Default)]
-pub struct ProgressBar(Arc<Mutex<Option<indicatif::ProgressBar>>>);
+pub struct ProgressBar(Arc<Mutex<ProgressBarInner>>);
+
+#[derive(Debug)]
+struct ProgressBarInner {
+  pb: Option<indicatif::ProgressBar>,
+  is_tty: bool,
+}
+
+impl Default for ProgressBarInner {
+  fn default() -> Self {
+    Self {
+      pb: None,
+      is_tty: colors::is_tty(),
+    }
+  }
+}
 
 impl ProgressBar {
   fn create() -> indicatif::ProgressBar {
@@ -26,19 +41,19 @@ impl ProgressBar {
   pub fn update(&self, msg: &str) {
     let mut inner = self.0.lock();
 
-    let progress_bar = match inner.as_ref() {
+    // If we're not running in TTY we're just gonna fallback
+    // to using logger crate.
+    if !inner.is_tty {
+      log::log!(log::Level::Info, "{} {}", colors::green("Download"), msg);
+      return;
+    }
+
+    let progress_bar = match inner.pb.as_ref() {
       Some(pb) => pb,
       None => {
-        // If we're not running in TTY we're just gonna fallback
-        // to using logger crate.
-        if !colors::is_tty() {
-          log::log!(log::Level::Info, "{} {}", colors::green("Download"), msg);
-          return;
-        }
-
         let pb = Self::create();
-        *inner = Some(pb);
-        inner.as_ref().unwrap()
+        inner.pb = Some(pb);
+        inner.pb.as_ref().unwrap()
       }
     };
     progress_bar.set_message(msg.to_string());
@@ -47,10 +62,10 @@ impl ProgressBar {
   pub fn finish(&self) {
     let mut inner = self.0.lock();
 
-    match inner.as_ref() {
+    match inner.pb.as_ref() {
       Some(pb) => {
         pb.finish_and_clear();
-        *inner = None;
+        inner.pb = None;
       }
       None => {}
     };
