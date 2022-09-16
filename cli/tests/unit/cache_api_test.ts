d0e2ba327d359f8169554733892fb7b4a772b710
Satya Rohith <me@satyarohith.com>
2022-09-16 22:21:22 +0530

    clean up

--- cli/tests/unit/cache_api_test.ts
+++ cli/tests/unit/cache_api_test.ts
@@ -1,50 +1,66 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
-import { assertEquals } from "../../../test_util/std/testing/asserts.ts";
-import { assert, assertFalse } from "./test_util.ts";
+import {
+  assert,
+  assertEquals,
+  assertFalse,
+  assertRejects,
+} from "./test_util.ts";
 
-Deno.test(
-  { permissions: {} },
-  async function cacheStorage() {
-    const cacheName = "cache-v1";
-    assertFalse(await caches.has(cacheName));
-    const _cache = await caches.open(cacheName);
-    assert(await caches.has(cacheName));
-    assert(await caches.delete(cacheName));
-  },
-);
+Deno.test(async function cacheStorage() {
+  const cacheName = "cache-v1";
+  const _cache = await caches.open(cacheName);
+  assert(await caches.has(cacheName));
+  assert(await caches.delete(cacheName));
+  assertFalse(await caches.has(cacheName));
+});
 
-Deno.test(
-  { permissions: {} },
-  async function cacheApi() {
-    const cacheName = "cache-v1";
-    assertFalse(await caches.has(cacheName));
-    const cache = await caches.open(cacheName);
-    // Test cache.put() with url string as key.
-    {
-      const req = "https://deno.com";
-      await cache.put(req, new Response("deno.com - key is string"));
-      const res = await cache.match(req);
-      assertEquals(await res?.text(), "deno.com - key is string");
-      assert(await cache.delete(req));
-    }
-    // Test cache.put() with url instance as key.
-    {
-      const req = new URL("https://deno.com");
-      await cache.put(req, new Response("deno.com - key is URL"));
-      const res = await cache.match(req);
-      assertEquals(await res?.text(), "deno.com - key is URL");
-      assert(await cache.delete(req));
-    }
+Deno.test(async function cacheApi() {
+  const cacheName = "cache-v1";
+  const cache = await caches.open(cacheName);
+  // Test cache.put() with url string as key.
+  {
+    const req = "https://deno.com";
+    await cache.put(req, new Response("deno.com - key is string"));
+    const res = await cache.match(req);
+    assertEquals(await res?.text(), "deno.com - key is string");
+    assert(await cache.delete(req));
+  }
+  // Test cache.put() with url instance as key.
+  {
+    const req = new URL("https://deno.com");
+    await cache.put(req, new Response("deno.com - key is URL"));
+    const res = await cache.match(req);
+    assertEquals(await res?.text(), "deno.com - key is URL");
+    assert(await cache.delete(req));
+  }
+  // Test cache.put() with request instance as key.
+  {
+    const req = new Request("https://deno.com");
+    await cache.put(req, new Response("deno.com - key is Request"));
+    const res = await cache.match(req);
+    assertEquals(await res?.text(), "deno.com - key is Request");
+    assert(await cache.delete(req));
+  }
 
-    // Test cache.put() with request instance as key.
-    {
-      const req = new Request("https://deno.com");
-      await cache.put(req, new Response("deno.com - key is Request"));
-      const res = await cache.match(req);
-      assertEquals(await res?.text(), "deno.com - key is Request");
-      assert(await cache.delete(req));
-    }
+  // Test cache.put() throws with response Vary header set to *.
+  {
+    const req = new Request("https://deno.com");
+    assertRejects(
+      async () => {
+        await cache.put(
+          req,
+          new Response("deno.com - key is Request", {
+            headers: { Vary: "*" },
+          }),
+        );
+      },
+      TypeError,
+      "Vary header must not contain '*'",
+    );
+  }
 
-    await caches.delete(cacheName);
-  },
-);
+  // TODO(@satyarohith): more tests with Vary header.
+
+  assert(await caches.delete(cacheName));
+  assertFalse(await caches.has(cacheName));
+});
