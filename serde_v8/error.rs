af0d2eeb7b79638c2cdf5c27537d8795312d60f9
Ben Noordhuis <info@bnoordhuis.nl>
2022-09-19 10:25:03 +0200

    chore: fix clippy warnings (#15944)
    Stop allowing clippy::derive-partial-eq-without-eq and fix warnings
    about deriving PartialEq without also deriving Eq.
    
    In one case I removed the PartialEq because it a) wasn't necessary,
    and b) sketchy because it was comparing floating point numbers.
    
    IMO, that's a good argument for enforcing the lint rule, because it
    would most likely have been caught during review if it had been enabled.

--- serde_v8/error.rs
+++ serde_v8/error.rs
@@ -5,7 +5,7 @@ use serde::{de, ser};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 #[non_exhaustive]
 pub enum Error {
   Message(String),
