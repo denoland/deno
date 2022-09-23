4d2222a70e8d7deae0cac4255cab219cf26adcc9
Satya Rohith <me@satyarohith.com>
2022-09-23 18:11:57 +0530

    add comments to lib.deno_cache.d.ts

--- ext/cache/lib.deno_cache.d.ts
+++ ext/cache/lib.deno_cache.d.ts
@@ -10,15 +10,39 @@ declare var caches: CacheStorage;
 
 /** @category Cache API */
 declare interface CacheStorage {
+  /** Open a cache storage for the provided name. */
   open(cacheName: string): Promise<Cache>;
+  /** Check if cache already exists for the provided name. */
   has(cacheName: string): Promise<boolean>;
+  /** Delete cache storage for the provided name. */
   delete(cacheName: string): Promise<boolean>;
 }
 
 /** @category Cache API */
 declare interface Cache {
+  /**
+   * Put the provided request/response into the cache.
+   *
+   * How is the API different from browsers?
+   * 1. You cannot match cache objects using by relative paths.
+   * 2. You cannot pass options like `ignoreVary`, `ignoreMethod`, `ignoreSearch`.
+   */
   put(request: RequestInfo | URL, response: Response): Promise<void>;
+  /**
+   * Return cache object matching the provided request.
+   *
+   * How is the API different from browsers?
+   * 1. You cannot match cache objects using by relative paths.
+   * 2. You cannot pass options like `ignoreVary`, `ignoreMethod`, `ignoreSearch`.
+   */
   match(request: RequestInfo | URL): Promise<Response | undefined>;
+  /**
+   * Delete cache object matching the provided request.
+   *
+   * How is the API different from browsers?
+   * 1. You cannot delete cache objects using by relative paths.
+   * 2. You cannot pass options like `ignoreVary`, `ignoreMethod`, `ignoreSearch`.
+   */
   delete(request: RequestInfo | URL): Promise<boolean>;
 }
 
