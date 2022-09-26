b513735f0a024b39c4d6112c02655f7848d41834
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-09-26 22:15:38 +0200

    update unstable check

--- runtime/ops/tty.rs
+++ runtime/ops/tty.rs
@@ -53,7 +53,7 @@ fn op_stdin_set_raw(
   is_raw: bool,
   cbreak: bool,
 ) -> Result<(), AnyError> {
-  super::check_unstable(state, "Deno.setRaw");
+  super::check_unstable(state, "Deno.stdin.setRaw");
 
   let rid = 0; // stdin is always rid=0
 
