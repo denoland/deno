d2096b6c60c65fc6644cbb4a3afc52bb0e27a830
Divy Srivastava <dj.srivastava23@gmail.com>
2022-09-26 11:53:19 +0530

    fix

--- cli/bench/http/deno_http_flash_spawn.js
+++ cli/bench/http/deno_http_flash_spawn.js
@@ -8,7 +8,9 @@ const path = new URL("./deno_http_flash.js", import.meta.url).pathname;
 const cpus = navigator.hardwareConcurrency / 2;
 const processes = new Array(cpus);
 for (let i = 0; i < cpus; i++) {
-  const proc = Deno.run({ cmd: [executable, "run", "-A", "--unstable", path] });
+  const proc = Deno.run({
+    cmd: [executable, "run", "-A", "--unstable", path, Deno.args[0]],
+  });
   processes.push(proc.status());
 }
 await Promise.all(processes);
