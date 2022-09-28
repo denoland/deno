a38b20e0b266b1dc0a0366d3977ec04d89b4aa32
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-28 17:21:38 +0900

    fix: add cfg

--- runtime/ops/fs.rs
+++ runtime/ops/fs.rs
@@ -1070,6 +1070,7 @@ fn get_stat(metadata: std::fs::Metadata) -> FsStat {
 
   #[cfg(unix)]
   use std::os::unix::fs::FileTypeExt;
+  #[cfg(unix)]
   use std::os::unix::fs::MetadataExt;
   let (mtime, mtime_set) = to_msec(metadata.modified());
   let (atime, atime_set) = to_msec(metadata.accessed());
