13bed6a82873b8b41a8c3dfa380e9754b1d5877c
sigmaSd <bedisnbiba@gmail.com>
2022-10-08 09:36:47 +0000

    fix sparse array inspection

--- ext/console/02_console.js
+++ ext/console/02_console.js
@@ -417,6 +417,7 @@
         }
     }
 
+    let entriesLengthWithoutEmptyItems = entriesLength
     if (options.typeName === "Array") {
       for (
         let i = 0, j = 0;
@@ -433,9 +434,10 @@
 
         if (skipTo) {
           // subtract skipped (empty) items
-          entriesLength -= skipTo - i;
+          entriesLengthWithoutEmptyItems -= skipTo - i;
           i = skipTo;
         }
+
       }
     } else {
       let i = 0;
@@ -478,8 +480,8 @@
       ArrayPrototypeSort(entries);
     }
 
-    if (entriesLength > inspectOptions.iterableLimit) {
-      const nmore = entriesLength - inspectOptions.iterableLimit;
+    if (entriesLengthWithoutEmptyItems > inspectOptions.iterableLimit) {
+      const nmore = entriesLengthWithoutEmptyItems - inspectOptions.iterableLimit;
       ArrayPrototypePush(entries, `... ${nmore} more items`);
     }
 
