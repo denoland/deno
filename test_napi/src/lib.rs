70c4ff271c7dbb5702c641617e33882ec0d149f9
AaronDewes <aaron.dewes@protonmail.com>
2022-10-09 12:25:25 +0000

    Fix one remaining use of c_char

--- test_napi/src/lib.rs
+++ test_napi/src/lib.rs
@@ -3,6 +3,7 @@
 #![allow(clippy::undocumented_unsafe_blocks)]
 
 use napi_sys::*;
+use std::os::raw::c_char;
 
 pub mod array;
 pub mod r#async;
@@ -42,7 +43,7 @@ macro_rules! get_callback_info {
 macro_rules! new_property {
   ($env: expr, $name: expr, $value: expr) => {
     napi_property_descriptor {
-      utf8name: $name.as_ptr() as *const i8,
+      utf8name: $name.as_ptr() as *const c_char,
       name: ptr::null_mut(),
       method: Some($value),
       getter: None,
