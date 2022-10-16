ea1b034386f1327bf016d29449363a6daf0d582d
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2022-10-16 22:32:42 +0100

    Fix linux symbols export list format

--- tools/napi/generate_symbols_lists.js
+++ tools/napi/generate_symbols_lists.js
@@ -5,15 +5,19 @@ import exports from "../../cli/napi_sym/symbol_exports.json" assert {
   type: "json",
 };
 
-for await (const os of ["linux", "macos", "windows"]) {
-  let def = os === "windows" ? "LIBRARY\nEXPORTS\n" : "";
-  const prefix = os === "windows" ? "  " : os === "macos" ? "_" : "";
-  for (const symbol of exports.symbols) {
-    def += `${prefix}${symbol}\n`;
-  }
+const symbolExportLists = {
+  linux: `{ ${exports.symbols.map((s) => `"${s}"`).join("; ")}; };`,
+  windows: `LIBRARY\nEXPORTS\n${
+    exports.symbols
+      .map((symbol) => "  " + symbol)
+      .join("\n")
+  }`,
+  macos: exports.symbols.map((symbol) => "_" + symbol).join("\n"),
+};
 
+for await (const [os, def] of Object.entries(symbolExportLists)) {
   const defUrl = new URL(
-    `../../cli/generated_symbol_exports_list_${os}.def`,
+    `../../cli/napi_sym/generated_symbol_exports_list_${os}.def`,
     import.meta.url,
   );
   await Deno.writeTextFile(defUrl.pathname, def, { create: true });
