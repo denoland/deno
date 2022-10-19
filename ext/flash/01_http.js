0238d3ffc74e165bb35a497ee86072ef9fccff1e
Yusuke Tanaka <yusuktan@maguro.dev>
2022-10-19 11:49:29 +0900

    revert some stuff to the original

--- ext/flash/01_http.js
+++ ext/flash/01_http.js
@@ -196,7 +196,7 @@
     return hostname === "0.0.0.0" ? "localhost" : hostname;
   }
 
-  async function writeFixedResponse(
+  function writeFixedResponse(
     server,
     requestId,
     response,
@@ -219,7 +219,7 @@
     }
 
     if (nwritten < responseLen) {
-      await core.opAsync(
+      core.opAsync(
         "op_flash_respond_async",
         server,
         requestId,
@@ -313,7 +313,7 @@
         respBody,
         length,
       );
-      await writeFixedResponse(
+      writeFixedResponse(
         serverId,
         i,
         responseStr,
@@ -323,95 +323,97 @@
       );
     }
 
-    if (!ws) {
-      if (hasBody && body[_state] !== "closed") {
-        // TODO(@littledivy): Optimize by draining in a single op.
-        try {
-          await req.arrayBuffer();
-        } catch { /* pass */ }
-      }
-    }
-
-    if (isStreamingResponseBody === true) {
-      const resourceBacking = getReadableStreamResourceBacking(respBody);
-      if (resourceBacking) {
-        if (respBody.locked) {
-          throw new TypeError("ReadableStream is locked.");
+    (async () => {
+      if (!ws) {
+        if (hasBody && body[_state] !== "closed") {
+          // TODO(@littledivy): Optimize by draining in a single op.
+          try {
+            await req.arrayBuffer();
+          } catch { /* pass */ }
         }
-        const reader = respBody.getReader(); // Aquire JS lock.
-        try {
-          await core.opAsync(
-            "op_flash_write_resource",
+      }
+
+      if (isStreamingResponseBody === true) {
+        const resourceBacking = getReadableStreamResourceBacking(respBody);
+        if (resourceBacking) {
+          if (respBody.locked) {
+            throw new TypeError("ReadableStream is locked.");
+          }
+          const reader = respBody.getReader(); // Aquire JS lock.
+          try {
+            core.opAsync(
+              "op_flash_write_resource",
+              http1Response(
+                method,
+                innerResp.status ?? 200,
+                innerResp.headerList,
+                0, // Content-Length will be set by the op.
+                null,
+                true,
+              ),
+              serverId,
+              i,
+              resourceBacking.rid,
+              resourceBacking.autoClose,
+            ).then(() => {
+              // Release JS lock.
+              readableStreamClose(respBody);
+            });
+          } catch (error) {
+            await reader.cancel(error);
+            throw error;
+          }
+        } else {
+          const reader = respBody.getReader();
+          writeFixedResponse(
+            serverId,
+            i,
             http1Response(
               method,
               innerResp.status ?? 200,
               innerResp.headerList,
-              0, // Content-Length will be set by the op.
+              respBody.byteLength,
               null,
-              true,
             ),
-            serverId,
-            i,
-            resourceBacking.rid,
-            resourceBacking.autoClose,
-          ).then(() => {
-            // Release JS lock.
-            readableStreamClose(respBody);
-          });
-        } catch (error) {
-          await reader.cancel(error);
-          throw error;
+            respBody.byteLength,
+            false,
+            respondFast,
+          );
+          while (true) {
+            const { value, done } = await reader.read();
+            await respondChunked(
+              i,
+              value,
+              done,
+            );
+            if (done) break;
+          }
         }
-      } else {
-        const reader = respBody.getReader();
-        await writeFixedResponse(
+      }
+
+      if (ws) {
+        const wsRid = await core.opAsync(
+          "op_flash_upgrade_websocket",
           serverId,
           i,
-          http1Response(
-            method,
-            innerResp.status ?? 200,
-            innerResp.headerList,
-            respBody.byteLength,
-            null,
-          ),
-          respBody.byteLength,
-          false,
-          respondFast,
         );
-        while (true) {
-          const { value, done } = await reader.read();
-          await respondChunked(
-            i,
-            value,
-            done,
+        ws[_rid] = wsRid;
+        ws[_protocol] = resp.headers.get("sec-websocket-protocol");
+
+        ws[_readyState] = WebSocket.OPEN;
+        const event = new Event("open");
+        ws.dispatchEvent(event);
+
+        ws[_eventLoop]();
+        if (ws[_idleTimeoutDuration]) {
+          ws.addEventListener(
+            "close",
+            () => clearTimeout(ws[_idleTimeoutTimeout]),
           );
-          if (done) break;
         }
+        ws[_serverHandleIdleTimeout]();
       }
-    }
-
-    if (ws) {
-      const wsRid = await core.opAsync(
-        "op_flash_upgrade_websocket",
-        serverId,
-        i,
-      );
-      ws[_rid] = wsRid;
-      ws[_protocol] = resp.headers.get("sec-websocket-protocol");
-
-      ws[_readyState] = WebSocket.OPEN;
-      const event = new Event("open");
-      ws.dispatchEvent(event);
-
-      ws[_eventLoop]();
-      if (ws[_idleTimeoutDuration]) {
-        ws.addEventListener(
-          "close",
-          () => clearTimeout(ws[_idleTimeoutTimeout]),
-        );
-      }
-      ws[_serverHandleIdleTimeout]();
-    }
+    })();
   }
 
   async function serve(arg1, arg2) {
