34a238309bbb469f92cfcb1121319b17e55f1287
Yusuke Tanaka <yusuktan@maguro.dev>
2022-10-20 19:31:36 +0900

    wait for the first server to start listening

--- cli/tests/unit/flash_test.ts
+++ cli/tests/unit/flash_test.ts
@@ -70,6 +70,8 @@ Deno.test(async function httpServerRejectsOnAddrInUse() {
     onError: createOnErrorCb(ac),
   });
 
+  await listeningPromise;
+
   assertRejects(
     () =>
       Deno.serve({
@@ -77,7 +79,6 @@ Deno.test(async function httpServerRejectsOnAddrInUse() {
         hostname: "localhost",
         port: 4501,
         signal: ac.signal,
-        onListen: onListen(listeningPromise),
         onError: createOnErrorCb(ac),
       }),
     Deno.errors.AddrInUse,
