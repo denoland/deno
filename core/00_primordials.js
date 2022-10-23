b2e5f8023528acf7a3e65ca313365c365dbb7093
Kenta Moriuchi <moriken@kimamass.com>
2022-10-23 22:38:54 +0900

    Stop using `SafeArrayIterator`

--- core/00_primordials.js
+++ core/00_primordials.js
@@ -312,17 +312,14 @@
     return SafeIterator;
   };
 
-  const SafeArrayIterator = createSafeIterator(
+  primordials.SafeArrayIterator = createSafeIterator(
     primordials.ArrayPrototypeSymbolIterator,
     primordials.ArrayIteratorPrototypeNext,
   );
-  primordials.SafeArrayIterator = SafeArrayIterator;
-
-  const SafeStringIterator = createSafeIterator(
+  primordials.SafeStringIterator = createSafeIterator(
     primordials.StringPrototypeSymbolIterator,
     primordials.StringIteratorPrototypeNext,
   );
-  primordials.SafeStringIterator = SafeStringIterator;
 
   const copyProps = (src, dest) => {
     ArrayPrototypeForEach(ReflectOwnKeys(src), (key) => {
@@ -442,18 +439,23 @@
   primordials.PromisePrototypeCatch = (thisPromise, onRejected) =>
     PromisePrototypeThen(thisPromise, undefined, onRejected);
 
-  primordials.SafePromiseAll = (iterable) =>
+  /**
+   * Creates a Promise that is resolved with an array of results when all of the
+   * provided Promises resolve, or rejected when any Promise is rejected.
+   * @param {unknown[]} values An array of Promises.
+   * @returns A new Promise.
+   */
+  primordials.SafePromiseAll = (values) =>
     // Wrapping on a new Promise is necessary to not expose the SafePromise
     // prototype to user-land.
     new Promise((a, b) =>
       SafePromise.all(
         ArrayPrototypeMap(
-          [...new SafeArrayIterator(iterable)],
+          values,
           (p) => {
             if (ObjectPrototypeIsPrototypeOf(PromisePrototype, p)) {
               return new SafePromise((c, d) => PromisePrototypeThen(p, c, d));
             }
-
             return p;
           },
         ),
