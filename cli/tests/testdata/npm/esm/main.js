e7bea1ce9a5cefa9131882c72180aa97198927bc
David Sherret <dsherret@gmail.com>
2022-10-25 12:21:24 -0400

    Revert "chore: fix flaky esm_module_deno_test test"
    This reverts commit a78b7d97e3256282da94c3a9734e2ecec22c15f2.

--- cli/tests/testdata/npm/esm/main.js
+++ cli/tests/testdata/npm/esm/main.js
@@ -1,8 +1,6 @@
 import chalk from "npm:chalk@5";
 
-if (import.meta.main) {
-  console.log(chalk.green("chalk esm loads"));
-}
+console.log(chalk.green("chalk esm loads"));
 
 export function test(value) {
   return chalk.red(value);
