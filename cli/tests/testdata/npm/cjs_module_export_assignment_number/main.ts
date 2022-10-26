5dcf8a0b8ea4ca7a18a210eaac2ae96e7c74bd5d
David Sherret <dsherret@gmail.com>
2022-10-26 18:32:24 -0400

    Add type checking to test

--- cli/tests/testdata/npm/cjs_module_export_assignment_number/main.ts
+++ cli/tests/testdata/npm/cjs_module_export_assignment_number/main.ts
@@ -1,4 +1,7 @@
 import defaultImport, * as namespaceImport from "npm:@denotest/cjs-module-export-assignment-number";
 
-console.log(defaultImport);
+const testDefault: 5 = defaultImport;
+console.log(testDefault);
+const testNamespace: 5 = namespaceImport.default;
+console.log(testNamespace);
 console.log(namespaceImport);
