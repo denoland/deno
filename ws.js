282ae25bfe3b24987039eeac9204e3f633335b74
Divy Srivastava <dj.srivastava23@gmail.com>
2022-10-28 08:28:25 +0530

    Delete ws.js

--- ws.js
+++
@@ -1,7 +0,0 @@
-Deno.serve((req) => {
-  const { socket, response } = Deno.upgradeWebSocket(req);
-  socket.onmessage = (e) => {
-    socket.send(e.data);
-  };
-  return response;
-}, { port: 8000 });
