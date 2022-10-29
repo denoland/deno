4ce73c822c3777077bc4d09957c8e0b3ac3d61cc
Marcos Casagrande <marcoscvp90@gmail.com>
2022-10-29 23:48:10 +0200

    add test

---
+++ test_napi/arraybuffer_test.js
@@ -0,0 +1,10 @@
+import { assertEquals, loadTestLibrary } from "./common.js";
+
+const typedarray = loadTestLibrary();
+
+Deno.test("napi arraybuffer detach", function () {
+  const buf = new ArrayBuffer(5);
+  assertEquals(buf.byteLength, 5);
+  typedarray.test_detached(buf);
+  assertEquals(buf.byteLength, 0);
+});
