4c64cc657ee842f0c8dfeccca3514e57361f09c6
Divy Srivastava <dj.srivastava23@gmail.com>
2022-11-02 20:28:57 +0530

    func

--- ops/fast_call.rs
+++ ops/fast_call.rs
@@ -3,11 +3,15 @@ use crate::optimizer::Optimizer;
 use proc_macro2::{Span, TokenStream};
 use quote::{quote, ToTokens};
 use syn::{
-  punctuated::Punctuated, Fields, Ident, ItemImpl, ItemStruct, Path,
-  PathArguments, PathSegment, Token, Type, TypePath, Visibility, ItemFn, parse_quote
+  parse_quote, punctuated::Punctuated, Fields, Ident, ItemFn, ItemImpl,
+  ItemStruct, Path, PathArguments, PathSegment, Token, Type, TypePath,
+  Visibility,
 };
 
-struct FastCallImpl<'s> {
+pub(crate) fn generate(
+  optimizer: &mut Optimizer,
+  item_fn: &ItemFn,
+) -> Result<TokenStream, ()> {
   /// impl <A> fast_api::FastFunction for T <A> where A: B {
   ///   fn function(&self) -> *const ::std::ffi::c_void  {
   ///     f as *const ::std::ffi::c_void
@@ -19,67 +23,44 @@ struct FastCallImpl<'s> {
   ///     CType::T
   ///   }
   /// }
-  item: ItemImpl,
+  let item: ItemImpl = ItemImpl {
+    attrs: vec![],
+    defaultness: None,
+    unsafety: None,
+    impl_token: Default::default(),
+    generics: Default::default(),
+    trait_: None,
+    self_ty: Box::new(Type::Path(TypePath {
+      qself: None,
+      path: Path {
+        leading_colon: None,
+        segments,
+      },
+    })),
+    brace_token: Default::default(),
+    items: vec![],
+  };
 
   /// struct T <A> {
   ///   _phantom: ::std::marker::PhantomData<A>,
   /// }
-  fast_ty: ItemStruct,
-
-  optimizer: &'s mut Optimizer,
-}
-
-impl<'s> FastCallImpl<'s> {
-  pub(crate) fn new(optimizer: &'s mut Optimizer) -> Self {
-    let ident = Ident::new("FastCall", Span::call_site());
-    let mut segments = Punctuated::new();
-    segments.push_value(PathSegment {
-      ident: ident.clone(),
-      arguments: PathArguments::None,
-    });
-
-    Self {
-      item: ItemImpl {
-        attrs: vec![],
-        defaultness: None,
-        unsafety: None,
-        impl_token: Default::default(),
-        generics: Default::default(),
-        trait_: None,
-        self_ty: Box::new(Type::Path(TypePath {
-          qself: None,
-          path: Path {
-            leading_colon: None,
-            segments,
-          },
-        })),
-        brace_token: Default::default(),
-        items: vec![],
-      },
-      fast_ty: parse_quote! {
-        struct #ident {
-          _phantom: ::std::marker::PhantomData<()>,
-        }
-      },
-      optimizer,
+  let fast_ty: ItemStruct = parse_quote! {
+    struct #ident {
+      _phantom: ::std::marker::PhantomData<()>,
     }
-  }
+  };
 
-  pub(crate) fn generate(
-    mut self,
-    item_fn: &ItemFn,
-  ) -> Result<TokenStream, ()> {
-    
-    Ok(quote! {})
-  }
+  let ident = Ident::new("FastCall", Span::call_site());
+  let mut segments = Punctuated::new();
+  segments.push_value(PathSegment {
+    ident: ident.clone(),
+    arguments: PathArguments::None,
+  });
+
+  Ok(quote! {})
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
-
-  #[test]
-  fn test_fast_call() {
-    let _ = FastCallImpl::new();
-  }
 }
