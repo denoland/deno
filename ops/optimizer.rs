311924e31413b05b4cf7b7a62707fa2dcaaa7467
Divy Srivastava <dj.srivastava23@gmail.com>
2022-11-02 12:51:58 +0530

    more analyze_param_type

--- ops/optimizer.rs
+++ ops/optimizer.rs
@@ -1,14 +1,34 @@
 /// Optimizer for #[op]
 use crate::Op;
 use syn::{
-  FnArg, ItemFn, Path, PathArguments, PathSegment, ReturnType, Signature, Type,
-  TypePath,
+  punctuated::Punctuated, token::Colon2, AngleBracketedGenericArguments, FnArg,
+  GenericArgument, ItemFn, Path, PathArguments, PathSegment, ReturnType,
+  Signature, Type, TypePath, TypeReference,
 };
 
 enum BailoutReason {
   FastAsync,
   MustBeSingleSegment,
   Other(&'static str),
+}
+
+enum TransformKind {
+  // serde_v8::Value
+  V8Value,
+}
+
+impl Transform {
+  fn serde_v8_value(index: usize) -> Self {
+    Transform {
+      kind: TransformKind::V8Value,
+      index,
+    }
+  }
+}
+
+struct Transform {
+  kind: TransformKind,
+  index: usize,
 }
 
 enum FastValue {
@@ -25,10 +45,16 @@ impl Default for FastValue {
 struct Optimizer {
   returns_result: bool,
 
+  has_ref_opstate: bool,
+
+  has_rc_opstate: bool,
+
   has_fast_callback_option: bool,
 
   fast_result: Option<FastValue>,
   fast_parameters: Vec<FastValue>,
+
+  transforms: Vec<Transform>,
 }
 
 impl Optimizer {
@@ -69,14 +95,7 @@ impl Optimizer {
         path: Path { segments, .. },
         ..
       }) => {
-        if segments.len() != 1 {
-          return Err(BailoutReason::MustBeSingleSegment);
-        }
-
-        let segment = match segments.last() {
-          Some(segment) => segment,
-          None => return Err(BailoutReason::MustBeSingleSegment),
-        };
+        let segment = single_segment(segments)?;
 
         match segment {
           // Result<T, E>
@@ -105,40 +124,143 @@ impl Optimizer {
     index: usize,
     arg: &FnArg,
   ) -> Result<(), BailoutReason> {
-    
     match arg {
       FnArg::Typed(typed) => match &*typed.ty {
         Type::Path(TypePath {
           path: Path { segments, .. },
           ..
-        }) => {
-          if segments.len() != 1 {
-            return Err(BailoutReason::MustBeSingleSegment);
+        }) if segments.len() == 2 => {
+          match double_segment(segments)? {
+            // -> serde_v8::Value
+            [PathSegment { ident: first, .. }, PathSegment { ident: last, .. }]
+              if first == "serde_v8" && last == "Value" =>
+            {
+              self.transforms.push(Transform::serde_v8_value(index));
+            }
+            _ => unreachable!(),
           }
-
-          let segment = match segments.last() {
-            Some(segment) => segment,
-            None => return Err(BailoutReason::MustBeSingleSegment),
-          };
+        }
+        Type::Path(TypePath {
+          path: Path { segments, .. },
+          ..
+        }) => {
+          let segment = single_segment(segments)?;
 
           match segment {
-            // Is Option<&mut FastApiCallbackOption>
+            // -> Option<T>
             PathSegment {
               ident, arguments, ..
             } if ident == "Option" => {
-              
+              if let PathArguments::AngleBracketed(
+                AngleBracketedGenericArguments { args, .. },
+              ) = arguments
+              {
+                match args.last() {
+                  // -> Option<&mut T>
+                  Some(GenericArgument::Type(Type::Reference(
+                    TypeReference { elem, .. },
+                  ))) => {
+                    match &**elem {
+                      Type::Path(TypePath {
+                        path: Path { segments, .. },
+                        ..
+                      }) => {
+                        let segment = single_segment(segments)?;
+                        match segment {
+                          // Is `T` a FastApiCallbackOption?
+                          PathSegment {
+                            ident, arguments, ..
+                          } if ident == "FastApiCallbackOption" => {
+                            self.has_fast_callback_option = true;
+                          }
+                          _ => {}
+                        }
+                      }
+                      _ => {}
+                    }
+                  }
+                  _ => {}
+                }
+              }
+            }
+            // -> Rc<T>
+            PathSegment {
+              ident, arguments, ..
+            } if ident == "Rc" => {
+              if let PathArguments::AngleBracketed(
+                AngleBracketedGenericArguments { args, .. },
+              ) = arguments
+              {
+                match args.last() {
+                  Some(GenericArgument::Type(Type::Path(TypePath {
+                    path: Path { segments, .. },
+                    ..
+                  }))) => {
+                    let segment = single_segment(segments)?;
+                    match segment {
+                      // -> Rc<RefCell<T>>
+                      PathSegment {
+                        ident, arguments, ..
+                      } if ident == "RefCell" => {
+                        if let PathArguments::AngleBracketed(
+                          AngleBracketedGenericArguments { args, .. },
+                        ) = arguments
+                        {
+                          match args.last() {
+                            // -> Rc<RefCell<OpState>>
+                            Some(GenericArgument::Type(Type::Path(
+                              TypePath {
+                                path: Path { segments, .. },
+                                ..
+                              },
+                            ))) => {
+                              let segment = single_segment(segments)?;
+                              match segment {
+                                PathSegment {
+                                  ident, arguments, ..
+                                } if ident == "OpState" => {
+                                  self.has_rc_opstate = true;
+                                }
+                                _ => {}
+                              }
+                            }
+                            _ => {}
+                          }
+                        }
+                      }
+                      _ => {}
+                    }
+                  }
+                  _ => {}
+                }
+              }
             }
             _ => {}
           };
         }
+        // &mut T
+        Type::Reference(TypeReference { elem, .. }) => match &**elem {
+          Type::Path(TypePath {
+            path: Path { segments, .. },
+            ..
+          }) => {
+            let segment = single_segment(segments)?;
+            match segment {
+              // Is `T` a OpState?
+              PathSegment {
+                ident, arguments, ..
+              } if ident == "OpState" => {
+                self.has_ref_opstate = true;
+              }
+              _ => {}
+            }
+          }
+          _ => {}
+        },
         _ => {}
       },
+      _ => {}
     };
-    // Is &mut OpState?
-
-    // Is Rc<RefCell<OpState>>?
-
-    // Is serde_v8::Value?
 
     // Is a scalar FastValue?
 
@@ -147,5 +269,28 @@ impl Optimizer {
     // Is &mut [u8] or &mut [u32]?
 
     Ok(())
+  }
+}
+
+fn single_segment(
+  segments: &Punctuated<PathSegment, Colon2>,
+) -> Result<&PathSegment, BailoutReason> {
+  if segments.len() != 1 {
+    return Err(BailoutReason::MustBeSingleSegment);
+  }
+
+  match segments.last() {
+    Some(segment) => Ok(segment),
+    None => Err(BailoutReason::MustBeSingleSegment),
+  }
+}
+
+fn double_segment(
+  segments: &Punctuated<PathSegment, Colon2>,
+) -> Result<[&PathSegment; 2], BailoutReason> {
+  match (segments.first(), segments.last()) {
+    (Some(first), Some(last)) => Ok([first, last]),
+    // Caller ensures that there are only two segments.
+    _ => unreachable!(),
   }
 }
