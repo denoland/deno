bcb64e13cacba8e95fb7daeb15646bbcd3326206
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-11-10 00:01:30 +0100

    feat(lock): don't require --unstable for auto discovery

--- cli/lockfile.rs
+++ cli/lockfile.rs
@@ -105,7 +105,7 @@ impl Lockfile {
 
     let filename = match flags.lock {
       Some(ref lock) => PathBuf::from(lock),
-      None if flags.unstable => match maybe_config_file {
+      None => match maybe_config_file {
         Some(config_file) => {
           if config_file.specifier.scheme() == "file" {
             let mut path = config_file.specifier.to_file_path().unwrap();
@@ -117,7 +117,6 @@ impl Lockfile {
         }
         None => return Ok(None),
       },
-      None => return Ok(None),
     };
 
     let lockfile = Self::new(filename, flags.lock_write)?;
