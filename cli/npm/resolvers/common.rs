72b5516a50d52817b64d474fccb96ac07318bf1d
David Sherret <dsherret@gmail.com>
2022-11-15 20:59:36 -0500

    Lint.

--- cli/npm/resolvers/common.rs
+++ cli/npm/resolvers/common.rs
@@ -134,7 +134,7 @@ pub fn types_package_name(package_name: &str) -> String {
   debug_assert!(!package_name.starts_with("@types/"));
   // Scoped packages will get two underscores for each slash
   // https://github.com/DefinitelyTyped/DefinitelyTyped/tree/15f1ece08f7b498f4b9a2147c2a46e94416ca777#what-about-scoped-packages
-  format!("@types/{}", package_name.replace("/", "__"))
+  format!("@types/{}", package_name.replace('/', "__"))
 }
 
 #[cfg(test)]
