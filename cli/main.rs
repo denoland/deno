cb23bfbe9e25cd8411ff873856f55c914be31e06
Asher Gomez <ashersaupingomez@gmail.com>
2022-11-24 12:17:11 +1100

    fix: change "module name" to "script argument"

--- cli/main.rs
+++ cli/main.rs
@@ -702,7 +702,7 @@ async fn run_command(
   }
 
   if !flags.has_permission() && flags.has_permission_in_argv() {
-    log::warn!("Permission flags have likely been incorrectly set after the module name. To grant permissions, define them before the module name.")
+    log::warn!("Permission flags have likely been incorrectly set after the script argument. To grant permissions, define them before the script argument.")
   }
 
   if flags.watch.is_some() {
