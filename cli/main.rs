0d277d8d5a432f3ecafcea1596a7a5bc189badb6
Asher Gomez <ashersaupingomez@gmail.com>
2022-11-24 12:58:33 +1100

    tweak

--- cli/main.rs
+++ cli/main.rs
@@ -702,7 +702,14 @@ async fn run_command(
   }
 
   if !flags.has_permission() && flags.has_permission_in_argv() {
-    log::warn!("Permission flags have likely been incorrectly set after the script argument. To grant permissions, define them before the script argument.")
+    log::warn!(
+      r#"
+Permission flags have likely been incorrectly set after the script argument.
+
+To grant permissions, set them before the script argument. For example:
+    deno run --allow-read=. main.js
+"#
+    )
   }
 
   if flags.watch.is_some() {
