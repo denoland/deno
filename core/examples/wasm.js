386bbc7901590967d0f39b76f4f4fea6e88ee390
Divy Srivastava <dj.srivastava23@gmail.com>
2022-11-27 12:17:55 +0530

    x

--- core/examples/wasm.js
+++ core/examples/wasm.js
@@ -2,7 +2,7 @@
 
 // asc wasm.ts --exportStart --initialMemory 6400 -O -o wasm.wasm
 // deno-fmt-ignore
-let bytes = new Uint8Array([
+const bytes = new Uint8Array([
     0,  97, 115, 109,   1,   0,   0,   0,   1,  4,   1, 96,   0,   0,   2,
    15,   1,   3, 111, 112, 115,   7, 111, 112, 95, 119, 97, 115, 109,   0,
     0,   3,   3,   2,   0,   0,   5,   4,   1,  0, 128, 50,   7,  36,   4,
@@ -14,14 +14,14 @@ let bytes = new Uint8Array([
 
 const { ops } = Deno.core;
 
-let module = new WebAssembly.Module(bytes);
-let instance = new WebAssembly.Instance(module, { ops });
+const module = new WebAssembly.Module(bytes);
+const instance = new WebAssembly.Instance(module, { ops });
 ops.op_set_wasm_mem(instance.exports.memory);
 
 instance.exports.call();
 
-let memory = instance.exports.memory;
-let view = new Uint8Array(memory.buffer);
+const memory = instance.exports.memory;
+const view = new Uint8Array(memory.buffer);
 
 if (view[0] !== 69) {
   throw new Error("Expected first byte to be 69");
