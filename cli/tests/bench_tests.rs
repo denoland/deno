50fa82d2b81f34b9ecc0295807b16c10aad470bf
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-11-28 20:05:05 +0100

    fmt

--- cli/tests/bench_tests.rs
+++ cli/tests/bench_tests.rs
@@ -170,18 +170,18 @@ mod bench {
     exit_code: 0,
     output: "bench/collect.out",
   });
-  
+
   itest!(bench_with_config2 {
     args: "bench --config bench/collect/deno2.jsonc bench/collect",
     exit_code: 0,
     output: "bench/collect2.out",
   });
-  
+
   itest!(bench_with_malformed_config {
     args: "bench --config bench/collect/deno.malformed.jsonc",
     exit_code: 1,
     output: "bench/collect_with_malformed_config.out",
-  });  
+  });
 
   #[test]
   fn recursive_permissions_pledge() {
