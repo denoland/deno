749a35cbe153cb6e1f6bf2e9506767b67b44d444
Divy Srivastava <dj.srivastava23@gmail.com>
2022-11-30 22:24:10 +0530

    fmt

--- ext/websocket/01_websocket.js
+++ ext/websocket/01_websocket.js
@@ -10,8 +10,14 @@
   const webidl = window.__bootstrap.webidl;
   const { HTTP_TOKEN_CODE_POINT_RE } = window.__bootstrap.infra;
   const { DOMException } = window.__bootstrap.domException;
-  const { Event, ErrorEvent, CloseEvent, MessageEvent, defineEventHandler, _skipInternalInit } =
-    window.__bootstrap.event;
+  const {
+    Event,
+    ErrorEvent,
+    CloseEvent,
+    MessageEvent,
+    defineEventHandler,
+    _skipInternalInit,
+  } = window.__bootstrap.event;
   const { EventTarget } = window.__bootstrap.eventTarget;
   const { Blob, BlobPrototype } = globalThis.__bootstrap.file;
   const {
