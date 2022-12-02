9b2b8df927ac23cfa99016a684179f2a3198ba2e
Divy Srivastava <dj.srivastava23@gmail.com>
2022-12-01 21:29:15 -0800

    feat(ops): Fast zero copy string arguments (#16777)
    Uses SeqOneByteString optimization to do zero-copy `&str` arguments in
    fast calls.
    
    - [x] Depends on https://github.com/denoland/rusty_v8/pull/1129
    - [x] Depends on
    https://chromium-review.googlesource.com/c/v8/v8/+/4036884
    - [x] Disable in async ops
    - [x] Make it work with owned `String` with an extra alloc in fast path.
    - [x] Support `Cow<'_, str>`. Owned for slow case, Borrowed for fast
    case
    
    ```rust
    #[op]
    fn op_string_len(s: &str) -> u32 { 
      str.len() as u32 
    }
    ```

--- cli/bench/console.js
+++ cli/bench/console.js
@@ -1,8 +1,3 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 const count = 100000;
-
-const start = Date.now();
 for (let i = 0; i < count; i++) console.log("Hello World");
-const elapsed = Date.now() - start;
-const rate = Math.floor(count / (elapsed / 1000));
-console.log(`time ${elapsed} ms rate ${rate}`);
