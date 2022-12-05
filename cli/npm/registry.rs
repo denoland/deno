aacc63030aa8401cc4f3be3b30aeaedc91d0651d
David Sherret <dsherret@gmail.com>
2022-12-05 10:15:32 -0500

    fix: add warning about DENO_NPM_REGISTRY being unstable.

--- cli/npm/registry.rs
+++ cli/npm/registry.rs
@@ -233,7 +233,15 @@ impl RealNpmRegistryApi {
       // ensure there is a trailing slash for the directory
       let registry_url = format!("{}/", registry_url.trim_end_matches('/'));
       match Url::parse(&registry_url) {
-        Ok(url) => url,
+        Ok(url) => {
+          log::warn!(
+            "{}",
+            colors::yellow(
+              "Note: DENO_NPM_REGISTRY is unstable and may change."
+            )
+          );
+          url
+        }
         Err(err) => {
           eprintln!("{}: Invalid {} environment variable. Please provide a valid url.\n\n{:#}",
           colors::red_bold("error"),
