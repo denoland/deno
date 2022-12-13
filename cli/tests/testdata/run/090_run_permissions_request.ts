00c6bad868beba8bda346aa391b9445cc5237604
Asher Gomez <ashersaupingomez@gmail.com>
2022-12-13 16:21:34 +1100

    reset tests II

--- cli/tests/testdata/run/090_run_permissions_request.ts
+++ cli/tests/testdata/run/090_run_permissions_request.ts
@@ -9,16 +9,10 @@ if (status2 != "prompt") {
   throw Error(`unexpected status2 ${status2}`);
 }
 const status3 =
-  (Deno.permissions.querySync({ name: "run", command: "cat" })).state;
-if (status3 != "prompt") {
-  throw Error(`unexpected status3 ${status3}`);
-}
-const status4 =
   (await Deno.permissions.request({ name: "run", command: "cat" })).state;
-if (status4 != "denied") {
-  throw Error(`unexpected status3 ${status4}`);
+if (status3 != "denied") {
+  throw Error(`unexpected status3 ${status3}`);
 }
 console.log(status1);
 console.log(status2);
 console.log(status3);
-console.log(status4);
