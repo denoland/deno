8bc3646c52a25153c9ab97971238b4c38eb52ea3
Asher Gomez <ashersaupingomez@gmail.com>
2022-12-13 16:36:14 +1100

    tests

--- cli/tests/testdata/run/090_run_permissions_request_sync.ts
+++ cli/tests/testdata/run/090_run_permissions_request_sync.ts
@@ -1,15 +1,15 @@
 const status1 =
-  (await Deno.permissions.request({ name: "run", command: "ls" })).state;
+  Deno.permissions.requestSync({ name: "run", command: "ls" }).state;
 if (status1 != "granted") {
   throw Error(`unexpected status1 ${status1}`);
 }
 const status2 =
-  (await Deno.permissions.query({ name: "run", command: "cat" })).state;
+  Deno.permissions.querySync({ name: "run", command: "cat" }).state;
 if (status2 != "prompt") {
   throw Error(`unexpected status2 ${status2}`);
 }
 const status3 =
-  (await Deno.permissions.request({ name: "run", command: "cat" })).state;
+  Deno.permissions.requestSync({ name: "run", command: "cat" }).state;
 if (status3 != "denied") {
   throw Error(`unexpected status3 ${status3}`);
 }
