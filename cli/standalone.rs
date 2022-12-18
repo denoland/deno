e7efb1495a839c90cc2a19b465a735c46d5e1e22
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-12-18 20:45:59 +0100

    lint

--- cli/standalone.rs
+++ cli/standalone.rs
@@ -287,6 +287,7 @@ pub async fn run(
       inspect: ps.options.is_inspecting(),
     },
     extensions: ops::cli_exts(ps.clone()),
+    extensions_with_js: vec![],
     startup_snapshot: Some(crate::js::deno_isolate_init()),
     unsafely_ignore_certificate_errors: metadata
       .unsafely_ignore_certificate_errors,
