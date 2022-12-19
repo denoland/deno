301458de04cbf4e2dce589e96f5cbb2706865925
Luca Casonato <hello@lcas.dev>
2022-12-19 13:37:35 +0100

    fix(ext/http): close stream on resp body error
    Previously errored streaming response body did not cause the HTTP stream
    to be aborted. It instead caused the stream to be closed gracefully,
    which had the result that the client could not detect the difference
    between a successful response and an errored response.
    
    This commit fixes the issue by aborting the stream on error.

--- ext/http/lib.rs
+++ ext/http/lib.rs
@@ -70,9 +70,12 @@ use tokio::io::AsyncRead;
 use tokio::io::AsyncWrite;
 use tokio::io::AsyncWriteExt;
 use tokio::task::spawn_local;
-use tokio_util::io::ReaderStream;
+
+use crate::reader_stream::ExternallyAbortableReaderStream;
+use crate::reader_stream::ShutdownHandle;
 
 pub mod compressible;
+mod reader_stream;
 
 pub fn init() -> Extension {
   Extension::builder()
@@ -414,14 +417,47 @@ impl Default for HttpRequestReader {
 /// The write half of an HTTP stream.
 enum HttpResponseWriter {
   Headers(oneshot::Sender<Response<Body>>),
-  Body(Pin<Box<dyn tokio::io::AsyncWrite>>),
-  BodyUncompressed(hyper::body::Sender),
+  Body(BodyCompressedHolder),
+  BodyUncompressed(BodyUncompressedSender),
   Closed,
 }
 
 impl Default for HttpResponseWriter {
   fn default() -> Self {
     Self::Closed
+  }
+}
+
+struct BodyCompressedHolder {
+  pub writer: Pin<Box<dyn tokio::io::AsyncWrite>>,
+  pub shutdown_handle: ShutdownHandle,
+}
+
+struct BodyUncompressedSender {
+  sender: Option<hyper::body::Sender>,
+  done: bool,
+}
+
+impl BodyUncompressedSender {
+  fn sender(&mut self) -> &mut hyper::body::Sender {
+    self.sender.as_mut().unwrap()
+  }
+}
+
+impl From<hyper::body::Sender> for BodyUncompressedSender {
+  fn from(sender: hyper::body::Sender) -> Self {
+    BodyUncompressedSender {
+      sender: Some(sender),
+      done: false,
+    }
+  }
+}
+
+impl Drop for BodyUncompressedSender {
+  fn drop(&mut self) {
+    if !self.done {
+      self.sender.take().unwrap().abort();
+    }
   }
 }
 
@@ -668,14 +704,20 @@ fn http_response(
         Encoding::Gzip => Box::pin(GzipEncoder::new(writer)),
         _ => unreachable!(), // forbidden by accepts_compression
       };
-      Ok((
-        HttpResponseWriter::Body(writer),
-        Body::wrap_stream(ReaderStream::new(reader)),
-      ))
+      let (stream, shutdown_handle) =
+        ExternallyAbortableReaderStream::new(reader);
+      let holder = BodyCompressedHolder {
+        writer,
+        shutdown_handle,
+      };
+      Ok((HttpResponseWriter::Body(holder), Body::wrap_stream(stream)))
     }
     None => {
       let (body_tx, body_rx) = Body::channel();
-      Ok((HttpResponseWriter::BodyUncompressed(body_tx), body_rx))
+      Ok((
+        HttpResponseWriter::BodyUncompressed(body_tx.into()),
+        body_rx,
+      ))
     }
   }
 }
@@ -768,10 +810,10 @@ async fn op_http_write_resource(
     }
 
     match &mut *wr {
-      HttpResponseWriter::Body(body) => {
-        let mut result = body.write_all(&view).await;
+      HttpResponseWriter::Body(holder) => {
+        let mut result = holder.writer.write_all(&view).await;
         if result.is_ok() {
-          result = body.flush().await;
+          result = holder.writer.flush().await;
         }
         if let Err(err) = result {
           assert_eq!(err.kind(), std::io::ErrorKind::BrokenPipe);
@@ -784,7 +826,7 @@ async fn op_http_write_resource(
       }
       HttpResponseWriter::BodyUncompressed(body) => {
         let bytes = Bytes::from(view);
-        if let Err(err) = body.send_data(bytes).await {
+        if let Err(err) = body.sender().send_data(bytes).await {
           assert!(err.is_closed());
           // Pull up the failure associated with the transport connection instead.
           http_stream.conn.closed().await?;
@@ -813,10 +855,10 @@ async fn op_http_write(
   match &mut *wr {
     HttpResponseWriter::Headers(_) => Err(http_error("no response headers")),
     HttpResponseWriter::Closed => Err(http_error("response already completed")),
-    HttpResponseWriter::Body(body) => {
-      let mut result = body.write_all(&buf).await;
+    HttpResponseWriter::Body(holder) => {
+      let mut result = holder.writer.write_all(&buf).await;
       if result.is_ok() {
-        result = body.flush().await;
+        result = holder.writer.flush().await;
       }
       match result {
         Ok(_) => Ok(()),
@@ -833,7 +875,7 @@ async fn op_http_write(
     }
     HttpResponseWriter::BodyUncompressed(body) => {
       let bytes = Bytes::from(buf);
-      match body.send_data(bytes).await {
+      match body.sender().send_data(bytes).await {
         Ok(_) => Ok(()),
         Err(err) => {
           assert!(err.is_closed());
@@ -862,17 +904,24 @@ async fn op_http_shutdown(
     .get::<HttpStreamResource>(rid)?;
   let mut wr = RcRef::map(&stream, |r| &r.wr).borrow_mut().await;
   let wr = take(&mut *wr);
-  if let HttpResponseWriter::Body(mut body_writer) = wr {
-    match body_writer.shutdown().await {
-      Ok(_) => {}
-      Err(err) => {
-        assert_eq!(err.kind(), std::io::ErrorKind::BrokenPipe);
-        // Don't return "broken pipe", that's an implementation detail.
-        // Pull up the failure associated with the transport connection instead.
-        stream.conn.closed().await?;
+  match wr {
+    HttpResponseWriter::Body(mut holder) => {
+      holder.shutdown_handle.shutdown();
+      match holder.writer.shutdown().await {
+        Ok(_) => {}
+        Err(err) => {
+          assert_eq!(err.kind(), std::io::ErrorKind::BrokenPipe);
+          // Don't return "broken pipe", that's an implementation detail.
+          // Pull up the failure associated with the transport connection instead.
+          stream.conn.closed().await?;
+        }
       }
     }
-  }
+    HttpResponseWriter::BodyUncompressed(mut body) => {
+      body.done = true;
+    }
+    _ => {}
+  };
   Ok(())
 }
 
