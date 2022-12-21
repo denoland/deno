c21251a261df23aa55c0860e7c4ce5c8411c63d2
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-12-21 18:48:59 +0100

    add some comments

--- tools/ci/_util.js
+++ tools/ci/_util.js
@@ -1,3 +1,7 @@
+/**
+ * Describes into how many separate archives, artifacts
+ * produced by CI should split.
+ */
 export const ARCHIVE_COUNT = 4;
 
 export class CargoBuildManifest {
@@ -11,10 +15,12 @@ export class CargoBuildManifest {
       .filter((item) => item.reason === "compiler-artifact");
   }
 
+  /** Return all artifacts that are executable binaries. */
   get bins() {
     return this.#artifacts.filter(({ target }) => target.kind.includes("bin"));
   }
 
+  /** Return all artifacts that are dynamic libraries. */
   get cdylibs() {
     return this.#artifacts.filter(({ target }) =>
       target.kind.includes("cdylib")
@@ -25,10 +31,12 @@ export class CargoBuildManifest {
     return this.#artifacts.filter((a) => a.profile.test);
   }
 
+  /** Return all artifacts that are test binaries. */
   get tests() {
     return this.#tests.filter((a) => !a.target.kind.includes("bench"));
   }
 
+  /** Return all artifacts that are benchmark binaries. */
   get benches() {
     return this.#tests.filter((a) => a.target.kind.includes("bench"));
   }
