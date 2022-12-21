d86a285155f912469dd6a61bc34df7cdcdbabc09
Ivan Zvonimir Horvat <ivanzvonimirhorvat@gmail.com>
2022-12-21 06:47:47 +0100

    17150: examples; fmt updated

--- core/examples/util/mod.rs
+++ core/examples/util/mod.rs
@@ -2,11 +2,11 @@ use anyhow::Error;
 use deno_core::{JsRuntime, ModuleSpecifier};
 
 pub(crate) async fn run_event_loop(
-    mut js_runtime: JsRuntime,
-    main_module: ModuleSpecifier,
+  mut js_runtime: JsRuntime,
+  main_module: ModuleSpecifier,
 ) -> Result<(), Error> {
-    let mod_id = js_runtime.load_main_module(&main_module, None).await?;
-    let result = js_runtime.mod_evaluate(mod_id);
-    js_runtime.run_event_loop(false).await?;
-    result.await?
+  let mod_id = js_runtime.load_main_module(&main_module, None).await?;
+  let result = js_runtime.mod_evaluate(mod_id);
+  js_runtime.run_event_loop(false).await?;
+  result.await?
 }
