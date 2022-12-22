bd71abc0dcb1b593a4c15f6dfbb0f66b40888ef3
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-12-22 14:52:50 +0100

    fix(ext/fetch): Guard against invalid URL before its used by reqwest

--- ext/fetch/lib.rs
+++ ext/fetch/lib.rs
@@ -31,7 +31,7 @@ use deno_core::ResourceId;
 use deno_core::ZeroCopyBuf;
 use deno_tls::rustls::RootCertStore;
 use deno_tls::Proxy;
-use http::header::CONTENT_LENGTH;
+use http::{header::CONTENT_LENGTH, Uri};
 use reqwest::header::HeaderMap;
 use reqwest::header::HeaderName;
 use reqwest::header::HeaderValue;
@@ -216,6 +216,12 @@ where
 
   let method = Method::from_bytes(&method)?;
   let url = Url::parse(&url)?;
+
+  // Make sure that we have a valid URI early, as reqwest's `RequestBuilder::send`
+  // internally uses `expect_uri`, which panics instead of returning a usable `Result`.
+  if url.as_str().parse::<Uri>().is_err() {
+    return Err(type_error("Invalid URL"));
+  }
 
   // Check scheme before asking for net permission
   let scheme = url.scheme();
