11198727083df8ee5fff9b853994888cbd3f6fe1
sigmaSd <bedisnbiba@gmail.com>
2022-12-25 19:39:55 +0000

    t

--- cli/tests/repl_tests.rs
+++ cli/tests/repl_tests.rs
@@ -976,24 +976,29 @@ mod repl {
 
     // NOTE: Running util::deno_cmd somehow doesn't error on windows
     //       Use cmd /C deno instead
-    let deno_cmd = {
-      let mut p = target_dir().join("deno");
+    let output = {
+      let mut p = util::target_dir().join("deno");
       if cfg!(windows) {
         p.set_extension("exe");
       }
       let deno_path = util::target_dir().join(p);
       if cfg!(windows) {
-        Command::new("cmd").arg("/C").arg(deno_path)
+        std::process::Command::new("cmd")
+          .arg("/C")
+          .arg(deno_path)
+          .arg("repl")
+          .stdout(process_that_already_closed.stdin.take().unwrap())
+          .output()
+          .unwrap()
       } else {
-        Command::new(deno_path)
+        std::process::Command::new(deno_path)
+          .arg("repl")
+          .stdout(process_that_already_closed.stdin.take().unwrap())
+          .output()
+          .unwrap()
       }
     };
 
-    let output = deno_cmd
-      .arg("repl")
-      .stdout(process_that_already_closed.stdin.take().unwrap())
-      .output()
-      .unwrap();
     assert!(!output.status.success());
     let stderr = String::from_utf8(output.stderr).unwrap();
     assert!(stderr.contains("failed printing to stdout: Broken pipe"));
