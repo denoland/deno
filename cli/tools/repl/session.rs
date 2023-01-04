f1ff5edc429b0661d301546032e2255db211cec4
David Sherret <dsherret@users.noreply.github.com>
2023-01-04 18:54:54 -0500

    fix: upgrade deno_ast to 0.23 (#17269)
    Closes #17172
    Closes #15669
    Closes #8529

--- cli/tools/repl/session.rs
+++ cli/tools/repl/session.rs
@@ -193,12 +193,13 @@ impl ReplSession {
     line: &str,
   ) -> EvaluationOutput {
     fn format_diagnostic(diagnostic: &deno_ast::Diagnostic) -> String {
+      let display_position = diagnostic.display_position();
       format!(
         "{}: {} at {}:{}",
         colors::red("parse error"),
         diagnostic.message(),
-        diagnostic.display_position.line_number,
-        diagnostic.display_position.column_number,
+        display_position.line_number,
+        display_position.column_number,
       )
     }
 
