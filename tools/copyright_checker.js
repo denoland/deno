1ab29aa7cf71aee2aaee468c76b86e7a74c73032
hzlinyiyu <linyiyu1992@gmail.com>
2023-01-07 12:04:41 +0800

    fix review

--- tools/copyright_checker.js
+++ tools/copyright_checker.js
@@ -26,20 +26,33 @@ async function checkCopyright() {
     "*Cargo.toml",
   ]);
 
-  if (!sourceFiles.length) {
-    return;
-  }
 
   let totalCount = 0;
-  for (const file of sourceFiles) {
-    const fileText = await Deno.readTextFile(file);
+  let sourceFilesSet = new Set(sourceFiles);
+  const buffer = new Uint8Array(1024);
+  const textDecoder = new TextDecoder();
+
+  for (const file of sourceFilesSet) {
+    async function readFirstPartOfFile(filePath) {
+      const file = await Deno.open(filePath, { read: true });
+      try {
+        const byteCount = await file.read(buffer);
+        return textDecoder.decode(buffer.slice(0, byteCount ?? 0));
+      } finally {
+        file.close();
+      }
+    }
+
+    const ERROR_MSG = "Copyright header is missing: ";
+
+    const fileText = await readFirstPartOfFile(file);
     if (file.endsWith("Cargo.toml")) {
       if (
         !fileText.startsWith(
           "# Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.",
         )
       ) {
-        console.log("Copyright is not sign: " + file);
+        console.log(ERROR_MSG + file);
         totalCount += 1;
       }
       continue;
@@ -50,12 +63,12 @@ async function checkCopyright() {
         "// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.",
       )
     ) {
-      console.log("Copyright is not sign: " + file);
+      console.log(ERROR_MSG + file);
       totalCount += 1;
     }
   }
 
-  console.log("\nTotal count: " + totalCount);
+  console.log("\nTotal errors: " + totalCount);
 
   if (totalCount > 0) {
     Deno.exit(1);
