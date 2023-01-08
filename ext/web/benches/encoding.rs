8927c5a1702e5fd7649944e75dd057944305d0ba
crowlkats <crowlkats@toaxl.com>
2023-01-08 05:22:41 +0100

    fix web benches

--- ext/web/benches/encoding.rs
+++ ext/web/benches/encoding.rs
@@ -24,6 +24,7 @@ fn setup() -> Vec<Extension> {
   vec![
     deno_webidl::init(),
     deno_url::init(),
+    deno_console::init(),
     deno_web::init::<Permissions>(BlobStore::default(), None),
     Extension::builder()
       .js(vec![(
