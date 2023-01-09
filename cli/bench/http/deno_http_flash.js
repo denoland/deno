fff7640164cc68facfd09ec50f478ad3113e8de0
Max Coplan <mchcopl@gmail.com>
2023-01-09 02:42:46 -0500

    (refactor) use optional chaining instead of `||` fallthrough
    This commit does not affect behavior.  Just adds some syntactic sugar
    
    Unless someone specifically runs `deno run --allow-net deno_http_flash.js ""` , which I don't think the current implementation was accounting for

--- cli/bench/http/deno_http_flash.js
+++ cli/bench/http/deno_http_flash.js
@@ -1,6 +1,6 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
-const addr = Deno.args[0] || "127.0.0.1:4500";
+const addr = Deno.args[0] ?? "127.0.0.1:4500";
 const [hostname, port] = addr.split(":");
 const { serve } = Deno;
 
