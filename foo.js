f1505f99efdea9faa016bdf666dd44068cf59ff2
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-01-09 22:06:25 +0100

    fix and add test

--- foo.js
+++
@@ -1,9 +0,0 @@
-import pl from "npm:nodejs-polars@0.5.4"; // latest version that works with deno
-const df = pl.DataFrame(
-  {
-    "A": [1, 2, 3, 4, 5],
-    "fruits": ["banana", "banana", "apple", "apple", "banana"],
-    "B": [5, 4, 3, 2, 1],
-    "cars": ["beetle", "audi", "beetle", "beetle", "beetle"],
-  },
-);
