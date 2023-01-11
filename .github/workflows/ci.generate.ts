b4bd6f751f9ddc6a0407ec2fc6df364817013d4d
David Sherret <dsherret@gmail.com>
2023-01-11 09:38:36 -0500

    Try again [ci] test

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -116,10 +116,8 @@ const ci = {
   jobs: {
     build: {
       name: "${{ matrix.job }} ${{ matrix.profile }} ${{ matrix.os }}",
-      if: [
-        "(github.event_name == 'push' || !startsWith(github.event.pull_request.head.label, 'denoland:')) &&",
-        "(github.event.pull_request.draft == false || contains(github.event.head_commit.message, '[ci]'))",
-      ].join("\n"),
+      if:
+        "github.event_name == 'push' && (github.event.pull_request.draft == false || contains(github.event.head_commit.message, '[ci]'))",
       "runs-on": "${{ matrix.os }}",
       "timeout-minutes": 120,
       strategy: {
