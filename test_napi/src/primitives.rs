d5a758c0e13585743e8a3e3981fb8feec1fb50e8
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-01-11 15:47:26 +0100

    refactor(napi): Cleanup tests (#17347)

--- test_napi/src/primitives.rs
+++ test_napi/src/primitives.rs
@@ -1,3 +1,5 @@
+use crate::assert_napi_ok;
+use crate::napi_new_property;
 use napi_sys::*;
 use std::ptr;
 
@@ -6,18 +8,21 @@ extern "C" fn test_get_undefined(
   _: napi_callback_info,
 ) -> napi_value {
   let mut result = ptr::null_mut();
-  unsafe { napi_get_undefined(env, &mut result) };
+  assert_napi_ok!(napi_get_undefined(env, &mut result));
   result
 }
 
 pub fn init(env: napi_env, exports: napi_value) {
-  let properties = &[crate::new_property!(
+  let properties = &[napi_new_property!(
     env,
-    "test_get_undefined\0",
+    "test_get_undefined",
     test_get_undefined
   )];
 
-  unsafe {
-    napi_define_properties(env, exports, properties.len(), properties.as_ptr())
-  };
+  assert_napi_ok!(napi_define_properties(
+    env,
+    exports,
+    properties.len(),
+    properties.as_ptr()
+  ));
 }
