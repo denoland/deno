0e75e50c26e0d533dc76a65e52a24f8e43332dab
David Sherret <dsherret@gmail.com>
2023-01-13 12:12:28 -0500

    Test this.

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -7,7 +7,7 @@ const Runners = {
     "${{ github.repository == 'denoland/deno' && 'ubuntu-20.04-xl' || 'ubuntu-20.04' }}",
   macos: "macos-12",
   windows:
-    "${{ github.repository == 'denoland/deno' && github.event_name != 'pull_request' && 'windows-2022-xl' || 'windows-2022' }}",
+    "${{ github.repository == 'denoland/deno' && 'windows-2022-xl' || 'windows-2022' }}",
 };
 
 const installPkgsCommand =
@@ -204,7 +204,8 @@ const ci = {
         "github.event_name == 'push' ||",
         "!startsWith(github.event.pull_request.head.label, 'denoland:')",
       ].join("\n"),
-      "runs-on": "${{ matrix.os }}",
+      // temp for testing
+      "runs-on": "${{ matrix.runner || matrix.os }}",
       "timeout-minutes": 120,
       strategy: {
         matrix: {
@@ -222,6 +223,9 @@ const ci = {
             },
             {
               os: Runners.windows,
+              // this is temporary for testing
+              runner:
+                "${{ github.event_name != 'pull_request' && 'windows-2022-xl' || 'windows-2022' }}",
               job: "test",
               profile: "fastci",
             },
