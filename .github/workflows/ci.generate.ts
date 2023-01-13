e543f8f73817c832f7ee91d932ce7a56dd6b5e1c
denobot <33910674+denobot@users.noreply.github.com>
2023-01-13 12:17:13 -0500

    1.29.3 (#17395)
    Co-authored-by: David Sherret <dsherret@users.noreply.github.com>

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -200,10 +200,6 @@ const ci = {
   jobs: {
     build: {
       name: "${{ matrix.job }} ${{ matrix.profile }} ${{ matrix.os }}",
-      if: [
-        "github.event_name == 'push' ||",
-        "!startsWith(github.event.pull_request.head.label, 'denoland:')",
-      ].join("\n"),
       "runs-on": "${{ matrix.os }}",
       "timeout-minutes": 120,
       strategy: {
