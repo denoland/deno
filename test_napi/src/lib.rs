291dcc31f778dc137f33b39d4750ed478a223ebd
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-01-13 12:53:45 +0100

    fix(napi): date and unwrap handling (#17369)

--- test_napi/src/lib.rs
+++ test_napi/src/lib.rs
@@ -11,6 +11,7 @@ pub mod arraybuffer;
 pub mod r#async;
 pub mod callback;
 pub mod coerce;
+pub mod date;
 pub mod numbers;
 pub mod object_wrap;
 pub mod primitives;
@@ -127,6 +128,7 @@ unsafe extern "C" fn napi_register_module_v1(
   object_wrap::init(env, exports);
   callback::init(env, exports);
   r#async::init(env, exports);
+  date::init(env, exports);
   tsfn::init(env, exports);
   init_cleanup_hook(env, exports);
 
