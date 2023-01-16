7e815077967b3d7218e6e89705b20c0b9f07d812
Nayeem Rahman <nayeemrmn99@gmail.com>
2023-01-16 20:01:26 +0000

    update test to command api

--- cli/tests/testdata/spawn_kill_permissions.ts
+++ cli/tests/testdata/spawn_kill_permissions.ts
@@ -1,6 +1,6 @@
-const child = Deno.spawnChild("deno", {
+const child = new Deno.Command("deno", {
   args: ["eval", "await new Promise(r => setTimeout(r, 2000))"],
   stdout: "null",
   stderr: "null",
-});
+}).spawn();
 child.kill("SIGTERM");
