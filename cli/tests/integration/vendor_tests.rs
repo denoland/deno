06461bfba9a7c969597823154f5d55fbc3c1010c
Mehrbod Akhlaghpour <m9.akhlaghpoor@gmail.com>
2023-02-04 02:17:16 -0500

    fix(cli): Add a test

--- cli/tests/integration/vendor_tests.rs
+++ cli/tests/integration/vendor_tests.rs
@@ -74,6 +74,39 @@ fn output_dir_exists() {
 }
 
 #[test]
+fn vendor_remote_module_test(){
+  let _server = http_server();
+  let t = TempDir::new();
+  let vendor_dir = t.path().join("vendor2");
+  t.write(
+    "my_app.ts",
+    "import { load } from 'https://x.nest.land/Yenv@1.0.0/mod.ts'; const env = await load({});",
+  );
+  let deno = util::deno_cmd()
+    .current_dir(t.path())
+    .arg("vendor")
+    .arg("my_app.ts")
+    .arg("--output")
+    .arg("vendor2")
+    .stdout(Stdio::piped())
+    .stderr(Stdio::piped())
+    .spawn()
+    .unwrap();
+  let output = deno.wait_with_output().unwrap();
+  assert!(vendor_dir.exists());
+  assert_eq!(
+    String::from_utf8_lossy(&output.stderr).trim(),
+    format!(
+      concat!(
+        "Download https://x.nest.land/Yenv@1.0.0/mod.ts\n",
+        "{}",
+      ),
+      success_text("9 modules", "vendor2", true),
+    )
+  );
+}
+
+#[test]
 fn standard_test() {
   let _server = http_server();
   let t = TempDir::new();
