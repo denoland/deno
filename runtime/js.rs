e3fb393a42cf9c137db7d9d08dfe3ca3dd7b547f
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-02-04 20:44:41 +0100

    tty, write_file

--- runtime/js.rs
+++ runtime/js.rs
@@ -35,6 +35,12 @@ pub fn deno_isolate_init() -> Snapshot {
   Snapshot::Static(&RUNTIME_SNAPSHOT)
 }
 
+pub fn get_40_tty() -> PathBuf {
+  let manifest = env!("CARGO_MANIFEST_DIR");
+  let path = PathBuf::from(manifest);
+  path.join("js").join("40_tty.js")
+}
+
 pub fn get_40_write_file() -> PathBuf {
   let manifest = env!("CARGO_MANIFEST_DIR");
   let path = PathBuf::from(manifest);
