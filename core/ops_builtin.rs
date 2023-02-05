2b63930d621e31877a1f30464668121888700df8
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-02-05 02:30:26 +0100

    fmt

--- core/ops_builtin.rs
+++ core/ops_builtin.rs
@@ -22,7 +22,9 @@ pub(crate) fn init_builtins() -> Extension {
   Extension::builder("deno_builtins")
     .js(include_js_files!(
       prefix "deno:core",
-      "00_primordials.js", "01_core.js", "02_error.js",
+      "00_primordials.js",
+      "01_core.js",
+      "02_error.js",
     ))
     .ops(vec![
       op_close::decl(),
