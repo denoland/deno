b492d617581ad7dc05e95437204d7eadea3aa9bc
Divy Srivastava <dj.srivastava23@gmail.com>
2023-02-08 11:08:20 +0530

    lint

--- test_napi/properties_test.js
+++ test_napi/properties_test.js
@@ -1,6 +1,6 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
-import { assertEquals, assertRejects, loadTestLibrary } from "./common.js";
+import { assertEquals, loadTestLibrary } from "./common.js";
 
 const properties = loadTestLibrary();
 
