7e0cc3cb2f7fce612b3baba74007752154c916e9
Filip Skokan <panva.ip@gmail.com>
2023-03-05 13:34:07 +0100

    fix(ext/crypto): correctly limit ECDSA and hash algorithms (#18030)
    Closes #18029

--- ext/crypto/00_crypto.js
+++ ext/crypto/00_crypto.js
@@ -827,6 +827,18 @@ class SubtleCrypto {
           throw new DOMException("Curve not supported", "NotSupportedError");
         }
 
+        if (
+          (key[_algorithm].namedCurve === "P-256" &&
+            hashAlgorithm !== "SHA-256") ||
+          (key[_algorithm].namedCurve === "P-384" &&
+            hashAlgorithm !== "SHA-384")
+        ) {
+          throw new DOMException(
+            "Not implemented",
+            "NotSupportedError",
+          );
+        }
+
         const signature = await core.opAsync("op_crypto_sign_key", {
           key: keyData,
           algorithm: "ECDSA",
@@ -1330,6 +1342,16 @@ class SubtleCrypto {
         }
         // 2.
         const hash = normalizedAlgorithm.hash.name;
+
+        if (
+          (key[_algorithm].namedCurve === "P-256" && hash !== "SHA-256") ||
+          (key[_algorithm].namedCurve === "P-384" && hash !== "SHA-384")
+        ) {
+          throw new DOMException(
+            "Not implemented",
+            "NotSupportedError",
+          );
+        }
 
         // 3-8.
         return await core.opAsync("op_crypto_verify_key", {
