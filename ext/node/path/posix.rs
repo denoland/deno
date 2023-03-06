98ed781acb738100d10559189bf48ca625aaa47a
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2023-03-06 16:40:09 -0500

    posix dirname optimizations
    Signed-off-by: 20k-ultra <3946250+20k-ultra@users.noreply.github.com>

--- ext/node/path/posix.rs
+++ ext/node/path/posix.rs
@@ -8,36 +8,24 @@ use super::constants;
 
 #[op]
 pub fn op_node_path_posix_dirname(path: &str) -> Result<String, AnyError> {
-  if path.chars().count() == 0 {
-    return Ok(String::from("."));
-  }
+  let has_root = match char_at_index(path, 0) {
+    Some(char) => char as u32 == constants::CHAR_FORWARD_SLASH,
+    None => return Ok(String::from(".")),
+  };
 
-  let has_root =
-    char_at_index(path, 0).unwrap() as u32 == constants::CHAR_FORWARD_SLASH;
-  let mut end: Option<usize> = None;
-  let mut matched_slash = true;
+  let end = match path.rfind('/') {
+    Some(0) => None,
+    Some(n) => Some(n),
+    _ => None,
+  };
 
-  for (i, c) in path.char_indices().rev() {
-    if i < 1 {
-      break;
-    }
-    match c as u32 {
-      constants::CHAR_FORWARD_SLASH if !matched_slash => {
-        end = Some(i);
-        break;
-      }
-      constants::CHAR_FORWARD_SLASH => {
-        // noop
-      }
-      _ => matched_slash = false,
-    }
-  }
+  let mut dirname = String::with_capacity(path.len());
 
-  let dirname = match (end, has_root) {
-    (None, true) => String::from("/"),
-    (None, false) => String::from("."),
-    (Some(1), true) => String::from("//"),
-    (Some(ending), _) => path.chars().take(ending).collect(),
+  match (end, has_root) {
+    (None, true) => dirname.push('/'),
+    (None, false) => dirname.push('.'),
+    (Some(1), true) => dirname.push_str("//"),
+    (Some(ending), _) => dirname.extend(path.chars().take(ending)),
   };
 
   Ok(dirname)
