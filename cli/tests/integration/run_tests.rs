93e66dabcb820f508628d5e88a937c5c24d58a68
David Sherret <dsherret@users.noreply.github.com>
2023-03-13 10:03:19 -0400

    fix: ensure no node_modules directory is created when a package.json exists and no npm dependencies are used (#18134)
    Closes #18133
    Closes #18038

--- cli/tests/integration/run_tests.rs
+++ cli/tests/integration/run_tests.rs
@@ -2867,6 +2867,44 @@ fn package_json_error_dep_value_test() {
     .assert_matches_file("package_json/invalid_value/task.out");
 }
 
+#[test]
+fn package_json_no_node_modules_dir_created() {
+  // it should not create a node_modules directory
+  let context = TestContextBuilder::new()
+    .add_npm_env_vars()
+    .use_temp_cwd()
+    .build();
+  let temp_dir = context.deno_dir();
+
+  temp_dir.write("deno.json", "{}");
+  temp_dir.write("package.json", "{}");
+  temp_dir.write("main.ts", "");
+
+  context.new_command().args("run main.ts").run();
+
+  assert!(!temp_dir.path().join("node_modules").exists());
+}
+
+#[test]
+fn node_modules_dir_no_npm_specifiers_no_dir_created() {
+  // it should not create a node_modules directory
+  let context = TestContextBuilder::new()
+    .add_npm_env_vars()
+    .use_temp_cwd()
+    .build();
+  let temp_dir = context.deno_dir();
+
+  temp_dir.write("deno.json", "{}");
+  temp_dir.write("main.ts", "");
+
+  context
+    .new_command()
+    .args("run --node-modules-dir main.ts")
+    .run();
+
+  assert!(!temp_dir.path().join("node_modules").exists());
+}
+
 itest!(wasm_streaming_panic_test {
   args: "run run/wasm_streaming_panic_test.js",
   output: "run/wasm_streaming_panic_test.js.out",
