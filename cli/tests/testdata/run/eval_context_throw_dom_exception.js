a637b86e0055383d1fc0eeaf7103f3c671c27e96
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-03-14 19:39:58 +0100

    fix qutoes

--- cli/tests/testdata/run/eval_context_throw_dom_exception.js
+++ cli/tests/testdata/run/eval_context_throw_dom_exception.js
@@ -1,5 +1,5 @@
 const [, errorInfo] = Deno[Deno.internal].core.evalContext(
   'throw new DOMException("foo")',
-  new URL(`..`, import.meta.url).href,
+  new URL("..", import.meta.url).href,
 );
 console.log(errorInfo);
