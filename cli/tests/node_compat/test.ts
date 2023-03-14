7ddc5a0fd220a2a3206ca3cb6d31320eee56d5b3
Nayeem Rahman <nayeemrmn99@gmail.com>
2023-03-14 04:20:13 +0000

    cleaner and smaller diff

--- cli/tests/node_compat/test.ts
+++ cli/tests/node_compat/test.ts
@@ -35,95 +35,90 @@ const darwinIgnorePaths = new Set(
   getPathsFromTestSuites(config.darwinIgnore),
 );
 
+let testSerialId = 0;
+
+async function runTest(t: Deno.TestContext, path: string): Promise<void> {
+  // If filter patterns are given and any pattern doesn't match
+  // to the file path, then skip the case
+  if (
+    filters.length > 0 &&
+    filters.every((pattern) => !path.includes(pattern))
+  ) {
+    return Promise.resolve();
+  }
+  const isTodo = path.includes("TODO");
+  const ignore =
+    (Deno.build.os === "windows" && windowsIgnorePaths.has(path)) ||
+    (Deno.build.os === "darwin" && darwinIgnorePaths.has(path)) || isTodo;
+  await t.step({
+    name: `Node.js compatibility "${path}"`,
+    ignore,
+    sanitizeOps: false,
+    sanitizeResources: false,
+    sanitizeExit: false,
+    fn: async () => {
+      const testCase = join(toolsPath, "test", path);
+
+      const v8Flags = ["--stack-size=4000"];
+      const testSource = await Deno.readTextFile(testCase);
+      // TODO(kt3k): Parse `Flags` directive correctly
+      if (testSource.includes("Flags: --expose_externalize_string")) {
+        v8Flags.push("--expose-externalize-string");
+      }
+
+      const args = [
+        "run",
+        "-A",
+        "--quiet",
+        "--unstable",
+        //"--unsafely-ignore-certificate-errors",
+        "--v8-flags=" + v8Flags.join(),
+        testCase.endsWith(".mjs") ? "--import-map=" + importMap : "runner.ts",
+        testCase,
+      ];
+
+      // Pipe stdout in order to output each test result as Deno.test output
+      // That way the tests will respect the `--quiet` option when provided
+      const command = new Deno.Command(Deno.execPath(), {
+        args,
+        env: {
+          DENO_NODE_COMPAT_URL: stdRootUrl,
+          TEST_SERIAL_ID: String(testSerialId++),
+        },
+        cwd,
+      });
+      const { code, stdout, stderr } = await command.output();
+
+      if (code !== 0) {
+        // If the test case failed, show the stdout, stderr, and instruction
+        // for repeating the single test case.
+        if (stdout.length) console.log(decoder.decode(stdout));
+        console.log(`Error: "${path}" failed`);
+        console.log(
+          "You can repeat only this test with the command:",
+          magenta(
+            `./target/debug/deno test -A --import-map cli/tests/node_compat/import_map.json cli/tests/node_compat/test.ts -- ${path}`,
+          ),
+        );
+        fail(decoder.decode(stderr));
+      } else if (hasFilters) {
+        // Even if the test case is successful, shows the stdout and stderr
+        // when test case filtering is specified.
+        if (stdout.length) console.log(decoder.decode(stdout));
+        if (stderr.length) console.log(decoder.decode(stderr));
+      }
+    },
+  });
+}
+
 const decoder = new TextDecoder();
 Deno.test("Node.js compatibility", async (t) => {
-  let testSerialId = 0;
-  let shouldBlock = true;
+  for (const path of testPaths.sequential) {
+    await runTest(t, path);
+  }
   const pending = [];
-  // Parallelizable tests are partitioned to the end so that we can simply stop
-  // blocking on tests when we get there.
-  for (const paths of [testPaths.sequential, testPaths.parallel]) {
-    for (const path of paths) {
-      // If filter patterns are given and any pattern doesn't match
-      // to the file path, then skip the case
-      if (
-        filters.length > 0 &&
-        filters.every((pattern) => !path.includes(pattern))
-      ) {
-        continue;
-      }
-      const isTodo = path.includes("TODO");
-      const ignore =
-        (Deno.build.os === "windows" && windowsIgnorePaths.has(path)) ||
-        (Deno.build.os === "darwin" && darwinIgnorePaths.has(path)) || isTodo;
-      const handle = t.step({
-        name: `Node.js compatibility "${path}"`,
-        ignore,
-        sanitizeOps: false,
-        sanitizeResources: false,
-        sanitizeExit: false,
-        fn: async () => {
-          const testCase = join(toolsPath, "test", path);
-
-          const v8Flags = ["--stack-size=4000"];
-          const testSource = await Deno.readTextFile(testCase);
-          // TODO(kt3k): Parse `Flags` directive correctly
-          if (testSource.includes("Flags: --expose_externalize_string")) {
-            v8Flags.push("--expose-externalize-string");
-          }
-
-          const args = [
-            "run",
-            "-A",
-            "--quiet",
-            "--unstable",
-            //"--unsafely-ignore-certificate-errors",
-            "--v8-flags=" + v8Flags.join(),
-            testCase.endsWith(".mjs")
-              ? "--import-map=" + importMap
-              : "runner.ts",
-            testCase,
-          ];
-
-          // Pipe stdout in order to output each test result as Deno.test output
-          // That way the tests will respect the `--quiet` option when provided
-          const command = new Deno.Command(Deno.execPath(), {
-            args,
-            env: {
-              DENO_NODE_COMPAT_URL: stdRootUrl,
-              TEST_SERIAL_ID: String(testSerialId++),
-            },
-            cwd,
-          });
-          const { code, stdout, stderr } = await command.output();
-
-          if (code !== 0) {
-            // If the test case failed, show the stdout, stderr, and instruction
-            // for repeating the single test case.
-            if (stdout.length) console.log(decoder.decode(stdout));
-            console.log(`Error: "${path}" failed`);
-            console.log(
-              "You can repeat only this test with the command:",
-              magenta(
-                `./target/debug/deno test -A --import-map cli/tests/node_compat/import_map.json cli/tests/node_compat/test.ts -- ${path}`,
-              ),
-            );
-            fail(decoder.decode(stderr));
-          } else if (hasFilters) {
-            // Even if the test case is successful, shows the stdout and stderr
-            // when test case filtering is specified.
-            if (stdout.length) console.log(decoder.decode(stdout));
-            if (stderr.length) console.log(decoder.decode(stderr));
-          }
-        },
-      });
-      if (shouldBlock) {
-        await handle;
-      } else {
-        pending.push(handle);
-      }
-    }
-    shouldBlock = false;
+  for (const path of testPaths.parallel) {
+    pending.push(runTest(t, path));
   }
   await Promise.all(pending);
 });
