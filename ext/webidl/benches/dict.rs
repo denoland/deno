ab15f29d32a03731a4b26818a67181cfd1838019
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-03-18 20:52:11 +0100

    fix specifier

--- ext/webidl/benches/dict.rs
+++ ext/webidl/benches/dict.rs
@@ -14,7 +14,7 @@ fn setup() -> Vec<Extension> {
     deno_webidl::deno_webidl::init_ops_and_esm(),
     Extension::builder("deno_webidl_bench")
       .esm(vec![ExtensionFileSource {
-        specifier: "ext/deno_webidl_bench:setup",
+        specifier: "ext:deno_webidl_bench/setup.js",
         code: ExtensionFileSourceCode::IncludedInBinary(include_str!(
           "dict.js"
         )),
