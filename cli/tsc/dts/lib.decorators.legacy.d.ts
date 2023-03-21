5e3aa193ee92fee1d611bdd2cd04499d6f1a81b9
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-03-21 18:13:15 +0100

    ignore decorators

--- cli/tsc/dts/lib.decorators.legacy.d.ts
+++ cli/tsc/dts/lib.decorators.legacy.d.ts
@@ -13,22 +13,10 @@ See the Apache Version 2.0 License for specific language governing permissions
 and limitations under the License.
 ***************************************************************************** */
 
+
 /// <reference no-default-lib="true"/>
 
-declare type ClassDecorator = <TFunction extends Function>(
-  target: TFunction,
-) => TFunction | void;
-declare type PropertyDecorator = (
-  target: Object,
-  propertyKey: string | symbol,
-) => void;
-declare type MethodDecorator = <T>(
-  target: Object,
-  propertyKey: string | symbol,
-  descriptor: TypedPropertyDescriptor<T>,
-) => TypedPropertyDescriptor<T> | void;
-declare type ParameterDecorator = (
-  target: Object,
-  propertyKey: string | symbol,
-  parameterIndex: number,
-) => void;
+declare type ClassDecorator = <TFunction extends Function>(target: TFunction) => TFunction | void;
+declare type PropertyDecorator = (target: Object, propertyKey: string | symbol) => void;
+declare type MethodDecorator = <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>) => TypedPropertyDescriptor<T> | void;
+declare type ParameterDecorator = (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
