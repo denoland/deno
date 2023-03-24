0645be7d65a09e60331cbf8ec8481c98aa8b0b7f
Mike Mulchrone <mtmulch0191@outlook.com>
2023-03-23 21:19:21 -0400

    using default error message for op_reapath_sync and op_realpath_async

--- ext/fs/lib.rs
+++ ext/fs/lib.rs
@@ -1456,7 +1456,9 @@ where
   debug!("op_realpath_sync {}", path.display());
   // corresponds to the realpath on Unix and
   // CreateFile and GetFinalPathNameByHandle on Windows
-  let realpath = canonicalize_path(&path)?;
+  let realpath = canonicalize_path(&path).map_err(|error| {
+    default_err_mapper(error, format!("op_realpath_sync '{}', Deno.realPathSync", path.display()))
+  })?;
   let realpath_str = into_string(realpath.into_os_string())?;
   Ok(realpath_str)
 }
@@ -1488,7 +1490,9 @@ where
     debug!("op_realpath_async {}", path.display());
     // corresponds to the realpath on Unix and
     // CreateFile and GetFinalPathNameByHandle on Windows
-    let realpath = canonicalize_path(&path)?;
+    let realpath = canonicalize_path(&path).map_err(|error| {
+      default_err_mapper(error, format!("op_realpath_async '{}', Deno.realPath", path.display()))
+    })?;
     let realpath_str = into_string(realpath.into_os_string())?;
     Ok(realpath_str)
   })
