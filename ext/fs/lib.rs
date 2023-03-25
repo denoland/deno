7adcfcbc04ade4f75e7eb5755767e7b1581d10fc
Mike Mulchrone <mtmulch0191@outlook.com>
2023-03-25 10:13:16 -0400

    #17526 removing second Deno.realPath error from default_err_mapper

--- ext/fs/lib.rs
+++ ext/fs/lib.rs
@@ -1457,10 +1457,7 @@ where
   // corresponds to the realpath on Unix and
   // CreateFile and GetFinalPathNameByHandle on Windows
   let realpath = canonicalize_path(&path).map_err(|error| {
-    default_err_mapper(
-      error,
-      format!("op_realpath_sync '{}', Deno.realPathSync", path.display()),
-    )
+    default_err_mapper(error, format!("op_realpath_sync '{}'", path.display()))
   })?;
   let realpath_str = into_string(realpath.into_os_string())?;
   Ok(realpath_str)
@@ -1496,7 +1493,7 @@ where
     let realpath = canonicalize_path(&path).map_err(|error| {
       default_err_mapper(
         error,
-        format!("op_realpath_async '{}', Deno.realPath", path.display()),
+        format!("op_realpath_async '{}'", path.display()),
       )
     })?;
     let realpath_str = into_string(realpath.into_os_string())?;
