6627d4f171766777c2b8806212b2de17e07e5528
Marvin Hagemeister <hello@marvinh.dev>
2023-04-01 08:15:17 +0200

    fix(ext/node): fix unable to resolve fraction.js

--- ext/node/ops.rs
+++ ext/node/ops.rs
@@ -470,7 +470,13 @@ where
   {
     modules_path
   } else {
-    path_resolve(vec![modules_path, name])
+    let orignal = modules_path.clone();
+    let mod_dir = path_resolve(vec![modules_path, name]);
+    if Path::new(&mod_dir).is_dir() {
+      mod_dir
+    } else {
+      orignal
+    }
   };
   let pkg = PackageJson::load::<Env::Fs>(
     &*resolver,
