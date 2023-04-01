ed65f4e5b8dd9ae798e9377ebb9b4f41b558b227
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2023-04-01 15:56:42 +0200

    Revert "fix metadata error in output test"
    This reverts commit 13aee11272dc8eeefefe88d31fc9e3da23f63624.

--- cli/tools/upgrade.rs
+++ cli/tools/upgrade.rs
@@ -267,10 +267,7 @@ pub async fn upgrade(
   let current_exe_path = std::env::current_exe()?;
   let output_exe_path =
     upgrade_flags.output.as_ref().unwrap_or(&current_exe_path);
-
-  let metadata =
-    fs::metadata(output_exe_path).unwrap_or(fs::metadata(&current_exe_path)?);
-
+  let metadata = fs::metadata(output_exe_path)?;
   let permissions = metadata.permissions();
 
   if permissions.readonly() {
