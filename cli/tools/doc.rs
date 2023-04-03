bb7b58a9912883c52599603a84ee808ba7ee728a
Yoshiya Hinosawa <stibium121@gmail.com>
2023-04-03 21:05:39 +0900

    Revert "fix(cli): don't store blob and data urls in the module cache (#18261)" (#18572)
    This reverts commit b4c61c146a50dea0c4a53d8d505a4308ea7da279.
    
    cc @nayeemrmn

--- cli/tools/doc.rs
+++ cli/tools/doc.rs
@@ -17,6 +17,7 @@ use deno_core::resolve_path;
 use deno_core::resolve_url_or_path;
 use deno_doc as doc;
 use deno_graph::ModuleSpecifier;
+use std::path::PathBuf;
 
 pub async fn print_docs(
   flags: Flags,
@@ -68,6 +69,7 @@ pub async fn print_docs(
       let root_specifier =
         resolve_path("./$deno$doc.ts", ps.options.initial_cwd()).unwrap();
       let root = File {
+        local: PathBuf::from("./$deno$doc.ts"),
         maybe_types: None,
         media_type: MediaType::TypeScript,
         source: format!("export * from \"{module_specifier}\";").into(),
