acf33dfa83152ba2d2846fe448c6b46ab1b8178c
Divy Srivastava <dj.srivastava23@gmail.com>
2023-04-04 17:19:41 +0530

    cleanup

--- core/01_core.js
+++ core/01_core.js
@@ -199,7 +199,7 @@
     const id = rollPromiseId();
     let promise = PromisePrototypeThen(setPromise(id), unwrapOpResult);
     try {
-      ops[name](id, ...args);
+      ops[name](id, ...new SafeArrayIterator(args));
     } catch (err) {
       // Cleanup the just-created promise
       getPromise(id);
