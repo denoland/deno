1ebd036ceb57bd77cd267f6c6749c4833f34ab52
jerry4718 <jerry4718@qq.com>
2023-04-06 11:01:26 +0800

    fix (ext/node/http) clarify the intent of `http.request`test cases

--- cli/tests/node_compat/test/parallel/test-http-request-create-url-from-options.js
+++ cli/tests/node_compat/test/parallel/test-http-request-create-url-from-options.js
@@ -3,30 +3,31 @@
 
 'use strict';
 const common = require('../common');
-const assert = require('assert');
 const http = require('http');
 const { urlToHttpOptions } = require('url');
 
-const testPath = '/foo?bar';
+const server = http.createServer((_, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end();
+});
 
-const server = http.createServer(common.mustCall((req, res) => {
-  assert.strictEqual(req.url, testPath);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('hello\n');
-  res.end();
-}));
+server.listen(0, common.localhostIPv4, () => {
+  const url = new URL(`http://${common.localhostIPv4}:${server.address().port}/anypath`);
+  const httpOptions = urlToHttpOptions(url);
 
-server.listen(0, common.localhostIPv4, common.mustCall(() => {
-  const httpOptions = urlToHttpOptions(new URL(`http://${common.localhostIPv4}:${server.address().port}${testPath}`))
-
-  delete httpOptions.href
+  delete httpOptions.href;
 
   const opts = {
-    ...httpOptions,
-    host: `${httpOptions.hostname}:${options.port}`
+      ...httpOptions,
+      host: `${httpOptions.hostname}:${httpOptions.port}`,
   };
 
-  http.request(opts, common.mustCall(() => {
-    server.close();
+  const req = http.request(opts, common.mustCall(res => {
+      res.on('data', () => { })
+      res.on('end', common.mustCall(() => server.close()));
   }));
-}));
+
+  req.on('error', common.mustNotCall());
+
+  req.end();
+});
