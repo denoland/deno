16c1e023a56ed236c68829435f4a09f4c5729fbb
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-04-06 15:08:14 +0200

    refactor(ext/node): add more methods to 'NodeFs' trait (#18604)
    Added more methods to `ext/node/clippy.toml` that are not allowed
    to be used in the crate.
    
    Prerequisite for https://github.com/denoland/deno/pull/18544

--- ext/node/ops.rs
+++ ext/node/ops.rs
@@ -264,8 +264,8 @@ where
 {
   let path = PathBuf::from(path);
   ensure_read_permission::<Env::P>(state, &path)?;
-  if let Ok(metadata) = Env::Fs::metadata(&path) {
-    if metadata.is_file() {
+  if Env::Fs::exists(&path) {
+    if Env::Fs::is_file(&path) {
       return Ok(0);
     } else {
       return Ok(1);
@@ -285,7 +285,7 @@ where
 {
   let path = PathBuf::from(request);
   ensure_read_permission::<Env::P>(state, &path)?;
-  let mut canonicalized_path = path.canonicalize()?;
+  let mut canonicalized_path = Env::Fs::canonicalize(&path)?;
   if cfg!(windows) {
     canonicalized_path = PathBuf::from(
       canonicalized_path
