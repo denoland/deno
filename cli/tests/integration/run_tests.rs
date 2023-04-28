0137e9a31c9e58734c72dbeb93699caf5f8b63a3
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-04-28 15:38:23 +0200

    reenable idletimeout test

--- cli/tests/integration/run_tests.rs
+++ cli/tests/integration/run_tests.rs
@@ -4228,13 +4228,12 @@ async fn websocket_server_multi_field_connection_header() {
 // TODO(bartlomieju): this should use `deno run`, not `deno test`; but the
 // test hangs then. https://github.com/denoland/deno/issues/14283
 #[tokio::test]
-#[ignore]
 async fn websocket_server_idletimeout() {
   let script =
     util::testdata_path().join("run/websocket_server_idletimeout.ts");
   let root_ca = util::testdata_path().join("tls/RootCA.pem");
   let mut child = util::deno_cmd()
-    .arg("test")
+    .arg("run")
     .arg("--unstable")
     .arg("--allow-net")
     .arg("--cert")
