4e9272a82d385b5839ed966f522a196159ea7b46
solach <solach67@protonmail.ch>
2023-05-04 14:44:17 -0600

    reappliese 3db79f5abe21fa7b4c5b89a2c8406b75ed4a0b32

--- cli/tools/upgrade.rs
+++ cli/tools/upgrade.rs
@@ -270,13 +270,15 @@ pub async fn upgrade(
   let factory = CliFactory::from_flags(flags).await?;
   let client = factory.http_client();
   let current_exe_path = std::env::current_exe()?;
+  let output_exe_path =
+    upgrade_flags.output.as_ref().unwrap_or(&current_exe_path);
   let metadata = fs::metadata(&current_exe_path)?;
   let permissions = metadata.permissions();
 
   if permissions.readonly() {
     bail!(
       "You do not have write permission to {}",
-      current_exe_path.display()
+      output_exe_path.display()
     );
   }
   #[cfg(unix)]
@@ -287,7 +289,7 @@ pub async fn upgrade(
       "You don't have write permission to {} because it's owned by root.\n",
       "Consider updating deno through your package manager if its installed from it.\n",
       "Otherwise run `deno upgrade` as root.",
-    ), current_exe_path.display());
+    ), output_exe_path.display());
   }
 
   let install_version = match upgrade_flags.version {
