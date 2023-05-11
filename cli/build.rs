853965ae5b16856af44194bd824da7561d7d38e6
David Sherret <dsherret@gmail.com>
2023-05-11 09:19:16 -0400

    Forgot to hit save

--- cli/build.rs
+++ cli/build.rs
@@ -467,7 +467,7 @@ fn main() {
   );
 
   let ts_version = ts::version();
-  debug_assert_eq!(ts_version, "5.0.3"); // bump this assertion when it changes
+  debug_assert_eq!(ts_version, "5.0.4"); // bump this assertion when it changes
   println!("cargo:rustc-env=TS_VERSION={}", ts_version);
   println!("cargo:rerun-if-env-changed=TS_VERSION");
 
