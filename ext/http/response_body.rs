eb65462c0d821ca805739348fc8c3536d5c0aaf5
Levente Kurusa <lkurusa@kernelstuff.org>
2023-05-22 18:24:02 +0200

    wip

--- ext/http/response_body.rs
+++ ext/http/response_body.rs
@@ -2,11 +2,13 @@
 use std::borrow::Cow;
 use std::cell::RefCell;
 use std::future::Future;
+use std::io::BufWriter;
 use std::io::Write;
 use std::pin::Pin;
 use std::rc::Rc;
 use std::task::Waker;
 
+use brotli;
 use bytes::Bytes;
 use bytes::BytesMut;
 use deno_core::error::bad_resource;
@@ -460,24 +462,21 @@ impl GZipResponseStream {
 
 #[derive(Copy, Clone, Debug)]
 enum BrotliState {
-  Header,
   Streaming,
-  Flushing,
-  Trailer,
   EndOfStream,
 }
 
 #[pin_project]
 pub struct BrotliResponseStream {
+  state: BrotliState,
   #[pin]
   underlying: ResponseStream,
-  state: BrotliState,
 }
 
 impl BrotliResponseStream {
   pub fn new(underlying: ResponseStream) -> Self {
     Self {
-      state: BrotliState::Header,
+      state: BrotliState::Streaming,
       underlying,
     }
   }
@@ -490,96 +489,29 @@ impl PollFrame for BrotliResponseStream {
   ) -> std::task::Poll<ResponseStreamResult> {
     let this = self.get_mut();
     let state = &mut this.state;
-    let orig_state = *state;
+    //let orig_state = *state;
     let frame = match *state {
       BrotliState::EndOfStream => {
         return std::task::Poll::Ready(ResponseStreamResult::EndOfStream)
       }
-      BrotliState::Header => {
-        *state = BrotliState::Streaming;
-        return std::task::Poll::Ready(ResponseStreamResult::NonEmptyBuf(
-          BufView::from(GZIP_HEADER.clone()),
-        ));
-      }
-      BrotliState::Trailer => {
-        *state = BrotliState::EndOfStream;
-        let mut v = Vec::with_capacity(8);
-        v.extend(&this.crc.sum().to_le_bytes());
-        v.extend(&this.crc.amount().to_le_bytes());
-        return std::task::Poll::Ready(ResponseStreamResult::NonEmptyBuf(
-          BufView::from(v),
-        ));
-      }
       BrotliState::Streaming => {
-        if let Some(partial) = this.partial.take() {
-          ResponseStreamResult::NonEmptyBuf(partial)
-        } else {
-          ready!(Pin::new(&mut this.underlying).poll_frame(cx))
-        }
+        ready!(Pin::new(&mut this.underlying).poll_frame(cx))
       }
-      BrotliState::Flushing => ResponseStreamResult::EndOfStream,
     };
 
-    let stm = &mut this.stm;
-
-    // Ideally we could use MaybeUninit here, but flate2 requires &[u8]. We should also try
-    // to dynamically adjust this buffer.
-    let mut buf = this
-      .next_buf
-      .take()
-      .unwrap_or_else(|| BytesMut::zeroed(64 * 1024));
-
-    let start_in = stm.total_in();
-    let start_out = stm.total_out();
     let res = match frame {
-      // Short-circuit these and just return
-      x @ (ResponseStreamResult::NoData
-      | ResponseStreamResult::Error(..)
-      | ResponseStreamResult::Trailers(..)) => {
-        return std::task::Poll::Ready(x)
+      ResponseStreamResult::NonEmptyBuf(buf) => {
+        let mut writer = brotli::CompressorWriter::new(
+          Vec::new(),
+          4096, /* buffer size */
+          6,
+          22,
+        );
+        writer.write_all(buf.as_ref());
+
+        ResponseStreamResult::NonEmptyBuf(BufView::from(writer.into_inner()))
       }
-      ResponseStreamResult::EndOfStream => {
-        *state = GZipState::Flushing;
-        stm.compress(&[], &mut buf, flate2::FlushCompress::Finish)
-      }
-      ResponseStreamResult::NonEmptyBuf(mut input) => {
-        let res = stm.compress(&input, &mut buf, flate2::FlushCompress::None);
-        let len_in = (stm.total_in() - start_in) as usize;
-        debug_assert!(len_in <= input.len());
-        this.crc.update(&input[..len_in]);
-        if len_in < input.len() {
-          input.advance_cursor(len_in);
-          this.partial = Some(input);
-        }
-        res
-      }
-    };
-    let len = stm.total_out() - start_out;
-    let res = match res {
-      Err(err) => ResponseStreamResult::Error(err.into()),
-      Ok(flate2::Status::BufError) => {
-        // This should not happen
-        unreachable!("old={orig_state:?} new={state:?} buf_len={}", buf.len());
-      }
-      Ok(flate2::Status::Ok) => {
-        if len == 0 {
-          this.next_buf = Some(buf);
-          ResponseStreamResult::NoData
-        } else {
-          buf.truncate(len as usize);
-          ResponseStreamResult::NonEmptyBuf(BufView::from(buf.freeze()))
-        }
-      }
-      Ok(flate2::Status::StreamEnd) => {
-        *state = GZipState::Trailer;
-        if len == 0 {
-          this.next_buf = Some(buf);
-          ResponseStreamResult::NoData
-        } else {
-          buf.truncate(len as usize);
-          ResponseStreamResult::NonEmptyBuf(BufView::from(buf.freeze()))
-        }
-      }
+      _ => frame,
     };
 
     std::task::Poll::Ready(res)
