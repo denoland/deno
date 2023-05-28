a20bb8131775e5d6f138703da98915384c964415
k-nasa <htilcs1115@gmail.com>
2023-05-28 10:18:02 +0900

    Implement FileHandle#readFile

--- ext/node/polyfills/internal/fs/handle.ts
+++ ext/node/polyfills/internal/fs/handle.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from "ext:deno_node/events.ts";
 import { Buffer } from "ext:deno_node/buffer.ts";
-import { notImplemented } from "ext:deno_node/_utils.ts";
+import { promises } from "ext:deno_node/fs.ts";
 
 export class FileHandle extends EventEmitter {
   readonly rid: number;
@@ -9,9 +9,12 @@ export class FileHandle extends EventEmitter {
     this.rid = rid;
   }
 
-  // TODO implement this. https://github.com/nodejs/node/blob/959142a4652f7b6e90743be874fe9c065ed31d99/lib/internal/fs/promises.js#L173
-  read(buffer: Buffer, offset: string, length: number, position?: number | null): void {
-    notImplemented("not implemented FileHandle.read")
+  get fd() {
+    return this.rid;
+  }
+
+  readFile(options: { encoding?: string | null; flag?: string; } | null = null): Promise<Buffer> {
+    return promises.readFile(this, options);
   }
 }
 
