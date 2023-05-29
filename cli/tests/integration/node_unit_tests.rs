fc6ba92024d76d44349c36dcedd13994116db45b
egg <takaharu.kato@route06.co.jp>
2023-05-29 22:03:14 +0900

    test(ext/node): add os_test.ts (#19305)
    Part of #17840
    
    I haven't made any changes or additions to the tests themselves, just
    moved the tests over and updated to match.
    
    ---------
    
    Co-authored-by: Bartek Iwańczuk <biwanczuk@gmail.com>

--- cli/tests/integration/node_unit_tests.rs
+++ cli/tests/integration/node_unit_tests.rs
@@ -58,6 +58,7 @@ util::unit_test_factory!(
     pbkdf2_test = internal / pbkdf2_test,
     scrypt_test = internal / scrypt_test,
     module_test,
+    os_test,
     process_test,
     querystring_test,
     readline_test,
