f5a911a799c594f2721e290d4794ebbfeb886371
Matt Mastracci <matthew@mastracci.com>
2023-05-31 09:18:16 -0600

    chore(ext/node): Implement stubs for Http2Session

--- ext/node/polyfills/http2.ts
+++ ext/node/polyfills/http2.ts
@@ -2,12 +2,116 @@
 // Copyright Joyent and Node contributors. All rights reserved. MIT license.
 
 import { notImplemented } from "ext:deno_node/_utils.ts";
+import { EventEmitter } from "ext:deno_node/events.ts";
+import { Buffer } from "ext:deno_node/buffer.ts";
+import { Socket } from "ext:deno_node/net.ts";
 
-export class Http2Session {
+export class Http2Session extends EventEmitter {
   constructor() {
-    notImplemented("Http2Session.prototype.constructor");
+    super();
+  }
+
+  get alpnProtocol(): string | undefined {
+    notImplemented("Http2Session.alpnProtocol");
+    return undefined;
+  }
+
+  close(_callback?: () => void) {
+    notImplemented("Http2Session.close");
+  }
+
+  get closed(): boolean {
+    notImplemented("Http2Session.closed");
+    return false;
+  }
+
+  get connecting(): boolean {
+    notImplemented("Http2Session.connecting");
+    return false;
+  }
+
+  destroy(_error?: Error, _code?: number) {
+    notImplemented("Http2Session.destroy");
+  }
+
+  get destroyed(): boolean {
+    notImplemented("Http2Session.destroyed");
+    return false;
+  }
+
+  get encrypted(): boolean {
+    notImplemented("Http2Session.encrypted");
+    return false;
+  }
+
+  goaway(_code: number, _lastStreamID: number, _opaqueData: Buffer | DataView) {
+    notImplemented("Http2Session.goaway");
+  }
+
+  get localSettings(): Record<string, unknown> {
+    notImplemented("Http2Session.localSettings");
+    return {};
+  }
+
+  get originSet(): string[] | undefined {
+    notImplemented("Http2Session.originSet");
+    return undefined;
+  }
+
+  get pendingSettingsAck(): boolean {
+    notImplemented("Http2Session.pendingSettingsAck");
+    return false;
+  }
+
+  ping(
+    _payload: Buffer | TypedArray | DataView,
+    _callback: () => void,
+  ): boolean {
+    notImplemented("Http2Session.ping");
+    return false;
+  }
+
+  ref() {
+    notImplemented("Http2Session.ref");
+  }
+
+  get remoteSettings(): Record<string, unknown> {
+    notImplemented("Http2Session.remoteSettings");
+    return {};
+  }
+
+  setLocalWindowSize(_windowSize: number) {
+    notImplemented("Http2Session.setLocalWindowSize");
+  }
+
+  setTimeout(_msecs: number, _callback: () => void) {
+    notImplemented("Http2Session.setTimeout");
+  }
+
+  get socket(): Socket /*| TlsSocket*/ {
+    notImplemented("Http2Session.socket");
+    return null;
+  }
+
+  get state(): Record<string, unknown> {
+    notImplemented("Http2Session.state");
+    return {};
+  }
+
+  settings(_settings: Record<string, unknown>, _callback: () => void) {
+    notImplemented("Http2Session.settings");
+  }
+
+  get type(): number {
+    notImplemented("Http2Session.type");
+    return 0;
+  }
+
+  unref() {
+    notImplemented("Http2Session.unref");
   }
 }
+
 export class ServerHttp2Session {
   constructor() {
     notImplemented("ServerHttp2Session");
