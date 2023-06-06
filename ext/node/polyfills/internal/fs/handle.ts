f942eeb54b3ae8298a417bf56f46f296d67f6a9e
k-nasa <htilcs1115@gmail.com>
2023-06-06 21:25:56 +0900

    Implement write method

--- ext/node/polyfills/internal/fs/handle.ts
+++ ext/node/polyfills/internal/fs/handle.ts
@@ -1,12 +1,25 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import { EventEmitter } from "ext:deno_node/events.ts";
 import { Buffer } from "ext:deno_node/buffer.ts";
-import { promises } from "ext:deno_node/fs.ts";
+import { promises, write } from "ext:deno_node/fs.ts";
 import {
   BinaryOptionsArgument,
   FileOptionsArgument,
   TextOptionsArgument,
 } from "ext:deno_node/_fs/_fs_common.ts";
+
+
+interface WriteResult {
+  bytesRead: number;
+  buffer: Buffer;
+}
+
+type WriteOptions = {
+  buffer: Buffer | Uint8Array;
+  offset: number;
+  length: number;
+  position: number | null;
+};
 
 export class FileHandle extends EventEmitter {
   #rid: number;
@@ -23,6 +36,18 @@ export class FileHandle extends EventEmitter {
     opt?: TextOptionsArgument | BinaryOptionsArgument | FileOptionsArgument,
   ): Promise<string | Buffer> {
     return promises.readFile(this, opt);
+  }
+
+  write(buffer: Buffer, offset: number, length: number, position: number): promise<WriteResult> {
+    return new Promise((resolve, reject) => {
+      write(this.rid, buffer, offset, length, position, (err, bytesWritten, buffer) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ bytesRead: bytesWritten, buffer });
+        }
+      }
+    };
   }
 
   close(): Promise<void> {
