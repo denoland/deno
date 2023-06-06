2c1f6ade431f0f3e4c935206a2405a6b92d3ec3f
Matt Mastracci <matthew@mastracci.com>
2023-06-06 11:50:42 -0600

    pin dax and use for progress

--- tools/build_bench.ts
+++ tools/build_bench.ts
@@ -1,10 +1,9 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
-import $ from "https://deno.land/x/dax/mod.ts";
-import { MultiProgressBar } from "https://deno.land/x/progress@v1.3.8/mod.ts";
+import $ from "https://deno.land/x/dax@0.32.0/mod.ts";
 
 if (Deno.args.length === 0) {
-  console.log(
+  $.log(
     "Usage: build_bench [-v] [--profile release|debug] commit1 [commit2 [comment3...]]",
   );
   Deno.exit(1);
@@ -23,18 +22,8 @@ if (args[0] == "--profile") {
   profile = args.shift();
 }
 
-const progresses: { completed: number; total: number; text: string }[] = [];
-
-const bars = new MultiProgressBar({
-  title: `building profile ${profile}`,
-  display: "[:bar] :time :text",
-});
-
 function exit(msg: string) {
-  if (!verbose) {
-    bars.end();
-  }
-  console.error(msg);
+  $.logError(msg);
   Deno.exit(1);
 }
 
@@ -42,73 +31,78 @@ function exit(msg: string) {
 const gitDir = Deno.cwd() + "/.git";
 await Deno.stat(gitDir);
 
-const GIT_STEPS = 8;
-const infos = [];
-
-async function runCommand(human, cmd) {
+async function runCommand(human: string, cmd) {
   if (verbose) {
-    const out = await cmd;
+    const out = await cmd.noThrow();
     if (out.code != 0) {
       exit(human);
     }
   } else {
-    const out = await cmd.stdout("piped").stderr("piped");
+    const out = await cmd.stdout("piped").stderr("piped").noThrow();
     if (out.code != 0) {
-      console.log(out.stdout);
-      console.log(out.stderr);
+      $.logLight("stdout");
+      $.logGroup();
+      $.log(out.stdout);
+      $.logGroupEnd();
+      $.logLight("stderr");
+      $.logGroup();
+      $.log(out.stderr);
+      $.logGroupEnd();
       exit(human);
     }
   }
 }
 
-async function buildGitCommit(commit) {
-  const progress = { completed: 0, total: GIT_STEPS, text: "?" };
-  progresses.push(progress);
-
+async function buildGitCommit(progress, commit) {
   const tempDir = await Deno.makeTempDir();
-  progress.completed++;
 
   const gitInfo =
     await $`git log --pretty=oneline --abbrev-commit -n1 ${commit}`.stdout(
       "piped",
     ).stderr("piped");
   if (gitInfo.code != 0) {
-    console.log(gitInfo.stdout);
-    console.log(gitInfo.stderr);
+    $.log(gitInfo.stdout);
+    $.log(gitInfo.stderr);
     exit(`Failed to get git info for commit ${commit}`);
   }
-  progress.completed++;
 
   const hash = gitInfo.stdout.split(" ")[0];
-  progress.text = hash;
+  progress.message(`${commit} is ${hash}`);
 
-  progress.text = `clone ${hash}`;
+  progress.message(`clone ${hash}`);
   await runCommand(
     `Failed to clone commit ${commit}`,
     $`git clone ${gitDir} ${tempDir}`,
   );
-  progress.completed++;
 
-  progress.text = `reset ${hash}`;
+  progress.message(`reset ${hash}`);
   await runCommand(
     `Failed to reset commit ${commit}`,
     $`git reset --hard ${hash}`.cwd(tempDir),
   );
-  progress.completed++;
 
-  progress.text = `build ${hash}`;
-  if (profile === "debug") {
-    await runCommand(
-      `Failed to build commit ${commit}`,
-      $`cargo build`.cwd(tempDir),
-    );
-  } else {
-    await runCommand(
-      `Failed to build commit ${commit}`,
-      $`cargo build --profile ${profile}`.cwd(tempDir),
-    );
+  progress.message(`build ${hash} (please wait)`);
+  const now = Date.now();
+  const interval = setInterval(() => {
+    const elapsed = Math.round((Date.now() - now) / 1000);
+    progress.message(`build ${hash} (${elapsed}s)`);
+  }, 100);
+  try {
+    if (profile === "debug") {
+      await runCommand(
+        `Failed to build commit ${commit}`,
+        $`cargo build`.cwd(tempDir),
+      );
+    } else {
+      await runCommand(
+        `Failed to build commit ${commit}`,
+        $`cargo build --profile ${profile}`.cwd(tempDir),
+      );
+    }
+  } finally {
+    clearInterval(interval);
   }
-  progress.completed++;
+  const elapsed = Math.round((Date.now() - now) / 1000);
 
   let file;
   if (profile === "release") {
@@ -116,39 +110,26 @@ async function buildGitCommit(commit) {
   } else {
     file = `deno-${profile}-${hash}`;
   }
+  progress.message(`copy ${hash}`);
   await Deno.copyFile(`${tempDir}/target/${profile}/deno`, file);
-  progress.completed++;
 
-  progress.text = `cleanup ${hash}`;
+  progress.message(`cleanup ${hash}`);
   await Deno.remove(tempDir, { recursive: true });
-  progress.completed++;
 
-  progress.text = "done";
-  infos.push(`Built ./${file} (${commit}): ${gitInfo.stdout}`);
-  progress.completed++;
+  progress.message("done");
+  $.log(`Built ./${file} (${commit}) in ${elapsed}s: ${gitInfo.stdout}`);
 }
 
 const promises = [];
 for (const arg of args) {
-  promises.push(buildGitCommit(arg));
-}
-
-let barUpdater;
-if (!verbose) {
-  barUpdater = setInterval(() => {
-    bars.render(progresses);
-  }, 100);
-  bars.render(progresses);
+  if (verbose) {
+    promises.push(buildGitCommit({ message() {} }, arg));
+  } else {
+    const progress = $.progress(`${arg}`);
+    promises.push(progress.with(async () => {
+      await buildGitCommit(progress, arg);
+    }));
+  }
 }
 
 await Promise.all(promises);
-
-if (!verbose) {
-  bars.render(progresses);
-  clearInterval(barUpdater);
-  bars.end();
-}
-
-for (const info of infos) {
-  console.log(info);
-}
