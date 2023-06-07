19f2350b63bfa0be6a83e109dccea1ee4155c9ec
Divy Srivastava <dj.srivastava23@gmail.com>
2023-06-07 17:59:50 +0530

    add cpyright

--- ext/node/ops/zlib/brotli.rs
+++ ext/node/ops/zlib/brotli.rs
@@ -1,3 +1,4 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 use brotli::enc::encode::BrotliEncoderParameter;
 use brotli::ffi::compressor::*;
 use deno_core::error::type_error;
