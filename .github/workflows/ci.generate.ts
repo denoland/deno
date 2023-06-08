dd6458b30a4c1ecf4fab1eaffb4563734c5df284
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-06-08 04:04:07 +0200

    chore: upgrade to Rust 1.68.2 (#19416)
    Also prints disk size in release builds.

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -575,7 +575,12 @@ const ci = {
             "(github.ref == 'refs/heads/main' ||",
             "startsWith(github.ref, 'refs/tags/'))))",
           ].join("\n"),
-          run: "cargo build --release --locked --all-targets",
+          run: [
+            // output fs space before and after building
+            "df -h",
+            "cargo build --release --locked --all-targets",
+            "df -h",
+          ].join("\n"),
         },
         {
           name: "Upload PR artifact (linux)",
