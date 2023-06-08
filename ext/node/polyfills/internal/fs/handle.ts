262571e63e3086e0a4ea6125b3836c357a21af86
nasa <htilcs1115@gmail.com>
2023-06-08 21:37:19 +0900

    feat(node_compat): Add a read method to the FileHandle class (#19359)
    ref: #19165
    
    The FileHandle class has many missing methods compared to node.

--- ext/node/polyfills/internal/fs/handle.ts
+++ ext/node/polyfills/internal/fs/handle.ts
@@ -1,12 +1,19 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import { EventEmitter } from "ext:deno_node/events.ts";
 import { Buffer } from "ext:deno_node/buffer.ts";
-import { promises } from "ext:deno_node/fs.ts";
+import { promises, read } from "ext:deno_node/fs.ts";
+import type { Buffer } from "ext:deno_node/buffer.ts";
 import {
   BinaryOptionsArgument,
   FileOptionsArgument,
+  ReadOptions,
   TextOptionsArgument,
 } from "ext:deno_node/_fs/_fs_common.ts";
+
+interface ReadResult {
+  bytesRead: number;
+  buffer: Buffer;
+}
 
 export class FileHandle extends EventEmitter {
   #rid: number;
@@ -17,6 +24,43 @@ export class FileHandle extends EventEmitter {
 
   get fd() {
     return this.rid;
+  }
+
+  read(
+    buffer: Buffer,
+    offset?: number,
+    length?: number,
+    position?: number | null,
+  ): Promise<ReadResult>;
+  read(options?: ReadOptions): Promise<ReadResult>;
+  read(
+    bufferOrOpt: Buffer | ReadOptions,
+    offset?: number,
+    length?: number,
+    position?: number | null,
+  ): Promise<ReadResult> {
+    if (bufferOrOpt instanceof Buffer) {
+      return new Promise((resolve, reject) => {
+        read(
+          this.fd,
+          bufferOrOpt,
+          offset,
+          length,
+          position,
+          (err, bytesRead, buffer) => {
+            if (err) reject(err);
+            else resolve({ buffer: buffer, bytesRead: bytesRead });
+          },
+        );
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        read(this.fd, bufferOrOpt, (err, bytesRead, buffer) => {
+          if (err) reject(err);
+          else resolve({ buffer: buffer, bytesRead: bytesRead });
+        });
+      });
+    }
   }
 
   readFile(
