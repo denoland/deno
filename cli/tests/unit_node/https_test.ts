f3e7cb0d920b69b644c189b0937c858470a6c716
Felipe Baltor <fbaltor@gmail.com>
2023-06-19 15:52:44 -0300

    wip: working on the https test

--- cli/tests/unit_node/https_test.ts
+++ cli/tests/unit_node/https_test.ts
@@ -6,18 +6,19 @@ import { assertEquals } from "../../../test_util/std/testing/asserts.ts";
 import { Agent } from "node:https";
 
 const tlsTestdataDir = fromFileUrl(
-    new URL("../testdata/tls", import.meta.url),
-  );
+  new URL("../testdata/tls", import.meta.url),
+);
 const keyFile = join(tlsTestdataDir, "localhost.key");
 const certFile = join(tlsTestdataDir, "localhost.crt");
 const dec = new TextDecoder();
+const httpsRequestFilePath = "cli/tests/unit_node/testdata/https_request.ts";
 
 Deno.test("[node/https] request makes https request", async () => {
   const controller = new AbortController();
   const signal = controller.signal;
 
   const serveFinish = serveTls((_req) => {
-    return new Response("abcd\n".repeat(1_000));
+    return new Response("abcd\n".repeat(1_0));
   }, { keyFile, certFile, port: 4505, hostname: "localhost", signal });
 
   const command = new Deno.Command(Deno.execPath(), {
@@ -27,7 +28,7 @@ Deno.test("[node/https] request makes https request", async () => {
       "--unstable",
       "--allow-all",
       "--no-check",
-      "cli/tests/unit_node/testdata/https_request.ts",
+      httpsRequestFilePath,
     ],
     env: {
       NODE_EXTRA_CA_CERTS: join(tlsTestdataDir, "RootCA.pem"),
@@ -40,34 +41,34 @@ Deno.test("[node/https] request makes https request", async () => {
   await serveFinish;
 });
 
-// Deno.test("[node/https] get makes https GET request", async () => {
-//   const controller = new AbortController();
-//   const signal = controller.signal;
+Deno.test("[node/https] get makes https GET request", async () => {
+  const controller = new AbortController();
+  const signal = controller.signal;
 
-//   const serveFinish = serveTls((_req) => {
-//     return new Response("abcd\n".repeat(1_000));
-//   }, { keyFile, certFile, port: 4505, hostname: "localhost", signal });
+  const serveFinish = serveTls((_req) => {
+    return new Response("abcd\n".repeat(1_0));
+  }, { keyFile, certFile, port: 4505, hostname: "localhost", signal });
 
-//   const command = new Deno.Command(Deno.execPath(), {
-//     args: [
-//       "run",
-//       "--quiet",
-//       "--unstable",
-//       "--allow-all",
-//       "--no-check",
-//       "node/testdata/https_get.ts",
-//     ],
-//     env: {
-//       NODE_EXTRA_CA_CERTS: join(tlsTestdataDir, "RootCA.pem"),
-//     },
-//   });
-//   const { stdout, stderr } = await command.output();
-//   assertEquals(dec.decode(stderr), "");
-//   assertEquals(dec.decode(stdout), "abcd\n".repeat(1_000) + "\n");
-//   controller.abort();
-//   await serveFinish;
-// });
+  const command = new Deno.Command(Deno.execPath(), {
+    args: [
+      "run",
+      "--quiet",
+      "--unstable",
+      "--allow-all",
+      "--no-check",
+      httpsRequestFilePath,
+    ],
+    env: {
+      NODE_EXTRA_CA_CERTS: join(tlsTestdataDir, "RootCA.pem"),
+    },
+  });
+  const { stdout, stderr } = await command.output();
+  assertEquals(dec.decode(stderr), "");
+  assertEquals(dec.decode(stdout), "abcd\n".repeat(1_0) + "\n");
+  controller.abort();
+  await serveFinish;
+});
 
-// Deno.test("new Agent doesn't throw", () => {
-//   new Agent();
-// });
+Deno.test("new Agent doesn't throw", () => {
+  new Agent();
+});
--- cli/tests/unit_node/testdata/https_request.ts
+++ cli/tests/unit_node/testdata/https_request.ts
@@ -1,14 +1,12 @@
 import https from "node:https";
-import {
-    assert,
-} from "../../../../test_util/std/testing/asserts.ts";
+import { assert } from "../../../../test_util/std/testing/asserts.ts";
 
 // deno-lint-ignore no-explicit-any
 https.request("https://localhost:4505", (res: any) => {
   let data = "";
   assert(res.socket);
   assert(Object.hasOwn(res.socket, "authorized"));
-  // @ts-ignore socket is TLSSocket, and it has "authoried"
+  // // @ts-ignore socket is TLSSocket, and it has "authorized"
   assert(res.socket.authorized);
   // deno-lint-ignore no-explicit-any
   res.on("data", (chunk: any) => {
