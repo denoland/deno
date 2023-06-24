cace24f0158edcacc7e06c5e91e260c96f262a06
Matt Mastracci <matthew@mastracci.com>
2023-06-24 11:11:14 +0200

    Don't use unsupported op args for this test

--- ops/op2/test_cases/sync/doc_comment.rs
+++ ops/op2/test_cases/sync/doc_comment.rs
@@ -1,5 +1,5 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
 /// This is a doc comment.
-#[op2]
-pub fn op_print(#[string] msg: &str, is_err: bool) -> Result<(), Error> {}
+#[op2(fast)]
+pub fn op_has_doc_comment() -> () {}
