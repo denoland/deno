579c05df365c98b59e4b329d5cf0681c054d6ea4
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-06-27 10:09:27 +0200

    fix: lint on main branch

--- ext/node/polyfills/internal_binding/handle_wrap.ts
+++ ext/node/polyfills/internal_binding/handle_wrap.ts
@@ -1,6 +1,3 @@
-// TODO(petamoriken): enable prefer-primordials for node polyfills
-// deno-lint-ignore-file prefer-primordials
-
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 // Copyright Joyent, Inc. and other Node contributors.
 //
