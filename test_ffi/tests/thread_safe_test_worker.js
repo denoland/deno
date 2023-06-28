ec5e78a7e209687cc5bb54391368b0f75fa6ee1f
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2023-06-28 21:50:33 +0300

    Remove random call import

--- test_ffi/tests/thread_safe_test_worker.js
+++ test_ffi/tests/thread_safe_test_worker.js
@@ -1,8 +1,6 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 // deno-lint-ignore-file
 
-import { call } from "../../core/examples/wasm.ts";
-
 const targetDir = Deno.execPath().replace(/[^\/\\]+$/, "");
 const [libPrefix, libSuffix] = {
   darwin: ["lib", "dylib"],
