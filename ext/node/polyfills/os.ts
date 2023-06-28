ae2ee0ea8c96641987c31527c898c78407afbbf6
crowlkats <crowlkats@toaxl.com>
2023-06-29 01:17:24 +0200

    add TODO

--- ext/node/polyfills/os.ts
+++ ext/node/polyfills/os.ts
@@ -328,6 +328,10 @@ export function userInfo(
     gid = -1;
   }
 
+  // TODO(@crowlKats): figure out how to do this correctly:
+  //  The value of homedir returned by os.userInfo() is provided by the operating system.
+  //  This differs from the result of os.homedir(), which queries environment
+  //  variables for the home directory before falling back to the operating system response.
   let _homedir = homedir();
   if (!_homedir) {
     throw new ERR_OS_NO_HOMEDIR();
