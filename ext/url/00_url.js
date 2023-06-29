b50c02094e5943d4d90568167481cadb8d15b57c
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2023-06-29 07:03:52 -0700

    feat(ext/url): two-argument delete() and has()

--- ext/url/00_url.js
+++ ext/url/00_url.js
@@ -169,19 +169,31 @@ class URLSearchParams {
 
   /**
    * @param {string} name
+   * @param {string} [value]
    */
-  delete(name) {
+  delete(name, value) {
     webidl.assertBranded(this, URLSearchParamsPrototype);
     const prefix = "Failed to execute 'append' on 'URLSearchParams'";
     webidl.requiredArguments(arguments.length, 1, prefix);
     name = webidl.converters.USVString(name, prefix, "Argument 1");
     const list = this[_list];
     let i = 0;
-    while (i < list.length) {
-      if (list[i][0] === name) {
-        ArrayPrototypeSplice(list, i, 1);
-      } else {
-        i++;
+    if (value === undefined) {
+      while (i < list.length) {
+        if (list[i][0] === name) {
+          ArrayPrototypeSplice(list, i, 1);
+        } else {
+          i++;
+        }
+      }
+    } else {
+      value = webidl.converters.USVString(value, prefix, "Argument 2");
+      while (i < list.length) {
+        if (list[i][0] === name && list[i][1] === value) {
+          ArrayPrototypeSplice(list, i, 1);
+        } else {
+          i++;
+        }
       }
     }
     this.#updateUrlSearch();
@@ -228,13 +240,21 @@ class URLSearchParams {
 
   /**
    * @param {string} name
+   * @param {string} [value]
    * @return {boolean}
    */
-  has(name) {
+  has(name, value) {
     webidl.assertBranded(this, URLSearchParamsPrototype);
     const prefix = "Failed to execute 'has' on 'URLSearchParams'";
     webidl.requiredArguments(arguments.length, 1, prefix);
     name = webidl.converters.USVString(name, prefix, "Argument 1");
+    if (value !== undefined) {
+      value = webidl.converters.USVString(value, prefix, "Argument 2");
+      return ArrayPrototypeSome(
+        this[_list],
+        (entry) => entry[0] === name && entry[1] === value,
+      );
+    }
     return ArrayPrototypeSome(this[_list], (entry) => entry[0] === name);
   }
 
