b79528e8c4a469739890b907a9433d6b104d2e95
Nicholas Berlette <nick@berlette.com>
2023-06-29 01:46:29 -0700

    fix(node/worker_threads): typo in Worker constructor

--- ext/node/polyfills/worker_threads.ts
+++ ext/node/polyfills/worker_threads.ts
@@ -126,7 +126,7 @@ class _Worker extends EventEmitter {
       }
       if (
         !(specifier.toString().endsWith(".mjs") ||
-          (pkg && pkg.exists && pkg.typ == "module"))
+          (pkg && pkg.exists && pkg.type == "module"))
       ) {
         const cwdFileUrl = toFileUrl(Deno.cwd());
         specifier =
