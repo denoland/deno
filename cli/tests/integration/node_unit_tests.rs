9befc5cd48843ed2f6997181af14362d933679d9
Kaique da Silva <44123854+ktfth@users.noreply.github.com>
2023-06-29 23:44:14 -0300

    test(ext/node): added unit test for net node modules compat from std (#19663)

--- cli/tests/integration/node_unit_tests.rs
+++ cli/tests/integration/node_unit_tests.rs
@@ -66,6 +66,7 @@ util::unit_test_factory!(
     pbkdf2_test = internal / pbkdf2_test,
     scrypt_test = internal / scrypt_test,
     module_test,
+    net_test,
     os_test,
     process_test,
     querystring_test,
