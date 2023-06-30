2b9c1aa30c2e5c1886b3c7adbf3c2b0fc404851c
Yoshiya Hinosawa <stibium121@gmail.com>
2023-06-30 12:04:33 +0900

    fix

--- cli/tests/unit_node/perf_hooks_test.ts
+++ cli/tests/unit_node/perf_hooks_test.ts
@@ -1,12 +1,12 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import * as perfHooks from "node:perf_hooks";
 import { performance } from "node:perf_hooks";
-import { assertEquals } from ".../../../test_util/std/testing/asserts.ts";
+import { assertEquals } from "../../../test_util/std/testing/asserts.ts";
 import {
   assertSpyCall,
   assertSpyCalls,
   spy,
-} from ".../../../test_util/std/testing/mock.ts";
+} from "../../../test_util/std/testing/mock.ts";
 
 Deno.test({
   name: "[perf_hooks] performance",
@@ -15,11 +15,13 @@ Deno.test({
     assertEquals(perfHooks.performance.clearMarks, performance.clearMarks);
     assertEquals(perfHooks.performance.mark, performance.mark);
     assertEquals(perfHooks.performance.now, performance.now);
+    // @ts-ignore
     assertEquals(perfHooks.performance.toJSON, performance.toJSON);
     perfHooks.performance.measure("test");
     perfHooks.performance.mark("test");
     perfHooks.performance.clearMarks("test");
     perfHooks.performance.now();
+    // @ts-ignore
     perfHooks.performance.toJSON();
   },
 });
@@ -31,6 +33,7 @@ Deno.test({
     performance.mark("test");
     performance.clearMarks("test");
     performance.now();
+    // @ts-ignore
     performance.toJSON();
   },
 });
@@ -39,23 +42,5 @@ Deno.test({
   name: "[perf_hooks] PerformanceEntry",
   fn() {
     assertEquals<unknown>(perfHooks.PerformanceEntry, PerformanceEntry);
-  },
-});
-
-Deno.test({
-  name: "[perf_hooks] EventTarget methods",
-  fn() {
-    const handler = spy();
-    performance.addEventListener("event", handler);
-    assertSpyCalls(handler, 0);
-    const e = new Event("event");
-    performance.dispatchEvent(e);
-    // handler is called once
-    assertSpyCalls(handler, 1);
-    assertSpyCall(handler, 0, { args: [e] });
-    performance.removeEventListener("event", handler);
-    performance.dispatchEvent(e);
-    // handler is not called anymore
-    assertSpyCalls(handler, 1);
   },
 });
