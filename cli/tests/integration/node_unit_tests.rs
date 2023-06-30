a8a9a3e0aeecdb76addfe11124624670eaa0ed51
Yoshiya Hinosawa <stibium121@gmail.com>
2023-06-30 11:57:18 +0900

    fix: add entry in node_unit_tests.rs

--- cli/tests/integration/node_unit_tests.rs
+++ cli/tests/integration/node_unit_tests.rs
@@ -66,6 +66,7 @@ util::unit_test_factory!(
     scrypt_test = internal / scrypt_test,
     module_test,
     os_test,
+    perf_hooks_test,
     process_test,
     querystring_test,
     readline_test,
