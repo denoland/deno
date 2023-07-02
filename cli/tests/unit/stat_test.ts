e746b6d80654ba4e4e26370fe6e4f784ce841d92
Matt Mastracci <matthew@mastracci.com>
2023-07-01 18:00:14 -0600

    refactor(core): Extract deno_core (#19658)
    `deno_core` is moving out! You'll find it at
    https://github.com/denoland/deno_core/ once this PR lands.

--- cli/tests/unit/stat_test.ts
+++ cli/tests/unit/stat_test.ts
@@ -135,11 +135,11 @@ Deno.test({ permissions: { read: true } }, function lstatSyncSuccess() {
   assert(!modulesInfoByUrl.isDirectory);
   assert(modulesInfoByUrl.isSymlink);
 
-  const coreInfo = Deno.lstatSync("core");
+  const coreInfo = Deno.lstatSync("cli");
   assert(coreInfo.isDirectory);
   assert(!coreInfo.isSymlink);
 
-  const coreInfoByUrl = Deno.lstatSync(pathToAbsoluteFileUrl("core"));
+  const coreInfoByUrl = Deno.lstatSync(pathToAbsoluteFileUrl("cli"));
   assert(coreInfoByUrl.isDirectory);
   assert(!coreInfoByUrl.isSymlink);
 });
@@ -261,11 +261,11 @@ Deno.test({ permissions: { read: true } }, async function lstatSuccess() {
   assert(!modulesInfoByUrl.isDirectory);
   assert(modulesInfoByUrl.isSymlink);
 
-  const coreInfo = await Deno.lstat("core");
+  const coreInfo = await Deno.lstat("cli");
   assert(coreInfo.isDirectory);
   assert(!coreInfo.isSymlink);
 
-  const coreInfoByUrl = await Deno.lstat(pathToAbsoluteFileUrl("core"));
+  const coreInfoByUrl = await Deno.lstat(pathToAbsoluteFileUrl("cli"));
   assert(coreInfoByUrl.isDirectory);
   assert(!coreInfoByUrl.isSymlink);
 });
