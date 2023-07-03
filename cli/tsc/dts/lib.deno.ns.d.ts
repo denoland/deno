a5cb59fdce85621ccc2e235c03fa6b382e66e315
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-07-03 14:28:30 +0200

    don't extend Deno.ListenOptions

--- cli/tsc/dts/lib.deno.ns.d.ts
+++ cli/tsc/dts/lib.deno.ns.d.ts
@@ -5672,7 +5672,22 @@ declare namespace Deno {
    *
    * @category HTTP Server
    */
-  export interface ServeOptions extends Partial<Deno.ListenOptions> {
+  export interface ServeOptions {
+    /** The port to listen on.
+     *
+     * @default {8000} */
+    port?: number;
+
+    /** A literal IP address or host name that can be resolved to an IP address.
+     *
+     * __Note about `0.0.0.0`__ While listening `0.0.0.0` works on all platforms,
+     * the browsers on Windows don't work with the address `0.0.0.0`.
+     * You should show the message like `server running on localhost:8080` instead of
+     * `server running on 0.0.0.0:8080` if your program supports Windows.
+     *
+     * @default {"0.0.0.0"} */
+    hostname?: string;
+
     /** An {@linkcode AbortSignal} to close the server and all connections. */
     signal?: AbortSignal;
 
