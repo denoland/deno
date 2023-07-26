d2b03b1aea01ca7c976bc837e2ddfbc5d646abc9
Matt Mastracci <matthew@mastracci.com>
2023-07-26 12:39:56 -0400

    Update sync.rs

--- ext/fs/sync.rs
+++ ext/fs/sync.rs
@@ -45,12 +45,9 @@ mod inner {
 
 #[cfg(not(feature = "sync_fs"))]
 mod inner {
-  pub use std::rc::Rc as MaybeArc;
-<<<<<<< HEAD
-=======
   use std::ops::Deref;
   use std::ops::DerefMut;
->>>>>>> 2e7254f2a (fix(ext/fs): fix MaybeArc when not sync_fs)
+  pub use std::rc::Rc as MaybeArc;
 
   pub trait MaybeSync {}
   impl<T> MaybeSync for T where T: ?Sized {}
