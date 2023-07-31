ffede172c3fbfbe0965394b4cd33dd96387bf44a
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-07-31 23:31:25 +0200

    feat(node): add polyfill for node:test module

---
+++ ext/node/polyfills/testing.ts
@@ -0,0 +1,82 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+import { notImplemented } from "ext:deno_node/_utils.ts";
+
+export function run() {
+  notImplemented("test.run");
+}
+
+export function test() {
+  notImplemented("test.test");
+}
+
+export function describe() {
+  notImplemented("test.describe");
+}
+
+export function it() {
+  notImplemented("test.it");
+}
+
+export function before() {
+  notImplemented("test.before");
+}
+
+export function after() {
+  notImplemented("test.after");
+}
+
+export function beforeEach() {
+  notImplemented("test.beforeEach");
+}
+
+export function afterEach() {
+  notImplemented("test.afterEach");
+}
+
+export const mock = {
+  fn: () => {
+    notImplemented("test.mock.fn");
+  },
+  getter: () => {
+    notImplemented("test.mock.getter");
+  },
+  method: () => {
+    notImplemented("test.mock.method");
+  },
+  reset: () => {
+    notImplemented("test.mock.reset");
+  },
+  restoreAll: () => {
+    notImplemented("test.mock.restoreAll");
+  },
+  setter: () => {
+    notImplemented("test.mock.setter");
+  },
+  timers: {
+    enable: () => {
+      notImplemented("test.mock.timers.enable");
+    },
+    reset: () => {
+      notImplemented("test.mock.timers.reset");
+    },
+    tick: () => {
+      notImplemented("test.mock.timers.tick");
+    },
+    runAll: () => {
+      notImplemented("test.mock.timers.runAll");
+    },
+  }
+};
+
+export default {
+  run,
+  test,
+  describe,
+  it,
+  before,
+  after,
+  beforeEach,
+  afterEach,
+  mock,
+};
