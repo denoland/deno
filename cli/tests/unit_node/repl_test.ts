2952e215d954527882c95b64d48c121c7bbde6c3
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-08-04 01:22:37 +0200

    fix(node): repl._builtinLibs

---
+++ cli/tests/unit_node/repl_test.ts
@@ -0,0 +1,15 @@
+// deno-lint-ignore-file no-undef
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+import repl from "node:repl";
+import { assert } from "../../../test_util/std/testing/asserts.ts";
+
+Deno.test({
+  name: "repl module exports",
+  fn() {
+    assert(typeof repl.REPLServer !== "undefined");
+    assert(typeof repl.builtinModules !== "undefined");
+    assert(typeof repl._builtinLibs !== "undefined");
+    assert(typeof repl.start !== "undefined");
+  },
+});
