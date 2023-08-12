c8d3e8e2955716206040c6f022fd67191aaa18f4
Divy Srivastava <dj.srivastava23@gmail.com>
2023-08-12 11:16:08 +0530

    refactor: rewrite websocket to use op2 macro

--- ext/websocket/lib.rs
+++ ext/websocket/lib.rs
@@ -5,6 +5,7 @@ use deno_core::error::invalid_hostname;
 use deno_core::error::type_error;
 use deno_core::error::AnyError;
 use deno_core::op;
+use deno_core::op2;
 use deno_core::url;
 use deno_core::AsyncMutFuture;
 use deno_core::AsyncRefCell;
@@ -405,8 +406,12 @@ pub fn ws_create_server_stream(
   Ok(rid)
 }
 
-#[op(fast)]
-pub fn op_ws_send_binary(state: &mut OpState, rid: ResourceId, data: &[u8]) {
+#[op2(fast)]
+pub fn op_ws_send_binary(
+  state: &mut OpState,
+  #[smi] rid: ResourceId,
+  #[buffer] data: &[u8],
+) {
   let resource = state.resource_table.get::<ServerWebSocket>(rid).unwrap();
   let data = data.to_vec();
   let len = data.len();
@@ -424,8 +429,12 @@ pub fn op_ws_send_binary(state: &mut OpState, rid: ResourceId, data: &[u8]) {
   });
 }
 
-#[op(fast)]
-pub fn op_ws_send_text(state: &mut OpState, rid: ResourceId, data: String) {
+#[op2(fast)]
+pub fn op_ws_send_text(
+  state: &mut OpState,
+  #[smi] rid: ResourceId,
+  #[string] data: String,
+) {
   let resource = state.resource_table.get::<ServerWebSocket>(rid).unwrap();
   let len = data.len();
   resource.buffered.set(resource.buffered.get() + len);
@@ -485,8 +494,12 @@ pub async fn op_ws_send_text_async(
 
 const EMPTY_PAYLOAD: &[u8] = &[];
 
-#[op(fast)]
-pub fn op_ws_get_buffered_amount(state: &mut OpState, rid: ResourceId) -> u32 {
+#[op2(fast)]
+#[smi]
+pub fn op_ws_get_buffered_amount(
+  state: &mut OpState,
+  #[smi] rid: ResourceId,
+) -> u32 {
   state
     .resource_table
     .get::<ServerWebSocket>(rid)
@@ -495,10 +508,10 @@ pub fn op_ws_get_buffered_amount(state: &mut OpState, rid: ResourceId) -> u32 {
     .get() as u32
 }
 
-#[op]
+#[op2(async)]
 pub async fn op_ws_send_pong(
   state: Rc<RefCell<OpState>>,
-  rid: ResourceId,
+  #[smi] rid: ResourceId,
 ) -> Result<(), AnyError> {
   let resource = state
     .borrow_mut()
@@ -510,10 +523,10 @@ pub async fn op_ws_send_pong(
     .await
 }
 
-#[op]
+#[op2(async)]
 pub async fn op_ws_send_ping(
   state: Rc<RefCell<OpState>>,
-  rid: ResourceId,
+  #[smi] rid: ResourceId,
 ) -> Result<(), AnyError> {
   let resource = state
     .borrow_mut()
@@ -549,23 +562,29 @@ pub async fn op_ws_close(
   Ok(())
 }
 
-#[op]
-pub fn op_ws_get_buffer(state: &mut OpState, rid: ResourceId) -> ToJsBuffer {
+#[op2]
+#[serde]
+pub fn op_ws_get_buffer(
+  state: &mut OpState,
+  #[smi] rid: ResourceId,
+) -> ToJsBuffer {
   let resource = state.resource_table.get::<ServerWebSocket>(rid).unwrap();
   resource.buffer.take().unwrap().into()
 }
 
-#[op]
+#[op2]
+#[string]
 pub fn op_ws_get_buffer_as_string(
   state: &mut OpState,
-  rid: ResourceId,
+  #[smi] rid: ResourceId,
 ) -> String {
   let resource = state.resource_table.get::<ServerWebSocket>(rid).unwrap();
   resource.string.take().unwrap()
 }
 
-#[op]
-pub fn op_ws_get_error(state: &mut OpState, rid: ResourceId) -> String {
+#[op2]
+#[string]
+pub fn op_ws_get_error(state: &mut OpState, #[smi] rid: ResourceId) -> String {
   let Ok(resource) = state.resource_table.get::<ServerWebSocket>(rid) else {
     return "Bad resource".into();
   };
