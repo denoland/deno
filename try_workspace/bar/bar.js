4067dab6c1b6ca8202440f5de426cda8670149a6
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-08-23 13:50:05 +0200

    don't depend on local import map

--- try_workspace/bar/bar.js
+++ try_workspace/bar/bar.js
@@ -3,10 +3,10 @@ import * as chain from "chain/file1.ts";
 import * as colors from "@std/fmt/colors.ts";
 // import { foo } from "deno:@foo/foo@1";
 
-console.log(" > BAR > codeBlockWriter > ", codeBlockWriter);
-console.log(" > BAR > chain > ", chain);
-console.log(" > BAR > @std/fmt/colors > ", colors.red("Hello!"));
-// console.log(" > BAR > foo > ", foo);
+console.log(">> BAR > codeBlockWriter > ", codeBlockWriter);
+console.log(">> BAR > chain > ", chain);
+console.log(">> BAR > @std/fmt/colors > ", colors.red("Hello!"));
+// console.log(">> BAR > foo > ", foo);
 
 export function bar() {
 }
--- try_workspace/foo/foo.js
+++ try_workspace/foo/foo.js
@@ -2,9 +2,9 @@ import codeBlockWriter from "codeBlockWriter";
 import * as tsMorph from "tsMorph";
 import * as assert from "@std/assert/mod.ts";
 
-console.log(" > FOO > codeBlockWriter > ", codeBlockWriter);
-console.log(" > FOO > tsMorph > ", tsMorph);
-console.log(" > FOO > @std/assert > ", assert.assertEquals(1, 1));
+console.log(">> FOO > codeBlockWriter > ", codeBlockWriter);
+console.log(">> FOO > tsMorph > ", tsMorph.SetAccessorDeclaration);
+console.log(">> FOO > @std/assert > ", assert.assertEquals);
 
 export function foo() {
 }
