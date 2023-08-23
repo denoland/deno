16ad20a203e5ee204083ccb7b463974f6772482f
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-08-23 12:02:43 +0200

    use published std modules

--- try_workspace/bar/bar.js
+++ try_workspace/bar/bar.js
@@ -1,9 +1,11 @@
 import codeBlockWriter from "codeBlockWriter";
 import * as chain from "chain/file1.ts";
-// import { foo } from "foo/foo.js";
+import * as colors from "@std/fmt/colors.ts";
+// import { foo } from "deno:@foo/foo@1";
 
 console.log(" > BAR > codeBlockWriter > ", codeBlockWriter);
 console.log(" > BAR > chain > ", chain);
+console.log(" > BAR > @std/fmt/colors > ", colors.red("Hello!"));
 // console.log(" > BAR > foo > ", foo);
 
 export function bar() {
--- try_workspace/foo/foo.js
+++ try_workspace/foo/foo.js
@@ -1,8 +1,10 @@
 import codeBlockWriter from "codeBlockWriter";
 import * as tsMorph from "tsMorph";
+import * as assert from "@std/assert/mod.ts";
 
 console.log(" > FOO > codeBlockWriter > ", codeBlockWriter);
 console.log(" > FOO > tsMorph > ", tsMorph);
+console.log(" > FOO > @std/assert > ", assert.assertEquals(1, 1));
 
 export function foo() {
 }
