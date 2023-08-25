a526cff0a9888a475d5b542efda443fe720a93d0
Valentin Anger <syrupthinker@gryphno.de>
2023-08-26 01:19:23 +0200

    feat(cli/tools): add TAP test reporter (#14390) (#20073)
    This PR adds a test reporter for the [Test Anything
    Protocol](https://testanything.org).
    
    It makes the following implementation decisions:
    - No TODO pragma, as there is no such marker in `Deno.test`
    - SKIP pragma for `ignore`d tests
    - Test steps are treated as TAP14 subtests
      - Support for this in consumers seems spotty
    - Some consumers will incorrectly interpret these markers, resulting in
    unexpected output
    - Considering the lack of support, and to avoid implementation
    complexity,
    subtests are at most one level deep (all test steps are in the same
    subtest)
    - To accommodate consumers that use comments to indicate test-suites
    (unspecced)
      - The test module path is output as a comment
      - This is disabled for `--parallel` testing
    - Failure diagnostics are output as JSON, which is also valid YAML
    - The structure is not specified, so the format roughly follows the spec
    example:
      ```
      ---
      message: "Failed with error 'hostname peebles.example.com not found'"
      severity: fail
      found:
        hostname: 'peebles.example.com'
        address: ~
      wanted:
        hostname: 'peebles.example.com'
        address: '85.193.201.85'
      at:
        file: test/dns-resolve.c
        line: 142
      ...
      ```

--- cli/tools/test/mod.rs
+++ cli/tools/test/mod.rs
@@ -89,6 +89,7 @@ use reporters::CompoundTestReporter;
 use reporters::DotTestReporter;
 use reporters::JunitTestReporter;
 use reporters::PrettyTestReporter;
+use reporters::TapTestReporter;
 use reporters::TestReporter;
 
 /// The test mode is used to determine how a specifier is to be tested.
@@ -404,6 +405,9 @@ fn get_test_reporter(options: &TestSpecifiersOptions) -> Box<dyn TestReporter> {
     TestReporterConfig::Junit => {
       Box::new(JunitTestReporter::new("-".to_string()))
     }
+    TestReporterConfig::Tap => Box::new(TapTestReporter::new(
+      options.concurrent_jobs > NonZeroUsize::new(1).unwrap(),
+    )),
   };
 
   if let Some(junit_path) = &options.junit_path {
