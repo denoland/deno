3914ac5f21ebab1568d8f68277c780951ac9bf91
Matt Mastracci <matthew@mastracci.com>
2023-08-25 10:11:40 -0600

    fix(ext/tls): upgrade webpki version

--- cli/args/mod.rs
+++ cli/args/mod.rs
@@ -487,7 +487,7 @@ pub fn get_root_cert_store(
     match store.as_str() {
       "mozilla" => {
         root_cert_store.add_server_trust_anchors(
-          webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+          webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
             rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
               ta.subject,
               ta.spki,
--- ext/tls/lib.rs
+++ ext/tls/lib.rs
@@ -146,7 +146,7 @@ pub fn create_default_root_cert_store() -> RootCertStore {
   let mut root_cert_store = RootCertStore::empty();
   // TODO(@justinmchase): Consider also loading the system keychain here
   root_cert_store.add_server_trust_anchors(
-    webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+    webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
       rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
         ta.subject,
         ta.spki,
