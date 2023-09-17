6dca5f07a550656370f5a02ac8400a0f98973363
Nayeem Rahman <nayeemrmn99@gmail.com>
2023-09-17 02:23:08 +0100

    format

--- cli/build.rs
+++ cli/build.rs
@@ -437,8 +437,9 @@ fn main() {
   // Host snapshots won't work when cross compiling.
   let target = env::var("TARGET").unwrap();
   let host = env::var("HOST").unwrap();
-  let skip_cross_check = env::var("DENO_SKIP_CROSS_BUILD_CHECK").map_or(false, |v| v == "1");
-  if (!skip_cross_check) && (target != host) {
+  let skip_cross_check =
+    env::var("DENO_SKIP_CROSS_BUILD_CHECK").map_or(false, |v| v == "1");
+  if !skip_cross_check && target != host {
     panic!("Cross compiling with snapshot is not supported.");
   }
 
