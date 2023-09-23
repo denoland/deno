4b7a491217721d5a0f8c29c6816c0dd697b2fa78
Laurence Rowe <l@lrowe.co.uk>
2023-09-22 20:20:47 -0700

    use scopeguard defer to handle drop

--- ext/http/slab.rs
+++ ext/http/slab.rs
@@ -10,6 +10,7 @@ use http::HeaderMap;
 use hyper1::body::Incoming;
 use hyper1::upgrade::OnUpgrade;
 
+use scopeguard::defer;
 use slab::Slab;
 use std::cell::RefCell;
 use std::cell::RefMut;
@@ -50,6 +51,27 @@ impl Drop for HttpRequestBodyAutocloser {
       res.close();
     }
   }
+}
+
+pub async fn new_slab_future(
+  request: Request,
+  request_info: HttpConnectionProperties,
+  refcount: RefCount,
+  tx: tokio::sync::mpsc::Sender<SlabId>,
+) -> Result<Response, hyper::Error> {
+  let index = slab_insert(request, request_info, refcount);
+  defer! {
+    slab_drop(index);
+  }
+  let rx = slab_get(index).promise();
+  if tx.send(index).await.is_ok() {
+    http_trace!(index, "SlabFuture await");
+    // We only need to wait for completion if we aren't closed
+    rx.await;
+    http_trace!(index, "SlabFuture complete");
+  }
+  let response = slab_get(index).take_response();
+  Ok(response)
 }
 
 pub struct HttpSlabRecord {
