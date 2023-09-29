3d5e434dc21b878b83535ebbf8d4c29a217327b7
Nayeem Rahman <nayeemrmn99@gmail.com>
2023-09-29 02:56:31 +0100

    add extension to specifier for tsc, normalize all tsc result specifiers

--- cli/tests/integration/lsp_tests.rs
+++ cli/tests/integration/lsp_tests.rs
@@ -7741,7 +7741,7 @@ fn lsp_jupyter_diagnostics() {
   client.initialize_default();
   let diagnostics = client.did_open(json!({
     "textDocument": {
-      "uri": "deno-notebook-cell:/a/file.ts#abc.ts",
+      "uri": "deno-notebook-cell:/a/file.ts#abc",
       "languageId": "typescript",
       "version": 1,
       "text": "Deno.readTextFileSync(1234);",
@@ -7751,7 +7751,7 @@ fn lsp_jupyter_diagnostics() {
     json!(diagnostics.all_messages()),
     json!([
       {
-        "uri": "deno-notebook-cell:/a/file.ts#abc.ts",
+        "uri": "deno-notebook-cell:/a/file.ts#abc",
         "diagnostics": [
           {
             "range": {
@@ -9547,7 +9547,7 @@ fn lsp_data_urls_with_jsx_compiler_option() {
         "end": { "line": 1, "character": 1 }
       }
     }, {
-      "uri": "deno:/5c42b5916c4a3fb55be33fdb0c3b1f438639420592d150fca1b6dc043c1df3d9/data_url.ts",
+      "uri": "deno:/ed0224c51f7e2a845dfc0941ed6959675e5e3e3d2a39b127f0ff569c1ffda8d8/data_url.ts",
       "range": {
         "start": { "line": 0, "character": 7 },
         "end": {"line": 0, "character": 14 },
