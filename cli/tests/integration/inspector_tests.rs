266d45de5697fdb40a2552f5e409a7d3f5e284f3
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-10-02 14:34:51 +0200

    chore: reenable inspector tests for npm support

--- cli/tests/integration/inspector_tests.rs
+++ cli/tests/integration/inspector_tests.rs
@@ -1182,10 +1182,6 @@ async fn inspector_profile() {
   tester.child.wait().unwrap();
 }
 
-// TODO(bartlomieju): this test became flaky on CI after wiring up "ext/node"
-// compatibility layer. Can't reproduce this problem locally for either Mac M1
-// or Linux. Ignoring for now to unblock further integration of "ext/node".
-#[ignore]
 #[tokio::test]
 async fn inspector_break_on_first_line_npm_esm() {
   let _server = http_server();
@@ -1252,10 +1248,6 @@ async fn inspector_break_on_first_line_npm_esm() {
   tester.child.wait().unwrap();
 }
 
-// TODO(bartlomieju): this test became flaky on CI after wiring up "ext/node"
-// compatibility layer. Can't reproduce this problem locally for either Mac M1
-// or Linux. Ignoring for now to unblock further integration of "ext/node".
-#[ignore]
 #[tokio::test]
 async fn inspector_break_on_first_line_npm_cjs() {
   let _server = http_server();
@@ -1321,10 +1313,6 @@ async fn inspector_break_on_first_line_npm_cjs() {
   tester.child.wait().unwrap();
 }
 
-// TODO(bartlomieju): this test became flaky on CI after wiring up "ext/node"
-// compatibility layer. Can't reproduce this problem locally for either Mac M1
-// or Linux. Ignoring for now to unblock further integration of "ext/node".
-#[ignore]
 #[tokio::test]
 async fn inspector_error_with_npm_import() {
   let script = util::testdata_path().join("inspector/error_with_npm_import.js");
