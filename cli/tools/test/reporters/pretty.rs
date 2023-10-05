ac464ead49c9f676d7117baa2ab06433e132e188
Nayeem Rahman <nayeemrmn99@gmail.com>
2023-10-05 21:45:11 +0100

    feat(jupyter): support Deno.test() (#20778)

--- cli/tools/test/reporters/pretty.rs
+++ cli/tools/test/reporters/pretty.rs
@@ -43,6 +43,10 @@ impl PrettyTestReporter {
     }
   }
 
+  pub fn with_writer(self, writer: Box<dyn std::io::Write>) -> Self {
+    Self { writer, ..self }
+  }
+
   fn force_report_wait(&mut self, description: &TestDescription) {
     if !self.in_new_line {
       writeln!(&mut self.writer).unwrap();
@@ -368,6 +372,9 @@ impl TestReporter for PrettyTestReporter {
     _test_steps: &IndexMap<usize, TestStepDescription>,
   ) {
     common::report_summary(&mut self.writer, &self.cwd, &self.summary, elapsed);
+    if !self.repl {
+      writeln!(&mut self.writer).unwrap();
+    }
     self.in_new_line = true;
   }
 
