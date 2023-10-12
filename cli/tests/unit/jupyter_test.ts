9dc59e4f053ce9aae422b338dc9db31ed4bed2b3
Kyle Kelley <rgbkrk@gmail.com>
2023-10-11 18:18:13 -0700

    trim down to unit testable

--- cli/tests/unit/jupyter_test.ts
+++ cli/tests/unit/jupyter_test.ts
@@ -1,9 +1,10 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import { assertThrows, assertEquals } from "./test_util.ts";
+import { assertThrows } from "./test_util.ts";
 
-import vl from "npm:vega-lite-api";
-
-const { format } = Deno.jupyter;
+import {
+  assertSpyCall,
+  spy,
+} from "https://deno.land/std@0.202.0/testing/mock.ts";
 
 Deno.test("Deno.jupyter is not available", () => {
   assertThrows(
@@ -11,15 +12,6 @@ Deno.test("Deno.jupyter is not available", () => {
     "Deno.jupyter is only available in `deno jupyter` subcommand.",
   );
 });
-
-import {
-  assertSpyCall,
-  spy,
-} from "https://deno.land/std@0.202.0/testing/mock.ts";
-
-import { createCanvas } from "https://deno.land/x/canvas@v1.4.1/mod.ts";
-
-const { display } = Deno.jupyter;
 
 export async function fakeBroadcast(
   _msgType: string,
@@ -34,6 +26,10 @@ export async function fakeBroadcast(
 }
 
 export async function assertDisplayedAs(obj: unknown, result: object) {
+  Deno[Deno.internal].enableJupyter();
+
+  const { display } = Deno.jupyter;
+
   const originalBroadcast = Deno.jupyter.broadcast;
   const mockedBroadcast = spy(fakeBroadcast);
 
@@ -49,10 +45,12 @@ export async function assertDisplayedAs(obj: unknown, result: object) {
 }
 
 Deno.test("display(canvas) creates a PNG", async () => {
-  const canvas = createCanvas(5, 5);
-  const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "red";
-  ctx.fillRect(0, 0, 5, 5);
+  // Let's make a fake Canvas with a fake Data URL
+  class FakeCanvas {
+    toDataURL() {
+      return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAAXNSR0IArs4c6QAAAARzQklUCAgICHwIZIgAAAAVSURBVAiZY/zPwPCfAQ0woQtQQRAAzqkCCB/D3o0AAAAASUVORK5CYII=";
+    }
+  }
 
   await assertDisplayedAs(canvas, {
     "image/png":
@@ -105,59 +103,3 @@ Deno.test(
     await assertDisplayedAs(example, { "application/json": { x: 3 } });
   },
 );
-
-
-Deno.test("display(vl_plot) returns a MediaBundle", () => {
-  const plot = vl
-    .markBar({ tooltip: true })
-    .data([
-      { a: "A", b: 28 },
-      { a: "B", b: 55 },
-      { a: "C", b: 43 },
-      { a: "D", b: 91 },
-      { a: "E", b: 81 },
-      { a: "F", b: 53 },
-      { a: "G", b: 19 },
-      { a: "H", b: 87 },
-      { a: "I", b: 52 },
-    ])
-    .encode(
-      vl.x().fieldQ("b"),
-      vl.y().fieldN("a"),
-      vl.tooltip([vl.fieldQ("b"), vl.fieldN("a")]),
-    );
-
-  const bundle = format(plot);
-
-  const expected = {
-    "application/vnd.vegalite.v5+json": {
-      $schema: "https://vega.github.io/schema/vega-lite/v5.json",
-      mark: { type: "bar", tooltip: true },
-      data: {
-        values: [
-          { a: "A", b: 28 },
-          { a: "B", b: 55 },
-          { a: "C", b: 43 },
-          { a: "D", b: 91 },
-          { a: "E", b: 81 },
-          { a: "F", b: 53 },
-          { a: "G", b: 19 },
-          { a: "H", b: 87 },
-          { a: "I", b: 52 },
-        ],
-      },
-      encoding: {
-        x: { field: "b", type: "quantitative" },
-        y: { field: "a", type: "nominal" },
-        tooltip: [
-          { field: "b", type: "quantitative" },
-          { field: "a", type: "nominal" },
-        ],
-      },
-    },
-  };
-
-  assertEquals(bundle, expected);
-});
-
-
