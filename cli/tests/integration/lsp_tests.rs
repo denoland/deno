b5c6ac8c2f04bae11c0689ab0a0557deaa8b5380
Nayeem Rahman <nayeemrmn99@gmail.com>
2023-11-15 10:24:17 +0000

    import map test

--- cli/tests/integration/lsp_tests.rs
+++ cli/tests/integration/lsp_tests.rs
@@ -10656,6 +10656,124 @@ fn lsp_config_scopes_lockfile() {
 }
 
 #[test]
+fn lsp_config_scopes_import_map() {
+  let context = TestContextBuilder::new()
+    .use_http_server()
+    .use_temp_cwd()
+    .build();
+  let temp_dir = context.temp_dir();
+  temp_dir.create_dir_all("project1");
+  temp_dir.write(
+    "project1/deno.json",
+    json!({
+      "imports": {
+        "hellojs": "http://localhost:4545/run/001_hello.js",
+      },
+    })
+    .to_string(),
+  );
+  temp_dir.write(
+    "project1/file.ts",
+    r#"
+      import "hellojs";
+      import "hellots";
+    "#,
+  );
+  temp_dir.create_dir_all("project2");
+  temp_dir.write(
+    "project2/deno.json",
+    json!({
+      "imports": {
+        "hellots": "http://localhost:4545/run/002_hello.ts",
+      },
+    })
+    .to_string(),
+  );
+  temp_dir.write(
+    "project2/file.ts",
+    r#"
+      import "hellojs";
+      import "hellots";
+    "#,
+  );
+  let mut client = context.new_lsp_command().build();
+  client.initialize_default();
+  client.write_request(
+    "workspace/executeCommand",
+    json!({
+      "command": "deno.cache",
+      "arguments": [[], temp_dir.uri().join("project1/file.ts").unwrap()],
+    }),
+  );
+  client.write_request(
+    "workspace/executeCommand",
+    json!({
+      "command": "deno.cache",
+      "arguments": [[], temp_dir.uri().join("project2/file.ts").unwrap()],
+    }),
+  );
+  let diagnostics = client.did_open(json!({
+    "textDocument": {
+      "uri": temp_dir.uri().join("project1/file.ts").unwrap(),
+      "languageId": "typescript",
+      "version": 1,
+      "text": temp_dir.read_to_string("project1/file.ts"),
+    },
+  }));
+  assert_eq!(
+    json!(diagnostics.messages_with_source("deno")),
+    json!({
+      "uri": temp_dir.uri().join("project1/file.ts").unwrap(),
+      "diagnostics": [{
+        "range": {
+          "start": { "line": 2, "character": 13 },
+          "end": { "line": 2, "character": 22 },
+        },
+        "severity": 1,
+        "code": "resolver-error",
+        "source": "deno",
+        "message": format!("Relative import path \"hellots\" not prefixed with / or ./ or ../ and not in import map from \"{}\"", temp_dir.uri().join("project1/file.ts").unwrap()),
+      }],
+      "version": 1,
+    })
+  );
+  client.write_notification(
+    "textDocument/didClose",
+    json!({
+      "textDocument": {
+        "uri": temp_dir.uri().join("project1/file.ts").unwrap(),
+      },
+    }),
+  );
+  let diagnostics = client.did_open(json!({
+    "textDocument": {
+      "uri": temp_dir.uri().join("project2/file.ts").unwrap(),
+      "languageId": "typescript",
+      "version": 1,
+      "text": temp_dir.read_to_string("project2/file.ts"),
+    },
+  }));
+  assert_eq!(
+    json!(diagnostics.messages_with_source("deno")),
+    json!({
+      "uri": temp_dir.uri().join("project2/file.ts").unwrap(),
+      "diagnostics": [{
+        "range": {
+          "start": { "line": 1, "character": 13 },
+          "end": { "line": 1, "character": 22 },
+        },
+        "severity": 1,
+        "code": "resolver-error",
+        "source": "deno",
+        "message": format!("Relative import path \"hellojs\" not prefixed with / or ./ or ../ and not in import map from \"{}\"", temp_dir.uri().join("project2/file.ts").unwrap()),
+      }],
+      "version": 1,
+    })
+  );
+  client.shutdown();
+}
+
+#[test]
 fn lsp_config_scopes_package_json() {
   let context = TestContextBuilder::new()
     .use_http_server()
