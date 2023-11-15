d2a19093a024d53d3db4e33540e5e16d0809fb29
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-11-16 00:57:58 +0100

    unstable check, use published import_map crate

--- cli/args/mod.rs
+++ cli/args/mod.rs
@@ -661,6 +661,12 @@ impl CliOptions {
         None
       };
 
+    // TODO(bartlomieju): remove in v1.39 or v1.40.
+    if maybe_workspace_config.is_some() && !flags.unstable_workspaces {
+      eprintln!("Use of unstable 'workspaces' feature. The --unstable-workspaces flags must be provided.");
+      std::process::exit(70);
+    }
+
     if let Some(env_file_name) = &flags.env_file {
       if (from_filename(env_file_name)).is_err() {
         bail!("Unable to load '{env_file_name}' environment variable file")
