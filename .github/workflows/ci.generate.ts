9ae2cb04c1e7f3ba4dd4d65fd928782f71c50ebd
Matt Mastracci <matthew@mastracci.com>
2023-11-17 14:39:11 -0700

    .

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -513,7 +513,7 @@ const ci = {
             "./tools/install_prebuilt.js ld64.lld",
             "echo $GITHUB_WORKSPACE/third_party/prebuilt/mac >> $GITHUB_PATH",
           ],
-          if: [`matrix.os == '${macosArmRunner}'`],
+          if: `matrix.os == '${macosArmRunner}'`,
         },
         {
           name: "Log versions",
