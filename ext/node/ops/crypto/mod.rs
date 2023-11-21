ef5f276ea27d8929a0a5ad10cde5169d36161674
Divy Srivastava <dj.srivastava23@gmail.com>
2023-11-21 18:45:31 +0530

    fix(ext/node): sign with private keys

--- ext/node/ops/crypto/mod.rs
+++ ext/node/ops/crypto/mod.rs
@@ -363,23 +363,30 @@ pub fn op_node_sign(
   #[buffer] digest: &[u8],
   #[string] digest_type: &str,
   #[serde] key: StringOrBuffer,
-  #[string] key_type: &str,
-  #[string] key_format: &str,
+  #[string] type_: &str,
+  #[string] format: &str,
 ) -> Result<ToJsBuffer, AnyError> {
-  match key_type {
-    "rsa" => {
+  let (label, doc) =
+    pkcs8::SecretDocument::from_pem(std::str::from_utf8(&key).unwrap())?;
+
+  let pk_info = match format {
+    "pem" => {
+      if label != "PRIVATE KEY" {
+        pkcs8::PrivateKeyInfo::try_from(doc.as_bytes())?
+      } else if label != "RSA PRIVATE KEY" {
+        pkcs8::PrivateKeyInfo::try_from(doc.as_bytes())?
+      } else {
+        return Err(type_error("Invalid PEM label"));
+      }
+    }
+    _ => return Err(type_error("Unsupported key format")),
+  };
+
+  let alg = pk_info.algorithm.oid;
+  match alg {
+    RSA_ENCRYPTION_OID => {
       use rsa::pkcs1v15::SigningKey;
-      let key = match key_format {
-        "pem" => RsaPrivateKey::from_pkcs8_pem((&key).try_into()?)
-          .map_err(|_| type_error("Invalid RSA private key"))?,
-        // TODO(kt3k): Support der and jwk formats
-        _ => {
-          return Err(type_error(format!(
-            "Unsupported key format: {}",
-            key_format
-          )))
-        }
-      };
+      let key = RsaPrivateKey::try_from(pk_info)?;
       Ok(
         match digest_type {
           "sha224" => {
@@ -408,10 +415,7 @@ pub fn op_node_sign(
         .into(),
       )
     }
-    _ => Err(type_error(format!(
-      "Signing with {} keys is not supported yet",
-      key_type
-    ))),
+    _ => Err(type_error("Unsupported signing key")),
   }
 }
 
