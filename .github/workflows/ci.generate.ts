831dc5206179f3c57c5f7d69a4106f589aae8180
Matt Mastracci <matthew@mastracci.com>
2023-11-22 18:21:30 -0700

    .

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -701,7 +701,7 @@ const ci = {
             'echo "Key is $(echo $APPLE_CODESIGN_KEY | base64 -d | wc -c) bytes"',
             "rcodesign sign target/release/deno " +
             "--code-signature-flags=runtime " +
-            "--p12-password='$APPLE_CODESIGN_PASSWORD' " +
+            '--p12-password="$APPLE_CODESIGN_PASSWORD" ' +
             "--p12-file=<(echo $APPLE_CODESIGN_KEY | base64 -d) " +
             "--entitlements-xml-file=cli/entitlements.plist",
             "cd target/release",
