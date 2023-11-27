2b7e145e56c7f1fbb4498d4ec6c6f9d237405db7
scarf <greenscarf005@gmail.com>
2023-11-28 00:32:12 +0900

    feat(fmt): support formatting code blocks in Jupyter notebooks (#21310)

--- cli/args/flags.rs
+++ cli/args/flags.rs
@@ -1579,7 +1579,9 @@ Ignore formatting a file by adding an ignore comment at the top of the file:
             .help("Set content type of the supplied file")
             // prefer using ts for formatting instead of js because ts works in more scenarios
             .default_value("ts")
-            .value_parser(["ts", "tsx", "js", "jsx", "md", "json", "jsonc"]),
+            .value_parser([
+              "ts", "tsx", "js", "jsx", "md", "json", "jsonc", "ipynb",
+            ]),
         )
         .arg(
           Arg::new("ignore")
