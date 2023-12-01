3642493b3e0d997992600dfe4ec8713295edb43c
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-12-01 01:46:02 +0100

    performance.mark split into args and no args

--- cli/lsp/performance.rs
+++ cli/lsp/performance.rs
@@ -132,10 +132,7 @@ impl Performance {
       .collect()
   }
 
-  /// Marks the start of a measurement which returns a performance mark
-  /// structure, which is then passed to `.measure()` to finalize the duration
-  /// and add it to the internal buffer.
-  pub fn mark<S: AsRef<str>, V: Serialize>(
+  fn mark_inner<S: AsRef<str>, V: Serialize>(
     &self,
     name: S,
     maybe_args: Option<V>,
@@ -163,6 +160,24 @@ impl Performance {
       count: *count,
       start: Instant::now(),
     }
+  }
+
+  /// Marks the start of a measurement which returns a performance mark
+  /// structure, which is then passed to `.measure()` to finalize the duration
+  /// and add it to the internal buffer.
+  pub fn mark<S: AsRef<str>>(&self, name: S) -> PerformanceMark {
+    self.mark_inner(name, None::<()>)
+  }
+
+  /// Marks the start of a measurement which returns a performance mark
+  /// structure, which is then passed to `.measure()` to finalize the duration
+  /// and add it to the internal buffer.
+  pub fn mark_with_args<S: AsRef<str>, V: Serialize>(
+    &self,
+    name: S,
+    args: V,
+  ) -> PerformanceMark {
+    self.mark_inner(name, Some(args))
   }
 
   /// A function which accepts a previously created performance mark which will
@@ -201,9 +216,9 @@ mod tests {
   #[test]
   fn test_average() {
     let performance = Performance::default();
-    let mark1 = performance.mark("a", None::<()>);
-    let mark2 = performance.mark("a", None::<()>);
-    let mark3 = performance.mark("b", None::<()>);
+    let mark1 = performance.mark("a");
+    let mark2 = performance.mark("a");
+    let mark3 = performance.mark("b");
     performance.measure(mark2);
     performance.measure(mark1);
     performance.measure(mark3);
@@ -217,8 +232,8 @@ mod tests {
   #[test]
   fn test_averages() {
     let performance = Performance::default();
-    let mark1 = performance.mark("a", None::<()>);
-    let mark2 = performance.mark("a", None::<()>);
+    let mark1 = performance.mark("a");
+    let mark2 = performance.mark("a");
     performance.measure(mark2);
     performance.measure(mark1);
     let averages = performance.averages();
