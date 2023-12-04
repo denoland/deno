240ce61f9a971e80a4f4f5c9e760f0cc7cc4a2fb
Raashid Anwar <raashid12anwar@gmail.com>
2023-12-04 22:35:43 +0530

    lint fix

--- cli/tests/unit/kv_queue_undelivered_test.ts
+++ cli/tests/unit/kv_queue_undelivered_test.ts
@@ -1,5 +1,5 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import { assert, assertEquals, assertRejects } from "./test_util.ts";
+import { assertEquals, assertRejects } from "./test_util.ts";
 
 const sleep = (time: number) => new Promise((r) => setTimeout(r, time));
 
