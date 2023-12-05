103ba9d2c8315c353de3558ccee500d7d1f44dba
Raashid Anwar <raashid12anwar@gmail.com>
2023-12-05 23:18:25 +0530

    test fix

--- cli/tests/unit/kv_queue_test.ts
+++ cli/tests/unit/kv_queue_test.ts
@@ -1,11 +1,12 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-import { assertEquals } from "./test_util.ts";
+import { assertEquals, assertFalse } from "./test_util.ts";
 
 Deno.test({}, async function queueTestDbClose() {
   const db: Deno.Kv = await Deno.openKv(":memory:");
   db.close();
   try {
     await db.listenQueue(() => {});
+    assertFalse(false);
   } catch (e) {
     assertEquals(e.message, "already closed");
   }
