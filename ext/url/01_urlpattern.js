15b40fe5b635d4168cb6a70625f697c2349373d1
Luca Casonato <hello@lcas.dev>
2023-12-07 13:28:59 +0100

    perf(ext/url): improve URLPattern perf
    This significantly optimizes URLPattern in the case where the same
    URL is matched against many patterns (like in a router).
    
    Also minor speedups to other use-cases.

--- ext/url/01_urlpattern.js
+++ ext/url/01_urlpattern.js
@@ -13,17 +13,20 @@ import * as webidl from "ext:deno_webidl/00_webidl.js";
 import { createFilteredInspectProxy } from "ext:deno_console/01_console.js";
 const primordials = globalThis.__bootstrap.primordials;
 const {
-  ArrayPrototypePop,
+  ArrayPrototypePush,
+  MathRandom,
+  ObjectAssign,
+  ObjectCreate,
+  ObjectPrototypeIsPrototypeOf,
   RegExpPrototypeExec,
   RegExpPrototypeTest,
-  ObjectPrototypeIsPrototypeOf,
+  SafeMap,
   SafeRegExp,
   Symbol,
   SymbolFor,
   TypeError,
 } = primordials;
 
-const EMPTY_MATCH = [""];
 const _components = Symbol("components");
 
 /**
@@ -55,9 +58,67 @@ const COMPONENTS_KEYS = [
  * @property {string[]} groupNameList
  */
 
+/**
+ * @template K
+ * @template V
+ */
+class LRUCache {
+  /** @type {Map<K, V>} */
+  #map = new SafeMap();
+  #capacity = 0;
+
+  /** @type {K} */
+  #lastUsedKey = undefined;
+  /** @type {V} */
+  #lastUsedValue = undefined;
+
+  /** @param {number} capacity */
+  constructor(capacity) {
+    this.#capacity = capacity;
+  }
+
+  /**
+   * @param {K} key
+   * @param {(key: K) => V} factory
+   * @return {V}
+   */
+  getOrInsert(key, factory) {
+    if (this.#lastUsedKey === key) return this.#lastUsedValue;
+    const value = this.#map.get(key);
+    if (value !== undefined) {
+      if (MathRandom() < 0.1) {
+        // put the item into the map
+        this.#map.delete(key);
+        this.#map.set(key, value);
+      }
+      this.#lastUsedKey = key;
+      this.#lastUsedValue = value;
+      return value;
+    } else {
+      // value doesn't exist yet, create
+      const value = factory(key);
+      if (MathRandom() < 0.1) {
+        // if the map is at capacity, delete the oldest (first) element
+        if (this.#map.size > this.#capacity) {
+          this.#map.delete(this.#map.keys().next().value);
+        }
+        // insert the new value
+        this.#map.set(key, value);
+      }
+      this.#lastUsedKey = key;
+      this.#lastUsedValue = value;
+      return value;
+    }
+  }
+}
+
+const matchInputCache = new LRUCache(4096);
+
 class URLPattern {
   /** @type {Components} */
   [_components];
+
+  #reusedResult;
 
   /**
    * @param {URLPatternInput} input
@@ -81,9 +142,6 @@ class URLPattern {
           components[key].regexpString,
           "u",
         );
-        // used for fast path
-        components[key].matchOnEmptyInput =
-          components[key].regexpString === "^$";
       } catch (e) {
         throw new TypeError(`${prefix}: ${key} is invalid; ${e.message}`);
       }
@@ -145,20 +203,28 @@ class URLPattern {
       baseURL = webidl.converters.USVString(baseURL, prefix, "Argument 2");
     }
 
-    const res = ops.op_urlpattern_process_match_input(
-      input,
-      baseURL,
-    );
-    if (res === null) {
-      return false;
-    }
+    const res = baseURL === undefined
+      ? matchInputCache.getOrInsert(
+        input,
+        ops.op_urlpattern_process_match_input,
+      )
+      : ops.op_urlpattern_process_match_input(input, baseURL);
+    if (res === null) return false;
 
     const values = res[0];
 
     for (let i = 0; i < COMPONENTS_KEYS.length; ++i) {
       const key = COMPONENTS_KEYS[i];
-      if (!RegExpPrototypeTest(this[_components][key].regexp, values[key])) {
-        return false;
+      const component = this[_components][key];
+      switch (component.regexpString) {
+        case "^$":
+          if (values[key] !== "") return false;
+          break;
+        case "^(.*)$":
+          break;
+        default: {
+          if (!RegExpPrototypeTest(component.regexp, values[key])) return false;
+        }
       }
     }
 
@@ -179,48 +245,65 @@ class URLPattern {
       baseURL = webidl.converters.USVString(baseURL, prefix, "Argument 2");
     }
 
-    const res = ops.op_urlpattern_process_match_input(
-      input,
-      baseURL,
-    );
+    const res = baseURL === undefined
+      ? matchInputCache.getOrInsert(
+        input,
+        ops.op_urlpattern_process_match_input,
+      )
+      : ops.op_urlpattern_process_match_input(input, baseURL);
     if (res === null) {
       return null;
     }
 
-    const { 0: values, 1: inputs } = res;
-    if (inputs[1] === null) {
-      ArrayPrototypePop(inputs);
-    }
+    const { 0: values, 1: inputs } = res; /** @type {URLPatternResult} */
 
-    /** @type {URLPatternResult} */
-    const result = { inputs };
+    // globalThis.allocAttempt++;
+    this.#reusedResult ??= { inputs: [undefined] };
+    const result = this.#reusedResult;
+    // We don't construct the `inputs` until after the matching is done under
+    // the assumption that most patterns do not match.
+
+    const components = this[_components];
 
     for (let i = 0; i < COMPONENTS_KEYS.length; ++i) {
       const key = COMPONENTS_KEYS[i];
       /** @type {Component} */
-      const component = this[_components][key];
-      const input = values[key];
+      const component = components[key];
 
-      const match = component.matchOnEmptyInput && input === ""
-        ? EMPTY_MATCH // fast path
-        : RegExpPrototypeExec(component.regexp, input);
-
-      if (match === null) {
-        return null;
-      }
-
-      const groups = {};
-      const groupList = component.groupNameList;
-      for (let i = 0; i < groupList.length; ++i) {
-        groups[groupList[i]] = match[i + 1] ?? "";
-      }
-
-      result[key] = {
-        input,
-        groups,
+      const res = result[key] ??= {
+        input: values[key],
+        groups: component.regexpString === "^(.*)$" ? { "0": values[key] } : {},
       };
+
+      switch (component.regexpString) {
+        case "^$":
+          if (values[key] !== "") return null;
+          break;
+        case "^(.*)$":
+          res.groups["0"] = values[key];
+          break;
+        default: {
+          const match = RegExpPrototypeExec(component.regexp, values[key]);
+          if (match === null) return null;
+          const groupList = component.groupNameList;
+          const groups = res.groups;
+          for (let i = 0; i < groupList.length; ++i) {
+            // TODO: this should use ObjectDefineProperty
+            groups[groupList[i]] = match[i + 1] ?? "";
+          }
+          break;
+        }
+      }
+      res.input = values[key];
     }
 
+    // Now populate result.inputs
+    result.inputs[0] = typeof inputs[0] === "string"
+      ? inputs[0]
+      : ObjectAssign(ObjectCreate(null), inputs[0]);
+    if (inputs[1] !== null) ArrayPrototypePush(result.inputs, inputs[1]);
+
+    this.#reusedResult = undefined;
     return result;
   }
 
