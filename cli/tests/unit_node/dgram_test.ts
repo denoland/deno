f635ae21ed0040507ea9a57e16d6e49a4fd6f3d4
CosminPerRam <cosmin.p@live.com>
2023-12-16 12:15:26 +0100

    fix: run linting

--- cli/tests/unit_node/dgram_test.ts
+++ cli/tests/unit_node/dgram_test.ts
@@ -5,33 +5,39 @@ import { createSocket } from "node:dgram";
 const listenPort = 4503;
 const listenPort2 = 4504;
 
-Deno.test("[node/dgram] udp ref and unref",
-  { permissions: { read: true, run: true, net: true } },
-  async () => {
-    const { promise, resolve } = Promise.withResolvers<Uint8Array>();
+Deno.test("[node/dgram] udp ref and unref", {
+  permissions: { read: true, run: true, net: true },
+}, async () => {
+  const { promise, resolve } = Promise.withResolvers<Uint8Array>();
 
-    const udpSocket = createSocket('udp4');
-    udpSocket.bind(listenPort);
+  const udpSocket = createSocket("udp4");
+  udpSocket.bind(listenPort);
 
-    udpSocket.unref();
-    udpSocket.ref();
+  udpSocket.unref();
+  udpSocket.ref();
 
-    udpSocket.on('message', (buffer, rinfo) => {
-      resolve(Uint8Array.from(buffer));
-    });
+  udpSocket.on("message", (buffer, _rinfo) => {
+    resolve(Uint8Array.from(buffer));
+  });
 
-    const conn = await Deno.listenDatagram({ port: listenPort2, transport: 'udp' });
-    await conn.send(new Uint8Array([0, 1, 2, 3]), {transport: "udp", port: listenPort, hostname: "127.0.0.1"});
+  const conn = await Deno.listenDatagram({
+    port: listenPort2,
+    transport: "udp",
+  });
+  await conn.send(new Uint8Array([0, 1, 2, 3]), {
+    transport: "udp",
+    port: listenPort,
+    hostname: "127.0.0.1",
+  });
 
-    const data = await promise;
-    assertEquals(data, new Uint8Array([0, 1, 2, 3]));
-  }
-);
+  const data = await promise;
+  assertEquals(data, new Uint8Array([0, 1, 2, 3]));
+});
 
-Deno.test("[node/dgram] udp unref",
-  { permissions: { read: true, run: true, net: true } },
-  async () => {
-    const [statusCode, _output] = await execCode(`
+Deno.test("[node/dgram] udp unref", {
+  permissions: { read: true, run: true, net: true },
+}, async () => {
+  const [statusCode, _output] = await execCode(`
       import { createSocket } from "node:dgram";
       const udpSocket = createSocket('udp4');
       udpSocket.bind(${listenPort2});
@@ -43,5 +49,5 @@ Deno.test("[node/dgram] udp unref",
       udpSocket.on('message', (buffer, rinfo) => {
       });
     `);
-    assertEquals(statusCode, 0);
+  assertEquals(statusCode, 0);
 });
