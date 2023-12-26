6c81af28e2811375fd9ec0c0a93ddb50c0977bdb
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-12-26 22:30:08 +0100

    registry server

--- test_util/src/servers/registry.rs
+++ test_util/src/servers/registry.rs
@@ -1,37 +1,80 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
-use hyper::server::Server;
-use hyper::service::make_service_fn;
-use hyper::service::service_fn;
-use hyper::Body;
-use hyper::Request;
-use hyper::Response;
-use hyper::StatusCode;
+use bytes::Bytes;
+use futures::Future;
+use futures::FutureExt;
+use http_body_util::combinators::UnsyncBoxBody;
+use http_body_util::Empty;
+use http_body_util::Full;
+use hyper1::body::Incoming;
+use hyper1::service::service_fn;
+use hyper1::Request;
+use hyper1::Response;
+use hyper1::StatusCode;
+use hyper_util::rt::TokioIo;
 use serde_json::json;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::pin::Pin;
+use tokio::net::TcpListener;
 
-pub async fn registry_server(port: u16) {
-  let registry_server_addr = SocketAddr::from(([127, 0, 0, 1], port));
-  let registry_server_svc = make_service_fn(|_| async {
-    Ok::<_, Infallible>(service_fn(registry_server_handler))
-  });
-  let registry_server =
-    Server::bind(&registry_server_addr).serve(registry_server_svc);
-  if let Err(e) = registry_server.await {
-    eprintln!("Registry server error: {:?}", e);
+async fn run_server<F, S>(
+  addr: SocketAddr,
+  service_fn_handler: F,
+  error_msg: &'static str,
+) where
+  F: Fn(Request<Incoming>) -> S + Copy + 'static,
+  S: Future<
+    Output = Result<
+      Response<UnsyncBoxBody<Bytes, Infallible>>,
+      hyper1::http::Error,
+    >,
+  >,
+{
+  let fut: Pin<Box<dyn Future<Output = Result<(), anyhow::Error>>>> =
+    async move {
+      let listener = TcpListener::bind(addr).await?;
+      loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        let service = service_fn(service_fn_handler);
+        deno_unsync::spawn(async move {
+          if let Err(e) = hyper1::server::conn::http1::Builder::new()
+            .serve_connection(io, service)
+            .await
+          {
+            eprintln!("{}: {:?}", error_msg, e);
+          }
+        });
+      }
+    }
+    .boxed_local();
+
+  if let Err(e) = fut.await {
+    eprintln!("{}: {:?}", error_msg, e);
   }
 }
 
+pub async fn registry_server(port: u16) {
+  let registry_server_addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+  run_server(
+    registry_server_addr,
+    registry_server_handler,
+    "Registry server error",
+  )
+  .await
+}
+
 async fn registry_server_handler(
-  req: Request<Body>,
-) -> Result<Response<Body>, hyper::http::Error> {
+  req: Request<Incoming>,
+) -> Result<Response<UnsyncBoxBody<Bytes, Infallible>>, hyper1::http::Error> {
   let path = req.uri().path();
 
   // TODO(bartlomieju): add a proper router here
   if path.starts_with("/api/scope/") {
     let body = serde_json::to_string_pretty(&json!({})).unwrap();
-    let res = Response::new(Body::from(body));
+    let res = Response::new(UnsyncBoxBody::new(Full::from(body)));
     return Ok(res);
   } else if path.starts_with("/api/scopes/") {
     let body = serde_json::to_string_pretty(&json!({
@@ -40,7 +83,7 @@ async fn registry_server_handler(
       "error": null
     }))
     .unwrap();
-    let res = Response::new(Body::from(body));
+    let res = Response::new(UnsyncBoxBody::new(Full::from(body)));
     return Ok(res);
   } else if path.starts_with("/api/publish_status/") {
     let body = serde_json::to_string_pretty(&json!({
@@ -49,11 +92,12 @@ async fn registry_server_handler(
       "error": null
     }))
     .unwrap();
-    let res = Response::new(Body::from(body));
+    let res = Response::new(UnsyncBoxBody::new(Full::from(body)));
     return Ok(res);
   }
 
+  let empty_body = UnsyncBoxBody::new(Empty::new());
   Response::builder()
     .status(StatusCode::NOT_FOUND)
-    .body(Body::empty())
+    .body(empty_body)
 }
