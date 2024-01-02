8e4feacd258b2fc019f2b9612133231fa8be14c0
Divy Srivastava <dj.srivastava23@gmail.com>
2024-01-02 10:30:09 +0530

    fix(ext/http): use arraybuffer binaryType for server websocket  (#21741)
    Ref
    https://github.com/denoland/deno/issues/15340#issuecomment-1872353134

--- ext/websocket/01_websocket.js
+++ ext/websocket/01_websocket.js
@@ -590,7 +590,11 @@ function createWebSocketBranded() {
   socket[_extensions] = "";
   socket[_protocol] = "";
   socket[_url] = "";
-  socket[_binaryType] = "blob";
+  // We use ArrayBuffer for server websockets for backwards compatibility
+  // and performance reasons.
+  //
+  // https://github.com/denoland/deno/issues/15340#issuecomment-1872353134
+  socket[_binaryType] = "arraybuffer";
   socket[_idleTimeoutDuration] = 0;
   socket[_idleTimeoutTimeout] = undefined;
   return socket;
