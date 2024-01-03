1b0c1715b7bb69d001dbc81859ab95c09e75247b
Jovi De Croock <decroockjovi@gmail.com>
2024-01-03 08:16:15 +0100

    Update flags.rs

--- cli/args/flags.rs
+++ cli/args/flags.rs
@@ -441,7 +441,6 @@ pub struct Flags {
   pub config_flag: ConfigFlag,
   pub node_modules_dir: Option<bool>,
   pub vendor: Option<bool>,
-  pub enable_sourcemaps: Option<bool>,
   pub enable_op_summary_metrics: bool,
   pub enable_testing_features: bool,
   pub ext: Option<String>,
