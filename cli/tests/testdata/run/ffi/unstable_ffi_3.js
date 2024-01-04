684922f88eff6e9e3ed35b3ab7df9d93a82f71b7
Matt Mastracci <matthew@mastracci.com>
2024-01-04 10:39:32 -0700

    fmt

--- cli/tests/testdata/run/ffi/unstable_ffi_3.js
+++ cli/tests/testdata/run/ffi/unstable_ffi_3.js
@@ -1,4 +1,5 @@
-const { op_ffi_call_ptr_nonblocking } = Deno[Deno.internal].core.ensureFastOps();
+const { op_ffi_call_ptr_nonblocking } = Deno[Deno.internal].core
+  .ensureFastOps();
 op_ffi_call_ptr_nonblocking(null, {
   name: null,
   parameters: [],
