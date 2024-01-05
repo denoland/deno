d6d85ec6a204f7b6f27350ec9a56fbef46cc6257
Kenta Moriuchi <moriken@kimamass.com>
2024-01-05 22:04:21 +0900

    remove false positives

--- ext/node/polyfills/internal/util/types.ts
+++ ext/node/polyfills/internal/util/types.ts
@@ -21,9 +21,6 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// TODO(petamoriken): enable prefer-primordials for node polyfills
-// deno-lint-ignore-file prefer-primordials
-
 import { primordials } from "ext:core/mod.js";
 import * as bindingTypes from "ext:deno_node/internal_binding/types.ts";
 export {
