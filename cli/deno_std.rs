ff2a08fe9fd27f89933cbff88591f4121b3fb3bf
denobot <33910674+denobot@users.noreply.github.com>
2024-01-12 09:58:36 -0500

    Cherry-pick version bump commit with conflicts

--- cli/deno_std.rs
+++ cli/deno_std.rs
@@ -2,4 +2,8 @@
 
 // WARNING: Ensure this is the only deno_std version reference as this
 // is automatically updated by the version bump workflow.
+<<<<<<< HEAD
 pub const CURRENT_STD_URL_STR: &str = "https://deno.land/std@0.211.0/";
+=======
+pub const CURRENT_STD_URL_STR: &str = "https://deno.land/std@0.212.0/";
+>>>>>>> aa23ccd1a (1.39.3 (#21913))
