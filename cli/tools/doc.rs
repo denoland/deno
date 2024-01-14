803ef12f97f7c95fbca9ee6603ae94ac7de3fe1b
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-01-14 17:50:58 +0100

    fix

--- cli/tools/doc.rs
+++ cli/tools/doc.rs
@@ -1,4 +1,4 @@
-<<// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
 use crate::args::DocFlags;
 use crate::args::DocHtmlFlag;
