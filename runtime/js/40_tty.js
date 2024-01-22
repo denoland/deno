5e1bff38c2d5e653e74947947fe38550887781e4
Asher Gomez <ashersaupingomez@gmail.com>
2024-01-22 11:25:34 +1100

    fix

--- runtime/js/40_tty.js
+++ runtime/js/40_tty.js
@@ -19,7 +19,7 @@ function isatty(rid) {
   internals.warnOnDeprecatedApi(
     "Deno.isatty()",
     new Error().stack,
-    "Use `resource.isatty()` instead.",
+    "Use `resource.isTerminal()` instead.",
   );
   return op_is_terminal(rid);
 }
