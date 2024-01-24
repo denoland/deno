4c0b66baab854617993cb242e3be8fe91fb9b0fc
Asher Gomez <ashersaupingomez@gmail.com>
2024-01-24 12:36:51 +1100

    refactor: set removal version for `deno bundle` (#22062)
    This change sets the removal version for the `deno bundle` sub-command
    for Deno v2. The warnings appear when `deno bundle` is run and in the
    `--help` menu.

--- cli/tools/bundle.rs
+++ cli/tools/bundle.rs
@@ -21,11 +21,8 @@ pub async fn bundle(
   bundle_flags: BundleFlags,
 ) -> Result<(), AnyError> {
   log::info!(
-    "{} \"deno bundle\" is deprecated and will be removed in the future.",
-    colors::yellow("Warning"),
-  );
-  log::info!(
-    "Use alternative bundlers like \"deno_emit\", \"esbuild\" or \"rollup\" instead."
+    "{}",
+    colors::yellow("⚠️ Warning: `deno bundle` is deprecated and will be removed in Deno 2.0.\nUse an alternative bundler like \"deno_emit\", \"esbuild\" or \"rollup\" instead."),
   );
 
   if let Some(watch_flags) = &bundle_flags.watch {
