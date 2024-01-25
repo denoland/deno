denoland/deno

commit aac0ad32bd589394316223f75e6f511331ff124c

feat: deprecate `Deno.FsFile` constructor and `Deno.FsFile.rid` (#22072)

ext/fs/30_fs.js
@@ -670,85 +670,90 @@ class FsFile {
   }
 
   get rid() {
+    internals.warnOnDeprecatedApi(
+      "Deno.FsFile.rid",
+      new Error().stack,
+      "Use `Deno.FsFile` methods directly instead.",
+    );
     return this.#rid;
   }
 
   write(p) {
-    return write(this.rid, p);
+    return write(this.#rid, p);
   }
 
   writeSync(p) {
-    return writeSync(this.rid, p);
+    return writeSync(this.#rid, p);
   }
 
   truncate(len) {
-    return ftruncate(this.rid, len);
+    return ftruncate(this.#rid, len);
   }
 
   truncateSync(len) {
-    return ftruncateSync(this.rid, len);
+    return ftruncateSync(this.#rid, len);
   }
 
   read(p) {
-    return read(this.rid, p);
+    return read(this.#rid, p);
   }
 
   readSync(p) {
-    return readSync(this.rid, p);
+    return readSync(this.#rid, p);
   }
 
   seek(offset, whence) {
-    return seek(this.rid, offset, whence);
+    return seek(this.#rid, offset, whence);
   }
 
   seekSync(offset, whence) {
-    return seekSync(this.rid, offset, whence);
+    return seekSync(this.#rid, offset, whence);
   }
 
   stat() {
-    return fstat(this.rid);
+    return fstat(this.#rid);
   }
 
   statSync() {
-    return fstatSync(this.rid);
+    return fstatSync(this.#rid);
   }
 
   async dataSync() {
-    await op_fs_fdatasync_async(this.rid);
+    await op_fs_fdatasync_async(this.#rid);
   }
 
   dataSyncSync() {
-    op_fs_fdatasync_sync(this.rid);
+    op_fs_fdatasync_sync(this.#rid);
   }
 
   close() {
-    core.close(this.rid);
+    core.close(this.#rid);
   }
 
   get readable() {
     if (this.#readable === undefined) {
-      this.#readable = readableStreamForRid(this.rid);
+      this.#readable = readableStreamForRid(this.#rid);
     }
     return this.#readable;
   }
 
   get writable() {
     if (this.#writable === undefined) {
-      this.#writable = writableStreamForRid(this.rid);
+      this.#writable = writableStreamForRid(this.#rid);
     }
     return this.#writable;
   }
 
   async sync() {
-    await op_fs_fsync_async(this.rid);
+    await op_fs_fsync_async(this.#rid);
   }
 
   syncSync() {
-    op_fs_fsync_sync(this.rid);
+    op_fs_fsync_sync(this.#rid);
   }
 
   [SymbolDispose]() {
-    core.tryClose(this.rid);
+    core.tryClose(this.#rid);
   }
 }
 

ext/node/polyfills/_fs/_fs_readFile.ts
@@ -18,6 +18,7 @@ import {
   Encodings,
   TextEncodings,
 } from "ext:deno_node/_utils.ts";
+import { FsFile } from "ext:deno_fs/30_fs.js";
 
 function maybeDecode(data: Uint8Array, encoding: TextEncodings): string;
 function maybeDecode(
@@ -72,7 +73,7 @@ export function readFile(
 
   let p: Promise<Uint8Array>;
   if (path instanceof FileHandle) {
-    const fsFile = new Deno.FsFile(path.fd);
+    const fsFile = new FsFile(path.fd);
     p = readAll(fsFile);
   } else {
     p = Deno.readFile(path);

ext/node/polyfills/_fs/_fs_writeFile.ts
@@ -24,6 +24,7 @@ import {
   validateStringAfterArrayBufferView,
 } from "ext:deno_node/internal/fs/utils.mjs";
 import { promisify } from "ext:deno_node/internal/util.mjs";
+import { FsFile } from "ext:deno_fs/30_fs.js";
 
 interface Writer {
   write(p: Uint8Array): Promise<number>;
@@ -73,7 +74,7 @@ export function writeFile(
   (async () => {
     try {
       file = isRid
-        ? new Deno.FsFile(pathOrRid as number)
+        ? new FsFile(pathOrRid as number)
         : await Deno.open(pathOrRid as string, openOptions);
 
       // ignore mode because it's not supported on windows
@@ -138,7 +139,7 @@ export function writeFileSync(
   let error: Error | null = null;
   try {
     file = isRid
-      ? new Deno.FsFile(pathOrRid as number)
+      ? new FsFile(pathOrRid as number)
       : Deno.openSync(pathOrRid as string, openOptions);
 
     // ignore mode because it's not supported on windows

runtime/js/90_deno_ns.js
@@ -31,6 +31,17 @@ import * as kv from "ext:deno_kv/01_db.ts";
 import * as cron from "ext:deno_cron/01_cron.ts";
 import * as webgpuSurface from "ext:deno_webgpu/02_surface.js";
 
+class FsFile extends fs.FsFile {
+  constructor(rid) {
+    super(rid);
+    internals.warnOnDeprecatedApi(
+      "Deno.Fs",
+      new Error().stack,
+      "Use `Deno.open()` or `Deno.openSync()` instead.",
+    );
+  }
+}
+
 const denoNs = {
   metrics: core.metrics,
   Process: process.Process,
@@ -115,7 +126,7 @@ const denoNs = {
   write: io.write,
   writeSync: io.writeSync,
   File: fs.File,
-  FsFile: fs.FsFile,
+  FsFile,
   open: fs.open,
   openSync: fs.openSync,
   create: fs.create,

cli/tsc/dts/lib.deno.ns.d.ts
@@ -2299,8 +2299,13 @@ declare namespace Deno {
       SeekerSync,
       Closer,
       Disposable {
-    /** The resource ID associated with the file instance. The resource ID
-     * should be considered an opaque reference to resource. */
+    /**
+     * The resource ID associated with the file instance. The resource ID
+     * should be considered an opaque reference to resource.
+     *
+     * @deprecated Use {@linkcode Deno.FsFile} instance methods instead.
+     * {@linkcode Deno.FsFile.rid} will be removed in Deno 2.0.
+     */
     readonly rid: number;
     /** A {@linkcode ReadableStream} instance representing to the byte contents
      * of the file. This makes it easy to interoperate with other web streams
@@ -2330,9 +2335,15 @@ declare namespace Deno {
      * ```
      */
     readonly writable: WritableStream<Uint8Array>;
-    /** The constructor which takes a resource ID. Generally `FsFile` should
+    /**
+     * The constructor which takes a resource ID. Generally `FsFile` should
      * not be constructed directly. Instead use {@linkcode Deno.open} or
-     * {@linkcode Deno.openSync} to create a new instance of `FsFile`. */
+     * {@linkcode Deno.openSync} to create a new instance of `FsFile`.
+     *
+     * @deprecated Use {@linkcode Deno.open} or {@linkcode Deno.openSync}
+     * instead. {@linkcode Deno.FsFile.constructor} will be removed in Deno
+     * 2.0.
+     */
     constructor(rid: number);
     /** Write the contents of the array buffer (`p`) to the file.
      *

commit 0b0fb94ce2489da642cffd82e0498446d4a1fe1f

fix(fs): instanceof check for Deno.FsFile (#22121)
Regression caused by https://github.com/denoland/deno/pull/22072.

I added a relevant test so we don't regress again.

Fixes https://github.com/denoland/deno/issues/22115

ext/fs/30_fs.js
@@ -88,6 +88,7 @@ const {
   StringPrototypeStartsWith,
   SymbolAsyncIterator,
   SymbolIterator,
+  SymbolFor,
   Uint32Array,
 } = primordials;
 
@@ -619,7 +620,7 @@ function openSync(
     options,
   );
 
-  return new FsFile(rid);
+  return new FsFile(rid, SymbolFor("Deno.internal.FsFile"));
 }
 
 async function open(
@@ -632,7 +633,7 @@ async function open(
     options,
   );
 
-  return new FsFile(rid);
+  return new FsFile(rid, SymbolFor("Deno.internal.FsFile"));
 }
 
 function createSync(path) {
@@ -659,8 +660,15 @@ class FsFile {
   #readable;
   #writable;
 
-  constructor(rid) {
+  constructor(rid, symbol) {
     this.#rid = rid;
+    if (!symbol || symbol !== SymbolFor("Deno.internal.FsFile")) {
+      internals.warnOnDeprecatedApi(
+        "new Deno.FsFile()",
+        new Error().stack,
+        "Use `Deno.open` or `Deno.openSync` instead.",
+      );
+    }
   }
 
   get rid() {

ext/node/polyfills/_fs/_fs_fdatasync.ts
@@ -10,9 +10,12 @@ export function fdatasync(
   fd: number,
   callback: CallbackWithError,
 ) {
-  new FsFile(fd).syncData().then(() => callback(null), callback);
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).syncData().then(
+    () => callback(null),
+    callback,
+  );
 }
 
 export function fdatasyncSync(fd: number) {
-  new FsFile(fd).syncDataSync();
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).syncDataSync();
 }

ext/node/polyfills/_fs/_fs_fstat.ts
@@ -41,7 +41,7 @@ export function fstat(
 
   if (!callback) throw new Error("No callback function supplied");
 
-  new FsFile(fd).stat().then(
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).stat().then(
     (stat) => callback(null, CFISBIS(stat, options.bigint)),
     (err) => callback(err),
   );
@@ -60,6 +60,6 @@ export function fstatSync(
   fd: number,
   options?: statOptions,
 ): Stats | BigIntStats {
-  const origin = new FsFile(fd).statSync();
+  const origin = new FsFile(fd, Symbol.for("Deno.internal.FsFile")).statSync();
   return CFISBIS(origin, options?.bigint || false);
 }

ext/node/polyfills/_fs/_fs_fsync.ts
@@ -10,9 +10,12 @@ export function fsync(
   fd: number,
   callback: CallbackWithError,
 ) {
-  new FsFile(fd).sync().then(() => callback(null), callback);
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).sync().then(
+    () => callback(null),
+    callback,
+  );
 }
 
 export function fsyncSync(fd: number) {
-  new FsFile(fd).syncSync();
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).syncSync();
 }

ext/node/polyfills/_fs/_fs_ftruncate.ts
@@ -20,9 +20,12 @@ export function ftruncate(
 
   if (!callback) throw new Error("No callback function supplied");
 
-  new FsFile(fd).truncate(len).then(() => callback(null), callback);
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).truncate(len).then(
+    () => callback(null),
+    callback,
+  );
 }
 
 export function ftruncateSync(fd: number, len?: number) {
-  new FsFile(fd).truncateSync(len);
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).truncateSync(len);
 }

ext/node/polyfills/_fs/_fs_futimes.ts
@@ -40,7 +40,10 @@ export function futimes(
   mtime = getValidTime(mtime, "mtime");
 
   // TODO(@littledivy): Treat `fd` as real file descriptor.
-  new FsFile(fd).utime(atime, mtime).then(() => callback(null), callback);
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).utime(atime, mtime).then(
+    () => callback(null),
+    callback,
+  );
 }
 
 export function futimesSync(
@@ -52,5 +55,5 @@ export function futimesSync(
   mtime = getValidTime(mtime, "mtime");
 
   // TODO(@littledivy): Treat `fd` as real file descriptor.
-  new FsFile(fd).utimeSync(atime, mtime);
+  new FsFile(fd, Symbol.for("Deno.internal.FsFile")).utimeSync(atime, mtime);
 }

ext/node/polyfills/_fs/_fs_readFile.ts
@@ -73,7 +73,7 @@ export function readFile(
 
   let p: Promise<Uint8Array>;
   if (path instanceof FileHandle) {
-    const fsFile = new FsFile(path.fd);
+    const fsFile = new FsFile(path.fd, Symbol.for("Deno.internal.FsFile"));
     p = readAll(fsFile);
   } else {
     p = Deno.readFile(path);

ext/node/polyfills/_fs/_fs_writeFile.ts
@@ -74,7 +74,7 @@ export function writeFile(
   (async () => {
     try {
       file = isRid
-        ? new FsFile(pathOrRid as number)
+        ? new FsFile(pathOrRid as number, Symbol.for("Deno.internal.FsFile"))
         : await Deno.open(pathOrRid as string, openOptions);
 
       // ignore mode because it's not supported on windows
@@ -139,7 +139,7 @@ export function writeFileSync(
   let error: Error | null = null;
   try {
     file = isRid
-      ? new FsFile(pathOrRid as number)
+      ? new FsFile(pathOrRid as number, Symbol.for("Deno.internal.FsFile"))
       : Deno.openSync(pathOrRid as string, openOptions);
 
     // ignore mode because it's not supported on windows

runtime/js/40_process.js
@@ -21,6 +21,7 @@ const {
   PromisePrototypeThen,
   SafePromiseAll,
   Symbol,
+  SymbolFor,
 } = primordials;
 
 import { FsFile } from "ext:deno_fs/30_fs.js";
@@ -76,15 +77,21 @@ class Process {
     this.pid = res.pid;
 
     if (res.stdinRid && res.stdinRid > 0) {
-      this.stdin = new FsFile(res.stdinRid);
+      this.stdin = new FsFile(res.stdinRid, SymbolFor("Deno.internal.FsFile"));
     }
 
     if (res.stdoutRid && res.stdoutRid > 0) {
-      this.stdout = new FsFile(res.stdoutRid);
+      this.stdout = new FsFile(
+        res.stdoutRid,
+        SymbolFor("Deno.internal.FsFile"),
+      );
     }
 
     if (res.stderrRid && res.stderrRid > 0) {
-      this.stderr = new FsFile(res.stderrRid);
+      this.stderr = new FsFile(
+        res.stderrRid,
+        SymbolFor("Deno.internal.FsFile"),
+      );
     }
   }
 

runtime/js/90_deno_ns.js
@@ -31,17 +31,6 @@ import * as kv from "ext:deno_kv/01_db.ts";
 import * as cron from "ext:deno_cron/01_cron.ts";
 import * as webgpuSurface from "ext:deno_webgpu/02_surface.js";
 
-class FsFile extends fs.FsFile {
-  constructor(rid) {
-    super(rid);
-    internals.warnOnDeprecatedApi(
-      "Deno.Fs",
-      new Error().stack,
-      "Use `Deno.open()` or `Deno.openSync()` instead.",
-    );
-  }
-}
-
 const denoNs = {
   metrics: () => {
     internals.warnOnDeprecatedApi("Deno.metrics()", new Error().stack);
@@ -171,7 +160,7 @@ const denoNs = {
     return io.writeSync(rid, data);
   },
   File: fs.File,
-  FsFile,
+  FsFile: fs.FsFile,
   open: fs.open,
   openSync: fs.openSync,
   create: fs.create,

cli/tests/unit/files_test.ts
@@ -19,6 +19,8 @@ Deno.test(function filesStdioFileDescriptors() {
 Deno.test({ permissions: { read: true } }, async function filesCopyToStdout() {
   const filename = "cli/tests/testdata/assets/fixture.json";
   using file = await Deno.open(filename);
+  assert(file instanceof Deno.File);
+  assert(file instanceof Deno.FsFile);
   assert(file.rid > 2);
   const bytesWritten = await copy(file, Deno.stdout);
   const fileSize = Deno.statSync(filename).size;