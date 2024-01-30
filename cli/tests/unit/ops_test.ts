b9b821547b7b8d9a48739ba8f124ef73b8c62ac8
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-01-30 22:49:36 +0100

    update the test

--- cli/tests/unit/ops_test.ts
+++ cli/tests/unit/ops_test.ts
@@ -1,13 +1,17 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
+const EXPECTED_OP_COUNT = 151;
+
 Deno.test(function checkExposedOps() {
   // @ts-ignore TS doesn't allow to index with symbol
   const core = Deno[Deno.internal].core;
   const opNames = Object.keys(core.ops);
 
-  if (opNames.length !== 200) {
+  if (opNames.length !== EXPECTED_OP_COUNT) {
     throw new Error(
-      `Expected 200 ops, but got ${opNames.length}:\n${opNames.join("\n")}`,
+      `Expected ${EXPECTED_OP_COUNT} ops, but got ${opNames.length}:\n${
+        opNames.join("\n")
+      }`,
     );
   }
 });
