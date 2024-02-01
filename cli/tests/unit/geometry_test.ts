36378c733a69bb206da958e0a67e310fefc69582
Kenta Moriuchi <moriken@kimamass.com>
2024-02-02 03:13:24 +0900

    add tests

--- cli/tests/unit/geometry_test.ts
+++ cli/tests/unit/geometry_test.ts
@@ -53,6 +53,32 @@ Deno.test(function matrixMultiplySelf() {
   );
 });
 
+Deno.test(function matrixMultiplySelfWithSelf() {
+  // deno-fmt-ignore
+  const init = {
+    m11:  1, m21:  2, m31:  3, m41:  4,
+    m12:  5, m22:  6, m32:  7, m42:  8,
+    m13:  9, m23: 10, m33: 11, m43: 12,
+    m14: 13, m24: 14, m34: 15, m44: 16,
+  };
+  const matrix = DOMMatrix.fromMatrix(init);
+  const matrix2 = matrix.multiplySelf(matrix);
+  assertStrictEquals(
+    matrix,
+    matrix2,
+  );
+  assertEquals(
+    matrix,
+    // deno-fmt-ignore
+    DOMMatrix.fromMatrix({
+      m11:  90, m21: 100, m31: 110, m41: 120,
+      m12: 202, m22: 228, m32: 254, m42: 280,
+      m13: 314, m23: 356, m33: 398, m43: 440,
+      m14: 426, m24: 484, m34: 542, m44: 600,
+    }),
+  );
+});
+
 Deno.test(function matrixPreMultiplySelf() {
   // deno-fmt-ignore
   const init = {
@@ -78,3 +104,30 @@ Deno.test(function matrixPreMultiplySelf() {
     }),
   );
 });
+
+Deno.test(function matrixPreMultiplySelfWithSelf() {
+  // deno-fmt-ignore
+  const init = {
+    m11:  1, m21:  2, m31:  3, m41:  4,
+    m12:  5, m22:  6, m32:  7, m42:  8,
+    m13:  9, m23: 10, m33: 11, m43: 12,
+    m14: 13, m24: 14, m34: 15, m44: 16,
+  };
+  const matrix = DOMMatrix.fromMatrix(init);
+  const matrix2 = matrix.preMultiplySelf(matrix);
+  assertStrictEquals(
+    matrix,
+    matrix2,
+  );
+  assertEquals(
+    matrix,
+    // deno-fmt-ignore
+    DOMMatrix.fromMatrix({
+      m11:  90, m21: 100, m31: 110, m41: 120,
+      m12: 202, m22: 228, m32: 254, m42: 280,
+      m13: 314, m23: 356, m33: 398, m43: 440,
+      m14: 426, m24: 484, m34: 542, m44: 600,
+    }),
+  );
+});
+
