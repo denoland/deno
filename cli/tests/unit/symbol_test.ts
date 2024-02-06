bc9dbaa14d367f00c6559deb3a6159679fbfcf3e
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-02-06 03:17:14 +0100

    update a comment

--- cli/tests/unit/symbol_test.ts
+++ cli/tests/unit/symbol_test.ts
@@ -1,7 +1,8 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { assert } from "./test_util.ts";
 
-// Note tests for Deno.stdin.setRaw is in integration tests.
+// Test that `Symbol.metadata` is defined. This file can be removed when V8
+// supports `Symbol.metadata` natively.
 
 Deno.test(
   function symbolMetadataIsDefined() {
