5d6bcc434d0a708a9f2ad4c301c898ff714e41c4
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-02-07 02:37:45 +0100

    chore: don't expose FFI ops to user code

--- cli/tests/testdata/run/ffi/unstable_ffi_3.js
+++
@@ -1,7 +0,0 @@
-const { op_ffi_call_ptr_nonblocking } = Deno[Deno.internal].core
-  .ensureFastOps();
-op_ffi_call_ptr_nonblocking(null, {
-  name: null,
-  parameters: [],
-  result: "void",
-}, []);
