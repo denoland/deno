58e50acd7a7fa205c3eddf7bac8cd1a8332cdc3d
Divy Srivastava <dj.srivastava23@gmail.com>
2024-02-08 14:15:30 +0530

    lint

--- cli/tools/registry/diagnostics.rs
+++ cli/tools/registry/diagnostics.rs
@@ -260,29 +260,29 @@ impl Diagnostic for PublishDiagnostic {
 
   fn snippet_fixed(&self) -> Option<DiagnosticSnippet<'_>> {
     match &self {
-      PublishDiagnostic::InvalidExternalImport {
-        imported, referrer, ..
-      } => match super::api::get_jsr_alternative(imported) {
-        Some(replacement) => {
-          let replacement = SourceTextInfo::new(replacement.into());
-          let start = replacement.line_start(0);
-          let end = replacement.line_end(0);
-          Some(DiagnosticSnippet {
-            source: DiagnosticSnippetSource::SourceTextInfo(Cow::Owned(
-              replacement,
-            )),
-            highlight: DiagnosticSnippetHighlight {
-              style: DiagnosticSnippetHighlightStyle::Hint,
-              range: DiagnosticSourceRange {
-                start: DiagnosticSourcePos::SourcePos(start),
-                end: DiagnosticSourcePos::SourcePos(end),
+      PublishDiagnostic::InvalidExternalImport { imported, .. } => {
+        match super::api::get_jsr_alternative(imported) {
+          Some(replacement) => {
+            let replacement = SourceTextInfo::new(replacement.into());
+            let start = replacement.line_start(0);
+            let end = replacement.line_end(0);
+            Some(DiagnosticSnippet {
+              source: DiagnosticSnippetSource::SourceTextInfo(Cow::Owned(
+                replacement,
+              )),
+              highlight: DiagnosticSnippetHighlight {
+                style: DiagnosticSnippetHighlightStyle::Hint,
+                range: DiagnosticSourceRange {
+                  start: DiagnosticSourcePos::SourcePos(start),
+                  end: DiagnosticSourcePos::SourcePos(end),
+                },
+                description: Some("try this specifier".into()),
               },
-              description: Some("try this specifier".into()),
-            },
-          })
+            })
+          }
+          None => None,
         }
-        None => None,
-      },
+      }
       _ => None,
     }
   }
