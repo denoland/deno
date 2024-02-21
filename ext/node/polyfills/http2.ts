f643cb3d3092841529da6eb17a598138c4bf792b
Satya Rohith <me@satyarohith.com>
2024-02-21 12:31:39 +0530

    fix(ext/node): pass alpnProtocols to Deno.startTls

--- ext/node/polyfills/http2.ts
+++ ext/node/polyfills/http2.ts
@@ -210,11 +210,12 @@ export class Http2Session extends EventEmitter {
   }
 
   goaway(
-    _code: number,
-    _lastStreamID: number,
-    _opaqueData: Buffer | TypedArray | DataView,
+    code?: number,
+    lastStreamID?: number,
+    opaqueData?: Buffer | TypedArray | DataView,
   ) {
-    warnNotImplemented("Http2Session.goaway");
+    // TODO(satyarohith): create goaway op and pass the args
+    debugHttp2(">>> goaway - ignored args", code, lastStreamID, opaqueData);
     if (this[kDenoConnRid]) {
       core.tryClose(this[kDenoConnRid]);
     }
