619acce305ac77f98327718bc1e6a1ae13d8bcf6
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-02-23 18:08:17 +0000

    fix(compile): allow to compile for ARM linux (#22542)
    Closes https://github.com/denoland/deno/issues/22527

--- cli/args/flags.rs
+++ cli/args/flags.rs
@@ -1312,6 +1312,7 @@ supported in canary.
           .help("Target OS architecture")
           .value_parser([
             "x86_64-unknown-linux-gnu",
+            "aarch64-unknown-linux-gnu",
             "x86_64-pc-windows-msvc",
             "x86_64-apple-darwin",
             "aarch64-apple-darwin",
