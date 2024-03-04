d4b3b39cc021bfac76e023eedd217fe0c2df9978
cui fliter <imcusg@gmail.com>
2024-03-04 22:28:57 +0800

    chore: fix typos (#22677)

--- cli/main.rs
+++ cli/main.rs
@@ -319,7 +319,7 @@ pub fn main() {
   let args: Vec<String> = env::args().collect();
 
   // NOTE(lucacasonato): due to new PKU feature introduced in V8 11.6 we need to
-  // initalize the V8 platform on a parent thread of all threads that will spawn
+  // initialize the V8 platform on a parent thread of all threads that will spawn
   // V8 isolates.
 
   let future = async move {
