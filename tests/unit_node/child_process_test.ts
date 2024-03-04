d296b81c63c1464fb1865e59d7d98d68a6937202
Divy Srivastava <dj.srivastava23@gmail.com>
2024-03-04 20:35:21 +0530

    fmt

--- tests/unit_node/child_process_test.ts
+++ tests/unit_node/child_process_test.ts
@@ -776,8 +776,9 @@ Deno.test(async function spawnCommandNotFoundErrno() {
   const { promise, resolve } = Promise.withResolvers<void>();
   const cp = CP.spawn("no-such-command");
   cp.on("error", (err) => {
+    const errno = Deno.build.os === "windows" ? -4058 : -2;
     // @ts-ignore: errno missing from typings
-    assertEquals(err.errno, -2);
+    assertEquals(err.errno, errno);
     resolve();
   });
   await promise;
