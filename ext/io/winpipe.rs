a90640b57b097089135046e982502ffea6e27e67
Matt Mastracci <matthew@mastracci.com>
2024-03-06 11:55:14 -0700

    .

--- ext/io/winpipe.rs
+++ ext/io/winpipe.rs
@@ -216,10 +216,10 @@ mod tests {
       let (server, client) = create_named_pipe().unwrap();
       // SAFETY: For testing
       let server = unsafe { File::from_raw_handle(server) };
-      server.write_all([0; 1024]).unwrap();
+      server.write_all(&[0; 1024]).unwrap();
       // SAFETY: For testing
       let client = unsafe { File::from_raw_handle(client) };
-      client.write_all([0; 1024]).unwrap();
+      client.write_all(&[0; 1024]).unwrap();
       handles.push((server, client))
     }
   }
@@ -238,10 +238,10 @@ mod tests {
           let (server, client) = create_named_pipe().unwrap();
           // SAFETY: For testing
           let server = unsafe { File::from_raw_handle(server) };
-          server.write_all([0; 1024]).unwrap();
+          server.write_all(&[0; 1024]).unwrap();
           // SAFETY: For testing
           let client = unsafe { File::from_raw_handle(client) };
-          client.write_all([0; 1024]).unwrap();
+          client.write_all(&[0; 1024]).unwrap();
           handles.push((server, client))
         }
         std::thread::sleep(std::time::Duration::from_millis(100));
