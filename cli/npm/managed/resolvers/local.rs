a542d9ca6056dbc77d75ca39ab0126b550b50d84
Divy Srivastava <dj.srivastava23@gmail.com>
2024-03-07 21:44:07 +0530

    work with cross-volumes and fallback

--- cli/npm/managed/resolvers/local.rs
+++ cli/npm/managed/resolvers/local.rs
@@ -332,7 +332,12 @@ async fn sync_resolution_with_fs(
           .with_context(|| format!("Creating '{}'", folder_path.display()))?;
         let cache_folder = cache
           .package_folder_for_name_and_version(&package.id.nv, &registry_url);
-        fs::hard_link(cache_folder, &package_path)?;
+        if let Err(_) = fs::hard_link(&cache_folder, &package_path) {
+          // Fallback to copying the directory.
+          //
+          // Also handles EXDEV when when trying to hard link across volumes.
+          copy_dir_recursive(&cache_folder, &package_path)?;
+        }
         // write out a file that indicates this folder has been initialized
         fs::write(initialized_file, "")?;
         // finally stop showing the progress bar
