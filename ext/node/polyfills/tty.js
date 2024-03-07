180af445fec201ce6948018f66200253ce5f357c
Divy Srivastava <dj.srivastava23@gmail.com>
2024-03-07 11:09:48 +0530

    add copyright from source

--- ext/node/polyfills/tty.js
+++ ext/node/polyfills/tty.js
@@ -109,6 +109,17 @@ WriteStream.prototype.getColorDepth = getColorDepth;
 
 WriteStream.prototype.hasColors = hasColors;
 
+// MIT License
+
+// Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com)
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
 
