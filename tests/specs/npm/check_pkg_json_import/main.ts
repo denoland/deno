7f79018e56a5a4d5326ea51acc83c01a97461b09
David Sherret <dsherret@gmail.com>
2024-03-12 16:49:56 -0400

    Start working on directory resolution issue

---
+++ tests/specs/npm/check_pkg_json_import/main.ts
@@ -0,0 +1,17 @@
+import { createContext } from "npm:@denotest/types-pkg-json-import";
+import { useContext } from "npm:@denotest/types-pkg-json-import/hooks";
+
+export interface Foo {
+  foo: string;
+}
+
+export const CTX = createContext<Foo | undefined>(undefined);
+
+function unwrap(foo: Foo) {}
+
+export function useCSP() {
+  const foo = useContext(CTX);
+  if (foo) {
+    unwrap(foo);
+  }
+}
