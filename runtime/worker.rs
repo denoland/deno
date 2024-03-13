7e9da01b166040aa3f42c47e523e0f473dad58d1
Divy Srivastava <dj.srivastava23@gmail.com>
2024-03-13 08:40:38 -0700

    chore: upgrade deno_core to 0.270.0 (#22826)
    Upgrade to V8 12.3

--- runtime/worker.rs
+++ runtime/worker.rs
@@ -497,6 +497,17 @@ impl MainWorker {
     if let Some(op_summary_metrics) = op_summary_metrics {
       js_runtime.op_state().borrow_mut().put(op_summary_metrics);
     }
+    extern "C" fn message_handler(
+      _msg: v8::Local<v8::Message>,
+      _exception: v8::Local<v8::Value>,
+    ) {
+      // TODO(@littledivy): Propogate message to users.
+    }
+
+    // Register message listener
+    js_runtime
+      .v8_isolate()
+      .add_message_listener(message_handler);
 
     if let Some(server) = options.maybe_inspector_server.clone() {
       server.register_inspector(
