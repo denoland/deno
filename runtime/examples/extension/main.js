12ac8f0a16e8b7ce605d7f1e2d7850eb4713fe03
Matt Mastracci <matthew@mastracci.com>
2024-03-13 21:23:37 -0600

    fix(cli): unbreak extension example and fix __runtime_js_sources (#22906)
    Better example to close https://github.com/denoland/deno/issues/22600
    
    ---------
    
    Signed-off-by: Matt Mastracci <matthew@mastracci.com>

--- runtime/examples/extension/main.js
+++ runtime/examples/extension/main.js
@@ -1,4 +1,4 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
-console.log("Hello world!");
-console.log(Deno);
+console.log("Hello world from JS!");
+console.log(Deno.build);
 Extension.hello();
--- runtime/examples/extension/main.rs
+++ runtime/examples/extension/main.rs
@@ -4,23 +4,31 @@ use std::path::Path;
 use std::rc::Rc;
 
 use deno_core::error::AnyError;
+use deno_core::op2;
 use deno_core::FsModuleLoader;
 use deno_core::ModuleSpecifier;
 use deno_runtime::permissions::PermissionsContainer;
 use deno_runtime::worker::MainWorker;
 use deno_runtime::worker::WorkerOptions;
 
+#[op2(fast)]
+fn op_hello(#[string] text: &str) {
+  println!("Hello {} from an op!", text);
+}
+
 deno_core::extension!(
   hello_runtime,
+  ops = [op_hello],
   esm_entry_point = "ext:hello_runtime/bootstrap.js",
-  esm = [dir "examples/extension_with_esm", "bootstrap.js"]
+  esm = [dir "examples/extension", "bootstrap.js"]
 );
 
 #[tokio::main]
 async fn main() -> Result<(), AnyError> {
-  let js_path = Path::new(env!("CARGO_MANIFEST_DIR"))
-    .join("examples/extension_with_esm/main.js");
+  let js_path =
+    Path::new(env!("CARGO_MANIFEST_DIR")).join("examples/extension/main.js");
   let main_module = ModuleSpecifier::from_file_path(js_path).unwrap();
+  eprintln!("Running {main_module}...");
   let mut worker = MainWorker::bootstrap_from_options(
     main_module.clone(),
     PermissionsContainer::allow_all(),
