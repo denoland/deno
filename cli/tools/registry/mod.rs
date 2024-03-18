6632ad29639944b70ac36e76e74d124953170039
Asher Gomez <ashersaupingomez@gmail.com>
2024-03-19 10:46:03 +1100

    work

--- cli/tools/registry/mod.rs
+++ cli/tools/registry/mod.rs
@@ -962,7 +962,15 @@ pub async fn publish(
     && !publish_flags.allow_dirty
     && check_if_git_repo_dirty(cli_options.initial_cwd()).await
   {
-    bail!("Aborting due to uncommitted changes. Check in source code or run with --allow-dirty");
+    if publish_flags.dry_run {
+      log::warn!(
+        "{} Aborting due to --dry-run uncommitted changes",
+        colors::yellow("Warning")
+      );
+      return Ok(());
+    } else {
+      bail!("Aborting due to uncommitted changes. Check in source code or run with --allow-dirty");
+    }
   }
 
   if publish_flags.dry_run {
