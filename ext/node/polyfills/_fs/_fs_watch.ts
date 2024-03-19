73f098702e7159bad8800f495751c5cbd60bdb58
Divy Srivastava <dj.srivastava23@gmail.com>
2024-03-19 12:39:11 +0530

    lint

--- ext/node/polyfills/_fs/_fs_watch.ts
+++ ext/node/polyfills/_fs/_fs_watch.ts
@@ -13,8 +13,6 @@ import { stat, Stats } from "ext:deno_node/_fs/_fs_stat.ts";
 import { Buffer } from "node:buffer";
 import { delay } from "ext:deno_node/_util/async.ts";
 
-import { core } from "ext:core/mod.js";
-
 const statPromisified = promisify(stat);
 const statAsync = (filename: string): Promise<Stats | null> => {
   try {
