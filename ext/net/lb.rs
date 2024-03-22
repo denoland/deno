c05911973576ff0930bbf6fcb241b6e66b1d3a9b
Matt Mastracci <matthew@mastracci.com>
2024-03-22 11:22:26 -0700

    .

--- ext/net/lb.rs
+++ ext/net/lb.rs
@@ -1,8 +1,12 @@
 use pin_project::pin_project;
 use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::net::TcpListener;
 use std::ops::Deref;
 use std::ops::DerefMut;
+use std::os::fd::AsRawFd;
+use std::os::fd::FromRawFd;
+use std::os::fd::IntoRawFd;
 use std::pin::Pin;
 use std::sync::Arc;
 use tokio::io::AsyncRead;
@@ -14,33 +18,27 @@ static CONNS: std::sync::OnceLock<std::sync::Mutex<Connections>> =
 
 #[derive(Default)]
 struct Connections {
-  tcp: HashMap<SocketAddr, (Arc<Mutex<TcpConnection>>, SocketAddr)>,
+  tcp: HashMap<SocketAddr, (TcpConnection, SocketAddr)>,
 }
 
 pub struct TcpConnection {
-  streams: tokio::sync::mpsc::Receiver<
-    std::io::Result<(std::net::TcpStream, std::net::SocketAddr)>,
-  >,
+  streams: i32,
 }
 
 impl TcpConnection {
   pub fn start(addr: SocketAddr) -> std::io::Result<(Self, SocketAddr)> {
     let listener = std::net::TcpListener::bind(addr)?;
     let addr = listener.local_addr()?;
-    let (tx, rx) = tokio::sync::mpsc::channel(1);
-    std::thread::spawn(move || loop {
-      let res = listener.accept();
-      let err = res.is_err();
-      if tx.blocking_send(res).is_err() || err {
-        break;
-      }
-    });
-    Ok((Self { streams: rx }, addr))
+    let socket = socket2::Socket::from(listener);
+    socket.set_nonblocking(true)?;
+    let fd = socket.into_raw_fd();
+
+    Ok((Self { streams: fd }, addr))
   }
 }
 
 pub struct TcpLbListener {
-  listener: Arc<Mutex<TcpConnection>>,
+  listener: tokio::net::TcpListener,
   socket_addr: SocketAddr,
 }
 
@@ -50,20 +48,27 @@ impl TcpLbListener {
       Default::default()
     }).lock().unwrap().tcp;
     if let Some(tcp) = tcp.get(&socket_addr) {
-      return Ok(Self { listener: tcp.0.clone(), socket_addr: tcp.1 });
+      let listener = unsafe { std::net::TcpListener::from_raw_fd(tcp.0.streams) };
+      let listener = tokio::net::TcpListener::from_std(listener)?;
+  
+      return Ok(Self { listener, socket_addr: tcp.1 });
     }
     let (conn, addr) = TcpConnection::start(socket_addr)?;
-    let conn = Arc::new(Mutex::new(conn));
-    tcp.insert(socket_addr, (conn.clone(), socket_addr));
-    return Ok(Self { listener: conn.clone(), socket_addr: addr });
+    // let conn = Arc::new(Mutex::new(conn));
+    let listener = unsafe { std::net::TcpListener::from_raw_fd(conn.streams) };
+    let listener = tokio::net::TcpListener::from_std(listener)?;
+    tcp.insert(socket_addr, (conn, socket_addr));
+    return Ok(Self { listener, socket_addr: addr });
   } 
-  pub async fn accept(&self) -> std::io::Result<(TcpLbStream, SocketAddr)> {
-    let Some(res) = self.listener.lock().await.streams.recv().await else {
-      return Err(std::io::ErrorKind::NotConnected.into());
-    };
 
-    let (tcp, addr) = res?;
-    let tcp = tokio::net::TcpStream::from_std(tcp)?;
+  pub async fn accept(&self) -> std::io::Result<(TcpLbStream, SocketAddr)> {
+    let (tcp, addr) = self.listener.accept().await?;
+    // let Some(res) = self.listener.lock().await.streams.recv().await else {
+    //   return Err(std::io::ErrorKind::NotConnected.into());
+    // };
+
+    // let (tcp, addr) = res?;
+    // let tcp = tokio::net::TcpStream::from_std(tcp)?;
     Ok((TcpLbStream(tcp), addr))
   }
   pub fn local_addr(&self) -> std::io::Result<SocketAddr> {
