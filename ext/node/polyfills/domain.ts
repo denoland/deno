5400813abec562d6b40b5d17816bb2cc55d5132a
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-03-26 23:59:08 +0100

    fix(ext/node): polyfill node:domain module

--- ext/node/polyfills/domain.ts
+++ ext/node/polyfills/domain.ts
@@ -4,12 +4,70 @@
 
 import { EventEmitter } from "node:events";
 
+function emitError(e) {
+  this.emit("error", e);
+}
+
 export function create() {
   return new Domain();
 }
 export class Domain extends EventEmitter {
   constructor() {
     super();
+  }
+
+  add(emitter) {
+    emitter.on("error", emitError.bind(this));
+  }
+
+  remove(emitter) {
+    emitter.removeListener("error", emitError.bind(this));
+  }
+
+  bind(fn) {
+    return function () {
+      try {
+        fn.apply(null, arguments);
+      } catch (e) {
+        emitError.call(this, e);
+      }
+    };
+  }
+
+  intercept(fn) {
+    return function (e) {
+      if (e) {
+        emitError.call(this, e);
+      } else {
+        try {
+          fn.apply(null, arguments);
+        } catch (e) {
+          emitError.call(this, e);
+        }
+      }
+    };
+  }
+
+  run(fn) {
+    try {
+      fn();
+    } catch (e) {
+      emitError.call(this, e);
+    }
+    return this;
+  }
+
+  dispose() {
+    this.removeAllListeners();
+    return this;
+  }
+
+  enter() {
+    return this;
+  }
+
+  exit() {
+    return this;
   }
 }
 export default {
