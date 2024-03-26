2959f996bb5e752dbc449f964d19ef771996cac1
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-03-26 23:04:37 +0100

    start working on node:domain polyfill

--- ext/node/polyfills/domain.ts
+++ ext/node/polyfills/domain.ts
@@ -1,14 +1,15 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 // Copyright Joyent and Node contributors. All rights reserved. MIT license.
+// This code has been inspired by https://github.com/bevry/domain-browser/commit/8bce7f4a093966ca850da75b024239ad5d0b33c6
 
-import { notImplemented } from "ext:deno_node/_utils.ts";
+import { EventEmitter } from "node:events";
 
 export function create() {
-  notImplemented("domain.create");
+  return new Domain();
 }
-export class Domain {
+export class Domain extends EventEmitter {
   constructor() {
-    notImplemented("domain.Domain.prototype.constructor");
+    super();
   }
 }
 export default {
