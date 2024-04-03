b5a489b057d1db06d65208244e583ffbe98463d2
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-04-03 15:00:46 +0200

    add copyright

--- tests/unit_node/domain_test.ts
+++ tests/unit_node/domain_test.ts
@@ -1,4 +1,5 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+// Copyright © Benjamin Lupton
 // This code has been forked by https://github.com/bevry/domain-browser/commit/8bce7f4a093966ca850da75b024239ad5d0b33c6
 
 import domain from "node:domain";
