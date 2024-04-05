c18354d81cf8b2fed205ba2f1ceed3570e936400
Divy Srivastava <dj.srivastava23@gmail.com>
2024-04-05 16:38:14 +0530

    fix

--- ext/node/polyfills/01_require.js
+++ ext/node/polyfills/01_require.js
@@ -218,7 +218,7 @@ function setupBuiltinModules() {
     "internal/util/inspect": internalUtilInspect,
     "internal/util": internalUtil,
     net,
-    module: {},
+    module: Module,
     os,
     "path/posix": pathPosix,
     "path/win32": pathWin32,
