ee3a3041716f46c2c5ab7c7d6c8e4a216929a5ae
Nayeem Rahman <nayeemrmn99@gmail.com>
2024-04-15 23:25:18 +0100

    fix(lsp): slice strings by byte index in code actions

--- cli/lsp/analysis.rs
+++ cli/lsp/analysis.rs
@@ -1126,8 +1126,10 @@ impl CodeActionCollection {
 /// Prepend the whitespace characters found at the start of line_content to content.
 fn prepend_whitespace(content: String, line_content: Option<String>) -> String {
   if let Some(line) = line_content {
-    let whitespaces =
-      line.chars().position(|c| !c.is_whitespace()).unwrap_or(0);
+    let whitespaces = line
+      .char_indices()
+      .find_map(|(i, c)| (!c.is_whitespace()).then_some(i))
+      .unwrap_or(0);
     let whitespace = &line[0..whitespaces];
     format!("{}{}", &whitespace, content)
   } else {
@@ -1269,6 +1271,15 @@ mod tests {
       )
       .unwrap(),
       "utils/sub_utils"
+    );
+  }
+
+  #[test]
+  fn test_prepend_whitespace() {
+    // Regression test for https://github.com/denoland/deno/issues/23361.
+    assert_eq!(
+      &prepend_whitespace("foo".to_string(), Some("\u{a0}bar".to_string())),
+      "\u{a0}foo"
     );
   }
 }
