f36a8951a420e34d8189cda5792f5eeaa5ce85b7
Divy Srivastava <dj.srivastava23@gmail.com>
2024-04-15 18:24:42 +0530

    fix(ext/node): add stub for AsyncResource#asyncId() (#23372)
    Ref https://github.com/denoland/deno/issues/23263

--- ext/node/polyfills/async_hooks.ts
+++ ext/node/polyfills/async_hooks.ts
@@ -10,6 +10,7 @@
 import { core } from "ext:core/mod.js";
 import { op_node_is_promise_rejected } from "ext:core/ops";
 import { validateFunction } from "ext:deno_node/internal/validators.mjs";
+import { newAsyncId } from "ext:deno_node/internal/async_hooks.ts";
 
 function assert(cond: boolean) {
   if (!cond) throw new Error("Assertion failed");
@@ -180,9 +181,16 @@ class AsyncContextFrame {
 export class AsyncResource {
   frame: AsyncContextFrame;
   type: string;
+  #asyncId: number;
+
   constructor(type: string) {
     this.type = type;
     this.frame = AsyncContextFrame.current();
+    this.#asyncId = newAsyncId();
+  }
+
+  asyncId() {
+    return this.#asyncId;
   }
 
   runInAsyncScope(
