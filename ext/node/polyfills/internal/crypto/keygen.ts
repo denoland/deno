cacd701814790dfabafa204a618cbbdf3e7dfb61
Divy Srivastava <dj.srivastava23@gmail.com>
2024-04-29 17:33:00 +0530

    Update async version

--- ext/node/polyfills/internal/crypto/keygen.ts
+++ ext/node/polyfills/internal/crypto/keygen.ts
@@ -768,8 +768,8 @@ export function generateKeyPairSync(
   | KeyPairSyncResult<string | Buffer, string | Buffer> {
   let [privateKey, publicKey] = createJob(kSync, type, options);
 
-  privateKey = new KeyObject("private", setOwnedKey(privateKey));
-  publicKey = new KeyObject("public", setOwnedKey(publicKey));
+  privateKey = new PrivateKeyObject(setOwnedKey(privateKey), { type });
+  publicKey = new PublicKeyObject(setOwnedKey(publicKey), { type });
 
   if (typeof options === "object" && options !== null) {
     const { publicKeyEncoding, privateKeyEncoding } = options as any;
