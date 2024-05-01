b1b35bd43b7ca33ad7a687ad04f8f289940c2441
Divy Srivastava <dj.srivastava23@gmail.com>
2024-05-01 20:26:05 +0530

    fmt

--- tests/specs/node/worker_threads_cache/main.ts
+++ tests/specs/node/worker_threads_cache/main.ts
@@ -1,13 +1,13 @@
-import fs from 'node:fs/promises';
-import { Worker, isMainThread } from 'node:worker_threads';
+import fs from "node:fs/promises";
+import { isMainThread, Worker } from "node:worker_threads";
 
-await fs.writeFile('mod.mjs', 'export default ' + isMainThread);
+await fs.writeFile("mod.mjs", "export default " + isMainThread);
 
-const path = new URL('mod.mjs', import.meta.url);
+const path = new URL("mod.mjs", import.meta.url);
 const i = await import(path.href);
 console.log(i);
 
 if (isMainThread) {
-  const worker = new Worker(new URL('test.mjs', import.meta.url));
-  worker.on('message', (msg) => console.log(msg));
+  const worker = new Worker(new URL("test.mjs", import.meta.url));
+  worker.on("message", (msg) => console.log(msg));
 }
