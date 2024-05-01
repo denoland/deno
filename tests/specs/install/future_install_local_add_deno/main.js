f6ad0c0e9345c0c71d384eac6e270cebc16e9866
Nathan Whitaker <nathan@deno.com>
2024-05-01 11:21:37 -0700

    Test cleanup

--- tests/specs/install/future_install_local_add_deno/main.js
+++ tests/specs/install/future_install_local_add_deno/main.js
@@ -1,2 +1,7 @@
 import { setValue } from "@denotest/esm-basic";
 setValue(5);
+
+const denoJson = JSON.parse(await Deno.readTextFile("./deno.json"));
+if (!denoJson.imports || !denoJson.imports["@denotest/esm-basic"]) {
+  throw new Error("deno.json missing dep!");
+}
