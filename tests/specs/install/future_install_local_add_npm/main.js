fc66f9c3f19d45615c8a25515560e62d4c6e05ff
Nathan Whitaker <nathan@deno.com>
2024-05-07 13:04:56 -0700

    Cleanup tests

--- tests/specs/install/future_install_local_add_npm/main.js
+++ tests/specs/install/future_install_local_add_npm/main.js
@@ -1,10 +1,2 @@
 import { setValue } from "@denotest/esm-basic";
 setValue(5);
-
-const packageJson = JSON.parse(await Deno.readTextFile("./package.json"));
-
-if (
-  !packageJson.dependencies || !packageJson.dependencies["@denotest/esm-basic"]
-) {
-  throw new Error("Package json missing dep!");
-}
