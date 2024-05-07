ae0dfba1d30c8d0efcbc88cf693e47b3ee26bb32
Nathan Whitaker <nathan@deno.com>
2024-05-06 18:57:06 -0700

    Add extension trait for infallible unwrap

---
+++ cli/util/result.rs
@@ -0,0 +1,14 @@
+use std::convert::Infallible;
+
+pub trait InfallibleResultExt<T> {
+  fn unwrap_infallible(self) -> T;
+}
+
+impl<T> InfallibleResultExt<T> for Result<T, Infallible> {
+  fn unwrap_infallible(self) -> T {
+    match self {
+      Ok(value) => value,
+      Err(never) => match never {},
+    }
+  }
+}
