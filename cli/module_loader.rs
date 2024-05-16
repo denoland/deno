ed80cb7a1deb1db3a4411ca2b76fed524b233ba4
David Sherret <dsherret@gmail.com>
2024-05-16 17:34:54 -0400

    fix to use resolve_path

--- cli/module_loader.rs
+++ cli/module_loader.rs
@@ -457,7 +457,7 @@ impl<TGraphContainer: ModuleGraphContainer> CliModuleLoader<TGraphContainer> {
       deno_core::resolve_url(referrer).map_err(|e| e.into())
     } else if referrer == "." {
       // main module, use the initial cwd
-      deno_core::resolve_url_or_path(referrer, &self.shared.initial_cwd)
+      deno_core::resolve_path(referrer, &self.shared.initial_cwd)
         .map_err(|e| e.into())
     } else {
       // this cwd check is slow, so try to avoid it
