41256bc0894c668fc0f94f2853ac8d05e8971127
Alex Gleason <alex@alexgleason.me>
2024-05-25 23:35:24 -0500

    Add a comment to idleTimeout

--- ext/http/02_websocket.ts
+++ ext/http/02_websocket.ts
@@ -91,6 +91,7 @@ function upgradeWebSocket(request, options = { __proto__: null }) {
   const socket = createWebSocketBranded(WebSocket);
   setEventTargetData(socket);
   socket[_server] = true;
+  // Nginx timeout is 60s, so default to a lower number: https://github.com/denoland/deno/pull/23985
   socket[_idleTimeoutDuration] = options.idleTimeout ?? 30;
   socket[_idleTimeoutTimeout] = null;
 
