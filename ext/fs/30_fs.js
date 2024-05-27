506c275053c880a4c6c3f49921f99fd41759064f
charlotte ✨ <charlotte@som.codes>
2024-05-27 23:14:35 +0100

    fix(ext/fs): truncate files when a ReadableStream is passed to writeFile (#23330)
    Closes #19697. This fixes a bug where the writeFile API can create
    partially-overwritten files which may lead to invalid / corrupt files or
    data leakage. It also aligns the behavior of writing a ReadableStream
    and writing a Uint8Array to the disk.

--- ext/fs/30_fs.js
+++ ext/fs/30_fs.js
@@ -926,6 +926,7 @@ async function writeFile(
         append: options.append ?? false,
         create: options.create ?? true,
         createNew: options.createNew ?? false,
+        truncate: !(options.append ?? false),
         write: true,
       });
       await data.pipeTo(file.writable, {
