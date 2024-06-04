75bffe5e255beb77dcf75a4e1cc572c2ad8efeea
David Sherret <dsherret@gmail.com>
2024-06-04 17:59:15 -0400

    ci: label for running CI on draft PRs

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -339,6 +339,8 @@ const ci = {
         ...cloneRepoStep,
         {
           id: "check",
+          if:
+            "${{ !contains(github.event.pull_request.labels.*.name, 'ci-draft') }}",
           run: [
             "GIT_MESSAGE=$(git log --format=%s -n 1 ${{github.event.after}})",
             "echo Commit message: $GIT_MESSAGE",
