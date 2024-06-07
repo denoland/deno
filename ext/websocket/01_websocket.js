921bcd3dc5b49757476de5d5dc7284bb5dbc9a2d
Luca Casonato <hello@lcas.dev>
2024-06-07 23:11:31 +0200

    fix

--- ext/websocket/01_websocket.js
+++ ext/websocket/01_websocket.js
@@ -112,44 +112,15 @@ const _extensions = Symbol("[[extensions]]");
 const _protocol = Symbol("[[protocol]]");
 const _binaryType = Symbol("[[binaryType]]");
 const _eventLoop = Symbol("[[eventLoop]]");
+const _sendQueue = Symbol("[[sendQueue]]");
+const _queueSend = Symbol("[[queueSend]]");
 
 const _server = Symbol("[[server]]");
 const _idleTimeoutDuration = Symbol("[[idleTimeout]]");
 const _idleTimeoutTimeout = Symbol("[[idleTimeoutTimeout]]");
 const _serverHandleIdleTimeout = Symbol("[[serverHandleIdleTimeout]]");
+
 class WebSocket extends EventTarget {
-  #sendQueue = [];
-
-  async #addSendQueue(data) {
-    ArrayPrototypePush(this.#sendQueue, data);
-
-    if (this.#sendQueue.length > 1) {
-      // There is already a send in progress, so we just push to the queue
-      // and let that task handle sending of this data.
-      return;
-    }
-
-    while (this.#sendQueue.length > 0) {
-      const data = this.#sendQueue[0];
-      if (ArrayBufferIsView(data)) {
-        op_ws_send_binary(this[_rid], data);
-      } else if (isArrayBuffer(data)) {
-        op_ws_send_binary_ab(this[_rid], data);
-      } else if (ObjectPrototypeIsPrototypeOf(BlobPrototype, data)) {
-        // deno-lint-ignore prefer-primordials
-        const ab = await data.slice().arrayBuffer();
-        op_ws_send_binary_ab(this[_rid], ab);
-      } else {
-        const string = String(data);
-        op_ws_send_text(
-          this[_rid],
-          string,
-        );
-      }
-      ArrayPrototypeShift(this.#sendQueue);
-    }
-  }
-
   constructor(url, protocols = []) {
     super();
     this[webidl.brand] = webidl.brand;
@@ -162,6 +133,7 @@ class WebSocket extends EventTarget {
     this[_binaryType] = "blob";
     this[_idleTimeoutDuration] = 0;
     this[_idleTimeoutTimeout] = undefined;
+    this[_sendQueue] = [];
 
     const prefix = "Failed to construct 'WebSocket'";
     webidl.requiredArguments(arguments.length, 1, prefix);
@@ -360,7 +332,7 @@ class WebSocket extends EventTarget {
       throw new DOMException("readyState not OPEN", "InvalidStateError");
     }
 
-    if (this.#sendQueue.length === 0) {
+    if (this[_sendQueue].length === 0) {
       // Fast path if the send queue is empty, for example when only synchronous
       // data is being sent.
       if (ArrayBufferIsView(data)) {
@@ -368,7 +340,7 @@ class WebSocket extends EventTarget {
       } else if (isArrayBuffer(data)) {
         op_ws_send_binary_ab(this[_rid], data);
       } else if (ObjectPrototypeIsPrototypeOf(BlobPrototype, data)) {
-        this.#addSendQueue(data);
+        this[_queueSend](data);
       } else {
         const string = String(data);
         op_ws_send_text(
@@ -379,7 +351,7 @@ class WebSocket extends EventTarget {
     } else {
       // Slower path if the send queue is not empty, for example when sending
       // asynchronous data like a Blob.
-      this.#addSendQueue(data);
+      this[_queueSend](data);
     }
   }
 
@@ -546,6 +518,38 @@ class WebSocket extends EventTarget {
     }
   }
 
+  async [_queueSend](data) {
+    const queue = this[_sendQueue];
+
+    ArrayPrototypePush(queue, data);
+
+    if (queue.length > 1) {
+      // There is already a send in progress, so we just push to the queue
+      // and let that task handle sending of this data.
+      return;
+    }
+
+    while (queue.length > 0) {
+      const data = queue[0];
+      if (ArrayBufferIsView(data)) {
+        op_ws_send_binary(this[_rid], data);
+      } else if (isArrayBuffer(data)) {
+        op_ws_send_binary_ab(this[_rid], data);
+      } else if (ObjectPrototypeIsPrototypeOf(BlobPrototype, data)) {
+        // deno-lint-ignore prefer-primordials
+        const ab = await data.slice().arrayBuffer();
+        op_ws_send_binary_ab(this[_rid], ab);
+      } else {
+        const string = String(data);
+        op_ws_send_text(
+          this[_rid],
+          string,
+        );
+      }
+      ArrayPrototypeShift(queue);
+    }
+  }
+
   [_serverHandleIdleTimeout]() {
     if (this[_idleTimeoutDuration]) {
       clearTimeout(this[_idleTimeoutTimeout]);
@@ -646,6 +650,7 @@ function createWebSocketBranded() {
   socket[_binaryType] = "arraybuffer";
   socket[_idleTimeoutDuration] = 0;
   socket[_idleTimeoutTimeout] = undefined;
+  socket[_sendQueue] = [];
   return socket;
 }
 
