498e6e99944cfc031852cf2032947b5138c3ac5c
Luca Casonato <hello@lcas.dev>
2024-06-08 20:20:39 +0200

    review comment 2

--- tests/testdata/test/fail_color.ts
+++ tests/testdata/test/fail_color.ts
@@ -2,8 +2,7 @@ Deno.test("fail color", () => {
   throw new Error(`RedMessage: \x1b[31mThis should be red text\x1b[39m`);
 });
 
-// deno-lint-ignore no-explicit-any
-Deno.test("step fail color", async (t: any) => {
+Deno.test("step fail color", async (t) => {
   await t.step("step", () => {
     throw new Error(`RedMessage: \x1b[31mThis should be red text\x1b[39m`);
   });
