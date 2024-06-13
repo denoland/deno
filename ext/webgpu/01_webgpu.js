05258ba198aa340f640e4ea6e2285707a6a21b44
Divy Srivastava <dj.srivastava23@gmail.com>
2024-06-13 16:54:08 +0530

    BREAKING(ext/webgpu): move `width` and `height` options to `UnsafeWindowSurface` constructor
    Fixes https://github.com/denoland/deno/issues/23508

--- ext/webgpu/01_webgpu.js
+++ ext/webgpu/01_webgpu.js
@@ -7420,16 +7420,6 @@ const dictMembersGPUCanvasConfiguration = [
     converter: webidl.converters["GPUPresentMode"],
   },
   {
-    key: "width",
-    converter: webidl.converters["long"],
-    required: true,
-  },
-  {
-    key: "height",
-    converter: webidl.converters["long"],
-    required: true,
-  },
-  {
     key: "viewFormats",
     converter: webidl.createSequenceConverter(
       webidl.converters["GPUTextureFormat"],
