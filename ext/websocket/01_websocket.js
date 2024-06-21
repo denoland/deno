69db5463e0e3d204fa2afed7c6689551f470a9ef
Divy Srivastava <dj.srivastava23@gmail.com>
2024-06-21 17:31:21 +0530

    x

--- ext/websocket/01_websocket.js
+++ ext/websocket/01_websocket.js
@@ -426,7 +426,8 @@ class WebSocket extends EventTarget {
       const kind = await op_ws_next_event(rid);
       /* close the connection if read was cancelled, and we didn't get a close frame */
       if (
-        this[_readyState] == CLOSING && kind <= 3 && this[_role] !== CLIENT
+        (this[_readyState] == CLOSING) &&
+        kind <= 3 && this[_role] !== CLIENT
       ) {
         this[_readyState] = CLOSED;
 
