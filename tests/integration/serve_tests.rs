b2147fe8e3ae47f35174afbd5198440999724161
losfair <zhy20000919@hotmail.com>
2024-07-02 00:06:24 +0800

    feat(serve): env var `DENO_SERVE_ADDRESS` for configuring default listen address

--- tests/integration/serve_tests.rs
+++ tests/integration/serve_tests.rs
@@ -1,5 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Read;
 
 use deno_fetch::reqwest;
@@ -88,6 +90,79 @@ async fn deno_serve_no_args() {
 
   let body = res.text().await.unwrap();
   assert_eq!(body, "deno serve with no args in fetch() works!");
+
+  child.kill().unwrap();
+  child.wait().unwrap();
+}
+
+#[tokio::test]
+async fn deno_run_serve_with_tcp_from_env() {
+  let mut child = util::deno_cmd()
+    .current_dir(util::testdata_path())
+    .arg("run")
+    .arg("--allow-env=DENO_SERVE_ADDRESS")
+    .arg("--allow-net")
+    .arg("./serve/run_serve.ts")
+    .env("DENO_SERVE_ADDRESS", format!("tcp/127.0.0.1:0"))
+    .stdout_piped()
+    .spawn()
+    .unwrap();
+  let stdout = BufReader::new(child.stdout.as_mut().unwrap());
+  let msg = stdout.lines().next().unwrap().unwrap();
+
+  // Deno.serve() listens on 0.0.0.0 by default. This checks DENO_SERVE_ADDRESS
+  // is not ignored by ensuring it's listening on 127.0.0.1.
+  let port_regex = Regex::new(r"http:\/\/127\.0\.0\.1:(\d+)").unwrap();
+  let port = port_regex.captures(&msg).unwrap().get(1).unwrap().as_str();
+
+  let client = reqwest::Client::builder().build().unwrap();
+
+  let res = client
+    .get(&format!("http://127.0.0.1:{port}"))
+    .send()
+    .await
+    .unwrap();
+  assert_eq!(200, res.status());
+
+  let body = res.text().await.unwrap();
+  assert_eq!(body, "Deno.serve() works!");
+
+  child.kill().unwrap();
+  child.wait().unwrap();
+}
+
+#[tokio::test]
+#[cfg(unix)]
+async fn deno_run_serve_with_unix_socket_from_env() {
+  use tokio::io::AsyncReadExt;
+  use tokio::io::AsyncWriteExt;
+  use tokio::net::UnixStream;
+
+  let dir = tempfile::TempDir::new().unwrap();
+  let sock = dir.path().join("listen.sock");
+  let mut child = util::deno_cmd()
+    .current_dir(util::testdata_path())
+    .arg("run")
+    .arg("--allow-env=DENO_SERVE_ADDRESS")
+    .arg(format!("--allow-read={}", sock.display()))
+    .arg(format!("--allow-write={}", sock.display()))
+    .arg("./serve/run_serve.ts")
+    .env("DENO_SERVE_ADDRESS", format!("unix/{}", sock.display()))
+    .stdout_piped()
+    .spawn()
+    .unwrap();
+  let stdout = BufReader::new(child.stdout.as_mut().unwrap());
+  stdout.lines().next().unwrap().unwrap();
+
+  // reqwest does not support connecting to unix sockets yet, so here we send the http
+  // payload directly
+  let mut conn = UnixStream::connect(dir.path().join("listen.sock"))
+    .await
+    .unwrap();
+  conn.write_all(b"GET / HTTP/1.0\r\n\r\n").await.unwrap();
+  let mut response = String::new();
+  conn.read_to_string(&mut response).await.unwrap();
+  assert!(response.ends_with("\r\nDeno.serve() works!"));
 
   child.kill().unwrap();
   child.wait().unwrap();
