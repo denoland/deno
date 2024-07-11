ae6e677fe818b99033ddc73eb0b9b1ab8f3b35c2
Marvin Hagemeister <marvin@deno.com>
2024-07-11 09:23:23 +0200

    chore: add stub test file

---
+++ tests/unit_node/_fs/_fs_dirent_test.ts
@@ -0,0 +1,5 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+
+// Empty file to make check_test_glob happy. The Dirent API
+// should not be consumed directly, but rather as a result of
+// fs.readdir and fs.readdirSync
