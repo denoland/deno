8e75b6379375f71e8b2d1a389e45a53525a3aa07
vwh <vwhe@proton.me>
2024-07-12 22:04:27 +0300

    fix(cli/init): broken link in deno init sample template (#24545)
    This commit fixes the broken link in the sample template provided by the
    deno init command.

--- cli/tools/init/mod.rs
+++ cli/tools/init/mod.rs
@@ -68,7 +68,7 @@ Deno.test(function addTest() {
   return a + b;
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
 }
