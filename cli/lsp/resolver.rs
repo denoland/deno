8ae5cd26d8a5a836f20e32d643de07c11faca727
David Sherret <dsherret@gmail.com>
2024-07-16 19:00:51 -0400

    fix comment

--- cli/lsp/resolver.rs
+++ cli/lsp/resolver.rs
@@ -533,7 +533,7 @@ fn create_graph_resolver(
         // todo(dsherret): we need a better way of determining the workspace
         // root for this config data, but this is fine for now because this
         // value is only used for determining if a specifier is in the workspace
-        // in order to resolve bare specifiers to deno.json files.
+        // in order to resolve bare specifiers to workspace npm packages
         npm_resolver
           .and_then(|r| r.root_node_modules_path())
           .and_then(|p| p.parent())
