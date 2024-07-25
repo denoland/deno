e598fe9fa91ffe73e3297ff2287cd6f4fa2daee4
yazan-abdalrahman <yazan.abdalrahman@exalt.ps>
2024-07-25 10:57:03 +0300

    fix lint #

--- tests/integration/serve_tests.rs
+++ tests/integration/serve_tests.rs
@@ -1,8 +1,11 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
+#[cfg(windows)]
 use std::io::Read;
 
+#[cfg(windows)]
 use regex::Regex;
+#[cfg(windows)]
 use test_util as util;
 
 #[cfg(windows)]
