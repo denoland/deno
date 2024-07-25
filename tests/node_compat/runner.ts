88731ff502b129f550f11a255451aab3a8d7d875
Nathan Whitaker <nathan@deno.com>
2024-07-24 18:34:03 -0700

    Remove argv patch because I keep forkbombing myself

--- tests/node_compat/runner.ts
+++ tests/node_compat/runner.ts
@@ -4,25 +4,10 @@ import { createRequire } from "node:module";
 import { toFileUrl } from "@std/path/to-file-url";
 import process from "node:process";
 
-// exclude argv[1], which is the path to this runner script.
-// some tests expect that argv[1] is the path to the test file.
-function patchArgv() {
-  const out = [];
-  for (let i = 0; i < process.argv.length; i++) {
-    if (i === 1) {
-      continue;
-    }
-    out.push(process.argv[i]);
-  }
-  process.argv = out;
-}
-
 const file = Deno.args[0];
 if (!file) {
   throw new Error("No file provided");
 }
-
-patchArgv();
 
 if (file.endsWith(".mjs")) {
   await import(toFileUrl(file).href);
