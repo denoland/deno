46489799d2d5b0486f865696d5f3b26267b8ed46
Asher Gomez <ashersaupingomez@gmail.com>
2024-07-25 11:34:41 +1000

    fix

--- tests/node_compat/runner.ts
+++ tests/node_compat/runner.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import "./polyfill_globals.js";
 import { createRequire } from "node:module";
-import { toFileUrl } from "@std/path";
+import { toFileUrl } from "@std/path/to-file-url";
 const file = Deno.args[0];
 if (!file) {
   throw new Error("No file provided");
