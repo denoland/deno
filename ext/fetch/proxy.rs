8c2f1f5a55a2a9bb9e04c12236faa341b3fd49b6
Satya Rohith <me@satyarohith.com>
2024-07-29 22:28:04 +0530

    chore: upgrade to rust 1.80 (#24778)

--- ext/fetch/proxy.rs
+++ ext/fetch/proxy.rs
@@ -269,10 +269,10 @@ impl NoProxy {
   /// * If neither environment variable is set, `None` is returned
   /// * Entries are expected to be comma-separated (whitespace between entries is ignored)
   /// * IP addresses (both IPv4 and IPv6) are allowed, as are optional subnet masks (by adding /size,
-  /// for example "`192.168.1.0/24`").
+  ///   for example "`192.168.1.0/24`").
   /// * An entry "`*`" matches all hostnames (this is the only wildcard allowed)
   /// * Any other entry is considered a domain name (and may contain a leading dot, for example `google.com`
-  /// and `.google.com` are equivalent) and would match both that domain AND all subdomains.
+  ///   and `.google.com` are equivalent) and would match both that domain AND all subdomains.
   ///
   /// For example, if `"NO_PROXY=google.com, 192.168.1.0/24"` was set, all of the following would match
   /// (and therefore would bypass the proxy):
