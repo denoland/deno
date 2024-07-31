e9a8cd3338355a44db0c542b78373f1383f67222
Luca Casonato <hello@lcas.dev>
2024-07-31 21:12:28 +0200

    fix(ext/crypto): respect offsets when writing into ab views in randomFillSync

--- tests/unit_node/crypto/crypto_misc_test.ts
+++ tests/unit_node/crypto/crypto_misc_test.ts
@@ -1,6 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { randomFillSync, randomUUID } from "node:crypto";
 import { assert, assertEquals } from "../../unit/test_util.ts";
+import { assertNotEquals } from "@std/assert/assert_not_equals.ts";
 
 Deno.test("[node/crypto.getRandomUUID] works the same way as Web Crypto API", () => {
   assertEquals(randomUUID().length, crypto.randomUUID().length);
@@ -15,4 +16,15 @@ Deno.test("[node/crypto.randomFillSync] supported arguments", () => {
   // @ts-ignore: arraybuffer arguments are valid.
   assert(randomFillSync(buf.buffer));
   assert(randomFillSync(new DataView(buf.buffer)));
+});
+
+Deno.test("[node/crypto.randomFillSync] array buffer view", () => {
+  const buf = new Uint8Array(32);
+  const view = new Uint8Array(buf.buffer, 8, 16);
+
+  assert(randomFillSync(view));
+  assertEquals(view.length, 16);
+  assertNotEquals(view, new Uint8Array(16));
+  assertEquals(buf.subarray(0, 8), new Uint8Array(8));
+  assertEquals(buf.subarray(24, 32), new Uint8Array(8));
 });
