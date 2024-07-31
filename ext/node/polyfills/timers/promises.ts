1e2581e57b46e5e7512ceb5637d6007db67c3ed4
Marvin Hagemeister <marvin@deno.com>
2024-07-31 12:00:01 +0200

    fix(node/timers/promises): add scheduler APIs (#24802)
    This PR adds the experimental `scheduler` APIs in Node's
    `timers/promises` module. See
    https://nodejs.org/api/timers.html#timerspromisesschedulerwaitdelay-options
    
    Fixes https://github.com/denoland/deno/issues/24800

--- ext/node/polyfills/timers/promises.ts
+++ ext/node/polyfills/timers/promises.ts
@@ -6,6 +6,13 @@ export const setTimeout = promisify(timers.setTimeout),
   setImmediate = promisify(timers.setImmediate),
   setInterval = promisify(timers.setInterval);
 
+export const scheduler = {
+  async wait(delay: number, options?: { signal?: AbortSignal }): Promise<void> {
+    return await setTimeout(delay, undefined, options);
+  },
+  yield: setImmediate,
+};
+
 export default {
   setTimeout,
   setImmediate,
