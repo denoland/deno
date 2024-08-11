59a250e1e0b791664e22d909947015e51c6c2617
Kenta Moriuchi <moriken@kimamass.com>
2024-08-11 22:08:59 +0900

    fix

--- ext/node/polyfills/internal/buffer.mjs
+++ ext/node/polyfills/internal/buffer.mjs
@@ -39,6 +39,7 @@ const {
   SymbolFor,
   SymbolToPrimitive,
   TypeError,
+  TypeErrorPrototype,
   TypedArrayPrototypeCopyWithin,
   TypedArrayPrototypeFill,
   TypedArrayPrototypeGetBuffer,
@@ -945,7 +946,7 @@ function _utf8Slice(buf, start, end) {
     // deno-lint-ignore prefer-primordials
     return decoder.decode(buf.slice(start, end));
   } catch (err) {
-    if (ObjectPrototypeIsPrototypeOf(TypeError, err)) {
+    if (ObjectPrototypeIsPrototypeOf(TypeErrorPrototype, err)) {
       throw new NodeError("ERR_STRING_TOO_LONG", "String too long");
     }
     throw err;
