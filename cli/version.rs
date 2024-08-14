56aff0e3239dc5211eddc83c2077c7507c25c95a
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-08-14 13:16:18 +0200

    remove some fields

--- cli/version.rs
+++ cli/version.rs
@@ -35,11 +35,7 @@ pub static DENO_VERSION_INFO: Lazy<DenoVersionInfo> = Lazy::new(|| {
 
       release_channel: RELEASE_CHANNEL,
 
-      version: env!("CARGO_PKG_VERSION"),
-
       git_hash: env!("GIT_COMMIT_HASH"),
-
-      git_hash_short: env!("GIT_COMMIT_HASH_SHORT"),
 
       version_or_git_hash: if IS_CANARY {
         GIT_COMMIT_HASH
@@ -77,16 +73,8 @@ pub struct DenoVersionInfo {
   #[allow(unused)]
   pub release_channel: ReleaseChannel,
 
-  #[allow(unused)]
-  /// Version from `Cargo.toml`.
-  pub version: &'static str,
-
   /// A full git hash.
   pub git_hash: &'static str,
-
-  #[allow(unused)]
-  /// A 7-char git hash.
-  pub git_hash_short: &'static str,
 
   /// For stable release, a semver like, eg. `v1.46.2`.
   /// For canary release a full git hash, eg. `9bdab6fb6b93eb43b1930f40987fa4997287f9c8`.
