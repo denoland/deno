c765d9ad2fbd82be1b025cae3930fdfe8e30f9e2
Marvin Hagemeister <marvin@deno.com>
2024-08-14 15:34:24 +0200

    fix(node/inspector): Session constructor should not throw (#25041)
    There is no constructor code when creating an inspector `Session`
    instance in Node. Also get rid of some symbols which should've been
    private properties. This PR doesn't yet add any new implementations
    though as these are mostly cosmetic changes.

--- tests/unit_node/inspector_test.ts
+++ tests/unit_node/inspector_test.ts
@@ -1,7 +1,11 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
-import inspector from "node:inspector";
+import inspector, { Session } from "node:inspector";
 import { assertEquals } from "@std/assert/equals";
 
 Deno.test("[node/inspector] - importing inspector works", () => {
   assertEquals(typeof inspector.open, "function");
+});
+
+Deno.test("[node/inspector] - Session constructor should not throw", () => {
+  new Session();
 });
