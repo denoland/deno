3a9254c72f4dbe421ae0b55d0a0c3d51b313cb36
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-08-29 22:36:03 +0200

    fix LSP tests
    Co-Authored-By: Nayeem Rahman <nayeemrmn99@gmail.com>

--- tests/integration/lsp_tests.rs
+++ tests/integration/lsp_tests.rs
@@ -8757,9 +8757,9 @@ fn lsp_npm_specifier_unopened_file() {
   assert_eq!(output.status.code(), Some(0));
 
   let stdout = String::from_utf8(output.stdout).unwrap();
-  assert!(stdout.is_empty());
+  assert_eq!(stdout.as_str(), "");
   let stderr = String::from_utf8(output.stderr).unwrap();
-  assert!(stderr.is_empty());
+  assert_eq!(stderr.as_str(), "");
 
   // open main.ts, which imports other.ts (unopened)
   client.did_open(json!({
@@ -12362,15 +12362,20 @@ fn lsp_data_urls_with_jsx_compiler_option() {
   client.shutdown();
 }
 
-// TODO(2.0): this test needs to be reenabled
 #[test]
-#[ignore]
 fn lsp_node_modules_dir() {
   let context = TestContextBuilder::new()
     .use_http_server()
     .use_temp_cwd()
     .build();
   let temp_dir = context.temp_dir();
+  temp_dir.write(
+    "deno.json",
+    json!({
+      "nodeModulesDir": false,
+    })
+    .to_string(),
+  );
 
   // having a package.json should have no effect on whether
   // a node_modules dir is created
@@ -14366,15 +14371,22 @@ fn lsp_deno_json_workspace_jsr_resolution() {
   client.shutdown();
 }
 
-// TODO(2.0): this test needs to be reenabled
 #[test]
-#[ignore]
 fn lsp_npm_workspace() {
   let context = TestContextBuilder::new()
     .use_http_server()
     .use_temp_cwd()
     .build();
   let temp_dir = context.temp_dir();
+  // TODO(nayeemrmn): Goto definition for local npm package imports should work
+  // even with byonm. Remove this when fixed.
+  temp_dir.write(
+    "deno.json",
+    json!({
+      "nodeModulesDir": false,
+    })
+    .to_string(),
+  );
   temp_dir.write(
     "package.json",
     json!({
