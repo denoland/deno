b3d6988bb9936a35996c12558b692f345c130d0a
Mujahed <safaa.mujahed@exalt.ps>
2024-08-31 19:43:58 +0300

    fix(console): prevent duplicate error printing when the cause is assigned

--- ext/console/01_console.js
+++ ext/console/01_console.js
@@ -1294,6 +1294,7 @@ function getKeys(value, showHidden) {
       ArrayPrototypePushApply(keys, ArrayPrototypeFilter(symbols, filter));
     }
   }
+  keys =  ArrayPrototypeFilter(keys, key => key !== 'cause');
   return keys;
 }
 
