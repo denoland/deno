2d07fc5864def764b3d4959bfdaf0bce90eb15d3
HasanAlrimawi <hasan.alrimawi@exalt.ps>
2024-09-02 17:34:15 +0300

    fix: deprecate eval itests to specs

--- tests/integration/eval_tests.rs
+++ tests/integration/eval_tests.rs
@@ -44,47 +44,47 @@ fn eval_randomness() {
   assert!(numbers.len() > 1);
 }
 
-itest!(eval_basic {
-  args: "eval console.log(\"hello\")",
-  output_str: Some("hello\n"),
-});
+// itest!(eval_basic {
+//   args: "eval console.log(\"hello\")",
+//   output_str: Some("hello\n"),
+// });
 
 // Ugly parentheses due to whitespace delimiting problem.
-itest!(eval_ts {
-  args: "eval --quiet --ext=ts console.log((123)as(number))", // 'as' is a TS keyword only
-  output_str: Some("123\n"),
-});
+// itest!(eval_ts {
+//   args: "eval --quiet --ext=ts console.log((123)as(number))", // 'as' is a TS keyword only
+//   output_str: Some("123\n"),
+// });
 
-itest!(dyn_import_eval {
-  args: "eval import('./subdir/mod4.js').then(console.log)",
-  output: "eval/dyn_import_eval.out",
-});
+// itest!(dyn_import_eval {
+//   args: "eval import('./subdir/mod4.js').then(console.log)",
+//   output: "eval/dyn_import_eval.out",
+// });
 
 // Cannot write the expression to evaluate as "console.log(typeof gc)"
 // because itest! splits args on whitespace.
-itest!(v8_flags_eval {
-  args: "eval --v8-flags=--expose-gc console.log(typeof(gc))",
-  output: "run/v8_flags.js.out",
-});
+// itest!(v8_flags_eval {
+//   args: "eval --v8-flags=--expose-gc console.log(typeof(gc))",
+//   output: "run/v8_flags.js.out",
+// });
 
-itest!(check_local_by_default {
-  args: "eval --quiet import('http://localhost:4545/subdir/type_error.ts').then(console.log);",
-  output: "eval/check_local_by_default.out",
-  http_server: true,
-});
+// itest!(check_local_by_default {
+//   args: "eval --quiet import('http://localhost:4545/subdir/type_error.ts').then(console.log);",
+//   output: "eval/check_local_by_default.out",
+//   http_server: true,
+// });
 
-itest!(check_local_by_default2 {
-  args: "eval --quiet import('./eval/check_local_by_default2.ts').then(console.log);",
-  output: "eval/check_local_by_default2.out",
-  http_server: true,
-});
+// itest!(check_local_by_default2 {
+//   args: "eval --quiet import('./eval/check_local_by_default2.ts').then(console.log);",
+//   output: "eval/check_local_by_default2.out",
+//   http_server: true,
+// });
 
-itest!(env_file {
-  args: "eval --env=env console.log(Deno.env.get(\"ANOTHER_FOO\"))",
-  output_str: Some("ANOTHER_BAR\n"),
-});
+// itest!(env_file {
+//   args: "eval --env=env console.log(Deno.env.get(\"ANOTHER_FOO\"))",
+//   output_str: Some("ANOTHER_BAR\n"),
+// });
 
-itest!(env_file_missing {
-  args: "eval --env=missing console.log(Deno.env.get(\"ANOTHER_FOO\"))",
-  output: "eval/env_file_missing.out",
-});
+// itest!(env_file_missing {
+//   args: "eval --env=missing console.log(Deno.env.get(\"ANOTHER_FOO\"))",
+//   output: "eval/env_file_missing.out",
+// });
