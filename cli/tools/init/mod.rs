3e6a84b178ba11defd4e6ec5ceff64aa8047d9b1
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-03 08:27:00 +1000

    tweak

--- cli/tools/init/mod.rs
+++ cli/tools/init/mod.rs
@@ -82,6 +82,7 @@ Deno.test(async function serverFetchStatic() {
   const req = new Request("https://deno.land/static/hello.js");
   const res = await server.fetch(req);
   assertEquals(await res.text(), 'console.log("Hello, world!");\n');
+  assertEquals(res.headers.get("content-type"), "text/javascript; charset=UTF-8");
 });
 "#,
     )?;
