a4af8ca14d92a4bf3a9ccc80a2d9666048d37904
David Sherret <dsherret@gmail.com>
2024-09-02 19:39:38 +0200

    fixes

--- tests/specs/permission/path_not_permitted/main.ts
+++ tests/specs/permission/path_not_permitted/main.ts
@@ -2,7 +2,7 @@ const binaryName = Deno.build.os === "windows" ? "deno.exe" : "deno";
 Deno.copyFileSync(Deno.execPath(), binaryName);
 
 console.log("Running...");
-const result = new Deno.Command(
+new Deno.Command(
   Deno.execPath(),
   {
     args: [
@@ -16,5 +16,3 @@ const result = new Deno.Command(
     stdout: "inherit",
   },
 ).outputSync();
-
-console.assert(result.code == 1, "Expected failure");
--- tests/specs/permission/path_not_permitted/sub.ts
+++ tests/specs/permission/path_not_permitted/sub.ts
@@ -4,13 +4,31 @@ const pathSep = Deno.build.os === "windows" ? "\\" : "/";
 Deno.mkdirSync("subdir");
 Deno.copyFileSync(binaryName, "subdir/" + binaryName);
 
-const { code, stdout, stderr } = new Deno.Command(
-  binaryName,
-  {
-    env: { "PATH": Deno.cwd() + pathSep + "subdir" },
-    stdout: "inherit",
-    stderr: "inherit",
-  },
-).outputSync();
+try {
+  const commandResult = new Deno.Command(
+    binaryName,
+    {
+      env: { "PATH": Deno.cwd() + pathSep + "subdir" },
+      stdout: "inherit",
+      stderr: "inherit",
+    },
+  ).outputSync();
 
-console.log(code);
+  console.log(commandResult.code);
+} catch (err) {
+  console.log(err);
+}
+
+try {
+  const child = Deno.run(
+    {
+      cmd: [binaryName],
+      env: { "PATH": Deno.cwd() + pathSep + "subdir" },
+      stdout: "inherit",
+      stderr: "inherit",
+    },
+  );
+  console.log((await child.status()).code);
+} catch (err) {
+  console.log(err);
+}
