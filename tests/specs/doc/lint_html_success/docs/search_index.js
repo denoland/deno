f4a01497b0827edfa84d4f83836ce249ec333cc5
David Sherret <dsherret@gmail.com>
2024-09-05 09:30:07 +0200

    remove temp files

--- tests/specs/doc/lint_html_success/docs/search_index.js
+++
@@ -1,3 +0,0 @@
-(function () {
-  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["class"],"name":"Test","file":".","doc":"My test class.","location":{"filename":"default","line":2,"col":0,"byteIndex":22},"url":"././~/Test.html","category":"","declarationKind":"export","deprecated":false}]};
-})()
