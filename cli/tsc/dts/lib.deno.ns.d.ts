1b18cab03b0248f93b84e84abe770ae0327e3547
Asher Gomez <ashersaupingomez@gmail.com>
2024-09-10 08:36:15 +1000

    revert

--- cli/tsc/dts/lib.deno.ns.d.ts
+++ cli/tsc/dts/lib.deno.ns.d.ts
@@ -2606,6 +2606,30 @@ declare namespace Deno {
   }
 
   /**
+   * Read Reader `r` until EOF (`null`) and resolve to the content as
+   * Uint8Array`.
+   *
+   * @deprecated This will be removed in Deno 2.0. See the
+   * {@link https://docs.deno.com/runtime/manual/advanced/migrate_deprecations | Deno 1.x to 2.x Migration Guide}
+   * for migration instructions.
+   *
+   * @category I/O
+   */
+  export function readAll(r: Reader): Promise<Uint8Array>;
+
+  /**
+   * Synchronously reads Reader `r` until EOF (`null`) and returns the content
+   * as `Uint8Array`.
+   *
+   * @deprecated This will be removed in Deno 2.0. See the
+   * {@link https://docs.deno.com/runtime/manual/advanced/migrate_deprecations | Deno 1.x to 2.x Migration Guide}
+   * for migration instructions.
+   *
+   * @category I/O
+   */
+  export function readAllSync(r: ReaderSync): Uint8Array;
+
+  /**
    * Options which can be set when using {@linkcode Deno.mkdir} and
    * {@linkcode Deno.mkdirSync}.
    *
