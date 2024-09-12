fbbf1b5fd951a31cab4495b22f3de495f7c57780
snek <snek@deno.com>
2024-09-11 22:07:31 -0700

    Update async_hooks_test.ts
    Signed-off-by: snek <snek@deno.com>

--- tests/unit_node/async_hooks_test.ts
+++ tests/unit_node/async_hooks_test.ts
@@ -163,6 +163,7 @@ Deno.test(async function worksWithAsyncAPIs() {
 
 Deno.test(async function worksWithDynamicImports() {
   const store = new AsyncLocalStorage();
+  // @ts-expect-error implicit any
   globalThis.alsDynamicImport = () => store.getStore();
   const dataUrl =
     `data:application/javascript,export const data = alsDynamicImport()`;
