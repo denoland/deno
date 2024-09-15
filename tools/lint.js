99d1a1ef44aef37d3b19f09014041ae63722b59a
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-09-16 01:39:24 +0200

    lint

--- tools/lint.js
+++ tools/lint.js
@@ -220,7 +220,7 @@ async function ensureNoNewITests() {
     "pm_tests.rs": 0,
     "publish_tests.rs": 0,
     "repl_tests.rs": 0,
-    "run_tests.rs": 335,
+    "run_tests.rs": 334,
     "shared_library_tests.rs": 0,
     "task_tests.rs": 4,
     "test_tests.rs": 73,
