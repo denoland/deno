30cc7dc4aa34edcc8f878047a9eaa32414041d22
Yoshiya Hinosawa <stibium121@gmail.com>
2024-09-18 11:51:36 +0900

    fix(ext/node): do not throw error for unsupported signal binding on windows

--- ext/node/polyfills/process.ts
+++ ext/node/polyfills/process.ts
@@ -517,6 +517,9 @@ Process.prototype.on = function (
       // Ignores SIGBREAK if the platform is not windows.
     } else if (event === "SIGTERM" && Deno.build.os === "windows") {
       // Ignores SIGTERM on windows.
+    } else if (event !== "SIGBREAK" && event !== "SIGINT" && Deno.build.os === "windows") {
+      // Ignores all signals except SIGBREAK and SIGINT on windows.
+      console.warn(`Ignoring signal "${event}" on Windows`);
     } else {
       EventEmitter.prototype.on.call(this, event, listener);
       Deno.addSignalListener(event as Deno.Signal, listener);
@@ -541,8 +544,8 @@ Process.prototype.off = function (
   } else if (event.startsWith("SIG")) {
     if (event === "SIGBREAK" && Deno.build.os !== "windows") {
       // Ignores SIGBREAK if the platform is not windows.
-    } else if (event === "SIGTERM" && Deno.build.os === "windows") {
-      // Ignores SIGTERM on windows.
+    } else if (event !== "SIGBREAK" && event !== "SIGINT" && Deno.build.os === "windows") {
+      // Ignores all signals except SIGBREAK and SIGINT on windows.
     } else {
       EventEmitter.prototype.off.call(this, event, listener);
       Deno.removeSignalListener(event as Deno.Signal, listener);
