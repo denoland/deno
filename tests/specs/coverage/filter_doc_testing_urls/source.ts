fd7ac4ec4be212ff365f666cd60907da9dfae882
Yoshiya Hinosawa <stibium121@gmail.com>
2024-09-19 23:07:29 +0900

    add test

---
+++ tests/specs/coverage/filter_doc_testing_urls/source.ts
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @example Usage
+ * ```ts
+ * import { add } from "./source.ts";
+ *
+ * add(1, 2); // 3
+ * ```
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+}
