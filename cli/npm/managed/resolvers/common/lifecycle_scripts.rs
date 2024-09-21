3117d12c059e084306f5219991f8c60c12d28fee
Nathan Whitaker <nathan@deno.com>
2024-09-20 21:43:20 -0700

    Appease lint

--- cli/npm/managed/resolvers/common/lifecycle_scripts.rs
+++ cli/npm/managed/resolvers/common/lifecycle_scripts.rs
@@ -1,3 +1,5 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+
 use super::bin_entries::BinEntries;
 use crate::args::LifecycleScriptsConfig;
 use deno_npm::resolution::NpmResolutionSnapshot;
