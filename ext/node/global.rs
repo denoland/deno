7946daaabcfc68caf245f3ac7bb0bf159088d497
Marvin Hagemeister <marvin@deno.com>
2024-09-23 10:43:49 +0200

    fix: managed globals length

--- ext/node/global.rs
+++ ext/node/global.rs
@@ -61,13 +61,12 @@ const fn str_to_utf16<const N: usize>(s: &str) -> [u16; N] {
 
 // UTF-16 encodings of the managed globals. THIS LIST MUST BE SORTED.
 #[rustfmt::skip]
-const MANAGED_GLOBALS: [&[u16]; 12] = [
+const MANAGED_GLOBALS: [&[u16]; 11] = [
   &str_to_utf16::<6>("Buffer"),
   &str_to_utf16::<17>("WorkerGlobalScope"),
   &str_to_utf16::<14>("clearImmediate"),
   &str_to_utf16::<13>("clearInterval"),
   &str_to_utf16::<12>("clearTimeout"),
-  &str_to_utf16::<6>("global"),
   &str_to_utf16::<11>("performance"),
   &str_to_utf16::<4>("self"),
   &str_to_utf16::<12>("setImmediate"),
