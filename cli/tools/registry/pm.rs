d8ddd28fc098070fb10369e8627c31e45bdeb605
Nathan Whitaker <nathan@deno.com>
2024-09-24 15:58:07 -0700

    Default to "latest" tag for npm reqs in deno install

--- cli/tools/registry/pm.rs
+++ cli/tools/registry/pm.rs
@@ -5,6 +5,7 @@ mod cache_deps;
 pub use cache_deps::cache_top_level_deps;
 use deno_semver::jsr::JsrPackageReqReference;
 use deno_semver::npm::NpmPackageReqReference;
+use deno_semver::VersionReq;
 
 use std::borrow::Cow;
 use std::path::PathBuf;
@@ -654,6 +655,14 @@ struct AddPackageReq {
   value: AddPackageReqValue,
 }
 
+static LATEST_REQ: std::sync::LazyLock<VersionReq> =
+  std::sync::LazyLock::new(|| {
+    VersionReq::from_raw_text_and_inner(
+      "latest".into(),
+      deno_semver::RangeSetOrTag::Tag("latest".into()),
+    )
+  });
+
 impl AddPackageReq {
   pub fn parse(entry_text: &str) -> Result<Result<Self, PackageReq>, AnyError> {
     enum Prefix {
@@ -717,7 +726,14 @@ impl AddPackageReq {
       Prefix::Npm => {
         let req_ref =
           NpmPackageReqReference::from_str(&format!("npm:{}", entry_text))?;
-        let package_req = req_ref.into_inner().req;
+        let mut package_req = req_ref.into_inner().req;
+        // deno_semver defaults to a version req of `*` if none is specified
+        // we want to default to `latest` instead
+        if &package_req.version_req == &*deno_semver::WILDCARD_VERSION_REQ
+          && !entry_text.contains("@*")
+        {
+          package_req.version_req = LATEST_REQ.clone();
+        }
         Ok(Ok(AddPackageReq {
           alias: maybe_alias.unwrap_or_else(|| package_req.name.to_string()),
           value: AddPackageReqValue::Npm(package_req),
