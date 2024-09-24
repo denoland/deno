d82f4f32e50e95b0de67a3f77378f111e8e65d8a
yazan-abdalrahman <yazan.abdalrahman@exalt.ps>
2024-09-24 16:58:16 +0300

    fix #24791

--- ext/fs/std_fs.rs
+++ ext/fs/std_fs.rs
@@ -1047,7 +1047,7 @@ fn open_options(options: OpenOptions) -> fs::OpenOptions {
 }
 
 #[inline(always)]
-fn open_with_access_check(
+pub fn open_with_access_check(
   options: OpenOptions,
   path: &Path,
   access_check: Option<AccessCheckCb>,
@@ -1055,7 +1055,9 @@ fn open_with_access_check(
   if let Some(access_check) = access_check {
     let path_bytes = path.as_os_str().as_encoded_bytes();
     let is_windows_device_path = cfg!(windows)
-      && path_bytes.starts_with(br"\\.\")
+      && (path_bytes.starts_with(br"\\.\")
+        || path_bytes.starts_with(b"//./")
+        || path_bytes.ends_with(b"$"))
       && !path_bytes.contains(&b':');
     let path = if is_windows_device_path {
       // On Windows, normalize_path doesn't work with device-prefix-style
