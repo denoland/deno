03776cdca3ca9b0b1234ff6935ff4eb590ef2bc1
yazan-abdalrahman <yazan.abdalrahman@exalt.ps>
2024-09-25 16:42:06 +0300

    fix

--- cli/npm/managed/resolvers/local.rs
+++ cli/npm/managed/resolvers/local.rs
@@ -527,7 +527,7 @@ async fn sync_resolution_with_fs(
                 // Handle pre-release versions differently, if needed
                 log::warn!("{} Version '{}' for alias '{}' is a pre-release version.",  colors::yellow("Warning"), version_str, remote_alias);
                 if let Some(req_tag) = remote.req.version_req.tag() {
-                  if version_str.contains(req_tag) { false } else { true } } else { true }
+                  if version_str.contains(req_tag) { true } else { false } } else { false }
               } else {
                 !remote.req.version_req.matches(&nv.get().version) // compatible version
               }
