93779003ce1461fbf0dafabae7dfc750081af16f
Will Atlas <atlas@falcon.local>
2024-10-08 10:39:20 +0100

    update generate script

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -13,7 +13,7 @@ const ubuntuARMRunner = "ubicloud-standard-16-arm";
 const windowsX86Runner = "windows-2022";
 const windowsX86XlRunner = "windows-2022-xl";
 const macosX86Runner = "macos-13";
-const macosArmRunner = "macos-14";
+const macosArmRunner = "ghcr.io/cirruslabs/macos-runner:sonoma";
 
 const Runners = {
   linuxX86: {
