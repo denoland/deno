b159746f1a864e4962ee2e38a51f6c7c5e6b4119
Nathan Whitaker <nathan@deno.com>
2024-10-15 12:03:47 -0700

    5 attempts, clamp delay

--- cli/util/retry.rs
+++ cli/util/retry.rs
@@ -4,7 +4,9 @@ use std::time::Duration;
 pub fn retry<F: FnMut() -> Fut, Fut: Future<Output = Result<T, E>>, T, E>(
   mut f: F,
 ) -> impl Future<Output = Result<T, E>> {
-  const ATTEMPTS: usize = 4;
+  const ATTEMPTS: usize = 5;
+  const MAX_WAIT: Duration = Duration::from_secs(1);
+  const MIN_WAIT: Duration = Duration::from_micros(1);
   async move {
     let mut wait = Duration::from_millis(1);
     let mut attempt = 1;
@@ -19,6 +21,7 @@ pub fn retry<F: FnMut() -> Fut, Fut: Future<Output = Result<T, E>>, T, E>(
       tokio::time::sleep(wait).await;
       attempt += 1;
       wait *= 10;
+      wait = wait.clamp(MIN_WAIT, MAX_WAIT);
     }
   }
 }
