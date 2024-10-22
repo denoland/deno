cb6062380eee794b130d63b2d5f0c9c1ac1ffe45
David Sherret <dsherret@gmail.com>
2024-10-21 21:01:50 -0400

    feat(task): dependencies

--- cli/tools/task.rs
+++ cli/tools/task.rs
@@ -16,7 +16,10 @@ use deno_core::anyhow::anyhow;
 use deno_core::anyhow::bail;
 use deno_core::anyhow::Context;
 use deno_core::error::AnyError;
+use deno_core::futures::stream::futures_unordered;
+use deno_core::futures::StreamExt;
 use deno_path_util::normalize_path;
+use deno_runtime::deno_node::NodeResolver;
 use deno_task_shell::ShellCommand;
 
 use crate::args::CliOptions;
@@ -71,132 +74,163 @@ pub async fn execute_script(
   let node_resolver = factory.node_resolver().await?;
   let env_vars = task_runner::real_env_vars();
 
-  match tasks_config.task(task_name) {
-    Some((dir_url, task_or_script)) => match task_or_script {
-      TaskOrScript::Task(_tasks, script) => {
-        let cwd = match task_flags.cwd {
-          Some(path) => canonicalize_path(&PathBuf::from(path))
-            .context("failed canonicalizing --cwd")?,
-          None => normalize_path(dir_url.to_file_path().unwrap()),
-        };
+  let task_runner = TaskRunner {
+    tasks_config,
+    task_flags: &task_flags,
+    npm_resolver: npm_resolver.as_ref(),
+    node_resolver: node_resolver.as_ref(),
+    env_vars,
+    cli_options,
+  };
+  task_runner.run_task(task_name).await
+}
 
-        let custom_commands = task_runner::resolve_custom_commands(
-          npm_resolver.as_ref(),
-          node_resolver,
-        )?;
-        run_task(RunTaskOptions {
-          task_name,
-          script,
-          cwd: &cwd,
-          env_vars,
-          custom_commands,
-          npm_resolver: npm_resolver.as_ref(),
-          cli_options,
-        })
-        .await
-      }
-      TaskOrScript::Script(scripts, _script) => {
-        // ensure the npm packages are installed if using a managed resolver
-        if let Some(npm_resolver) = npm_resolver.as_managed() {
-          npm_resolver.ensure_top_level_package_json_install().await?;
-        }
+struct RunSingleOptions<'a> {
+  task_name: &'a str,
+  script: &'a str,
+  cwd: &'a Path,
+  custom_commands: HashMap<String, Rc<dyn ShellCommand>>,
+}
 
-        let cwd = match task_flags.cwd {
-          Some(path) => canonicalize_path(&PathBuf::from(path))?,
-          None => normalize_path(dir_url.to_file_path().unwrap()),
-        };
+struct TaskRunner<'a> {
+  tasks_config: WorkspaceTasksConfig,
+  task_flags: &'a TaskFlags,
+  npm_resolver: &'a dyn CliNpmResolver,
+  node_resolver: &'a NodeResolver,
+  env_vars: HashMap<String, String>,
+  cli_options: &'a CliOptions,
+}
 
-        // At this point we already checked if the task name exists in package.json.
-        // We can therefore check for "pre" and "post" scripts too, since we're only
-        // dealing with package.json here and not deno.json
-        let task_names = vec![
-          format!("pre{}", task_name),
-          task_name.clone(),
-          format!("post{}", task_name),
-        ];
-        let custom_commands = task_runner::resolve_custom_commands(
-          npm_resolver.as_ref(),
-          node_resolver,
-        )?;
-        for task_name in &task_names {
-          if let Some(script) = scripts.get(task_name) {
-            let exit_code = run_task(RunTaskOptions {
-              task_name,
-              script,
-              cwd: &cwd,
-              env_vars: env_vars.clone(),
-              custom_commands: custom_commands.clone(),
-              npm_resolver: npm_resolver.as_ref(),
-              cli_options,
-            })
-            .await?;
+impl<'a> TaskRunner<'a> {
+  async fn run_task(
+    &self,
+    task_name: &String,
+  ) -> Result<i32, deno_core::anyhow::Error> {
+    match self.tasks_config.task(task_name) {
+      Some((dir_url, task_or_script)) => match task_or_script {
+        TaskOrScript::Task(_tasks, definition) => {
+          let mut futures_unordered =
+            futures_unordered::FuturesUnordered::new();
+          for dep in &definition.dependencies {
+            let dep = dep.clone();
+            futures_unordered.push(async move { self.run_task(&dep).await })
+          }
+          while let Some(result) = futures_unordered.next().await {
+            let exit_code = result?;
             if exit_code > 0 {
               return Ok(exit_code);
             }
           }
-        }
+          let cwd = match &self.task_flags.cwd {
+            Some(path) => canonicalize_path(&PathBuf::from(path))
+              .context("failed canonicalizing --cwd")?,
+            None => normalize_path(dir_url.to_file_path().unwrap()),
+          };
 
-        Ok(0)
+          let custom_commands = task_runner::resolve_custom_commands(
+            self.npm_resolver,
+            self.node_resolver,
+          )?;
+          self
+            .run_single(RunSingleOptions {
+              task_name,
+              script: &definition.command,
+              cwd: &cwd,
+              custom_commands,
+            })
+            .await
+        }
+        TaskOrScript::Script(scripts, _script) => {
+          // ensure the npm packages are installed if using a managed resolver
+          if let Some(npm_resolver) = self.npm_resolver.as_managed() {
+            npm_resolver.ensure_top_level_package_json_install().await?;
+          }
+
+          let cwd = match &self.task_flags.cwd {
+            Some(path) => canonicalize_path(&PathBuf::from(path))?,
+            None => normalize_path(dir_url.to_file_path().unwrap()),
+          };
+
+          // At this point we already checked if the task name exists in package.json.
+          // We can therefore check for "pre" and "post" scripts too, since we're only
+          // dealing with package.json here and not deno.json
+          let task_names = vec![
+            format!("pre{}", task_name),
+            task_name.clone(),
+            format!("post{}", task_name),
+          ];
+          let custom_commands = task_runner::resolve_custom_commands(
+            self.npm_resolver,
+            self.node_resolver,
+          )?;
+          for task_name in &task_names {
+            if let Some(script) = scripts.get(task_name) {
+              let exit_code = self
+                .run_single(RunSingleOptions {
+                  task_name,
+                  script,
+                  cwd: &cwd,
+                  custom_commands: custom_commands.clone(),
+                })
+                .await?;
+              if exit_code > 0 {
+                return Ok(exit_code);
+              }
+            }
+          }
+
+          Ok(0)
+        }
+      },
+      None => {
+        if self.task_flags.is_run {
+          return Err(anyhow!("Task not found: {}", task_name));
+        }
+        log::error!("Task not found: {}", task_name);
+        if log::log_enabled!(log::Level::Error) {
+          print_available_tasks(
+            &mut std::io::stderr(),
+            &self.cli_options.start_dir,
+            &self.tasks_config,
+          )?;
+        }
+        Ok(1)
       }
-    },
-    None => {
-      if task_flags.is_run {
-        return Err(anyhow!("Task not found: {}", task_name));
-      }
-      log::error!("Task not found: {}", task_name);
-      if log::log_enabled!(log::Level::Error) {
-        print_available_tasks(
-          &mut std::io::stderr(),
-          &cli_options.start_dir,
-          &tasks_config,
-        )?;
-      }
-      Ok(1)
     }
   }
-}
 
-struct RunTaskOptions<'a> {
-  task_name: &'a str,
-  script: &'a str,
-  cwd: &'a Path,
-  env_vars: HashMap<String, String>,
-  custom_commands: HashMap<String, Rc<dyn ShellCommand>>,
-  npm_resolver: &'a dyn CliNpmResolver,
-  cli_options: &'a CliOptions,
-}
-
-async fn run_task(opts: RunTaskOptions<'_>) -> Result<i32, AnyError> {
-  let RunTaskOptions {
-    task_name,
-    script,
-    cwd,
-    env_vars,
-    custom_commands,
-    npm_resolver,
-    cli_options,
-  } = opts;
-
-  output_task(
-    opts.task_name,
-    &task_runner::get_script_with_args(script, cli_options.argv()),
-  );
-
-  Ok(
-    task_runner::run_task(task_runner::RunTaskOptions {
+  async fn run_single(
+    &self,
+    opts: RunSingleOptions<'_>,
+  ) -> Result<i32, AnyError> {
+    let RunSingleOptions {
       task_name,
       script,
       cwd,
-      env_vars,
       custom_commands,
-      init_cwd: opts.cli_options.initial_cwd(),
-      argv: cli_options.argv(),
-      root_node_modules_dir: npm_resolver.root_node_modules_path(),
-      stdio: None,
-    })
-    .await?
-    .exit_code,
-  )
+    } = opts;
+
+    output_task(
+      opts.task_name,
+      &task_runner::get_script_with_args(script, self.cli_options.argv()),
+    );
+
+    Ok(
+      task_runner::run_task(task_runner::RunTaskOptions {
+        task_name,
+        script,
+        cwd,
+        env_vars: self.env_vars.clone(),
+        custom_commands,
+        init_cwd: self.cli_options.initial_cwd(),
+        argv: self.cli_options.argv(),
+        root_node_modules_dir: self.npm_resolver.root_node_modules_path(),
+        stdio: None,
+      })
+      .await?
+      .exit_code,
+    )
+  }
 }
 
 fn output_task(task_name: &str, script: &str) {
@@ -252,7 +286,19 @@ fn print_available_tasks(
                 && config.folder_url
                   == *workspace_dir.workspace.root_dir().as_ref();
               config.tasks.iter().map(move |(k, v)| {
-                (is_root, false, (k, Cow::Owned(Task::Definition(v.clone()))))
+                (
+                  is_root,
+                  false,
+                  (
+                    k,
+                    Cow::Owned(Task::Definition(
+                      deno_config::deno_json::TaskDefinition {
+                        command: v.clone(),
+                        dependencies: vec![],
+                      },
+                    )),
+                  ),
+                )
               })
             })
             .into_iter()
@@ -292,7 +338,7 @@ fn print_available_tasks(
             )?;
           }
         }
-        writeln!(writer, "    {definition}")?;
+        writeln!(writer, "    {}", definition.command)?;
       }
     }
   }
