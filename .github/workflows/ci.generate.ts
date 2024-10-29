d75bce08b6ea2a0c1399004834d66ec43b43bfa3
Satya Rohith <me@satyarohith.com>
2024-10-29 11:45:44 +0530

    use noble

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -7,8 +7,8 @@ import { stringify } from "jsr:@std/yaml@^0.221/stringify";
 // automatically via regex, so ensure that this line maintains this format.
 const cacheVersion = 23;
 
-const ubuntuX86Runner = "ubuntu-22.04";
-const ubuntuX86XlRunner = "ubuntu-22.04-xl";
+const ubuntuX86Runner = "ubuntu-24.04";
+const ubuntuX86XlRunner = "ubuntu-24.04-xl";
 const ubuntuARMRunner = "ubicloud-standard-16-arm";
 const windowsX86Runner = "windows-2022";
 const windowsX86XlRunner = "windows-2022-xl";
@@ -74,8 +74,8 @@ sudo apt-get -qq remove \
   'clang-12*' 'clang-13*' 'clang-14*' 'clang-15*' 'clang-16*' 'clang-17*' 'llvm-12*' 'llvm-13*' 'llvm-14*' 'llvm-15*' 'llvm-16*' 'lld-12*' 'lld-13*' 'lld-14*' 'lld-15*' 'lld-16*' 'lld-17*' > /dev/null 2> /dev/null
 
 # Install clang-XXX, lld-XXX, and debootstrap.
-echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-${llvmVersion} main" |
-  sudo dd of=/etc/apt/sources.list.d/llvm-toolchain-jammy-${llvmVersion}.list
+echo "deb http://apt.llvm.org/noble/ llvm-toolchain-noble-${llvmVersion} main" |
+  sudo dd of=/etc/apt/sources.list.d/llvm-toolchain-noble-${llvmVersion}.list
 curl https://apt.llvm.org/llvm-snapshot.gpg.key |
   gpg --dearmor                                 |
 sudo dd of=/etc/apt/trusted.gpg.d/llvm-snapshot.gpg
