4230868c3da82921816628ebc3f55726e83d2e2e
Yoshiya Hinosawa <stibium121@gmail.com>
2024-11-01 20:27:34 +0900

    Revert "Rollback mode changes"
    This reverts commit a2814b8792c80229a5d95f6ba25ff65e9fe2d2cd.

--- ext/fs/30_fs.js
+++ ext/fs/30_fs.js
@@ -380,7 +380,7 @@ function parseFileInfo(response) {
       : null,
     ctime: response.ctimeSet === true ? new Date(response.ctime) : null,
     dev: response.dev,
-    mode: unix ? response.mode : null,
+    mode: response.mode,
     ino: unix ? response.ino : null,
     nlink: unix ? response.nlink : null,
     uid: unix ? response.uid : null,
