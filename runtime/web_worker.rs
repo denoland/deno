700f54a13cce0fcdcf19d1893e3254579c7347f4
snek <snek@deno.com>
2024-11-06 15:08:26 +0100

    fix(ext/node): better inspector support (#26471)
    implement local inspector
    
    future changes:
    - wire up InspectorServer to enable open/close/url
    - wire up connectToMainThread
    
    Fixes https://github.com/denoland/deno/issues/25004

--- runtime/web_worker.rs
+++ runtime/web_worker.rs
@@ -562,7 +562,7 @@ impl WebWorker {
       extension_transpiler: Some(Rc::new(|specifier, source| {
         maybe_transpile_source(specifier, source)
       })),
-      inspector: services.maybe_inspector_server.is_some(),
+      inspector: true,
       feature_checker: Some(services.feature_checker),
       op_metrics_factory_fn,
       import_meta_resolve_callback: Some(Box::new(
@@ -579,18 +579,18 @@ impl WebWorker {
       js_runtime.op_state().borrow_mut().put(op_summary_metrics);
     }
 
+    // Put inspector handle into the op state so we can put a breakpoint when
+    // executing a CJS entrypoint.
+    let op_state = js_runtime.op_state();
+    let inspector = js_runtime.inspector();
+    op_state.borrow_mut().put(inspector);
+
     if let Some(server) = services.maybe_inspector_server {
       server.register_inspector(
         options.main_module.to_string(),
         &mut js_runtime,
         false,
       );
-
-      // Put inspector handle into the op state so we can put a breakpoint when
-      // executing a CJS entrypoint.
-      let op_state = js_runtime.op_state();
-      let inspector = js_runtime.inspector();
-      op_state.borrow_mut().put(inspector);
     }
 
     let (internal_handle, external_handle) = {
