50cb619a542d5014956ebfa195a9143f0f6ea7d6
Divy Srivastava <dj.srivastava23@gmail.com>
2024-11-12 17:46:28 +0530

    Exit

--- cli/tools/registry/mod.rs
+++ cli/tools/registry/mod.rs
@@ -856,6 +856,7 @@ async fn perform_publish(
   assert_eq!(prepared_package_by_name.len(), authorizations.len());
   let mut futures: FuturesUnordered<LocalBoxFuture<Result<String, AnyError>>> =
     Default::default();
+  let mut has_errors = false;
   loop {
     let next_batch = publish_order_graph.next();
 
@@ -911,10 +912,17 @@ async fn perform_publish(
         publish_order_graph.finish_package(&package_name);
       }
       Err(err) => {
+        has_errors = true;
         #[allow(clippy::print_stderr)]
-        eprintln!("{}: {}", colors::red("Error publishing"), err);
+        {
+          eprintln!("{}: {}", colors::red("Error publishing"), err);
+        }
       }
     }
+  }
+
+  if has_errors {
+    std::process::exit(1);
   }
 
   Ok(())
