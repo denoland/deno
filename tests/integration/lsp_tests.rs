bf83c3a2d74a08e4a5b422d4f6294c1acaa0ad0b
Nayeem Rahman <nayeemrmn99@gmail.com>
2024-11-12 12:51:10 +0000

    debug

--- tests/integration/lsp_tests.rs
+++ tests/integration/lsp_tests.rs
@@ -8309,7 +8309,8 @@ fn lsp_npm_auto_import_with_deno_types() {
       import "lz-string";
     "#,
   );
-  let mut client = context.new_lsp_command().build();
+  #[allow(deprecated)]
+  let mut client = context.new_lsp_command().print_stderr().build();
   client.initialize_default();
   client.did_open(json!({
     "textDocument": {
