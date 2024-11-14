ce620cecbbd9e9cf1f758e6dd7f063d94f2a87bd
Yoshiya Hinosawa <stibium121@gmail.com>
2024-11-14 19:14:22 +0900

    restore cares workaround for windows

--- ext/node/polyfills/internal_binding/cares_wrap.ts
+++ ext/node/polyfills/internal_binding/cares_wrap.ts
@@ -36,6 +36,7 @@ import {
 } from "ext:deno_node/internal_binding/async_wrap.ts";
 import { ares_strerror } from "ext:deno_node/internal_binding/ares.ts";
 import { notImplemented } from "ext:deno_node/_utils.ts";
+import { isWindows } from "ext:deno_node/_util/os.ts";
 
 interface LookupAddress {
   address: string;
@@ -67,7 +68,7 @@ export function getaddrinfo(
   _hints: number,
   verbatim: boolean,
 ): number {
-  const addresses: string[] = [];
+  let addresses: string[] = [];
 
   // TODO(cmorten): use hints
   // REF: https://nodejs.org/api/dns.html#dns_supported_getaddrinfo_flags
@@ -104,6 +105,13 @@ export function getaddrinfo(
 
         return 0;
       });
+    }
+
+    // TODO(@bartlomieju): Forces IPv4 as a workaround for Deno not
+    // aligning with Node on implicit binding on Windows
+    // REF: https://github.com/denoland/deno/issues/10762
+    if (isWindows && hostname === "localhost") {
+      addresses = addresses.filter((address) => isIPv4(address));
     }
 
     req.oncomplete(error, addresses);
