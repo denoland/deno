b835b450fd88c2d7a4d5184dab6514a0bda49417
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-11-16 12:55:33 +0100

    add basic tests

---
+++ tests/specs/task/dependencies2/util.js
@@ -0,0 +1,5 @@
+
+export async function randomTimeout(min, max) {
+    const timeout = Math.floor(Math.random() * (max - min + 1) + min);
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
