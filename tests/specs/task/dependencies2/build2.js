9e206dbad9e289abe535d540b2012cc4d41dbea0
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-11-16 13:03:36 +0100

    update tests

--- tests/specs/task/dependencies2/build2.js
+++
@@ -1,9 +0,0 @@
-import { randomTimeout } from "./util.js";
-
-console.log("Starting build2");
-
-await randomTimeout(250, 750);
-console.log("build2 performing more work...");
-await randomTimeout(250, 750);
-
-console.log("build2 finished");
