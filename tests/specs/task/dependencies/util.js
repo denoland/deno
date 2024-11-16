e8a5459b4066174208a9ae97cc4f506a70e235cf
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-11-16 13:12:38 +0100

    fmt and lint

--- tests/specs/task/dependencies/util.js
+++ tests/specs/task/dependencies/util.js
@@ -1,5 +1,4 @@
-
 export async function randomTimeout(min, max) {
-    const timeout = Math.floor(Math.random() * (max - min + 1) + min);
-    return new Promise(resolve => setTimeout(resolve, timeout));
+  const timeout = Math.floor(Math.random() * (max - min + 1) + min);
+  return new Promise((resolve) => setTimeout(resolve, timeout));
 }
