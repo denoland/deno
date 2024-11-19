6b478cd0a3fdff15d5d9d8849a3019652b919921
David Sherret <dsherret@users.noreply.github.com>
2024-11-19 18:20:14 -0500

    feat(compile): ability to embed directory in executable (#26939)

--- cli/standalone/binary.rs
+++ cli/standalone/binary.rs
@@ -620,9 +620,17 @@ impl<'a> DenoCompileBinaryWriter<'a> {
     };
     for include_file in include_files {
       let path = deno_path_util::url_to_file_path(include_file)?;
-      vfs
-        .add_file_at_path(&path)
-        .with_context(|| format!("Including {}", path.display()))?;
+      if path.is_dir() {
+        // TODO(#26941): we should analyze if any of these are
+        // modules in order to include their dependencies
+        vfs
+          .add_dir_recursive(&path)
+          .with_context(|| format!("Including {}", path.display()))?;
+      } else {
+        vfs
+          .add_file_at_path(&path)
+          .with_context(|| format!("Including {}", path.display()))?;
+      }
     }
     let mut remote_modules_store = RemoteModulesStoreBuilder::default();
     let mut code_cache_key_hasher = if self.cli_options.code_cache_enabled() {
