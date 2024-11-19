6356329b37282178701c0064a4a2cd04286a51e7
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-11-20 00:18:55 +0100

    dedup, rename, fix tests

--- cli/tools/info.rs
+++ cli/tools/info.rs
@@ -126,7 +126,7 @@ fn print_cache_info(
   let registry_cache = dir.registries_folder_path();
   let mut origin_dir = dir.origin_data_folder_path();
   let deno_dir = dir.root_path_for_display().to_string();
-  let deno_cache_dir = std::env::temp_dir().join("deno_cache");
+  let web_cache_dir = crate::worker::get_cache_storage_dir();
 
   if let Some(location) = &location {
     origin_dir =
@@ -144,7 +144,7 @@ fn print_cache_info(
       "typescriptCache": typescript_cache,
       "registryCache": registry_cache,
       "originStorage": origin_dir,
-      "denoCacheDir": deno_cache_dir,
+      "webCacheStorage": web_cache_dir,
     });
 
     if location.is_some() {
@@ -181,8 +181,8 @@ fn print_cache_info(
     );
     println!(
       "{} {}",
-      colors::bold("Deno Cache:"),
-      deno_cache_dir.display()
+      colors::bold("Web Cache storage:"),
+      web_cache_dir.display()
     );
     if location.is_some() {
       println!(
