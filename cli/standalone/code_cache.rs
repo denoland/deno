a0f8462ff19eea62004484782fc6144fc5dab34d
David Sherret <dsherret@gmail.com>
2024-11-21 11:56:50 -0500

    chore(compile): log code cache file path

--- cli/standalone/code_cache.rs
+++ cli/standalone/code_cache.rs
@@ -42,7 +42,11 @@ impl DenoCompileCodeCache {
     // attempt to deserialize the cache data
     match deserialize(&file_path, cache_key) {
       Ok(data) => {
-        log::debug!("Loaded {} code cache entries", data.len());
+        log::debug!(
+          "Loaded {} code cache entries from {}",
+          data.len(),
+          file_path.display()
+        );
         Self {
           strategy: CodeCacheStrategy::SubsequentRun(
             SubsequentRunCodeCacheStrategy {
@@ -53,7 +57,11 @@ impl DenoCompileCodeCache {
         }
       }
       Err(err) => {
-        log::debug!("Failed to deserialize code cache: {:#}", err);
+        log::debug!(
+          "Failed to deserialize code cache from {}: {:#}",
+          file_path.display(),
+          err
+        );
         Self {
           strategy: CodeCacheStrategy::FirstRun(FirstRunCodeCacheStrategy {
             cache_key,
