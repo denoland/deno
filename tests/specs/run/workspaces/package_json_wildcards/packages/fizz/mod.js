b8800f60637f698d5c65dd99a644ab33720435a5
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-11-23 02:10:35 +0100

    add a test, but it fails

---
+++ tests/specs/run/workspaces/package_json_wildcards/packages/fizz/mod.js
@@ -0,0 +1,3 @@
+export function fizz() {
+  return "fizz";
+}
---
+++ tests/specs/run/workspaces/package_json_wildcards/packages/foo/mod.js
@@ -0,0 +1,7 @@
+import { bar } from "@foo/bar";
+import { buzz } from "@foo/buzz";
+import { fizz } from "@foo/fizz";
+
+console.log(bar());
+console.log(buzz());
+console.log(fizz());
