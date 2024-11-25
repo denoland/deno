fb22e5ced269d13a1bab499148e6e4463eb3c065
张瑞 <2896402717@qq.com>
2024-11-25 17:12:46 +0800

    Add sync method to filehandle method

--- ext/node/polyfills/internal/fs/handle.ts
+++ ext/node/polyfills/internal/fs/handle.ts
@@ -148,7 +148,7 @@ export class FileHandle extends EventEmitter {
   stat(options: { bigint: true }): Promise<BigIntStats>;
   stat(options?: { bigint: boolean }): Promise<Stats | BigIntStats> {
     return fsCall(promises.fstat, this, options);
-  };
+  }
   // readLines(options?):Promise<InterfaceConstructor>
   sync():Promise<void>{
     return Promise.resolve(promises.sync());
