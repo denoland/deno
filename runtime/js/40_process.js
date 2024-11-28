6343d5ffff87da7627eb6d2599fd281cd08cd427
张瑞 <2896402717@qq.com>
2024-11-28 16:49:44 +0800

    feat(runtime): ChildProcess.kill() error

--- runtime/js/40_process.js
+++ runtime/js/40_process.js
@@ -354,7 +354,11 @@ class ChildProcess {
     if (this.#waitComplete) {
       throw new TypeError("Child process has already terminated");
     }
-    op_spawn_kill(this.#rid, signo);
+    try {
+      op_spawn_kill(this.#rid, signo);
+    } catch (e) {
+      // issue #27112
+    }
   }
 
   async [SymbolAsyncDispose]() {
