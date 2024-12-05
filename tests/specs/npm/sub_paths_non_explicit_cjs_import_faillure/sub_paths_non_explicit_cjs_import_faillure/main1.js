e24433d8493a8c0858769c8751e677c477c14c18
Hajime-san <41257923+Hajime-san@users.noreply.github.com>
2024-12-05 09:48:11 +0900

    add spec test

---
+++ tests/specs/npm/sub_paths_non_explicit_cjs_import_faillure/sub_paths_non_explicit_cjs_import_faillure/main1.js
@@ -0,0 +1,2 @@
+import Server from "npm:@denotest/cjs-non-explicit-file@1.0.0/server";
+console.log(Server);
---
+++ tests/specs/npm/sub_paths_non_explicit_cjs_import_faillure/sub_paths_non_explicit_cjs_import_faillure/main3.js
@@ -0,0 +1,3 @@
+// deno-fmt-ignore
+const Server = await import("npm:@denotest/cjs-non-explicit-file@1.0.0/server");
+console.log(Server);
