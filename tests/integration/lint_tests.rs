32bd8698758bdb168987357e562894e5cc081d34
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-12-10 00:51:35 +0100

    this should work

--- tests/integration/lint_tests.rs
+++ tests/integration/lint_tests.rs
@@ -1,13 +1,13 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
+use deno_core::serde_json;
+use pretty_assertions::assert_eq;
+use serde::Deserialize;
+use serde::Serialize;
 use test_util as util;
-use test_util::assert_contains;
-use test_util::assert_not_contains;
-use util::TestContext;
 use util::TestContextBuilder;
 
-#[test]
-fn all_lint_rules_have_docs() {
+fn get_all_rules() -> Vec<String> {
   let context = TestContextBuilder::new().build();
 
   let output = context
@@ -17,12 +17,22 @@ fn all_lint_rules_have_docs() {
 
   output.assert_exit_code(0);
   let output_text = output.combined_output();
-  let lines = output_text.lines();
+  let mut rules = output_text
+    .lines()
+    .into_iter()
+    .map(|s| s.to_string())
+    .collect::<Vec<String>>();
+  rules.sort();
+  rules
+}
 
+#[test]
+fn all_lint_rules_have_docs() {
+  let all_rules = get_all_rules();
   let mut missing_docs = vec![];
 
-  for line in lines {
-    let snake_case_name = line.replace("-", "_");
+  for rule in all_rules {
+    let snake_case_name = rule.replace("-", "_");
     let doc_path = format!("cli/tools/lint/docs/{}.md", snake_case_name);
     if !util::root_path().join(&doc_path).exists() {
       missing_docs.push(doc_path)
@@ -40,33 +50,39 @@ fn all_lint_rules_have_docs() {
 
 #[test]
 fn all_lint_rules_are_listed_in_schema_file() {
-  let context = TestContextBuilder::new().build();
+  let all_rules = get_all_rules();
 
-  let output = context
-    .new_command()
-    .args("lint --internal-print-all-rules")
-    .run();
+  let rules_schema_path =
+    util::root_path().join("cli/schemas/lint-rules.v1.json");
+  let rules_schema_file = std::fs::read_to_string(&rules_schema_path).unwrap();
 
-  output.assert_exit_code(0);
-  let output_text = output.combined_output();
-  let mut rules = output_text.lines().collect::<Vec<_>>();
-  rules.sort();
+  #[derive(Serialize, Deserialize)]
+  struct RulesSchema {
+    #[serde(rename = "$schema")]
+    schema: String,
 
-  let mut missing_docs = vec![];
-
-  for line in lines {
-    let snake_case_name = line.replace("-", "_");
-    let doc_path = format!("cli/tools/lint/docs/{}.md", snake_case_name);
-    if !util::root_path().join(&doc_path).exists() {
-      missing_docs.push(doc_path)
-    }
+    #[serde(rename = "enum")]
+    rules: Vec<String>,
   }
+  let schema: RulesSchema = serde_json::from_str(&rules_schema_file).unwrap();
 
-  assert!(
-    missing_docs.is_empty(),
-    "Missing lint rule docs:\n{}",
-    missing_docs.join("\n")
-  );
+  const UPDATE_ENV_VAR_NAME: &'static str = "UPDATE_EXPECTED";
 
-  // TODO(bartlomieju): assert that the schema file is up to date
+  if std::env::var(UPDATE_ENV_VAR_NAME).ok().is_none() {
+    assert_eq!(
+      schema.rules, all_rules,
+      "Lint rules schema file not up to date. Run again with {} to update the expected output",
+      UPDATE_ENV_VAR_NAME
+    );
+  } else {
+    std::fs::write(
+      &rules_schema_path,
+      serde_json::to_string_pretty(&RulesSchema {
+        schema: schema.schema,
+        rules: all_rules,
+      })
+      .unwrap(),
+    )
+    .unwrap();
+  }
 }
