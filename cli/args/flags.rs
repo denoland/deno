8b125a4d3a570692e5b147159c61be102cc8283f
Ian Bull <irbull@gmail.com>
2024-12-11 08:55:29 -0800

    chore: add a note in the help about the unstable flag
    The `--unstable` flag was removed in Deno 2.0, and will be completely
    removed in 2.5. Add a note about the removal of this flag and how
    users should use `--unstable-*` instead.

--- cli/args/flags.rs
+++ cli/args/flags.rs
@@ -4350,7 +4350,7 @@ impl CommandExt for Command {
     let mut cmd = self.arg(
       Arg::new("unstable")
       .long("unstable")
-      .help(cstr!("Enable all unstable features and APIs. Instead of using this flag, consider enabling individual unstable features
+      .help(cstr!("The `--unstable` flag has been removed in Deno 2.0. Use granular `--unstable-*` flags instead
   <p(245)>To view the list of individual unstable feature flags, run this command again with --help=unstable</>"))
       .action(ArgAction::SetTrue)
       .hide(matches!(cfg, UnstableArgsConfig::None))
