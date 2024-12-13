b3a6f018dd71afe053d206cb4cc8146efae00c78
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-12-13 23:12:15 +0100

    add DashMap like API

--- resolvers/deno/sync.rs
+++ resolvers/deno/sync.rs
@@ -13,7 +13,22 @@ mod inner {
 
 #[cfg(not(feature = "sync"))]
 mod inner {
+  use std::hash::RandomState;
   pub use std::rc::Rc as MaybeArc;
 
-  pub use std::collections::HashMap as MaybeDashMap;
+  // Wrapper struct that exposes a subset of `DashMap` API.
+  #[derive(Default)]
+  struct MaybeDashMap<K, V, S = RandomState>(RefCell<HashMap<K, V, S>>);
+
+  impl MaybeDashMap<K, V, S> {
+    pub fn get(&'a self, key: &K) -> Option<&'a V> {
+      let inner = self.0.borrow();
+      inner.get(key)
+    }
+
+    pub fn insert(&self, key: K, value: V) -> Option<V> {
+      let inner = self.0.borrow_mut();
+      inner.insert(key, value)
+    }
+  }
 }
