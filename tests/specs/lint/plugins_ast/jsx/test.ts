08a44c31b845d6214403968a6bed55b0e9f9697e
Marvin Hagemeister <marvin@deno.com>
2024-12-20 21:33:10 +0100

    remove lint plugin ast tests

--- tests/specs/lint/plugins_ast/jsx/test.ts
+++
@@ -1,7 +0,0 @@
-import plugin from "./plugin.ts";
-const fileName = "source.tsx";
-const sourceText = await Deno.readTextFile(`./${fileName}`);
-
-Deno.test(function testTs() {
-  Deno[Deno.internal].runLintPlugin(plugin, fileName, sourceText);
-});
