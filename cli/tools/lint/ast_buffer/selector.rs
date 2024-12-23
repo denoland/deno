f79eb998519a46999c66b50a8384f6d3c6939970
Marvin Hagemeister <marvin@deno.com>
2024-12-24 00:26:09 +0100

    port css lexer to rust

--- cli/tools/lint/ast_buffer/selector.rs
+++ cli/tools/lint/ast_buffer/selector.rs
@@ -1,11 +1,13 @@
-use std::str::{CharIndices, Chars};
+use std::{iter::Peekable, str::Chars};
 
+#[derive(Debug, PartialEq)]
 enum RelationOp {
   Space,
   Plus,
   Tilde,
 }
 
+#[derive(Debug, PartialEq)]
 enum AttrOp {
   Equal,
   NotEqual,
@@ -15,6 +17,7 @@ enum AttrOp {
   LessEqual,
 }
 
+#[derive(Debug, PartialEq)]
 enum AttrValue {
   True,
   False,
@@ -25,6 +28,7 @@ enum AttrValue {
   Regex(String),
 }
 
+#[derive(Debug, PartialEq)]
 enum SelPart {
   Wildcard,
   Elem(u8),
@@ -36,20 +40,30 @@ enum SelPart {
   NthChild,
 }
 
-type Selector = Vec<SelPart>;
+#[derive(Debug, PartialEq)]
+enum OpValue {
+  Plus,
+  Tilde,
+  Equal,
+  NotEqual,
+  Greater,
+  GreaterThan,
+  Less,
+  LessThan,
+}
 
+#[derive(Debug, PartialEq)]
 enum Token {
-  Eof,
-  Word,
+  Word(String),
   Space,
-  Op,
+  Op(OpValue),
   Colon,
   Comma,
   BraceOpen,
   BraceClose,
   BracketOpen,
   BracketClose,
-  String,
+  String(String),
   Number,
   Bool,
   Null,
@@ -58,101 +72,294 @@ enum Token {
   Minus,
 }
 
+enum ParseError {
+  UnexpectedEnd(usize),
+}
+
 struct Lexer<'a> {
   i: usize,
-  input: String,
-  len: usize,
-  iter: CharIndices<'a>,
-  token: Token,
-  start: usize,
-  end: usize,
+  input: &'a str,
+  iter: Peekable<Chars<'a>>,
 }
 
 impl<'a> Lexer<'a> {
-  fn next(&mut self) {
-    while let Some((i, ch)) = self.iter.next() {
+  fn new(input: &'a str) -> Self {
+    Self {
+      i: 0,
+      input,
+      iter: input.chars().peekable(),
+    }
+  }
+
+  fn step(&mut self) -> Option<char> {
+    self.i += 1;
+    self.iter.next()
+  }
+
+  fn skip_whitespace(&mut self, ch: char) -> char {
+    while let Some(ch) = self.iter.peek() {
+      if !is_whitespace(&ch) {
+        return *ch;
+      }
+
+      self.step();
+    }
+
+    return ch;
+  }
+
+  fn next_token(&mut self) -> Option<Token> {
+    while let Some(next) = self.step() {
+      let mut ch = next;
+
       match ch {
         ' ' => {
-          while is_whitespace(&ch) {
-            if let Some((j, next_ch)) = self.iter.next() {}
-          }
+          ch = self.skip_whitespace(ch);
 
           if is_op_continue(&ch) {
             continue;
           }
 
-          self.token = Token::Space;
-          return;
+          return Some(Token::Space);
         }
         '[' => {
-          self.token = Token::BracketOpen;
-          self.iter.next();
-          return;
+          return Some(Token::BracketOpen);
         }
         ']' => {
-          self.token = Token::BracketClose;
-          self.iter.next();
-          return;
+          return Some(Token::BracketClose);
         }
         '(' => {
-          self.token = Token::BraceOpen;
-          self.iter.next();
-          return;
+          return Some(Token::BraceOpen);
         }
         ')' => {
-          self.token = Token::BraceClose;
-          self.iter.next();
-          return;
+          return Some(Token::BraceClose);
         }
         ',' => {
-          self.token = Token::Comma;
-          self.iter.next();
-          return;
+          self.skip_whitespace(ch);
+          return Some(Token::Comma);
         }
         '.' => {
-          self.token = Token::Dot;
-          self.iter.next();
-          return;
+          return Some(Token::Dot);
+        }
+        ':' => {
+          return Some(Token::Colon);
         }
         '_' => {
-          self.token = Token::Minus;
-          self.iter.next();
-          return;
+          return Some(Token::Minus);
         }
-        '+' | '~' | '>' | '<' | '=' | '!' => {
-          self.token = Token::Op;
-          self.start = i;
+        '+' => {
+          return Some(Token::Op(OpValue::Plus));
+        }
+        '~' => {
+          return Some(Token::Op(OpValue::Tilde));
+        }
+        '=' => {
+          return Some(Token::Op(OpValue::Equal));
+        }
+        '>' => {
+          if let Some(next) = self.iter.peek() {
+            if *next == '=' {
+              let ch = self.step().unwrap();
+              self.skip_whitespace(ch);
+              return Some(Token::Op(OpValue::GreaterThan));
+            }
+          }
 
-          self.iter.next();
-          //
+          self.skip_whitespace(ch);
+          return Some(Token::Op(OpValue::Greater));
         }
+        '<' => {
+          if let Some(next) = self.iter.peek() {
+            if *next == '=' {
+              let ch = self.step().unwrap();
+              self.skip_whitespace(ch);
+              return Some(Token::Op(OpValue::LessThan));
+            }
+          }
+
+          self.skip_whitespace(ch);
+          return Some(Token::Op(OpValue::Less));
+        }
+        '!' => {
+          if let Some(next) = self.iter.peek() {
+            if *next == '=' {
+              let ch = self.step().unwrap();
+              self.skip_whitespace(ch);
+              return Some(Token::Op(OpValue::NotEqual));
+            }
+          }
+
+          return None;
+        }
+        '\'' | '"' => {
+          let start_ch = ch;
+
+          let start = self.i;
+          self.step();
+
+          while let Some(next) = self.iter.peek() {
+            if *next == start_ch {
+              break;
+            }
+
+            self.step();
+          }
+
+          let s = self.input[start..self.i].to_string();
+          // TODO: Parse error
+
+          self.step();
+          return Some(Token::String(s));
+        }
+
         _ => {
-          // TODO
+          let start = self.i - 1;
+
+          while let Some(next) = self.iter.peek() {
+            if !is_word_continue(&next) {
+              break;
+            }
+
+            self.step();
+          }
+
+          let s = self.input[start..self.i].to_string();
+          return Some(Token::Word(s));
         }
       }
     }
 
-    self.token = Token::Eof;
+    None
+  }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+  type Item = Token;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next_token()
   }
 }
 
 fn is_word_continue(ch: &char) -> bool {
-  match ch {
-    '-' | '_' | 'a'..='z' | 'A'..='Z' | '0'..='9' => true,
-    _ => false,
-  }
+  matches!( ch,
+    '-' | '_' | 'a'..='z' | 'A'..='Z' | '0'..='9')
 }
 
 fn is_op_continue(ch: &char) -> bool {
-  match ch {
-    '=' | '!' | '>' | '<' | '~' | '+' => true,
-    _ => false,
-  }
+  matches!(ch, '=' | '!' | '>' | '<' | '~' | '+')
 }
 
 fn is_whitespace(ch: &char) -> bool {
-  match ch {
-    ' ' | '\t' => true,
-    _ => false,
+  matches!(ch, ' ' | '\t')
+}
+
+#[cfg(test)]
+mod test {
+  use super::{Lexer, OpValue, Token};
+
+  fn test_lex(s: &str) -> Vec<Token> {
+    let lex = Lexer::new(s);
+    lex.into_iter().collect::<Vec<_>>()
+  }
+
+  #[test]
+  fn lex_space() {
+    let result = test_lex("foo bar");
+
+    assert_eq!(
+      result,
+      vec![
+        Token::Word("foo".to_string()),
+        Token::Space,
+        Token::Word("bar".to_string())
+      ]
+    );
+
+    let result = test_lex("foo  bar");
+
+    assert_eq!(
+      result,
+      vec![
+        Token::Word("foo".to_string()),
+        Token::Space,
+        Token::Word("bar".to_string())
+      ]
+    );
+  }
+
+  #[test]
+  fn lex_child() {
+    let result = test_lex("foo>bar");
+
+    assert_eq!(
+      result,
+      vec![
+        Token::Word("foo".to_string()),
+        Token::Op(OpValue::Greater),
+        Token::Word("bar".to_string())
+      ]
+    );
+
+    let result = test_lex("foo  >  bar");
+
+    assert_eq!(
+      result,
+      vec![
+        Token::Word("foo".to_string()),
+        Token::Op(OpValue::Greater),
+        Token::Word("bar".to_string())
+      ]
+    );
+  }
+
+  #[test]
+  fn lex_attr() {
+    let result = test_lex("foo[attr]");
+
+    assert_eq!(
+      result,
+      vec![
+        Token::Word("foo".to_string()),
+        Token::BracketOpen,
+        Token::Word("attr".to_string()),
+        Token::BracketClose,
+      ]
+    );
+  }
+
+  #[test]
+  fn lex_attr_value() {
+    let result = test_lex("foo[attr='value']");
+
+    assert_eq!(
+      result,
+      vec![
+        Token::Word("foo".to_string()),
+        Token::BracketOpen,
+        Token::Word("attr".to_string()),
+        Token::Op(OpValue::Equal),
+        Token::String("value".to_string()),
+        Token::BracketClose,
+      ]
+    );
+  }
+
+  #[test]
+  fn lex_attr_pseudo() {
+    let result = test_lex(":has-child(foo, bar)");
+
+    assert_eq!(
+      result,
+      vec![
+        Token::Colon,
+        Token::Word("has-child".to_string()),
+        Token::BraceOpen,
+        Token::Word("foo".to_string()),
+        Token::Comma,
+        Token::Word("bar".to_string()),
+        Token::BraceClose,
+      ]
+    );
   }
 }
