69560422d677c1ed48d4d810ad590a7ed2bec636
Nathan Whitaker <nathan@deno.com>
2024-12-30 16:36:29 -0500

    fix

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -707,7 +707,7 @@ const ci = {
           if: [
             "(matrix.job == 'test' || matrix.job == 'bench') &&",
             "matrix.profile == 'release' && (matrix.use_sysroot ||",
-            "github.repository == 'denoland/deno' || github.repository == 'nathanwhit/deno')",
+            "(github.repository == 'denoland/deno' || github.repository == 'nathanwhit/deno'))",
           ].join("\n"),
           run: [
             // output fs space before and after building
@@ -769,7 +769,7 @@ const ci = {
             "matrix.os == 'linux' &&",
             "matrix.job == 'test' &&",
             "matrix.profile == 'release' &&",
-            "github.repository == 'denoland/deno' || github.repository == 'nathanwhit/deno'",
+            "(github.repository == 'denoland/deno' || github.repository == 'nathanwhit/deno')",
           ].join("\n"),
           run: [
             "cd target/release",
