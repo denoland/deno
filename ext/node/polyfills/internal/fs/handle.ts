a244a6d4412cc8af7e834d4a3450033e418a9085
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-01-08 17:51:31 +0200

    fix formatting

--- ext/node/polyfills/internal/fs/handle.ts
+++ ext/node/polyfills/internal/fs/handle.ts
@@ -14,6 +14,7 @@ import {
   ReadStream,
 } from "node:fs";
 import { core } from "ext:core/mod.js";
+export type { BigIntStats, Stats } from "ext:deno_node/_fs/_fs_stat.ts";
 import {
   BinaryOptionsArgument,
   FileOptionsArgument,
