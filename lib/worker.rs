ea0c6e14f810b84eda82ce22f3c85a0ccc3bbc23
David Sherret <dsherret@gmail.com>
2025-01-14 20:19:48 -0500

    fix

--- lib/worker.rs
+++ lib/worker.rs
@@ -124,7 +124,7 @@ pub fn get_cache_storage_dir() -> PathBuf {
 /// Instead probe for the total memory on the system and use it instead
 /// as a default.
 pub fn create_isolate_create_params() -> Option<v8::CreateParams> {
-  let maybe_mem_info = deno_runtime::sys_info::mem_info();
+  let maybe_mem_info = deno_runtime::deno_os::sys_info::mem_info();
   maybe_mem_info.map(|mem_info| {
     v8::CreateParams::default()
       .heap_limits_from_system_memory(mem_info.total, 0)
