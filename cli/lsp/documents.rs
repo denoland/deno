642c25b1859e584a0f7c755c29b36bd1a4db75ff
David Sherret <dsherret@gmail.com>
2025-01-22 14:36:39 -0500

    update more removed functions

--- cli/lsp/documents.rs
+++ cli/lsp/documents.rs
@@ -1756,10 +1756,11 @@ fn bytes_to_content(
     // we use the dts representation for Wasm modules
     Ok(deno_graph::source::wasm::wasm_module_to_dts(&bytes)?)
   } else {
-    Ok(deno_graph::source::decode_owned_source(
-      specifier,
-      bytes,
-      maybe_charset,
+    let charset = maybe_charset.unwrap_or_else(|| {
+      deno_media_type::encoding::detect_charset(specifier, &bytes)
+    });
+    Ok(deno_media_type::encoding::decode_owned_source(
+      charset, bytes,
     )?)
   }
 }
