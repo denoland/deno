da11f6bb0b9e7af9ae308878acced12693bcc3b5
David Sherret <dsherret@gmail.com>
2025-01-22 14:14:45 -0500

    refactor: use DataUrl from deno_media_type

--- cli/lsp/documents.rs
+++ cli/lsp/documents.rs
@@ -936,7 +936,7 @@ impl FileSystemDocuments {
         file_referrer.cloned(),
       )
     } else if specifier.scheme() == "data" {
-      let source = deno_graph::source::RawDataUrl::parse(specifier)
+      let source = deno_media_type::data_url::RawDataUrl::parse(specifier)
         .ok()?
         .decode()
         .ok()?;
