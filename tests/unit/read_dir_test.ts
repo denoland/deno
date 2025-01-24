4fca06c989c0fe0fabda01137ddabbf6b0b937e3
Kenta Moriuchi <moriken@kimamass.com>
2025-01-24 22:58:06 +0900

    format

--- tests/unit/read_dir_test.ts
+++ tests/unit/read_dir_test.ts
@@ -25,10 +25,13 @@ Deno.test({ permissions: { read: true } }, function readDirSyncSuccess() {
   assertSameContent(files);
 });
 
-Deno.test({ permissions: { read: true } }, function readDirSyncResultHasIteratorHelperMethods() {
-  const iterator = Deno.readDirSync("tests/testdata");
-  assertEquals(typeof iterator.map, "function");
-});
+Deno.test(
+  { permissions: { read: true } },
+  function readDirSyncResultHasIteratorHelperMethods() {
+    const iterator = Deno.readDirSync("tests/testdata");
+    assertEquals(typeof iterator.map, "function");
+  },
+);
 
 Deno.test({ permissions: { read: true } }, function readDirSyncWithUrl() {
   const files = [
