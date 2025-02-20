2e9c497edff545acc9f8270a30e74102b8ebd82a
Nathan Whitaker <nathan@deno.com>
2025-02-20 12:12:15 -0800

    fix otel test flakiness

--- tests/specs/cli/otel_basic/http_metric.ts
+++ tests/specs/cli/otel_basic/http_metric.ts
@@ -1,10 +1,16 @@
+let port;
 const server = Deno.serve(
-  { port: 8080, onListen: () => {} },
+  {
+    port: 0,
+    onListen: (addr) => {
+      port = addr.port;
+    },
+  },
   () => new Response("foo"),
 );
 
 for (let i = 0; i < 3; i++) {
-  await fetch(`http://localhost:8080`);
+  await fetch(`http://localhost:${port}`);
 }
 
 await server.shutdown();
