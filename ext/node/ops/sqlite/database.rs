c1276d8fa08a6f66c1c02c3fcf5a113e7f61f2e6
Divy Srivastava <dj.srivastava23@gmail.com>
2025-02-20 18:34:34 +0530

    fix(ext/node): rewrite SQLite named parameter handing (#28197)
    Allow bare named params and handle invalid param name. Also adds
    `StatementSync#setAllowBareNamedParameters`
    
    Fixes https://github.com/denoland/deno/issues/28183

--- ext/node/ops/sqlite/database.rs
+++ ext/node/ops/sqlite/database.rs
@@ -205,6 +205,7 @@ impl DatabaseSync {
       inner: raw_stmt,
       db: self.conn.clone(),
       use_big_ints: Cell::new(false),
+      allow_bare_named_params: Cell::new(true),
       is_iter_finished: false,
     })
   }
