c2dd93822d39769cf00cae69529c71a8bf76873b
Bartek Iwańczuk <biwanczuk@gmail.com>
2025-02-24 16:14:52 +0100

    fix

--- cli/tools/doc.rs
+++ cli/tools/doc.rs
@@ -12,7 +12,6 @@ use deno_core::anyhow::Context;
 use deno_core::error::AnyError;
 use deno_core::serde_json;
 use deno_doc as doc;
-use deno_doc::html::DocNodeKind;
 use deno_doc::html::UrlResolveKind;
 use deno_doc::html::UsageComposer;
 use deno_doc::html::UsageComposerEntry;
@@ -517,7 +516,9 @@ fn print_docs_to_stdout(
   doc_flags: DocFlags,
   mut doc_nodes: Vec<deno_doc::DocNode>,
 ) -> Result<(), AnyError> {
-  doc_nodes.retain(|doc_node| doc_node.kind() != doc::DocNodeKind::Import);
+  doc_nodes.retain(|doc_node| {
+    matches!(doc_node.def, doc::node::DocNodeDef::Import { .. })
+  });
   let details = if let Some(filter) = doc_flags.filter {
     let nodes = doc::find_nodes_by_name_recursively(doc_nodes, &filter);
     if nodes.is_empty() {
