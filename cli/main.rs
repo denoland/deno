c1743e8e4deb037f7d13f611bb7ac22402be92ea
David Sherret <dsherret@gmail.com>
2025-02-28 10:05:10 -0500

    prevent stack overflow on windows

--- cli/main.rs
+++ cli/main.rs
@@ -206,9 +206,10 @@ async fn run_subcommand(flags: Arc<Flags>) -> Result<i32, AnyError> {
     }
     DenoSubcommand::Run(run_flags) => spawn_subcommand(async move {
       if run_flags.is_stdin() {
-        tools::run::run_from_stdin(flags.clone()).await
+        // these futures are boxed to prevent stack overflows on Windows
+        tools::run::run_from_stdin(flags.clone()).boxed_local().await
       } else if flags.eszip {
-        tools::run::run_eszip(flags, run_flags).await
+        tools::run::run_eszip(flags, run_flags).boxed_local().await
       } else {
         let result = tools::run::run_script(WorkerExecutionMode::Run, flags.clone(), run_flags.watch).await;
         match result {
