a368bf1de5cc5b27dacea2d8df558cb52b10ec9c
Marvin Hagemeister <marvin@deno.com>
2025-03-05 20:31:43 +0100

    fix(unstable): wrong node with shorthand ObjectPattern + AssignPattern

--- cli/tools/lint/ast_buffer/swc.rs
+++ cli/tools/lint/ast_buffer/swc.rs
@@ -1898,24 +1898,23 @@ fn serialize_pat(ctx: &mut TsEsTreeBuilder, pat: &Pat) -> NodeRef {
             )
           }
           ObjectPatProp::Assign(assign_pat_prop) => {
-            let ident = serialize_binding_ident(ctx, &assign_pat_prop.key);
+            let key = serialize_binding_ident(ctx, &assign_pat_prop.key);
+            let mut value = serialize_binding_ident(ctx, &assign_pat_prop.key);
 
             let shorthand = assign_pat_prop.value.is_none();
-            let value = assign_pat_prop.value.as_ref().map_or(
-              // SWC has value as optional with shorthand properties,
-              // but TSESTree expects the value to be a duplicate of
-              // the binding ident.
-              serialize_binding_ident(ctx, &assign_pat_prop.key),
-              |value| serialize_expr(ctx, value),
-            );
+
+            if let Some(assign) = &assign_pat_prop.value {
+              let expr = serialize_expr(ctx, &assign);
+              value = ctx.write_assign_pat(&assign_pat_prop.span, value, expr);
+            }
 
             ctx.write_property(
-              &assign_pat_prop.span,
+              &node.span,
               shorthand,
               false,
               false,
               PropertyKind::Init,
-              ident,
+              key,
               value,
             )
           }
