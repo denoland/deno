f17e3207429653214f790b49e02363ffe083bfaf
Anshul Sinha <anshul1708@gmail.com>
2025-03-08 19:29:44 +0530

    Fix import ordering in database.rs

--- ext/node/ops/sqlite/database.rs
+++ ext/node/ops/sqlite/database.rs
@@ -19,13 +19,12 @@ use rusqlite::ffi as libsqlite3_sys;
 use rusqlite::limits::Limit;
 use serde::Deserialize;
 
-use crate::ops::sqlite::SqliteResultExt;
-
 use super::session::SessionOptions;
 use super::Session;
 use super::SqliteError;
 use super::StatementSync;
 
+use crate::ops::sqlite::SqliteResultExt;
 const SQLITE_DBCONFIG_ENABLE_LOAD_EXTENSION: i32 = 1005;
 const SQLITE_DBCONFIG_ENABLE_ATTACH_WRITE: i32 = 1021;
 
