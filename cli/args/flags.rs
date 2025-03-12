a72ecbc9bcafb0098b3bf4d2c47fd6885350f413
Jo Franchetti <jofranchetti@gmail.com>
2025-03-12 10:05:50 +0000

    making copy a little clearer

--- cli/args/flags.rs
+++ cli/args/flags.rs
@@ -1906,7 +1906,7 @@ Under the hood, it bundles a slimmed down version of the Deno runtime along with
 JavaScript or TypeScript code.
 
 Cross-compiling to different target architectures is supported using the <c>--target</> flag.
-On the first invocation of `deno compile`, Deno will download the appropriate binary and cache it in <c>$DENO_DIR</>.
+On the first invocation of `deno compile`, Deno will download the relevant binary and cache it in <c>$DENO_DIR</>.
 
 <y>Read more:</> <c>https://docs.deno.com/go/compile</>
 "),
