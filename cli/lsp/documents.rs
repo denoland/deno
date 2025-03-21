1f5aba5f6e245b28c4f53d1a3773ab4ab78c33dd
Nayeem Rahman <nayeemrmn99@gmail.com>
2025-03-21 05:39:07 +0000

    fixup! rename

--- cli/lsp/documents.rs
+++ cli/lsp/documents.rs
@@ -561,9 +561,7 @@ impl Documents2 {
 
 #[derive(Debug)]
 pub struct DocumentModule {
-  // TODO(nayeemrmn): Remove, return document alongside this instead!
   pub uri: Arc<Uri>,
-  // TODO(nayeemrmn): Remove, return document alongside this instead!
   pub lsp_version: Option<i32>,
   pub specifier: Arc<Url>,
   pub scope: Option<Arc<Url>>,
