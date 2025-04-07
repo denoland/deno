c51cb2bd0b511e97968c3fc35dbeeb1f346578dd
Divy Srivastava <dj.srivastava23@gmail.com>
2025-04-07 19:53:14 +0530

    docs

--- cli/tsc/dts/lib.deno.ns.d.ts
+++ cli/tsc/dts/lib.deno.ns.d.ts
@@ -6105,6 +6105,7 @@ declare namespace Deno {
      * @default {false}
      */
     allowHost?: boolean;
+    /** Sets the local address where the socket will connect from. */
     localAddress?: string;
   }
 
