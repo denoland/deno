b7b27cf9bc21eb15201116318cf9b128b88e9731
Divy Srivastava <dj.srivastava23@gmail.com>
2025-04-13 22:49:47 +0530

    Rewrite `node:stream`

--- ext/node/polyfills/internal/streams/duplex.mjs
+++
@@ -1,9 +0,0 @@
-// Copyright 2018-2025 the Deno authors. MIT license.
-// Copyright Joyent and Node contributors. All rights reserved. MIT license.
-// deno-lint-ignore-file
-
-import { Duplex } from "ext:deno_node/_stream.mjs";
-const { from, fromWeb, toWeb } = Duplex;
-
-export default Duplex;
-export { from, fromWeb, toWeb };
