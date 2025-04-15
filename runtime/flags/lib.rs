c632d84a38e653033477efe2a44408583f059dfc
Bartek Iwańczuk <biwanczuk@gmail.com>
2025-04-15 22:48:02 +0200

    generate in the same directory, some adjustments

--- runtime/flags/lib.rs
+++ runtime/flags/lib.rs
@@ -1,6 +1,8 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
-include!(concat!(env!("OUT_DIR"), "/rust_list.rs"));
+mod rust_list;
+
+pub use rust_list::UNSTABLE_FLAGS;
 
 pub const JS_SOURCE: deno_core::FastStaticString =
-  deno_core::ascii_str_include!(concat!(env!("OUT_DIR"), "/js_list.js"));
+  deno_core::ascii_str_include!("./js_list.js");
