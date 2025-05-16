46fbd0eafa8b859441d7005e12340bb3200c6455
Bartek Iwańczuk <biwanczuk@gmail.com>
2025-05-16 02:44:34 +0200

    feat: Use a single 'performance' global

--- ext/node/polyfills/perf_hooks.js
+++ ext/node/polyfills/perf_hooks.js
@@ -4,10 +4,7 @@
 // deno-lint-ignore-file prefer-primordials
 
 import { notImplemented } from "ext:deno_node/_utils.ts";
-import {
-  performance as shimPerformance,
-  PerformanceEntry,
-} from "ext:deno_web/15_performance.js";
+import { performance, PerformanceEntry } from "ext:deno_web/15_performance.js";
 import { EldHistogram } from "ext:core/ops";
 
 class PerformanceObserver {
@@ -22,47 +19,19 @@ class PerformanceObserver {
 
 const constants = {};
 
-const performance = {
-  clearMarks: (markName) => shimPerformance.clearMarks(markName),
-  eventLoopUtilization: () => {
-    // TODO(@marvinhagemeister): Return actual non-stubbed values
-    return { idle: 0, active: 0, utilization: 0 };
-  },
-  mark: (markName) => shimPerformance.mark(markName),
-  measure: (
-    measureName,
-    startMark,
-    endMark,
-  ) => {
-    if (endMark) {
-      return shimPerformance.measure(
-        measureName,
-        startMark,
-        endMark,
-      );
-    } else {
-      return shimPerformance.measure(
-        measureName,
-        startMark,
-      );
-    }
-  },
-  nodeTiming: {},
-  now: () => shimPerformance.now(),
-  timerify: () => notImplemented("timerify from performance"),
-  get timeOrigin() {
-    return shimPerformance.timeOrigin;
-  },
-  getEntriesByName: (name, type) =>
-    shimPerformance.getEntriesByName(name, type),
-  getEntriesByType: (type) => shimPerformance.getEntriesByType(type),
-  markResourceTiming: () => {},
-  toJSON: () => shimPerformance.toJSON(),
-  addEventListener: (...args) => shimPerformance.addEventListener(...args),
-  removeEventListener: (...args) =>
-    shimPerformance.removeEventListener(...args),
-  dispatchEvent: (...args) => shimPerformance.dispatchEvent(...args),
+performance.eventLoopUtilization = () => {
+  // TODO(@marvinhagemeister): Return actual non-stubbed values
+  return { idle: 0, active: 0, utilization: 0 };
 };
+
+// TODO(bartlomieju):
+performance.nodeTiming = {};
+
+// TODO(bartlomieju):
+performance.timerify = () => notImplemented("timerify from performance");
+
+// TODO(bartlomieju):
+performance.timerify = () => {};
 
 function monitorEventLoopDelay(options = {}) {
   const { resolution = 10 } = options;
