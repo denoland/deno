ef09dbd725963e4f6eb9f526d9184780d0a97aa1
Yoshiya Hinosawa <stibium121@gmail.com>
2025-05-21 11:01:17 +0900

    fix test case of subprocess coverage

--- tests/specs/run/run_coverage/child_process_coverage_test.ts
+++ tests/specs/run/run_coverage/child_process_coverage_test.ts
@@ -1,5 +1,5 @@
-Deno.test("foo 1 0", () => runDenoCommand("run --coverage foo.ts 1 0"));
-Deno.test("foo 0 1", () => runDenoCommand("run --coverage foo.ts 0 1"));
+Deno.test("foo 1 0", () => runDenoCommand("run foo.ts 1 0"));
+Deno.test("foo 0 1", () => runDenoCommand("run foo.ts 0 1"));
 
 async function runDenoCommand(args: string) {
   await new Deno.Command(Deno.execPath(), { args: args.split(" ") }).output();
