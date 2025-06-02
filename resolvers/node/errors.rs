6b00b58d041f3a7443a148c194ffd58e1c3655a5
David Sherret <dsherret@users.noreply.github.com>
2025-06-02 18:53:34 -0400

    refactor(node): improve `ResolutionMode::Require` resolution (#29551)
    Co-authored-by: nathanwhit <nathanwhit@users.noreply.github.com>

--- resolvers/node/errors.rs
+++ resolvers/node/errors.rs
@@ -647,6 +647,9 @@ pub enum FinalizeResolutionErrorKind {
   ModuleNotFound(#[from] ModuleNotFoundError),
   #[class(inherit)]
   #[error(transparent)]
+  TypesNotFound(#[from] TypesNotFoundError),
+  #[class(inherit)]
+  #[error(transparent)]
   UnsupportedDirImport(#[from] UnsupportedDirImportError),
   #[class(inherit)]
   #[error(transparent)]
@@ -659,6 +662,7 @@ impl NodeJsErrorCoded for FinalizeResolutionErrorKind {
     match self {
       FinalizeResolutionErrorKind::InvalidModuleSpecifierError(e) => e.code(),
       FinalizeResolutionErrorKind::ModuleNotFound(e) => e.code(),
+      FinalizeResolutionErrorKind::TypesNotFound(e) => e.code(),
       FinalizeResolutionErrorKind::UnsupportedDirImport(e) => e.code(),
       FinalizeResolutionErrorKind::UrlToFilePath(_) => {
         NodeJsErrorCode::ERR_INVALID_FILE_URL_PATH
@@ -670,9 +674,8 @@ impl NodeJsErrorCoded for FinalizeResolutionErrorKind {
 #[derive(Debug, Error, JsError)]
 #[class(generic)]
 #[error(
-  "[{}] Cannot find {} '{}'{}{}",
+  "[{}] Cannot find module '{}'{}{}",
   self.code(),
-  typ,
   specifier,
   maybe_referrer.as_ref().map(|referrer| format!(" imported from '{}'", referrer)).unwrap_or_default(),
   suggested_ext.as_ref().map(|m| format!("\nDid you mean to import with the \".{}\" extension?", m)).unwrap_or_default()
@@ -681,7 +684,6 @@ impl NodeJsErrorCoded for FinalizeResolutionErrorKind {
 pub struct ModuleNotFoundError {
   pub specifier: UrlOrPath,
   pub maybe_referrer: Option<UrlOrPath>,
-  pub typ: &'static str,
   pub suggested_ext: Option<&'static str>,
 }
 
