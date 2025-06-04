60e30018f10fa069650bd335881a1fb37a031d6f
Jo Franchetti <jofranchetti@gmail.com>
2025-06-04 09:48:34 +0100

    updating docs

--- cli/tsc/dts/lib.webworker.d.ts
+++ cli/tsc/dts/lib.webworker.d.ts
@@ -660,7 +660,12 @@ interface RTCEncodedVideoFrameMetadata {
  * @category Streams
  */
 interface ReadableStreamGetReaderOptions {
-  mode?: ReadableStreamReaderMode;
+    /**
+     * Creates a ReadableStreamBYOBReader and locks the stream to the new reader.
+     *
+     * This call behaves the same way as the no-argument variant, except that it only works on readable byte streams, i.e. streams which were constructed specifically with the ability to handle "bring your own buffer" reading. The returned BYOB reader provides the ability to directly read individual chunks from the stream via its read() method, into developer-supplied buffers, allowing more precise control over allocation.
+     */
+    mode?: ReadableStreamReaderMode;
 }
 
 interface ReadableStreamIteratorOptions {
