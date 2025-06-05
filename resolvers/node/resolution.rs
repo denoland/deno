18e599bc68612977e0c0260b30b3b313ea2f2ed7
Nayeem Rahman <nayeemrmn99@gmail.com>
2025-06-05 23:55:57 +0100

    refactor(lsp): remove resolution lookup store from node resolver

--- resolvers/node/resolution.rs
+++ resolvers/node/resolution.rs
@@ -277,7 +277,6 @@ pub struct NodeResolver<
   sys: NodeResolutionSys<TSys>,
   condition_resolver: ConditionResolver,
   typescript_version: Option<Version>,
-  package_resolution_lookup_cache: Option<DashMap<Url, String>>,
 }
 
 impl<
@@ -309,14 +308,6 @@ impl<
       sys,
       condition_resolver: ConditionResolver::new(options.conditions),
       typescript_version: options.typescript_version,
-      package_resolution_lookup_cache: None,
-    }
-  }
-
-  pub fn with_package_resolution_lookup_cache(self) -> Self {
-    Self {
-      package_resolution_lookup_cache: Some(Default::default()),
-      ..self
     }
   }
 
@@ -347,9 +338,7 @@ impl<
       return Ok(NodeResolution::BuiltIn(specifier.to_string()));
     }
 
-    let mut specifier_is_url = false;
     if let Ok(url) = Url::parse(specifier) {
-      specifier_is_url = true;
       if url.scheme() == "data" {
         return Ok(NodeResolution::Module(UrlOrPath::Url(url)));
       }
@@ -397,21 +386,6 @@ impl<
       resolution_kind,
       Some(&referrer),
     )?;
-    let maybe_cache_resolution = || {
-      let package_resolution_lookup_cache =
-        self.package_resolution_lookup_cache.as_ref()?;
-      if specifier_is_url
-        || specifier.starts_with("./")
-        || specifier.starts_with("../")
-        || specifier.starts_with("/")
-      {
-        return None;
-      }
-      let url = url_or_path.clone().into_url().ok()?;
-      package_resolution_lookup_cache.insert(url, specifier.to_string());
-      Some(())
-    };
-    maybe_cache_resolution();
     let resolve_response = NodeResolution::Module(url_or_path);
     // TODO(bartlomieju): skipped checking errors for commonJS resolution and
     // "preserveSymlinksMain"/"preserveSymlinks" options.
@@ -615,17 +589,6 @@ impl<
         )
       }
     }
-  }
-
-  pub fn lookup_package_specifier_for_resolution(
-    &self,
-    url: &Url,
-  ) -> Option<String> {
-    self
-      .package_resolution_lookup_cache
-      .as_ref()?
-      .get(url)
-      .map(|r| r.value().clone())
   }
 
   fn module_not_found_ext_suggestion(
