86143bf2221aa8ef86b5d98d100fe550561f7b66
Yoshiya Hinosawa <stibium121@gmail.com>
2025-06-06 15:32:24 +0900

    fix wrong import of primordials

--- ext/node/polyfills/internal/events/abort_listener.mjs
+++ ext/node/polyfills/internal/events/abort_listener.mjs
@@ -1,7 +1,7 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 // Copyright Joyent, Inc. and Node.js contributors. All rights reserved. MIT license.
 
-import { primordials } from "ext:deno_node/internal/test/binding.ts";
+import { primordials } from "ext:core/mod.js";
 const { queueMicrotask } = primordials;
 import { SymbolDispose } from "ext:deno_web/00_infra.js";
 import * as abortSignal from "ext:deno_web/03_abort_signal.js";
