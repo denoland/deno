05b25a48ff046dcc0dc1bbceb729edce16615d0b
Nathan Whitaker <nathan@deno.com>
2025-06-12 16:49:18 -0700

    simplify test

--- tests/specs/bundle/sourcemap/main.ts
+++ tests/specs/bundle/sourcemap/main.ts
@@ -1,14 +1,1 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
-
-function multiply(x: number, y: number): number {
-  return x * y;
-}
-
-const result1 = add(2, 3);
-const result2 = multiply(4, 5);
-
-console.log("Addition result:", result1);
-console.log("Multiplication result:", result2);
-throw new Error("test");
+console.log("test");
