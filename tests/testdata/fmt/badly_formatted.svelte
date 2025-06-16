1d9dec525c8780f1393cb7ffe07db95a6dd0be07
Janosh Riebesell <janosh.riebesell@gmail.com>
2025-06-16 08:46:29 -0400

    revert accidental changes in badly_formatted.svelte

--- tests/testdata/fmt/badly_formatted.svelte
+++ tests/testdata/fmt/badly_formatted.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-  let a: number;
-  let b: number;
-</script>
-
-<div class:active style="width: {  a  +  b }px; height: 1px">
-  {a + b}
-</div>
-
-<style>
-  .active {}
-</style>
+  let a:number
+  let b  :   number
+  </script>
+  
+  <div class:active={ active } style=" width : {  a  +  b }PX; height:1PX; " > {a+b} </div>
+  
+  <style>
+  .active{}
+  </style>
+  
