c9de072b618c155f729f05c6ae105af4f9a525c9
Divy Srivastava <dj.srivastava23@gmail.com>
2025-06-15 19:48:20 -0700

    fix(ext/node): throw invalid state from `getAuthTag` (#29752)

--- ext/node/polyfills/internal/crypto/cipher.ts
+++ ext/node/polyfills/internal/crypto/cipher.ts
@@ -47,6 +47,7 @@ import {
   isAnyArrayBuffer,
   isArrayBufferView,
 } from "ext:deno_node/internal/util/types.ts";
+import { ERR_CRYPTO_INVALID_STATE } from "ext:deno_node/internal/errors.ts";
 
 const FastBuffer = Buffer[SymbolSpecies];
 
@@ -226,7 +227,10 @@ export class Cipheriv extends Transform implements Cipher {
   }
 
   getAuthTag(): Buffer {
-    return this.#authTag!;
+    if (!this.#authTag) {
+      throw new ERR_CRYPTO_INVALID_STATE("getAuthTag");
+    }
+    return this.#authTag;
   }
 
   setAAD(
