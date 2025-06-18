94e26a90ea2f04d2fcf30c3a6229e4e8050cb9ec
Yoshiya Hinosawa <stibium121@gmail.com>
2025-06-18 09:50:37 +0900

    fix(ext/node): improve scrypt support (#29773)

--- ext/node/ops/crypto/mod.rs
+++ ext/node/ops/crypto/mod.rs
@@ -644,7 +644,7 @@ fn scrypt(
     parallelization,
     keylen as usize,
   )
-  .map_err(|_| JsErrorBox::generic("scrypt params construction failed"))?;
+  .map_err(|_| JsErrorBox::generic("Invalid scrypt param"))?;
 
   // Call into scrypt
   let res = scrypt::scrypt(&password, &salt, &params, output_buffer);
