b3921ae00e1544bb56aab2b4344f9a1b522b0fec
Daniel Osvaldo Rahmanto <daniel.rahmanto@gmail.com>
2025-06-22 21:45:17 +0700

    fix: use primordials for promise reject
    Also adds `getValidatedPathToString` util function.

--- ext/node/polyfills/_fs/_fs_lchmod.ts
+++ ext/node/polyfills/_fs/_fs_lchmod.ts
@@ -1,28 +1,26 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
-// deno-lint-ignore-file prefer-primordials
-
 import { type Buffer } from "node:buffer";
 import {
   type CallbackWithError,
   makeCallback,
 } from "ext:deno_node/_fs/_fs_common.ts";
 import { ERR_METHOD_NOT_IMPLEMENTED } from "ext:deno_node/internal/errors.ts";
-import { getValidatedPath } from "ext:deno_node/internal/fs/utils.mjs";
+import { getValidatedPathToString } from "ext:deno_node/internal/fs/utils.mjs";
 import { isMacOS } from "ext:deno_node/_util/os.ts";
 import { op_node_lchmod, op_node_lchmod_sync } from "ext:core/ops";
 import { parseFileMode } from "ext:deno_node/internal/validators.mjs";
 import { primordials } from "ext:core/mod.js";
 import { promisify } from "ext:deno_node/internal/util.mjs";
 
-const { PromisePrototypeThen } = primordials;
+const { PromisePrototypeThen, PromiseReject } = primordials;
 
 export const lchmod = !isMacOS ? undefined : (
   path: string | Buffer | URL,
   mode: number,
   callback: CallbackWithError,
 ) => {
-  path = getValidatedPath(path).toString();
+  path = getValidatedPathToString(path);
   mode = parseFileMode(mode, "mode");
   callback = makeCallback(callback);
 
@@ -34,7 +32,7 @@ export const lchmod = !isMacOS ? undefined : (
 };
 
 export const lchmodPromise = !isMacOS
-  ? () => Promise.reject(new ERR_METHOD_NOT_IMPLEMENTED("lchmod()"))
+  ? () => PromiseReject(new ERR_METHOD_NOT_IMPLEMENTED("lchmod()"))
   : promisify(lchmod) as (
     path: string | Buffer | URL,
     mode: number,
@@ -43,7 +41,7 @@ export const lchmodPromise = !isMacOS
 export const lchmodSync = !isMacOS
   ? undefined
   : (path: string | Buffer | URL, mode: number) => {
-    path = getValidatedPath(path).toString();
+    path = getValidatedPathToString(path);
     mode = parseFileMode(mode, "mode");
     return op_node_lchmod_sync(path, mode);
   };
