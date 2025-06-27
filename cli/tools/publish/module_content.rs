b65bfa2c4c385ecf3c75e679b9cd94ecd54bf971
Nayeem Rahman <nayeemrmn99@gmail.com>
2025-06-27 21:12:15 +0100

    fix

--- cli/tools/publish/module_content.rs
+++ cli/tools/publish/module_content.rs
@@ -284,7 +284,8 @@ mod test {
 
   use deno_config::workspace::WorkspaceDiscoverStart;
   use deno_path_util::url_from_file_path;
-  use deno_resolver::factory::WorkspaceDirectoryProvider;
+  use deno_resolver::factory::ConfigDiscoveryOption;
+use deno_resolver::factory::WorkspaceDirectoryProvider;
   use deno_resolver::npm::ByonmNpmResolverCreateOptions;
   use deno_resolver::npm::CreateInNpmPkgCheckerOptions;
   use deno_resolver::npm::DenoInNpmPackageChecker;
@@ -435,6 +436,7 @@ mod test {
       &sys,
       &WorkspaceDirectoryProvider::from_initial_dir(&Arc::new(workspace_dir)),
       &node_resolver,
+      &ConfigDiscoveryOption::DiscoverCwd,
     ));
     ModuleContentProvider::new(
       Arc::new(ParsedSourceCache::default()),
