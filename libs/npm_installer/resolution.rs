1c3d4667b56b49b27a0d8c3305e825fdca232592
David Sherret <dsherret@gmail.com>
2025-08-04 17:20:54 +0200

    fix: make unmet peer dep error message less verbose

--- libs/npm_installer/resolution.rs
+++ libs/npm_installer/resolution.rs
@@ -1,5 +1,9 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::rc::Rc;
 use std::sync::Arc;
 
 use capacity_builder::StringBuilder;
@@ -14,6 +18,7 @@ use deno_npm::resolution::AddPkgReqsOptions;
 use deno_npm::resolution::DefaultTarballUrlProvider;
 use deno_npm::resolution::NpmResolutionError;
 use deno_npm::resolution::NpmResolutionSnapshot;
+use deno_npm::resolution::UnmetPeerDepDiagnostic;
 use deno_npm_cache::NpmCacheHttpClient;
 use deno_npm_cache::NpmCacheSys;
 use deno_npm_cache::RegistryInfoProvider;
@@ -172,32 +177,8 @@ impl<TNpmCacheHttpClient: NpmCacheHttpClient, TSys: NpmResolutionInstallerSys>
     if !result.unmet_peer_diagnostics.is_empty()
       && log::log_enabled!(log::Level::Warn)
     {
-      let root_node = DisplayTreeNode {
-        text: format!(
-          "{} The following peer dependency issues were found:",
-          colors::yellow("Warning")
-        ),
-        children: result
-          .unmet_peer_diagnostics
-          .iter()
-          .map(|diagnostic| {
-            let mut node = DisplayTreeNode {
-              text: format!(
-                "peer {}: resolved to {}",
-                diagnostic.dependency, diagnostic.resolved
-              ),
-              children: Vec::new(),
-            };
-            for ancestor in &diagnostic.ancestors {
-              node = DisplayTreeNode {
-                text: ancestor.to_string(),
-                children: vec![node],
-              };
-            }
-            node
-          })
-          .collect(),
-      };
+      let root_node =
+        peer_dep_diagnostics_to_display_tree(&result.unmet_peer_diagnostics);
       let mut text = String::new();
       _ = root_node.print(&mut text);
       log::warn!("{}", text);
@@ -301,4 +282,82 @@ impl<TNpmCacheHttpClient: NpmCacheHttpClient, TSys: NpmResolutionInstallerSys>
       lockfile.insert_npm_package(npm_package_to_lockfile_info(package));
     }
   }
+}
+
+fn peer_dep_diagnostics_to_display_tree(
+  diagnostics: &[UnmetPeerDepDiagnostic],
+) -> DisplayTreeNode {
+  // combine the nodes into a unified tree
+  struct UnifiedNode {
+    text: Rc<String>,
+    children: RefCell<Vec<Rc<UnifiedNode>>>,
+  }
+
+  let mut nodes: BTreeMap<Rc<String>, Rc<UnifiedNode>> = BTreeMap::new();
+  let mut top_level_nodes = Vec::new();
+
+  for diagnostic in diagnostics {
+    let text = Rc::new(format!(
+      "peer {}: resolved to {}",
+      diagnostic.dependency, diagnostic.resolved
+    ));
+    let mut node = Rc::new(UnifiedNode {
+      text: text.clone(),
+      children: Default::default(),
+    });
+    let mut skip_push = false;
+    for ancestor in &diagnostic.ancestors {
+      let nv_string = Rc::new(ancestor.to_string());
+      if let Some(current_node) = nodes.get(&nv_string) {
+        {
+          let mut children = current_node.children.borrow_mut();
+          if let Err(insert_index) =
+            children.binary_search_by(|n| n.text.cmp(&node.text))
+          {
+            children.insert(insert_index, node);
+          }
+        }
+        node = current_node.clone();
+        skip_push = true;
+        break;
+      } else {
+        let current_node = Rc::new(UnifiedNode {
+          text: nv_string.clone(),
+          children: RefCell::new(vec![node]),
+        });
+        nodes.insert(nv_string.clone(), current_node.clone());
+        node = current_node;
+      }
+    }
+    if !skip_push {
+      top_level_nodes.push(node);
+    }
+  }
+
+  // now output the unified tree
+  let mut root_node = DisplayTreeNode {
+    text: format!(
+      "{} The following peer dependency issues were found:",
+      colors::yellow("Warning")
+    ),
+    children: Vec::new(),
+  };
+
+  fn convert_node(node: &Rc<UnifiedNode>) -> DisplayTreeNode {
+    DisplayTreeNode {
+      text: node.text.to_string(),
+      children: node
+        .children
+        .borrow()
+        .iter()
+        .map(|child| convert_node(child))
+        .collect(),
+    }
+  }
+
+  for top_level_node in top_level_nodes {
+    root_node.children.push(convert_node(&top_level_node));
+  }
+
+  root_node
 }
