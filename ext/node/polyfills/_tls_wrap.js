760ed9cdca6fe7be1ba28422dcb547f377047aad
Divy Srivastava <dj.srivastava23@gmail.com>
2025-08-20 18:16:19 +0530

    stuff

--- ext/node/polyfills/_tls_wrap.js
+++ ext/node/polyfills/_tls_wrap.js
@@ -227,8 +227,7 @@ export class TLSSocket extends net.Socket {
 
           tlssock.emit("secure");
           tlssock.removeListener("end", onConnectEnd);
-        } catch (e) {
-          console.log(e);
+        } catch {
           // TODO(kt3k): Handle this
         }
       };
