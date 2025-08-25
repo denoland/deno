1b59cb3138fb5e571537931f3881c2a7f5f74183
Divy Srivastava <dj.srivastava23@gmail.com>
2025-08-25 20:34:20 +0530

    fix(ext/node): handle `null` keypair in tls connect

--- ext/node/polyfills/_tls_wrap.js
+++ ext/node/polyfills/_tls_wrap.js
@@ -87,8 +87,8 @@ export class TLSSocket extends net.Socket {
 
     const cert = tlsOptions?.secureContext?.cert;
     const key = tlsOptions?.secureContext?.key;
-    const hasTlsKey = key !== undefined &&
-      cert !== undefined;
+    const hasTlsKey = key != undefined &&
+      cert != undefined;
     const keyPair = hasTlsKey
       ? op_tls_key_static(cert, key)
       : op_tls_key_null();
