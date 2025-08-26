1ba8bbfcea3abe19ca65d53b10da65e2c18a93ad
Daniel Rahmanto <daniel.rahmanto@gmail.com>
2025-08-26 08:45:07 +0700

    fix `exists` on vfs

--- cli/rt/file_system.rs
+++ cli/rt/file_system.rs
@@ -208,14 +208,18 @@ impl FileSystem for DenoRtSys {
   }
 
   fn exists_sync(&self, path: &CheckedPath) -> bool {
-    if self.error_if_in_vfs(path).is_err() {
-      return false;
+    if self.0.is_path_within(path) {
+      self.0.exists(path)
+    } else {
+      RealFs.exists_sync(path)
     }
-    RealFs.exists_sync(path)
   }
   async fn exists_async(&self, path: CheckedPathBuf) -> FsResult<bool> {
-    self.error_if_in_vfs(&path)?;
-    RealFs.exists_async(path).await
+    if self.0.is_path_within(&path) {
+      Ok(self.0.exists(&path))
+    } else {
+      RealFs.exists_async(path).await
+    }
   }
 
   fn lchmod_sync(&self, path: &CheckedPath, mode: u32) -> FsResult<()> {
@@ -1487,6 +1491,10 @@ impl FileBackedVfs {
 
   pub fn is_path_within(&self, path: &Path) -> bool {
     path.starts_with(&self.fs_root.root_path)
+  }
+
+  pub fn exists(&self, path: &Path) -> bool {
+    self.fs_root.find_entry(path, self.case_sensitivity).is_ok()
   }
 
   pub fn open_file(
