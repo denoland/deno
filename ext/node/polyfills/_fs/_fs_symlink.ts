704ef248eaf6f2f7b20d16fde91728f881b693dc
Daniel Rahmanto <daniel.rahmanto@gmail.com>
2025-08-28 18:54:55 +0700

    fix(ext/node): `fs.symlink` and `fs.symlinkSync` compatibility

--- ext/node/polyfills/_fs/_fs_symlink.ts
+++ ext/node/polyfills/_fs/_fs_symlink.ts
@@ -2,62 +2,65 @@
 
 import { primordials } from "ext:core/mod.js";
 
-import { CallbackWithError } from "ext:deno_node/_fs/_fs_common.ts";
-import { pathFromURL } from "ext:deno_web/00_infra.js";
+import {
+  CallbackWithError,
+  makeCallback,
+} from "ext:deno_node/_fs/_fs_common.ts";
 import { promisify } from "ext:deno_node/internal/util.mjs";
+import type { Buffer } from "node:buffer";
+import { validateOneOf } from "ext:deno_node/internal/validators.mjs";
+import { getValidatedPathToString } from "ext:deno_node/internal/fs/utils.mjs";
 
-const {
-  Error,
-  ObjectPrototypeIsPrototypeOf,
-  PromisePrototypeThen,
-} = primordials;
+const { PromisePrototypeThen } = primordials;
 
-type SymlinkType = "file" | "dir" | "junction";
+export type SymlinkType = "file" | "dir" | "junction";
 
 export function symlink(
-  target: string | URL,
-  path: string | URL,
-  typeOrCallback: SymlinkType | CallbackWithError,
-  maybeCallback?: CallbackWithError,
+  target: string | Buffer | URL,
+  path: string | Buffer | URL,
+  type?: SymlinkType | CallbackWithError,
+  callback?: CallbackWithError,
 ) {
-  target = ObjectPrototypeIsPrototypeOf(URL, target)
-    ? pathFromURL(target)
-    : target;
-  path = ObjectPrototypeIsPrototypeOf(URL, path) ? pathFromURL(path) : path;
-
-  const type: SymlinkType = typeof typeOrCallback === "string"
-    ? typeOrCallback
-    : "file";
-
-  const callback: CallbackWithError = typeof typeOrCallback === "function"
-    ? typeOrCallback
-    : (maybeCallback as CallbackWithError);
-
-  if (!callback) throw new Error("No callback function supplied");
+  if (callback === undefined) {
+    callback = makeCallback(type as CallbackWithError);
+    type = undefined;
+  } else {
+    validateOneOf(type, "type", ["dir", "file", "junction", null, undefined]);
+  }
+  target = getValidatedPathToString(target, "target");
+  path = getValidatedPathToString(path);
+  type ??= undefined;
 
   PromisePrototypeThen(
-    Deno.symlink(target, path, { type }),
+    Deno.symlink(
+      target,
+      path,
+      { type },
+    ),
     () => callback(null),
     callback,
   );
 }
 
 export const symlinkPromise = promisify(symlink) as (
-  target: string | URL,
-  path: string | URL,
+  target: string | Buffer | URL,
+  path: string | Buffer | URL,
   type?: SymlinkType,
 ) => Promise<void>;
 
 export function symlinkSync(
-  target: string | URL,
-  path: string | URL,
+  target: string | Buffer | URL,
+  path: string | Buffer | URL,
   type?: SymlinkType,
 ) {
-  target = ObjectPrototypeIsPrototypeOf(URL, target)
-    ? pathFromURL(target)
-    : target;
-  path = ObjectPrototypeIsPrototypeOf(URL, path) ? pathFromURL(path) : path;
-  type = type || "file";
+  validateOneOf(type, "type", ["dir", "file", "junction", null, undefined]);
+  target = getValidatedPathToString(target, "target");
+  path = getValidatedPathToString(path);
+  type ??= undefined;
 
-  Deno.symlinkSync(target, path, { type });
+  Deno.symlinkSync(
+    target,
+    path,
+    { type },
+  );
 }
