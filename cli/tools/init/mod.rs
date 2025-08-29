cdc31cb2e347ae0d00b018c72d99ee0d3a0f5b63
David Sherret <dsherret@gmail.com>
2025-08-29 17:39:19 -0400

    use a temp directory for node_modules folders

--- cli/tools/init/mod.rs
+++ cli/tools/init/mod.rs
@@ -1,9 +1,11 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
+use std::ffi::OsString;
 use std::io::IsTerminal;
 use std::io::Write;
 use std::path::Path;
 
+use chrono::NaiveDate;
 use color_print::cformat;
 use color_print::cstr;
 use deno_config::deno_json::NodeModulesDirMode;
@@ -22,6 +24,8 @@ use crate::args::InternalFlags;
 use crate::args::PermissionFlags;
 use crate::args::RunFlags;
 use crate::colors;
+use crate::util::fs::FsCleaner;
+use crate::util::progress_bar::ProgressBar;
 
 pub async fn init_project(init_flags: InitFlags) -> Result<i32, AnyError> {
   if let Some(package) = &init_flags.package {
@@ -376,14 +380,25 @@ async fn init_npm(name: &str, args: Vec<String>) -> Result<i32, AnyError> {
   result
 }
 
+/// Creates a node_modules directory in a folder with the following format:
+///
+///   <tmp-dir>/deno_init_nm/<date>/<random-value>
+///
+/// Old folders are automatically deleted by this function.
 fn create_temp_node_modules_parent_dir() -> Result<tempfile::TempDir, AnyError>
 {
-  let root_temp_folder = std::env::temp_dir().join("deno_nm");
-  std::fs::create_dir_all(&root_temp_folder).with_context(|| {
-    format!("Failed creating '{}'", root_temp_folder.display())
-  })?;
-  let temp_node_modules_parent_dir =
-    tempfile::TempDir::new_in(&root_temp_folder)?;
+  let root_temp_folder = std::env::temp_dir().join("deno_init_nm");
+  let today = chrono::Utc::now().date_naive();
+  // remove any old/stale temp dirs
+  if let Err(err) =
+    attempt_temp_dir_garbage_collection(&root_temp_folder, &today)
+  {
+    log::debug!("Failed init temp folder garbage collection: {:#?}", err);
+  }
+  let day_folder = root_temp_folder.join(folder_name_for_date(&today));
+  std::fs::create_dir_all(&day_folder)
+    .with_context(|| format!("Failed creating '{}'", day_folder.display()))?;
+  let temp_node_modules_parent_dir = tempfile::TempDir::new_in(&day_folder)?;
   // write a package.json to make this be considered a "node" project to deno
   let package_json_path =
     temp_node_modules_parent_dir.path().join("package.json");
@@ -391,6 +406,53 @@ fn create_temp_node_modules_parent_dir() -> Result<tempfile::TempDir, AnyError>
     format!("Failed creating '{}'", package_json_path.display())
   })?;
   Ok(temp_node_modules_parent_dir)
+}
+
+fn attempt_temp_dir_garbage_collection(
+  root_temp_folder: &Path,
+  utc_now: &NaiveDate,
+) -> Result<(), AnyError> {
+  let previous_day_str = folder_name_for_date(
+    &utc_now
+      .checked_sub_days(chrono::Days::new(1))
+      .unwrap_or_else(|| utc_now.clone()),
+  );
+  let current_day_str = folder_name_for_date(utc_now);
+  let next_day_str = folder_name_for_date(
+    &utc_now
+      .checked_add_days(chrono::Days::new(1))
+      .unwrap_or_else(|| utc_now.clone()),
+  );
+  let progress_bar =
+    ProgressBar::new(crate::util::progress_bar::ProgressBarStyle::TextOnly);
+  let update_guard = progress_bar.deferred_update_with_prompt(
+    crate::util::progress_bar::ProgressMessagePrompt::Cleaning,
+    "old temp node_modules folders...",
+  );
+  let mut cleaner = FsCleaner::new(Some(update_guard));
+
+  for entry in std::fs::read_dir(root_temp_folder)? {
+    let Ok(entry) = entry else {
+      continue;
+    };
+    if entry.file_name() != previous_day_str
+      && entry.file_name() != current_day_str
+      && entry.file_name() != next_day_str
+      && let Err(err) = cleaner.rm_rf(&entry.path())
+    {
+      log::debug!(
+        "Failed cleaning '{}': {:#?}",
+        entry.file_name().display(),
+        err
+      );
+    }
+  }
+
+  Ok(())
+}
+
+fn folder_name_for_date(date: &chrono::NaiveDate) -> OsString {
+  OsString::from(date.format("%Y-%m-%d").to_string())
 }
 
 fn create_json_file(
@@ -428,6 +490,10 @@ fn create_file(
 
 #[cfg(test)]
 mod test {
+
+  use test_util::TempDir;
+
+  use super::attempt_temp_dir_garbage_collection;
   use crate::tools::init::npm_name_to_create_package;
 
   #[test]
@@ -456,6 +522,46 @@ mod test {
     assert_eq!(
       npm_name_to_create_package("@foo/bar@1.0.0"),
       "npm:@foo/create-bar@1.0.0".to_string()
+    );
+  }
+
+  #[test]
+  fn test_attempt_temp_dir_garbage_collection() {
+    let temp_dir = TempDir::new();
+    let reference_date = chrono::NaiveDate::from_ymd_opt(2020, 5, 13).unwrap();
+    temp_dir.path().join("0000-00-00").create_dir_all();
+    temp_dir
+      .path()
+      .join("2020-05-01/sub_dir/sub")
+      .create_dir_all();
+    temp_dir
+      .path()
+      .join("2020-05-01/sub_dir/sub/test.txt")
+      .write("");
+    temp_dir.path().join("2020-05-02/sub_dir").create_dir_all();
+    temp_dir.path().join("2020-05-11").create_dir_all();
+    temp_dir.path().join("2020-05-12").create_dir_all();
+    temp_dir.path().join("2020-05-13").create_dir_all();
+    temp_dir.path().join("2020-05-14").create_dir_all();
+    temp_dir.path().join("2020-05-15").create_dir_all();
+    attempt_temp_dir_garbage_collection(
+      temp_dir.path().as_path(),
+      &reference_date,
+    )
+    .unwrap();
+    let mut entries = std::fs::read_dir(temp_dir.path())
+      .unwrap()
+      .map(|e| e.unwrap().file_name().into_string().unwrap())
+      .collect::<Vec<_>>();
+    entries.sort();
+    // should only have the current day +- 1
+    assert_eq!(
+      entries,
+      vec![
+        "2020-05-12".to_string(),
+        "2020-05-13".to_string(),
+        "2020-05-14".to_string()
+      ]
     );
   }
 }
