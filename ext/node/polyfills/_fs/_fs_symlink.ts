5ed9df6d3eb3540ea168bb24811873b0642d1e9e
Daniel Rahmanto <daniel.rahmanto@gmail.com>
2025-08-31 22:54:31 +0700

    fix: don't use absolute path as the target path argument

--- ext/node/polyfills/_fs/_fs_symlink.ts
+++ ext/node/polyfills/_fs/_fs_symlink.ts
@@ -10,35 +10,13 @@ import { promisify } from "ext:deno_node/internal/util.mjs";
 import type { Buffer } from "node:buffer";
 import { validateOneOf } from "ext:deno_node/internal/validators.mjs";
 import { getValidatedPathToString } from "ext:deno_node/internal/fs/utils.mjs";
-import { op_node_symlink, op_node_symlink_sync } from "ext:core/ops";
 import * as pathModule from "node:path";
 import { isWindows } from "ext:deno_node/_util/os.ts";
+import { stat, statSync } from "ext:deno_node/_fs/_fs_stat.ts";
 
 const { PromisePrototypeThen } = primordials;
 
 export type SymlinkType = "file" | "dir" | "junction";
-
-function getAbsoluteTarget(
-  path: string,
-  target: string,
-  type: SymlinkType | undefined,
-): string | undefined {
-  if (!isWindows || type !== undefined) {
-    return target;
-  }
-
-  try {
-    // Symlinks targets can be relative to the newly created path.
-    // Calculate absolute file name of the symlink target, and check
-    // if it is a directory. Ignore resolve error to keep symlink
-    // errors consistent between platforms if invalid path is
-    // provided.
-    return pathModule.resolve(path, "..", target);
-  } catch {
-    // Fall back to the original target if resolution fails
-    return target;
-  }
-}
 
 export function symlink(
   target: string | Buffer | URL,
@@ -62,13 +40,42 @@ export function symlink(
   callback = makeCallback(callback);
   target = getValidatedPathToString(target, "target");
   path = getValidatedPathToString(path);
-  linkType ??= undefined;
+
+  if (isWindows && !linkType) {
+    let absoluteTarget;
+    try {
+      // Symlinks targets can be relative to the newly created path.
+      // Calculate absolute file name of the symlink target, and check
+      // if it is a directory. Ignore resolve error to keep symlink
+      // errors consistent between platforms if invalid path is
+      // provided.
+      absoluteTarget = pathModule.resolve(path, "..", target);
+    } catch {
+      // Continue regardless of error.
+    }
+    if (absoluteTarget !== undefined) {
+      stat(absoluteTarget, (err, stat) => {
+        const resolvedType = !err && stat.isDirectory() ? "dir" : "file";
+
+        PromisePrototypeThen(
+          Deno.symlink(
+            target,
+            path,
+            { type: resolvedType },
+          ),
+          () => callback(null),
+          callback,
+        );
+      });
+      return;
+    }
+  }
 
   PromisePrototypeThen(
-    op_node_symlink(
-      getAbsoluteTarget(path, target, linkType),
+    Deno.symlink(
+      target,
       path,
-      linkType,
+      { type: linkType ?? "file" },
     ),
     () => callback(null),
     callback,
@@ -89,7 +96,16 @@ export function symlinkSync(
   validateOneOf(type, "type", ["dir", "file", "junction", null, undefined]);
   target = getValidatedPathToString(target, "target");
   path = getValidatedPathToString(path);
-  type ??= undefined;
 
-  op_node_symlink_sync(getAbsoluteTarget(path, target, type), path, type);
+  if (isWindows && !type) {
+    const absoluteTarget = pathModule.resolve(path, "..", target);
+    if (
+      statSync(absoluteTarget, { bigint: false, throwIfNoEntry: false })
+        ?.isDirectory()
+    ) {
+      type = "dir";
+    }
+  }
+
+  Deno.symlinkSync(target, path, { type: type ?? "file" });
 }
