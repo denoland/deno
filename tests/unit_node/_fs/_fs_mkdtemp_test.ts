589b476f5ea3fb255ef446896e7a17d3f84d6d4c
Daniel Rahmanto <daniel.rahmanto@gmail.com>
2025-09-04 09:11:36 +0700

    test: unit

--- tests/unit_node/_fs/_fs_mkdtemp_test.ts
+++ tests/unit_node/_fs/_fs_mkdtemp_test.ts
@@ -1,8 +1,10 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
-import { assert, assertRejects, assertThrows } from "@std/assert";
+import { assert, assertRejects, assertThrows, fail } from "@std/assert";
+import { Buffer } from "node:buffer";
 import { EncodingOption, existsSync, mkdtemp, mkdtempSync } from "node:fs";
 import { env } from "node:process";
 import { promisify } from "node:util";
+import { join } from "@std/path";
 
 const prefix = Deno.build.os === "windows"
   ? env.TEMP + "\\"
@@ -47,6 +49,46 @@ Deno.test({
 });
 
 Deno.test({
+  name: "[node/fs] mkdtemp (utf16le encoding)",
+  ignore: Deno.build.os !== "windows" && Deno.build.os !== "darwin",
+  fn: async () => {
+    const dirName = await mkdtempP(prefix, { encoding: "utf16le" });
+    const directory = Buffer.from(dirName, "utf16le").toString();
+    assert(existsSync(directory));
+    Deno.removeSync(directory);
+  },
+});
+
+Deno.test({
+  name: "[node/fs] mkdtemp (buffer encoding)",
+  fn: async () => {
+    const dirBuffer = await mkdtempP(prefix, { encoding: "buffer" });
+    assert(dirBuffer instanceof Buffer);
+    const directory = dirBuffer.toString();
+    assert(existsSync(directory));
+    Deno.removeSync(directory);
+  },
+});
+
+Deno.test({
+  name: "[node/fs] mkdtemp assert error",
+  fn: async () => {
+    const recursiveDir = join("noop", prefix);
+    try {
+      await mkdtempP(recursiveDir);
+      fail("mkdtemp should have failed");
+    } catch (err) {
+      // deno-lint-ignore no-explicit-any
+      assert((err as any).code === "ENOENT");
+      // deno-lint-ignore no-explicit-any
+      assert((err as any).syscall === "mkdtemp");
+      // deno-lint-ignore no-explicit-any
+      assert((err as any).path === recursiveDir + "XXXXXX");
+    }
+  },
+});
+
+Deno.test({
   name: "[node/fs] mkdtempSync",
   fn: () => {
     const directory = mkdtempSync(prefix);
@@ -78,5 +120,45 @@ Deno.test({
   fn: () => {
     // @ts-expect-error No overload matches this call
     assertThrows(() => mkdtempSync(prefix, badOptions));
+  },
+});
+
+Deno.test({
+  name: "[node/fs] mkdtempSync (utf16le encoding)",
+  ignore: Deno.build.os !== "windows" && Deno.build.os !== "darwin",
+  fn: () => {
+    const dirName = mkdtempSync(prefix, { encoding: "utf16le" });
+    const directory = Buffer.from(dirName, "utf16le").toString();
+    assert(existsSync(directory));
+    Deno.removeSync(directory);
+  },
+});
+
+Deno.test({
+  name: "[node/fs] mkdtempSync (buffer encoding)",
+  fn: () => {
+    const dirBuffer = mkdtempSync(prefix, { encoding: "buffer" });
+    assert(dirBuffer instanceof Buffer);
+    const directory = dirBuffer.toString();
+    assert(existsSync(directory));
+    Deno.removeSync(directory);
+  },
+});
+
+Deno.test({
+  name: "[node/fs] mkdtempSync assert error",
+  fn: () => {
+    const recursiveDir = join("noop", prefix);
+    try {
+      mkdtempSync(recursiveDir);
+      fail("mkdtemp should have failed");
+    } catch (err) {
+      // deno-lint-ignore no-explicit-any
+      assert((err as any).code === "ENOENT");
+      // deno-lint-ignore no-explicit-any
+      assert((err as any).syscall === "mkdtemp");
+      // deno-lint-ignore no-explicit-any
+      assert((err as any).path === recursiveDir + "XXXXXX");
+    }
   },
 });
