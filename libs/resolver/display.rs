e89512576db56bb2161d4b3484dd6ec6e25d5051
KnorpelSenf <shtrog@gmail.com>
2025-09-08 21:00:54 +0000

    perf: speed up file diffing
    This swaps out dissimilar for imara which is substantially faster at diffing strings.
    
    Note that this is a proof of concept and I did not have enough time to make the output pretty. I just shows that the diff is fast. Applying colors should be doable without changing much about the perf.
    
    Fixes https://github.com/denoland/deno/issues/30634

--- libs/resolver/display.rs
+++ libs/resolver/display.rs
@@ -3,11 +3,10 @@
 //! It would be best to move these utilities out of this
 //! crate as this is not specific to resolution, but for
 //! the time being it's fine for this to live here.
-use std::fmt::Write as _;
-
 use deno_terminal::colors;
-use dissimilar::Chunk;
-use dissimilar::diff as difference;
+use imara_diff::{
+  BasicLineDiffPrinter, Diff, InternedInput, UnifiedDiffConfig,
+};
 
 /// Print diff of the same file_path, before and after formatting.
 ///
@@ -25,153 +24,96 @@ pub fn diff(orig_text: &str, edit_text: &str) -> String {
     return " | Text differed by line endings.\n".to_string();
   }
 
-  DiffBuilder::build(&orig_text, &edit_text)
+  build(&orig_text, &edit_text)
 }
 
-struct DiffBuilder {
-  output: String,
-  line_number_width: usize,
-  orig_line: usize,
-  edit_line: usize,
-  orig: String,
-  edit: String,
-  has_changes: bool,
+pub fn build(orig_text: &str, edit_text: &str) -> String {
+  let input = InternedInput::new(orig_text, edit_text);
+  let mut diff = Diff::compute(imara_diff::Algorithm::Histogram, &input);
+  diff.postprocess_lines(&input);
+
+  diff
+    .unified_diff(
+      &BasicLineDiffPrinter(&input.interner),
+      UnifiedDiffConfig::default(),
+      &input,
+    )
+    .to_string()
 }
 
-impl DiffBuilder {
-  pub fn build(orig_text: &str, edit_text: &str) -> String {
-    let mut diff_builder = DiffBuilder {
-      output: String::new(),
-      orig_line: 1,
-      edit_line: 1,
-      orig: String::new(),
-      edit: String::new(),
-      has_changes: false,
-      line_number_width: {
-        let line_count = std::cmp::max(
-          orig_text.split('\n').count(),
-          edit_text.split('\n').count(),
-        );
-        line_count.to_string().chars().count()
-      },
-    };
+// fn handle_diff(&mut self, diff: &Diff, input: &InternedInput<&str>) {
+//   let mut old_line = 0u32;
+//   let mut new_line = 0u32;
 
-    let chunks = difference(orig_text, edit_text);
-    diff_builder.handle_chunks(chunks);
-    diff_builder.output
-  }
+//   for hunk in diff.hunks() {
+//     // Process unchanged lines before this hunk
+//     if old_line < hunk.before.start || new_line < hunk.after.start {
+//       let unchanged_start = std::cmp::max(old_line, new_line);
+//       let unchanged_end = std::cmp::min(hunk.before.start, hunk.after.start);
 
-  fn handle_chunks<'a>(&'a mut self, chunks: Vec<Chunk<'a>>) {
-    for chunk in chunks {
-      match chunk {
-        Chunk::Delete(s) => {
-          let split = s.split('\n').enumerate();
-          for (i, s) in split {
-            if i > 0 {
-              self.orig.push('\n');
-            }
-            self.orig.push_str(&fmt_rem_text_highlight(s));
-          }
-          self.has_changes = true
-        }
-        Chunk::Insert(s) => {
-          let split = s.split('\n').enumerate();
-          for (i, s) in split {
-            if i > 0 {
-              self.edit.push('\n');
-            }
-            self.edit.push_str(&fmt_add_text_highlight(s));
-          }
-          self.has_changes = true
-        }
-        Chunk::Equal(s) => {
-          let split = s.split('\n').enumerate();
-          for (i, s) in split {
-            if i > 0 {
-              self.flush_changes();
-            }
-            self.orig.push_str(&fmt_rem_text(s));
-            self.edit.push_str(&fmt_add_text(s));
-          }
-        }
-      }
-    }
+//       for line_idx in unchanged_start..unchanged_end {
+//         if line_idx > unchanged_start {
+//           self.flush_changes();
+//         }
+//         if (line_idx as usize) < input.before.len() {
+//           let line_text = &input.interner[input.before[line_idx as usize]];
+//           self.orig.push_str(&fmt_rem_text(line_text));
+//         }
+//         if (line_idx as usize) < input.after.len() {
+//           let line_text = &input.interner[input.after[line_idx as usize]];
+//           self.edit.push_str(&fmt_add_text(line_text));
+//         }
+//       }
+//     }
 
-    self.flush_changes();
-  }
+//     // Process deletions (lines only in before)
+//     if hunk.before.start < hunk.before.end {
+//       for line_idx in hunk.before.start..hunk.before.end {
+//         if line_idx > hunk.before.start {
+//           self.orig.push('\n');
+//         }
+//         let line_text = &input.interner[input.before[line_idx as usize]];
+//         self.orig.push_str(&fmt_rem_text_highlight(line_text));
+//       }
+//       self.has_changes = true;
+//     }
 
-  fn flush_changes(&mut self) {
-    if self.has_changes {
-      self.write_line_diff();
+//     // Process insertions (lines only in after)
+//     if hunk.after.start < hunk.after.end {
+//       for line_idx in hunk.after.start..hunk.after.end {
+//         if line_idx > hunk.after.start {
+//           self.edit.push('\n');
+//         }
+//         let line_text = &input.interner[input.after[line_idx as usize]];
+//         self.edit.push_str(&fmt_add_text_highlight(line_text));
+//       }
+//       self.has_changes = true;
+//     }
 
-      self.orig_line += self.orig.split('\n').count();
-      self.edit_line += self.edit.split('\n').count();
-      self.has_changes = false;
-    } else {
-      self.orig_line += 1;
-      self.edit_line += 1;
-    }
+//     old_line = hunk.before.end;
+//     new_line = hunk.after.end;
+//   }
 
-    self.orig.clear();
-    self.edit.clear();
-  }
+//   // Process any remaining unchanged lines
+//   let max_lines = std::cmp::max(input.before.len(), input.after.len()) as u32;
+//   if old_line < max_lines || new_line < max_lines {
+//     for line_idx in std::cmp::max(old_line, new_line)..max_lines {
+//       if line_idx > std::cmp::max(old_line, new_line) {
+//         self.flush_changes();
+//       }
+//       if (line_idx as usize) < input.before.len() {
+//         let line_text = &input.interner[input.before[line_idx as usize]];
+//         self.orig.push_str(&fmt_rem_text(line_text));
+//       }
+//       if (line_idx as usize) < input.after.len() {
+//         let line_text = &input.interner[input.after[line_idx as usize]];
+//         self.edit.push_str(&fmt_add_text(line_text));
+//       }
+//     }
+//   }
 
-  fn write_line_diff(&mut self) {
-    let split = self.orig.split('\n').enumerate();
-    for (i, s) in split {
-      write!(
-        self.output,
-        "{:width$}{} ",
-        self.orig_line + i,
-        colors::gray(" |"),
-        width = self.line_number_width
-      )
-      .unwrap();
-      self.output.push_str(&fmt_rem());
-      self.output.push_str(s);
-      self.output.push('\n');
-    }
-
-    let split = self.edit.split('\n').enumerate();
-    for (i, s) in split {
-      write!(
-        self.output,
-        "{:width$}{} ",
-        self.edit_line + i,
-        colors::gray(" |"),
-        width = self.line_number_width
-      )
-      .unwrap();
-      self.output.push_str(&fmt_add());
-      self.output.push_str(s);
-      self.output.push('\n');
-    }
-  }
-}
-
-fn fmt_add() -> String {
-  colors::green_bold("+").to_string()
-}
-
-fn fmt_add_text(x: &str) -> String {
-  colors::green(x).to_string()
-}
-
-fn fmt_add_text_highlight(x: &str) -> String {
-  colors::black_on_green(x).to_string()
-}
-
-fn fmt_rem() -> String {
-  colors::red_bold("-").to_string()
-}
-
-fn fmt_rem_text(x: &str) -> String {
-  colors::red(x).to_string()
-}
-
-fn fmt_rem_text_highlight(x: &str) -> String {
-  colors::white_on_red(x).to_string()
-}
+//   self.flush_changes();
+// }
 
 pub struct DisplayTreeNode {
   pub text: String,
