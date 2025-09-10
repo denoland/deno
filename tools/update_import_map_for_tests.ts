5a8c1f5c51a2c086ce5aac34d6c8a95fcda1cb41
Yoshiya Hinosawa <stibium121@gmail.com>
2025-09-10 16:45:47 +0900

    update

--- tools/update_import_map_for_tests.ts
+++ tools/update_import_map_for_tests.ts
@@ -11,7 +11,10 @@
 import denoConfig from "../tests/util/std/deno.json" with { type: "json" };
 import { join } from "jsr:@std/path@1/posix/join";
 
-const importMap: { imports: Record<string, string> } = { imports: {} };
+const rootImportMap: { imports: Record<string, string> } = { imports: {} };
+const coreImportMap: { imports: Record<string, string> } = JSON.parse(
+  Deno.readTextFileSync("./tools/core_import_map.json"),
+);
 for (const workspace of denoConfig.workspace) {
   const { default: pkgDenoConfig } = await import(
     `../tests/util/std/${workspace}/deno.json`,
@@ -20,23 +23,33 @@ for (const workspace of denoConfig.workspace) {
     }
   );
 
-  if (typeof pkgDenoConfig.exports === "string") {
-    importMap.imports[pkgDenoConfig.name] = `./${
-      join(workspace, pkgDenoConfig.exports)
-    }`;
-  } else {
-    for (
-      const [moduleName, modulePath] of Object.entries(
-        pkgDenoConfig.exports as Record<string, string>,
-      )
-    ) {
-      importMap.imports[join(pkgDenoConfig.name, moduleName)] =
-        `./tests/util/std/${join(workspace, modulePath)}`;
-    }
+  for (
+    const [moduleName, modulePath] of Object.entries(
+      pkgDenoConfig.exports as Record<string, string>,
+    )
+  ) {
+    rootImportMap.imports[join(pkgDenoConfig.name, moduleName)] =
+      `./tests/util/std/${join(workspace, modulePath)}`;
+    coreImportMap.imports[join(pkgDenoConfig.name, moduleName)] =
+      `../tests/util/std/${join(workspace, modulePath)}`;
   }
 }
 
+rootImportMap.imports = sortObjectByKey(rootImportMap.imports);
+coreImportMap.imports = sortObjectByKey(coreImportMap.imports);
+
+
+console.log("Writing to ./import_map.json");
 await Deno.writeTextFile(
   "./import_map.json",
-  JSON.stringify(importMap, null, 2) + "\n",
+  JSON.stringify(rootImportMap, null, 2) + "\n",
 );
+console.log("Writing to ./tools/core_import_map.json");
+await Deno.writeTextFile(
+  "./tools/core_import_map.json",
+  JSON.stringify(coreImportMap, null, 2) + "\n",
+);
+
+function sortObjectByKey<T>(obj: Record<string, T>): Record<string, T> {
+  return Object.fromEntries(Object.entries(obj).sort(([a], [b]) => a.localeCompare(b)));
+}
