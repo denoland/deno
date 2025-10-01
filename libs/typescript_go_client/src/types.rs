d9709e28b0140075a6bb4bb170c12a63ce2b9679
Nathan Whitaker <nathan@deno.com>
2025-09-30 17:48:25 -0700

    fmt

--- libs/typescript_go_client/src/types.rs
+++ libs/typescript_go_client/src/types.rs
@@ -5,28 +5,28 @@ use indexmap::IndexMap;
 #[derive(serde::Deserialize, Debug, Clone, Copy)]
 #[serde(rename_all = "camelCase")]
 pub struct Position {
-    pub line: u64,
-    pub character: u64,
+  pub line: u64,
+  pub character: u64,
 }
 
 #[derive(serde::Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Diagnostic {
-    pub id: DiagnosticId,
-    pub file_name: String,
-    pub start: Position,
-    pub end: Position,
-    pub start_pos: u32,
-    pub end_pos: u32,
-    pub code: u32,
-    pub category: String,
-    pub message: String,
-    pub message_chain: Vec<DiagnosticId>,
-    pub related_information: Vec<DiagnosticId>,
-    pub reports_unnecessary: bool,
-    pub reports_deprecated: bool,
-    pub skipped_on_no_emit: bool,
-    pub source_line: String,
+  pub id: DiagnosticId,
+  pub file_name: String,
+  pub start: Position,
+  pub end: Position,
+  pub start_pos: u32,
+  pub end_pos: u32,
+  pub code: u32,
+  pub category: String,
+  pub message: String,
+  pub message_chain: Vec<DiagnosticId>,
+  pub related_information: Vec<DiagnosticId>,
+  pub reports_unnecessary: bool,
+  pub reports_deprecated: bool,
+  pub skipped_on_no_emit: bool,
+  pub source_line: String,
 }
 
 pub type DiagnosticId = u32;
@@ -34,96 +34,98 @@ pub type DiagnosticId = u32;
 #[derive(serde_repr::Deserialize_repr, serde_repr::Serialize_repr, Debug)]
 #[repr(u32)]
 pub enum ResolutionMode {
-    None = 0,
-    CommonJS = 1,
-    ESM = 99,
+  None = 0,
+  CommonJS = 1,
+  ESM = 99,
 }
 
 #[derive(serde::Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ResolveModuleNamePayload {
-    pub module_name: String,
-    pub containing_file: String,
-    pub resolution_mode: ResolutionMode,
-    // redirected_reference: Handle<Project>,
+  pub module_name: String,
+  pub containing_file: String,
+  pub resolution_mode: ResolutionMode,
+  // redirected_reference: Handle<Project>,
 }
 
 #[derive(serde::Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ResolveTypeReferenceDirectivePayload {
-    pub type_reference_directive_name: String,
-    pub containing_file: String,
-    pub resolution_mode: ResolutionMode,
+  pub type_reference_directive_name: String,
+  pub containing_file: String,
+  pub resolution_mode: ResolutionMode,
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(from = "String", into = "String")]
 pub struct Handle<T> {
-    pub id: String,
-    #[serde(skip)]
-    _phantom: PhantomData<T>,
+  pub id: String,
+  #[serde(skip)]
+  _phantom: PhantomData<T>,
 }
 
 impl<T> std::fmt::Debug for Handle<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("Handle").field(&self.id).finish()
-    }
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_tuple("Handle").field(&self.id).finish()
+  }
 }
 
 impl<T> From<Handle<T>> for String {
-    fn from(value: Handle<T>) -> Self {
-        value.id
-    }
+  fn from(value: Handle<T>) -> Self {
+    value.id
+  }
 }
 
 impl<T> Clone for Handle<T> {
-    fn clone(&self) -> Self {
-        Self {
-            id: self.id.clone(),
-            _phantom: PhantomData,
-        }
+  fn clone(&self) -> Self {
+    Self {
+      id: self.id.clone(),
+      _phantom: PhantomData,
     }
+  }
 }
 
 impl<T> From<String> for Handle<T> {
-    fn from(id: String) -> Self {
-        Self {
-            id,
-            _phantom: PhantomData,
-        }
+  fn from(id: String) -> Self {
+    Self {
+      id,
+      _phantom: PhantomData,
     }
+  }
 }
 
 #[derive(serde::Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Project {
-    pub id: Handle<Self>,
-    pub config_file_name: String,
-    pub root_files: Vec<String>,
-    pub compiler_options: IndexMap<String, serde_json::Value>,
+  pub id: Handle<Self>,
+  pub config_file_name: String,
+  pub root_files: Vec<String>,
+  pub compiler_options: IndexMap<String, serde_json::Value>,
 }
 
-#[derive(Debug, Clone, serde_repr::Deserialize_repr, serde_repr::Serialize_repr)]
+#[derive(
+  Debug, Clone, serde_repr::Deserialize_repr, serde_repr::Serialize_repr,
+)]
 #[repr(u32)]
 pub enum ModuleKind {
-    None = 0,
-    CommonJS = 1,
-    AMD = 2,
-    UMD = 3,
-    System = 4,
-    ES2015 = 5,
-    ES2020 = 6,
-    ES2022 = 7,
-    ESNext = 99,
-    Node16 = 100,
-    Node18 = 101,
-    NodeNext = 199,
-    Preserve = 200,
+  None = 0,
+  CommonJS = 1,
+  AMD = 2,
+  UMD = 3,
+  System = 4,
+  ES2015 = 5,
+  ES2020 = 6,
+  ES2022 = 7,
+  ESNext = 99,
+  Node16 = 100,
+  Node18 = 101,
+  NodeNext = 199,
+  Preserve = 200,
 }
 
 #[derive(serde::Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct GetImpliedNodeFormatForFilePayload {
-    pub file_name: String,
-    pub package_json_type: String,
+  pub file_name: String,
+  pub package_json_type: String,
 }
