106fe2b4171aa498e2328d4a02130925c368970e
Nathan Whitaker <nathan@deno.com>
2025-10-08 12:24:31 -0700

    improve test

--- tests/specs/cache/nmd_manual/main.ts
+++ tests/specs/cache/nmd_manual/main.ts
@@ -1,8 +1,8 @@
 import { add } from "@denotest/add";
 import { getValue } from "@denotest/esm-basic";
 
-import { sub } from "jsr:@denotest/subtract";
+import { subtract } from "jsr:@denotest/subtract";
 
-console.log(add(1, 2));
+console.log(add(3, 2));
 console.log(getValue());
-console.log(sub(1, 2));
+console.log(subtract(3, 2));
