8558d1d080d4d85f7b2dc5403d2a4ab711fe61df
Divy Srivastava <me@littledivy.com>
2025-11-06 10:58:12 +0100

    x

--- ext/web/01_dom_exception.js
+++ ext/web/01_dom_exception.js
@@ -11,7 +11,6 @@ import { primordials } from "ext:core/mod.js";
 const {
   ErrorPrototype,
   ObjectDefineProperty,
-  ObjectCreate,
   ObjectEntries,
   ObjectHasOwn,
   ObjectPrototypeIsPrototypeOf,
