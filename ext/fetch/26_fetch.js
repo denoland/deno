aa6aac72117d0588ec5b696a75a6f23e68cb241a
Divy Srivastava <me@littledivy.com>
2025-11-11 23:08:35 +0530

    x

--- ext/fetch/26_fetch.js
+++ ext/fetch/26_fetch.js
@@ -30,8 +30,6 @@ const {
   SafePromisePrototypeFinally,
   String,
   StringPrototypeEndsWith,
-  StringPrototypeStartsWith,
-  StringPrototypeToLowerCase,
   TypeError,
   TypedArrayPrototypeGetSymbolToStringTag,
 } = primordials;
