e6f6dba58ab1fd3409542c55ea7c1bbd60ddb1f9
David Sherret <dsherret@gmail.com>
2025-11-13 10:33:15 -0500

    Bump rust-automation version to fix bug

--- tools/release/deps.ts
+++ tools/release/deps.ts
@@ -1,4 +1,4 @@
 // Copyright 2018-2025 the Deno authors. MIT license.
 
-export * from "jsr:@deno/rust-automation@0.22.2";
-export * from "jsr:@deno/rust-automation@0.22.2/github-actions";
+export * from "jsr:@deno/rust-automation@0.22.3";
+export * from "jsr:@deno/rust-automation@0.22.3/github-actions";
