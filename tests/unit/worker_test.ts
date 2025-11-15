361abd34608533cfbb93cbfcbef76d212edbb4fe
snek <snek@deno.com>
2025-11-16 02:45:29 +0900

    fix(ext/web): Event dispatch should continue after reporting exception (#31297)
    https://dom.spec.whatwg.org/#concept-event-listener-inner-invoke
    
    ---------
    
    Signed-off-by: snek <snek@deno.com>

--- tests/unit/worker_test.ts
+++ tests/unit/worker_test.ts
@@ -301,7 +301,7 @@ Deno.test({
     worker.postMessage("boom");
     worker.postMessage("ping");
     assertEquals(await promise, {
-      messageHandlersCalled: 4,
+      messageHandlersCalled: 3,
       errorHandlersCalled: 4,
     });
     worker.terminate();
