172263b3ba8ae4156b3a49e0561066abcd3b1906
Maksim Bondarenkov <119937608+ognevny@users.noreply.github.com>
2025-11-17 11:24:38 +0300

    build(deps): update aws-lc-rs (#31307)
    This pulls some build improvements and fixes, including
    https://github.com/aws/aws-lc/pull/2381/

--- cli/tools/publish/provenance.rs
+++ cli/tools/publish/provenance.rs
@@ -326,7 +326,7 @@ pub async fn attest(
   let pae = pre_auth_encoding(type_, data);
 
   let signer = FulcioSigner::new(http_client)?;
-  let (signature, key_material) = signer.sign(&pae).await?;
+  let (signature, key_material) = Box::pin(signer.sign(&pae)).await?;
 
   let content = SignatureBundle {
     case: "dsseSignature",
@@ -471,9 +471,8 @@ impl<'a> FulcioSigner<'a> {
     let pem = spki.to_pem(LineEnding::LF)?;
 
     // Create signing certificate
-    let certificates = self
-      .create_signing_certificate(&token, pem, challenge)
-      .await?;
+    let certificates =
+      Box::pin(self.create_signing_certificate(&token, pem, challenge)).await?;
 
     let signature = self.ephemeral_signer.sign(&self.rng, data)?;
 
