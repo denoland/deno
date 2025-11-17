654d813b1813d00bff85ad2f84a6794aa681e7c0
Asher Gomez <ashersaupingomez@gmail.com>
2025-11-18 10:48:29 +1100

    fixes

--- cli/tsc/dts/lib.deno.window.d.ts
+++ cli/tsc/dts/lib.deno.window.d.ts
@@ -2,6 +2,7 @@
 
 /// <reference no-default-lib="true" />
 /// <reference lib="deno.ns" />
+/// <reference lib="deno.broadcast_channel" />
 /// <reference lib="deno.shared_globals" />
 /// <reference lib="deno.webstorage" />
 /// <reference lib="esnext" />
