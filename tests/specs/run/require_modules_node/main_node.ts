28d1f27498d1ece85e35ab6949d83d25fada5d4a
Felipe Cardozo <fraifelipe@gmail.com>
2025-11-17 18:59:04 -0300

    chore: clean up require tests

--- tests/specs/run/require_modules_node/main_node.ts
+++
@@ -1,9 +0,0 @@
-console.log("main_node.ts starts");
-
-if (globalThis.__require_node_worked__) {
-  console.log("SUCCESS: require('node:*') worked in --require module");
-} else {
-  console.log("ERROR: require('node:*') did NOT work");
-}
-
-console.log("main_node.ts finished");
