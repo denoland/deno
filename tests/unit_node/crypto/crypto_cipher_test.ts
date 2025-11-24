345125db16c71b04256c0e995b9376e57f88875b
Daniel Osvaldo Rahmanto <daniel.rahmanto@gmail.com>
2025-11-24 18:33:57 +0700

    fix(ext/node): autopadding behavior on `crypto.Cipheriv` (#31389)

--- tests/unit_node/crypto/crypto_cipher_test.ts
+++ tests/unit_node/crypto/crypto_cipher_test.ts
@@ -569,3 +569,26 @@ Deno.test({
     assertEquals(decryptedText, text);
   },
 });
+
+Deno.test({
+  name: "createCipheriv - setAutoPadding behavior",
+  fn() {
+    const algorithm = "aes-256-cbc";
+    const key = Buffer.alloc(32, 0);
+    const iv = Buffer.alloc(16, 0);
+
+    const cipherWithAutoPadding = crypto
+      .createCipheriv(algorithm, key, iv)
+      .setAutoPadding(true);
+    let encrypted = cipherWithAutoPadding.update("", "utf8", "binary");
+    encrypted += cipherWithAutoPadding.final("binary");
+    assertEquals(encrypted.length, 16);
+
+    const cipherWithoutAutoPadding = crypto
+      .createCipheriv(algorithm, key, iv)
+      .setAutoPadding(false);
+    let otherEncrypted = cipherWithoutAutoPadding.update("", "utf8", "binary");
+    otherEncrypted += cipherWithoutAutoPadding.final("binary");
+    assertEquals(otherEncrypted.length, 0);
+  },
+});
