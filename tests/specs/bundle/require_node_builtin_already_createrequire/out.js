c7a8fa883f09b85f4cd90012c5bf8e64f988c7cb
Nathan Whitaker <nathan@deno.com>
2025-11-25 11:54:58 -0500

    tempdir

--- tests/specs/bundle/require_node_builtin_already_createrequire/out.js
+++
@@ -1,9 +0,0 @@
-import { createRequire as __deno_internal_createRequire } from "node:module";
-var __require = __deno_internal_createRequire(import.meta.url);
-
-// main.cjs
-var { createRequire } = __require("node:module");
-var os = __require("node:os");
-console.log(os.freemem.name);
-var require2 = createRequire(import.meta.url);
-console.log(require2.name);
