5ce48b7cc83a5c0451392147d1bf9033709c7f2f
Nathan Whitaker <nathan@deno.com>
2025-11-25 11:44:58 -0500

    fmt

--- tests/specs/bundle/require_node_builtin_already_createrequire/out.js
+++ tests/specs/bundle/require_node_builtin_already_createrequire/out.js
@@ -1,7 +1,6 @@
 import { createRequire as __deno_internal_createRequire } from "node:module";
 var __require = __deno_internal_createRequire(import.meta.url);
 
-
 // main.cjs
 var { createRequire } = __require("node:module");
 var os = __require("node:os");
