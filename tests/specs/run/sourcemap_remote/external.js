7aba54630fa9eb023df891cb4232000601a84775
Felipe Cardozo <fraifelipe@gmail.com>
2025-11-28 12:37:43 -0300

    test: use localhost for sourcemap remote tests

--- tests/specs/run/sourcemap_remote/external.js
+++ tests/specs/run/sourcemap_remote/external.js
@@ -5,4 +5,4 @@ function throwError() {
 
 throwError();
 
-//# sourceMappingURL=https://raw.githubusercontent.com/denoland/deno/3e987b146a4d90ffe9ec421925dddd3b05ce0abe/tests/specs/run/sourcemap_external/external.js.map
+//# sourceMappingURL=https://localhost:5545/run/sourcemap_external.js.map
