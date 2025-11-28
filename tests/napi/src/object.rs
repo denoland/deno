440eead410d419b9ec89da20508b2f0623753a7d
Divy Srivastava <me@littledivy.com>
2025-11-28 14:03:50 +0900

    feat(ext/node): add `napi_create_object_with_properties` API
    Closes https://github.com/denoland/deno/issues/31433

--- tests/napi/src/object.rs
+++ tests/napi/src/object.rs
@@ -8,6 +8,18 @@ use crate::assert_napi_ok;
 use crate::napi_get_callback_info;
 use crate::napi_new_property;
 
+// Experimental NAPI function not yet in napi-sys crate
+unsafe extern "C" {
+  fn napi_create_object_with_properties(
+    env: napi_env,
+    prototype_or_null: napi_value,
+    property_names: *const napi_value,
+    property_values: *const napi_value,
+    property_count: usize,
+    result: *mut napi_value,
+  ) -> napi_status;
+}
+
 extern "C" fn test_object_new(
   env: napi_env,
   info: napi_callback_info,
@@ -66,6 +78,132 @@ extern "C" fn test_object_attr_property(
   obj
 }
 
+extern "C" fn test_create_object_with_properties(
+  env: napi_env,
+  _info: napi_callback_info,
+) -> napi_value {
+  let mut names: [napi_value; 3] = [ptr::null_mut(); 3];
+  let mut values: [napi_value; 3] = [ptr::null_mut(); 3];
+
+  // Create "name" property
+  assert_napi_ok!(napi_create_string_utf8(
+    env,
+    c"name".as_ptr(),
+    4,
+    &mut names[0]
+  ));
+  assert_napi_ok!(napi_create_string_utf8(
+    env,
+    c"Foo".as_ptr(),
+    3,
+    &mut values[0]
+  ));
+
+  // Create "age" property
+  assert_napi_ok!(napi_create_string_utf8(
+    env,
+    c"age".as_ptr(),
+    3,
+    &mut names[1]
+  ));
+  assert_napi_ok!(napi_create_int32(env, 42, &mut values[1]));
+
+  // Create "active" property
+  assert_napi_ok!(napi_create_string_utf8(
+    env,
+    c"active".as_ptr(),
+    6,
+    &mut names[2]
+  ));
+  assert_napi_ok!(napi_get_boolean(env, true, &mut values[2]));
+
+  let mut result: napi_value = ptr::null_mut();
+  let mut null_proto: napi_value = ptr::null_mut();
+  assert_napi_ok!(napi_get_null(env, &mut null_proto));
+
+  assert_napi_ok!(napi_create_object_with_properties(
+    env,
+    null_proto,
+    names.as_ptr(),
+    values.as_ptr(),
+    3,
+    &mut result
+  ));
+
+  result
+}
+
+extern "C" fn test_create_object_with_properties_empty(
+  env: napi_env,
+  _info: napi_callback_info,
+) -> napi_value {
+  let mut result: napi_value = ptr::null_mut();
+
+  assert_napi_ok!(napi_create_object_with_properties(
+    env,
+    ptr::null_mut(),
+    ptr::null(),
+    ptr::null(),
+    0,
+    &mut result
+  ));
+
+  result
+}
+
+extern "C" fn test_create_object_with_custom_prototype(
+  env: napi_env,
+  _info: napi_callback_info,
+) -> napi_value {
+  // Create a prototype object with a method
+  let mut prototype: napi_value = ptr::null_mut();
+  assert_napi_ok!(napi_create_object(env, &mut prototype));
+
+  let mut method_name: napi_value = ptr::null_mut();
+  assert_napi_ok!(napi_create_string_utf8(
+    env,
+    c"test".as_ptr(),
+    4,
+    &mut method_name
+  ));
+
+  let mut method_func: napi_value = ptr::null_mut();
+  assert_napi_ok!(napi_create_function(
+    env,
+    c"test".as_ptr(),
+    4,
+    Some(test_object_new),
+    ptr::null_mut(),
+    &mut method_func
+  ));
+
+  assert_napi_ok!(napi_set_property(env, prototype, method_name, method_func));
+
+  // Create object with custom prototype and a property
+  let mut names: [napi_value; 1] = [ptr::null_mut(); 1];
+  let mut values: [napi_value; 1] = [ptr::null_mut(); 1];
+
+  assert_napi_ok!(napi_create_string_utf8(
+    env,
+    c"value".as_ptr(),
+    5,
+    &mut names[0]
+  ));
+  assert_napi_ok!(napi_create_int32(env, 42, &mut values[0]));
+
+  let mut result: napi_value = ptr::null_mut();
+  assert_napi_ok!(napi_create_object_with_properties(
+    env,
+    prototype,
+    names.as_ptr(),
+    values.as_ptr(),
+    1,
+    &mut result
+  ));
+
+  result
+}
+
 pub fn init(env: napi_env, exports: napi_value) {
   let properties = &[
     napi_new_property!(env, "test_object_new", test_object_new),
@@ -74,6 +212,21 @@ pub fn init(env: napi_env, exports: napi_value) {
       env,
       "test_object_attr_property",
       test_object_attr_property
+    ),
+    napi_new_property!(
+      env,
+      "test_create_object_with_properties",
+      test_create_object_with_properties
+    ),
+    napi_new_property!(
+      env,
+      "test_create_object_with_properties_empty",
+      test_create_object_with_properties_empty
+    ),
+    napi_new_property!(
+      env,
+      "test_create_object_with_custom_prototype",
+      test_create_object_with_custom_prototype
     ),
   ];
 
