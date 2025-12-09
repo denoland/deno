1aa7f33fbcfc714240454e5228b8c211950acb5c
Daniel Rahmanto <daniel.rahmanto@gmail.com>
2025-12-09 18:26:34 +0700

    chore: omit URL error from rust side

--- ext/node/ops/sqlite/validators.rs
+++ ext/node/ops/sqlite/validators.rs
@@ -11,9 +11,6 @@ pub enum Error {
   #[class(type)]
   #[error("Cannot call constructor without `new`")]
   ConstructCallRequired,
-  #[class(type)]
-  #[error("The URL must be of scheme file:")]
-  InvalidUrlScheme,
 }
 
 impl Error {
@@ -21,7 +18,6 @@ impl Error {
     match self {
       Self::InvalidArgType(_) => ErrorCode::ERR_INVALID_ARG_TYPE,
       Self::ConstructCallRequired => ErrorCode::ERR_CONSTRUCT_CALL_REQUIRED,
-      Self::InvalidUrlScheme => ErrorCode::ERR_INVALID_URL_SCHEME,
     }
   }
 }
@@ -30,7 +26,6 @@ impl Error {
 pub enum ErrorCode {
   ERR_INVALID_ARG_TYPE,
   ERR_CONSTRUCT_CALL_REQUIRED,
-  ERR_INVALID_URL_SCHEME,
 }
 
 impl std::fmt::Display for ErrorCode {
@@ -44,7 +39,6 @@ impl ErrorCode {
     match self {
       Self::ERR_INVALID_ARG_TYPE => "ERR_INVALID_ARG_TYPE",
       Self::ERR_CONSTRUCT_CALL_REQUIRED => "ERR_CONSTRUCT_CALL_REQUIRED",
-      Self::ERR_INVALID_URL_SCHEME => "ERR_INVALID_URL_SCHEME",
     }
   }
 }
