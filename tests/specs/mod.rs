d25de99fd84c2bd4087c3836ed9f438de0bd966c
Divy Srivastava <me@littledivy.com>
2025-12-11 12:23:37 +0530

    Revert "chore: macos-15-intel runner on ci (#31419)"
    This reverts commit b18b8347f7095d85608dda41c47af3c744539787.

--- tests/specs/mod.rs
+++ tests/specs/mod.rs
@@ -484,10 +484,6 @@ fn should_run(if_cond: Option<&str>) -> bool {
       "mac" => cfg!(target_os = "macos"),
       "linux" => cfg!(target_os = "linux"),
       "notCI" => std::env::var_os("CI").is_none(),
-      "notMacIntel" => {
-        cfg!(unix)
-          && !(cfg!(target_os = "macos") && cfg!(target_arch = "x86_64"))
-      }
       value => panic!("Unknown if condition: {}", value),
     }
   } else {
