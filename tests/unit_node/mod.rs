81cc0a810183f06f51e219bac344bd91d05b6752
David Sherret <dsherret@gmail.com>
2025-12-10 20:31:00 -0500

    refactor: attempt to improve ci test stability

---
+++ tests/unit_node/mod.rs
@@ -0,0 +1,66 @@
+// Copyright 2018-2025 the Deno authors. MIT license.
+
+use file_test_runner::RunOptions;
+use file_test_runner::TestResult;
+use file_test_runner::collect_and_run_tests;
+use file_test_runner::collection::CollectOptions;
+use file_test_runner::collection::CollectedTest;
+use file_test_runner::collection::strategies::TestPerFileCollectionStrategy;
+use test_util as util;
+use test_util::flaky_test::Parallelism;
+use test_util::flaky_test::flaky_test_ci;
+use test_util::tests_path;
+use util::deno_config_path;
+use util::env_vars_for_npm_tests;
+
+fn main() {
+  let _g = util::http_server();
+  let parallelism = Parallelism::default();
+  collect_and_run_tests(
+    CollectOptions {
+      base: tests_path().join("unit_node").to_path_buf(),
+      strategy: Box::new(TestPerFileCollectionStrategy {
+        file_pattern: Some(".*_test\\.ts$".to_string()),
+      }),
+      filter_override: None,
+    },
+    RunOptions {
+      parallelism: parallelism.for_run_options(),
+      ..Default::default()
+    },
+    move |test| flaky_test_ci(&test.name, &parallelism, || run_test(test)),
+  )
+}
+
+fn run_test(test: &CollectedTest) -> TestResult {
+  let mut deno = util::deno_cmd()
+    .disable_diagnostic_logging()
+    .current_dir(util::root_path())
+    .arg("test")
+    .arg("--config")
+    .arg(deno_config_path())
+    .arg("--no-lock")
+    .arg("--unstable-net")
+    .arg("-A");
+
+  // Some tests require the root CA cert file to be loaded.
+  if test.name.ends_with("::http2_test")
+    || test.name.ends_with("::http_test")
+    || test.name.ends_with("::https_test")
+  {
+    deno = deno.arg("--cert=./tests/testdata/tls/RootCA.pem");
+  }
+
+  // Parallel tests for crypto
+  if test.name.contains("::crypto::") {
+    deno = deno.arg("--parallel");
+  }
+
+  deno
+    .arg(test.path.clone())
+    .envs(env_vars_for_npm_tests())
+    .piped_output()
+    .spawn()
+    .expect("failed to spawn script")
+    .wait_to_test_result(&test.name)
+}
