d4adade6217a5e2bdcdef71a8dba2a84eb9123be
David Sherret <dsherret@users.noreply.github.com>
2026-01-08 19:23:30 -0500

    chore: remove itest macro (#31795)

--- tests/integration/task_tests.rs
+++ tests/integration/task_tests.rs
@@ -7,31 +7,6 @@ use test_util as util;
 use util::TestContextBuilder;
 use util::test;
 
-// use test_util::env_vars_for_npm_tests;
-// use test_util::TestContext;
-
-// TODO(2.0): this should first run `deno install`
-// itest!(task_package_json_npm_bin {
-//   args: "task bin extra",
-//   cwd: Some("task/package_json/"),
-//   output: "task/package_json/bin.out",
-//   copy_temp_dir: Some("task/package_json/"),
-//   envs: env_vars_for_npm_tests(),
-//   exit_code: 0,
-//   http_server: true,
-// });
-
-// TODO(2.0): not entirely clear what's wrong with this test but it hangs for more than 60s
-// itest!(task_npx_on_own {
-//   args: "task on-own",
-//   cwd: Some("task/npx/"),
-//   output: "task/npx/on_own.out",
-//   copy_temp_dir: Some("task/npx/"),
-//   envs: env_vars_for_npm_tests(),
-//   exit_code: 1,
-//   http_server: true,
-// });
-
 #[test(flaky)]
 fn deno_task_ansi_escape_codes() {
   let context = TestContextBuilder::default().use_temp_cwd().build();
