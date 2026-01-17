a548216e65d12e687d7fadac31884c8082ae5922
Yusuke Tanaka <wing0920@gmail.com>
2026-01-17 12:14:56 +0900

    emit warn message when file is not writable

--- ext/tls/keylog.rs
+++ ext/tls/keylog.rs
@@ -6,16 +6,29 @@
 //! are written to the specified file in NSS Key Log format, which can be
 //! used by tools like Wireshark to decrypt TLS traffic.
 
+use std::env;
+use std::fs::OpenOptions;
 use std::sync::Arc;
 use std::sync::OnceLock;
 
 use rustls::KeyLog;
 use rustls::KeyLogFile;
 
-static SSL_KEY_LOG: OnceLock<Arc<KeyLogFile>> = OnceLock::new();
+static SSL_KEY_LOG: OnceLock<Arc<dyn KeyLog + Send + Sync>> = OnceLock::new();
 
 pub fn get_ssl_key_log() -> Arc<dyn KeyLog> {
   SSL_KEY_LOG
-    .get_or_init(|| Arc::new(KeyLogFile::new()))
+    .get_or_init(|| {
+      if let Some(path) = env::var_os("SSLKEYLOGFILE") {
+        if let Err(e) =
+          OpenOptions::new().append(true).create(true).open(&path)
+        {
+          log::warn!(
+            "SSLKEYLOGFILE is set but the file could not be opened: {e}"
+          );
+        }
+      }
+      Arc::new(KeyLogFile::new())
+    })
     .clone()
 }
