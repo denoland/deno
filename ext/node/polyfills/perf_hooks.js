ef29726adf37872ad34e045e09f508f0b768e8c7
Felipe Cardozo <fraifelipe@gmail.com>
2026-01-18 11:46:50 -0300

    fix(node/perf_hooks): add missing constants and node error handling

--- ext/node/polyfills/perf_hooks.js
+++ ext/node/polyfills/perf_hooks.js
@@ -6,12 +6,51 @@
 import {
   performance,
   PerformanceEntry,
-  PerformanceObserver,
+  PerformanceObserver as WebPerformanceObserver,
   PerformanceObserverEntryList,
 } from "ext:deno_web/15_performance.js";
 import { EldHistogram } from "ext:core/ops";
+import { ERR_INVALID_ARG_TYPE } from "ext:deno_node/internal/errors.ts";
 
-const constants = {};
+const constants = {
+  NODE_PERFORMANCE_ENTRY_TYPE_NODE: 0,
+  NODE_PERFORMANCE_ENTRY_TYPE_MARK: 1,
+  NODE_PERFORMANCE_ENTRY_TYPE_MEASURE: 2,
+  NODE_PERFORMANCE_ENTRY_TYPE_GC: 3,
+  NODE_PERFORMANCE_ENTRY_TYPE_FUNCTION: 4,
+  NODE_PERFORMANCE_ENTRY_TYPE_HTTP2: 5,
+  NODE_PERFORMANCE_ENTRY_TYPE_HTTP: 6,
+  NODE_PERFORMANCE_ENTRY_TYPE_DNS: 7,
+  NODE_PERFORMANCE_ENTRY_TYPE_NET: 8,
+};
+
+// Node-compatible PerformanceObserver that throws proper Node.js errors
+class PerformanceObserver extends WebPerformanceObserver {
+  constructor(callback) {
+    if (typeof callback !== "function") {
+      throw new ERR_INVALID_ARG_TYPE("callback", "Function", callback);
+    }
+    super(callback);
+  }
+
+  observe(options) {
+    if (typeof options !== "object" || options === null) {
+      throw new ERR_INVALID_ARG_TYPE("options", "Object", options);
+    }
+    if (options.entryTypes !== undefined && !Array.isArray(options.entryTypes)) {
+      throw new ERR_INVALID_ARG_TYPE(
+        "options.entryTypes",
+        "string[]",
+        options.entryTypes,
+      );
+    }
+    return super.observe(options);
+  }
+
+  static get supportedEntryTypes() {
+    return WebPerformanceObserver.supportedEntryTypes;
+  }
+}
 
 performance.eventLoopUtilization = () => {
   // TODO(@marvinhagemeister): Return actual non-stubbed values
