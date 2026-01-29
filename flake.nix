92ff5aaf7c63fc4279c781294df7abab17277ae1
Divy Srivastava <me@littledivy.com>
2026-01-29 15:20:01 +0530

    build deno in nix

--- flake.nix
+++ flake.nix
@@ -19,8 +19,76 @@
         };
 
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        # Pre-fetch rusty_v8 static library so the build works in Nix's sandbox
+        rustyV8Version = "145.0.0";
+        rustyV8Target = {
+          "x86_64-linux" = "x86_64-unknown-linux-gnu";
+          "aarch64-linux" = "aarch64-unknown-linux-gnu";
+          "x86_64-darwin" = "x86_64-apple-darwin";
+          "aarch64-darwin" = "aarch64-apple-darwin";
+        }.${system};
+        rustyV8 = pkgs.fetchurl {
+          url = "https://github.com/denoland/rusty_v8/releases/download/v${rustyV8Version}/librusty_v8_release_${rustyV8Target}.a.gz";
+          sha256 = {
+            "x86_64-linux" = "sha256-chV1PAx40UH3Ute5k3lLrgfhih39Rm3KqE+mTna6ysE=";
+            "aarch64-linux" = "sha256-4IivYskhUSsMLZY97+g23UtUYh4p5jk7CzhMbMyqXyY=";
+            "x86_64-darwin" = "sha256-1jUuC+z7saQfPYILNyRJanD4+zOOhXU2ac/LFoytwho=";
+            "aarch64-darwin" = "sha256-yHa1eydVCrfYGgrZANbzgmmf25p7ui1VMas2A7BhG6k=";
+          }.${system};
+        };
+
+        commonNativeBuildInputs = with pkgs; [
+          rustToolchain
+          llvmPackages_20.clang
+          lld_20
+          llvmPackages_20.libllvm
+          pkg-config
+          cmake
+          protobuf
+        ];
+
+        commonBuildInputs = with pkgs; [
+          openssl
+        ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+          glib
+        ];
+
+        buildDenoBin = { pname, binName ? pname }: pkgs.rustPlatform.buildRustPackage {
+          inherit pname;
+          version = "2.6.6";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = commonNativeBuildInputs;
+          buildInputs = commonBuildInputs;
+
+          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.llvmPackages_20.clang-unwrapped.lib
+          ];
+          RUSTY_V8_ARCHIVE = rustyV8;
+
+          buildPhase = ''
+            unset AS
+            cargo build --release --bin ${binName}
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/${binName} $out/bin/
+          '';
+
+          doCheck = false;
+        };
       in
       {
+        packages = {
+          deno = buildDenoBin { pname = "deno"; };
+          denort = buildDenoBin { pname = "denort"; };
+
+          default = self.packages.${system}.deno;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             # Rust toolchain (from rust-toolchain.toml)
