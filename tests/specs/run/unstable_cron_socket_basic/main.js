69d436e789cbf9acb54592f7a28f6f9c120a5b23
Arnau Orriols <arnauorriolsmiro@gmail.com>
2026-02-03 00:01:24 +0100

    fix: flaky cron socket tests

--- tests/specs/run/unstable_cron_socket_basic/main.js
+++ tests/specs/run/unstable_cron_socket_basic/main.js
@@ -11,5 +11,5 @@ Deno.cron("other-cron", "*/5 * * * * *", () => {
 console.error("[ISOLATE] isolate booted");
 
 console.error("[ISOLATE] Waiting for execution...");
-await new Promise((resolve) => setTimeout(resolve, 2000));
+await new Promise({port: 0}, (resolve) => setTimeout(resolve, 2000));
 console.error("[ISOLATE] Exiting");
