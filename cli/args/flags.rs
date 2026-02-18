52fcc461bdf1f55a46d310a564607eb1bf96e91b
David Sherret <dsherret@gmail.com>
2026-02-18 17:50:23 -0500

    update

--- cli/args/flags.rs
+++ cli/args/flags.rs
@@ -2786,7 +2786,7 @@ On the first invocation of `deno compile`, Deno will download the relevant binar
       .arg(
         Arg::new("self-extracting")
           .long("self-extracting")
-          .help("Create a self-extracting binary that extracts the embedded file system to disk at runtime")
+          .help("Create a self-extracting binary that extracts the embedded file system on first run")
           .action(ArgAction::SetTrue)
           .help_heading(COMPILE_HEADING),
       )
