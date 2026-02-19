4b26a4c601bc3173145e68294e29a3ddeca895b2
Angel98518 <daniel98518@gmail.com>
2026-02-19 04:55:49 +0100

    chore: fix import order - Context before bail (alphabetical)

--- cli/tools/pm/outdated/mod.rs
+++ cli/tools/pm/outdated/mod.rs
@@ -7,9 +7,9 @@ use std::sync::Arc;
 
 use deno_cache_dir::GlobalOrLocalHttpCache;
 use deno_cache_dir::file_fetcher::CacheSetting;
-use deno_core::anyhow::bail;
 #[allow(unused_imports)]
 use deno_core::anyhow::Context;
+use deno_core::anyhow::bail;
 use deno_core::error::AnyError;
 use deno_semver::StackString;
 use deno_semver::VersionReq;
