a192d3fa4da60c2ca8132478395ef0ba1b95f5ea
Bartek Iwańczuk <biwanczuk@gmail.com>
2026-02-27 12:29:30 +0100

    assert

--- tests/specs/run/npm_pkg_requires_esm_js/catch_throwing_esm.cjs
+++ tests/specs/run/npm_pkg_requires_esm_js/catch_throwing_esm.cjs
@@ -2,11 +2,14 @@
 const assert = require("node:assert");
 
 // Test 1: require of ESM that throws a simple error can be caught
+let caught = false;
 try {
   require("./throw.mjs");
-  assert.fail("should have thrown");
 } catch (e) {
+  caught = true;
   assert.strictEqual(e.message, "STOP");
+} finally {
+  assert.ok(caught, "require of throwing ESM should have been caught");
 }
 
 // Test 2: require of ESM that throws a global error can be caught
