a5a79b474ca490772b87e18a03c7fe5260b4d19c
Leo Kettmeir <crowlkats@toaxl.com>
2026-03-18 15:32:22 +0100

    remove unrelated files

--- test2.js
+++
@@ -1,9 +0,0 @@
-const inputBlob = new Blob([await Deno.readFile("./input.png")], {
-  type: "image/png",
-});
-const bitmap = await createImageBitmap(inputBlob);
-const canvas = new OffscreenCanvas(200, 200);
-const bitmaprenderer = canvas.getContext("bitmaprenderer");
-bitmaprenderer.transferFromImageBitmap(bitmap);
-const outputBlob = await canvas.convertToBlob();
-await Deno.writeFile("./output.png", outputBlob.stream());
