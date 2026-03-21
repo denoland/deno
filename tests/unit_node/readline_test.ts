4a396e7f15783be4cd6b8837afee0ac1ae9bac9c
KnorpelSenf <shtrog@gmail.com>
2026-03-21 11:06:03 +0100

    test: cover readline fix

--- tests/unit_node/readline_test.ts
+++ tests/unit_node/readline_test.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2026 the Deno authors. MIT license.
 import { createInterface, Interface } from "node:readline";
-import { assertInstanceOf } from "@std/assert";
-import { Readable, Writable } from "node:stream";
+import { assertFalse, assertInstanceOf } from "@std/assert";
+import { Readable, Stream, Writable } from "node:stream";
 
 Deno.test("[node/readline] createInstance", () => {
   const rl = createInterface({
@@ -24,4 +24,25 @@ Deno.test("[node/readline] don't throw on rl.question()", () => {
   // Calling this would throw
   rli.question("foo", () => rli.close());
   rli.close();
+});
+
+Deno.test("[node/readline] createInstance on non-TTY with terminal: true", () => {
+  const stream = new Stream() as any;
+  stream.isTTY = false;
+  stream.resume = function () {};
+  stream.pause = function () {};
+
+  let setRawModeCalled = false;
+  stream.setRawMode = function () {
+    setRawModeCalled = true;
+    throw new Error("setRawMode should not be called for non-TTY streams");
+  };
+
+  const rl = createInterface({
+    input: stream,
+    output: stream,
+    terminal: true,
+  });
+  rl.close();
+  assertFalse(setRawModeCalled);
 });
