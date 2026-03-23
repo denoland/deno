7a2a390432fc3eddef21db0ea6db6c392f63d13d
David Sherret <dsherret@users.noreply.github.com>
2026-03-23 15:40:30 -0400

    refactor: `deno_npmrc` crate (#32936)
    Split out npmrc reading from the deno_npm crate.

--- libs/npmrc/lib.rs
+++ libs/npmrc/lib.rs
@@ -1,5 +1,9 @@
 // Copyright 2018-2026 the Deno authors. MIT license.
 
+#![deny(clippy::print_stderr)]
+#![deny(clippy::print_stdout)]
+#![deny(clippy::unused_async)]
+
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::sync::Arc;
