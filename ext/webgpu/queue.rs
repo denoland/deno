b8ec8611217c6bdd0a1821c16a45869489910358
KnorpelSenf <shtrog@gmail.com>
2026-04-08 08:57:01 +0200

    fix(ext/webgpu): accept `ArrayBuffer` in `GPUQueue.writeBuffer()` as data source (#33152)
    Fixes #33151

--- ext/webgpu/queue.rs
+++ ext/webgpu/queue.rs
@@ -159,6 +159,13 @@ impl GPUQueue {
           // SAFETY: the slice is within the bounds of the backing store
           unsafe { std::slice::from_raw_parts(ptr as *const u8, byte_len) };
       (buf, bpe)
+    } else if let Ok(ab) = v8::Local::<v8::ArrayBuffer>::try_from(data_arg) {
+      let byte_len = ab.byte_length();
+      let ptr = ab.data().unwrap().as_ptr();
+      let buf =
+        // SAFETY: Pointer is non-null and byte_len is within the backing store.
+        unsafe { std::slice::from_raw_parts(ptr as *const u8, byte_len) };
+      (buf, 1)
     } else if let Ok(view) =
       v8::Local::<v8::ArrayBufferView>::try_from(data_arg)
     {
