b5585e41952e5d9c5b97f396e731294c2737001c
divybot <divybot@users.noreply.github.com>
2026-04-27 22:46:39 +0530

    fix(ext/node): emit node_trace.${rotation}.log on --trace-event-categories for node.bootstrap
    Enables tests/node_compat/runner/suite/test/parallel/test-trace-events-bootstrap.js
    
    Co-authored-by: Divy Srivastava <me@littledivy.com>

--- ext/node/polyfills/02_init.js
+++ ext/node/polyfills/02_init.js
@@ -5,7 +5,10 @@
 import { core, internals } from "ext:core/mod.js";
 const requireImpl = internals.requireImpl;
 
-import { op_stream_base_register_state } from "ext:core/ops";
+import {
+  op_node_maybe_write_trace_events_bootstrap_log,
+  op_stream_base_register_state,
+} from "ext:core/ops";
 import { nodeGlobals } from "ext:deno_node/00_globals.js";
 import { streamBaseState } from "ext:deno_node/internal_binding/stream_wrap.ts";
 import "node:module";
@@ -31,6 +34,7 @@ function initialize(args) {
     if (usesLocalNodeModulesDir) {
       requireImpl.setUsesLocalNodeModulesDir();
     }
+    op_node_maybe_write_trace_events_bootstrap_log();
 
     // FIXME(bartlomieju): not nice to depend on `Deno` namespace here
     // but it's the only way to get `args` and `version` and this point.
