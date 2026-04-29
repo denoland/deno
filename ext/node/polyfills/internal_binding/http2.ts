ba8ab7889a651b84d184e0a78ca7ad9ceb4c75a5
divybot <divybot@users.noreply.github.com>
2026-04-30 04:13:19 +0530

    fix(ext/node): expose http2 internal binding info hook for node compat tests
    Enables tests/node_compat/runner/suite/test/parallel/test-http2-info-headers-errors.js
    
    Co-authored-by: Divy Srivastava <me@littledivy.com>

---
+++ ext/node/polyfills/internal_binding/http2.ts
@@ -0,0 +1,29 @@
+// Copyright 2018-2026 the Deno authors. MIT license.
+
+import { op_http2_error_string } from "ext:core/ops";
+import * as constants from "ext:deno_node/internal/http2/constants.ts";
+
+class Http2Session {
+  request(...args) {
+    return this.request(...args);
+  }
+}
+
+class Http2Stream {
+  info(...args) {
+    return this.info(...args);
+  }
+}
+
+function nghttp2ErrorString(integerCode: number) {
+  return op_http2_error_string(integerCode);
+}
+
+export { constants, Http2Session, Http2Stream, nghttp2ErrorString };
+
+export default {
+  constants,
+  Http2Session,
+  Http2Stream,
+  nghttp2ErrorString,
+};
