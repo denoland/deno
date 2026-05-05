206d93587791df077fc81ab9fb91f70ffae393a1
Bartek Iwańczuk <biwanczuk@gmail.com>
2026-05-05 21:12:34 +0200

    fix: format assert.mjs

--- ext/node/polyfills/internal/assert.mjs
+++ ext/node/polyfills/internal/assert.mjs
@@ -1,21 +1,21 @@
 // Copyright 2018-2026 the Deno authors. MIT license.
 (function () {
-const { core } = globalThis.__bootstrap;
-const { ERR_INTERNAL_ASSERTION } = core.loadExtScript(
-  "ext:deno_node/internal/errors.ts",
-);
+  const { core } = globalThis.__bootstrap;
+  const { ERR_INTERNAL_ASSERTION } = core.loadExtScript(
+    "ext:deno_node/internal/errors.ts",
+  );
 
-function assert(value, message) {
-  if (!value) {
+  function assert(value, message) {
+    if (!value) {
+      throw new ERR_INTERNAL_ASSERTION(message);
+    }
+  }
+
+  function fail(message) {
     throw new ERR_INTERNAL_ASSERTION(message);
   }
-}
 
-function fail(message) {
-  throw new ERR_INTERNAL_ASSERTION(message);
-}
+  assert.fail = fail;
 
-assert.fail = fail;
-
-return assert;
+  return assert;
 })();
