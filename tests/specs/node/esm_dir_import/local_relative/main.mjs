1bd9e0780abb5670e8cec1874a2b1768f49566aa
divybot <divybot@users.noreply.github.com>
2026-05-15 13:52:25 +0000

    test(node): stabilize local directory import spec

--- tests/specs/node/esm_dir_import/local_relative/main.mjs
+++ tests/specs/node/esm_dir_import/local_relative/main.mjs
@@ -2,7 +2,17 @@ try {
   await import("./dir");
 } catch (err) {
   console.log(err instanceof TypeError);
-  console.log(err.code);
+  console.log(hasUnsupportedDirImportCode(err));
   console.log(String(err).includes("ERR_UNSUPPORTED_DIR_IMPORT"));
   console.log(String(err).includes("os error"));
+}
+
+function hasUnsupportedDirImportCode(err) {
+  while (err) {
+    if (err.code === "ERR_UNSUPPORTED_DIR_IMPORT") {
+      return true;
+    }
+    err = err.cause;
+  }
+  return false;
 }
