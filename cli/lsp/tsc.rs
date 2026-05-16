5f99fe693bee511d012d2c85d9da43fd5813c0e5
fibibot <fibibot@users.noreply.github.com>
2026-05-16 23:02:14 +0000

    fix(lsp): keep Node fetch out of Deno globals

--- cli/lsp/tsc.rs
+++ cli/lsp/tsc.rs
@@ -5273,7 +5273,7 @@ fn op_project_version(state: &mut OpState) -> usize {
 #[op2]
 #[serde]
 fn op_tsc_constants() -> crate::tsc::TscConstants {
-  crate::tsc::TscConstants::new()
+  crate::tsc::TscConstants::new_for_lsp()
 }
 
 struct TscRuntime {
