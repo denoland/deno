6a7409b20d348cbe22b11a46a0ebd339a3c760ac
Avocet <avocet@deno.com>
2026-06-11 11:57:54 +0000

    tools: publish benchmark JSONL files

--- tools/build_benchmark_jsons.js
+++ tools/build_benchmark_jsons.js
@@ -3,7 +3,9 @@ import { buildPath, existsSync, join } from "./util.js";
 
 const currentDataFile = join(buildPath(), "bench.json");
 const allDataFile = "gh-pages/data.json"; // Includes all benchmark data.
+const allDataJsonlFile = "gh-pages/data.jsonl"; // JSONL version of all benchmark data.
 const recentDataFile = "gh-pages/recent.json"; // Includes recent 20 benchmark data.
+const recentDataJsonlFile = "gh-pages/recent.jsonl"; // JSONL version of recent benchmark data.
 
 function readJson(filename) {
   return JSON.parse(Deno.readTextFileSync(filename));
@@ -11,6 +13,13 @@ function readJson(filename) {
 
 function writeJson(filename, data) {
   return Deno.writeTextFileSync(filename, JSON.stringify(data));
+}
+
+function writeJsonl(filename, data) {
+  return Deno.writeTextFileSync(
+    filename,
+    data.map((row) => JSON.stringify(row)).join("\n") + "\n",
+  );
 }
 
 if (!existsSync(currentDataFile)) {
@@ -28,4 +37,6 @@ const allDataLen = allData.length;
 const recentData = allData.slice(allDataLen - 20);
 
 writeJson(allDataFile, allData);
+writeJson(allDataJsonlFile, allData);
 writeJson(recentDataFile, recentData);
+writeJson(recentDataJsonlFile, recentData);
