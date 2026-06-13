1fee1db069b5a7aa616ac246819d2ee52405d696
Nathan Whitaker <nathan@deno.com>
2026-06-12 22:51:37 -0700

    fix(node): map windows udp EINVAL

--- ext/node/ops/udp.rs
+++ ext/node/ops/udp.rs
@@ -147,6 +147,8 @@ const UV_UNKNOWN: i32 = -4094;
 fn io_error_to_uv(err: &std::io::Error) -> i32 {
   match err.raw_os_error() {
     #[cfg(windows)]
+    Some(code) if code == libc::EINVAL || code == 10022 => uv_compat::UV_EINVAL,
+    #[cfg(windows)]
     Some(10040) => -4065,
     Some(code @ (40 | 90)) => -code,
     Some(code) => -code,
