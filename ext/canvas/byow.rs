2cb74bb844fe8e252bf98201183e51ea0f66c759
Dan Gieschen Knutson <19314778+Giesch@users.noreply.github.com>
2026-07-13 19:06:02 -0400

    Update ext/canvas/byow.rs
    Co-authored-by: Kenta Moriuchi <moriken@kimamass.com>
    Signed-off-by: Dan Gieschen Knutson <19314778+Giesch@users.noreply.github.com>

--- ext/canvas/byow.rs
+++ ext/canvas/byow.rs
@@ -141,11 +141,7 @@ impl UnsafeWindowSurface {
     scope: &mut v8::PinScope<'_, '_>,
     value: u32,
   ) -> Result<(), JsErrorBox> {
-    {
-      // We need to be sure to drop the borrow before using the context below
-      let mut data = self.data.borrow_mut();
-      data.height = value;
-    }
+    self.data.borrow_mut().height = value;
 
     if let Some((id, active_context)) = self.active_context.get() {
       let active_context = v8::Local::new(scope, active_context);
