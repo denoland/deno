958bef5c3d03da750cf3e5f09989a06a9a37249b
kunjrathod2005 <rathodkunj2005@gmail.com>
2026-08-03 20:46:15 -0700

    test: update desktop worker op count

--- tests/unit/ops_test.ts
+++ tests/unit/ops_test.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2026 the Deno authors. MIT license.
 
 const EXPECTED_OP_COUNT = 42;
-const EXPECTED_WORKER_OP_COUNT = 19;
+const EXPECTED_WORKER_OP_COUNT = 20;
 
 function getExposedOpNames(): string[] {
   // @ts-ignore TS doesn't allow to index with symbol
